90a9d23cfb2f727ef1e0731a89de7594f4a00cfa
emil <emil@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-05-02 18:33:48 +0000

    Remove junk
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@6 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- starttomcat.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/sh
-#cygwin translation mess
-#JAVA_CMD=$JAVA_HOME/bin/java
-#debug command line
-script=`cygpath -w /cygdrive/c/src/adsl/script/runtomcat.xml`
-# JVM_ARGS="-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8000"
-ant -buildfile $script \
-    -Dtomcathome=$TOMCAT_HOME -Djvm=$JAVA_HOME/bin/java $@ &
-    
--- stoptomcat.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/sh
-script=`cygpath -w /cygdrive/c/src/adsl/script/runtomcat.xml`
-echo $script
-$ANT_HOME/bin/ant -buildfile $script stop \
-    -Dtomcathome=$TOMCAT_HOME -Djvm=$JAVA_HOME/bin/java
