76411afc8d92ded0cafded9ce9b67257a7ccb34a
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-05-09 16:07:29 +0000

    *** empty log message ***
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@50 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/identity/provider/imp/IdentityProviderConfigManagerClient.java
@@ -0,0 +1,43 @@
+package com.l7tech.identity.provider.imp;
+
+import com.l7tech.identity.provider.IdentityProviderConfig;
+
+import java.util.Collection;
+
+/**
+ * Layer 7 Technologies, inc.
+ * User: flascelles
+ * Date: May 9, 2003
+ *
+ */
+public class IdentityProviderConfigManagerClient implements com.l7tech.identity.provider.IdentityProviderConfigManager {
+    public void delete(IdentityProviderConfig identityProviderConfig) {
+        // todo (fla) implementation
+    }
+
+    public IdentityProviderConfig create() {
+        // todo (fla) implementation
+        return null;
+    }
+
+    public Collection findByPrimaryKey(long oid) {
+        // todo (fla) implementation
+        return null;
+    }
+
+    public Collection findAll() {
+        // todo (fla) implementation
+        return null;
+    }
+
+    public Collection findAll(int offset, int windowSize) {
+        // todo (fla) implementation
+        return null;
+    }
+
+    // ************************************************
+    // PRIVATES
+    // ************************************************
+
+    // stub to the client-side of the web service
+}
