400e3815ad64998c0a69b73ddd5f73767269c316
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-05-12 21:57:46 +0000

    *** empty log message ***
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@78 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/objectmodel/imp/EntityHeaderImp.java
@@ -0,0 +1,40 @@
+package com.l7tech.objectmodel.imp;
+
+/**
+ * Layer 7 Technologies, inc.
+ * User: flascelles
+ * Date: May 12, 2003
+ *
+ */
+public class EntityHeaderImp implements com.l7tech.objectmodel.EntityHeader {
+    public Class getType() {
+        return type;
+    }
+
+    public void setType(Class type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getOid() {
+        return oid;
+    }
+
+    public void setOid(long oid) {
+        this.oid = oid;
+    }
+
+    // ************************************************
+    // PRIVATES
+    // ************************************************
+    long oid;
+    String name;
+    Class type;
+}
