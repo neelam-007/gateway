fd469d18a1ab8c03c9b4e7e7b35afb4bc879b721
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-05-13 22:22:07 +0000

    Added getMemberHeaders()
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@129 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/identity/Group.java
+++ src/com/l7tech/identity/Group.java
@@ -6,8 +6,9 @@ import java.util.Collection;
 
 public interface Group extends NamedEntity {
     String getDescription();
-    
+
     Collection getMembers();
+    Collection getMemberHeaders();
 
     void setDescription( String description );
 }
