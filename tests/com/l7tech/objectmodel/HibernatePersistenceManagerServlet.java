8c716c8614613f67bf9f5e680e68eb5f5c36aaea
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-05-13 03:52:37 +0000

    Test servlet for Hibernate Persistence
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@97 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- tests/com/l7tech/objectmodel/HibernatePersistenceManagerServlet.java
+++ tests/com/l7tech/objectmodel/HibernatePersistenceManagerServlet.java
@@ -6,13 +6,21 @@
 
 package com.l7tech.objectmodel;
 
+import com.l7tech.identity.*;
+import com.l7tech.identity.imp.IdentityProviderConfigManagerImp;
+import com.l7tech.identity.imp.IdentityProviderConfigImp;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collection;
 
 /**
  * @author alex
  */
-public class HibernatePersistenceManagerServlet extends javax.servlet.http.HttpServlet {
+public class HibernatePersistenceManagerServlet extends HttpServlet {
     public void init( ServletConfig config ) throws ServletException {
         super.init( config );
         try {
@@ -22,4 +30,30 @@ public class HibernatePersistenceManagerServlet extends javax.servlet.http.HttpS
             throw new ServletException( e );
         }
     }
+
+    public void doGet( HttpServletRequest request, HttpServletResponse response ) throws IOException, ServletException {
+        response.setContentType( "text/html" );
+        PrintWriter out = response.getWriter();
+
+        IdentityProviderConfigManager ipcm = new IdentityProviderConfigManagerImp();
+
+        String op = request.getParameter("op");
+        if ( op.equals("list") ) {
+            Collection c = ipcm.findAll();
+            out.println( c );
+        } else if ( op.equals( "get") ) {
+            String soid = request.getParameter("oid");
+            long oid = Long.parseLong( soid );
+            IdentityProviderConfig config = ipcm.findByPrimaryKey( oid );
+            out.println( config );
+        } else if ( op.equals( "create") ) {
+            IdentityProviderConfig config = new IdentityProviderConfigImp();
+            config.setName("Identity Provider #1");
+            config.setDescription("This object is bogus.");
+            long oid = ipcm.save( config );
+            out.println( "Saved " + oid );
+        }
+        out.close();
+    }
+
 }
