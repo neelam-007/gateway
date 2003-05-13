844c4b063942962d79432979849aef1ae21c8d42
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-05-13 18:31:17 +0000

    Initial checkin.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@111 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/objectmodel/TransactionException.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2003 Layer 7 Technologies Inc.
+ *
+ * $Id$
+ */
+
+package com.l7tech.objectmodel;
+
+/**
+ * @author alex
+ * @version $Revision$
+ */
+public class TransactionException extends ObjectModelException {
+    public TransactionException() {
+        super();
+    }
+
+    public TransactionException( String message ) {
+        super( message );
+    }
+
+    public TransactionException( String message, Throwable cause ) {
+        super( message, cause );
+    }
+}
