be021d1e9f0575f6854e8c8ab0ac8741515332d6
emil <emil@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-05-13 05:56:09 +0000

    Added properties based test.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@98 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- tests/com/l7tech/util/locator/AbstractLocatorTest.java
+++ tests/com/l7tech/util/locator/AbstractLocatorTest.java
@@ -1,14 +1,19 @@
 package com.l7tech.util.locator;
 
+import com.l7tech.util.Locator;
+import com.l7tech.identity.IdentityProviderConfigManager;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+
+import java.util.logging.Logger;
 
 /**
  * Class AbstractLocatorTest.
  * @author <a href="mailto:emarceta@layer7-tech.com>Emil Marceta</a>
  */
 public class AbstractLocatorTest extends TestCase {
+    static Logger logger = Logger.getLogger(AbstractLocatorTest.class.getName());
     /**
      * test <code>AbstractLocatorTest</code> constructor
      */
@@ -43,11 +48,33 @@ public class AbstractLocatorTest extends TestCase {
 
         Object result2 = locator.lookup(String.class);
         assertTrue(result == result2);
+    }
+
+    public void testPropertiesLocator() throws Exception {
+        LocatorStub.recycle();
+        System.setProperty("com.l7tech.util.locator.properties",
+                           "/com/l7tech/console/resources/services.properties");
+        logger.info(Locator.getDefault().toString());
+        Object result =
+          Locator.getDefault().lookup(IdentityProviderConfigManager.class);
+        if (result == null) {
+            fail("Exptected successfull lookup.");
+        }
 
     }
 
+    /**
+     * The test locator class
+     */
     static class TesLocator extends AbstractLocator {
     }
+
+    static abstract class LocatorStub extends Locator {
+        protected static void recycle() {
+            Locator.recycle();
+        }
+    }
+
 
     /**
      * Test <code>AbstractLocatorTest</code> main.
