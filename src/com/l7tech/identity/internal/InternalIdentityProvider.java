b2052d781c1ad5fc3aab81d4a797446b28b8e752
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-05-14 17:38:35 +0000

    turned this into an interface
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@158 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/identity/internal/InternalIdentityProvider.java
+++ src/com/l7tech/identity/internal/InternalIdentityProvider.java
@@ -6,31 +6,5 @@ import com.l7tech.objectmodel.PersistenceManager;
 /**
  * @author alex
  */
-public class InternalIdentityProvider implements IdentityProvider {
-    public InternalIdentityProvider() {
-    }
-
-    public void initialize( IdentityProviderConfig config ) {
-        _identityProviderConfig = config;
-        _userManager = (UserManager)PersistenceManager.getEntityManager( User.class );
-        _groupManager = (GroupManager)PersistenceManager.getEntityManager( Group.class );
-        _userManager.setIdentityProviderOid( config.getOid() );
-        _groupManager.setIdentityProviderOid( config.getOid() );
-    }
-
-    public UserManager getUserManager() {
-        return _userManager;
-    }
-
-    public GroupManager getGroupManager() {
-        return _groupManager;
-    }
-
-    public boolean authenticate(User user, Object credential) {
-        return false;
-    }
-
-    private IdentityProviderConfig _identityProviderConfig;
-    private UserManager _userManager;
-    private GroupManager _groupManager;
+public interface InternalIdentityProvider extends IdentityProvider {
 }
