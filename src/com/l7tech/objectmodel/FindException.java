dc374e635d686f0aa63cee0a7298d4281c362b61
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-05-14 15:37:28 +0000

    implementation
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@145 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/objectmodel/FindException.java
+++ src/com/l7tech/objectmodel/FindException.java
@@ -10,4 +10,15 @@ package com.l7tech.objectmodel;
  * @author alex
  */
 public class FindException extends ObjectModelException {
+    public FindException() {
+        super();
+    }
+
+    public FindException( String message ) {
+        super( message );
+    }
+
+    public FindException( String message, Throwable cause ) {
+        super( message, cause );
+    }
 }
