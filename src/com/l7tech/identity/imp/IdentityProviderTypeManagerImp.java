2cd31590443f71fc31fb5e9f38fb98a634343119
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-05-14 18:07:36 +0000

    Initial checkin.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@167 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/identity/imp/IdentityProviderTypeManagerImp.java
@@ -0,0 +1,37 @@
+package com.l7tech.identity.imp;
+
+import com.l7tech.identity.*;
+import com.l7tech.objectmodel.*;
+
+import java.util.*;
+import java.sql.SQLException;
+
+/**
+ * @author alex
+ */
+public class IdentityProviderTypeManagerImp extends HibernateEntityManager implements IdentityProviderTypeManager {
+    public IdentityProviderTypeManagerImp( PersistenceContext context ) {
+        super(context);
+    }
+
+    public void delete(IdentityProviderType identityProviderType) throws DeleteException {
+        _manager.delete( _context, identityProviderType );
+    }
+
+    public IdentityProviderType findByPrimaryKey( long oid ) throws FindException {
+        return (IdentityProviderType)_manager.findByPrimaryKey( _context, IdentityProviderType.class, oid );
+    }
+
+    public long save( IdentityProviderType identityProviderType ) throws SaveException {
+        return _manager.save( _context, identityProviderType );
+    }
+
+    public Collection findAll() throws FindException {
+        return _manager.find( _context, "from identity_provider_type in class com.l7tech.identity.imp.IdentityProviderTypeImp" );
+    }
+
+    public Collection findAll(int offset, int windowSize) throws FindException {
+        return null;
+    }
+
+}
