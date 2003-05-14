dcbf40021eb5c8d7293203d3396a21a11d3aa42d
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-05-14 15:51:40 +0000

    implementation (fla)
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@146 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/objectmodel/DeleteException.java
+++ src/com/l7tech/objectmodel/DeleteException.java
@@ -10,4 +10,15 @@ package com.l7tech.objectmodel;
  * @author alex
  */
 public class DeleteException extends ObjectModelException {
+    public DeleteException() {
+        super();
+    }
+
+    public DeleteException( String message ) {
+        super( message );
+    }
+
+    public DeleteException( String message, Throwable cause ) {
+        super( message, cause );
+    }
 }
