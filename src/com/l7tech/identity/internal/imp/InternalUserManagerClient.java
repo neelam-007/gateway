7d46efefae1eee45fda37788a4813bb91d426158
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-05-14 16:56:06 +0000

    implementation
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@154 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/identity/internal/imp/InternalUserManagerClient.java
+++ src/com/l7tech/identity/internal/imp/InternalUserManagerClient.java
@@ -4,6 +4,10 @@ import com.l7tech.identity.User;
 import com.l7tech.objectmodel.FindException;
 import com.l7tech.objectmodel.DeleteException;
 import com.l7tech.objectmodel.SaveException;
+import com.l7tech.adminws.identity.Identity;
+import com.l7tech.adminws.identity.IdentityService;
+import com.l7tech.adminws.identity.IdentityServiceLocator;
+import com.l7tech.adminws.translation.TypeTranslator;
 
 import java.util.Collection;
 
@@ -14,33 +18,84 @@ import java.util.Collection;
  *
  */
 public class InternalUserManagerClient implements com.l7tech.identity.UserManager {
+
+    public InternalUserManagerClient(long identityProviderConfigId){
+        this.identityProviderConfigId = identityProviderConfigId;
+    }
+
     public User findByPrimaryKey(long oid) throws FindException {
-        return null;
+        try {
+            return TypeTranslator.serviceUserToGenUser(getStub().findUserByPrimaryKey(identityProviderConfigId, oid));
+        } catch (java.rmi.RemoteException e) {
+            throw new FindException("RemoteException in findByPrimaryKey", e);
+        }
     }
 
     public void delete(User user) throws DeleteException {
+        try {
+            getStub().deleteUser(identityProviderConfigId, user.getOid());
+        } catch (java.rmi.RemoteException e) {
+            throw new DeleteException("RemoteException in delete", e);
+        }
     }
 
     public long save(User user) throws SaveException {
-        return 0;
+        try {
+            return getStub().saveUser(identityProviderConfigId, TypeTranslator.genUserToServiceUser(user));
+        } catch (java.rmi.RemoteException e) {
+            throw new SaveException("RemoteException in save", e);
+        }
     }
 
     public void setIdentityProviderOid(long oid) {
+        // what should i do with this?
     }
 
     public Collection findAllHeaders() throws FindException {
-        return null;
+        try {
+            return TypeTranslator.headerArrayToCollection(getStub().findAllUsers(identityProviderConfigId));
+        } catch (java.rmi.RemoteException e) {
+            throw new FindException("RemoteException in findAllHeaders", e);
+        }
     }
 
     public Collection findAllHeaders(int offset, int windowSize) throws FindException {
-        return null;
+        try {
+            return TypeTranslator.headerArrayToCollection(getStub().findAllUsersByOffset(identityProviderConfigId, offset, windowSize));
+        } catch (java.rmi.RemoteException e) {
+            throw new FindException("RemoteException in findAllHeaders", e);
+        }
     }
 
     public Collection findAll() throws FindException {
-        return null;
+        throw new FindException("Operation not supported in this manager implementation");
     }
 
     public Collection findAll(int offset, int windowSize) throws FindException {
-        return null;
+        throw new FindException("Operation not supported in this manager implementation");
     }
+
+    // ************************************************
+    // PRIVATES
+    // ************************************************
+    private Identity getStub() throws java.rmi.RemoteException {
+        if (localStub == null) {
+            IdentityService service = new IdentityServiceLocator();
+            try {
+                localStub = service.getidentities(new java.net.URL(getServiceURL()));
+            }
+            catch (Exception e) {
+                throw new java.rmi.RemoteException("cannot instantiate the admin service stub", e);
+            }
+        }
+        return localStub;
+    }
+    private String getServiceURL() {
+        // todo, read this url from a properties file
+        // maybe com.l7tech.console.util.Preferences
+        return "http://localhost:8080/UneasyRooster/services/identities";
+    }
+
+    private long identityProviderConfigId;
+    private Identity localStub = null;
 }
