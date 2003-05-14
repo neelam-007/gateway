a0dcb13308fd0544258afcf77706eb27e8a58175
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-05-14 18:08:50 +0000

    Compiles cleanly
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@169 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- tests/com/l7tech/identity/TestIdentityProviderManager.java
+++ tests/com/l7tech/identity/TestIdentityProviderManager.java
@@ -3,6 +3,8 @@ package com.l7tech.identity;
 import junit.framework.TestCase;
 import com.l7tech.identity.IdentityProviderConfigManager;
 import com.l7tech.identity.imp.IdentityProviderConfigManagerImp;
+import com.l7tech.objectmodel.PersistenceManager;
+import com.l7tech.objectmodel.PersistenceContext;
 
 import java.sql.SQLException;
 
@@ -13,12 +15,10 @@ public class TestIdentityProviderManager extends TestCase {
     public static void main( String[] args ) {
     }
 
-    public void setUp() {
-
-        try {
-            IdentityProviderConfigManager ipm = new IdentityProviderConfigManagerImp();
-        } catch ( SQLException se ) {
-            fail( se.toString() );
-        }
+    public void setUp() throws Exception {
+        PersistenceContext context = PersistenceManager.getContext();
+        _ipm = new IdentityProviderConfigManagerImp( context );
     }
+
+    private IdentityProviderConfigManager _ipm;
 }
