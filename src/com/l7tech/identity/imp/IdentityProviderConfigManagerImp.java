bc6e800b9f84c55c0dd8a14a65a98a4e48443088
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-05-14 00:14:32 +0000

    Added context stuff
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@139 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/identity/imp/IdentityProviderConfigManagerImp.java
+++ src/com/l7tech/identity/imp/IdentityProviderConfigManagerImp.java
@@ -10,12 +10,26 @@ import java.sql.SQLException;
  * @author alex
  */
 public class IdentityProviderConfigManagerImp extends HibernateEntityManager implements IdentityProviderConfigManager {
-    public IdentityProviderConfigManagerImp() {
+    /**
+     * Constructs a new <code>IdentityProviderConfigManagerImp</code> with a default PersistenceContext.  You get your own context and call the other constructor if you want to execute multiple operations in a transaction.
+     * @throws SQLException
+     */
+    public IdentityProviderConfigManagerImp() throws SQLException {
         _manager = PersistenceManager.getInstance();
+        _context = _manager.getContext();
+    }
+
+    /**
+     * Constructs a new <code>IdentityProviderConfigManagerImp</code> with a specific context.
+     * @param context
+     */
+    public IdentityProviderConfigManagerImp( PersistenceContext context ) {
+        _manager = PersistenceManager.getInstance();
+        _context = context;
     }
 
     public void delete(IdentityProviderConfig identityProviderConfig) throws DeleteException {
-        _manager.delete( identityProviderConfig );
+        _manager.delete( _context, identityProviderConfig );
     }
 
     public IdentityProviderConfig findByPrimaryKey( long oid ) throws FindException {
@@ -23,15 +37,11 @@ public class IdentityProviderConfigManagerImp extends HibernateEntityManager imp
     }
 
     public long save( IdentityProviderConfig identityProviderConfig ) throws SaveException {
-        try {
-            return _manager.save( identityProviderConfig );
-        } catch ( SQLException se ) {
-            throw new SaveException( se.toString(), se );
-        }
+        return _manager.save( _context, identityProviderConfig );
     }
 
     public Collection findAll() throws FindException {
-        return _manager.find( "select * from identity-provider", "hello", String.class );
+        return _manager.find( _context, "select * from identity-provider", "hello", String.class );
     }
 
     public Collection findAll(int offset, int windowSize) throws FindException {
@@ -39,4 +49,5 @@ public class IdentityProviderConfigManagerImp extends HibernateEntityManager imp
     }
 
     PersistenceManager _manager;
+    PersistenceContext _context;
 }
