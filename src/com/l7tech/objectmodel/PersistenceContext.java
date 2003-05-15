12163ec4e6db43ddfe028095374b5c9fc8a7f304
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-05-15 00:11:51 +0000

    Added transaction method
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@190 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/objectmodel/PersistenceContext.java
+++ src/com/l7tech/objectmodel/PersistenceContext.java
@@ -1,7 +1,12 @@
 package com.l7tech.objectmodel;
 
+import javax.transaction.UserTransaction;
+
 /**
  * @author alex
  */
 public interface PersistenceContext {
+    void beginTransaction() throws TransactionException;
+    void commitTransaction() throws TransactionException;
+    void rollbackTransaction() throws TransactionException;
 }
