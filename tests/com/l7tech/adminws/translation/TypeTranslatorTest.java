c83cb1fd260214462149c3e242fc6cea7e08225c
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-05-15 18:11:22 +0000

    more testing
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@209 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- tests/com/l7tech/adminws/translation/TypeTranslatorTest.java
+++ tests/com/l7tech/adminws/translation/TypeTranslatorTest.java
@@ -37,9 +37,9 @@ public class TypeTranslatorTest extends TestCase {
         headerArray[0].setOid(0);
         headerArray[1].setOid(1);
         headerArray[2].setOid(2);
-        headerArray[0].setType("com.l7tech.adminws.translation.TypeTranslatorTest");
-        headerArray[1].setType("com.l7tech.adminws.translation.TypeTranslatorTest");
-        headerArray[2].setType("com.l7tech.adminws.translation.TypeTranslatorTest");
+        headerArray[0].setType(getClass().getName());
+        headerArray[1].setType(getClass().getName());
+        headerArray[2].setType(getClass().getName());
         java.util.Collection res = TypeTranslator.headerArrayToCollection(headerArray);
         assertTrue("getting a result", res != null);
         assertTrue("the size is right", res.size() == 3);
@@ -62,12 +62,63 @@ public class TypeTranslatorTest extends TestCase {
         assertTrue("result type is valid", header.getType() == getClass());
         assertTrue("result name is valid", header.getName().equals("two"));
         assertTrue("result oid is valid", header.getOid() == 2);
+        res = TypeTranslator.headerArrayToCollection(null);
+        assertTrue("getting a result when passing null", res != null);
+        res = TypeTranslator.headerArrayToCollection(new com.l7tech.adminws.identity.Header[0]);
+        assertTrue("getting a result when passing empty array", res != null);
     }
 
-    public void collectionToServiceHeaders() {
+    public void testCollectionToServiceHeaders() throws Exception {
+        com.l7tech.adminws.identity.Header[] headerArray = new com.l7tech.adminws.identity.Header[3];
+        headerArray[0] = new com.l7tech.adminws.identity.Header();
+        headerArray[1] = new com.l7tech.adminws.identity.Header();
+        headerArray[2] = new com.l7tech.adminws.identity.Header();
+        headerArray[0].setName("zero");
+        headerArray[1].setName("one");
+        headerArray[2].setName("two");
+        headerArray[0].setOid(0);
+        headerArray[1].setOid(1);
+        headerArray[2].setOid(2);
+        headerArray[0].setType(getClass().getName());
+        headerArray[1].setType(getClass().getName());
+        headerArray[2].setType(getClass().getName());
+        java.util.Collection tmpres = TypeTranslator.headerArrayToCollection(headerArray);
+        com.l7tech.adminws.identity.Header[] res = TypeTranslator.collectionToServiceHeaders(tmpres);
+        assertTrue("getting a result", res != null);
+        for (int i = 0; i < 2; i++)
+        {
+            assertTrue("name integrity", res[i].getName().equals(headerArray[i].getName()));
+            assertTrue("oid integrity", res[i].getOid() == headerArray[i].getOid());
+            assertTrue("type integrity", res[i].getType().equals(headerArray[i].getType()));
+        }
     }
 
-    public void genericToServiceIdProviderConfig() {
+    public void testGenericToServiceIdProviderConfig() throws Exception {
+        com.l7tech.identity.IdentityProviderConfig genConfig = new com.l7tech.identity.imp.IdentityProviderConfigImp();
+        com.l7tech.adminws.identity.IdentityProviderConfig res = TypeTranslator.genericToServiceIdProviderConfig(genConfig);
+        assertTrue("getting a result when nothing is set", res != null);
+        genConfig.setDescription("description");
+        genConfig.setName("name");
+        genConfig.setOid(123);
+        res = TypeTranslator.genericToServiceIdProviderConfig(genConfig);
+        assertTrue("getting a result when no type is set", res != null);
+        com.l7tech.identity.IdentityProviderType configType = new com.l7tech.identity.imp.IdentityProviderTypeImp();
+        genConfig.setType(configType);
+        res = TypeTranslator.genericToServiceIdProviderConfig(genConfig);
+        assertTrue("getting a result with empty type", res != null);
+        configType.setClassName(getClass().getName());
+        configType.setDescription("description");
+        configType.setName("name");
+        configType.setOid(123);
+        res = TypeTranslator.genericToServiceIdProviderConfig(genConfig);
+        assertTrue("getting a result", res != null);
+        assertTrue("name integrity", res.getName().equals(genConfig.getName()));
+        assertTrue("oid integrity", res.getOid() == genConfig.getOid());
+        assertTrue("description integrity", res.getDescription().equals(genConfig.getDescription()));
+        assertTrue("type integrity", res.getTypeClassName().equals(configType.getClassName()));
+        assertTrue("type desc integrity", res.getTypeDescription().equals(configType.getDescription()));
+        assertTrue("type name integrity", res.getTypeName().equals(configType.getName()));
+        assertTrue("type oid integrity", res.getTypeOid() == configType.getOid());
     }
 
     public void serviceIdentityProviderConfigToGenericOne() {
