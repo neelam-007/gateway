b3295852d596dc1e0c757e1332c681d6cdc57d3f
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-05-15 17:33:20 +0000

    *** empty log message ***
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@205 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- tests/com/l7tech/adminws/translation/TypeTranslatorTest.java
+++ tests/com/l7tech/adminws/translation/TypeTranslatorTest.java
@@ -2,6 +2,10 @@ package com.l7tech.adminws.translation;
 
 import junit.framework.TestCase;
 
+import java.util.Iterator;
+
+import com.l7tech.objectmodel.EntityHeader;
+
 /**
  * Layer 7 Technologies, inc.
  * User: flascelles
@@ -22,7 +26,42 @@ public class TypeTranslatorTest extends TestCase {
         assertTrue("result oid is valid", res.getOid() == 123);
     }
 
-    public void headerArrayToCollection() {
+    public void testHeaderArrayToCollection() throws Exception {
+        com.l7tech.adminws.identity.Header[] headerArray = new com.l7tech.adminws.identity.Header[3];
+        headerArray[0] = new com.l7tech.adminws.identity.Header();
+        headerArray[1] = new com.l7tech.adminws.identity.Header();
+        headerArray[2] = new com.l7tech.adminws.identity.Header();
+        headerArray[0].setName("zero");
+        headerArray[1].setName("one");
+        headerArray[2].setName("two");
+        headerArray[0].setOid(0);
+        headerArray[1].setOid(1);
+        headerArray[2].setOid(2);
+        headerArray[0].setType("com.l7tech.adminws.translation.TypeTranslatorTest");
+        headerArray[1].setType("com.l7tech.adminws.translation.TypeTranslatorTest");
+        headerArray[2].setType("com.l7tech.adminws.translation.TypeTranslatorTest");
+        java.util.Collection res = TypeTranslator.headerArrayToCollection(headerArray);
+        assertTrue("getting a result", res != null);
+        assertTrue("the size is right", res.size() == 3);
+        Iterator iter = res.iterator();
+        iter.hasNext();
+        EntityHeader header = (EntityHeader)iter.next();
+        assertTrue("getting a result", header != null);
+        assertTrue("result type is valid", header.getType() == getClass());
+        assertTrue("result name is valid", header.getName().equals("zero"));
+        assertTrue("result oid is valid", header.getOid() == 0);
+        iter.hasNext();
+        header = (EntityHeader)iter.next();
+        assertTrue("getting a result", header != null);
+        assertTrue("result type is valid", header.getType() == getClass());
+        assertTrue("result name is valid", header.getName().equals("one"));
+        assertTrue("result oid is valid", header.getOid() == 1);
+        iter.hasNext();
+        header = (EntityHeader)iter.next();
+        assertTrue("getting a result", header != null);
+        assertTrue("result type is valid", header.getType() == getClass());
+        assertTrue("result name is valid", header.getName().equals("two"));
+        assertTrue("result oid is valid", header.getOid() == 2);
     }
 
     public void collectionToServiceHeaders() {
