4b9d654cd4ce4f3e9c1b80d618f384fb27bcb681
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-05-15 01:07:45 +0000

    Initial checkin./
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@197 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/identity/internal/imp/UserManagerImp.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2003 Layer 7 Technologies Inc.
+ *
+ * $Id$
+ */
+
+package com.l7tech.identity.internal.imp;
+
+import com.l7tech.identity.UserManager;
+import com.l7tech.identity.User;
+import com.l7tech.objectmodel.*;
+
+import java.util.Collection;
+
+/**
+ * @author alex
+ */
+public class UserManagerImp extends HibernateEntityManager implements UserManager {
+    public static final Class IMPCLASS = UserImp.class;
+
+    public UserManagerImp( PersistenceContext context ) {
+        super( context );
+    }
+
+    public User findByPrimaryKey(long oid) throws FindException {
+        return (User)_manager.findByPrimaryKey( _context, IMPCLASS, oid );
+    }
+
+    public void delete(User user) throws DeleteException {
+        _manager.delete( _context, user );
+    }
+
+    public long save(User user) throws SaveException {
+        return _manager.save( _context, user );
+    }
+
+    public void setIdentityProviderOid(long oid) {
+        _identityProviderOid = oid;
+    }
+
+    public Collection findAll() throws FindException {
+        return _manager.find( _context, "from user in class com.l7tech.identity.imp.UserImp");
+    }
+
+    public Collection findAll(int offset, int windowSize) throws FindException {
+        throw new IllegalArgumentException( "Not yet implemented!" );
+    }
+
+    public long _identityProviderOid;
+}
