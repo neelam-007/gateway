2f9adfcdc19b3b047c59bd83d6e022898d5006dc
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-05-15 17:28:01 +0000

    Implemented.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@204 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/identity/internal/imp/GroupManagerImp.java
+++ src/com/l7tech/identity/internal/imp/GroupManagerImp.java
@@ -6,8 +6,7 @@
 
 package com.l7tech.identity.internal.imp;
 
-import com.l7tech.identity.UserManager;
-import com.l7tech.identity.User;
+import com.l7tech.identity.*;
 import com.l7tech.objectmodel.*;
 
 import java.util.Collection;
@@ -15,23 +14,23 @@ import java.util.Collection;
 /**
  * @author alex
  */
-public class UserManagerImp extends HibernateEntityManager implements UserManager {
-    public static final Class IMPCLASS = UserImp.class;
+public class GroupManagerImp extends HibernateEntityManager implements GroupManager {
+    public static final Class IMPCLASS = GroupImp.class;
 
-    public UserManagerImp( PersistenceContext context ) {
+    public GroupManagerImp( PersistenceContext context ) {
         super( context );
     }
 
-    public User findByPrimaryKey(long oid) throws FindException {
-        return (User)_manager.findByPrimaryKey( _context, IMPCLASS, oid );
+    public Group findByPrimaryKey(long oid) throws FindException {
+        return (Group)_manager.findByPrimaryKey( _context, IMPCLASS, oid );
     }
 
-    public void delete(User user) throws DeleteException {
-        _manager.delete( _context, user );
+    public void delete(Group group) throws DeleteException {
+        _manager.delete( _context, group );
     }
 
-    public long save(User user) throws SaveException {
-        return _manager.save( _context, user );
+    public long save(Group group) throws SaveException {
+        return _manager.save( _context, group );
     }
 
     public void setIdentityProviderOid(long oid) {
@@ -39,7 +38,7 @@ public class UserManagerImp extends HibernateEntityManager implements UserManage
     }
 
     public Collection findAll() throws FindException {
-        String query ="from user in class com.l7tech.identity.imp.UserImp";
+        String query ="from group in class com.l7tech.identity.imp.GroupImp";
         if ( _identityProviderOid == -1 )
             throw new FindException( "Can't call findAll() without first calling setIdentityProviderOid!" );
         else
