9692fa91867df8db3dcdb75283c37a1e78280b6b
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-05-20 18:56:35 +0000

    *** empty log message ***
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@256 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- tests/com/l7tech/adminws/TestIdentity.java
+++ tests/com/l7tech/adminws/TestIdentity.java
@@ -6,6 +6,8 @@ import com.l7tech.adminws.identity.Identity;
 import com.l7tech.adminws.identity.Header;
 import com.l7tech.identity.imp.IdentityProviderConfigManagerClient;
 import com.l7tech.identity.IdentityProviderConfig;
+import com.l7tech.identity.IdentityProviderConfigManager;
+import com.l7tech.util.Locator;
 
 import java.util.Collection;
 import java.util.Iterator;
@@ -19,9 +21,26 @@ import java.util.Iterator;
  */
 public class TestIdentity {
     public static void main(String[] args) throws Exception {
+
+        /*
+        testLocator2();
+
         IdentityService service = new IdentityServiceLocator();
-        Identity servicePort = service.getidentities(new java.net.URL("http://localhost:8080/UneasyRooster/services/identities"));
-        //Identity servicePort = service.getidentities(new java.net.URL("http://localhost:8080/ssg/services/identities"));
+        Identity servicePort = service.getidentities(new java.net.URL("http://localhost:8080/ssg/services/identities"));
+        com.l7tech.adminws.identity.IdentityProviderConfig svcConfig = new com.l7tech.adminws.identity.IdentityProviderConfig();
+        svcConfig.setDescription("description");
+        svcConfig.setName("name");
+        svcConfig.setOid(123);
+        svcConfig.setTypeClassName("type class name");
+        svcConfig.setTypeDescription("type description");
+        svcConfig.setTypeName("type name");
+        svcConfig.setTypeOid(456);
+        long res = servicePort.saveIdentityProviderConfig(svcConfig);
+        System.out.println(res);
+        */
+
+        IdentityService service = new IdentityServiceLocator();
+        Identity servicePort = service.getidentities(new java.net.URL("http://localhost:8080/ssg/services/identities"));
         Header[] res = servicePort.findAlllIdentityProviderConfig();
         printres(res);
 
@@ -31,6 +50,7 @@ public class TestIdentity {
         printCollection(col);
         */
     }
+
     public static void printres(Header[] res) {
         for (int i = 0; i < res.length; i++) {
             System.out.println(res[i].getName() + " - " + res[i].getType());
@@ -42,4 +62,44 @@ public class TestIdentity {
             System.out.print(iter.next());
         }
     }
+
+    public static void testLocator() throws Exception {
+        //if (identityProviderConfigManager == null){
+            // instantiate the server-side manager
+            IdentityProviderConfigManager identityProviderConfigManager = (IdentityProviderConfigManager)Locator.getDefault().lookup(com.l7tech.identity.IdentityProviderConfigManager.class);
+            if (identityProviderConfigManager == null) throw new java.rmi.RemoteException("Cannot instantiate the IdentityProviderConfigManager");
+        //}
+        //return identityProviderConfigManager;
+    }
+
+    public static void testLocator2() throws Exception {
+        /*
+        com.l7tech.adminws.identity.IdentityProviderConfig svcConfig = new com.l7tech.adminws.identity.IdentityProviderConfig();
+        svcConfig.setDescription("description");
+        svcConfig.setName("name");
+        svcConfig.setOid(123);
+        svcConfig.setTypeClassName("type class name");
+        svcConfig.setTypeDescription("type description");
+        svcConfig.setTypeName("type name");
+        svcConfig.setTypeOid(456);
+        com.l7tech.adminws.identity.IdentitiesSoapBindingImpl service = new com.l7tech.adminws.identity.IdentitiesSoapBindingImpl();
+        service.saveIdentityProviderConfig(svcConfig);
+        */
+
+        IdentityProviderConfigManagerClient manager = new IdentityProviderConfigManagerClient();
+
+        com.l7tech.identity.IdentityProviderConfig genConfig = new com.l7tech.identity.imp.IdentityProviderConfigImp();
+        genConfig.setDescription("description");
+        genConfig.setName("name");
+        genConfig.setOid(123);
+        com.l7tech.identity.IdentityProviderType configType = new com.l7tech.identity.imp.IdentityProviderTypeImp();
+        configType.setClassName("blah");
+        configType.setDescription("description");
+        configType.setName("name");
+        configType.setOid(123);
+        genConfig.setType(configType);
+
+        manager.save(genConfig);
+    }
+
 }
