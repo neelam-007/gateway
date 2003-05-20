52a3ea05ccfe096c510a1e797f872b221c9c2a0a
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-05-20 20:53:46 +0000

    Moved here
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@261 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/server/HibernatePersistenceManagerServlet.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2003 Layer 7 Technologies Inc.
+ *
+ * $Id$
+ */
+
+package com.l7tech.server;
+
+import com.l7tech.identity.*;
+import com.l7tech.identity.imp.*;
+import com.l7tech.objectmodel.PersistenceContext;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.Random;
+
+/**
+ * @author alex
+ */
+public class HibernatePersistenceManagerServlet extends HttpServlet {
+    public void init( ServletConfig config ) throws ServletException {
+        super.init( config );
+    }
+
+    public void doGet( HttpServletRequest request, HttpServletResponse response ) throws IOException, ServletException {
+        response.setContentType( "text/html" );
+        PrintWriter out = response.getWriter();
+
+        IdentityProviderConfigManager ipcm;
+        IdentityProviderTypeManager iptm;
+        try {
+            PersistenceContext.getCurrent().beginTransaction();
+            ipcm = new IdentityProviderConfigManagerImp();
+            iptm = new IdentityProviderTypeManagerImp();
+        } catch ( Exception e ) {
+            throw new ServletException( e );
+        }
+
+        try {
+            String op = request.getParameter("op");
+
+            if ( "list".equals(op) ) {
+                Iterator i = ipcm.findAll().iterator();
+                if ( !i.hasNext() ) {
+                    out.println( "None!" );
+                } else {
+                    IdentityProviderConfig config;
+                    while ( i.hasNext() ) {
+                        config = (IdentityProviderConfig)i.next();
+                        out.println( config.getOid() );
+                        out.println( config.getName() );
+                    }
+                }
+            } else if ( "get".equals(op) ) {
+                String soid = request.getParameter("oid");
+                long oid = Long.parseLong( soid );
+                IdentityProviderConfig config = ipcm.findByPrimaryKey( oid );
+                out.println( config );
+            } else if ( "delete".equals(op) ) {
+                String soid = request.getParameter("oid");
+                long oid = Long.parseLong( soid );
+                IdentityProviderConfig config = ipcm.findByPrimaryKey( oid );
+                if ( config == null )
+                    out.println( "Couldn't find " + oid );
+                else {
+                    ipcm.delete(config);
+
+                    out.println( oid + " deleted." );
+                }
+            } else if ( "create".equals(op) ) {
+                IdentityProviderType type = new IdentityProviderTypeImp();
+                type.setClassName( "com.l7tech.identity.internal.InternalIdentityProvider" );
+                type.setName( "Internal Identity Provider" );
+                iptm.save(type);
+
+                IdentityProviderConfig config = new IdentityProviderConfigImp();
+                config.setName( new Integer( new Random().nextInt() ).toString() );
+                config.setDescription("This object is bogus.");
+                config.setType( type );
+
+                long oid = ipcm.save( config );
+                config.setName( "Identity Provider #" + oid );
+                ipcm.update( config );
+
+                out.println( "Saved " + oid );
+            } else {
+                out.println( "<b>The <tt>op</tt> parameter must be one of { get, create, delete, list }</b>" );
+            }
+        } catch ( Exception e ) {
+            throw new ServletException( e );
+        } finally {
+            try {
+                String rollback = request.getParameter("rollback");
+                if ( "true".equals( rollback ) )
+                    PersistenceContext.getCurrent().rollbackTransaction();
+                else {
+                    PersistenceContext.getCurrent().commitTransaction();
+                }
+            } catch ( Exception te ) {
+                throw new ServletException( te );
+            }
+            out.close();
+        }
+    }
+
+}
