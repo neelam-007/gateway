ffcab0cd9cb432697dc612dc076bd5c2d2f5f0e2
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-05-22 18:23:52 +0000

    Removed UserTransaction stuff.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@290 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- tests/com/l7tech/objectmodel/HibernatePersistenceContextStub.java
+++ tests/com/l7tech/objectmodel/HibernatePersistenceContextStub.java
@@ -8,9 +8,6 @@ package com.l7tech.objectmodel;
 
 import cirrus.hibernate.Session;
 
-import javax.transaction.UserTransaction;
-import javax.transaction.Status;
-
 /**
  * @author alex
  */
@@ -21,34 +18,5 @@ public class HibernatePersistenceContextStub extends HibernatePersistenceContext
 
     public HibernatePersistenceContextStub( Session session ) {
         super( session );
-    }
-
-    private class UserTransactionStub implements UserTransaction {
-        public void rollback() {
-            _status = Status.STATUS_ROLLING_BACK;
-        }
-        public void commit() {
-            _status = Status.STATUS_COMMITTING;
-        }
-        public void begin() {
-            _status = Status.STATUS_ACTIVE;
-        }
-        public void setRollbackOnly() {
-            _status = Status.STATUS_MARKED_ROLLBACK;
-        }
-
-        public void setTransactionTimeout( int timeout ) { }
-        public int getStatus() {
-            return _status;
-        }
-        private int _status = Status.STATUS_NO_TRANSACTION;
-    }
-
-    protected UserTransaction getUserTransaction() throws TransactionException {
-        try {
-            return new UserTransactionStub();
-        } catch ( Exception e ) {
-            throw new TransactionException( e.toString(), e );
-        }
     }
 }
