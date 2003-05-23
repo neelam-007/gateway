07ee43994de18b8543f60b361aee98afe57c0762
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-05-23 01:34:57 +0000

    *** empty log message ***
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@311 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- tests/com/l7tech/adminws/TestIdentity.java
+++ tests/com/l7tech/adminws/TestIdentity.java
@@ -5,8 +5,7 @@ import com.l7tech.adminws.identity.IdentityServiceLocator;
 import com.l7tech.adminws.identity.Identity;
 import com.l7tech.adminws.identity.Header;
 import com.l7tech.identity.imp.IdentityProviderConfigManagerClient;
-import com.l7tech.identity.IdentityProviderConfig;
-import com.l7tech.identity.IdentityProviderConfigManager;
+import com.l7tech.identity.*;
 import com.l7tech.util.Locator;
 
 import java.util.Collection;
@@ -21,6 +20,8 @@ import java.util.Iterator;
  */
 public class TestIdentity {
     public static void main(String[] args) throws Exception {
+
+        testGetProviders();
 
         /*
         testLocator2();
@@ -39,21 +40,26 @@ public class TestIdentity {
         System.out.println(res);
         */
 
+        /*
         IdentityService service = new IdentityServiceLocator();
         Identity servicePort = service.getidentities(new java.net.URL("http://localhost:8080/ssg/services/identities"));
         Header[] res = servicePort.findAlllIdentityProviderConfig();
         printres(res);
         for (int i = 0; i < res.length; i++) {
             com.l7tech.adminws.identity.IdentityProviderConfig ipc = servicePort.findIdentityProviderConfigByPrimaryKey(res[i].getOid());
+            System.out.println("IPC" + ipc.getDescription());
             System.out.println(ipc.toString());
         }
 
+        */
 
         /*
+
         IdentityProviderConfigManagerClient manager = new IdentityProviderConfigManagerClient();
         java.util.Collection col = manager.findAllHeaders();
         printCollection(col);
         */
+
     }
 
     public static void printres(Header[] res) {
@@ -105,6 +111,31 @@ public class TestIdentity {
         genConfig.setType(configType);
 
         manager.save(genConfig);
+    }
+
+    public static void testGetProviders() throws Exception {
+        IdentityProviderConfigManagerClient manager = new IdentityProviderConfigManagerClient();
+        Collection res = manager.findAllIdentityProviders();
+        Iterator it = res.iterator();
+        while (it.hasNext()) {
+            System.out.println("Provider found");
+            System.out.println(it.next());
+            IdentityProvider provider = (IdentityProvider)it.next();
+            UserManager users = provider.getUserManager();
+            Collection res2 = users.findAllHeaders();
+            Iterator it2 = res2.iterator();
+            while (it2.hasNext()) {
+                System.out.println("User found");
+                System.out.println(it2.next());
+            }
+            GroupManager groups = provider.getGroupManager();
+            res2 = groups.findAllHeaders();
+            it2 = res2.iterator();
+            while (it2.hasNext()) {
+                System.out.println("Group found");
+                System.out.println(it2.next());
+            }
+        }
     }
 
 }
