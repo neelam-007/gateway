d769266a8b6a7e26d825f9c301bc569814d25e9f
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-05-28 23:42:23 +0000

    Initial checkin.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@355 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/policy/assertion/Routing.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2003 Layer 7 Technologies Inc.
+ *
+ * $Id$
+ */
+
+package com.l7tech.policy.assertion;
+
+/**
+ * @author alex
+ */
+public class Routing extends Assertion {
+}
