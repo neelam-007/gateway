74f2e21a9c0cf0ae05f0e433601d8946e2513700
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-06 22:05:44 +0000

    Minor changes
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@451 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/policy/assertion/Assertion.java
+++ src/com/l7tech/policy/assertion/Assertion.java
@@ -13,16 +13,28 @@ import java.util.Map;
 import java.util.Collections;
 
 /**
- * Immutable.
+ * Immutable except for de-persistence.
  *
  * @author alex
  * @version $Revision$
  */
 public abstract class Assertion {
-    public abstract AssertionError checkRequest( Request request, Response response ) throws PolicyAssertionException;
-    public void init( Map params ) {
+    public Assertion() {
+    }
+
+    public Assertion( Map params ) {
         _params = params;
     }
+
+    public Map getParams() {
+        return _params;
+    }
+
+    public void setParams( Map params ) {
+        _params = params;
+    }
+
+    public abstract AssertionError checkRequest( Request request, Response response ) throws PolicyAssertionException;
 
     protected Map _params = Collections.EMPTY_MAP;
 }
--- src/com/l7tech/policy/assertion/RoutingAssertion.java
+++ src/com/l7tech/policy/assertion/RoutingAssertion.java
@@ -8,12 +8,33 @@ package com.l7tech.policy.assertion;
 
 import com.l7tech.message.Request;
 import com.l7tech.message.Response;
+import com.l7tech.service.ProtectedService;
 
 /**
  * @author alex
  */
 public class RoutingAssertion extends Assertion {
+    public RoutingAssertion( ProtectedService service ) {
+        super();
+        _protectedService = service;
+    }
+
+    /** Default constructor, for Hibernate only, don't call! */
+    public RoutingAssertion() {
+        super();
+    }
+
+    public ProtectedService getProtectedService() {
+        return _protectedService;
+    }
+
+    public void setProtectedService(ProtectedService protectedService) {
+        _protectedService = protectedService;
+    }
+
     public AssertionError checkRequest(Request request, Response response) throws PolicyAssertionException {
         return AssertionError.NONE;
     }
+
+    protected ProtectedService _protectedService;
 }
