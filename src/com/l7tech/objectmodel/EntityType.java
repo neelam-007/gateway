22aa718f56649bc14eca3abee1f33f9a924e8f83
emil <emil@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-06 21:27:51 +0000

    Added the service type
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@445 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/objectmodel/EntityType.java
+++ src/com/l7tech/objectmodel/EntityType.java
@@ -1,5 +1,7 @@
 package com.l7tech.objectmodel;
 
+import java.util.Hashtable;
+
 /**
  * Layer 7 Technologies, inc.
  * User: flascelles
@@ -10,21 +12,44 @@ public class EntityType {
     public static final EntityType ID_PROVIDER_CONFIG = new EntityType(1);
     public static final EntityType USER = new EntityType(2);
     public static final EntityType GROUP = new EntityType(3);
+    public static final EntityType SERVICE = new EntityType(4);
     public static final EntityType UNDEFINED = new EntityType(-1);
 
     public static EntityType fromInterface(Class interfaceType) throws IllegalArgumentException {
-        if (interfaceType.equals(com.l7tech.identity.IdentityProviderConfig.class)) return ID_PROVIDER_CONFIG;
-        else if (interfaceType.equals(com.l7tech.identity.User.class)) return USER;
+        if (interfaceType.equals(com.l7tech.identity.IdentityProviderConfig.class))
+            return ID_PROVIDER_CONFIG;
+        else if (interfaceType.equals(com.l7tech.identity.User.class))
+            return USER;
         else if (interfaceType.equals(com.l7tech.identity.Group.class)) return GROUP;
         throw new IllegalArgumentException("no EntityType for interface " + interfaceType.getName());
     }
 
-    public boolean equals(java.lang.Object obj) {
-        return ((EntityType)obj).val == val;
-    }
-
+    /**
+     * Returns a hash code value for the object.
+     * The method is implemented to satisfy general contract of <code>hashCode</code>
+     * and <code>equals</code>.
+     *
+     * @return  a hash code value for this object.
+     * @see     Object#equals(Object)
+     */
     public int hashCode() {
         return val;
+    }
+
+
+    /**
+     * Indicates whether some other object is "equal to" this one.
+     *
+     * @param   that   the reference object with which to compare.
+     * @return  <code>true</code> if this object is the same as the obj
+     *          argument; <code>false</code> otherwise.
+     * @see     #hashCode()
+     */
+    public boolean equals(java.lang.Object that) {
+        if (that == this) return true;
+        if (!(that instanceof EntityType)) return false;
+
+        return this.hashCode() == that.hashCode();
     }
 
     /**
