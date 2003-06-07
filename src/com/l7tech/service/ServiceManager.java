86d6cadffd9c5a7ba82d32a4fba41dd1e3a230f5
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-07 00:04:50 +0000

    added throws
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@460 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/service/ServiceManager.java
+++ src/com/l7tech/service/ServiceManager.java
@@ -18,7 +18,7 @@ public interface ServiceManager extends EntityManager {
      * @param url
      * @return a string containing the xml document
      */
-    public String resolveWsdlTarget(String url);
+    public String resolveWsdlTarget(String url) throws java.rmi.RemoteException;
 
     // NOTE:
     // add methods as they become necessary
