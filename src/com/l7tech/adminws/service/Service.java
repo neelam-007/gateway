e4c00098dcc4081ca913d37d457647964aa68db6
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-09 15:36:18 +0000

    implemented resolveWsdlTarget
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@466 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/adminws/service/Service.java
+++ src/com/l7tech/adminws/service/Service.java
@@ -7,8 +7,30 @@ package com.l7tech.adminws.service;
  *
  */
 public class Service {
+
     public String resolveWsdlTarget(String url) throws java.rmi.RemoteException {
-        // todo
-        return "blah from com.l7tech.adminws.service.Service";
+        try {
+            java.net.URL urltarget = new java.net.URL(url);
+            java.net.URLConnection connection = urltarget.openConnection();
+            java.io.InputStream in = connection.getInputStream();
+            byte[] buffer = new byte[2048];
+            int read = in.read(buffer);
+            StringBuffer out = new StringBuffer();
+            while (read > 0) {
+                out.append(new String(buffer, 0, read));
+                read = in.read(buffer);
+            }
+            return out.toString();
+        } catch (java.io.IOException e) {
+            e.printStackTrace(System.err);
+            throw new java.rmi.RemoteException("com.l7tech.adminws.service.Service cannot resolve WSDL " + e.getMessage(), e);
+        }
     }
+
+    /*
+    public static void main (String[] args) throws Exception {
+        Service me = new Service();
+        System.out.println(me.resolveWsdlTarget("http://192.168.0.2:8080/simplewsdl.xml"));
+    }
+    */
 }
