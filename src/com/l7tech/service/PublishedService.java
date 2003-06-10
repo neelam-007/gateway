b23be6216fe1d18ce85c6f98bdf5b0e9c507ba71
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-10 21:30:26 +0000

    Added policyXml field
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@492 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/service/PublishedService.java
+++ src/com/l7tech/service/PublishedService.java
@@ -7,34 +7,17 @@
 package com.l7tech.service;
 
 import com.l7tech.policy.assertion.Assertion;
+import com.l7tech.objectmodel.imp.NamedEntityImp;
 
 /**
  * @author alex
  */
-public class PublishedService {
-
-    public long getOid() {
-        return oid;
-    }
-
-    public void setOid(long oid) {
-        this.oid = oid;
-    }
-
-    public Assertion getRootAssertion() {
+public class PublishedService extends NamedEntityImp {
+    public Assertion rootAssertion() {
+        if ( _rootAssertion == null ) {
+            // TODO: Parse the policy
+        }
         return _rootAssertion;
-    }
-
-    public void setRootAssertion(Assertion _rootAssertion) {
-        this._rootAssertion = _rootAssertion;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public ProtectedService getProtectedService() {
@@ -45,11 +28,20 @@ public class PublishedService {
         _protectedService = protServ;
     }
 
+    public String getPolicyXml() {
+        return _policyXml;
+    }
+
+    public void setPolicyXml( String policyXml ) {
+        _policyXml = policyXml;
+        _rootAssertion = null;
+    }
+
     // ************************************************
     // PRIVATES
     // ************************************************
-    protected long oid;
-    protected Assertion _rootAssertion;
-    protected String name;
+    protected String _policyXml;
     protected ProtectedService _protectedService;
+
+    protected transient Assertion _rootAssertion;
 }
