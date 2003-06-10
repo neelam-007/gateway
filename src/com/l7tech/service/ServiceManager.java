7bad98959e93218c198f658b540f4ab07a492e1d
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-10 22:35:46 +0000

    commented the api
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@502 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/service/ServiceManager.java
+++ src/com/l7tech/service/ServiceManager.java
@@ -14,11 +14,47 @@ import java.util.HashMap;
  * Service API. Get instance of this through the Locator class.
  */
 public interface ServiceManager extends EntityManager {
+    /**
+     * Get what the server sees at that url.
+     * Meant to be used by admin console entity.
+     * @param url the url target to inspect.
+     * @return the payload returned at that url (hopefully)
+     * @throws java.rmi.RemoteException
+     */
     public String resolveWsdlTarget(String url) throws java.rmi.RemoteException;
+
+    /**
+     * saves a published service along with it's policy assertions
+     * @param service
+     * @return
+     * @throws SaveException
+     */
     public long save( PublishedService service ) throws SaveException;
+
+    /**
+     * updates a policy service. call this instead of save if the service
+     * has an history. on the console side implementation, you can call save
+     * either way and the oid will dictate whether the object should be saved
+     * or updated.
+     *
+     * @param service
+     * @throws UpdateException
+     */
     public void update( PublishedService service ) throws UpdateException;
+
+    /**
+     * deletes the service
+     * @param service
+     * @throws DeleteException
+     */
     public void delete( PublishedService service ) throws DeleteException;
 
+    /**
+     * called at run time to discover which service is being invoked based
+     * on the request headers and/or document.
+     * @param request
+     * @return
+     */
     public PublishedService resolveService( Request request );
 
     // NOTE:
