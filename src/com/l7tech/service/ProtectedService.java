bb745ba4b04d9fb7b3b582cf0f7e1a71ae2e22d8
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-10 18:09:23 +0000

    refactored as per June 10 meeting
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@486 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/service/ProtectedService.java
+++ src/com/l7tech/service/ProtectedService.java
@@ -6,32 +6,40 @@
 
 package com.l7tech.service;
 
-import java.util.Set;
-
 /**
+ * A reference to an existing web service.
+ * 
  * @author alex
  */
-public class ProtectedService extends Service {
-    /**
-     * Constructs a ProtectedService with a pre-existing Set of Operations.
-     *
-     * @param operations
-     */
-    public ProtectedService( String name, Set operations ) {
-        super( name, operations );
-        _wsdlUrl = null;
+public class ProtectedService {
+    public long getOid() {
+        return oid;
     }
 
-    /**
-     * Constructs a ProtectedService by parsing a WSDL document referred to by an URL.  This method will probably take some time to complete!
-     *
-     * @param wsdlUrl A String containing an URL to a WSDL document.
-     */
-    public ProtectedService( String wsdlUrl ) {
-        _wsdlUrl = wsdlUrl;
-        // TODO: Call the bloody WSDL parser
+    public void setOid(long oid) {
+        this.oid = oid;
     }
 
-    protected String _wsdlUrl;
-    protected Service _service;
+    public String getWsdl() {
+        return wsdl;
+    }
+
+    public void setWsdl(String wsdl) {
+        this.wsdl = wsdl;
+    }
+
+    public String getOriginalWSDLLocation() {
+        return originalWSDLLocation;
+    }
+
+    public void setOriginalWSDLLocation(String originalWSDLLocation) {
+        this.originalWSDLLocation = originalWSDLLocation;
+    }
+
+    // ************************************************
+    // PRIVATES
+    // ************************************************
+    private long oid;
+    private String wsdl;
+    private String originalWSDLLocation;
 }
--- src/com/l7tech/service/PublishedService.java
+++ src/com/l7tech/service/PublishedService.java
@@ -8,16 +8,48 @@ package com.l7tech.service;
 
 import com.l7tech.policy.assertion.Assertion;
 
-import java.util.Set;
-
 /**
  * @author alex
  */
-public class PublishedService extends Service {
-    public PublishedService( String name, Set operations, Assertion rootAssertion ) {
-        super(name, operations);
-        _rootAssertion = rootAssertion;
+public class PublishedService {
+
+    public long getOid() {
+        return oid;
     }
 
+    public void setOid(long oid) {
+        this.oid = oid;
+    }
+
+    public Assertion getRootAssertion() {
+        return _rootAssertion;
+    }
+
+    public void setRootAssertion(Assertion _rootAssertion) {
+        this._rootAssertion = _rootAssertion;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getAssociatedProtectedServiceOid() {
+        return associatedProtectedServiceOid;
+    }
+
+    public void setAssociatedProtectedServiceOid(long associatedProtectedServiceOid) {
+        this.associatedProtectedServiceOid = associatedProtectedServiceOid;
+    }
+
+    // ************************************************
+    // PRIVATES
+    // ************************************************
+    protected long oid;
     protected Assertion _rootAssertion;
+    protected String name;
+    protected long associatedProtectedServiceOid;
 }
