c7ccdb6d3c336a7b116c8b6dc5c03ce3a93f3bcf
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-11 16:42:10 +0000

    implemented and tested findAllPublishedServices and findAllPublishedServicesByOffset
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@527 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/adminws/service/Client.java
+++ src/com/l7tech/adminws/service/Client.java
@@ -37,6 +37,26 @@ public class Client {
         return (PublishedService)call.invoke(new Object[]{new Long(oid)});
     }
 
+    public com.l7tech.objectmodel.EntityHeader[] findAllPublishedServices() throws java.rmi.RemoteException {
+        Call call = createStubCall();
+        call.setOperationName(new QName(SERVICE_URN, "findAllPublishedServices"));
+		call.setReturnClass(com.l7tech.objectmodel.EntityHeader[].class);
+        com.l7tech.objectmodel.EntityHeader[] output = (com.l7tech.objectmodel.EntityHeader[])call.invoke(new Object[]{});
+        if (output == null) return new com.l7tech.objectmodel.EntityHeader[0];
+        return output;
+    }
+
+    public com.l7tech.objectmodel.EntityHeader[] findAllPublishedServicesByOffset(int offset, int windowSize) throws java.rmi.RemoteException {
+        Call call = createStubCall();
+        call.setOperationName(new QName(SERVICE_URN, "findAllPublishedServicesByOffset"));
+		call.setReturnClass(com.l7tech.objectmodel.EntityHeader[].class);
+        call.addParameter(new javax.xml.namespace.QName("", "offset"), new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "int"), int.class, javax.xml.rpc.ParameterMode.IN);
+        call.addParameter(new javax.xml.namespace.QName("", "windowSize"), new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "int"), int.class, javax.xml.rpc.ParameterMode.IN);
+        com.l7tech.objectmodel.EntityHeader[] output = (com.l7tech.objectmodel.EntityHeader[])call.invoke(new Object[]{new java.lang.Integer(offset), new java.lang.Integer(windowSize)});
+        if (output == null) return new com.l7tech.objectmodel.EntityHeader[0];
+        return output;
+    }
+
     // ************************************************
     // PRIVATES
     // ************************************************
@@ -46,6 +66,10 @@ public class Client {
         if (me == null) System.err.println("!!!!!!!");
         System.out.println(me.resolveWsdlTarget("http://localhost:8080/simplewsdl.xml"));
         System.out.println(me.findServiceByPrimaryKey(654));
+        com.l7tech.objectmodel.EntityHeader[] res = me.findAllPublishedServices();
+        for (int i = 0; i < res.length; i++) System.out.println(res[i].toString());
+        res = me.findAllPublishedServicesByOffset(231,5466);
+        for (int i = 0; i < res.length; i++) System.out.println(res[i].toString());
     }
 
     private Call createStubCall() throws java.rmi.RemoteException {
@@ -74,6 +98,12 @@ public class Client {
     private void registerTypeMappings(Call call) {
         QName qn = new QName(SERVICE_URN, "PublishedService");
         call.registerTypeMapping(PublishedService.class, qn, new org.apache.axis.encoding.ser.BeanSerializerFactory(PublishedService.class, qn), new org.apache.axis.encoding.ser.BeanDeserializerFactory(PublishedService.class, qn));
+        qn = new QName(SERVICE_URN, "EntityHeader");
+        call.registerTypeMapping(com.l7tech.objectmodel.EntityHeader.class, qn, new org.apache.axis.encoding.ser.BeanSerializerFactory(com.l7tech.objectmodel.EntityHeader.class, qn), new org.apache.axis.encoding.ser.BeanDeserializerFactory(com.l7tech.objectmodel.EntityHeader.class, qn));
+        qn = new QName(SERVICE_URN, "ArrayOfHeaders");
+        call.registerTypeMapping(com.l7tech.objectmodel.EntityHeader[].class, qn, new org.apache.axis.encoding.ser.ArraySerializerFactory(), new org.apache.axis.encoding.ser.ArrayDeserializerFactory());
+        qn = new QName(SERVICE_URN, "EntityType");
+        call.registerTypeMapping(com.l7tech.objectmodel.EntityType.class, qn, new org.apache.axis.encoding.ser.BeanSerializerFactory(com.l7tech.objectmodel.EntityType.class, qn), new org.apache.axis.encoding.ser.BeanDeserializerFactory(com.l7tech.objectmodel.EntityType.class, qn));
     }
 
     private static final String SERVICE_URN = "http://www.layer7-tech.com/service";
