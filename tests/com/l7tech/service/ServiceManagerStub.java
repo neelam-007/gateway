69618a2c83a174ad8eaa8b3811ec88330679a898
emil <emil@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-11 04:11:51 +0000

    Implement missing methods
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@519 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- tests/com/l7tech/service/ServiceManagerStub.java
+++ tests/com/l7tech/service/ServiceManagerStub.java
@@ -1,6 +1,11 @@
 package com.l7tech.service;
 
 import com.l7tech.objectmodel.FindException;
+import com.l7tech.objectmodel.SaveException;
+import com.l7tech.objectmodel.UpdateException;
+import com.l7tech.objectmodel.DeleteException;
+import com.l7tech.message.Request;
+import com.l7tech.service.resolution.ServiceResolutionException;
 
 import javax.wsdl.WSDLException;
 import java.rmi.RemoteException;
@@ -13,6 +18,17 @@ import java.net.URL;
  * @author <a href="mailto:emarceta@layer7-tech.com>Emil Marceta</a> 
  */
 public class ServiceManagerStub implements ServiceManager {
+    /**
+     * Retreive the actual PublishedService object from it's oid.
+     *
+     * @param oid
+     * @return
+     * @throws FindException
+     */
+    public PublishedService findByPrimaryKey(long oid) throws FindException {
+        return null;
+    }
+
     /**
      * Used by the console to retreive the actual wsdl located at a target
      * as seen by the ssg.
@@ -28,6 +44,48 @@ public class ServiceManagerStub implements ServiceManager {
         } catch (java.io.IOException e) {
             throw new RemoteException("resolveWsdlTarget()", e);
         }
+    }
+
+    /**
+     * saves a published service along with it's policy assertions
+     * @param service
+     * @return
+     * @throws SaveException
+     */
+    public long save(PublishedService service) throws SaveException {
+        return 0;
+    }
+
+    /**
+     * updates a policy service. call this instead of save if the service
+     * has an history. on the console side implementation, you can call save
+     * either way and the oid will dictate whether the object should be saved
+     * or updated.
+     *
+     * @param service
+     * @throws UpdateException
+     */
+    public void update(PublishedService service) throws UpdateException {
+
+    }
+
+    /**
+     * deletes the service
+     * @param service
+     * @throws DeleteException
+     */
+    public void delete(PublishedService service) throws DeleteException {
+
+    }
+
+    /**
+     * called at run time to discover which service is being invoked based
+     * on the request headers and/or document.
+     * @param request
+     * @return
+     */
+    public PublishedService resolveService(Request request) throws ServiceResolutionException {
+        return null;
     }
 
     /**
