6516100b9008071131b021aa58836e792f6c0728
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-12 05:21:25 +0000

    added handling for resolving service through oid and documented the behavior of the servlet.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@558 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/policy/server/PolicyServlet.java
+++ src/com/l7tech/policy/server/PolicyServlet.java
@@ -1,5 +1,7 @@
 package com.l7tech.policy.server;
 
+import com.l7tech.service.PublishedService;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,17 +14,50 @@ import java.io.IOException;
  * Date: Jun 11, 2003
  *
  * This servlet returns policy documents (type xml).
- * Pass the parameters "urn" and "soapaction" to resolve the policy documents such as the sample below
+ * The following parameters can be passed to resolve the PublishedService:
+ * serviceoid : the internal object identifier of the PublishedService. if specified, this parameter is sufficient to
+ *              retrieve the policy
+ * urn : the urn of the service. if more than one service have the same urn, at least one more paramater will be
+ *       necessary
+ * soapaction : the soapaction of the PublishedService
+ *
+ * Pass the parameters as part of the url as in the samples below
+ * http://localhost:8080/ssg/policy/disco.modulator?serviceoid=666
  * http://localhost:8080/ssg/policy/disco.modulator?urn=blah&soapaction=ugh
+ *
  */
 public class PolicyServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
+        // GET THE PARAMETERS PASSED
+        String str_oid = httpServletRequest.getParameter("serviceoid");
         String urnParameter = httpServletRequest.getParameter("urn");
         String soapactionParamater = httpServletRequest.getParameter("soapaction");
+        // RESOLVE THE SERVICE
+        PublishedService targetService = null;
+        if (str_oid != null && str_oid.length() > 0) targetService = resolveService(Long.parseLong(str_oid));
+        else targetService = resolveService(urnParameter, soapactionParamater);
+        // OUTPUT THE POLICY
+        outputPublishedServicePolicy(targetService, httpServletResponse);
+    }
 
-        // todo, remove this debug stuff
-        httpServletResponse.getOutputStream().println("urnParameter = " + urnParameter);
-        httpServletResponse.getOutputStream().println("soapactionParamater = " + soapactionParamater);
+    private PublishedService resolveService(long oid) {
+        // todo
+        return null;
+    }
+
+    private PublishedService resolveService(String urn, String soapAction) {
+        // todo
+        return null;
+    }
+
+    private void outputPublishedServicePolicy(PublishedService service, HttpServletResponse response) throws IOException {
+        if (service == null) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND, "ERROR cannot resolve target service");
+            return;
+        } else {
+            // todo
+            response.getOutputStream().println("todo");
+        }
     }
 }
