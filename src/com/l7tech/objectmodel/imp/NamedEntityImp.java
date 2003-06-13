4deece210ebcb0317e626be4636d4eaddf22ae31
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-13 00:46:08 +0000

    Actually implements NamedEntity now!
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@591 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/objectmodel/imp/NamedEntityImp.java
+++ src/com/l7tech/objectmodel/imp/NamedEntityImp.java
@@ -1,8 +1,9 @@
 package com.l7tech.objectmodel.imp;
 
 import com.l7tech.objectmodel.imp.EntityImp;
+import com.l7tech.objectmodel.NamedEntity;
 
-public abstract class NamedEntityImp extends EntityImp {
+public abstract class NamedEntityImp extends EntityImp implements NamedEntity {
     public String getName() { return _name; }
     public void setName( String name ) { _name = name; }
 
