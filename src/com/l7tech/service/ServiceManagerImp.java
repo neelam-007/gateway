8593b21308cf07c3a932fab7605bdfdd972c63f5
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-13 23:30:06 +0000

    Initializes ServiceResolvers
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@624 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/service/ServiceManagerImp.java
+++ src/com/l7tech/service/ServiceManagerImp.java
@@ -6,13 +6,17 @@
 
 package com.l7tech.service;
 
-import com.l7tech.message.Request;
 import com.l7tech.objectmodel.*;
-import com.l7tech.service.resolution.ServiceResolutionException;
+import com.l7tech.message.Request;
+import com.l7tech.service.resolution.*;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import java.rmi.RemoteException;
-import java.sql.SQLException;
 import java.util.*;
+import java.sql.SQLException;
+
+import org.apache.log4j.Category;
 
 /**
  * @author alex
@@ -27,8 +31,45 @@ public class ServiceManagerImp extends HibernateEntityManager implements Service
         return null;
     }
 
-    public ServiceManagerImp() {
+    public ServiceManagerImp() throws NamingException, ObjectModelException {
         super();
+
+        Collection services = findAll();
+
+        InitialContext ic = new InitialContext();
+        String serviceResolvers = (String)ic.lookup( "java:comp/env/ServiceResolvers" );
+        if ( serviceResolvers == null ) {
+            StringBuffer resolvers = new StringBuffer();
+            resolvers.append( UrnResolver.class.getName() );
+            resolvers.append( " " );
+            resolvers.append( SoapActionResolver.class.getName() );
+            serviceResolvers = resolvers.toString();
+        }
+
+        _resolvers = new TreeSet();
+        StringTokenizer stok = new StringTokenizer( serviceResolvers );
+        String className;
+        Class resolverClass;
+        ServiceResolver resolver;
+
+        while ( stok.hasMoreTokens() ) {
+            className = stok.nextToken();
+            try {
+                resolverClass = Class.forName( className );
+                resolver = (ServiceResolver)resolverClass.newInstance();
+                resolver.setServices( services );
+
+                _resolvers.add( resolver );
+            } catch ( ClassNotFoundException cnfe ) {
+                log.error( cnfe );
+            } catch ( InstantiationException ie ) {
+                log.error( ie );
+            } catch ( IllegalAccessException iae ) {
+                log.error( iae );
+            }
+        }
+
+
     }
 
     public PublishedService findByPrimaryKey(long oid) throws FindException {
@@ -75,7 +116,9 @@ public class ServiceManagerImp extends HibernateEntityManager implements Service
         return "published_service";
     }
 
+    private static final Category log = Category.getInstance(ServiceManagerImp.class);
+    protected SortedSet _resolvers;
     protected transient Collection _allServices;
-    protected transient Map _paramNameMap = new HashMap();
-    protected transient Map _paramToServiceMap = new HashMap();
+    //protected transient Map _paramNameMap = new HashMap();
+    //protected transient Map _paramToServiceMap = new HashMap();
 }
