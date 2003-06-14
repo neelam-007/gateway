03b45eeea72177e4e2dceed731f3d61e33efe2c0
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-14 00:20:14 +0000

    Added IfThenAssertion
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@632 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/policy/assertion/composite/IfThenAssertion.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2003 Layer 7 Technologies Inc.
+ *
+ * $Id$
+ */
+
+package com.l7tech.policy.assertion.composite;
+
+import com.l7tech.policy.assertion.AssertionError;
+import com.l7tech.policy.assertion.PolicyAssertionException;
+import com.l7tech.policy.assertion.Assertion;
+import com.l7tech.message.Request;
+import com.l7tech.message.Response;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Iterator;
+
+/**
+ * Evaluates child assertions in order until one fails or all children have been evaluated.
+ * Returns the last return code encountered.
+ * There is no WS-Policy analog of this construct..
+ *
+ * Semantically equivalent to a short-circuited AND.
+ *
+ * User: mike
+ * Date: Jun 13, 2003
+ * Time: 5:08:00 PM
+ */
+public class IfThenAssertion extends CompositeAssertion {
+    public IfThenAssertion() {
+    }
+
+    /** Convenience constructor for the common case of "if precondition then action" */
+    public IfThenAssertion(Assertion precondition, Assertion action) {
+        super(Arrays.asList(new Assertion[] { precondition, action }));
+    }
+
+    public IfThenAssertion(List children) {
+        super(children);
+    }
+
+    public IfThenAssertion(CompositeAssertion parent, List children) {
+        super(parent, children);
+    }
+
+    public AssertionError checkRequest(Request request, Response response) throws PolicyAssertionException {
+        AssertionError result = AssertionError.FALSIFIED;
+        for (Iterator iterator = children(); iterator.hasNext();) {
+            Assertion child = (Assertion)iterator.next();
+            result = child.checkRequest(request, response);
+            if (result != AssertionError.NONE)
+                return result;
+        }
+        return result;
+    }
+}
