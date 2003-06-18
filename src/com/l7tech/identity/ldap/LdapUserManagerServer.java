c6156ed890e4ea4f1b09e8288d76962e1a3324da
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-18 00:08:43 +0000

    implemented findUserByPrimaryKey (constructs a User object given a DN) tested against spock ldap directory manages directory context
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@659 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/identity/ldap/LdapUserManagerServer.java
+++ src/com/l7tech/identity/ldap/LdapUserManagerServer.java
@@ -7,6 +7,13 @@ import com.l7tech.objectmodel.DeleteException;
 import com.l7tech.objectmodel.SaveException;
 import com.l7tech.objectmodel.UpdateException;
 
+import javax.naming.Context;
+import javax.naming.NamingException;
+import javax.naming.NamingEnumeration;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
+import javax.naming.directory.Attributes;
+import javax.naming.directory.Attribute;
 import java.util.Collection;
 
 /**
@@ -22,11 +29,33 @@ public class LdapUserManagerServer implements UserManager {
     }
 
     /**
-     * This is actually not supported in this UserManager since ldap entries are not reffered to by a long id
-     * but rather by a DN.
+     * Get a User object for the given dn
+     *
+     * @param dn the distinguished name to look for in the directory
+     * @throws FindException
      */
-    public User findByPrimaryKey(String oid) throws FindException {
-        return null;
+    public User findByPrimaryKey(String dn) throws FindException {
+        try {
+            Attributes attributes = getAnonymousContext().getAttributes(dn);
+            LdapUser out = new LdapUser();
+            out.setDN(dn);
+            Object tmp = extractOneAttributeValue(attributes, EMAIL_ATTR_NAME);
+            if (tmp != null) out.setEmail(tmp.toString());
+            tmp = extractOneAttributeValue(attributes, FIRSTNAME_ATTR_NAME);
+            if (tmp != null) out.setFirstName(tmp.toString());
+            tmp = extractOneAttributeValue(attributes, LASTNAME_ATTR_NAME);
+            if (tmp != null) out.setLastName(tmp.toString());
+            tmp = extractOneAttributeValue(attributes, LOGIN_ATTR_NAME);
+            if (tmp != null) out.setLogin(tmp.toString());
+            tmp = extractOneAttributeValue(attributes, NAME_ATTR_NAME);
+            if (tmp != null) out.setName(tmp.toString());
+            tmp = extractOneAttributeValue(attributes, PASSWD_ATTR_NAME);
+            if (tmp != null) out.setPassword(tmp.toString());
+            return out;
+        } catch (NamingException e) {
+            e.printStackTrace(System.err);
+            throw new FindException(e.getMessage(), e);
+        }
     }
 
     /**
@@ -73,5 +102,64 @@ public class LdapUserManagerServer implements UserManager {
     // PRIVATES
     // ************************************************
 
+    public static void main(String[] args) throws Exception {
+        LdapIdentityProviderConfig config = new LdapIdentityProviderConfig();
+        config.setLdapHostURL("ldap://localhost:3899/");
+        LdapUserManagerServer me = new LdapUserManagerServer(config);
+        User usr = me.findByPrimaryKey("cn=flascelles,dc=layer7-tech,dc=com");
+        System.out.println(usr);
+
+        usr = me.findByPrimaryKey("cn=Alex Cruise,dc=layer7-tech,dc=com");
+        System.out.println(usr);
+
+        usr = me.findByPrimaryKey("cn=dsirota,dc=layer7-tech,dc=com");
+        System.out.println(usr);
+
+        usr = me.findByPrimaryKey("cn=stranger,dc=layer7-tech,dc=com");
+        System.out.println(usr);
+    }
+
+    private Object extractOneAttributeValue(Attributes attributes, String attrName) {
+        Attribute valuesWereLookingFor = attributes.get(attrName);
+        if (valuesWereLookingFor != null && valuesWereLookingFor.size() > 0) {
+            try {
+                return valuesWereLookingFor.get(0);
+            } catch (NamingException e) {
+                e.printStackTrace(System.err);
+            }
+        }
+        return null;
+    }
+
+    private DirContext getAnonymousContext() throws NamingException {
+        if (anonymousContext == null) createAnonymousContext();
+        return anonymousContext;
+    }
+
+    private synchronized void createAnonymousContext() throws NamingException {
+
+        // Create the initial directory context. So anonymous bind for search
+        // Identify service provider to use
+        java.util.Hashtable env = new java.util.Hashtable();
+        env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+        if (config.getSearchBase() != null && config.getSearchBase().length() > 0) {
+            env.put(Context.PROVIDER_URL, config.getLdapHostURL() + config.getSearchBase());
+        }
+        else env.put(Context.PROVIDER_URL, config.getLdapHostURL());
+
+        // Create the initial directory context. So anonymous bind for search
+        anonymousContext = new InitialDirContext(env);
+    }
+
+    private DirContext anonymousContext = null;
     private LdapIdentityProviderConfig config;
+
+    // mappings for attribute names
+    // these may become properties of the LdapIdentityProviderConfig
+    private static final String LOGIN_ATTR_NAME = "uid";
+    private static final String EMAIL_ATTR_NAME = "mail";
+    private static final String FIRSTNAME_ATTR_NAME = "givenName";
+    private static final String LASTNAME_ATTR_NAME = "sn";
+    private static final String NAME_ATTR_NAME = "cn";
+    private static final String PASSWD_ATTR_NAME = "userPassword";
 }
