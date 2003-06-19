c1d2f137a8c343c1892cce94c7ba1427b09eb5b6
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-19 16:09:38 +0000

    test that we can list groups from the directory on spock
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@685 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ tests/com/l7tech/identity/ldap/TestLdapGroupManagerServer.java
@@ -0,0 +1,41 @@
+package com.l7tech.identity.ldap;
+
+import java.util.Collection;
+import java.util.Iterator;
+import junit.framework.TestCase;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * Layer 7 Technologies, inc.
+ * User: flascelles
+ * Date: Jun 19, 2003
+ *
+ */
+public class TestLdapGroupManagerServer extends TestCase {
+
+    public static Test suite() {
+        TestSuite suite = new TestSuite(TestLdapGroupManagerServer.class);
+        return suite;
+    }
+
+    public void testFindAll() throws Exception {
+        LdapIdentityProviderConfig config = new LdapIdentityProviderConfig();
+        // use this url when ssh forwarding locally
+        config.setLdapHostURL("ldap://localhost:3899");
+        // use this url when in the office
+        //config.setLdapHostURL("ldap://spock:389");
+        config.setSearchBase("dc=layer7-tech,dc=com");
+        LdapGroupManagerServer me = new LdapGroupManagerServer(config);
+        Collection res = me.findAll();
+        Iterator i = res.iterator();
+        while (i.hasNext()) {
+            LdapGroup group = (LdapGroup)i.next();
+            System.out.println(group);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        junit.textui.TestRunner.run(suite());
+    }
+}
