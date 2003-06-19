8287660992e964ca00889134b3de4e30d0a46158
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-19 17:54:39 +0000

    name change
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@694 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ tests/com/l7tech/identity/ldap/LdapIdentityProviderServerTest.java
@@ -0,0 +1,90 @@
+package com.l7tech.identity.ldap;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+import com.l7tech.objectmodel.EntityHeader;
+import com.l7tech.identity.User;
+import com.l7tech.identity.UserManager;
+import com.l7tech.identity.GroupManager;
+
+/**
+ * Layer 7 Technologies, inc.
+ * User: flascelles
+ * Date: Jun 19, 2003
+ *
+ */
+public class LdapIdentityProviderServerTest extends junit.framework.TestCase {
+    public static Test suite() {
+        TestSuite suite = new TestSuite(LdapIdentityProviderServerTest.class);
+        return suite;
+    }
+
+    public void testFindAllUsers() throws Exception {
+        LdapIdentityProviderConfig config = new LdapIdentityProviderConfig();
+        // use this url when ssh forwarding locally
+        // config.setLdapHostURL("ldap://localhost:3899");
+        // use this url when in the office
+        config.setLdapHostURL("ldap://spock:389");
+        config.setSearchBase("dc=layer7-tech,dc=com");
+
+        // create the provider
+        LdapIdentityProviderServer provider = new LdapIdentityProviderServer();
+        provider.initialize(config);
+
+
+        // get the user manager
+        UserManager me = provider.getUserManager();
+
+        // use it
+        Collection headers = me.findAllHeaders();
+        Iterator i = headers.iterator();
+        boolean usrsReceived = false;
+        while (i.hasNext()) {
+            EntityHeader header = (EntityHeader)i.next();
+            User usr = me.findByPrimaryKey(header.getStrId());
+            if (usr != null) usrsReceived = true;
+            // uncomment if you want to see the content
+            //System.out.println(usr);
+        }
+        assertTrue("receiving users", usrsReceived);
+    }
+
+    public void testFindAllGroups() throws Exception {
+        LdapIdentityProviderConfig config = new LdapIdentityProviderConfig();
+        // use this url when ssh forwarding locally
+        // config.setLdapHostURL("ldap://localhost:3899");
+        // use this url when in the office
+        config.setLdapHostURL("ldap://spock:389");
+        config.setSearchBase("dc=layer7-tech,dc=com");
+
+        // create the provider
+        LdapIdentityProviderServer provider = new LdapIdentityProviderServer();
+        provider.initialize(config);
+
+
+        // get the user manager
+        GroupManager me = provider.getGroupManager();
+
+        // use it
+        Collection res = me.findAll();
+        Iterator i = res.iterator();
+        boolean grpsReceived = false;
+        while (i.hasNext()) {
+            LdapGroup group = (LdapGroup)i.next();
+            // uncomment if you want to see the content
+            //System.out.println(group);
+            if (group != null) grpsReceived = true;
+        }
+        assertTrue("receiving groups", grpsReceived);
+    }
+
+    public static void main(String[] args) throws Exception {
+        junit.textui.TestRunner.run(suite());
+    }
+}
+
+
