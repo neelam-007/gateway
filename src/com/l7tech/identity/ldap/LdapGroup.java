3fafaa3628153eed38a0e6b7f9666f6f5d58317c
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-19 00:41:31 +0000

    show members in toString
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@676 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/identity/ldap/LdapGroup.java
+++ src/com/l7tech/identity/ldap/LdapGroup.java
@@ -1,6 +1,10 @@
 package com.l7tech.identity.ldap;
 
 import com.l7tech.identity.internal.imp.GroupImp;
+import com.l7tech.objectmodel.EntityHeader;
+
+import java.util.Set;
+import java.util.Iterator;
 
 /**
  * Layer 7 Technologies, inc.
@@ -21,15 +25,14 @@ public class LdapGroup extends GroupImp {
         String out = "com.l7tech.identity.ldap.LdapGroup."
                 + "\n\tName=" + getName()
                 + "\n\tDN=" + getDN();
-        /*
-        Set membershipHeaders = this.getGroupHeaders();
+
+        Set membershipHeaders = getMemberHeaders();
         Iterator i = membershipHeaders.iterator();
-        out += "\n\tMember of: ";
+        out += "\n\tMembers: ";
         while (i.hasNext()) {
             EntityHeader header = (EntityHeader)i.next();
-            out += header.getName() + ", ";
+            out += header.getName() + "[" + header.getStrId() + "]" + ", ";
         }
-        */
         return out;
     }
 
