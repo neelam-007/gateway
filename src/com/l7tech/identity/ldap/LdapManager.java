779fb619c38a2aed9a6de05e6c2b940d06b2cc1d
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-19 00:43:17 +0000

    throw more exceptions. dont cache the context
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@680 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/identity/ldap/LdapManager.java
+++ src/com/l7tech/identity/ldap/LdapManager.java
@@ -21,35 +21,44 @@ public class LdapManager {
         this.config = config;
     }
 
-    protected Object extractOneAttributeValue(Attributes attributes, String attrName) {
+    protected Object extractOneAttributeValue(Attributes attributes, String attrName) throws NamingException {
         Attribute valuesWereLookingFor = attributes.get(attrName);
         if (valuesWereLookingFor != null && valuesWereLookingFor.size() > 0) {
-            try {
                 return valuesWereLookingFor.get(0);
-            } catch (NamingException e) {
-                e.printStackTrace(System.err);
-            }
         }
         return null;
     }
 
     protected DirContext getAnonymousContext() throws NamingException {
-        if (anonymousContext == null) createAnonymousContext();
-        return anonymousContext;
+        //if (anonymousContext == null) createAnonymousContext();
+        //return anonymousContext;
+
+        java.util.Hashtable env = new java.util.Hashtable();
+        env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+        env.put(Context.PROVIDER_URL, config.getLdapHostURL());
+
+        // Create the initial directory context. So anonymous bind for search
+        return new InitialDirContext(env);
     }
 
     protected synchronized void createAnonymousContext() throws NamingException {
 
         // Create the initial directory context. So anonymous bind for search
         // Identify service provider to use
-        java.util.Hashtable env = new java.util.Hashtable();
-        env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
-        env.put(Context.PROVIDER_URL, config.getLdapHostURL());
+        //java.util.Hashtable env = new java.util.Hashtable();
+        //env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+        //env.put(Context.PROVIDER_URL, config.getLdapHostURL());
 
         // Create the initial directory context. So anonymous bind for search
-        anonymousContext = new InitialDirContext(env);
+        //anonymousContext = new InitialDirContext(env);
     }
 
-    protected DirContext anonymousContext = null;
+    //protected DirContext anonymousContext = null;
     protected LdapIdentityProviderConfig config;
+
+    protected static final String DESCRIPTION_ATTR = "description";
+    protected static final String NAME_ATTR_NAME = "cn";
+    protected static final String GROUPOBJ_MEMBER_ATTR = "memberUid";
+    protected static final String USER_OBJCLASS = "inetOrgPerson";
+    protected static final String LOGIN_ATTR_NAME = "uid";
 }
