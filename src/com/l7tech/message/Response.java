81c0fa5500146c8f57ae871f50c6f7a34e1e06b0
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-20 00:34:28 +0000

    Added addResult() and results()
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@722 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/message/Response.java
+++ src/com/l7tech/message/Response.java
@@ -6,11 +6,16 @@
 
 package com.l7tech.message;
 
+import com.l7tech.policy.assertion.AssertionResult;
+
 import java.io.*;
+import java.util.Iterator;
 
 /**
  * @author alex
  */
 public interface Response extends Message {
     Reader getResponseReader() throws IOException;
+    void addResult( AssertionResult result );
+    Iterator results();
 }
--- src/com/l7tech/message/SoapResponse.java
+++ src/com/l7tech/message/SoapResponse.java
@@ -3,6 +3,9 @@ package com.l7tech.message;
 import org.w3c.dom.Document;
 
 import java.io.*;
+import java.util.*;
+
+import com.l7tech.policy.assertion.AssertionResult;
 
 /**
  * @author alex
@@ -21,5 +24,15 @@ public class SoapResponse extends XmlMessageAdapter implements SoapMessage, XmlR
         return null;
     }
 
+    public void addResult( AssertionResult result ) {
+        if ( _assertionResults.isEmpty() ) _assertionResults = new LinkedList();
+        _assertionResults.add( result );
+    }
+
+    public Iterator results() {
+        return Collections.unmodifiableList( _assertionResults ).iterator();
+    }
+
+    protected List _assertionResults = Collections.EMPTY_LIST;
     protected Reader _responseReader;
 }
