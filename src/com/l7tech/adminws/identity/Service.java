184a484658f760a0835f4468bf2d2113fd709749
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-20 18:49:18 +0000

    usage of the GlobalIdentityConfigManager
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@736 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/adminws/identity/Service.java
+++ src/com/l7tech/adminws/identity/Service.java
@@ -288,7 +288,9 @@ public class Service {
 
     private synchronized void initialiseConfigManager() throws java.rmi.RemoteException {
         try {
-            identityProviderConfigManager = (com.l7tech.identity.IdentityProviderConfigManager)Locator.getDefault().lookup(com.l7tech.identity.IdentityProviderConfigManager.class);
+            // identityProviderConfigManager = (com.l7tech.identity.IdentityProviderConfigManager)Locator.getDefault().lookup(com.l7tech.identity.IdentityProviderConfigManager.class);
+            // test
+            identityProviderConfigManager = new com.l7tech.identity.GlobalIdentityConfigManager();
             if (identityProviderConfigManager == null) throw new java.rmi.RemoteException("Cannot instantiate the IdentityProviderConfigManager");
         } catch (ClassCastException e) {
             e.printStackTrace(System.err);
