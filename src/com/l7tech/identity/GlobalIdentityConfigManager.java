9d7b7964686c8995a3bc56215f9991bb47f67423
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-20 18:50:19 +0000

    tested
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@737 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/identity/GlobalIdentityConfigManager.java
@@ -0,0 +1,118 @@
+package com.l7tech.identity;
+
+import com.l7tech.objectmodel.*;
+import com.l7tech.identity.ldap.LdapIdentityProviderConfigManager;
+import com.l7tech.identity.ldap.LdapIdentityProviderConfig;
+import com.l7tech.identity.internal.imp.InternalIdentityProviderImp;
+import com.l7tech.identity.imp.IdentityProviderConfigImp;
+
+import java.util.Collection;
+import java.util.ArrayList;
+
+/**
+ * Layer 7 Technologies, inc.
+ * User: flascelles
+ * Date: Jun 20, 2003
+ *
+ */
+public class GlobalIdentityConfigManager implements IdentityProviderConfigManager {
+
+    public static final long INTERNAL_ID_PROVIDER_CONFIG_OID = -354684;
+
+    public GlobalIdentityConfigManager() {
+
+        // construct the ldapidentity provider
+        ldapConfigManager = new LdapIdentityProviderConfigManager();
+
+        // construct the internal id provider
+        internalProvider = new InternalIdentityProviderImp();
+        IdentityProviderConfigImp iternalConfig = new IdentityProviderConfigImp();
+        iternalConfig.setName("Internal Identity Provider");
+        iternalConfig.setDescription("Internal Identity Provider");
+        iternalConfig.setOid(INTERNAL_ID_PROVIDER_CONFIG_OID);
+        internalProvider.initialize(iternalConfig);
+    }
+
+    public IdentityProvider getInternalIdentityProvider() {
+        return internalProvider;
+    }
+
+    public IdentityProviderConfig findByPrimaryKey(long oid) throws FindException {
+        if (oid == INTERNAL_ID_PROVIDER_CONFIG_OID) return internalProvider.getConfig();
+        else return ldapConfigManager.findByPrimaryKey(oid);
+    }
+
+    public long save(IdentityProviderConfig identityProviderConfig) throws SaveException {
+        // check the type and save accordingly
+        if (identityProviderConfig instanceof LdapIdentityProviderConfig) {
+            return ldapConfigManager.save(identityProviderConfig);
+        }
+        // handle other types as they are added
+        throw new SaveException("this type of config cannot be saved");
+    }
+
+    public void update(IdentityProviderConfig identityProviderConfig) throws UpdateException {
+        // check the type and save accordingly
+        if (identityProviderConfig instanceof LdapIdentityProviderConfig) {
+            ldapConfigManager.update(identityProviderConfig);
+        }
+        // handle other types as they are added
+        throw new UpdateException("this type of config cannot be updated");
+    }
+
+    public void delete(IdentityProviderConfig identityProviderConfig) throws DeleteException {
+        // check the type and delete accordingly
+        if (identityProviderConfig instanceof LdapIdentityProviderConfig) {
+            ldapConfigManager.delete(identityProviderConfig);
+        }
+        // handle other types as they are added
+        throw new DeleteException("this type of config cannot be deleted");
+    }
+
+    public Collection findAllIdentityProviders() throws FindException {
+        // return collection of ldap ones and add the internal one
+        return null;
+    }
+
+    public Collection findAllHeaders() throws FindException {
+        // return collection of ldap ones and add the internal one
+        // this is an unmodifiable collection so i must construct a new one
+        Collection out = new ArrayList(ldapConfigManager.findAllHeaders());
+        out.add(headerFromConfig(internalProvider.getConfig()));
+        return out;
+    }
+
+    public Collection findAllHeaders(int offset, int windowSize) throws FindException {
+        // return collection of ldap ones and add the internal one
+        Collection out = new ArrayList(ldapConfigManager.findAllHeaders(offset, windowSize-1));
+        out.add(headerFromConfig(internalProvider.getConfig()));
+        return out;
+    }
+
+    public Collection findAll() throws FindException {
+        Collection out = new ArrayList(ldapConfigManager.findAll());
+        out.add(internalProvider.getConfig());
+        return out;
+    }
+
+    public Collection findAll(int offset, int windowSize) throws FindException {
+        Collection out = new ArrayList(ldapConfigManager.findAll(offset, windowSize));
+        out.add(internalProvider.getConfig());
+        return out;
+    }
+
+    // ************************************************
+    // PRIVATES
+    // ************************************************
+
+    protected EntityHeader headerFromConfig(IdentityProviderConfig cfg) {
+        EntityHeader out = new EntityHeader();
+        out.setDescription(cfg.getDescription());
+        out.setName(cfg.getName());
+        out.setOid(cfg.getOid());
+        out.setType(EntityType.ID_PROVIDER_CONFIG);
+        return out;
+    }
+    LdapIdentityProviderConfigManager ldapConfigManager;
+    InternalIdentityProviderImp internalProvider;
+}
