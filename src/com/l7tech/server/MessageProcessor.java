62f6e6315fbe5cc0882171aae82005e0179a0ec0
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-21 03:16:33 +0000

    Implemented.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@771 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/server/MessageProcessor.java
+++ src/com/l7tech/server/MessageProcessor.java
@@ -8,22 +8,37 @@ package com.l7tech.server;
 
 import com.l7tech.message.Request;
 import com.l7tech.message.Response;
-import com.l7tech.policy.assertion.PolicyAssertionException;
-import com.l7tech.service.ServiceManager;
-import com.l7tech.service.ServiceManagerImp;
+import com.l7tech.policy.assertion.*;
+import com.l7tech.service.*;
+import com.l7tech.service.resolution.ServiceResolutionException;
 import com.l7tech.objectmodel.ObjectModelException;
 import org.apache.log4j.Category;
 
-import javax.naming.NamingException;
 
 /**
  * @author alex
  * @version $Revision$
  */
 public class MessageProcessor {
-    public void processMessage( Request request, Response response ) throws PolicyAssertionException, MessageProcessingException {
-        // TODO: Resolve service
-        // TODO: Implement :)
+    public AssertionStatus processMessage( Request request, Response response ) throws PolicyAssertionException, MessageProcessingException {
+        if ( _serviceManager == null ) throw new IllegalStateException( "ServiceManager is null!" );
+        try {
+            PublishedService service = _serviceManager.resolveService( request );
+            request.setParameter( Request.PARAM_SERVICE, service );
+            Assertion ass = service.getRootAssertion();
+
+            AssertionStatus status = ass.checkRequest( request, response );
+            if ( status == AssertionStatus.NONE ) {
+                _log.info( status );
+            } else {
+                _log.warn( status );
+            }
+
+            return status;
+        } catch ( ServiceResolutionException sre ) {
+            _log.warn( sre );
+            return AssertionStatus.SERVER_ERROR;
+        }
     }
 
     public static MessageProcessor getInstance() {
@@ -34,7 +49,6 @@ public class MessageProcessor {
     private MessageProcessor() {
         try {
             _serviceManager = new ServiceManagerImp();
-            _initialized = true;
         } catch ( ObjectModelException ome ) {
             _log.error( ome );
         }
@@ -43,7 +57,6 @@ public class MessageProcessor {
     private Category _log = Category.getInstance( getClass() );
 
     private ServiceManager _serviceManager;
-    private boolean _initialized = false;
 
     private static MessageProcessor _instance = null;
 }
