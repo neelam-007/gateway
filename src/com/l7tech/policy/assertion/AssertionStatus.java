1161c0c818091db71404a4a7974969108c0f1db4
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-21 03:25:25 +0000

    Initial checkin (Renamed from AssertionError)
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@775 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/policy/assertion/AssertionStatus.java
@@ -0,0 +1,60 @@
+package com.l7tech.policy.assertion;
+
+/**
+ * @author alex
+ * @version $Revision$
+ */
+public class AssertionStatus {
+    static final int CLIENT = 400;
+    static final int SERVER = 500;
+    static final int POLICY = 600;
+    static final int MISC   = 1000;
+
+    public static final AssertionStatus NONE          = make(           0, "No Error" );
+    public static final AssertionStatus BAD_REQUEST   = make( CLIENT |  0, "Bad Request" );
+    public static final AssertionStatus AUTH_REQUIRED = make( CLIENT |  1, "Authentication Required" );
+    public static final AssertionStatus AUTH_FAILED   = make( CLIENT |  2, "Authentication Failed" );
+    public static final AssertionStatus FORBIDDEN     = make( CLIENT |  3, "Forbidden" );
+    public static final AssertionStatus NOT_FOUND     = make( CLIENT |  4, "Service Not Found" );
+    public static final AssertionStatus BALEETED      = make( CLIENT | 99, "BALEETED!" );
+
+    public static final AssertionStatus NOT_YET_IMPLEMENTED = make( MISC | 0, "Not yet implemented!" );
+    public static final AssertionStatus NOT_APPLICABLE      = make( MISC | 1, "Not applicable in this context" );
+
+    public static final AssertionStatus SERVER_ERROR = make( SERVER | 0, "Internal Server Error" );
+
+    public static final AssertionStatus FALSIFIED = make( POLICY | 0, "Assertion Falsified" );
+    public static final AssertionStatus FAILED    = make( POLICY | 1, "Error in Assertion Processing" );
+
+    public int getNumeric() {
+        return _numeric;
+    }
+
+    public String getMessage() {
+        return _message;
+    }
+
+    public String getSoapFaultCode() {
+        if ( _numeric >= CLIENT && _numeric < SERVER )
+            return "Client";
+        else
+            return "Server";
+    }
+
+    private static AssertionStatus make( int numeric, String message ) {
+        return new AssertionStatus( numeric, message );
+    }
+
+    private AssertionStatus( int numeric, String message ) {
+        _numeric = numeric;
+        _message = message;
+    }
+
+    public String toString() {
+        return "<" + getClass() + ": " + getNumeric() + "=" + getMessage() + ">";
+    }
+
+    protected final int _numeric;
+    protected final String _message;
+
+}
