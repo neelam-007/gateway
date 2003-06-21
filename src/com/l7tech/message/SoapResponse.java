d54dacce1b965f2ca3e4791ed9b8cc5f4ebbe0c2
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-21 03:16:10 +0000

    Renamed ResponseReader property to ProtectedResponseReader, added results iterators, added AuthenticationMissing property
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@770 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/message/SoapResponse.java
+++ src/com/l7tech/message/SoapResponse.java
@@ -1,11 +1,13 @@
 package com.l7tech.message;
 
 import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 import java.io.*;
 import java.util.*;
 
 import com.l7tech.policy.assertion.AssertionResult;
+import com.l7tech.policy.assertion.AssertionStatus;
 
 /**
  * @author alex
@@ -16,23 +18,55 @@ public class SoapResponse extends XmlMessageAdapter implements SoapMessage, XmlR
         super(tm);
     }
 
-    public Reader getResponseReader() {
+    public synchronized void setProtectedResponseReader(Reader reader) {
+        _responseReader = reader;
+    }
+
+    public synchronized Reader getProtectedResponseReader() {
         return _responseReader;
     }
 
-    public Document getDocument() {
-        return null;
+    public synchronized Document getDocument() throws IOException, SAXException {
+        parse( getProtectedResponseReader() );
+        return _document;
     }
 
-    public void addResult( AssertionResult result ) {
+    public synchronized void addResult( AssertionResult result ) {
         if ( _assertionResults.isEmpty() ) _assertionResults = new LinkedList();
         _assertionResults.add( result );
     }
 
-    public Iterator results() {
+    public synchronized Iterator resultsWithStatus( AssertionStatus status ) {
+        return resultsWithStatus( new AssertionStatus[] { status } );
+    }
+
+    public synchronized Iterator resultsWithStatus( AssertionStatus[] statuses ) {
+        List results = new LinkedList();
+        AssertionResult result;
+        for ( int i = 0; i < _assertionResults.size(); i++ ) {
+            result = (AssertionResult)_assertionResults.get(i);
+
+            for ( int j = 0; j < statuses.length; j++ )
+                if ( statuses[j] == result.getStatus() ) results.add( result );
+
+        }
+        return Collections.unmodifiableList(results).iterator();
+    }
+
+    public synchronized Iterator results() {
         return Collections.unmodifiableList( _assertionResults ).iterator();
+    }
+
+    public boolean isAuthenticationMissing() {
+        return _authMissing;
+    }
+
+    public void setAuthenticationMissing( boolean authMissing ) {
+        _authMissing = authMissing;
     }
 
     protected List _assertionResults = Collections.EMPTY_LIST;
     protected Reader _responseReader;
+    protected Writer _responseWriter;
+    protected boolean _authMissing;
 }
