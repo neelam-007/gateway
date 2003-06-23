aa6fc1d15944ffa3870f1bf03ae295fecc2508e6
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-23 22:06:06 +0000

    added ldapconfigsettings
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@821 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/identity/ldap/LdapConfigSettings.java
@@ -0,0 +1,12 @@
+package com.l7tech.identity.ldap;
+
+/**
+ * Layer 7 Technologies, inc.
+ * User: flascelles
+ * Date: Jun 23, 2003
+ *
+ */
+public class LdapConfigSettings {
+    public static final String LDAP_HOST_URL = "ldapHostURL";
+    public static final String LDAP_SEARCH_BASE = "ldapSearchBase";
+}
