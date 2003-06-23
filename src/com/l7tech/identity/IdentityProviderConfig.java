248c700cfe2bc589c12469776682c13b7caa2a29
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-23 22:10:52 +0000

    turned this from interface to class. added the concept of properties map to be used across different id provider types
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@827 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/identity/IdentityProviderConfig.java
+++ src/com/l7tech/identity/IdentityProviderConfig.java
@@ -1,8 +1,107 @@
 package com.l7tech.identity;
 
-import com.l7tech.objectmodel.NamedEntity;
+import com.l7tech.objectmodel.imp.NamedEntityImp;
+import java.util.HashMap;
+import java.util.Set;
+import java.io.ByteArrayOutputStream;
+import java.io.ByteArrayInputStream;
 
-public interface IdentityProviderConfig extends NamedEntity {
-    String getDescription();
-    void setDescription( String description );
+
+/**
+ * User: flascell
+ * Date: Jun 23, 2003
+ * @author flascell
+ *
+ * Configuration of an Identity Provider object.
+ * The properties depend on the type of identity provider and are defined in the
+ * IdentityProvider type implementations.
+ *
+ */
+public class IdentityProviderConfig extends NamedEntityImp {
+
+    public IdentityProviderConfig(IdentityProviderType type) {
+        super();
+        props = new HashMap();
+        this.type = type;
+    }
+
+    public IdentityProviderConfig() {
+        super();
+        props = new HashMap();
+        // only default value which makes sense for now.
+        type = IdentityProviderType.LDAP;
+    }
+
+    /**
+     * the type is set in constructor
+     */
+    public IdentityProviderType type() {return type;}
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void putProperty(String name, String value) {
+        props.put(name, value);
+    }
+
+    public String getProperty(String name) {
+        return (String)props.get(name);
+    }
+
+    /**
+     * for serialization by axis and hibernate only.
+     * to get the properties, call getProperty
+     */
+    public String getSerializedProps() throws java.io.IOException {
+        // if no props, return empty string
+        Set keys = props.keySet();
+        if (keys.size() < 1) return "";
+        
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        java.beans.XMLEncoder encoder = new java.beans.XMLEncoder(output);
+        encoder.writeObject(props);
+        encoder.close();
+        output.close();
+        return output.toString();
+    }
+
+    /**
+     * for serialization by axis and hibernate only.
+     * to set the properties, call setProperty
+     */
+    public void setSerializedProps(String serializedProps) {
+        if (serializedProps == null || serializedProps.length() < 2) props = new HashMap();
+        else
+        {
+            ByteArrayInputStream in = new ByteArrayInputStream(serializedProps.getBytes());
+            java.beans.XMLDecoder decoder = new java.beans.XMLDecoder(in);
+            props = (HashMap)decoder.readObject();
+        }
+    }
+
+    /**
+     * for serialization by axis and hibernate only.
+     */
+    public int getTypeVal() {
+        return type.toVal();
+    }
+
+    /**
+     * for serialization by axis and hibernate only.
+     */
+    public void setTypeVal(int val) {
+        type = IdentityProviderType.fromVal(val);
+    }
+
+    // ************************************************
+    // PRIVATES
+    // ************************************************
+    private String description;
+    private transient IdentityProviderType type;
+    private transient HashMap props;
 }
