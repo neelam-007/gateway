b3c8ae5f1963683c482c7769c97011562ac7439f
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-24 16:34:08 +0000

    remove dependency with LdapIdentityProviderConfigManager
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@857 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/identity/IdProvConfManagerServer.java
+++ src/com/l7tech/identity/IdProvConfManagerServer.java
@@ -1,11 +1,11 @@
 package com.l7tech.identity;
 
 import com.l7tech.objectmodel.*;
-import com.l7tech.identity.ldap.LdapIdentityProviderConfigManager;
 import com.l7tech.identity.internal.imp.InternalIdentityProviderImp;
 
 import java.util.Collection;
 import java.util.ArrayList;
+import java.sql.SQLException;
 
 /**
  * Layer 7 Technologies, inc.
@@ -16,14 +16,15 @@ import java.util.ArrayList;
  * internal identity provider as well as the other providers (ldap) configured by the administrator.
  *
  */
-public class IdProvConfManagerServer implements GlobalIdProviderConfManager {
+public class IdProvConfManagerServer extends HibernateEntityManager implements GlobalIdProviderConfManager {
 
     // since this provider config is not persisted, we need a special id to identify it for certain operations
     public static final long INTERNALPROVIDER_SPECIAL_OID = -2;
 
     public IdProvConfManagerServer() {
+        super();
         // construct the ldapidentity provider
-        ldapConfigManager = new LdapIdentityProviderConfigManager();
+        // ldapConfigManager = new LdapIdentityProviderConfigManager();
 
         // construct the internal id provider
         internalProvider = new InternalIdentityProviderImp();
@@ -38,66 +39,85 @@ public class IdProvConfManagerServer implements GlobalIdProviderConfManager {
 
     public IdentityProviderConfig findByPrimaryKey(long oid) throws FindException {
         if (oid == INTERNALPROVIDER_SPECIAL_OID) return internalProvider.getConfig();
-        else return ldapConfigManager.findByPrimaryKey(oid);
+        else try {
+            return (IdentityProviderConfig)_manager.findByPrimaryKey( getContext(), getImpClass(), oid );
+        } catch ( SQLException se ) {
+            throw new FindException( se.toString(), se );
+        }
     }
 
     public long save(IdentityProviderConfig identityProviderConfig) throws SaveException {
-        // check the type and save accordingly
-        if (identityProviderConfig.type() == IdentityProviderType.LDAP) {
-            return ldapConfigManager.save(identityProviderConfig);
+        // we should not accept saving an internal type
+        if (identityProviderConfig.type() != IdentityProviderType.LDAP) throw new SaveException("this type of config cannot be saved");
+        try {
+            return _manager.save(getContext(), identityProviderConfig);
+        } catch (SQLException se) {
+            throw new SaveException(se.toString(), se);
         }
-        // handle other types as they are added
-        throw new SaveException("this type of config cannot be saved");
     }
 
     public void update(IdentityProviderConfig identityProviderConfig) throws UpdateException {
-        // check the type and save accordingly
-        if (identityProviderConfig.type() == IdentityProviderType.LDAP) {
-            ldapConfigManager.update(identityProviderConfig);
+        // we should not accept saving an internal type
+        if (identityProviderConfig.type() != IdentityProviderType.LDAP) throw new UpdateException("this type of config cannot be updated");
+        try {
+            _manager.update( getContext(), identityProviderConfig );
+        } catch ( SQLException se ) {
+            throw new UpdateException( se.toString(), se );
         }
-        // handle other types as they are added
-        throw new UpdateException("this type of config cannot be updated");
     }
 
     public void delete(IdentityProviderConfig identityProviderConfig) throws DeleteException {
-        // check the type and delete accordingly
-        if (identityProviderConfig.type() == IdentityProviderType.LDAP) {
-            ldapConfigManager.delete(identityProviderConfig);
+        // we should not accept deleting an internal type
+        if (identityProviderConfig.type() != IdentityProviderType.LDAP) throw new DeleteException("this type of config cannot be deleted");
+        try {
+            _manager.delete( getContext(), identityProviderConfig );
+        } catch ( SQLException se ) {
+            throw new DeleteException( se.toString(), se );
         }
-        // handle other types as they are added
-        throw new DeleteException("this type of config cannot be deleted");
     }
 
     public Collection findAllIdentityProviders() throws FindException {
-        // return collection of ldap ones and add the internal one
-        return null;
+        return IdentityProviderFactory.findAllIdentityProviders(this);
     }
 
     public Collection findAllHeaders() throws FindException {
         // return collection of ldap ones and add the internal one
         // this is an unmodifiable collection so i must construct a new one
-        Collection out = new ArrayList(ldapConfigManager.findAllHeaders());
+        Collection out = new ArrayList(super.findAllHeaders());
         out.add(headerFromConfig(internalProvider.getConfig()));
         return out;
     }
 
     public Collection findAllHeaders(int offset, int windowSize) throws FindException {
         // return collection of ldap ones and add the internal one
-        Collection out = new ArrayList(ldapConfigManager.findAllHeaders(offset, windowSize-1));
+        Collection out = new ArrayList(super.findAllHeaders(offset, windowSize-1));
         out.add(headerFromConfig(internalProvider.getConfig()));
         return out;
     }
 
     public Collection findAll() throws FindException {
-        Collection out = new ArrayList(ldapConfigManager.findAll());
+        Collection out = new ArrayList(super.findAll());
         out.add(internalProvider.getConfig());
         return out;
     }
 
     public Collection findAll(int offset, int windowSize) throws FindException {
-        Collection out = new ArrayList(ldapConfigManager.findAll(offset, windowSize));
+        Collection out = new ArrayList(super.findAll(offset, windowSize));
         out.add(internalProvider.getConfig());
         return out;
+    }
+
+
+    public Class getImpClass() {
+        return IdentityProviderConfig.class;
+    }
+
+    public Class getInterfaceClass() {
+        return IdentityProviderConfig.class;
+    }
+
+    public String getTableName() {
+        return "identity_provider";
     }
 
     // ************************************************
@@ -112,7 +132,7 @@ public class IdProvConfManagerServer implements GlobalIdProviderConfManager {
         out.setType(EntityType.ID_PROVIDER_CONFIG);
         return out;
     }
-    protected LdapIdentityProviderConfigManager ldapConfigManager;
+    // protected LdapIdentityProviderConfigManager ldapConfigManager;
     protected InternalIdentityProviderImp internalProvider;
 
 }
