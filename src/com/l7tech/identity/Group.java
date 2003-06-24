312bd069a0d2354ceb97b0aa1be6c09f075b32d6
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-24 18:50:20 +0000

    now a class
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@875 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/identity/Group.java
+++ src/com/l7tech/identity/Group.java
@@ -1,20 +1,54 @@
 package com.l7tech.identity;
 
-import com.l7tech.objectmodel.NamedEntity;
+import com.l7tech.objectmodel.imp.NamedEntityImp;
 
 import java.util.Set;
 import java.security.Principal;
 
-public interface Group extends NamedEntity, Principal {
-    long getProviderOid();
-    String getDescription();
+public class Group extends NamedEntityImp implements Principal {
 
-    Set getMembers();
-    Set getMemberHeaders();
+    public String getDescription() {
+        return _description;
+    }
 
-    void setMembers( Set members );
-    void setMemberHeaders( Set memberHeaders );
+    public Set getMembers() {
+        return _members;
+    }
 
-    void setProviderOid( long oid );
-    void setDescription( String description );
+    public Set getMemberHeaders() {
+        return _memberHeaders;
+    }
+
+    public void setMembers( Set members ) {
+        _members = members;
+    }
+
+    public void setMemberHeaders( Set memberHeaders ) {
+        _memberHeaders = memberHeaders;
+    }
+
+    public void setDescription(String description) {
+        _description = description;
+    }
+
+    public String toString() {
+        return "com.l7tech.identity.Group." +
+                "\n\tName=" + _name;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Group)) return false;
+        final Group groupImp = (Group) o;
+        if (_oid != DEFAULT_OID ? !(_oid == groupImp._oid) : groupImp._oid != DEFAULT_OID ) return false;
+        return true;
+    }
+
+    public int hashCode() {
+        return (int)getOid();
+    }
+
+    private String _description;
+    private Set _members;
+    private Set _memberHeaders;
 }
--- src/com/l7tech/identity/User.java
+++ src/com/l7tech/identity/User.java
@@ -1,32 +1,115 @@
 package com.l7tech.identity;
 
-import com.l7tech.objectmodel.NamedEntity;
+import com.l7tech.objectmodel.imp.NamedEntityImp;
 
 import java.util.Set;
 import java.security.Principal;
 
-public interface User extends NamedEntity, Principal {
-    long getProviderOid();
-    String getLogin();
-    String getPassword();
-    String getFirstName();
-    String getLastName();
-    String getEmail();
-    String getTitle();
-    String getDepartment();
+public class User extends NamedEntityImp implements Principal {
 
-    Set getGroups();
-    Set getGroupHeaders();
+    public String getLogin() {
+        return _login;
+    }
 
-    void setGroups( Set groups );
-    void setGroupHeaders( Set groupHeaders );
+    public String getPassword() {
+        return _password;
+    }
 
-    void setProviderOid( long oid );
-    void setLogin( String login );
-    void setPassword( String password );
-    void setFirstName( String firstName );
-    void setLastName( String lastName );
-    void setEmail( String email );
-    void setTitle( String title );
-    void setDepartment( String department );
+    public String getFirstName() {
+        return _firstName;
+    }
+
+    public String getLastName() {
+        return _lastName;
+    }
+
+    public String getEmail() {
+        return _email;
+    }
+
+    public String getTitle() {
+        return _title;
+    }
+
+    public String getDepartment() {
+        return _department;
+    }
+
+    public Set getGroups() {
+        if (_groups == null) return new java.util.HashSet();
+        return _groups;
+    }
+
+    public Set getGroupHeaders() {
+        if (_groupHeaders == null) return new java.util.HashSet();
+        return _groupHeaders;
+    }
+
+    public void setGroups( Set groups ) {
+        _groups = groups;
+    }
+
+    public void setGroupHeaders( Set groupHeaders ) {
+        _groupHeaders = groupHeaders;
+    }
+
+    public void setLogin(String login) {
+        _login = login;
+    }
+
+    public void setPassword(String password) {
+        _password = password;
+    }
+
+    public void setFirstName(String firstName) {
+        _firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        _lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        _email = email;
+    }
+
+    public void setTitle(String title) {
+        _title = title;
+    }
+
+    public void setDepartment(String department) {
+        _department = department;
+    }
+
+    public String toString() {
+        return "com.l7tech.identity.User." +
+                "\n\tName=" + _name +
+                "\n\tFirst name=" + _firstName +
+                "\n\tLast name=" + _lastName +
+                "\n\tLogin=" + _login +
+                "\n\tPassword=" + _password;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        final User userImp = (User) o;
+        if (_oid != DEFAULT_OID ? !(_oid == userImp._oid) : userImp._oid != DEFAULT_OID ) return false;
+        if (_login != null ? !_login.equals(userImp._login) : userImp._login != null) return false;
+        return true;
+    }
+
+    public int hashCode() {
+        return (int)getOid();
+    }
+
+    private String _login;
+    private String _password;
+    private String _firstName;
+    private String _lastName;
+    private String _email;
+    private String _title;
+    private String _department;
+    private Set _groups;
+    private Set _groupHeaders;
 }
