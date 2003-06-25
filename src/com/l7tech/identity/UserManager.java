f254d3041f88e3f9b50c33509885ca85845abd84
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-25 17:55:19 +0000

    Added findByLogin()
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@918 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/identity/UserManager.java
+++ src/com/l7tech/identity/UserManager.java
@@ -1,13 +1,13 @@
 package com.l7tech.identity;
 
 import com.l7tech.objectmodel.*;
-import com.l7tech.identity.User;
 
 /**
  * @author alex
  */
 public interface UserManager extends EntityManager {
     public User findByPrimaryKey( String oid ) throws FindException;
+    public User findByLogin( String login ) throws FindException;
     public void delete( User user ) throws DeleteException;
     public long save( User user ) throws SaveException;
     public void update( User user ) throws UpdateException;
