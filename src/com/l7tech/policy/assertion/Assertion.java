b4d76fcaad2fb7b3e0060ae16d8a1940d57e90a0
emil <emil@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-27 07:06:20 +0000

    preorderIterator method added for traversing the assertion tree in depth first order
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@987 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/policy/assertion/Assertion.java
+++ src/com/l7tech/policy/assertion/Assertion.java
@@ -13,6 +13,7 @@ import com.l7tech.proxy.datamodel.PendingRequest;
 
 import java.io.Serializable;
 import java.io.IOException;
+import java.util.*;
 
 /**
  * Immutable except for de-persistence.
@@ -30,7 +31,7 @@ public abstract class Assertion implements Cloneable, Serializable {
      * @return AssertionStatus.NONE if this Assertion did its business successfully; otherwise, some error code
      * @throws PolicyAssertionException if processing should not continue due to a serious error
      */
-    public abstract AssertionStatus checkRequest( Request request, Response response ) throws IOException, PolicyAssertionException;
+    public abstract AssertionStatus checkRequest(Request request, Response response) throws IOException, PolicyAssertionException;
 
     /**
      * ClientProxy client-side processing of the given request.
@@ -38,7 +39,7 @@ public abstract class Assertion implements Cloneable, Serializable {
      * @return AssertionStatus.NONE if this Assertion was applied to the request successfully; otherwise, some error code
      * @throws PolicyAssertionException if processing should not continue due to a serious error
      */
-    public abstract AssertionStatus decorateRequest( PendingRequest request ) throws PolicyAssertionException;
+    public abstract AssertionStatus decorateRequest(PendingRequest request) throws PolicyAssertionException;
 
     public Assertion() {
         this.parent = null;
@@ -63,7 +64,121 @@ public abstract class Assertion implements Cloneable, Serializable {
         return clone;
     }
 
+    /**
+     * Creates and returns an iterator that traverses the assertion subtree
+     * rooted at this assertion in preorder.  The first node returned by the
+     * iterator's
+     * <code>next()</code> method is this assertion.<P>
+
+     * @return	an <code>Iterator</code> for traversing the assertion tree in
+     *          preorder
+     */
+    public Iterator preorderIterator() {
+        return new PreorderIterator(this);
+    }
+
+
     public String toString() {
         return "<" + this.getClass().getName() + ">";
     }
+
+    /**
+     * preorder depth first assertion traversal.
+     * The class is not synchronized.
+     */
+    final class PreorderIterator implements Iterator {
+        private Stack stack = new Stack(); // oh well
+        private Assertion lastReturned = null;
+
+        public PreorderIterator(Assertion rootNode) {
+            List list = new ArrayList(1);
+            list.add(rootNode);
+            stack.push(list.iterator());
+        }
+
+        public boolean hasNext() {
+            return (!stack.empty() &&
+              ((Iterator)stack.peek()).hasNext());
+        }
+
+        public Object next() {
+            Iterator iterator = null;
+
+            try {
+                iterator = (Iterator)stack.peek();
+            } catch (EmptyStackException e) {
+                throw new NoSuchElementException(); // se contract
+            }
+
+            Assertion node = (Assertion)iterator.next();
+
+            Iterator children = Collections.EMPTY_LIST.iterator();
+            if (hasChildren(node)) {
+                children = ((CompositeAssertion)node).getChildren().iterator();
+            }
+
+            if (!iterator.hasNext()) {
+                stack.pop();
+            }
+            if (children.hasNext()) {
+                stack.push(children);
+            }
+            lastReturned = node;
+            return node;
+        }
+
+        /**
+         * remove the current assertion that has been returned from
+         * the <code>next()</code> method.
+         *
+         * @throws UnsupportedOperationException if unsuported remove
+         * operation is requested. For exmaple root assertion cannot
+         * be removed.
+         *
+         * @throws IllegalStateException as described by interface
+         * {@see Iterator}
+         */
+        public void remove()
+          throws UnsupportedOperationException, IllegalStateException {
+            if (lastReturned == null) {
+                throw new IllegalStateException();
+            }
+            CompositeAssertion assparent = lastReturned.getParent();
+            if (assparent == null) { // remove from parent
+                throw new UnsupportedOperationException("cannot remove root");
+            }
+            // pop children that were pushed in next()
+            if (hasChildren(lastReturned)) {
+                stack.pop();
+            }
+
+
+            List nc = new ArrayList(assparent.getChildren());
+            boolean removed = false;
+            Iterator i;
+
+            for (i = nc.iterator(); i.hasNext();) {
+                if (lastReturned.equals(i.next())) {
+                    i.remove();
+                    removed = true;
+                    break;
+                }
+            }
+            if (!removed) {
+                throw new IllegalStateException("Object missing in parent: "+lastReturned);
+            }
+            assparent.setChildren(nc);
+            if (i.hasNext()) {
+                stack.pop();
+                stack.push(i); // replace with new iterator
+            }
+            lastReturned = null;
+        }
+
+        private boolean hasChildren(Assertion a) {
+            return (a instanceof CompositeAssertion &&
+              ((CompositeAssertion)a).getChildren().size() > 0);
+        }
+    }
 }
+
