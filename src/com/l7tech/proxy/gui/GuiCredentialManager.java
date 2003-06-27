54daedbc6b9a090d6ebfd4a7c849fa608f9ad0c0
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-27 18:45:24 +0000

    A CredentialManager that pops up a login dialog box to get the credentials when they are missing.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@998 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/proxy/gui/GuiCredentialManager.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2003 Layer 7 Technologies Inc.
+ *
+ * $Id$
+ */
+
+package com.l7tech.proxy.gui;
+
+import com.l7tech.proxy.datamodel.CredentialManager;
+import com.l7tech.proxy.datamodel.Ssg;
+
+import javax.swing.*;
+import java.net.PasswordAuthentication;
+import java.lang.reflect.InvocationTargetException;
+
+import org.apache.log4j.Category;
+
+/**
+ * GUI implementation of the CredentialManager.
+ *
+ * User: mike
+ * Date: Jun 27, 2003
+ * Time: 10:36:01 AM
+ */
+public class GuiCredentialManager implements CredentialManager {
+    private static final Category log = Category.getInstance(GuiCredentialManager.class);
+    private static GuiCredentialManager INSTANCE = new GuiCredentialManager();
+
+    private GuiCredentialManager() {}
+
+    public static GuiCredentialManager getInstance() {
+        return INSTANCE;
+    }
+
+    /**
+     * Load credentials for this SSG.  If the SSG already contains credentials they will be
+     * overwritten with new ones.  For the GUI, we'll pop up a login window on the main Swing thread
+     * and hold here until it finishes.
+     * @param ssg
+     */
+    public void getCredentials(final Ssg ssg) {
+        // If this SSG isn't suppose to be hassling us with dialog boxes, stop now
+        if (!ssg.isPromptForUsernameAndPassword())
+            return;
+
+        try {
+            SwingUtilities.invokeAndWait(new Runnable() {
+                public void run() {
+                    PasswordAuthentication pw = LogonDialog.logon(Gui.getInstance().getFrame(), ssg.toString());
+                    if (pw == null) {
+                        if (ssg.incrementNumTimesLogonDialogCanceled() > 2) {
+                            ssg.setPromptForUsernameAndPassword(false);
+                            return;
+                        }
+                    }
+                    ssg.setUsername(pw.getUserName());
+                    ssg.setPassword(new String(pw.getPassword())); // TODO: encoding?
+                }
+            });
+        } catch (InterruptedException e) {
+            log.error(e);
+        } catch (InvocationTargetException e) {
+            log.error(e);
+        }
+    }
+
+    /**
+     * Notify that the credentials for this SSG have been tried and found to be no good.
+     * In the GUI, for now we'll just pop up an error dialog (yuck).
+     */
+    public void notifyInvalidCredentials(final Ssg ssg) {
+        // If this SSG isn't suppose to be hassling us with dialog boxes, stop now
+        if (!ssg.isPromptForUsernameAndPassword())
+            return;
+
+        try {
+            SwingUtilities.invokeAndWait(new Runnable() {
+                public void run() {
+                    Gui.errorMessage("Invalid username or password for SSG " + ssg);
+                }
+            });
+        } catch (InterruptedException e) {
+            log.error(e);
+        } catch (InvocationTargetException e) {
+            log.error(e);
+        }
+    }
+}
