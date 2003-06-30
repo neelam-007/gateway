9db77bad6ace1d0e70aa3d952d7aac7cff87c012
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-30 23:47:45 +0000

    simple perl script that md5s whatever is passed used by resetAdmin.sh when recording the admin password in database.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1078 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ etc/md5passwd.pl
@@ -0,0 +1,12 @@
+# -----------------------------------------------------------------------------
+# FILE [md5passwd.pl]
+# LAYER 7 TECHNOLOGIES
+# 30-06-2003, flascelles
+#
+# THIS SCRIPT PRODUCES THE HEX MD5 OF WHATEVER IS PASSED AS AN ARGUMENT
+#
+# this had been placed in an individual script because I was getting shell quoting issues
+#
+# -----------------------------------------------------------------------------
+use Digest::MD5 qw(md5_hex);
+print md5_hex($ARGV[0]);
