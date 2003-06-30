ae2991f78cfd008de6953272efbfebca9ce78eb2
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-30 23:46:49 +0000

    this script generates a keystore for use by tomcat for ssl. is prompt the user to export the certificate to a floppy for import in the console trusted keystore.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1077 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ etc/sslGen.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+# -----------------------------------------------------------------------------
+# FILE [sslGen.sh]
+# LAYER 7 TECHNOLOGIES
+# 30-06-2003, flascelles
+#
+# THIS SCRIPT GENERATES A CERTIFICATE FOR SSL USE OF TOMCAT
+#
+# PREREQUISITES
+# 1. THE DIRECTORY JAVA_HOME/bin IS PART OF THE PATH
+# 2. TOMCAT_HOME is set
+# 3. server.xml has already been copied to the tomcat_home/conf/ directory and contains
+#   Server/Service/Connector/Factory@keystorePass attribute
+#
+# POSTREQUISITES:
+# 1. Once the certificate has been generated, the certificate must be imported on the console PC.
+#
+# Command to import the certificate in the console store:
+# keytool -import -v -trustcacerts -alias tomcat -file ssg.cer -keystore somedirectory/client.keystore -keypass changeit -storepass changeit
+# Command to explicitely declare the trustStore on the console:
+# System.setProperty("javax.net.ssl.trustStore", "somedirectory/client.keystore");
+#
+# -----------------------------------------------------------------------------
+#
+
+KEYSTORE_FILE=$TOMCAT_HOME/kstores/tomcatSsl
+CERTIFICATE_FILE=$TOMCAT_HOME/kstores/ssg.cer
+SERVER_XML_FILE=$TOMCAT_HOME/conf/server.xml
+
+# VERIFY THAT THE TOMCAT_HOME VARIABLE IS SET
+if [ ! $TOMCAT_HOME ]; then
+        echo "ERROR: TOMCAT_HOME not set"
+        echo
+        exit -1
+fi
+
+# VERIFY THAT THE KEYSTORE IS NOT YET SET
+if [ -e $KEYSTORE_FILE ]; then
+        echo "THE KEYSTORE ALREADY EXIST."
+        echo "DO YOU WANT TO OVERWRITE? [y/n]"
+        read ADMIN_ANSWER
+        if [ $ADMIN_ANSWER = "n" ]
+        then
+                exit -1
+                echo
+        else
+                /bin/rm $KEYSTORE_FILE
+        fi
+fi
+
+# GET A KEYSTORE PASSWORD FROM CALLER
+echo "Please provide an keystore password"
+read -s KEYSTORE_PASSWORD
+echo "Please repeat"
+read -s KEYSTORE_PASSWORD_REPEAT
+
+# VERIFY THAT PASSWORDS ARE EQUAL
+if [ ! $KEYSTORE_PASSWORD = $KEYSTORE_PASSWORD_REPEAT ]; then
+	echo "ERROR : passwords do not match"
+	exit -1
+fi
+
+# VERIFY THAT THE PASSWORD IS LONG ENOUGH
+PASSWORD_LENGTH=${#KEYSTORE_PASSWORD}
+if [ "$PASSWORD_LENGTH" -lt 6 ]; then
+	echo "ERROR : the admin password must be at least 6 characters long"
+	exit -1
+fi
+
+# GENERATE THE KEYSTORE FILE
+echo "GENERATING THE KEYSTORE FILE FOR TOMCAT SSL"
+echo "Important: During the key generation, it is important that you enter the COMMON NAME value (CN) properly."
+echo "This value must be equal to the host name that the client uses to reach the ssg (e.g. ssg.acme.com)."
+echo
+echo "PRESS ANY KEY TO CONTINUE"
+read
+keytool -genkey -alias tomcat -keystore $KEYSTORE_FILE -keyalg RSA -keypass $KEYSTORE_PASSWORD -storepass $KEYSTORE_PASSWORD
+
+# CHECK THAT THIS KEYSTORE WAS SET SUCCESSFULLY
+if [ -e $KEYSTORE_FILE ]
+then
+# EXPORT THE SERVER CERTIFICATE
+        keytool -export -alias tomcat -storepass $KEYSTORE_PASSWORD -file $CERTIFICATE_FILE -keystore $KEYSTORE_FILE
+
+# EDIT THE server.xml file so that the magic value "__FunkySsgMojo__" is replaced by the actual password
+perl -pi.bak -e s/keystorePass=\".*\"/keystorePass=\"$KEYSTORE_PASSWORD\"/ $SERVER_XML_FILE
+
+# ASK THE USER IF HE WANTS TO COPY THE CERTIFICATE TO FLOPPY?
+        echo
+        echo
+        echo "The certificate was generated successfully."
+        echo "It must be imported in your Admin Console."
+        echo
+        echo 'DO YOU WANT TO COPY THE CERTIFICATE TO A FLOPPY? [y/n]'
+        read QUERY_ANSWER
+        if [ $QUERY_ANSWER = "y" ]; then
+                mount /mnt/floppy
+                cp $CERTIFICATE_FILE /mnt/floppy/ssg.cer
+        fi
+
+else
+# INFORM THE USER OF THE FAILURE
+        echo "ERROR: The keystore file was not generated"
+        echo
+        exit -1
+fi
