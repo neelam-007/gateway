95d6479385f4ba74870c97c57f8b799bf6d72e20
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-30 18:00:59 +0000

    A RequestInterceptor that logs all messages in and out.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1033 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/proxy/MessageLogger.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2003 Layer 7 Technologies Inc.
+ *
+ * $Id$
+ */
+
+package com.l7tech.proxy;
+
+import org.apache.axis.message.SOAPEnvelope;
+import org.apache.log4j.Category;
+
+/**
+ * RequestInterceptor that logs all messages in and out.
+ * User: mike
+ * Date: Jun 30, 2003
+ * Time: 10:52:55 AM
+ */
+public class MessageLogger implements RequestInterceptor {
+    private final static Category log = Category.getInstance(MessageLogger.class);
+
+    public MessageLogger() {
+    }
+
+    /**
+     * Fired when a message is received from a client, after it is parsed.
+     * @param message
+     */
+    public void onReceiveMessage(SOAPEnvelope message) {
+        log.info("Received client request: " + message);
+    }
+
+    /**
+     * Fired when a reply is read from the SSG, after it is parsed.
+     * @param reply
+     */
+    public void onReceiveReply(SOAPEnvelope reply) {
+        log.info("Received server response: " + reply);
+    }
+
+    /**
+     * Fired when an error is encountered while reading the message from a client.
+     * @param t The error that occurred during the request.
+     */
+    public void onMessageError(Throwable t) {
+        log.info("Error while processing request from client: " + t);
+    }
+
+    /**
+     * Fired when an error is encountered while obtaining a reply from the server.
+     * @param t The error that occurred during the request.
+     */
+    public void onReplyError(Throwable t) {
+        log.info("Error while processing response from server: " + t);
+    }
+}
