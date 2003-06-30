3b7139554850838dc0e9d7d8a87cc333a2350037
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-30 18:00:31 +0000

    com.l7tech.proxy.Main now starts a Client Proxy in Daemon mode (no interactive UI at all).  The GUI-mode entry point is now com.l7tech.proxy.gui.Main.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1031 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/proxy/Main.java
+++ src/com/l7tech/proxy/Main.java
@@ -1,30 +1,17 @@
 package com.l7tech.proxy;
 
-import org.mortbay.util.InetAddrPort;
-import org.mortbay.util.MultiException;
-import org.mortbay.http.HttpServer;
-import org.mortbay.http.HttpContext;
-import org.mortbay.http.SocketListener;
-
-import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
-import java.security.Provider;
-
-import com.l7tech.proxy.gui.Gui;
-import com.l7tech.proxy.gui.GuiCredentialManager;
 import com.l7tech.proxy.datamodel.Managers;
-
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManagerFactory;
-import javax.net.ssl.TrustManager;
+import org.apache.log4j.Category;
+import org.mortbay.util.MultiException;
 
 /**
- * Begin execution of client proxy along with an attached GUI.
+ * Begin execution of daemon-mode (no UI at all) client proxy.
  * User: mike
  * Date: May 15, 2003
  * Time: 3:33:14 PM
  */
 public class Main {
+    private static final Category log = Category.getInstance(Main.class);
     private static final int DEFAULT_PORT = 5555;
     private static final int MIN_THREADS = 4;
     private static final int MAX_THREADS = 20;
@@ -33,35 +20,24 @@ public class Main {
 
     /** Start a GUI-equipped client proxy and run it until it's shut down. */
     public static void main(final String[] argv) {
+        log.info("Starting new daemon mode Client Proxy");
+
         clientProxy = new ClientProxy(Managers.getSsgManager(),
                                       new MessageProcessor(Managers.getPolicyManager()),
                                       DEFAULT_PORT,
                                       MIN_THREADS,
                                       MAX_THREADS);
 
-        // Hook up the Message Viewer window
-        clientProxy.getRequestHandler().setRequestInterceptor(Gui.getInstance().getRequestInterceptor());
+        // Hook up the Message Logger facility
+        clientProxy.getRequestHandler().setRequestInterceptor(new MessageLogger());
 
         try {
             clientProxy.start();
         } catch (MultiException e) {
-            Gui.errorMessage("Unable to start the Client Proxy: " + e);
-            System.err.println("Unable to start httpServer");
-            e.printStackTrace(System.err);
+            log.error("Unable to start Client Proxy: " + e);
+            log.error(e);
             System.exit(2);
         }
-
-        // Make sure the proxy stops when the GUI does.
-        Gui.getInstance().setShutdownListener(new Gui.ShutdownListener() {
-            public void guiShutdown() {
-                clientProxy.stop();
-                System.exit(0);
-            }
-        });
-
-        Managers.setCredentialManager(GuiCredentialManager.getInstance());
-
-        Gui.getInstance().start();
 
         // We have nothing else for the main thread to do.
         return;
