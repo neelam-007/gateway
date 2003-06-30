f312e435dda1f5492ebe230ff42f58b040bb5b8a
emil <emil@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-30 16:31:52 +0000

    All tests for the policy tests
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1029 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ tests/com/l7tech/policy/AllTests.java
@@ -0,0 +1,55 @@
+package com.l7tech.policy;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import com.l7tech.policy.validator.DefaultPolicyValidatorTest;
+
+/**
+ * Class <code>AllTests</code> defines all tests to be run for the policy
+ * package.
+ * <p>
+ * @author <a href="mailto:emarceta@layer7-tech.com">Emil Marceta</a>
+ */
+public class AllTests extends TestCase {
+    /**
+     * test <code>AllTests</code> constructor
+     */
+    public AllTests(String name) {
+        super(name);
+    }
+
+    /**
+     * create the <code>TestSuite</code> for the containing the tests for
+     * the policy package.
+     * <p>
+     * Add new tests at the bottom of the list.
+     *
+     */
+    public static Test suite() {
+        TestSuite suite = new TestSuite();
+    	suite.addTest(new TestSuite(SamplePolicyTest.class));
+        suite.addTest(new TestSuite(WspReaderTest.class));
+        suite.addTest(new TestSuite(WspWriterTest.class));
+        suite.addTest(new TestSuite(CompositeAssertionTest.class));
+        suite.addTest(new TestSuite(AssertionTraversalTest.class));
+        suite.addTest(new TestSuite(DefaultPolicyValidatorTest.class));
+        return suite;
+    }
+
+    public void setUp() throws Exception {
+        // put set up code here
+    }
+
+    public void tearDown() throws Exception {
+        // put tear down code here
+    }
+
+    /**
+     * Test <code>AllTests</code> main.
+     */
+    public static void main(String[] args) throws
+      Throwable {
+        junit.textui.TestRunner.run(suite());
+    }
+}
