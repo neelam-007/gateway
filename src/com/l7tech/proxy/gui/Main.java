9f00918cb31daeca09c83876d6a361498e297c50
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-06-30 18:00:46 +0000

    The GUI-mode entry point has been moved here.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1032 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/proxy/gui/Main.java
@@ -0,0 +1,61 @@
+package com.l7tech.proxy.gui;
+
+import com.l7tech.proxy.ClientProxy;
+import com.l7tech.proxy.MessageProcessor;
+import com.l7tech.proxy.datamodel.Managers;
+import org.mortbay.util.MultiException;
+import org.apache.log4j.Category;
+
+/**
+ * Begin execution of client proxy along with an attached GUI.
+ * User: mike
+ * Date: May 15, 2003
+ * Time: 3:33:14 PM
+ */
+public class Main {
+    private static final Category log = Category.getInstance(Main.class);
+    private static final int DEFAULT_PORT = 5555;
+    private static final int MIN_THREADS = 4;
+    private static final int MAX_THREADS = 20;
+
+    private static ClientProxy clientProxy;
+
+    /** Start a GUI-equipped client proxy and run it until it's shut down. */
+    public static void main(final String[] argv) {
+        log.info("Starting new GUI-mode Client Proxy");
+
+        clientProxy = new ClientProxy(Managers.getSsgManager(),
+                                      new MessageProcessor(Managers.getPolicyManager()),
+                                      DEFAULT_PORT,
+                                      MIN_THREADS,
+                                      MAX_THREADS);
+
+        // Hook up the Message Viewer window
+        clientProxy.getRequestHandler().setRequestInterceptor(Gui.getInstance().getRequestInterceptor());
+
+        try {
+            clientProxy.start();
+        } catch (MultiException e) {
+            Gui.errorMessage("Unable to start the Client Proxy: " + e);
+            System.err.println("Unable to start httpServer");
+            e.printStackTrace(System.err);
+            System.exit(2);
+        }
+
+        // Make sure the proxy stops when the GUI does.
+        Gui.getInstance().setShutdownListener(new Gui.ShutdownListener() {
+            public void guiShutdown() {
+                clientProxy.stop();
+                System.exit(0);
+            }
+        });
+
+        Managers.setCredentialManager(GuiCredentialManager.getInstance());
+
+        Gui.getInstance().start();
+
+        // We have nothing else for the main thread to do.
+        return;
+    }
+}
+
