194ccaeb807a08cf7b25a7d45a472a268c8e9a35
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-07-03 00:08:21 +0000

    script that imports a ssg certificate into trust store of proxy or console
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1151 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ etc/importTrustedCertificate.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# -----------------------------------------------------------------------------
+# FILE [importTrustedCertificate.sh]
+# LAYER 7 TECHNOLOGIES
+# 02-07-2003, flascelles
+#
+# THIS SCRIPT IMPORTS A SSG CERTIFICATE INTO THE TRUSTED STORE OF THE SSG CONSOLE
+# OR SSG PROXY
+#
+# PREREQUISITES
+# 0. pass the certificate as argument
+# 1. $JAVA_HOME be defined
+# 2. $JAVA_HOME/bin/keytool be there
+#
+# -----------------------------------------------------------------------------
+#
+
+
+STOREDIR=$HOME/.ssg
+STOREFILE=$STOREDIR/trustStore
+KEYTOOLBIN=$JAVA_HOME/bin/keytool
+
+# CHECK THAT WE ARE RECIEVING THE CERTIFICATE FILE AS AN ARGUMENT
+if [ ! $1 ]; then
+    echo USAGE: %0 filetoimport.cer
+    exit -1
+fi
+
+# VERIFY THAT THE KEYSTORE IS NOT YET SET
+if [ -e $STOREFILE ]; then
+        echo "THE KEYSTORE ALREADY EXIST AT $STOREFILE"
+        echo "DO YOU WANT TO OVERWRITE? [y/n]"
+        read USR_ANSWER
+        if [ USR_ANSWER = "n" ]; then
+            exit -1
+            echo
+        fi
+fi
+
+# ENSURE THE DIRECTORY IS THERE
+if [ ! -e $STOREDIR ]; then
+    mkdir $STOREDIR
+fi
+
+
+# IMPORT THE CERTIFICATE
+$KEYTOOLBIN -import -v -trustcacerts -alias tomcat -file $1 -keystore $STOREFILE -keypass password -storepass password
