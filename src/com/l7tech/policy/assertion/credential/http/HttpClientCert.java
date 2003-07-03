b25b738fcbe0a75a72a0f81d32fe6a8fe4d29888
emil <emil@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-07-03 04:45:23 +0000

    Make not abstract so it can be instantiated (returning NYI only)
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1168 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/policy/assertion/credential/http/HttpClientCert.java
+++ src/com/l7tech/policy/assertion/credential/http/HttpClientCert.java
@@ -11,12 +11,14 @@ import com.l7tech.message.Response;
 import com.l7tech.credential.CredentialFinderException;
 import com.l7tech.credential.http.HttpDigestCredentialFinder;
 import com.l7tech.policy.assertion.AssertionStatus;
+import com.l7tech.policy.assertion.PolicyAssertionException;
+import com.l7tech.proxy.datamodel.PendingRequest;
 
 /**
  * @author alex
  * @version $Revision$
  */
-public abstract class HttpClientCert extends HttpCredentialSourceAssertion {
+public class HttpClientCert extends HttpCredentialSourceAssertion {
     public AssertionStatus doCheckRequest(Request request, Response response) throws CredentialFinderException {
         // FIXME: Implement
         return AssertionStatus.NOT_YET_IMPLEMENTED;
@@ -24,5 +26,15 @@ public abstract class HttpClientCert extends HttpCredentialSourceAssertion {
 
     public Class getCredentialFinderClass() {
         return HttpDigestCredentialFinder.class;
+    }
+
+    /**
+     * ClientProxy client-side processing of the given request.
+     * @param request    The request to decorate.
+     * @return AssertionStatus.NONE if this Assertion was applied to the request successfully; otherwise, some error code
+     * @throws PolicyAssertionException if processing should not continue due to a serious error
+     */
+    public AssertionStatus decorateRequest(PendingRequest request) throws PolicyAssertionException {
+        return AssertionStatus.NOT_YET_IMPLEMENTED;
     }
 }
