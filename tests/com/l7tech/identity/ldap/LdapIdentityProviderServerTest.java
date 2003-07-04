6390a70e82c84228cea955d8e11d32cf5bfc05c1
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-07-04 22:05:05 +0000

    added test for ldap authentication
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1225 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- tests/com/l7tech/identity/ldap/LdapIdentityProviderServerTest.java
+++ tests/com/l7tech/identity/ldap/LdapIdentityProviderServerTest.java
@@ -57,7 +57,7 @@ public class LdapIdentityProviderServerTest extends junit.framework.TestCase {
     public void testFindAllGroups() throws Exception {
         IdentityProviderConfig config = new IdentityProviderConfig(IdentityProviderType.LDAP);
         // use this url when ssh forwarding locally
-        //config.putProperty(LdapConfigSettings.LDAP_HOST_URL, "ldap://localhost:3899");
+        // config.putProperty(LdapConfigSettings.LDAP_HOST_URL, "ldap://localhost:3899");
         // use this url when in the office
         config.putProperty(LdapConfigSettings.LDAP_HOST_URL, "ldap://spock:389");
         config.putProperty(LdapConfigSettings.LDAP_SEARCH_BASE, "dc=layer7-tech,dc=com");
@@ -81,6 +81,28 @@ public class LdapIdentityProviderServerTest extends junit.framework.TestCase {
             if (group != null) grpsReceived = true;
         }
         assertTrue("receiving groups", grpsReceived);
+    }
+
+    public void testAuthenticate() throws Exception {
+        IdentityProviderConfig config = new IdentityProviderConfig(IdentityProviderType.LDAP);
+        // use this url when ssh forwarding locally
+        // config.putProperty(LdapConfigSettings.LDAP_HOST_URL, "ldap://localhost:3899");
+        // use this url when in the office
+        config.putProperty(LdapConfigSettings.LDAP_HOST_URL, "ldap://spock:389");
+        config.putProperty(LdapConfigSettings.LDAP_SEARCH_BASE, "dc=layer7-tech,dc=com");
+
+        // create the provider
+        LdapIdentityProviderServer provider = new LdapIdentityProviderServer();
+        provider.initialize(config);
+
+        // get the user manager
+        UserManager me = provider.getUserManager();
+        System.out.println("retrieving user");
+        User francois = me.findByLogin("flascelles");
+        System.out.println(francois);
+        System.out.println("authenticating");
+        if (provider.authenticate(francois, "rockclimbing".getBytes())) System.out.println("OK");
+        else System.out.println("not authenticated");
     }
 
     public static void main(String[] args) throws Exception {
