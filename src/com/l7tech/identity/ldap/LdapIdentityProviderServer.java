cbd3f252e0ccc29fce92a8c266bcb961d85014a8
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-07-05 00:34:23 +0000

    verify user before authentication
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1257 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/identity/ldap/LdapIdentityProviderServer.java
+++ src/com/l7tech/identity/ldap/LdapIdentityProviderServer.java
@@ -4,6 +4,7 @@ import com.l7tech.identity.*;
 import com.l7tech.credential.PrincipalCredentials;
 import com.l7tech.credential.CredentialFormat;
 import com.l7tech.logging.LogManager;
+import com.l7tech.objectmodel.FindException;
 
 import java.util.logging.Level;
 
@@ -34,9 +35,16 @@ public class LdapIdentityProviderServer implements com.l7tech.identity.IdentityP
     }
 
     public boolean authenticate( PrincipalCredentials pc ) {
+        User realUser = null;
+        try {
+            realUser = userManager.findByLogin(pc.getUser().getLogin());
+        } catch (FindException e) {
+            LogManager.getInstance().getSystemLogger().log(Level.SEVERE, "invalid user", e);
+            return false;
+        }
         if ( pc.getFormat() == CredentialFormat.CLEARTEXT ) {
             // basic authentication
-            return userManager.authenticateBasic( pc.getUser().getName(), new String( pc.getCredentials() ));
+            return userManager.authenticateBasic( realUser.getName(), new String( pc.getCredentials() ));
         } else {
             LogManager.getInstance().getSystemLogger().log(Level.SEVERE, "Attempt to authenticate using unsupported method" + pc.getFormat());
             throw new IllegalArgumentException( "Only cleartext credentials are currently supported!" );
