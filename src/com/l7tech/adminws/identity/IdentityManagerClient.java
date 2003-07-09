46fcbd356ed8fd4b677ee8dddd470fb164867781
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-07-09 22:18:42 +0000

    moved responsibility of getting creds to the actual creds manager
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1358 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/adminws/identity/IdentityManagerClient.java
+++ src/com/l7tech/adminws/identity/IdentityManagerClient.java
@@ -1,9 +1,6 @@
 package com.l7tech.adminws.identity;
 
 import com.l7tech.identity.IdentityProviderConfig;
-import com.l7tech.adminws.ClientCredentialManager;
-import com.l7tech.util.Locator;
-
 import java.io.IOException;
 
 /**
@@ -16,11 +13,6 @@ public class IdentityManagerClient {
 
     public IdentityManagerClient(IdentityProviderConfig config) {
         this.config = config;
-        credentialManager =
-          (ClientCredentialManager)Locator.getDefault().lookup(ClientCredentialManager.class);
-        if (credentialManager == null) { // bug
-            throw new RuntimeException("No credential manager configured in services");
-        }
     }
     // ************************************************
     // PRIVATES
@@ -28,7 +20,7 @@ public class IdentityManagerClient {
     protected Client getStub() throws java.rmi.RemoteException {
         if (localStub == null) {
             try {
-                localStub = new Client(getServiceURL(), getAdminUsername(), getAdminPassword());
+                localStub = new Client(getServiceURL()/*, getAdminUsername(), getAdminPassword()*/);
             }
             catch (Exception e) {
                 throw new java.rmi.RemoteException("cannot instantiate the admin service stub", e);
@@ -39,23 +31,14 @@ public class IdentityManagerClient {
     protected String getServiceURL() throws IOException {
         String prefUrl = com.l7tech.console.util.Preferences.getPreferences().getServiceUrl();
         if (prefUrl == null || prefUrl.length() < 1 || prefUrl.equals("null/ssg")) {
-            System.err.println("com.l7tech.console.util.Preferences.getPreferences does not resolve a server address");
-            prefUrl = "http://localhost:8080/ssg";
+            throw new IOException("com.l7tech.console.util.Preferences.getPreferences does not resolve a server address");
+            // System.err.println("com.l7tech.console.util.Preferences.getPreferences does not resolve a server address");
+            // prefUrl = "http://localhost:8080/ssg";
         }
-        prefUrl += "/services/identityAdmin";
+        prefUrl += Service.SERVICE_DEPENDENT_URL_PORTION;
         return prefUrl;
-        //return "http://localhost:8080/UneasyRooster/services/identities";
     }
 
-    protected String getAdminUsername() throws IOException {
-        return credentialManager.getUsername();
-    }
-
-    protected String getAdminPassword() throws IOException {
-        return credentialManager.getPassword();
-    }
-
-    private ClientCredentialManager credentialManager = null;
     protected Client localStub = null;
     protected IdentityProviderConfig config;
 }
