ffd0a653919c01519488ea940b84354316a6aa4b
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-07-10 18:06:50 +0000

    Added some group/header helpers
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1390 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/identity/internal/InternalGroupManagerServer.java
+++ src/com/l7tech/identity/internal/InternalGroupManagerServer.java
@@ -4,8 +4,10 @@ import com.l7tech.objectmodel.*;
 import com.l7tech.identity.GroupManager;
 import com.l7tech.identity.Group;
 import com.l7tech.identity.IdProvConfManagerServer;
+import com.l7tech.logging.LogManager;
 
 import java.sql.SQLException;
+import java.util.*;
 
 /**
  * Layer 7 Technologies, inc.
@@ -51,6 +53,47 @@ public class InternalGroupManagerServer extends HibernateEntityManager implement
             throw new UpdateException( se.toString(), se );
         }
     }
+
+    public EntityHeader groupToHeader( Group group ) {
+        return new EntityHeader( group.getOid(), EntityType.GROUP, group.getName(), group.getDescription() );
+    }
+
+    public Group headerToGroup( EntityHeader header ) throws FindException {
+        return findByPrimaryKey( header.getStrId() );
+    }
+
+    public Set groupsToHeaders(Set groups) {
+        Group group;
+        EntityHeader header;
+        Set result = Collections.EMPTY_SET;
+        for (Iterator i = groups.iterator(); i.hasNext();) {
+            group = (Group) i.next();
+            if ( result == Collections.EMPTY_SET ) result = new HashSet();
+            header = groupToHeader( group );
+            result.add( header );
+        }
+        return result;
+    }
+
+    public Set headersToGroups(Set headers) throws FindException {
+        Group group;
+        EntityHeader header;
+        Set result = Collections.EMPTY_SET;
+        for (Iterator i = headers.iterator(); i.hasNext();) {
+            header = (EntityHeader) i.next();
+            if ( header.getType() == EntityType.GROUP ) {
+                group = headerToGroup( header );
+                if ( result == Collections.EMPTY_SET ) result = new HashSet();
+                result.add( group );
+            } else {
+                IllegalArgumentException iae = new IllegalArgumentException( "EntityHeader " + header + " doesn't represent a Group!" );
+                LogManager.getInstance().getSystemLogger().throwing( getClass().getName(), "headersToGroups", iae );
+                throw iae;
+            }
+        }
+        return result;
+    }
+
 
     public String getTableName() {
         return "internal_group";
