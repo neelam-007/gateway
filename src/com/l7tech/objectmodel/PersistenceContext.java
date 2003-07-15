2f91eeb3be5242f12ec674a9d8b832a05dd31aa0
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-07-15 17:00:57 +0000

    Removed exceptions from close()
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1514 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/objectmodel/PersistenceContext.java
+++ src/com/l7tech/objectmodel/PersistenceContext.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
  * @author alex
  */
 public abstract class PersistenceContext {
-    public abstract void close() throws SQLException, ObjectModelException;
+    public abstract void close();
     public abstract void beginTransaction() throws TransactionException;
     public abstract void commitTransaction() throws TransactionException;
     public abstract void rollbackTransaction() throws TransactionException;
