5b25134bfe265baeaf77bb999bf42025f38cc590
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-07-21 20:52:11 +0000

    implemented search (using children searches)
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1589 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/identity/internal/InternalIdentityProviderServer.java
+++ src/com/l7tech/identity/internal/InternalIdentityProviderServer.java
@@ -3,6 +3,7 @@ package com.l7tech.identity.internal;
 import com.l7tech.identity.*;
 import com.l7tech.objectmodel.FindException;
 import com.l7tech.objectmodel.EntityType;
+import com.l7tech.objectmodel.EntityHeaderComparator;
 import com.l7tech.logging.LogManager;
 import com.l7tech.credential.PrincipalCredentials;
 import com.l7tech.credential.CredentialFormat;
@@ -10,6 +11,7 @@ import com.l7tech.credential.CredentialFormat;
 import java.io.UnsupportedEncodingException;
 import java.util.logging.Level;
 import java.util.Collection;
+import java.util.TreeSet;
 
 /**
  * Layer 7 Technologies, inc.
@@ -79,8 +81,26 @@ public class InternalIdentityProviderServer implements IdentityProvider {
 
     public boolean isReadOnly() { return false; }
 
+    /**
+     * searches for users and groups whose name (cn) match the pattern described in searchString
+     * pattern may include wildcard such as * character. result is sorted by name property
+     *
+     * todo: (once we dont use hibernate?) replace this by one union sql query and have the results sorted
+     * instead of sorting in collection.
+     */
     public Collection search(EntityType[] types, String searchString) throws FindException {
-        throw new FindException("not implemented");
+        if (types == null || types.length < 1) throw new IllegalArgumentException("must pass at least one type");
+        boolean wantUsers = false;
+        boolean wantGroups = false;
+        for (int i = 0; i < types.length; i++) {
+            if (types[i] == EntityType.USER) wantUsers = true;
+            else if (types[i] == EntityType.GROUP) wantGroups = true;
+        }
+        if (!wantUsers && !wantGroups) throw new IllegalArgumentException("types must contain users and or groups");
+        Collection searchResults = new TreeSet(new EntityHeaderComparator());
+        if (wantUsers) searchResults.addAll(userManager.search(searchString));
+        if (wantGroups) searchResults.addAll(groupManager.search(searchString));
+        return searchResults;
     }
 
     private IdentityProviderConfig cfg;
