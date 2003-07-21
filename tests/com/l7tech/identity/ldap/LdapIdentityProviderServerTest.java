c7423d849d1a08e581fdb496dc5576a264ccb9af
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-07-21 20:54:25 +0000

    constructor + testSearch
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1591 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- tests/com/l7tech/identity/ldap/LdapIdentityProviderServerTest.java
+++ tests/com/l7tech/identity/ldap/LdapIdentityProviderServerTest.java
@@ -4,6 +4,7 @@ import com.l7tech.credential.CredentialFormat;
 import com.l7tech.credential.PrincipalCredentials;
 import com.l7tech.identity.*;
 import com.l7tech.objectmodel.EntityHeader;
+import com.l7tech.objectmodel.EntityType;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 import java.util.Collection;
@@ -19,24 +20,26 @@ import java.util.Iterator;
  * and containing at least one user and one group.
  */
 public class LdapIdentityProviderServerTest extends junit.framework.TestCase {
+
+    public LdapIdentityProviderServerTest() {
+        IdentityProviderConfig config = new IdentityProviderConfig(IdentityProviderType.LDAP);
+        // use this url when ssh forwarding locally
+        config.putProperty(LdapConfigSettings.LDAP_HOST_URL, "ldap://localhost:3899");
+        // use this url when in the office
+        // config.putProperty(LdapConfigSettings.LDAP_HOST_URL, "ldap://spock:389");
+        config.putProperty(LdapConfigSettings.LDAP_SEARCH_BASE, "dc=layer7-tech,dc=com");
+
+        // create the provider
+        provider = new LdapIdentityProviderServer();
+        provider.initialize(config);
+    }
+
     public static Test suite() {
         TestSuite suite = new TestSuite(LdapIdentityProviderServerTest.class);
         return suite;
     }
 
     public void testFindAllUsers() throws Exception {
-        IdentityProviderConfig config = new IdentityProviderConfig(IdentityProviderType.LDAP);
-        // use this url when ssh forwarding locally
-        // config.putProperty(LdapConfigSettings.LDAP_HOST_URL, "ldap://localhost:3899");
-        // use this url when in the office
-        config.putProperty(LdapConfigSettings.LDAP_HOST_URL, "ldap://spock:389");
-        config.putProperty(LdapConfigSettings.LDAP_SEARCH_BASE, "dc=layer7-tech,dc=com");
-
-        // create the provider
-        LdapIdentityProviderServer provider = new LdapIdentityProviderServer();
-        provider.initialize(config);
-
-
         // get the user manager
         UserManager me = provider.getUserManager();
 
@@ -49,24 +52,12 @@ public class LdapIdentityProviderServerTest extends junit.framework.TestCase {
             User usr = me.findByPrimaryKey(header.getStrId());
             if (usr != null) usrsReceived = true;
             // uncomment if you want to see the content
-            // System.out.println(usr);
+            System.out.println(usr);
         }
         assertTrue("receiving users", usrsReceived);
     }
 
     public void testFindAllGroups() throws Exception {
-        IdentityProviderConfig config = new IdentityProviderConfig(IdentityProviderType.LDAP);
-        // use this url when ssh forwarding locally
-        // config.putProperty(LdapConfigSettings.LDAP_HOST_URL, "ldap://localhost:3899");
-        // use this url when in the office
-        config.putProperty(LdapConfigSettings.LDAP_HOST_URL, "ldap://spock:389");
-        config.putProperty(LdapConfigSettings.LDAP_SEARCH_BASE, "dc=layer7-tech,dc=com");
-
-        // create the provider
-        LdapIdentityProviderServer provider = new LdapIdentityProviderServer();
-        provider.initialize(config);
-
-
         // get the user manager
         GroupManager me = provider.getGroupManager();
 
@@ -77,24 +68,13 @@ public class LdapIdentityProviderServerTest extends junit.framework.TestCase {
         while (i.hasNext()) {
             Group group = (Group)i.next();
             // uncomment if you want to see the content
-            // System.out.println(group);
+            System.out.println(group);
             if (group != null) grpsReceived = true;
         }
         assertTrue("receiving groups", grpsReceived);
     }
 
     public void testAuthenticate() throws Exception {
-        IdentityProviderConfig config = new IdentityProviderConfig(IdentityProviderType.LDAP);
-        // use this url when ssh forwarding locally
-        // config.putProperty(LdapConfigSettings.LDAP_HOST_URL, "ldap://localhost:3899");
-        // use this url when in the office
-        config.putProperty(LdapConfigSettings.LDAP_HOST_URL, "ldap://spock:389");
-        config.putProperty(LdapConfigSettings.LDAP_SEARCH_BASE, "dc=layer7-tech,dc=com");
-
-        // create the provider
-        LdapIdentityProviderServer provider = new LdapIdentityProviderServer();
-        provider.initialize(config);
-
         // get the user manager
         UserManager me = provider.getUserManager();
         System.out.println("retrieving user");
@@ -109,9 +89,23 @@ public class LdapIdentityProviderServerTest extends junit.framework.TestCase {
         assertTrue("authenticate fails with invalid credentials", !provider.authenticate(invalidcreds));
     }
 
+    public void testSearch() throws Exception {
+        Collection res = provider.search(new EntityType[] {EntityType.GROUP, EntityType.USER}, "j*");
+        // Collection res = provider.search(new EntityType[] {EntityType.GROUP}, "j*");
+        boolean entitiesReceived = false;
+        for (Iterator i = res.iterator(); i.hasNext();) {
+            EntityHeader header = (EntityHeader)i.next();
+            System.out.println(header);
+            entitiesReceived = true;
+        }
+        assertTrue("receiving search results", entitiesReceived);
+    }
+
     public static void main(String[] args) throws Exception {
         junit.textui.TestRunner.run(suite());
     }
+
+    private LdapIdentityProviderServer provider;
 }
 
 
