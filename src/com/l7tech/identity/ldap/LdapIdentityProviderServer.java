2aea7d69c5637fd9e87ac7c1429c96584b474393
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-07-21 17:18:32 +0000

    implemented search
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1575 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/identity/ldap/LdapIdentityProviderServer.java
+++ src/com/l7tech/identity/ldap/LdapIdentityProviderServer.java
@@ -5,8 +5,16 @@ import com.l7tech.credential.PrincipalCredentials;
 import com.l7tech.credential.CredentialFormat;
 import com.l7tech.logging.LogManager;
 import com.l7tech.objectmodel.FindException;
+import com.l7tech.objectmodel.EntityHeader;
+import com.l7tech.objectmodel.EntityType;
 
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.Context;
+import javax.naming.directory.*;
 import java.util.logging.Level;
+import java.util.Collection;
+import java.util.ArrayList;
 
 /**
  * Layer 7 Technologies, inc.
@@ -67,6 +75,76 @@ public class LdapIdentityProviderServer implements com.l7tech.identity.IdentityP
         valid = false;
         groupManager.invalidate();
         userManager.invalidate();
+    }
+
+    /**
+     * searches for users and groups whose name (cn) match the pattern described in searchString
+     * pattern may include wildcard such as * character
+     */
+    public Collection search(EntityType[] types, String searchString) throws FindException {
+        if (!valid) {
+            LogManager.getInstance().getSystemLogger().log(Level.INFO, "invalid id provider asked for search");
+            throw new FindException("provider invalidated");
+        }
+        if (types == null || types.length < 1) throw new IllegalArgumentException("must pass at least one type");
+        boolean wantUsers = false;
+        boolean wantGroups = false;
+        for (int i = 0; i < types.length; i++) {
+            if (types[i] == EntityType.USER) wantUsers = true;
+            else if (types[i] == EntityType.GROUP) wantGroups = true;
+        }
+        if (!wantUsers && !wantGroups) throw new IllegalArgumentException("types must contain users and or groups");
+        Collection output = new ArrayList();
+        try
+        {
+            NamingEnumeration answer = null;
+            // search string for users and or groups based on passed types wanted
+            String filter = null;
+            if (wantUsers && wantGroups) {
+                filter = "(&(|(objectclass=" + LdapGroupManagerServer.GROUP_OBJCLASS + ")(objectclass=" + LdapUserManagerServer.USER_OBJCLASS + "))(" + LdapManager.NAME_ATTR_NAME + "=" + searchString + "))";
+            } else if (wantUsers) {
+                filter = "(&(objectclass=" + LdapUserManagerServer.USER_OBJCLASS + ")(" + LdapManager.NAME_ATTR_NAME + "=" + searchString + "))";
+            } else if (wantGroups) {
+                filter = "(&(objectclass=" + LdapGroupManagerServer.GROUP_OBJCLASS + ")(" + LdapManager.NAME_ATTR_NAME + "=" + searchString + "))";
+            }
+            SearchControls sc = new SearchControls();
+            sc.setSearchScope(SearchControls.SUBTREE_SCOPE);
+            java.util.Hashtable env = new java.util.Hashtable();
+            env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+            env.put(Context.PROVIDER_URL, cfg.getProperty(LdapConfigSettings.LDAP_HOST_URL));
+            DirContext context = new InitialDirContext(env);
+            answer = context.search(cfg.getProperty(LdapConfigSettings.LDAP_SEARCH_BASE), filter, sc);
+            while (answer.hasMore())
+            {
+                // get this item
+                SearchResult sr = (SearchResult)answer.next();
+                // set the dn (unique id)
+                String dn = sr.getName() + "," + cfg.getProperty(LdapConfigSettings.LDAP_SEARCH_BASE);
+                Attributes atts = sr.getAttributes();
+                // is it user or group ?
+                Attribute objectclasses = atts.get("objectclass");
+                EntityHeader header = null;
+                // construct header accordingly
+                if (objectclasses.contains(LdapGroupManagerServer.GROUP_OBJCLASS)) {
+                    header = new EntityHeader(dn, EntityType.GROUP, dn, null);
+                } else if (objectclasses.contains(LdapUserManagerServer.USER_OBJCLASS)) {
+                    Object tmp = LdapManager.extractOneAttributeValue(atts, LdapManager.LOGIN_ATTR_NAME);
+                    if (tmp != null) {
+                        header = new EntityHeader(dn, EntityType.USER, tmp.toString(), null);
+                    }
+                }
+                // if we successfully constructed a header, add it to result list
+                if (header != null) output.add(header);
+            }
+            if (answer != null) answer.close();
+            context.close();
+        }
+        catch (NamingException e)
+        {
+            LogManager.getInstance().getSystemLogger().log(Level.SEVERE, null, e);
+            throw new FindException(e.getMessage(), e);
+        }
+        return output;
     }
 
 
