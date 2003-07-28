27fcfe58e6b1eac84c0c4e13a4a336759bf6870f
emil <emil@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-07-28 14:35:36 +0000

    Initial revision
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1638 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/console/action/IdentityPolicyAction.java
@@ -0,0 +1,64 @@
+package com.l7tech.console.action;
+
+import com.l7tech.console.panels.Utilities;
+import com.l7tech.console.tree.policy.IdentityPolicyView;
+import com.l7tech.console.util.Registry;
+import com.l7tech.policy.assertion.identity.IdentityAssertion;
+
+import java.awt.*;
+import java.util.logging.Logger;
+
+
+/**
+ * The <code>IdentityPolicyAction</code> action views the identoty
+ * policy for user or group.
+ *
+ * @author <a href="mailto:emarceta@layer7-tech.com">Emil Marceta</a>
+ * @version 1.0
+ */
+public class IdentityPolicyAction extends BaseAction {
+    static final Logger log = Logger.getLogger(IdentityPolicyAction.class.getName());
+    IdentityAssertion assertion;
+
+    /**
+     * create the action that shows the identity assertino policy
+     * @param a the identity assertion
+     */
+    public IdentityPolicyAction(IdentityAssertion a) {
+        assertion = a;
+    }
+
+    /**
+     * @return the action name
+     */
+    public String getName() {
+        return "View Identity policy";
+    }
+
+    /**
+     * @return the aciton description
+     */
+    public String getDescription() {
+        return "View Identity policy assertion tree";
+    }
+
+    /**
+     * subclasses override this method specifying the resource name
+     */
+    protected String iconResource() {
+        return "com/l7tech/console/resources/policy16.gif";
+    }
+
+    /** Actually perform the action.
+     * This is the method which should be called programmatically.
+     * note on threading usage: do not access GUI components
+     * without explicitly asking for the AWT event thread!
+     */
+    public void performAction() {
+        Frame f = Registry.getDefault().getWindowManager().getMainWindow();
+        IdentityPolicyView pw = new IdentityPolicyView(f, assertion);
+        pw.pack();
+        Utilities.centerOnScreen(pw);
+        pw.show();
+    }
+}
