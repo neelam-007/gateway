1a578c1b522fa15e8b45fc3f71ba4a1469327baa
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-07-29 23:57:14 +0000

    tester for csr handler
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1678 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ tests/com/l7tech/identity/cert/CSRHandlerTest.java
@@ -0,0 +1,66 @@
+package com.l7tech.identity.cert;
+
+import java.net.*;
+import java.io.*;
+
+/**
+ * User: flascell
+ * Date: Jul 29, 2003
+ * Time: 3:35:27 PM
+ *
+ * test the csr handler servlet
+ */
+public class CSRHandlerTest {
+    public static void main (String[] args) throws Exception {
+
+        System.setProperty("javax.net.ssl.trustStore", System.getProperties().getProperty("user.home") + File.separator + ".l7tech" + File.separator + "trustStore");
+        System.setProperty("javax.net.ssl.trustStorePassword", "password");
+
+
+        URL url = new URL("https://riker:8443/ssg/csr");
+        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setDoOutput(true);
+
+        // todo, authentication
+        // connection.setRequestProperty("Authorization", "Basic 309b9c7ab4c3ee2144fce9b071acd440");
+
+        FileInputStream fis = new FileInputStream("/home/flascell/bogus.pkcs");
+        byte[] buf = new byte[256];
+        int read = fis.read(buf);
+        OutputStream os = connection.getOutputStream();
+        while (read > 0) {
+            os.write(buf, 0, read);
+            read = fis.read(buf);
+        }
+        os.close();
+
+        InputStream response = null;
+
+        try {
+            response = connection.getInputStream();
+            read = response.read(buf);
+            while (read > 0) {
+                System.out.print(new String(buf));
+                read = response.read(buf);
+            }
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+        }
+
+        System.out.println("END OF RESPONSE");
+
+        try {
+            response = connection.getErrorStream();
+            read = response.read(buf);
+            while (read > 0) {
+                System.out.print(new String(buf));
+                read = response.read(buf);
+            }
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+        }
+
+        System.out.println("END OF ERROR STREAM");
+    }
+}
