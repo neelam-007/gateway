96139bf31719e669cd5e421972a6426a9c19118c
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-07-29 17:21:06 +0000

    Added version and loadTime properties
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1651 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/objectmodel/NamedEntity.java
+++ src/com/l7tech/objectmodel/NamedEntity.java
@@ -3,4 +3,8 @@ package com.l7tech.objectmodel;
 public interface NamedEntity extends Entity {
     String getName();
     void setName( String name );
+    int getVersion();
+    void setVersion( int version );
+    long getLoadTime();
+    void setLoadTime( long loadTime );
 }
--- src/com/l7tech/objectmodel/imp/NamedEntityImp.java
+++ src/com/l7tech/objectmodel/imp/NamedEntityImp.java
@@ -7,5 +7,19 @@ public abstract class NamedEntityImp extends EntityImp implements NamedEntity {
     public String getName() { return _name; }
     public void setName( String name ) { _name = name; }
 
+    public int getVersion() { return _version; }
+    public void setVersion(int version) { _version = version; }
+
+    public long getLoadTime() {
+        return _loadTime;
+    }
+
+    public void setLoadTime(long loadTime) {
+        _loadTime = loadTime;
+    }
+
     protected String _name;
+    protected int _version;
+
+    protected transient long _loadTime;
 }
