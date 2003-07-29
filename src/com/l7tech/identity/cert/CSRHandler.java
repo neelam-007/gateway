09900ab0da2fee1675565742155d9a498ab6976b
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-07-29 18:17:22 +0000

    handle CSR (need to plug in RSA Signer)
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1659 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/identity/cert/CSRHandler.java
+++ src/com/l7tech/identity/cert/CSRHandler.java
@@ -1,10 +1,15 @@
 package com.l7tech.identity.cert;
 
+import com.l7tech.common.util.HexUtils;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 import java.io.IOException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateEncodingException;
+
 
 /**
  * Layer 7 Technologies, inc.
@@ -23,13 +28,32 @@ public class CSRHandler extends HttpServlet {
             return;
         }
         // todo, authenticate user
-        // todo, get the CSR from the payload
-        byte[] cert = null;
-        // todo, generate the new cert
-        response.setStatus(HttpServletResponse.SC_OK);
-        response.setContentType("application/x-x509-ca-cert");
-        response.setContentLength(cert.length);
-        response.getOutputStream().write(cert);
-        response.flushBuffer();
+        byte[] csr = readCSRFromRequest(request);
+        Certificate cert = sign(csr);
+        try {
+            byte[] certbytes = cert.getEncoded();
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.setContentType("application/x-x509-ca-cert");
+            response.setContentLength(certbytes.length);
+            response.getOutputStream().write(certbytes);
+            response.flushBuffer();
+        } catch (CertificateEncodingException e) {
+            // todo, send 500
+        }
+    }
+
+    private byte[] readCSRFromRequest(HttpServletRequest request) throws IOException {
+        byte[] b64Encoded = HexUtils.slurpStream(request.getInputStream(), 16384);
+        String tmpStr = new String(b64Encoded);
+        String beginKey = "-----BEGIN CERTIFICATE REQUEST-----";
+        String endKey = "-----END CERTIFICATE REQUEST-----";
+        int beggining = tmpStr.indexOf(beginKey) + beginKey.length();
+        int end = tmpStr.indexOf(endKey);
+        return tmpStr.substring(beggining, end).getBytes();
+    }
+
+    private Certificate sign(byte[] csr) {
+        // todo, user RSA signer
+        return null;
     }
 }
