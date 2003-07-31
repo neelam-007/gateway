ddc3f774534f0084bfafefa631bfe87e3e6efaa8
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-07-31 03:39:53 +0000

    Started work on making the Client Certificate assertion do something.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1701 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/policy/assertion/credential/http/HttpClientCert.java
+++ src/com/l7tech/policy/assertion/credential/http/HttpClientCert.java
@@ -10,17 +10,54 @@ import com.l7tech.credential.CredentialFinderException;
 import com.l7tech.credential.http.HttpDigestCredentialFinder;
 import com.l7tech.message.Request;
 import com.l7tech.message.Response;
+import com.l7tech.policy.assertion.AssertionResult;
 import com.l7tech.policy.assertion.AssertionStatus;
 import com.l7tech.policy.assertion.PolicyAssertionException;
 import com.l7tech.proxy.datamodel.PendingRequest;
+import com.l7tech.proxy.datamodel.ClientKeyManager;
+
+import java.security.cert.CertificateExpiredException;
+import java.security.cert.CertificateNotYetValidException;
+import java.security.cert.X509Certificate;
+
+import org.apache.log4j.Category;
 
 /**
  * @author alex
  * @version $Revision$
  */
 public class HttpClientCert extends HttpCredentialSourceAssertion {
-    public AssertionStatus doCheckRequest(Request request, Response response) throws CredentialFinderException {
-        // FIXME: Implement
+    private static final Category log = Category.getInstance(HttpClientCert.class);
+
+    public AssertionStatus doCheckRequest(Request request, Response response)
+            throws CredentialFinderException
+    {
+        X509Certificate clientCert = (X509Certificate)
+                request.getTransportMetadata().getParameter("javax.servlet.request.X509Certificate");
+
+        if (clientCert == null) {
+            response.setPolicyViolated(true);
+            response.addResult(new AssertionResult(this, request, AssertionStatus.NOT_FOUND,
+                                                   "No Client Certificate was present in the request."));
+            return AssertionStatus.NOT_FOUND;
+        }
+
+        try {
+            clientCert.checkValidity();
+        } catch (CertificateExpiredException e) {
+            response.addResult(new AssertionResult(this, request, AssertionStatus.NOT_FOUND,
+                                                   "Client Certificate has expired", e));
+            return AssertionStatus.NOT_FOUND;
+        } catch (CertificateNotYetValidException e) {
+            response.addResult(new AssertionResult(this, request, AssertionStatus.NOT_FOUND,
+                                                   "Client Certificate is not yet valid", e));
+            return AssertionStatus.NOT_FOUND;
+        }
+
+        // Get DN from cert, ie "CN=testuser, OU=ssg.example.com"
+        String certDn = clientCert.getSubjectDN().getName();
+
+        // TODO: whatever needs to be done to make this work.
         return AssertionStatus.NOT_YET_IMPLEMENTED;
     }
 
@@ -35,6 +72,17 @@ public class HttpClientCert extends HttpCredentialSourceAssertion {
      * @throws PolicyAssertionException if processing should not continue due to a serious error
      */
     public AssertionStatus decorateRequest(PendingRequest request) throws PolicyAssertionException {
-        return AssertionStatus.NOT_YET_IMPLEMENTED;
+        if (ClientKeyManager.isClientCertAvailabile(request.getSsg())) {
+            request.setClientCertRequired(true);
+            log.info("We appear to possess a Client Certificate for this SSG.");
+            return AssertionStatus.NONE;
+        }
+
+        if (request.getSsg().getUsername() == null || request.getSsg().getPassword() == null ||
+                request.getSsg().getUsername().length() < 1)
+            request.setCredentialsWouldHaveHelped(true);
+        request.setClientCertWouldHaveHelped(true);
+        log.info("We do not appear to have a Client Certificate for this SSG.");
+        return AssertionStatus.NOT_FOUND;
     }
 }
