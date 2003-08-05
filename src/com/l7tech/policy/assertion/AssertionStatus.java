12a157a0c4e30fae28d4bc6271ab37d238185aea
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-08-05 18:33:26 +0000

    Whitespace
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1743 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/policy/assertion/AssertionStatus.java
+++ src/com/l7tech/policy/assertion/AssertionStatus.java
@@ -12,14 +12,14 @@ public class AssertionStatus {
     static final int POLICY = 600;
     static final int MISC   = 1000;
 
-    public static final AssertionStatus NONE          = make(           0, "No Error" );
-    public static final AssertionStatus BAD_REQUEST   = make( CLIENT |  0, "Bad Request" );
-    public static final AssertionStatus AUTH_REQUIRED = make( CLIENT |  1, "Authentication Required" );
-    public static final AssertionStatus AUTH_FAILED   = make( CLIENT |  2, "Authentication Failed" );
-    public static final AssertionStatus FORBIDDEN     = make( CLIENT |  3, "Forbidden" );
-    public static final AssertionStatus SERVICE_NOT_FOUND     = make( CLIENT |  4, "Service Not Found" );
-    public static final AssertionStatus UNAUTHORIZED  = make( CLIENT |  2, "Unauthorized" );
-    public static final AssertionStatus BALEETED      = make( CLIENT | 99, "BALEETED!" );
+    public static final AssertionStatus NONE              = make(           0, "No Error" );
+    public static final AssertionStatus BAD_REQUEST       = make( CLIENT |  0, "Bad Request" );
+    public static final AssertionStatus AUTH_REQUIRED     = make( CLIENT |  1, "Authentication Required" );
+    public static final AssertionStatus AUTH_FAILED       = make( CLIENT |  2, "Authentication Failed" );
+    public static final AssertionStatus FORBIDDEN         = make( CLIENT |  3, "Forbidden" );
+    public static final AssertionStatus SERVICE_NOT_FOUND = make( CLIENT |  4, "Service Not Found" );
+    public static final AssertionStatus UNAUTHORIZED      = make( CLIENT |  2, "Unauthorized" );
+    public static final AssertionStatus BALEETED          = make( CLIENT | 99, "BALEETED!" );
 
     public static final AssertionStatus NOT_YET_IMPLEMENTED = make( MISC | 0, "Not yet implemented!" );
     public static final AssertionStatus NOT_APPLICABLE      = make( MISC | 1, "Not applicable in this context" );
