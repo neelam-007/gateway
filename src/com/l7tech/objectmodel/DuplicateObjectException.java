ff9cefaef752888e3e8b90d04d2393fd2fcba12e
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-08-05 21:22:30 +0000

    Initial checkin.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1752 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/objectmodel/DuplicateObjectException.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2003 Layer 7 Technologies Inc.
+ *
+ * $Id$
+ */
+
+package com.l7tech.objectmodel;
+
+/**
+ * @author alex
+ * @version $Revision$
+ */
+public class DuplicateObjectException extends ObjectModelException {
+    public DuplicateObjectException() {
+        super();
+    }
+
+    public DuplicateObjectException( String message ) {
+        super( message );
+    }
+
+    public DuplicateObjectException( String message, Throwable cause ) {
+        super( message, cause );
+    }
+
+}
