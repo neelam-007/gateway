1e75d333a90fb22bf2956e482ce56f52e829deb7
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-08-05 18:31:49 +0000

    Renamed doCheckRequest to checkCredentials, added scheme property, challenge method
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1741 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/policy/assertion/credential/http/HttpCredentialSourceAssertion.java
+++ src/com/l7tech/policy/assertion/credential/http/HttpCredentialSourceAssertion.java
@@ -7,12 +7,37 @@
 package com.l7tech.policy.assertion.credential.http;
 
 import com.l7tech.policy.assertion.credential.CredentialSourceAssertion;
+import com.l7tech.policy.assertion.AssertionStatus;
+import com.l7tech.policy.assertion.AssertionResult;
+import com.l7tech.message.Request;
+import com.l7tech.message.Response;
+import com.l7tech.credential.CredentialFinderException;
+import com.l7tech.credential.PrincipalCredentials;
 
 /**
  * @author alex
  * @version $Revision$
  */
 public abstract class HttpCredentialSourceAssertion extends CredentialSourceAssertion {
+
+    public AssertionStatus checkCredentials( Request request, Response response ) throws CredentialFinderException {
+        PrincipalCredentials pc = request.getPrincipalCredentials();
+        if ( pc == null ) return challenge( request, response );
+        String realm = pc.getRealm();
+        if ( ( realm == null && _realm == null ) || realm != null && realm.equals( _realm ) ) {
+            return AssertionStatus.NONE;
+        }
+        return challenge( request, response );
+    }
+
+    protected abstract String scheme();
+
+    protected AssertionStatus challenge( Request request, Response response ) {
+        AssertionResult result = new AssertionResult( this, request, AssertionStatus.AUTH_REQUIRED, "Authentication Required", new String[] { scheme() } );
+        response.addResult( result );
+        return AssertionStatus.FALSIFIED;
+    }
+
     public String getRealm() {
         return _realm;
     }
