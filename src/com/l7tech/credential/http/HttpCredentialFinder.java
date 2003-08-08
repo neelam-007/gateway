55bb45ae5e9376587d64ea1c515bdb872df4c7b6
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-08-08 21:29:09 +0000

    Moved throwError() here
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1794 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/credential/http/HttpCredentialFinder.java
+++ src/com/l7tech/credential/http/HttpCredentialFinder.java
@@ -7,9 +7,16 @@
 package com.l7tech.credential.http;
 
 import com.l7tech.credential.*;
+import com.l7tech.logging.LogManager;
+
+import java.util.logging.Level;
 
 /**
  * @author alex
  */
 public abstract class HttpCredentialFinder implements CredentialFinder {
+    public void throwError( String err ) throws CredentialFinderException {
+        LogManager.getInstance().getSystemLogger().log(Level.SEVERE, err);
+        throw new CredentialFinderException( err );
+    }
 }
