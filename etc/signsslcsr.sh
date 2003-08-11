2a6ad85ab04d3cab880525007f9504d709379bd1
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-08-11 16:52:47 +0000

    get root kstore password from user if not passed as argument.
    unzip in webapps instead of a tmp dir (only if the
    WAR was not previously exploded).
    
    verify that the root keystore is present and exit
    if it is not
    
    
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1812 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- etc/signsslcsr.sh
+++ etc/signsslcsr.sh
@@ -22,35 +22,46 @@
 if [ ! "$TOMCAT_HOME" ]; then
         echo "ERROR: TOMCAT_HOME not set"
         echo
-        exit -1
-fi
-
-# VERIFY THAT THE ROOT KSTORE PASSWORD IS PASSED
-if [ ! "$1" ]; then
-        echo "ERROR: you must provide the root keystore password as argument"
-        echo
-        exit -1
+        exit 0
 fi
 
 # =================================================================================
 KEYSTORE_DIR="$TOMCAT_HOME/kstores"
 CSR_FILE="$TOMCAT_HOME/kstores/tomcatSsl.csr"
 CERTIFICATE_FILE="$TOMCAT_HOME/kstores/ssl_rootcerted.cer"
-TMP_WAR_CONTENTS="$TOMCAT_HOME/kstores/tmp"
 WAR_FILE="$TOMCAT_HOME/webapps/ROOT.war"
 ROOT_KEY_STORE="$TOMCAT_HOME/kstores/ssgroot"
 ROOT_KEY_ALIAS=ssgroot
+WEBAPPS_ROOT="$TOMCAT_HOME/webapps/ROOT"
 # =================================================================================
 
-# extract our java stuff from war file
-mkdir $TMP_WAR_CONTENTS
-unzip $WAR_FILE -d $TMP_WAR_CONTENTS
+# VERIFY THAT THE ROOT KEYSTORE IS PRESENT
+if [ ! -e "$ROOT_KEY_STORE" ]; then
+        echo "ROOT Key Store not found"
+        exit
+fi
+
+# VERIFY THAT THE ROOT KSTORE PASSWORD IS PASSED. IF NOT, GET IT FROM CONSOLE
+if [ ! "$1" ]; then
+        # ax for the password
+        echo "Please type in the root keystore password"
+        read -s ROOT_KSTORE_PASSWORD
+else
+        ROOT_KSTORE_PASSWORD=$1
+fi
+
+# IF THE WAR FILE IS PRESENT BUT NOT YET EXPANDED, EXPAND IT
+if [ -e "$WEBAPPS_ROOT" ]; then
+        echo
+else
+        echo "expanding the war file..."
+        unzip $WAR_FILE -d $WEBAPPS_ROOT
+fi
+
 
 # set a classpath for the execution of "com.l7tech.identity.cert.RSASigner"
-CP=$TMP_WAR_CONTENTS/WEB-INF/classes:$TMP_WAR_CONTENTS/WEB-INF/lib/bcprov-jdk14-119.jar:$TMP_WAR_CONTENTS/WEB-INF/lib/jaxrpc.jar:$TMP_WAR_CONTENTS/WEB-INF/lib/wsdl4j.jar
+# CP=$TMP_WAR_CONTENTS/WEB-INF/classes:$TMP_WAR_CONTENTS/WEB-INF/lib/bcprov-jdk14-119.jar:$TMP_WAR_CONTENTS/WEB-INF/lib/jaxrpc.jar:$TMP_WAR_CONTENTS/WEB-INF/lib/wsdl4j.jar
+CP=$WEBAPPS_ROOT/WEB-INF/classes:$WEBAPPS_ROOT/WEB-INF/lib/bcprov-jdk14-119.jar:$WEBAPPS_ROOT/WEB-INF/lib/jaxrpc.jar:$WEBAPPS_ROOT/WEB-INF/lib/wsdl4j.jar
 
 # do it
-java -cp $CP com.l7tech.identity.cert.RSASigner $ROOT_KEY_STORE $1 $ROOT_KEY_ALIAS $1 $CSR_FILE $CERTIFICATE_FILE
-
-# clean tmp directory
-rm -R $TMP_WAR_CONTENTS
+java -cp $CP com.l7tech.identity.cert.RSASigner $ROOT_KEY_STORE $ROOT_KSTORE_PASSWORD $ROOT_KEY_ALIAS $ROOT_KSTORE_PASSWORD $CSR_FILE $CERTIFICATE_FILE
