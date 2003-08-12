4a7685f069cc6acb06d3a35345e9a8d8e4770ed4
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-08-12 17:46:24 +0000

    implemented and tested
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1830 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/credential/wss/WssBasicCredentialFinder.java
+++ src/com/l7tech/credential/wss/WssBasicCredentialFinder.java
@@ -8,13 +8,79 @@ package com.l7tech.credential.wss;
 
 import com.l7tech.credential.PrincipalCredentials;
 import com.l7tech.credential.CredentialFinderException;
+import com.l7tech.credential.CredentialFormat;
 import com.l7tech.message.Request;
+import com.l7tech.util.SAXParsingCompleteException;
+import com.l7tech.logging.LogManager;
+import com.l7tech.identity.User;
+import org.xml.sax.XMLReader;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.XMLReaderFactory;
+
+import java.util.logging.Level;
+import java.io.IOException;
+import java.io.StringReader;
 
 /**
  */
 public class WssBasicCredentialFinder extends WssCredentialFinder {
-    public PrincipalCredentials findCredentials( Request request ) throws CredentialFinderException {
-        // todo, use WsseBasicSaxHandler to find credentials
+    public PrincipalCredentials findCredentials(Request request) throws CredentialFinderException {
+        String xmlreq = null;
+        try {
+            xmlreq = request.getRequestXml();
+        } catch (IOException e) {
+            LogManager.getInstance().getSystemLogger().log(Level.SEVERE, "Exception getting a xml request " + e.getMessage(), e);
+            throw new CredentialFinderException("Exception getting the xml request " + e.getMessage(), e);
+        }
+        // in order to get WSS credentials, we need a soap document
+        if (xmlreq == null || xmlreq.length() < 1) {
+            LogManager.getInstance().getSystemLogger().log(Level.WARNING, "No xml request provided to extract wss element from");
+            return null;
+        }
+
+        // get xml parser
+        // todo, should this be set in some sort of startup class somewhere ?
+        System.setProperty("org.xml.sax.driver", "org.apache.xerces.parsers.SAXParser");
+        XMLReader reader = null;
+        try {
+            reader = XMLReaderFactory.createXMLReader();
+        } catch (SAXException e) {
+            LogManager.getInstance().getSystemLogger().log(Level.SEVERE, "Exception getting an XMLReader " + e.getMessage(), e);
+            throw new CredentialFinderException("Exception getting an XMLReader " + e.getMessage(), e);
+        }
+        WsseBasicSaxHandler handler = new WsseBasicSaxHandler();
+        reader.setContentHandler(handler);
+
+        // parse the request, look for SAXParsingCompleteException
+        StringReader stringReader = new StringReader(xmlreq);
+        try {
+            reader.parse(new InputSource(stringReader));
+        } catch (SAXParsingCompleteException e) {
+            String passwd = handler.getParsedPassword();
+            // make sure we got a password (avoid a NPE)
+            if (passwd == null || passwd.length() < 1) {
+                LogManager.getInstance().getSystemLogger().log(Level.WARNING, "parsing completed but no password available.");
+                return null;
+            }
+            // make sure we have a clear text passwd
+            if (!handler.DEFAULT_PASSWORD_TYPE.equals(handler.getPasswdType())) return null;
+
+            // return the whole thing
+            // this is good, we got what we needed
+            User u = new User();
+            u.setLogin(handler.getParsedUsername());
+            return new PrincipalCredentials(u, passwd.getBytes(), CredentialFormat.CLEARTEXT);
+        } catch (IOException e) {
+            LogManager.getInstance().getSystemLogger().log(Level.SEVERE, "Exception parsing xml request " + e.getMessage(), e);
+            throw new CredentialFinderException("Exception parsing xml request " + e.getMessage(), e);
+        } catch (SAXException e) {
+            LogManager.getInstance().getSystemLogger().log(Level.SEVERE, "Exception parsing xml request " + e.getMessage(), e);
+            throw new CredentialFinderException("Exception parsing xml request " + e.getMessage(), e);
+        } finally {
+            stringReader.close();
+        }
+        // note: the actual result is returned in the handling of the SAXParsingCompleteException
         return null;
     }
 }
