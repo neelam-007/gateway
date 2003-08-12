3ca1d349003a81a71b8f13e573d589db3897c458
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-08-12 16:05:38 +0000

    cleaned and documented
    throw a SAXParsingCompleteException to indicate that
    all the information was extracted out of the
    document. This avoids parsing the entire document
    to get username and password only which would occur
    in the header (beginning) anyway.
    
    
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1828 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/credential/wss/WsseBasicSaxHandler.java
+++ src/com/l7tech/credential/wss/WsseBasicSaxHandler.java
@@ -2,32 +2,56 @@ package com.l7tech.credential.wss;
 
 import org.xml.sax.*;
 import org.xml.sax.helpers.XMLReaderFactory;
-
-import java.io.StringReader;
+import com.l7tech.util.SAXParsingCompleteException;
 
 /**
- * Created by IntelliJ IDEA.
  * User: flascell
  * Date: Aug 11, 2003
  * Time: 4:14:08 PM
  * $Id$
  *
  * SAX Handler for xml document which contains a wsse usernametoken element.
+ *
+ * Extracts a username and password from an xml document with the following format:
+ * <?xml version="1.0" encoding="utf-8"?>
+ * <S:Envelope xmlns:S="http://www.w3.org/2001/12/soap-envelope" xmlns:ds="http://www.w3.org/2000/09/xmldsig#">
+ *         <S:Header>
+ *                 <wsse:Security xmlns:wsse="http://schemas.xmlsoap.org/ws/2002/04/secext">
+ *                         <wsse:UsernameToken Id="MyID">
+ *                                 <wsse:Username>Zoe</wsse:Username>
+ *                                 <Password Type="wsse:PasswordText">ILoveLlamas</Password>
+ *                         </wsse:UsernameToken>
+ *                 </wsse:Security>
+ *         </S:Header>
+ *         <S:Body Id="MsgBody">
+ *                 <tru:StockSymbol xmlns:tru="http://fabrikam123.com/payloads">QQQ</tru:StockSymbol>
+ *         </S:Body>
+ * </S:Envelope>
+ *
+ * This handler will throw a SAXParsingCompleteException to indicate that all the information was extracted
+ * out of the document. This stops parsing early and makes this operation faster.
  */
 public class WsseBasicSaxHandler implements ContentHandler {
-    public void setDocumentLocator(Locator locator) {
-    }
 
-    public void startDocument() throws SAXException {
-    }
-
-    public void endDocument() throws SAXException {
-    }
-
-    public void startPrefixMapping(String prefix, String uri) throws SAXException {
-    }
-
-    public void endPrefixMapping(String prefix) throws SAXException {
+    public void endElement(String namespaceURI, String localName, String qName) throws SAXException {
+        if (localName.equals(USERNAME_TAGNAME) && insideUsername) {
+            parsedUsername = databuf.toString();
+            databuf = new StringBuffer();
+            insideUsername = false;
+            // stop parsing if we got all
+            if (parsedPassword != null) throw new SAXParsingCompleteException();
+        }
+        if (localName.equals(PASSWORD_TAGNAME) && insidePassword) {
+            parsedPassword = databuf.toString();
+            databuf = null;
+            insidePassword = false;
+            // stop parsing if we got all
+            if (parsedUsername != null) throw new SAXParsingCompleteException();
+        }
+        else if (localName.equals(USERNAMETOKEN_TAGNAME) && insideUsernameToken) insideUsernameToken = false;
+        else if (localName.equals(SECURITY_TAGNAME) && insideSecurity) insideSecurity = false;
+        else if (localName.equals(HEADER_TAGNAME) && insideHeader) insideHeader = false;
+        else if (localName.equals(ENVELOPE_TAGNAME) && insideEnvelope) insideEnvelope = false;
     }
 
     public void startElement(String namespaceURI, String localName, String qName, Attributes atts) throws SAXException {
@@ -39,37 +63,11 @@ public class WsseBasicSaxHandler implements ContentHandler {
         else if (localName.equals(PASSWORD_TAGNAME) && insideEnvelope && insideHeader && insideSecurity) insidePassword = true;
     }
 
-    public void endElement(String namespaceURI, String localName, String qName) throws SAXException {
-        if (localName.equals(USERNAME_TAGNAME) && insideUsername) {
-            parsedUsername = databuf.toString();
-            databuf = new StringBuffer();
-            insideUsername = false;
-        }
-        if (localName.equals(PASSWORD_TAGNAME) && insidePassword) {
-            parsedPassword = databuf.toString();
-            databuf = null;
-            insidePassword = false;
-        }
-        else if (localName.equals(USERNAMETOKEN_TAGNAME) && insideUsernameToken) insideUsernameToken = false;
-        else if (localName.equals(SECURITY_TAGNAME) && insideSecurity) insideSecurity = false;
-        else if (localName.equals(HEADER_TAGNAME) && insideHeader) insideHeader = false;
-        else if (localName.equals(ENVELOPE_TAGNAME) && insideEnvelope) insideEnvelope = false;
-    }
-
     public void characters(char ch[], int start, int length) throws SAXException {
         if (insideUsername || insidePassword) {
             if (databuf == null) databuf = new StringBuffer();
             databuf.append(ch, start, length);
         }
-    }
-
-    public void ignorableWhitespace(char ch[], int start, int length) throws SAXException {
-    }
-
-    public void processingInstruction(String target, String data) throws SAXException {
-    }
-
-    public void skippedEntity(String name) throws SAXException {
     }
 
     public String getParsedUsername() {
@@ -80,8 +78,6 @@ public class WsseBasicSaxHandler implements ContentHandler {
         this.parsedUsername = parsedUsername;
     }
 
-    private String parsedUsername = null;
-
     public String getParsedPassword() {
         return parsedPassword;
     }
@@ -90,30 +86,45 @@ public class WsseBasicSaxHandler implements ContentHandler {
         this.parsedPassword = parsedPassword;
     }
 
+    public void ignorableWhitespace(char ch[], int start, int length) throws SAXException {}
+    public void processingInstruction(String target, String data) throws SAXException {}
+    public void skippedEntity(String name) throws SAXException {}
+    public void setDocumentLocator(Locator locator) {}
+    public void startDocument() throws SAXException {}
+    public void endDocument() throws SAXException {}
+    public void startPrefixMapping(String prefix, String uri) throws SAXException {}
+    public void endPrefixMapping(String prefix) throws SAXException {}
+
     // todo, move this to a test class
     public static void main(String[] args) throws Exception {
         System.setProperty("org.xml.sax.driver", "org.apache.xerces.parsers.SAXParser");
         XMLReader reader = XMLReaderFactory.createXMLReader();
         WsseBasicSaxHandler handler = new WsseBasicSaxHandler();
         reader.setContentHandler(handler);
-        reader.parse(new InputSource("/home/flascell/dev/wssebasic.xml"));
+        try {
+            reader.parse(new InputSource("/home/flascell/dev/wssebasic.xml"));
+        } catch (SAXParsingCompleteException e) {
+            System.out.println("parsing complete");
+        }
         System.out.println("Parsed username: " + handler.getParsedUsername());
         System.out.println("Parsed password: " + handler.getParsedPassword());
     }
 
     private String parsedPassword = null;
-    private StringBuffer databuf = null;
-    private boolean insideEnvelope = false;
-    private boolean insideHeader = false;
-    private boolean insideSecurity = false;
-    private boolean insideUsernameToken = false;
-    private boolean insideUsername = false;
-    private boolean insidePassword = false;
-    private static final String ENVELOPE_TAGNAME = "Envelope";
-    private static final String HEADER_TAGNAME = "Header";
-    private static final String SECURITY_TAGNAME = "Security";
-    private static final String USERNAMETOKEN_TAGNAME = "UsernameToken";
-    private static final String USERNAME_TAGNAME = "Username";
-    private static final String PASSWORD_TAGNAME = "Password";
+    private String parsedUsername = null;
+
+    protected StringBuffer databuf = null;
+    protected boolean insideEnvelope = false;
+    protected boolean insideHeader = false;
+    protected boolean insideSecurity = false;
+    protected boolean insideUsernameToken = false;
+    protected boolean insideUsername = false;
+    protected boolean insidePassword = false;
+    protected static final String ENVELOPE_TAGNAME = "Envelope";
+    protected static final String HEADER_TAGNAME = "Header";
+    protected static final String SECURITY_TAGNAME = "Security";
+    protected static final String USERNAMETOKEN_TAGNAME = "UsernameToken";
+    protected static final String USERNAME_TAGNAME = "Username";
+    protected static final String PASSWORD_TAGNAME = "Password";
 }
 
