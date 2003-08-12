5922eecdf204db8c11b7fb7bdb6a0488ca5619c7
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-08-12 16:06:25 +0000

    *** empty log message ***
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1829 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/util/SAXParsingCompleteException.java
@@ -0,0 +1,19 @@
+package com.l7tech.util;
+
+import org.xml.sax.SAXException;
+
+/**
+ * User: flascell
+ * Date: Aug 12, 2003
+ * Time: 8:52:25 AM
+ *
+ * This exception is thrown by a SAX content handler to express the fact that all information was extracted
+ * out of an xml document and that it is not necessary to continue parsing until the end of the document. See
+ * XMLReader.parse documentation for more info.
+ * 
+ */
+public class SAXParsingCompleteException extends SAXException {
+    public SAXParsingCompleteException() {
+        super("Parsing complete");
+    }
+}
