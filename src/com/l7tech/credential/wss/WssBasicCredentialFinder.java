6e6ef9e6f1b81d20b536a13b3d4951e86738f06f
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-08-12 01:12:12 +0000

    nothing (WsseBasicSaxHandler)
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1827 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/credential/wss/WssBasicCredentialFinder.java
+++ src/com/l7tech/credential/wss/WssBasicCredentialFinder.java
@@ -8,14 +8,13 @@ package com.l7tech.credential.wss;
 
 import com.l7tech.credential.PrincipalCredentials;
 import com.l7tech.credential.CredentialFinderException;
-import com.l7tech.credential.CredentialFormat;
 import com.l7tech.message.Request;
 
 /**
  */
 public class WssBasicCredentialFinder extends WssCredentialFinder {
     public PrincipalCredentials findCredentials( Request request ) throws CredentialFinderException {
-        // FIXME: Implement!
+        // todo, use WsseBasicSaxHandler to find credentials
         return null;
     }
 }
