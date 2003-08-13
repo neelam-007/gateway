4f32c0b453d503005b654088ecdaf1653d58256a
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-08-13 00:10:39 +0000

    implemented decorateRequest
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1836 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/policy/assertion/credential/wss/WssBasic.java
+++ src/com/l7tech/policy/assertion/credential/wss/WssBasic.java
@@ -14,6 +14,10 @@ import com.l7tech.message.Response;
 import com.l7tech.policy.assertion.AssertionStatus;
 import com.l7tech.policy.assertion.PolicyAssertionException;
 import com.l7tech.proxy.datamodel.PendingRequest;
+import org.apache.axis.message.MessageElement;
+import javax.xml.soap.SOAPException;
+import org.apache.axis.message.SOAPHeaderElement;
+import java.util.Iterator;
 
 /**
  * @author alex
@@ -37,23 +41,60 @@ public class WssBasic extends WssCredentialSourceAssertion {
     }
 
     /**
-     * decorate the xml soap message with a WSS header containing the username and password
+     * Decorate the xml soap message with a WSS header containing the username and password.
+     *
      * @param request
      * @return
      * @throws PolicyAssertionException
      */
     public AssertionStatus decorateRequest(PendingRequest request) throws PolicyAssertionException {
+
+        SOAPHeaderElement secHeader = null;
+        try {
+            secHeader = getSecurityElement(request);
+        } catch (SOAPException e) {
+            throw new PolicyAssertionException(e.getMessage(), e);
+        }
+        // check whether the UsernameToken is not already there
+        MessageElement usernametokenelement = null;
+        for (Iterator i = secHeader.getChildElements();i.hasNext();) {
+            MessageElement el = (MessageElement)i.next();
+            if (el.getName().equals(USERNAME_TOKEN_ELEMENT_NAME)) {
+                usernametokenelement = el;
+            }
+        }
+        // if not present, add it to security header
+        if (usernametokenelement == null) {
+            usernametokenelement = new MessageElement(SECURITY_NAMESPACE, USERNAME_TOKEN_ELEMENT_NAME);
+            try {
+                secHeader.addChild(usernametokenelement);
+            } catch (SOAPException e) {
+                throw new PolicyAssertionException(e.getMessage(), e);
+            }
+        }
+        else {
+            // the usernametoken element is already there. what should i do?
+            // todo, what should the correct course of action be here?
+            return AssertionStatus.NOT_APPLICABLE;
+        }
+
+        // get the username and passwords
         String username = request.getSsg().getUsername();
         char[] password = request.getSsg().getPassword();
         if (username == null || password == null || username.length() < 1) {
-            //log.info("HttpBasic: no credentials configured for the SSG " + request.getSsg());
             request.setCredentialsWouldHaveHelped(true);
             return AssertionStatus.AUTH_REQUIRED;
         }
-        // todo, what if there is already such a header element? let's make sure we dont override it
-        // add the sec element to the request
-        // org.apache.axis.message.SOAPHeaderElement soapheader = new org.apache.axis.message.SOAPHeaderElement(element);
-        // todo, change this return value
-        return AssertionStatus.NOT_YET_IMPLEMENTED;
+
+        // add username and password to the usernametoken element
+        try {
+            usernametokenelement.addChild(new MessageElement(SECURITY_NAMESPACE, "Username", username));
+            usernametokenelement.addChild(new MessageElement(SECURITY_NAMESPACE, "Password", new String(password)));
+        } catch (SOAPException e) {
+            throw new PolicyAssertionException(e.getMessage(), e);
+        }
+        return AssertionStatus.NONE;
     }
+
+    protected static final String USERNAME_TOKEN_ELEMENT_NAME = "UsernameToken";
 }
