1be88a74072530503dbb8c42ee447aa7285f1914
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-08-16 00:38:36 +0000

    Some constants moved up here from Request because they pertain to Responses too
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1911 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/message/Message.java
+++ src/com/l7tech/message/Message.java
@@ -12,6 +12,13 @@ import java.util.Iterator;
  * @author alex
  */
 public interface Message {
+    static final String PREFIX             = "com.l7tech.message.request";
+    static final String PREFIX_HTTP        = PREFIX + ".http";
+    static final String PREFIX_HTTP_HEADER = PREFIX_HTTP + ".header";
+    public static final String PARAM_HTTP_CONTENT_TYPE      = PREFIX_HTTP_HEADER + ".content-type";
+    public static final String PARAM_HTTP_CONTENT_LENGTH    = PREFIX_HTTP_HEADER + ".content-length";
+    public static final String PARAM_HTTP_DATE              = PREFIX_HTTP_HEADER + ".date";
+
     TransportMetadata getTransportMetadata();
     Iterator getParameterNames();
     void setParameter( String name, Object value );
