e7727b9fde4131b552abd4bca949372b96d01686
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-08-19 00:57:22 +0000

    Imports
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1937 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/policy/assertion/credential/wss/WssCredentialSourceAssertion.java
+++ src/com/l7tech/policy/assertion/credential/wss/WssCredentialSourceAssertion.java
@@ -7,15 +7,10 @@
 package com.l7tech.policy.assertion.credential.wss;
 
 import com.l7tech.policy.assertion.credential.CredentialSourceAssertion;
-import com.l7tech.proxy.datamodel.PendingRequest;
-import javax.xml.soap.SOAPException;
-import org.apache.axis.AxisFault;
-import org.apache.axis.message.SOAPHeaderElement;
 
 /**
  * @author alex
  * @version $Revision$
  */
 public abstract class WssCredentialSourceAssertion extends CredentialSourceAssertion {
-
 }
