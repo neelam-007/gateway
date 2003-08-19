f5ca8c836127631b8d966491253fb3665fb28e95
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-08-19 00:56:25 +0000

    Imports, scheme=ClientCert
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1933 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/policy/assertion/credential/http/HttpClientCert.java
+++ src/com/l7tech/policy/assertion/credential/http/HttpClientCert.java
@@ -6,17 +6,14 @@
 
 package com.l7tech.policy.assertion.credential.http;
 
-import com.l7tech.policy.assertion.AssertionStatus;
-import com.l7tech.policy.assertion.PolicyAssertionException;
-import com.l7tech.proxy.datamodel.SsgKeyStoreManager;
-import com.l7tech.proxy.datamodel.PendingRequest;
-import com.l7tech.credential.http.HttpClientCertCredentialFinder;
-import com.l7tech.server.policy.assertion.credential.http.ServerHttpCredentialSource;
-import org.apache.log4j.Category;
-
 /**
  * @author alex
  * @version $Revision$
  */
 public class HttpClientCert extends HttpCredentialSourceAssertion {
+    public String scheme() {
+        return SCHEME;
+    }
+
+    public static final String SCHEME = "ClientCert";
 }
