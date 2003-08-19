eff3c7e74e5a1461ffb93c6107be1b1aa29423e1
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-08-19 01:02:58 +0000

    Refactored
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1941 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/server/policy/assertion/credential/http/ServerHttpCredentialSource.java
+++ src/com/l7tech/server/policy/assertion/credential/http/ServerHttpCredentialSource.java
@@ -6,15 +6,17 @@
 
 package com.l7tech.server.policy.assertion.credential.http;
 
-import com.l7tech.credential.PrincipalCredentials;
-import com.l7tech.credential.CredentialFinderException;
+import com.l7tech.logging.LogManager;
 import com.l7tech.message.Request;
 import com.l7tech.message.Response;
-import com.l7tech.policy.assertion.AssertionResult;
 import com.l7tech.policy.assertion.AssertionStatus;
+import com.l7tech.policy.assertion.credential.CredentialFinderException;
+import com.l7tech.policy.assertion.credential.PrincipalCredentials;
 import com.l7tech.policy.assertion.credential.http.HttpCredentialSourceAssertion;
 import com.l7tech.server.policy.assertion.ServerAssertion;
 import com.l7tech.server.policy.assertion.credential.ServerCredentialSourceAssertion;
+
+import java.util.logging.Level;
 
 /**
  * @author alex
@@ -28,22 +30,25 @@ public abstract class ServerHttpCredentialSource extends ServerCredentialSourceA
 
     public AssertionStatus checkCredentials( Request request, Response response ) throws CredentialFinderException {
         PrincipalCredentials pc = request.getPrincipalCredentials();
-        if ( pc == null ) return challenge( request, response );
+        if ( pc == null ) return AssertionStatus.AUTH_REQUIRED;
         String realm = pc.getRealm();
         if ( ( realm == null && _data.getRealm() == null ) || realm != null && realm.equals( _data.getRealm() ) ) {
-            return AssertionStatus.NONE;
+            return doCheckCredentials( request, response );
+        } else {
+            throw new CredentialFinderException( "Realm mismatch", AssertionStatus.AUTH_FAILED );
         }
-        return challenge( request, response );
     }
 
-    protected abstract String scheme();
+    protected void throwError( String err ) throws CredentialFinderException {
+        throwError( Level.SEVERE, err );
+    }
+
+    protected void throwError( Level level, String err ) throws CredentialFinderException {
+        LogManager.getInstance().getSystemLogger().log( level, err );
+        throw new CredentialFinderException( err );
+    }
+
     protected abstract AssertionStatus doCheckCredentials( Request request, Response response );
-
-    protected AssertionStatus challenge( Request request, Response response ) {
-        AssertionResult result = new AssertionResult( _data, request, AssertionStatus.AUTH_REQUIRED, "Authentication Required", new String[] { scheme() } );
-        response.addResult( result );
-        return AssertionStatus.FALSIFIED;
-    }
 
     protected HttpCredentialSourceAssertion _data;
 }
