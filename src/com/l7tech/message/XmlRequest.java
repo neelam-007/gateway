272fdc9334caa854b2d077f912c015197ce53a0a
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-08-20 22:37:55 +0000

    XML stuff moved here from Request
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1995 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/message/XmlRequest.java
+++ src/com/l7tech/message/XmlRequest.java
@@ -6,9 +6,34 @@
 
 package com.l7tech.message;
 
+import java.io.IOException;
+
 /**
  * @author alex
  * @version $Revision$
  */
 public interface XmlRequest extends Request, XmlMessage {
+    /**
+     * Returns a String containing the XML document of the <code>Request</code>.
+     *
+     * This method consumes any underlying <code>InputStream</code> if necessary
+     * the first time it's called.  Subsequent calls will return the cached XML
+     * document, leaving the InputStream unchanged.
+     *
+     * Note that SocketInputStreams are not resettable!
+     *
+     * @return a String containing the XML document of the Request.
+     * @throws java.io.IOException
+     */
+    String getRequestXml() throws IOException;
+
+    /**
+     * Sets the String containing the XML document for this <code>Request</code>.
+     *
+     * Implementors <b>MUST</b> clear any cached data structures that are dependent on the
+     * XML content of the request (e.g. DOM Documents) when this method is called.
+     *
+     * @param xml
+     */
+    void setRequestXml( String xml );
 }
