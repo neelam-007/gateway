0d2d08995ee5ef604108a9354d3954854b26d809
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-08-20 20:54:32 +0000

    Initial commit
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1986 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/message/HttpSoapRequest.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2003 Layer 7 Technologies Inc.
+ *
+ * $Id$
+ */
+
+package com.l7tech.message;
+
+import java.io.IOException;
+import java.io.Reader;
+
+/**
+ * @author alex
+ * @version $Revision$
+ */
+public class HttpSoapRequest extends SoapRequest {
+    public HttpSoapRequest( HttpTransportMetadata htm ) throws IOException {
+        super( htm );
+    }
+
+    protected Reader getRequestReader() throws IOException {
+         HttpTransportMetadata htm = (HttpTransportMetadata)_transportMetadata;
+         return htm.getRequest().getReader();
+    }
+}
---
+++ src/com/l7tech/message/HttpSoapResponse.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2003 Layer 7 Technologies Inc.
+ *
+ * $Id$
+ */
+
+package com.l7tech.message;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Iterator;
+
+/**
+ * Encapsulates a SOAP response using HTTP transport.
+ *
+ * @author alex
+ * @version $Revision$
+ */
+public class HttpSoapResponse extends SoapResponse {
+    public HttpSoapResponse( HttpTransportMetadata htm ) {
+        super( htm );
+    }
+
+    public void setHeadersIn( HttpServletResponse hresponse ) {
+        String name, value;
+        for (Iterator i = _params.keySet().iterator(); i.hasNext();) {
+            name = (String)i.next();
+            value = (String)_params.get(name);
+
+            if ( name == null || value == null ) continue;
+
+            if ( name.startsWith( PREFIX_HTTP_HEADER ) ) {
+                String hname = name.substring( PREFIX_HTTP_HEADER.length() + 1 );
+                hresponse.setHeader( hname, value );
+            }
+        }
+    }
+}
