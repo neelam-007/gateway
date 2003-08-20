081c05df5049a9cb39fd27293e5f0ed600dc9f8c
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-08-20 17:45:46 +0000

    Javadoc
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1972 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/message/XmlMessage.java
+++ src/com/l7tech/message/XmlMessage.java
@@ -6,9 +6,24 @@ import org.xml.sax.SAXException;
 import java.io.IOException;
 
 /**
+ * Encapsulates a Message (i.e. a Request or Response) that contains XML.
+ *
  * @author alex
  * @version $Revision$
  */
 public interface XmlMessage extends Message {
+
+    /**
+     * Returns the DOM <code>Document</code> attached to this message.
+     *
+     * <b>Note</b> that this calling method may result in any underlying InputStream
+     * being consumed.
+     *
+     * @return
+     * @throws SAXException
+     * @throws IOException
+     */
     Document getDocument() throws SAXException, IOException;
+
+    // TODO: setDocument()?  Include DOM serialization functionality in XmlMessageAdapter?
 }
