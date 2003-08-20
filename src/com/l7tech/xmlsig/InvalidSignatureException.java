e8c59f5e23b11b16f369bcb41b191006f68ed272
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-08-20 20:10:07 +0000

    exceptions thrown by SoapMsgSigner class
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1979 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/xmlsig/InvalidSignatureException.java
@@ -0,0 +1,23 @@
+package com.l7tech.xmlsig;
+
+/**
+ * User: flascell
+ * Date: Aug 20, 2003
+ * Time: 10:52:49 AM
+ * $Id$
+ *
+ * A signature is invalid or of type unsupported
+ */
+public class InvalidSignatureException extends Exception {
+    public InvalidSignatureException(String msg) {
+        super(msg);
+    }
+
+    public InvalidSignatureException(Throwable e) {
+        super(e.getMessage(), e);
+    }
+
+    public InvalidSignatureException(String msg, Throwable e) {
+        super(msg, e);
+    }
+}
---
+++ src/com/l7tech/xmlsig/SignatureNotFoundException.java
@@ -0,0 +1,23 @@
+package com.l7tech.xmlsig;
+
+/**
+ * User: flascell
+ * Date: Aug 20, 2003
+ * Time: 10:33:00 AM
+ * $Id$
+ *
+ * Express the fact that no digital signature were found.
+ */
+public class SignatureNotFoundException extends Exception {
+    public SignatureNotFoundException(String msg) {
+        super(msg);
+    }
+
+    public SignatureNotFoundException(Throwable e) {
+        super(e.getMessage(), e);
+    }
+
+    public SignatureNotFoundException(String msg, Throwable e) {
+        super(msg, e);
+    }
+}
