de53da844d1ce4d6eaa44f37acdd003e244a4a16
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-08-20 17:12:53 +0000

    Javadoc
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1969 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/message/Response.java
+++ src/com/l7tech/message/Response.java
@@ -17,25 +17,79 @@ import java.util.Iterator;
  * @author alex
  */
 public interface Response extends Message {
-    public static final String PARAM_HTTP_WWWAUTHENTICATE = PREFIX_HTTP_HEADER + ".www-authenticate";
+    /** WWW-Authenticate header */
+    public static final String PARAM_HTTP_WWWAUTHENTICATE = PREFIX_HTTP_HEADER + ".WWW-Authenticate";
 
+    /**
+     * Sets the InputStream pointing to the response from the protected service.
+     * <p>
+     * Note that this stream will not be consumed if the responseXml property is also set.
+     *
+     * @param stream An <code>InputStream</code> pointing to the response from the protected service.
+     */
     void setProtectedResponseStream( InputStream stream );
+
+    /**
+     * Gets the InputStream pointing to the response from the protected service
+     * @return An <code>InputStream</code> pointing to the response from the protected service.
+     */
     InputStream getProtectedResponseStream() throws IOException;
 
+    /**
+     * Sets the XML string containing the response from the protected service.
+     * @param xml A String containing an XML document.  Will not be validated.
+     */
     void setResponseXml( String xml );
+
+    /**
+     * Gets the XML string containing the response from the protected service, consuming the <code>ProtectedResponseStream</code> if necessary in the process.
+     * @return a String containing an XML document returned from the protected service.
+     * @throws IOException
+     */
     String getResponseXml() throws IOException;
 
+    /**
+     * Adds an <code>AssertionResult</code> to this <code>Response</code>'s list of results.
+     *
+     * These are used to add additional detail beyond the AssertionStatus if necessary.
+     *
+     * @param result an <code>AssertionResult</code> object.
+     */
     void addResult( AssertionResult result );
+
+    /**
+     * Returns an Iterator containing all the <code>AssertionResult</code>s that have been attached to this <code>Response</code>.
+     * @return
+     */
     Iterator results();
+
+    /**
+     * Returns an Iterator containing any <code>AssertionResult</code> objects that have been attached to this <code>Response</code> that contain a given <code>AssertionStatus</code>.
+     * @param status The <code>AssertionStatus</code> to search for
+     * @return
+     */
     Iterator resultsWithStatus( AssertionStatus status );
+
+    /**
+     * Returns an Iterator containing any <code>AssertionResult</code> objects that have been attached to this <code>Response</code> that contain one of the given <code>AssertionStatus</code>es.
+     * @param statuses The <code>AssertionStatus</code>es to search for
+     * @return
+     */
     Iterator resultsWithStatus( AssertionStatus[] statuses );
 
+    /** A flag indicating whether the response constitutes a failure due to authentication. */
     boolean isAuthenticationMissing();
+    /** A flag indicating whether the response constitutes a failure due to authentication. */
     void setAuthenticationMissing( boolean authMissing );
 
+    /** A flag indicating whether the response constitutes a failure due to a failure to follow a policy. */
     boolean isPolicyViolated();
+    /** A flag indicating whether the response constitutes a failure due to a failure to follow a policy. */
     void setPolicyViolated( boolean policyViolated );
 
+    /** Adds a Runnable to a list of operations to be run when the response is closed (i.e. closing sockets or database connections) */
     void runOnClose( Runnable runMe );
+
+    /** Indicates that the response is done and any resources that were opened during the course of the request can now be closed. */
     void close();
 }
