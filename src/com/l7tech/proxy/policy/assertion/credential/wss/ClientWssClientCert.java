2f0079a0cfbd57026ab653f4f53960fbe627e9de
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-08-20 22:06:51 +0000

    implemented decorateRequest
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@1991 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/proxy/policy/assertion/credential/wss/ClientWssClientCert.java
+++ src/com/l7tech/proxy/policy/assertion/credential/wss/ClientWssClientCert.java
@@ -11,6 +11,13 @@ import com.l7tech.proxy.datamodel.PendingRequest;
 import com.l7tech.policy.assertion.AssertionStatus;
 import com.l7tech.policy.assertion.PolicyAssertionException;
 import com.l7tech.policy.assertion.credential.wss.WssClientCert;
+import com.l7tech.xmlsig.SoapMsgSigner;
+import com.ibm.xml.dsig.SignatureStructureException;
+import com.ibm.xml.dsig.XSignatureException;
+import org.w3c.dom.Document;
+
+import java.security.PrivateKey;
+import java.security.cert.X509Certificate;
 
 /**
  * @author alex
@@ -23,11 +30,34 @@ public class ClientWssClientCert implements ClientAssertion {
 
     /**
      * ClientProxy client-side processing of the given request.
-     * @param requst    The request to decorate.
+     * @param request    The request to decorate.
      * @return AssertionStatus.NONE if this Assertion was applied to the request successfully; otherwise, some error code
      * @throws PolicyAssertionException if processing should not continue due to a serious error
      */
-    public AssertionStatus decorateRequest(PendingRequest requst) throws PolicyAssertionException {
+    public AssertionStatus decorateRequest(PendingRequest request) throws PolicyAssertionException {
+        Document soapmsg = null;
+        try {
+            soapmsg = request.getSoapEnvelope().getAsDocument();
+        } catch (Exception e) {
+            throw new PolicyAssertionException("cannot get request document", e);
+        }
+
+        PrivateKey userPrivateKey = null;
+        X509Certificate userCert = null;
+        // todo, where do i get those two things ?
+
+        SoapMsgSigner dsigHelper = new SoapMsgSigner();
+        try {
+            dsigHelper.signEnvelope(soapmsg, userPrivateKey, userCert);
+        } catch (SignatureStructureException e) {
+            throw new PolicyAssertionException("error signing document", e);
+        } catch (XSignatureException e) {
+            throw new PolicyAssertionException("error signing document", e);
+        }
+
+        // todo, feed back soapmsg into the PendingRequest
+
+        // return AssertionStatus.NONE;
         return AssertionStatus.NOT_YET_IMPLEMENTED;
     }
 
