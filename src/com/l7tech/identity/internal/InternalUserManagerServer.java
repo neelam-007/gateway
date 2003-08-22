a721d277d98f366265baccf9490ce85286fa44d4
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-08-22 00:15:30 +0000

    Hibernatified (removed hardcoded JDBC stuff)
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@2020 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/identity/internal/InternalUserManagerServer.java
+++ src/com/l7tech/identity/internal/InternalUserManagerServer.java
@@ -1,27 +1,23 @@
 package com.l7tech.identity.internal;
 
-import com.l7tech.objectmodel.*;
-import com.l7tech.identity.UserManager;
-import com.l7tech.identity.User;
+import cirrus.hibernate.HibernateException;
+import cirrus.hibernate.Session;
 import com.l7tech.identity.IdProvConfManagerServer;
+import com.l7tech.identity.User;
+import com.l7tech.identity.UserManager;
 import com.l7tech.logging.LogManager;
-import com.l7tech.common.util.HexUtils;
+import com.l7tech.objectmodel.*;
 
-import java.sql.*;
-import java.util.List;
-import java.util.Collection;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.logging.Level;
+import java.io.ByteArrayInputStream;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateEncodingException;
 import java.security.cert.CertificateFactory;
-import java.security.SignatureException;
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.io.FileInputStream;
-
-import cirrus.hibernate.HibernateException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.logging.Level;
 
 /**
  * Layer 7 Technologies, inc.
@@ -95,18 +91,14 @@ public class InternalUserManagerServer extends HibernateEntityManager implements
      * if a cert is present and has never been user -> user can reset maximum of ten times
      */
     public boolean userCanResetCert(String oid) throws FindException{
+        HibernatePersistenceContext hpc = null;
         try {
-            // fla note, this will move to a seperate manager once we redesign persistence layer
-            String colname = "cert_reset_counter";
-            Connection connection = ((HibernatePersistenceContext)PersistenceContext.getCurrent()).getSession().connection();
-            Statement statement = connection.createStatement();
-            String sqlStr = "SELECT " + colname + " FROM " + getTableName() + " WHERE oid=" + oid;
-            ResultSet rs = statement.executeQuery(sqlStr);
-            if (!rs.next()) {
-                LogManager.getInstance().getSystemLogger().log(Level.WARNING, "cannot retrieve " + colname + " value for user " + oid + ". user deleted?");
-                return false; // this should not happen unless user no longer exist
-            }
-            int res = rs.getInt(colname);
+            hpc = hibernateContext();
+            Session s = hpc.getSession();
+            List results = s.find( getFieldQuery( oid, F_CERTRESETCOUNTER ) );
+            Integer i = (Integer)results.get(0);
+            int res = i.intValue();
+
             LogManager.getInstance().getSystemLogger().log(Level.INFO, "cert_reset_counter value for user " + oid + " = " + res);
             if (res < 10) return true;
             else {
@@ -120,12 +112,7 @@ public class InternalUserManagerServer extends HibernateEntityManager implements
             LogManager.getInstance().getSystemLogger().log(Level.SEVERE, "error getting cert_reset_counter value for " + oid, e);
             throw new FindException(e.toString(), e);
         } finally {
-            try {
-                PersistenceContext.getCurrent().close();
-            } catch (SQLException e) {
-                LogManager.getInstance().getSystemLogger().log(Level.SEVERE, e.getMessage(), e);
-                throw new FindException(e.toString(), e);
-            }
+            if ( hpc != null ) hpc.close();
         }
     }
 
@@ -133,13 +120,23 @@ public class InternalUserManagerServer extends HibernateEntityManager implements
      * records new cert and updates the reset counter accordingly
      */
     public void recordNewCert(String oid, Certificate cert) throws UpdateException {
+        HibernatePersistenceContext hpc = null;
         try {
+            sun.misc.BASE64Encoder encoder = new sun.misc.BASE64Encoder();
+            String encodedcert = encoder.encode(cert.getEncoded());
+
+            hpc = hibernateContext();
+            hpc.beginTransaction();
+            User u = findByPrimaryKey( oid );
+            u.setCert( encodedcert );
+            u.setCertResetCounter( u.getCertResetCounter() + 1 );
+            hpc.commitTransaction();
+
+            /*
             // fla note, this will move to a seperate manager once we redesign persistence layer
             String colname = "cert";
             Connection connection = ((HibernatePersistenceContext)PersistenceContext.getCurrent()).getSession().connection();
             PreparedStatement statement = connection.prepareStatement("UPDATE " + getTableName() + " SET " + colname + "=? WHERE oid=?");
-            sun.misc.BASE64Encoder encoder = new sun.misc.BASE64Encoder();
-            String encodedcert = encoder.encode(cert.getEncoded());
             //statement.setBinaryStream(1, new ByteArrayInputStream(certbytes), certbytes.length);
             statement.setString(1, encodedcert);
             statement.setString(2, oid);
@@ -158,22 +155,21 @@ public class InternalUserManagerServer extends HibernateEntityManager implements
             statement.setInt(1, currentValue+1);
             statement.setString(2, oid);
             statement.executeUpdate();
+            */
         } catch (SQLException e) {
-            LogManager.getInstance().getSystemLogger().log(Level.SEVERE, e.getMessage(), e);
-            throw new UpdateException(e.toString(), e);
-        } catch (HibernateException e) {
             LogManager.getInstance().getSystemLogger().log(Level.SEVERE, e.getMessage(), e);
             throw new UpdateException(e.toString(), e);
         } catch (CertificateEncodingException e) {
             LogManager.getInstance().getSystemLogger().log(Level.SEVERE, e.getMessage(), e);
             throw new UpdateException(e.toString(), e);
+        } catch ( TransactionException e ) {
+            LogManager.getInstance().getSystemLogger().log(Level.WARNING, e.getMessage(), e);
+            throw new UpdateException( e.toString(), e );
+        } catch ( FindException e ) {
+            LogManager.getInstance().getSystemLogger().log(Level.WARNING, e.getMessage(), e);
+            throw new UpdateException( e.toString(), e );
         } finally {
-            try {
-                PersistenceContext.getCurrent().close();
-            } catch (SQLException e) {
-                LogManager.getInstance().getSystemLogger().log(Level.SEVERE, e.getMessage(), e);
-                throw new UpdateException(e.toString(), e);
-            }
+            if ( hpc != null ) hpc.close();
         }
     }
 
@@ -181,18 +177,13 @@ public class InternalUserManagerServer extends HibernateEntityManager implements
      * returns null if no certificate is registered for this user
      */
     public Certificate retrieveUserCert(String oid) throws FindException {
+        HibernatePersistenceContext hpc = null;
         try {
-            // fla note, this will move to a seperate manager once we redesign persistence layer
-            String colname = "cert";
-            Connection connection = ((HibernatePersistenceContext)PersistenceContext.getCurrent()).getSession().connection();
-
-            Statement statement = connection.createStatement();
-            String sqlStr = "SELECT " + colname + " FROM " + getTableName() + " WHERE oid=" + oid;
-            ResultSet rs = statement.executeQuery(sqlStr);
-            if (!rs.next()) {
-                return null;
-            }
-            String dbcert = rs.getString(colname);
+            hpc = hibernateContext();
+            String hql = getFieldQuery( oid, F_CERT );
+            Session s = hpc.getSession();
+            List results = s.find( hql );
+            String dbcert = (String)results.get(0);
             if (dbcert == null) return null;
             sun.misc.BASE64Decoder base64decoder = new sun.misc.BASE64Decoder();
             byte[] certbytes = base64decoder.decodeBuffer(dbcert);
@@ -221,12 +212,7 @@ public class InternalUserManagerServer extends HibernateEntityManager implements
             LogManager.getInstance().getSystemLogger().log(Level.SEVERE, e.getMessage(), e);
             throw new FindException(e.toString(), e);
         } finally {
-            try {
-                PersistenceContext.getCurrent().close();
-            } catch (SQLException e) {
-                LogManager.getInstance().getSystemLogger().log(Level.SEVERE, e.getMessage(), e);
-                throw new FindException(e.toString(), e);
-            }
+            if ( hpc != null ) hpc.close();
         }
     }
 
@@ -234,24 +220,33 @@ public class InternalUserManagerServer extends HibernateEntityManager implements
      * removed cert from db and resets counter
      */
     public void revokeCert(String oid) throws UpdateException{
+        PersistenceContext pc = null;
         try {
+            pc = getContext();
+
+            pc.beginTransaction();
+            User u = findByPrimaryKey( oid );
+            u.setCert( null );
+            u.setCertResetCounter( 0 );
+            pc.commitTransaction();
+
+            /*
             // fla note, this will move to a seperate manager once we redesign persistence layer
             Connection connection = ((HibernatePersistenceContext)PersistenceContext.getCurrent()).getSession().connection();
             Statement statement = connection.createStatement();
             statement.executeUpdate("UPDATE " + getTableName() + " SET cert=NULL, cert_reset_counter=0 WHERE oid=" + oid);
+            */
         } catch (SQLException e) {
             LogManager.getInstance().getSystemLogger().log(Level.SEVERE, e.getMessage(), e);
             throw new UpdateException(e.toString(), e);
-        } catch (HibernateException e) {
-            LogManager.getInstance().getSystemLogger().log(Level.SEVERE, e.getMessage(), e);
+        } catch ( FindException e ) {
+            LogManager.getInstance().getSystemLogger().log(Level.WARNING, e.getMessage(), e);
+            throw new UpdateException(e.toString(), e);
+        } catch ( TransactionException e ) {
+            LogManager.getInstance().getSystemLogger().log(Level.WARNING, e.getMessage(), e);
             throw new UpdateException(e.toString(), e);
         } finally {
-            try {
-                PersistenceContext.getCurrent().close();
-            } catch (SQLException e) {
-                LogManager.getInstance().getSystemLogger().log(Level.SEVERE, e.getMessage(), e);
-                throw new UpdateException(e.toString(), e);
-            }
+            if ( pc != null ) pc.close();
         }
     }
 
@@ -306,4 +301,37 @@ public class InternalUserManagerServer extends HibernateEntityManager implements
     public Class getInterfaceClass() {
         return User.class;
     }
+
+    private HibernatePersistenceContext hibernateContext() throws SQLException {
+        return (HibernatePersistenceContext)PersistenceContext.getCurrent();
+    }
+
+    /**
+     * Generates a Hibernate query string for retrieving a single field from a User.
+     * @param oid The objectId of the User to query
+     * @param getfield the (aliased) name of the field to return
+     * @return
+     */
+    private String getFieldQuery( String oid, String getfield ) {
+        String alias = getTableName();
+        StringBuffer sqlBuffer = new StringBuffer( "SELECT " );
+        sqlBuffer.append( alias );
+        sqlBuffer.append( "." );
+        sqlBuffer.append( getfield );
+        sqlBuffer.append( " FROM " );
+        sqlBuffer.append( alias );
+        sqlBuffer.append( " in class " );
+        sqlBuffer.append( getImpClass().getName() );
+        sqlBuffer.append( " WHERE " );
+        sqlBuffer.append( alias );
+        sqlBuffer.append( "." );
+        sqlBuffer.append( F_OID );
+        sqlBuffer.append( " = " );
+        sqlBuffer.append( oid );
+        return sqlBuffer.toString();
+    }
+
+    private static final String F_CERTRESETCOUNTER = "certResetCounter";
+    private static final String F_OID = "oid";
+    public static final String F_CERT = "cert";
 }
