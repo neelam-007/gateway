60cbfc88baef31d1a6b1ee1c87e4bd5987c78649
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-08-25 20:50:24 +0000

    moved .getOrMakeHeader to SoapUtil
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@2060 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/proxy/policy/assertion/credential/wss/ClientWssCredentialSource.java
+++ src/com/l7tech/proxy/policy/assertion/credential/wss/ClientWssCredentialSource.java
@@ -20,41 +20,6 @@ import javax.xml.soap.SOAPException;
  * @version $Revision$
  */
 public abstract class ClientWssCredentialSource implements ClientAssertion {
-
-    // todo, move this to util class
-    protected Element getOrMakeHeader(Document soapMsg) {
-        // use the soap flavor of this document
-        String soapEnvNS = soapMsg.getDocumentElement().getNamespaceURI();
-        NodeList list = soapMsg.getElementsByTagNameNS(soapEnvNS, HEADER_EL_NAME);
-        if (list.getLength() < 1) {
-            String soapEnvNamespacePrefix = soapMsg.getDocumentElement().getPrefix();
-            Element header = soapMsg.createElementNS(soapEnvNS, HEADER_EL_NAME);
-            Element body = getOrMakeBody(soapMsg);
-            header.setPrefix(soapEnvNamespacePrefix);
-            soapMsg.getDocumentElement().insertBefore(header, body);
-            return header;
-        }
-        else return (Element)list.item(0);
-    }
-
-    // todo, move this to util class
-    protected Element getOrMakeBody(Document soapMsg) {
-        // use the soap flavor of this document
-        String soapEnvNS = soapMsg.getDocumentElement().getNamespaceURI();
-        NodeList list = soapMsg.getElementsByTagNameNS(soapEnvNS, BODY_EL_NAME);
-        if (list.getLength() < 1) {
-            String soapEnvNamespacePrefix = soapMsg.getDocumentElement().getPrefix();
-            Element body = soapMsg.createElementNS(soapEnvNS, BODY_EL_NAME);
-            body.setPrefix(soapEnvNamespacePrefix);
-            soapMsg.getDocumentElement().appendChild(body);
-            return body;
-        }
-        else return (Element)list.item(0);
-    }
-
     protected static final String SECURITY_NAMESPACE = "http://schemas.xmlsoap.org/ws/2002/04/secext";
     protected static final String SECURITY_NAME = "Security";
-
-    private static final String HEADER_EL_NAME = "Header";
-    private static final String BODY_EL_NAME = "Body";
 }
