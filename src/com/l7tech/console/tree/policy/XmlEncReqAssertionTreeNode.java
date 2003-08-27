0e808b0c7270ddf26cec4baf906f3b1af591b6d7
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-08-27 18:38:00 +0000

    new nodes to address design change of xml-enc assertion (separate assertion for request and response)
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@2147 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/console/tree/policy/XmlEncReqAssertionTreeNode.java
@@ -0,0 +1,58 @@
+package com.l7tech.console.tree.policy;
+
+
+import com.l7tech.policy.assertion.Assertion;
+import com.l7tech.console.action.XmlEncPropertiesAction;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Class <code>XmlEncReqAssertionTreeNode</code> specifies the policy
+ * element that represents the XML encryption of the request.
+ * <p>
+ * @author flascell
+ */
+public class XmlEncReqAssertionTreeNode extends LeafAssertionTreeNode {
+
+    public XmlEncReqAssertionTreeNode(Assertion assertion) {
+        super(assertion);
+    }
+
+    /**
+     * @return the node name that is displayed
+     */
+    public String getName() {
+        return "XML encryption of request";
+    }
+
+    /**
+       * Get the set of actions associated with this node.
+       * This may be used e.g. in constructing a context menu.
+       *
+       * @return actions appropriate to the node
+       */
+      public Action[] getActions() {
+        return new Action[0];
+      }
+
+
+    /**
+     *Test if the node can be deleted. Default is <code>true</code>
+     *
+     * @return true if the node can be deleted, false otherwise
+     */
+    public boolean canDelete() {
+        return true;
+    }
+
+    /**
+     * subclasses override this method specifying the resource name
+     *
+     * @param open for nodes that can be opened, can have children
+     */
+    protected String iconResource(boolean open) {
+        return "com/l7tech/console/resources/xmlencryption.gif";
+    }
+}
