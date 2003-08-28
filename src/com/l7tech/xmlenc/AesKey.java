b7eaddea7b2fe13b298c357d8faa1920e43ee2e3
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-08-28 16:21:05 +0000

    moved this out of XmlMangler so that ALL developers can enjoy it ;)
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@2183 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/xmlenc/AesKey.java
@@ -0,0 +1,35 @@
+package com.l7tech.xmlenc;
+
+import javax.crypto.SecretKey;
+
+/**
+ * LAYER 7 TECHNOLOGIES, INC
+ *
+ * User: flascell, mike
+ * Date: Aug 28, 2003
+ * Time: 9:17:45 AM
+ * $Id$
+ *
+ * Key Used by the xml mangler
+ */
+public class AesKey implements SecretKey {
+    private byte[] bytes;
+
+    public AesKey(byte[] bytes) {
+        if (bytes.length != 16 && bytes.length != 24 && bytes.length != 32)
+            throw new IllegalArgumentException("Byte array is wrong length for AES128, AES192 or AES256");
+        this.bytes = bytes;
+    }
+
+    public String getAlgorithm() {
+        return "AES";
+    }
+
+    public String getFormat() {
+        return "RAW";
+    }
+
+    public byte[] getEncoded() {
+        return bytes;
+    }
+}
