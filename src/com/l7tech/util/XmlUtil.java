d8122281448c31ef80183a78d4f7cda5df06e447
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-08-28 23:25:17 +0000

    added
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@2223 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/util/XmlUtil.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2003 Layer 7 Technologies Inc.
+ *
+ * $Id$
+ */
+
+package com.l7tech.util;
+
+import org.apache.log4j.Category;
+import org.apache.tools.ant.filters.StringInputStream;
+import org.apache.xml.serialize.XMLSerializer;
+import org.apache.xml.serialize.OutputFormat;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.DocumentBuilder;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+
+/**
+ * Thread-local XML parsing and pretty-printing utilities.
+ * User: mike
+ * Date: Aug 28, 2003
+ * Time: 4:20:59 PM
+ */
+public class XmlUtil {
+    private static final Category log = Category.getInstance(XmlUtil.class);
+
+    private static ThreadLocal documentBuilder = new ThreadLocal() {
+        protected synchronized Object initialValue() {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            dbf.setNamespaceAware(true);
+            try {
+                return dbf.newDocumentBuilder();
+            } catch (ParserConfigurationException e) {
+                log.error(e);
+                throw new RuntimeException(e); // can't happen
+            }
+        }
+    };
+
+    public static DocumentBuilder getDocumentBuilder() {
+        return (DocumentBuilder) documentBuilder.get();
+    }
+
+    public static Document stringToDocument(String inputXmlNotAUrl) throws IOException, SAXException {
+        StringInputStream sis = new StringInputStream(inputXmlNotAUrl);
+        return parse(sis);
+    }
+
+    public static Document parse(InputStream input) throws IOException, SAXException {
+        return getDocumentBuilder().parse(input);
+    }
+
+
+    public static ThreadLocal xmlSerializer = new ThreadLocal() {
+        protected synchronized Object initialValue() {
+            XMLSerializer xmlSerializer = new XMLSerializer();
+            OutputFormat of = new OutputFormat();
+            of.setIndent(4);
+            xmlSerializer.setOutputFormat(of);
+            return xmlSerializer;
+        }
+    };
+
+    public static XMLSerializer getXmlSerializer() {
+        return (XMLSerializer) xmlSerializer.get();
+    }
+
+    public static String documentToString(Document doc) throws IOException {
+        final StringWriter sw = new StringWriter();
+        getXmlSerializer().setOutputCharStream(sw);
+        getXmlSerializer().serialize(doc);
+        return sw.toString();
+    }
+}
