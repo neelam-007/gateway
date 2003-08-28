b35e5c7e191f4b7e171b2dd2fc18ea985393beea
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-08-28 20:26:49 +0000

    documented the format of requests and responses with regards to the WSSC tokens
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@2195 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/xmlsig/SecureConversationTokenHandler.java
+++ src/com/l7tech/xmlsig/SecureConversationTokenHandler.java
@@ -12,9 +12,54 @@ import com.l7tech.util.SoapUtil;
  * Date: Aug 28, 2003
  * Time: 10:31:55 AM
  * $Id$
- *
+ * <p>
  * Appends or reads nonces, sequence numbers, and session ids from a
  * WS Secure Conversation Token header inside a soap message.
+ *
+ * <p>
+ *
+ * Format for requests :
+ * <PRE>
+ * &lt;S:Envelope xmlns:S="..." xmlns:ds="..." xmlns:wsse="..."&gt;
+ *       &lt;S:Header&gt;
+ *               &lt;wsse:Security&gt;
+ *                       &lt;wsse:SecurityContextToken&gt;
+ *                               &lt;wsu:Identifier&gt;sessionid&lt;/wsu:Identifier&gt;
+ *                               &lt;l7:SeqNr xmlns:l7="http://l7tech.com/ns/msgid"&gt;seqNr&lt;/wsu:SeqNr&gt;
+ *                       &lt;/wsse:SecurityContextToken&gt;
+ *                       &lt;ds:Signature&gt;
+ *                               ...
+ *                       &lt;/ds:Signature&gt;
+ *               &lt;/wsse:Security&gt;
+ *       &lt;/S:Header&gt;
+ *       &lt;S:Body&gt;
+ *               &lt;tru:StockSymbol xmlns:tru="http://fabrikam123.com/payloads"&gt;
+ *                       QQQ
+ *               &lt;/tru:StockSymbol&gt;
+ *       &lt;/S:Body&gt;
+ * &lt;/S:Envelope&gt;
+ * </PRE>
+ *
+ * Format for responses :
+ * <PRE>
+ * &lt;S:Envelope xmlns:S="..." xmlns:ds="..." xmlns:wsse="..."&gt;
+ *       &lt;S:Header&gt;
+ *               &lt;wsse:Security&gt;
+ *                       &lt;wsse:SecurityContextToken&gt;
+ *                               &lt;wsu:Identifier&gt;noncenr&lt;/wsu:Identifier&gt;
+ *                       &lt;/wsse:SecurityContextToken&gt;
+ *                       &lt;ds:Signature&gt;
+ *                               ...
+ *                       &lt;/ds:Signature&gt;
+ *               &lt;/wsse:Security&gt;
+ *       &lt;/S:Header&gt;
+ *       &lt;S:Body&gt;
+ *               &lt;tru:StockSymbol xmlns:tru="http://fabrikam123.com/payloads"&gt;
+ *                       QQQ
+ *               &lt;/tru:StockSymbol&gt;
+ *       &lt;/S:Body&gt;
+ * &lt;/S:Envelope&gt;
+ * </PRE>
  *
  */
 public class SecureConversationTokenHandler {
