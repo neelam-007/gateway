95262a1ad8f8f166ad19ba39268c10e14abf24a7
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-08-28 21:50:11 +0000

    added getOrMakeSecurityElement
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@2204 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/util/SoapUtil.java
+++ src/com/l7tech/util/SoapUtil.java
@@ -32,8 +32,11 @@ public class SoapUtil {
         ENVELOPE_URIS.add( "urn:schemas-xmlsoap-org:soap.v1" );
     }
 
-    public static final String HEADER = "Header";
-    public static final String BODY   = "Body";
+    public static final String BODY_EL_NAME   = "Body";
+    public static final String HEADER_EL_NAME = "Header";
+    public static final String SECURITY_EL_NAME = "Security";
+    public static final String SECURITY_NAMESPACE_PREFIX = "wsse";
+    public static final String SECURITY_NAMESPACE = "http://schemas.xmlsoap.org/ws/2002/12/secext";
 
     public static Document getDocument(Message soapmsg) throws SAXException, IOException {
         if ( soapmsg instanceof XmlMessage ) {
@@ -56,19 +59,19 @@ public class SoapUtil {
     }
 
     public static Element getHeaderElement( Request request ) throws SAXException, IOException {
-        return getEnvelopePart( request, HEADER );
+        return getEnvelopePart( request, HEADER_EL_NAME );
     }
 
     public static Element getHeaderElement( Document request ) {
-        return getEnvelopePart( request, HEADER );
+        return getEnvelopePart( request, HEADER_EL_NAME );
     }
 
     public static Element getBodyElement( Request request ) throws SAXException, IOException {
-        return getEnvelopePart( request, BODY );
+        return getEnvelopePart( request, BODY_EL_NAME );
     }
 
     public static Element getBodyElement( Document request ) {
-        return getEnvelopePart( request, BODY );
+        return getEnvelopePart( request, BODY_EL_NAME );
     }
 
     static Element getEnvelopePart( Request request, String elementName ) throws SAXException, IOException {
@@ -140,17 +143,17 @@ public class SoapUtil {
     public static Element getOrMakeHeader(Document soapMsg) {
         // use the soap flavor of this document
         String soapEnvNS = soapMsg.getDocumentElement().getNamespaceURI();
-        NodeList list = soapMsg.getElementsByTagNameNS(soapEnvNS, HEADER);
+        NodeList list = soapMsg.getElementsByTagNameNS(soapEnvNS, HEADER_EL_NAME);
         if (list.getLength() < 1) {
             String soapEnvNamespacePrefix = soapMsg.getDocumentElement().getPrefix();
 
             // create header element
-            Element header = soapMsg.createElementNS(soapEnvNS, HEADER);
+            Element header = soapMsg.createElementNS(soapEnvNS, HEADER_EL_NAME);
             header.setPrefix(soapEnvNamespacePrefix);
 
             // if the body is there, get it so that the header can be inserted before it
             Element body = null;
-            NodeList bodylist = soapMsg.getElementsByTagNameNS(soapEnvNS, BODY);
+            NodeList bodylist = soapMsg.getElementsByTagNameNS(soapEnvNS, BODY_EL_NAME);
             if (bodylist.getLength() > 0) {
                 body = (Element)bodylist.item(0);
             }
@@ -163,6 +166,28 @@ public class SoapUtil {
             return header;
         }
         else return (Element)list.item(0);
+    }
+
+    /**
+     * Returns the Security element from the header of a soap message. If the message does not have a header yet, it
+     * creates one and a child Security element and adds it all to the envelope, and returns back the Security element
+     * to you. If a body element exists, the header element will be inserted right before the body element.
+     * @param soapMsg DOM document containing the soap message
+     * @return the security element (never null)
+     */
+    public static Element getOrMakeSecurityElement(Document soapMsg) {
+        NodeList listSecurityElements = soapMsg.getElementsByTagNameNS(SECURITY_NAMESPACE, SECURITY_EL_NAME);
+        if (listSecurityElements.getLength() < 1) {
+            // element does not exist
+            Element header = SoapUtil.getOrMakeHeader(soapMsg);
+            Element securityEl = soapMsg.createElementNS(SECURITY_NAMESPACE, SECURITY_EL_NAME);
+            securityEl.setPrefix(SECURITY_NAMESPACE_PREFIX);
+            securityEl.setAttribute("xmlns:" + SECURITY_NAMESPACE_PREFIX, SECURITY_NAMESPACE);
+            header.insertBefore(securityEl, null);
+            return securityEl;
+        } else {
+            return (Element)listSecurityElements.item(0);
+        }
     }
 
     public static final String FC_CLIENT = "Client";
