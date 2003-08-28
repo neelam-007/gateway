dbe99e1e23f8c2fb8fa6ed5d50a15adacbc9be69
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-08-28 20:45:08 +0000

    Moved BadCredentialsException into proxy/datamodel
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@2197 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/proxy/datamodel/BadCredentialsException.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2003 Layer 7 Technologies Inc.
+ *
+ * $Id$
+ */
+
+package com.l7tech.proxy.datamodel;
+
+/**
+ * Exception thrown during policy processing if the SSG username and/or password is found to be wrong.
+ * User: mike
+ * Date: Aug 22, 2003
+ * Time: 3:26:17 PM
+ */
+public class BadCredentialsException extends Exception {
+    public BadCredentialsException() {
+    }
+
+    public BadCredentialsException(String message) {
+        super(message);
+    }
+
+    public BadCredentialsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BadCredentialsException(Throwable cause) {
+        super(cause);
+    }
+}
