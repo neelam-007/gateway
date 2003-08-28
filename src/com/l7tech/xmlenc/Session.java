b39375e44c52e2484d0fe15220fbca7c9dcaffa4
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-08-28 22:45:17 +0000

    Added a full constructor for use inside the client proxy class SsgSessionManager
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@2211 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/xmlenc/Session.java
+++ src/com/l7tech/xmlenc/Session.java
@@ -11,10 +11,28 @@ package com.l7tech.xmlenc;
 public class Session {
 
     /**
-     * get a session from the SessionManager
+     * Create a new Session, if you are the SessionManager.  Otherwise, use the SessionManager if you are
+     * running on the SSG, or the SsgSessionManager if you are running on the client proxy.
      */
     Session() {
         creationTimestamp = System.currentTimeMillis();
+    }
+
+    /**
+     * Create a new Session with the given state.  Used by the SsgSessionManager on the client to fill in
+     * session information obtained from the session servlet.
+     *
+     * @param id                 id assigned to this session by the server
+     * @param creationTimestamp  timestamp of session creation
+     * @param keyreq             key to be used for encrypting requests
+     * @param keyres             key to be used for encrypting responses
+     */
+    public Session(long id, long creationTimestamp, byte[] keyreq, byte[] keyres, long seq) {
+        this.id = id;
+        this.creationTimestamp = creationTimestamp;
+        this.keyreq = keyreq;
+        this.keyres = keyres;
+        this.highestSeq = seq;
     }
 
     public long getHighestSeq() {
