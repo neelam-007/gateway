edfdcacbaea07b4b8fe1e14c283e8189d6457030
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-08-31 23:00:51 +0000

    Catch and log all hibernate exceptions so we can diagnose why the server does not boot (when it happens).
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@2265 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/server/BootServlet.java
+++ src/com/l7tech/server/BootServlet.java
@@ -7,6 +7,7 @@
 package com.l7tech.server;
 
 import com.l7tech.objectmodel.HibernatePersistenceManager;
+import com.l7tech.logging.LogManager;
 
 import javax.servlet.http.*;
 import javax.servlet.ServletException;
@@ -14,6 +15,8 @@ import javax.servlet.ServletConfig;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
 /**
  * @author alex
@@ -22,16 +25,22 @@ import java.sql.SQLException;
 public class BootServlet extends HttpServlet {
     public void init( ServletConfig config ) throws ServletException {
         super.init( config );
+        // note fla, more exception catching => important to diagnose why server does not boot properly
         try {
             HibernatePersistenceManager.initialize();
-        } catch ( SQLException se ) {
-            se.printStackTrace();
-            throw new ServletException( se );
+        } catch (SQLException e) {
+            logger.log(Level.SEVERE, "SQL ERROR IN BOOT SERVLET", e);
+            throw new ServletException(e);
+        } catch (Throwable e) {
+            logger.log(Level.SEVERE, "ERROR IN BOOT SERVLET", e);
+            throw new ServletException(e);
         }
+        logger.info("Boot servlet complete.");
     }
 
     public void doGet( HttpServletRequest request, HttpServletResponse response ) throws IOException, ServletException {
         PrintWriter out = response.getWriter();
         out.println( "<b>The server has already been initialized!</b>" );
     }
+    private Logger logger = LogManager.getInstance().getSystemLogger();
 }
