eda09f7ebba2f5f3fd468e1be76c9779568e6733
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-09-04 00:09:37 +0000

    Work in progress
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@2360 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/server/policy/assertion/credential/http/ServerHttpDigest.java
+++ src/com/l7tech/server/policy/assertion/credential/http/ServerHttpDigest.java
@@ -12,14 +12,17 @@ import com.l7tech.message.Response;
 import com.l7tech.policy.assertion.AssertionStatus;
 import com.l7tech.policy.assertion.credential.CredentialFinderException;
 import com.l7tech.policy.assertion.credential.PrincipalCredentials;
+import com.l7tech.policy.assertion.credential.CredentialFormat;
 import com.l7tech.policy.assertion.credential.http.HttpDigest;
 import com.l7tech.server.policy.assertion.ServerAssertion;
 import com.l7tech.server.policy.assertion.credential.DigestSessions;
+import com.l7tech.identity.User;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Map;
+import java.io.UnsupportedEncodingException;
 
 /**
  * @author alex
@@ -37,54 +40,26 @@ public class ServerHttpDigest extends ServerHttpCredentialSource implements Serv
     }
 
     public static final String ENCODING = "UTF-8";
-    public static final String SCHEME = "Digest";
-
 
     /**
-     * Generates the WWW-Authenticate header.
-     * <p>
-     * The header MUST follow this template :
-     * <pre>
-     *      WWW-Authenticate    = "WWW-Authenticate" ":" "Digest"
-     *                            digest-challenge
+     * We have little choice but to return a hardcoded value here.
      *
-     *      digest-challenge    = 1#( realm | [ domain ] | nOnce |
-     *                  [ digest-opaque ] |[ stale ] | [ algorithm ] )
-     *
-     *      realm               = "realm" "=" realm-value
-     *      realm-value         = quoted-string
-     *      domain              = "domain" "=" <"> 1#URI <">
-     *      nonce               = "nonce" "=" nonce-value
-     *      nonce-value         = quoted-string
-     *      opaque              = "opaque" "=" quoted-string
-     *      stale               = "stale" "=" ( "true" | "false" )
-     *      algorithm           = "algorithm" "=" ( "MD5" | token )
-     * </pre>
-     *
-     * @param request HTTP Servlet request
-     * @param response HTTP Servlet response
-     * @param nOnce nonce token
+     * @param request
+     * @return
      */
-    protected void setAuthenticateHeader( Request request,
-                                          Response response,
-                                          String nOnce ) {
-
-        // Get the realm name
-        String realmName = _data.getRealm();
-        if (realmName == null)
-            realmName = request.getParameter( Request.PARAM_SERVER_NAME ) + ":"
+    protected String realm( Request request ) {
+        return HttpDigest.REALM;
+        /*
+        String realm = _data.getRealm();
+        if ( realm == null || realm.length() == 0 ) {
+            realm = request.getParameter( Request.PARAM_SERVER_NAME ) + ":"
                 + request.getParameter( Request.PARAM_SERVER_PORT );
-
-        byte[] buffer = _md5.digest(nOnce.getBytes());
-
-        String authenticateHeader = "Digest realm=\"" + realmName + "\", "
-            +  "qop=\"auth\", nonce=\"" + nOnce + "\", " + "opaque=\""
-            + HexUtils.encodeMd5Digest(buffer) + "\"";
-
-        response.setParameter( Response.PARAM_HTTP_WWWAUTHENTICATE, authenticateHeader );
+        }
+        return realm;
+        */
     }
 
-    protected AssertionStatus doCheckCredentials( Request request, Response response ) {
+    protected AssertionStatus doCheckCredentials(Request request, Response response) {
         Map authParams = (Map)request.getParameter( Request.PARAM_HTTP_AUTH_PARAMS );
         String nonce = (String)authParams.get( HttpDigest.PARAM_NONCE );
         String userName = (String)authParams.get( HttpDigest.PARAM_USERNAME );
@@ -93,54 +68,86 @@ public class ServerHttpDigest extends ServerHttpCredentialSource implements Serv
         String cnonce = (String)authParams.get( HttpDigest.PARAM_CNONCE );
         String qop = (String)authParams.get( HttpDigest.PARAM_QOP );
         String uri = (String)authParams.get( HttpDigest.PARAM_URI );
-        String digestResponse = null;
-        String opaque = null;
-        String method = (String)request.getParameter( Request.PARAM_HTTP_METHOD );
+        String digestResponse = (String)authParams.get( HttpDigest.PARAM_RESPONSE );
 
         if ( (userName == null) || (realmName == null) || (nonce == null)
-             || (uri == null) || (response == null) )
-            return AssertionStatus.AUTH_FAILED;
+             || (uri == null) || ( digestResponse == null) )
+            return AssertionStatus.AUTH_REQUIRED;
 
         if (qop != null && (cnonce == null || nc == null))
+            return AssertionStatus.AUTH_REQUIRED;
+
+        DigestSessions sessions = DigestSessions.getInstance();
+
+        if ( sessions.use( nonce ) ) {
+            _log.fine( "Nonce " + nonce + " still valid" );
+            return AssertionStatus.NONE;
+        } else {
+            _log.info( "Nonce " + nonce + " expired" );
+            sessions.invalidate( nonce );
             return AssertionStatus.AUTH_FAILED;
-
-        String a2 = method + ":" + uri;
-
-        String md5a2 = HexUtils.encodeMd5Digest( _md5.digest(a2.getBytes()) );
-
-        return ( authenticate(userName, digestResponse, nonce, nc, cnonce, qop,
-                                   realmName, md5a2));
+        }
     }
 
     protected PrincipalCredentials doFindCredentials( Request request, Response response ) throws CredentialFinderException {
         Map authParams = (Map)request.getParameter( Request.PARAM_HTTP_AUTH_PARAMS );
-        // TODO
-        return null;
+
+        if ( authParams == null ) return null;
+
+        String userName = (String)authParams.get( HttpDigest.PARAM_USERNAME );
+        String realmName = (String)authParams.get( HttpDigest.PARAM_REALM );
+        String digestResponse = (String)authParams.get( HttpDigest.PARAM_RESPONSE );
+
+        authParams.put( HttpDigest.PARAM_METHOD, request.getParameter( Request.PARAM_HTTP_REQUEST_URI ) );
+
+        if ( (userName == null) || (realmName == null) || ( digestResponse == null) )
+            return null;
+
+        User u = new User();
+        u.setLogin( userName );
+
+        try {
+            return new PrincipalCredentials( u, digestResponse.getBytes( ENCODING ), CredentialFormat.DIGEST, realmName, authParams );
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException( e );
+        }
+    }
+
+    private Map myChallengeParams( String nonce ) {
+        Map params = new HashMap();
+        params.put( HttpDigest.PARAM_QOP, HttpDigest.QOP_AUTH );
+        params.put( HttpDigest.PARAM_NONCE, nonce );
+        params.put( HttpDigest.PARAM_OPAQUE, HexUtils.encodeMd5Digest( _md5.digest( nonce.getBytes() ) ) );
+        return params;
     }
 
     protected Map challengeParams( Request request, Response response ) {
         Map requestAuthParams = (Map)request.getParameter( Request.PARAM_HTTP_AUTH_PARAMS );
-        Map params = new HashMap();
+        DigestSessions sessions = DigestSessions.getInstance();
 
-        String requestNonce = (String)requestAuthParams.get( HttpDigest.PARAM_NONCE );
-        if ( requestNonce == null || requestNonce.length() == 0 ) {
-            String responseNonce = DigestSessions.getInstance().generateNonce( request, _data.getNonceTimeout(), _data.getMaxNonceCount() );
+        String nonce;
+        if ( requestAuthParams == null )
+            nonce = null;
+        else
+            nonce = (String)requestAuthParams.get( HttpDigest.PARAM_NONCE );
 
+        if ( nonce == null || nonce.length() == 0 ) {
+            // New session
+            String newNonce = sessions.generate( request, _data.getNonceTimeout(), _data.getMaxNonceCount() );
+            return myChallengeParams( newNonce );
         } else {
-
+            // Existing digest session
+            if ( !sessions.use( nonce ) ) {
+                // Nonce has been invalidated or is expired
+                sessions.invalidate( nonce );
+                nonce = sessions.generate( request, _data.getNonceTimeout(), _data.getMaxNonceCount() );
+            }
+            return myChallengeParams( nonce );
         }
-
-
-        // TODO
-        return null;
     }
 
     protected String scheme() {
-        return SCHEME;
-    }
-
-    private AssertionStatus authenticate( String userName, String digestResponse, String nonce, String nc, String cnonce, String qop, String realmName, String md5a2 ) {
-        return AssertionStatus.NOT_YET_IMPLEMENTED;
+        return HttpDigest.SCHEME;
     }
 
     protected HttpDigest _data;
