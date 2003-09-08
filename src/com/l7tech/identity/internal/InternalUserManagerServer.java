bb4b2de6d0f60cabb142cb340bd3c2a881b10de0
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-09-08 16:55:14 +0000

    prevents last administrator to be deleted. enforce revoke cert at passwd change logic.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@2442 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/identity/internal/InternalUserManagerServer.java
+++ src/com/l7tech/identity/internal/InternalUserManagerServer.java
@@ -5,6 +5,7 @@ import cirrus.hibernate.Session;
 import com.l7tech.identity.IdProvConfManagerServer;
 import com.l7tech.identity.User;
 import com.l7tech.identity.UserManager;
+import com.l7tech.identity.Group;
 import com.l7tech.logging.LogManager;
 import com.l7tech.objectmodel.*;
 
@@ -18,6 +19,8 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.rmi.RemoteException;
 
 /**
  * Layer 7 Technologies, inc.
@@ -30,6 +33,7 @@ import java.util.logging.Level;
 public class InternalUserManagerServer extends HibernateEntityManager implements UserManager {
     public InternalUserManagerServer() {
         super();
+        logger = LogManager.getInstance().getSystemLogger();
     }
 
     public User findByPrimaryKey(String oid) throws FindException {
@@ -56,11 +60,11 @@ public class InternalUserManagerServer extends HibernateEntityManager implements
                 return u;
             default:
                 String err = "Found more than one user with the login " + login;
-                LogManager.getInstance().getSystemLogger().log(Level.SEVERE, err);
+                logger.log(Level.SEVERE, err);
                 throw new FindException( err );
             }
         } catch ( SQLException se ) {
-            LogManager.getInstance().getSystemLogger().log(Level.SEVERE, null, se);
+            logger.log(Level.SEVERE, null, se);
             throw new FindException( se.toString(), se );
         }
     }
@@ -74,13 +78,13 @@ public class InternalUserManagerServer extends HibernateEntityManager implements
         try {
             List users = _manager.find(getContext(), "from " + getTableName() + " in class " + getImpClass().getName() + " where " + getTableName() + ".login like ?", searchString, String.class);
             Collection output = new ArrayList();
-            LogManager.getInstance().getSystemLogger().log(Level.INFO, "search for " + searchString + " returns " + users.size() + " users.");
+            logger.log(Level.INFO, "search for " + searchString + " returns " + users.size() + " users.");
             for (Iterator i = users.iterator(); i.hasNext();) {
                 output.add(userToHeader((User)i.next()));
             }
             return output;
         } catch (SQLException e) {
-            LogManager.getInstance().getSystemLogger().log(Level.SEVERE, "error searching users with pattern " + searchString, e);
+            logger.log(Level.SEVERE, "error searching users with pattern " + searchString, e);
             throw new FindException(e.toString(), e);
         }
     }
@@ -99,17 +103,17 @@ public class InternalUserManagerServer extends HibernateEntityManager implements
             Integer i = (Integer)results.get(0);
             int res = i.intValue();
 
-            LogManager.getInstance().getSystemLogger().log(Level.INFO, "cert_reset_counter value for user " + oid + " = " + res);
+            logger.log(Level.INFO, "cert_reset_counter value for user " + oid + " = " + res);
             if (res < CERTRESETCOUNTER_MAX) return true;
             else {
-                LogManager.getInstance().getSystemLogger().log(Level.SEVERE, "user " + oid + " not authorized to regen cert.");
+                logger.log(Level.SEVERE, "user " + oid + " not authorized to regen cert.");
                 return false;
             }
         } catch (SQLException e) {
-            LogManager.getInstance().getSystemLogger().log(Level.SEVERE, "error getting cert_reset_counter value for " + oid, e);
+            logger.log(Level.SEVERE, "error getting cert_reset_counter value for " + oid, e);
             throw new FindException(e.toString(), e);
         } catch (HibernateException e) {
-            LogManager.getInstance().getSystemLogger().log(Level.SEVERE, "error getting cert_reset_counter value for " + oid, e);
+            logger.log(Level.SEVERE, "error getting cert_reset_counter value for " + oid, e);
             throw new FindException(e.toString(), e);
         } finally {
             if ( hpc != null ) hpc.close();
@@ -132,16 +136,16 @@ public class InternalUserManagerServer extends HibernateEntityManager implements
             u.setCertResetCounter( u.getCertResetCounter() + 1 );
             hpc.commitTransaction();
         } catch (SQLException e) {
-            LogManager.getInstance().getSystemLogger().log(Level.SEVERE, e.getMessage(), e);
+            logger.log(Level.SEVERE, e.getMessage(), e);
             throw new UpdateException(e.toString(), e);
         } catch (CertificateEncodingException e) {
-            LogManager.getInstance().getSystemLogger().log(Level.SEVERE, e.getMessage(), e);
+            logger.log(Level.SEVERE, e.getMessage(), e);
             throw new UpdateException(e.toString(), e);
         } catch ( TransactionException e ) {
-            LogManager.getInstance().getSystemLogger().log(Level.WARNING, e.getMessage(), e);
+            logger.log(Level.WARNING, e.getMessage(), e);
             throw new UpdateException( e.toString(), e );
         } catch ( FindException e ) {
-            LogManager.getInstance().getSystemLogger().log(Level.WARNING, e.getMessage(), e);
+            logger.log(Level.WARNING, e.getMessage(), e);
             throw new UpdateException( e.toString(), e );
         } finally {
             if ( hpc != null ) hpc.close();
@@ -165,7 +169,7 @@ public class InternalUserManagerServer extends HibernateEntityManager implements
             Certificate dledcert = CertificateFactory.getInstance("X.509").generateCertificate(new ByteArrayInputStream(certbytes));
             return dledcert;
         } catch (Exception e) {
-            LogManager.getInstance().getSystemLogger().log(Level.SEVERE, e.getMessage(), e);
+            logger.log(Level.SEVERE, e.getMessage(), e);
             throw new FindException(e.toString(), e);
         } finally {
             if ( hpc != null ) hpc.close();
@@ -186,13 +190,13 @@ public class InternalUserManagerServer extends HibernateEntityManager implements
             u.setCertResetCounter(CERTRESETCOUNTER_MAX);
             pc.commitTransaction();
         } catch (SQLException e) {
-            LogManager.getInstance().getSystemLogger().log(Level.SEVERE, e.getMessage(), e);
+            logger.log(Level.SEVERE, e.getMessage(), e);
             throw new UpdateException(e.toString(), e);
         } catch ( FindException e ) {
-            LogManager.getInstance().getSystemLogger().log(Level.WARNING, e.getMessage(), e);
+            logger.log(Level.WARNING, e.getMessage(), e);
             throw new UpdateException(e.toString(), e);
         } catch ( TransactionException e ) {
-            LogManager.getInstance().getSystemLogger().log(Level.WARNING, e.getMessage(), e);
+            logger.log(Level.WARNING, e.getMessage(), e);
             throw new UpdateException(e.toString(), e);
         } finally {
             if ( pc != null ) pc.close();
@@ -213,13 +217,13 @@ public class InternalUserManagerServer extends HibernateEntityManager implements
             u.setCertResetCounter( 0 );
             pc.commitTransaction();
         } catch (SQLException e) {
-            LogManager.getInstance().getSystemLogger().log(Level.SEVERE, e.getMessage(), e);
+            logger.log(Level.SEVERE, e.getMessage(), e);
             throw new UpdateException(e.toString(), e);
         } catch ( FindException e ) {
-            LogManager.getInstance().getSystemLogger().log(Level.WARNING, e.getMessage(), e);
+            logger.log(Level.WARNING, e.getMessage(), e);
             throw new UpdateException(e.toString(), e);
         } catch ( TransactionException e ) {
-            LogManager.getInstance().getSystemLogger().log(Level.WARNING, e.getMessage(), e);
+            logger.log(Level.WARNING, e.getMessage(), e);
             throw new UpdateException(e.toString(), e);
         } finally {
             if ( pc != null ) pc.close();
@@ -228,10 +232,19 @@ public class InternalUserManagerServer extends HibernateEntityManager implements
 
     public void delete(User user) throws DeleteException {
         try {
+            User originalUser = findByPrimaryKey(Long.toString(user.getOid()));
+            if (isLastAdmin(originalUser)) {
+                String msg = "An attempt was made to nuke the last standing adminstrator";
+                logger.severe(msg);
+                throw new DeleteException(msg);
+            }
             _manager.delete( getContext(), user );
         } catch ( SQLException se ) {
-            LogManager.getInstance().getSystemLogger().log(Level.SEVERE, null, se);
+            logger.log(Level.SEVERE, null, se);
             throw new DeleteException( se.toString(), se );
+        } catch ( FindException e ) {
+            logger.log(Level.SEVERE, null, e);
+            throw new DeleteException( e.toString(), e );
         }
     }
 
@@ -239,17 +252,58 @@ public class InternalUserManagerServer extends HibernateEntityManager implements
         try {
             return _manager.save( getContext(), user );
         } catch ( SQLException se ) {
-            LogManager.getInstance().getSystemLogger().log(Level.SEVERE, null, se);
+            logger.log(Level.SEVERE, null, se);
             throw new SaveException( se.toString(), se );
         }
     }
 
+    /**
+     * checks that passwd was changed. if so, also revokes the existing cert
+     * checks if the user is the last standing admin account, throws if so
+     * @param user existing user
+     */
     public void update( User user ) throws UpdateException {
         try {
-            _manager.update( getContext(), user );
+            User originalUser = findByPrimaryKey(Long.toString(user.getOid()));
+            // here, we should make sure that IF the user is an administrator, he should not
+            // delete his membership to the admin group unless there is another exsiting member
+            if (isLastAdmin(originalUser)) {
+                // was he stupid enough to nuke his admin membership?
+                boolean stillAdmin = false;
+                Iterator newgrpsiterator = user.getGroups().iterator();
+                while (newgrpsiterator.hasNext()) {
+                    Group grp = (Group)newgrpsiterator.next();
+                    if (Group.ADMIN_GROUP_NAME.equals(grp.getName())) {
+                        stillAdmin = true;
+                        break;
+                    }
+                }
+                if (!stillAdmin) {
+                    logger.severe("An attempt was made to update the last standing administrator with no membership to the admin group!");
+                    throw new UpdateException("This update would revoke admin membership on the last standing administrator");
+                }
+            }
+            // checks whether the user changed his password
+            String originalPasswd = originalUser.getPassword();
+            String newPasswd = user.getPassword();
+
+            if (!originalPasswd.equals(newPasswd)) {
+                // must revoke the cert
+                originalUser.setCert( null );
+                originalUser.setCertResetCounter( 0 );
+                user.setCert( null );
+                user.setCertResetCounter( 0 );
+            }
+            // update user
+            originalUser.copyFrom(user);
+            // update from existing user
+            _manager.update( getContext(), originalUser );
         } catch ( SQLException se ) {
-            LogManager.getInstance().getSystemLogger().log(Level.SEVERE, null, se);
+            logger.log(Level.SEVERE, null, se);
             throw new UpdateException( se.toString(), se );
+        } catch ( FindException e ) {
+            logger.log(Level.SEVERE, null, e);
+            throw new UpdateException( e.toString(), e );
         }
     }
 
@@ -261,7 +315,7 @@ public class InternalUserManagerServer extends HibernateEntityManager implements
         try {
             return findByPrimaryKey(header.getStrId());
         } catch (FindException e) {
-            LogManager.getInstance().getSystemLogger().log(Level.SEVERE, e.getMessage(), e);
+            logger.log(Level.SEVERE, e.getMessage(), e);
             throw new RuntimeException(e);
         }
     }
@@ -307,6 +361,26 @@ public class InternalUserManagerServer extends HibernateEntityManager implements
         return sqlBuffer.toString();
     }
 
+    /**
+     * check whether this user is the last standing administrator
+     * this is used at update time to prevent the last administrator to nuke his admin accout membership
+     * @param currentUser
+     * @return true is this user is an administrator and no other users are
+     */
+    private boolean isLastAdmin(User currentUser) {
+        Iterator i = currentUser.getGroups().iterator();
+        while (i.hasNext()) {
+            Group grp = (Group)i.next();
+            // is he an administrator?
+            if (Group.ADMIN_GROUP_NAME.equals(grp.getName())) {
+                // is he the last one ?
+                if (grp.getMembers().size() <= 1) return true;
+                return false;
+            }
+        }
+        return false;
+    }
+
     private static final String F_CERTRESETCOUNTER = "certResetCounter";
     // this value for the F_CERTRESETCOUNTER column represent the maximum number of retries that
     // a user can regen his cert until the admin must revoke. once a cert is used in the authentication
@@ -315,4 +389,5 @@ public class InternalUserManagerServer extends HibernateEntityManager implements
     private static final int CERTRESETCOUNTER_MAX = 10;
     private static final String F_OID = "oid";
     public static final String F_CERT = "cert";
+    private Logger logger = null;
 }
