5aa2ec98523b3cc2bf4b12bad82092d7213c3eaf
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-09-09 21:46:31 +0000

    Utility widget: a pseudo-label that does word wrapping.  Currently can also be selected (since it's based on a JTextArea).
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@2501 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/common/gui/WrappingLabel.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2003 Layer 7 Technologies Inc.
+ *
+ * $Id$
+ */
+
+package com.l7tech.common.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Sort of like a JLabel, but supports wrapping the text inside it.
+ *
+ * User: mike
+ * Date: Sep 9, 2003
+ * Time: 2:03:08 PM
+ */
+public class WrappingLabel extends JTextArea {
+    private int setLines = 0;
+
+    /**
+     * Create a new WrappingLabel with no text.
+     */
+    public WrappingLabel() {
+        super();
+        initWrappingText();
+    }
+
+    /**
+     * Create a new WrappingLabel with the specified label text, and no preferred size.
+     */
+    public WrappingLabel(String text) {
+        super(text);
+        initWrappingText();
+    }
+
+    /**
+     * Create a new WrappingLabel with the specified label text, that will have its preferredSize
+     * set to a width of (textWidth / lines) and a height of (textHeight * lines).
+     *
+     * @param text
+     */
+    public WrappingLabel(String text, int lines) {
+        super(text);
+        initWrappingText();
+        this.setLines = lines;
+        initWrappingTextPreferredSize();
+    }
+
+    private void initWrappingText() {
+        setEditable(false);
+        setLineWrap(true);
+        setWrapStyleWord(true);
+        setBackground((Color)UIManager.get("Label.background"));
+        setForeground((Color)UIManager.get("Label.foreground"));
+        setFont((Font)UIManager.get("Label.font"));
+    }
+
+    private void initWrappingTextPreferredSize() {
+        if (setLines > 0) {
+            int fw = getFontMetrics(getFont()).stringWidth(getText());
+            int fh = getFontMetrics(getFont()).getHeight();
+            setPreferredSize(new Dimension(fw / setLines, fh * setLines));
+            invalidate();
+        } else {
+            setPreferredSize(null);
+        }
+    }
+
+    public void setText(String text) {
+        super.setText(text);
+        if (setLines != 0)
+            initWrappingTextPreferredSize();
+    }
+
+    public void setLines(int lines) {
+        this.setLines = lines;
+        initWrappingTextPreferredSize();
+    }
+}
