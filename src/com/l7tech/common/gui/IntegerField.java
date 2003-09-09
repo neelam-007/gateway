7863c91e885e67b0cf14cc50778f3314d7a97f80
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-09-09 23:43:42 +0000

    Moved into common
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@2508 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/common/gui/IntegerField.java
@@ -0,0 +1,70 @@
+package com.l7tech.common.gui;
+
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.PlainDocument;
+
+
+/**
+ * <CODE>IntegerField</CODE> is the implementation of
+ * <CODE>PlainDocument</CODE> accepting integers fields only.
+ * 
+ * The instance may be creted by specifiyng the min and max
+ * values, thus limiting the integer range.
+ * 
+ * @author <a href="mailto:emarceta@layer7-tech.com">Emil Marceta</a>
+ * @version 1.1
+ * @see javax.swing.text.Document
+ * @see javax.swing.JTextField
+ */
+public class IntegerField extends PlainDocument {
+  private int minInt;
+  private int maxInt;
+
+  public IntegerField() {
+    minInt = Integer.MIN_VALUE;
+    maxInt = Integer.MAX_VALUE;
+  }
+
+  public IntegerField(int min, int max) {
+    // sanity check
+    if (!(min < max)) {
+      throw new IllegalArgumentException("min < max");
+    }
+    minInt = min;
+    maxInt = max;
+  }
+
+  /**
+   * Inserts some content into the document.
+   *
+   * @param offs the starting offset >= 0
+   * @param str the string to insert; does nothing with null/empty strings
+   * @param a the attributes for the inserted content
+   * @exception javax.swing.text.BadLocationException  the given insert position is not a valid
+   *   position within the document
+   * @see javax.swing.text.Document#insertString
+   */
+  public void insertString(int offs, String str, AttributeSet a)
+  throws BadLocationException
+  {
+    if (str == null) return;
+
+    char[] cArr = str.toCharArray();
+    for (int i=0;i< cArr.length;i++) {
+      if (!Character.isDigit(cArr[i])) {
+        return;
+      }
+    }
+    try {
+      String s = getText(0, getLength())+str;
+      int newInt = Integer.parseInt(s);
+      if (newInt >=minInt && newInt <= maxInt) {
+        super.insertString(offs, str, a);
+      } else {
+      }
+    } catch(NumberFormatException e) {
+    }
+  }
+}
+
