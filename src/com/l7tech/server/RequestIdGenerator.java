62047a3b6b48b23352efc22d648f2bf609d43dbf
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-09-10 23:29:04 +0000

    Done and tested
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@2565 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/server/RequestIdGenerator.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2003 Layer 7 Technologies Inc.
+ *
+ * $Id$
+ */
+
+package com.l7tech.server;
+
+import com.l7tech.server.ServerConfig;
+
+import java.math.BigInteger;
+
+/**
+ * Generates reasonably-unique request identifiers incorporating a server ID, (see {@link ServerConfig#getServerId()}) a boot time and sequence number.
+ *
+ * @author alex
+ * @version $Revision$
+ */
+public class RequestIdGenerator {
+    public static BigInteger next() {
+        RequestIdGenerator instance = getInstance();
+        long seq = instance.nextSequence();
+        BigInteger next = new BigInteger( instance._seed.toString() );
+        return next.add( new BigInteger( new Long( seq ).toString() ) );
+    }
+
+    private synchronized static RequestIdGenerator getInstance() {
+        if ( _instance == null ) _instance = new RequestIdGenerator();
+        return _instance;
+    }
+
+    private synchronized void reseed( long time ) {
+        BigInteger seedServerId = new BigInteger( new Byte( _serverId ).toString() ).shiftLeft(120);
+        BigInteger bigTime = new BigInteger( new Long( time ).toString() ).shiftLeft(72);
+
+        _seed = seedServerId.or( bigTime );
+    }
+
+    private RequestIdGenerator() {
+        ServerConfig config = ServerConfig.getInstance();
+        _serverId = config.getServerId();
+        reseed( config.getServerBootTime() );
+    }
+
+    private synchronized long nextSequence() {
+        if ( _sequence >= MAX_SEQ ) {
+            _sequence = 0;
+            reseed( System.currentTimeMillis() );
+        }
+        return _sequence++;
+    }
+
+    private BigInteger _seed;
+    private byte _serverId;
+    private long _sequence;
+
+    private static RequestIdGenerator _instance;
+    public static final long MAX_SEQ = 0xffffffffffffffL; // 2^56 - 1
+}
