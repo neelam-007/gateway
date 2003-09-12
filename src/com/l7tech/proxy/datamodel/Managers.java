b90bfe02b15096985681bc4ab67cb3050d0de713
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-09-12 20:19:02 +0000

    Fix javadoc header.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@2647 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/proxy/datamodel/Managers.java
+++ src/com/l7tech/proxy/datamodel/Managers.java
@@ -8,10 +8,10 @@ package com.l7tech.proxy.datamodel;
 
 /**
  * Used to obtain datamodel classes.
+ *
  * User: mike
  * Date: Jun 3, 2003
  * Time: 2:45:08 PM
- * To change this template use Options | File Templates.
  */
 public class Managers {
     private static CredentialManager credentialManager = null;
