653d4b58d403381c43d01b4c2a5bf9970cb1f9ab
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-09-12 21:15:35 +0000

    Javadoc
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@2655 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/objectmodel/StaleUpdateException.java
+++ src/com/l7tech/objectmodel/StaleUpdateException.java
@@ -7,6 +7,9 @@
 package com.l7tech.objectmodel;
 
 /**
+ * Indicates an attempt to update a persistent object with data that is obsolete, because
+ * the persistent data has been changed since the candidate object was loaded.
+ *
  * @author alex
  * @version $Revision$
  */
