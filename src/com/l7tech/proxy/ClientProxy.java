ecf02eae29bca78669ac425685e3839617eb7607
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-09-15 22:36:11 +0000

    Added CVS_REVISION.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@2699 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/proxy/ClientProxy.java
+++ src/com/l7tech/proxy/ClientProxy.java
@@ -45,6 +45,7 @@ import java.security.cert.X509Certificate;
  */
 public class ClientProxy {
     private static final Category log = Category.getInstance(ClientProxy.class);
+    public static final String CVS_REVISION = "$Revision$"; // sorry emil
     public static final String PROXY_CONFIG =
             System.getProperties().getProperty("user.home") + File.separator + ".l7tech";
 
