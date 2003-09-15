f3e5cd81b6e187071e1f78808450c18077be663e
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-09-15 22:33:13 +0000

    Added a simple about box that shows the CVS revision of the ClientProxy class.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@2694 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/proxy/gui/AgentAboutBox.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2003 Layer 7 Technologies Inc.
+ *
+ * $Id$
+ */
+
+package com.l7tech.proxy.gui;
+
+import com.l7tech.proxy.ClientProxy;
+import com.l7tech.proxy.gui.util.IconManager;
+
+import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+/**
+ *
+ * User: mike
+ * Date: Sep 15, 2003
+ * Time: 3:14:26 PM
+ */
+public class AgentAboutBox extends JDialog {
+    public AgentAboutBox() {
+        super();
+        setModal(true);
+        Container pane = getContentPane();
+        pane.setLayout(new GridBagLayout());
+        JLabel thinger = new JLabel(IconManager.getSmallSplashImageIcon());
+        thinger.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED));
+        pane.add(thinger,
+                 new GridBagConstraints(0, 0, 2, 1, 1.0, 1.0,
+                                        GridBagConstraints.CENTER,
+                                        GridBagConstraints.BOTH,
+                                        new Insets(8, 20, 0, 20), 0, 0));
+
+        JLabel version = new JLabel(ClientProxy.CVS_REVISION);
+        pane.add(version,
+                 new GridBagConstraints(0, 1, 1, 1, 1.0, 1.0,
+                                        GridBagConstraints.WEST,
+                                        GridBagConstraints.NONE,
+                                        new Insets(0, 20, 8, 0), 0, 0));
+
+        JButton okButton = new JButton(" Close ");
+        okButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                AgentAboutBox.this.dispose();
+            }
+        });
+        pane.add(okButton,
+                 new GridBagConstraints(1, 1, 1, 1, 1.0, 1.0,
+                                        GridBagConstraints.SOUTHEAST,
+                                        GridBagConstraints.NONE,
+                                        new Insets(5, 0, 8, 20), 0, 0));
+        pack();
+    }
+
+    public static void main(String[] args) {
+        new AgentAboutBox().show();
+        System.exit(1);
+    }
+}
