aa354526869c17968bfd57a455b52d417e0266b5
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-09-16 21:43:59 +0000

    getParameter() moved down
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@2779 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/message/TransportMetadata.java
+++ src/com/l7tech/message/TransportMetadata.java
@@ -6,28 +6,11 @@
 
 package com.l7tech.message;
 
-import java.util.*;
-
 /**
  * @author alex
  * @version $Revision$
  */
 public abstract class TransportMetadata {
-    public void setParameter( String name, Object value ) {
-        if ( _params == Collections.EMPTY_MAP ) _params = new HashMap();
-        _params.put( name, value );
-    }
-
-    public Object getParameter( String name ) {
-        Object param = doGetParameter( name );
-        if ( param == null )
-            return _params.get(name);
-        else
-            return param;
-    }
-
     public abstract TransportProtocol getProtocol();
-    protected abstract Object doGetParameter( String name );
-
-    protected Map _params = Collections.EMPTY_MAP;
+    abstract Object getParameter( String name );
 }
