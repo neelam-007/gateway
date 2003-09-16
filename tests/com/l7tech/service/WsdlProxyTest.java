902b952186c400f3ee2775b397e44bf147482494
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-09-16 22:07:22 +0000

    to test wsdl proxy functionality
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@2787 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ tests/com/l7tech/service/WsdlProxyTest.java
@@ -0,0 +1,48 @@
+package com.l7tech.service;
+
+import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.HttpURLConnection;
+
+/**
+ * LAYER 7 TECHNOLOGIES, INC
+ *
+ * User: flascell
+ * Date: Sep 16, 2003
+ * Time: 2:11:32 PM
+ * $Id$
+ *
+ *
+ */
+public class WsdlProxyTest {
+    public static void main (String[] args) throws Exception {
+        System.setProperty("javax.net.ssl.trustStore", System.getProperties().getProperty("user.home") + File.separator + ".l7tech" + File.separator + "trustStore");
+        System.setProperty("javax.net.ssl.trustStorePassword", "password");
+
+        URL url = new URL("https://riker:8443/ssg/wsdl");
+        //https://riker:8443/ssg/wsdl
+        //https://riker:8443/ssg/wsdl?serviceoid=4980736
+        //https://riker:8443/ssg/wsdl?serviceoid=5373952
+        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setDoOutput(true);
+        // authentication for ssgadmin/ssgadminpasswd
+        connection.setRequestProperty("Authorization", "Basic c3NnYWRtaW46c3NnYWRtaW5wYXNzd2Q=");
+
+        int res  = connection.getResponseCode();
+        System.out.println("Response: " + res + "\n" + connection.getResponseMessage());
+
+        InputStream is = connection.getInputStream();
+        byte[] buf = new byte[2048];
+        int read = is.read(buf);
+        StringBuffer input = new StringBuffer();
+        while (read > 0) {
+
+            input.append(new String(buf, 0, read));
+            read = is.read(buf);
+        }
+        System.out.println(input.toString());
+
+    }
+}
