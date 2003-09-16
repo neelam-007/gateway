279036e9f0a65ed55cbde49e987e855adc288d50
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-09-16 22:34:55 +0000

    DONE
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@2791 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/server/BuildInfo.java
+++ src/com/l7tech/server/BuildInfo.java
@@ -11,25 +11,34 @@ package com.l7tech.server;
  * @version $Revision$
  */
 public class BuildInfo {
-    private static final String BUILD_NUMBER = "9";
-    private static final String PRODUCT_VERSION = "0.98";
-    private static final String PRODUCT_NAME = "ssg";
+    private static final String BUILD_NUMBER = "11";
+    private static final String PRODUCT_VERSION = "0.98b";
+    private static final String PRODUCT_NAME = "Layer7 SecureSpan Suite";
     private static final String BUILD_DATE = "20030916";
-    private static final String BUILD_TIME = "150820";
+    private static final String BUILD_TIME = "153238";
     private static final String BUILD_TAG = "$Name$";
-    private static final String BUILD_USER = "$Author$";
+    private static final String BUILD_USER = "alex";
+    private static final String BUILD_MACHINE = "locutus.l7tech.com";
+
+    public static String getLongBuildString() {
+        StringBuffer string = new StringBuffer();
+        string.append( getBuildString() );
+        string.append( " (" ).append( getBuildTag() ).append( ")" );
+        string.append( ", built " ).append( getBuildDate() ).append( getBuildTime() );
+        string.append( " by " ).append( getBuildUser() ).append(" at ").append( getBuildMachine() );
+        return string.toString();
+    }
 
     public static String getBuildString() {
         StringBuffer string = new StringBuffer();
-        string.append( PRODUCT_NAME ).append( " " );
-        string.append( "version " ).append( PRODUCT_VERSION ).append( ", " );
-        string.append( "build " ).append( BUILD_NUMBER ).append( ", " );
-        string.append( "built on " ).append( BUILD_DATE ).append( " at " );
-        string.append( BUILD_TIME ).append( ", tag " ).append( getBuildTag() );
+        string.append( getProductName() ).append( " " );
+        string.append( getProductVersion() );
+        string.append( " build " ).append( getBuildNumber() );
         return string.toString();
     }
 
     public static void main(String[] args) {
+        System.out.println( getLongBuildString() );
         System.out.println( getBuildString() );
     }
 
@@ -49,6 +58,14 @@ public class BuildInfo {
         return PRODUCT_NAME;
     }
 
+    public static String getBuildMachine() {
+        return BUILD_MACHINE;
+    }
+
+    public static String getBuildUser() {
+        return stripCvsCrap(BUILD_USER);
+    }
+
     public static String getBuildTime() {
         return BUILD_TIME;
     }
@@ -62,6 +79,8 @@ public class BuildInfo {
     private static String stripCvsCrap( String crappy ) {
         if ( crappy.startsWith("$") && crappy.endsWith("$") )
             crappy = crappy.substring( 1, crappy.length()-1 );
+        else
+            return crappy;
 
         int cpos = crappy.indexOf(":");
         if ( cpos >= 0 )
