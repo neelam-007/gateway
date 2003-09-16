7d266e9b018cf9b6497a22e13ddc26ac300d0d84
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-09-16 18:17:25 +0000

    loadTime property moved
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@2756 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/objectmodel/NamedEntity.java
+++ src/com/l7tech/objectmodel/NamedEntity.java
@@ -5,6 +5,4 @@ public interface NamedEntity extends Entity {
     void setName( String name );
     int getVersion();
     void setVersion( int version );
-    long getLoadTime();
-    void setLoadTime( long loadTime );
 }
