e6f8552b93613591db17c207c3443a3585b129f0
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-09-16 18:15:32 +0000

    Added checkUpdate(), moved getFieldQuery() here
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@2751 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/objectmodel/HibernateEntityManager.java
+++ src/com/l7tech/objectmodel/HibernateEntityManager.java
@@ -6,11 +6,14 @@
 
 package com.l7tech.objectmodel;
 
-import com.l7tech.objectmodel.EntityManager;
-import com.l7tech.objectmodel.HibernatePersistenceManager;
+import cirrus.hibernate.HibernateException;
+import cirrus.hibernate.Session;
+import com.l7tech.logging.LogManager;
 
-import java.util.*;
 import java.sql.SQLException;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author alex
@@ -18,6 +21,8 @@ import java.sql.SQLException;
  */
 public abstract class HibernateEntityManager implements EntityManager {
     public static final String EMPTY_STRING = "";
+    public static final String F_OID = "oid";
+    public static final String F_VERSION = "version";
 
     /**
      * Constructs a new <code>HibernateEntityManager</code>.
@@ -26,6 +31,59 @@ public abstract class HibernateEntityManager implements EntityManager {
         PersistenceManager manager = PersistenceManager.getInstance();
         if ( !(manager instanceof HibernatePersistenceManager ) ) throw new IllegalStateException( "Can't instantiate a " + getClass().getName() + "without first initializing a HibernatePersistenceManager!");
         _manager = manager;
+    }
+
+    /**
+     * Generates a Hibernate query string for retrieving a single field from a User.
+     * @param oid The objectId of the User to query
+     * @param getfield the (aliased) name of the field to return
+     * @return
+     */
+    protected String getFieldQuery( String oid, String getfield ) {
+        String alias = getTableName();
+        StringBuffer sqlBuffer = new StringBuffer( "SELECT " );
+        sqlBuffer.append( alias );
+        sqlBuffer.append( "." );
+        sqlBuffer.append( getfield );
+        sqlBuffer.append( " FROM " );
+        sqlBuffer.append( alias );
+        sqlBuffer.append( " in class " );
+        sqlBuffer.append( getImpClass().getName() );
+        sqlBuffer.append( " WHERE " );
+        sqlBuffer.append( alias );
+        sqlBuffer.append( "." );
+        sqlBuffer.append( F_OID );
+        sqlBuffer.append( " = :value:" );
+        return sqlBuffer.toString();
+    }
+
+    public void checkUpdate( NamedEntity ent ) throws UpdateException {
+        String stmt = getFieldQuery( new Long( ent.getOid() ).toString(), F_VERSION );
+
+        try {
+            HibernatePersistenceContext hpc = (HibernatePersistenceContext)PersistenceContext.getCurrent();
+            Session s = hpc.getSession();
+            List results = s.find( stmt );
+            if ( results.size() == 0 ) {
+                String err = "Object to be updated does not exist!";
+                _log.log( Level.WARNING, err );
+                throw new UpdateException( err );
+            }
+
+            int savedVersion = ((Integer)results.get(0)).intValue();
+
+            if ( savedVersion != ent.getVersion() ) {
+                String err = "Object to be updated is stale (a later version exists in the database)!";
+                _log.log( Level.WARNING, err );
+                throw new StaleUpdateException( err );
+            }
+        } catch (SQLException e) {
+            _log.log( Level.SEVERE, e.getMessage(), e );
+            throw new UpdateException( e.getMessage(), e );
+        } catch (HibernateException e) {
+            _log.log( Level.SEVERE, e.getMessage(), e );
+            throw new UpdateException( e.getMessage(), e );
+        }
     }
 
     public abstract Class getImpClass();
@@ -79,4 +137,5 @@ public abstract class HibernateEntityManager implements EntityManager {
     }
 
     protected PersistenceManager _manager;
+    protected Logger _log = LogManager.getInstance().getSystemLogger();
 }
