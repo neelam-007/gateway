e63509cd14d154c19a75a16497f8e53ff353fc52
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-09-17 18:29:33 +0000

    deployment and redeployment scripts using distribution files produced by build.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@2842 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ etc/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# -----------------------------------------------------------------------------
+# FILE [deploy.sh]
+# LAYER 7 TECHNOLOGIES
+# $Id$
+#
+# DEPLOYS THE SERVER USING DISTRIBUTION FILES PRODUCED BY BUILD
+#
+# PREREQUISITES
+#
+# -----------------------------------------------------------------------------
+#
+
+echo "todo"
---
+++ etc/redeploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# -----------------------------------------------------------------------------
+# FILE [redeploy.sh]
+# LAYER 7 TECHNOLOGIES
+# $Id$
+#
+# REDEPLOYS THE SERVER USING DISTRIBUTION FILES PRODUCED BY BUILD
+#
+# PREREQUISITES
+# 1. a successful deployment
+# -----------------------------------------------------------------------------
+#
+
+echo "todo"
