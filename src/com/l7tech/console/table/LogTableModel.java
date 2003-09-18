b1adb44e55ea91e22170bbca4d7d9a5049cd7bb3
fpang <fpang@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-09-18 18:51:21 +0000

    Created for logging feature
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@2907 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/console/table/LogTableModel.java
@@ -0,0 +1,22 @@
+package com.l7tech.console.table;
+
+import javax.swing.table.DefaultTableModel;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: fpang
+ * Date: Sep 18, 2003
+ * Time: 10:21:18 AM
+ * To change this template use Options | File Templates.
+ */
+public class LogTableModel extends DefaultTableModel {
+    public LogTableModel(String[][] data, String[] columnNames){
+        super(data, columnNames);
+    }
+
+    public boolean isCellEditable(int row, int col){
+        // the table cells are not editable
+        return false;
+    }
+
+}
