4b80784b01ddf53c3f845d6c384f1dfabc62519a
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-09-23 18:10:45 +0000

    added public static boolean isValidConfigObject(IdentityProviderConfig arg)
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@3020 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/identity/ldap/LdapConfigSettings.java
+++ src/com/l7tech/identity/ldap/LdapConfigSettings.java
@@ -1,5 +1,7 @@
 package com.l7tech.identity.ldap;
 
+import com.l7tech.identity.IdentityProviderConfig;
+
 /**
  * Layer 7 Technologies, inc.
  * User: flascelles
@@ -9,4 +11,16 @@ package com.l7tech.identity.ldap;
 public class LdapConfigSettings {
     public static final String LDAP_HOST_URL = "ldapHostURL";
     public static final String LDAP_SEARCH_BASE = "ldapSearchBase";
+
+    /**
+     * checks the format of an ldap id provider config.
+     * makes sure all the necessary elements are there
+     */
+    public static boolean isValidConfigObject(IdentityProviderConfig arg) {
+        String hostUrl = arg.getProperty(LDAP_HOST_URL);
+        String sbase = arg.getProperty(LDAP_SEARCH_BASE);
+        if (hostUrl == null || sbase == null) return false;
+        if (hostUrl.length() < 1 || sbase.length() < 1) return false;
+        return true;
+    }
 }
