e3a039704c9b21b3024519eabbd867558fa7630e
fpang <fpang@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-09-23 16:58:25 +0000

    Added an interface for retrieving logs by specifying start and end points.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@3012 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/logging/BasicLogManager.java
+++ src/com/l7tech/logging/BasicLogManager.java
@@ -18,4 +18,21 @@ public class BasicLogManager extends LogManager {
     public LogRecord[] getRecorded(int offset, int size) {
         return new LogRecord[0];
     }
+
+   /**
+     * Retrieve the system logs in between the startMsgNumber and endMsgNumber specified
+     * up to the specified size.
+     * NOTE: the log messages whose message number equals to startMsgNumber and endMsgNumber
+     * are not returned.
+     *
+     * @param startMsgNumber the message number to locate the start point.
+     *                       Start from beginning of the message buffer if it equals to -1.
+     * @param endMsgNumber   the message number to locate the end point.
+     *                       Retrieve messages until the end of the message buffer is hit if it equals to -1.
+     * @param size  the max. number of messages retrieved
+     * @return LogRecord[] the array of log records retrieved
+     */
+    public LogRecord[] getRecorded(long startMsgNumber, long endMsgNumber, int size) {
+        return new LogRecord[0];
+    }
 }
