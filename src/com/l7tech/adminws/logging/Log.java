b37e8910f8ae754cdd9107717cf7880a3373efb7
fpang <fpang@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-09-23 16:55:41 +0000

    Added interface for retrieving logs by specifying the start and end points.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@3010 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/adminws/logging/Log.java
+++ src/com/l7tech/adminws/logging/Log.java
@@ -13,5 +13,22 @@ import java.rmi.Remote;
  */
 public interface Log extends Remote {
     String[] getSystemLog(int offset, int size) throws RemoteException;
+
+    /**
+     * Retrieve the system logs in between the startMsgNumber and endMsgNumber specified
+     * up to the specified size.
+     * NOTE: the log messages whose message number equal to startMsgNumber and endMsgNumber
+     * are not returned.
+     *
+     * @param startMsgNumber the message number to locate the start point.
+     *                       Start from beginning of the message buffer if it equals to -1.
+     * @param endMsgNumber   the message number to locate the end point.
+     *                       Retrieve messages until the end of the message buffer is hit if it equals to -1.
+     * @param size  the max. number of messages retrieved
+     * @return String[] the array of messages retrieved
+     * @throws RemoteException
+     */
+    String[] getSystemLog(long startMsgNumber, long endMsgNumber, int size) throws RemoteException;
+
     UptimeMetrics getUptime() throws RemoteException;
 }
