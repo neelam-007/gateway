134616a7ce45812eff2616c10f96836f2a69c5a2
fpang <fpang@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-09-23 17:24:41 +0000

    Also store the message number in the LogMesssage class.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@3018 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/logging/LogMessage.java
+++ src/com/l7tech/logging/LogMessage.java
@@ -10,15 +10,17 @@ import java.util.StringTokenizer;
  * To change this template use Options | File Templates.
  */
 public class LogMessage {
-
+    private long msgNumber = 0;
     private String time = null;
     private String severity = null;
     private String msgClass = null;
     private String msgMethod = null;
-    private String msgDetail = null;
+    private String msgDetails = null;
 
     public LogMessage(String log){
 
+
+         System.out.println(log);
         StringTokenizer st = new StringTokenizer(log, "|");
         //System.out.println("number of fields is: " + fields.length);
 
@@ -26,34 +28,41 @@ public class LogMessage {
         while(st.hasMoreTokens()){
           String s = st.nextToken();
                if(index == 0){
-                   time = s;
+                   msgNumber = Long.parseLong(s);
                    index++;
                }
                else if (index == 1){
+                   time = s;
+                   index++;
+               }
+               else if (index == 2){
                    severity = s;
                    index++;
                }
-               else if (index == 2) {
+               else if (index == 3) {
                    msgClass = s;
                    index++;
                }
-                else if(index == 3){
+                else if(index == 4){
                     msgMethod = s;
                     index++;
                 }
-                else if(index == 4){
-                    msgDetail = s;
+                else if(index == 5){
+                    msgDetails = s;
                     index++;
                 }
                 else
                 {
-                    msgDetail = msgDetail + s;
+                    msgDetails = msgDetails + s;
                 }
 
           }
 
     }
 
+    public long getMsgNumber() {
+        return msgNumber;
+    }
     public String getTime() {
         return time;
     }
@@ -70,19 +79,19 @@ public class LogMessage {
         return msgMethod;
     }
 
-    public String getMessageDetail(){
-        return msgDetail;
+    public String getMessageDetails(){
+        return msgDetails;
     }
 
 
     public static void main(String[] args) {
-        LogMessage logRec = new LogMessage("20030917 13:07:36.281|INFO|com.l7tech.identity.internal.InternalIdentityProviderServer|authenticate|Couldn't find user with login admin");
-
+        LogMessage logRec = new LogMessage("11|20030917 13:07:36.281|INFO|com.l7tech.identity.internal.InternalIdentityProviderServer|authenticate|Couldn't find user with login admin");
+        System.out.println("msgNumber : " + logRec.getMsgNumber());
         System.out.println("time: " + logRec.getTime());
         System.out.println("severity: " + logRec.getSeverity());
         System.out.println("msgClass: " + logRec.getMessageClass());
         System.out.println("msgMethod: " + logRec.getMessageMethod());
-        System.out.println("msgDetail: " + logRec.getMessageDetail());
+        System.out.println("msgDetails: " + logRec.getMessageDetails());
 
     }
 }
