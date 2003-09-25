3435bc55b291462d6c7686f75cbaed60c3ddbb23
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-09-25 22:29:39 +0000

    Done
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@3085 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/common/RequestId.java
+++ src/com/l7tech/common/RequestId.java
@@ -12,7 +12,7 @@ package com.l7tech.common;
  * @author alex
  * @version $Revision$
  */
-public class RequestId {
+public class RequestId implements Comparable {
     public RequestId( int serverId, long bootTime, long sequence ) {
         _serverId = serverId;
         _bootTime = bootTime;
@@ -29,6 +29,51 @@ public class RequestId {
 
     public long getSequence() {
         return _sequence;
+    }
+
+    public int compareTo(Object o) {
+        RequestId other = (RequestId)o;
+        if ( _serverId > other._serverId ) return 1;
+        if ( _serverId < other._serverId ) return -1;
+        if ( _bootTime > other._bootTime ) return 1;
+        if ( _bootTime < other._bootTime ) return -1;
+        if ( _sequence > other._sequence ) return 1;
+        if ( _sequence < other._sequence ) return -1;
+        return 0;
+    }
+
+    public boolean equals( Object o ) {
+        RequestId other = (RequestId)o;
+        return ( _serverId == other._serverId &&
+                _bootTime == other._bootTime &&
+                _sequence == other._sequence );
+    }
+
+    public String toString() {
+        StringBuffer result = new StringBuffer();
+        result.append( paddedHex( _serverId ) );
+        result.append( "-" );
+        result.append( paddedHex( _bootTime ) );
+        result.append( "-" );
+        result.append( paddedHex( _sequence ) );
+        return result.toString();
+    }
+
+    private String paddedHex( long num ) {
+        return paddedHex( Long.toHexString( num ), 16 );
+    }
+
+    private String paddedHex( int num ) {
+        return paddedHex( Integer.toHexString( num ), 8 );
+    }
+
+    private String paddedHex( String num, int len ) {
+        StringBuffer result = new StringBuffer();
+        while ( result.length() < ( len - num.length() )) {
+            result.append("0");
+        }
+        result.append( num );
+        return result.toString();
     }
 
     private int _serverId;
