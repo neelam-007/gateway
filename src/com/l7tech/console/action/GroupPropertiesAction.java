f75b0eaf338a639a2d33d278b0db1083877a58ba
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-09-25 18:50:48 +0000

    prevent loading properties panel for ldap users
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@3076 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/console/action/GroupPropertiesAction.java
+++ src/com/l7tech/console/action/GroupPropertiesAction.java
@@ -9,8 +9,10 @@ import com.l7tech.console.util.Registry;
 import com.l7tech.objectmodel.EntityHeader;
 import com.l7tech.objectmodel.EntityType;
 import com.l7tech.common.gui.util.Utilities;
+import com.l7tech.identity.IdProvConfManagerServer;
 
 import javax.swing.*;
+import javax.swing.tree.TreeNode;
 
 /**
  * The <code>GroupPropertiesAction</code> edits the group entity.
@@ -55,6 +57,11 @@ public class GroupPropertiesAction extends NodeAction {
         SwingUtilities.invokeLater(
           new Runnable() {
             public void run() {
+                // fla note. make sure this user is internal. otherwise dont show panel
+                if (!isParentIdProviderInternal((EntityHeaderNode)node)) {
+                    JOptionPane.showMessageDialog(null, "This group is read-only.", "Read-only", JOptionPane.INFORMATION_MESSAGE);
+                    return;
+                }
                 GroupPanel panel = new GroupPanel();
                 JFrame f = Registry.getDefault().getComponentRegistry().getMainWindow();
                 EditorDialog dialog = new EditorDialog(f, panel);
@@ -65,5 +72,22 @@ public class GroupPropertiesAction extends NodeAction {
                 dialog.show();
             }
         });
+    }
+
+    private boolean isParentIdProviderInternal(EntityHeaderNode usernode) {
+        TreeNode parentNode = usernode.getParent();
+        while (parentNode != null) {
+            if (parentNode instanceof EntityHeaderNode) {
+                EntityHeader header = ((EntityHeaderNode)parentNode).getEntityHeader();
+                if (header.getType().equals(EntityType.ID_PROVIDER_CONFIG)) {
+                    // we found the parent, see if it's internal one
+                    if (header.getOid() != IdProvConfManagerServer.INTERNALPROVIDER_SPECIAL_OID) return false;
+                    return true;
+                }
+            }
+            parentNode = parentNode.getParent();
+        }
+        // assume it is unless proven otherwise
+        return true;
     }
 }
--- src/com/l7tech/console/action/UserPropertiesAction.java
+++ src/com/l7tech/console/action/UserPropertiesAction.java
@@ -10,8 +10,10 @@ import com.l7tech.console.tree.EntityHeaderNode;
 import com.l7tech.console.util.Registry;
 import com.l7tech.objectmodel.EntityHeader;
 import com.l7tech.objectmodel.EntityType;
+import com.l7tech.identity.IdProvConfManagerServer;
 
 import javax.swing.*;
+import javax.swing.tree.TreeNode;
 
 /**
  * The <code>UserPropertiesAction</code> edits the user entity.
@@ -56,15 +58,39 @@ public class UserPropertiesAction extends NodeAction {
         SwingUtilities.invokeLater(
           new Runnable() {
             public void run() {
+                // fla note. make sure this user is internal. otherwise dont show panel
+                if (!isParentIdProviderInternal((EntityHeaderNode)node)) {
+                    JOptionPane.showMessageDialog(null, "This user is read-only.", "Read-only", JOptionPane.INFORMATION_MESSAGE);
+                    return;
+                }
+                EntityHeader header = ((EntityHeaderNode)node).getEntityHeader();
                 UserPanel panel = new UserPanel();
                 JFrame f = Registry.getDefault().getComponentRegistry().getMainWindow();
                 EditorDialog dialog = new EditorDialog(f, panel);
 
-                panel.edit(((EntityHeaderNode)node).getEntityHeader());
+                panel.edit(header);
                 dialog.pack();
                 Utilities.centerOnScreen(dialog);
                 dialog.show();
             }
         });
     }
+
+    private boolean isParentIdProviderInternal(EntityHeaderNode usernode) {
+        TreeNode parentNode = usernode.getParent();
+        while (parentNode != null) {
+            if (parentNode instanceof EntityHeaderNode) {
+                EntityHeader header = ((EntityHeaderNode)parentNode).getEntityHeader();
+                if (header.getType().equals(EntityType.ID_PROVIDER_CONFIG)) {
+                    // we found the parent, see if it's internal one
+                    if (header.getOid() != IdProvConfManagerServer.INTERNALPROVIDER_SPECIAL_OID) return false;
+                    return true;
+                }
+            }
+            parentNode = parentNode.getParent();
+        }
+        // assume it is unless proven otherwise
+        return true;
+    }
+
 }
