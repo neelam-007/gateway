62c7487d454ef1dd4d239bf1eafae99ef79ff188
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-09-26 18:55:03 +0000

    protocol version update
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@3122 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/adminws/identity/Service.java
+++ src/com/l7tech/adminws/identity/Service.java
@@ -22,7 +22,7 @@ import java.security.cert.CertificateEncodingException;
  */
 public class Service implements IdentityService {
 
-    public static final String VERSION = "20030922";
+    public static final String VERSION = "20030926";
     public static final String SERVICE_DEPENDENT_URL_PORTION = "/services/identityAdmin";
 
     public Service() {
