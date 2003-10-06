313d12758babaa9343f41c3d1e492ff3163c7d57
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-10-06 20:54:15 +0000

    Remove unused exception
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@3292 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/proxy/policy/assertion/ClientRoutingAssertion.java
+++ src/com/l7tech/proxy/policy/assertion/ClientRoutingAssertion.java
@@ -8,7 +8,6 @@ package com.l7tech.proxy.policy.assertion;
 
 import com.l7tech.policy.assertion.RoutingAssertion;
 import com.l7tech.policy.assertion.AssertionStatus;
-import com.l7tech.policy.assertion.PolicyAssertionException;
 import com.l7tech.proxy.datamodel.PendingRequest;
 import com.l7tech.proxy.datamodel.SsgResponse;
 
