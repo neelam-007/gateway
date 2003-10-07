0b368cba835d94f2c75d9a61b858b8d61b7d4027
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-10-07 22:50:38 +0000

    fix an oops
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@3327 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/proxy/datamodel/Ssg.java
+++ src/com/l7tech/proxy/datamodel/Ssg.java
@@ -565,7 +565,7 @@ public class Ssg implements Serializable, Cloneable, Comparable {
      * Get the configured credentials for this Ssg, if any.
      * @return The configuration credentials, or null if there aren't any yet.
      */
-    public PasswordAuthentication fgetCredentials() {
+    public PasswordAuthentication getCredentials() {
         String username = getUsername();
         char[] password = cmPassword();
         if (username != null && username.length() > 0 && password != null)
