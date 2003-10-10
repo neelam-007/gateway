12b77c865f13156b19792bd80a5066d3ac19fb6b
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-10-10 01:13:47 +0000

    Added JAVA_OPTS for jini.server.hostname etc.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@3399 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- etc/ssgruntimedefs.sh
+++ etc/ssgruntimedefs.sh
@@ -6,13 +6,18 @@
 # Edit at deployment time to ensure values are accurate
 #
 
+export SSG_HOME=/ssg
+
 # define java home
 JAVA_HOME=/usr/java/j2sdk1.4.1
 export JAVA_HOME
 
 # add to path
-PATH=$PATH:$JAVA_HOME/bin
+PATH=$PATH:$JAVA_HOME/bin:$SSG_HOME/bin
 export PATH
+
+# Set Java system properties
+export JAVA_OPTS=-Djini.server.hostname=`hostname`
 
 # define tomcat home
 TOMCAT_HOME=/usr/java/tomcat-4.1.27-l7p2
