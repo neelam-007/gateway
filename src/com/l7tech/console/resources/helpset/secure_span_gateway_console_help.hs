16a7ee083f2a5a9132bdcaaac6e45961e23096fb
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-10-14 20:50:23 +0000

    New version from Kate
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@3439 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/console/resources/helpset/secure_span_gateway_console_help.hs
+++ src/com/l7tech/console/resources/helpset/secure_span_gateway_console_help.hs
@@ -1,7 +1,7 @@
 <?xml version='1.0' encoding='ISO-8859-1' ?>
 <!DOCTYPE helpset PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 1.0//EN" "http://java.sun.com/products/javahelp/helpset_1_0.dtd">
 
-<helpset version="2.0">
+<helpset version="1.0">
   <title>Secure Span Gateway Console Help</title>
    <maps>
     <homeID>welcome_htm</homeID>
@@ -21,19 +21,13 @@
     <type>javax.help.IndexView</type>
     <data>secure_span_gateway_console_help_ndx.xml</data>
   </view>
-    
-  <view>
-    <name>Favorites</name>
-    <label>Favorites</label>
-    <type>javax.help.FavoritesView</type>
-  </view>
-  
+
   <view>
     <name>Search</name>
     <label>Search</label>
     <type>javax.help.SearchView</type>
-    <data engine="com.sun.java.help.search.DefaultSearchEngine">
-    <!-- JavaHelpSearch -->
-    </data>
+    <data engine="com.sun.java.help.search.DefaultSearchEngine">secure_span_gateway_console_help_JavaHelpSearch</data>
   </view>
+
+
 </helpset>
