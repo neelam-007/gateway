746dc6af2a4d6f5c5098d84c3da04c50e4004566
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-10-17 19:23:06 +0000

    added ou objclass name and ou attr name made static methods static
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@3500 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/identity/ldap/AbstractLdapConstants.java
+++ src/com/l7tech/identity/ldap/AbstractLdapConstants.java
@@ -14,16 +14,18 @@ public abstract class AbstractLdapConstants {
     public String descriptionAttribute() {return DESCRIPTION_ATTR;}
 
     public abstract String groupObjectClass();
-    public String groupNameAttribute() { return NAME_ATTR_NAME;}
+    public static String groupNameAttribute() { return NAME_ATTR_NAME;}
     public abstract String groupMemberAttribute();
 
     public abstract String userObjectClass();
     public abstract String userLoginAttribute();
-    public String userNameAttribute() {return NAME_ATTR_NAME;}
-    public String userEmailAttribute() {return EMAIL_ATTR_NAME;}
-    public String userFirstnameAttribute() {return FIRSTNAME_ATTR_NAME;}
-    public String userLastnameAttribute() {return LASTNAME_ATTR_NAME;}
-    public String userPasswordAttribute() {return PASSWD_ATTR_NAME;}
+    public static String userNameAttribute() {return NAME_ATTR_NAME;}
+    public static String userEmailAttribute() {return EMAIL_ATTR_NAME;}
+    public static String userFirstnameAttribute() {return FIRSTNAME_ATTR_NAME;}
+    public static String userLastnameAttribute() {return LASTNAME_ATTR_NAME;}
+    public static String userPasswordAttribute() {return PASSWD_ATTR_NAME;}
+    public static String oUObjClassName() {return OU_OBJCLASS_NAME;}
+    public static String oUObjAttrName() {return OU_ATTR_NAME;}
 
     protected static final String NAME_ATTR_NAME = "cn";
     protected static final String DESCRIPTION_ATTR = "description";
@@ -31,5 +33,6 @@ public abstract class AbstractLdapConstants {
     protected static final String EMAIL_ATTR_NAME = "mail";
     protected static final String FIRSTNAME_ATTR_NAME = "givenName";
     protected static final String PASSWD_ATTR_NAME = "userPassword";
-
+    protected static final String OU_OBJCLASS_NAME = "organizationalUnit";
+    protected static final String OU_ATTR_NAME = "ou";
 }
