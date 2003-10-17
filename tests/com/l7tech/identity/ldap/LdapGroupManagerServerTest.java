7edd145067254dcbb7bcfade50c9358d6c471cbf
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-10-17 20:27:38 +0000

    test group memberships
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@3506 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- tests/com/l7tech/identity/ldap/LdapGroupManagerServerTest.java
+++ tests/com/l7tech/identity/ldap/LdapGroupManagerServerTest.java
@@ -7,6 +7,8 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 import com.l7tech.identity.IdentityProviderConfig;
 import com.l7tech.identity.Group;
+import com.l7tech.identity.GroupManager;
+import com.l7tech.identity.User;
 
 /**
  * Layer 7 Technologies, inc.
@@ -24,17 +26,29 @@ public class LdapGroupManagerServerTest extends TestCase {
     public void testFindAll() throws Exception {
         IdentityProviderConfig config = new IdentityProviderConfig();
         // use this url when ssh forwarding locally
-        config.putProperty(LdapConfigSettings.LDAP_HOST_URL, "ldap://localhost:3899");
+        // config.putProperty(LdapConfigSettings.LDAP_HOST_URL, "ldap://localhost:3899");
+        config.putProperty(LdapConfigSettings.LDAP_HOST_URL, "ldap://spock:389");
         // use this url when in the office
-        //config.setLdapHostURL("ldap://spock:389");
         config.putProperty(LdapConfigSettings.LDAP_SEARCH_BASE, "dc=layer7-tech,dc=com");
-        LdapGroupManagerServer me = new LdapGroupManagerServer(config);
+        LdapIdentityProviderServer provider = new LdapIdentityProviderServer();
+        provider.initialize(config);
+        GroupManager me = provider.getGroupManager();
         Collection res = me.findAll();
         Iterator i = res.iterator();
         while (i.hasNext()) {
             Group group = (Group)i.next();
-            System.out.println(group);
+            System.out.println(printGroup(group));
         }
+    }
+
+    private String printGroup(Group grp) {
+        String out = "Group: " + grp.toString();
+        Iterator i = grp.getMembers().iterator();
+        while (i.hasNext()) {
+            User u = (User)i.next();
+            out += "\n\n\tmember:" + u.toString();
+        }
+        return out;
     }
 
     public static void main(String[] args) throws Exception {
