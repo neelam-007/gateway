e6ca46f1a172e8c1638bdbdf00b00fc1021d3daf
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-10-21 20:32:19 +0000

    Moved some stuff down to specific versions, removed static methods
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@3542 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/identity/ldap/AbstractLdapConstants.java
+++ src/com/l7tech/identity/ldap/AbstractLdapConstants.java
@@ -14,20 +14,19 @@ public abstract class AbstractLdapConstants {
     public String descriptionAttribute() {return DESCRIPTION_ATTR;}
 
     public abstract String groupObjectClass();
-    public static String groupNameAttribute() { return NAME_ATTR_NAME;}
+    public abstract String groupNameAttribute();
     public abstract String groupMemberAttribute();
 
     public abstract String userObjectClass();
     public abstract String userLoginAttribute();
-    public static String userNameAttribute() {return NAME_ATTR_NAME;}
-    public static String userEmailAttribute() {return EMAIL_ATTR_NAME;}
-    public static String userFirstnameAttribute() {return FIRSTNAME_ATTR_NAME;}
-    public static String userLastnameAttribute() {return LASTNAME_ATTR_NAME;}
-    public static String userPasswordAttribute() {return PASSWD_ATTR_NAME;}
-    public static String oUObjClassName() {return OU_OBJCLASS_NAME;}
-    public static String oUObjAttrName() {return OU_ATTR_NAME;}
+    public abstract String userNameAttribute();
+    public String userEmailAttribute() {return EMAIL_ATTR_NAME;}
+    public String userFirstnameAttribute() {return FIRSTNAME_ATTR_NAME;}
+    public String userLastnameAttribute() {return LASTNAME_ATTR_NAME;}
+    public String userPasswordAttribute() {return PASSWD_ATTR_NAME;}
+    public String oUObjClassName() {return OU_OBJCLASS_NAME;}
+    public String oUObjAttrName() {return OU_ATTR_NAME;}
 
-    protected static final String NAME_ATTR_NAME = "cn";
     protected static final String DESCRIPTION_ATTR = "description";
     protected static final String LASTNAME_ATTR_NAME = "sn";
     protected static final String EMAIL_ATTR_NAME = "mail";
