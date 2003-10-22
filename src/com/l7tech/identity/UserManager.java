a4915835e7619f76015a5873c6a25fae8415232a
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-10-22 23:52:04 +0000

    Remove redundant "public" modifier from interface
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@3576 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/identity/UserManager.java
+++ src/com/l7tech/identity/UserManager.java
@@ -6,11 +6,12 @@ import com.l7tech.objectmodel.*;
  * @author alex
  */
 public interface UserManager extends EntityManager {
-    public User findByPrimaryKey( String oid ) throws FindException;
-    public User findByLogin( String login ) throws FindException;
-    public void delete( User user ) throws DeleteException;
-    public long save( User user ) throws SaveException;
-    public void update( User user ) throws UpdateException;
-    public EntityHeader userToHeader(User user);
-    public User headerToUser(EntityHeader header);
+    User findByPrimaryKey( String oid ) throws FindException;
+    User findByLogin( String login ) throws FindException;
+    void delete( User user ) throws DeleteException;
+    long save( User user ) throws SaveException;
+    void update( User user ) throws UpdateException;
+
+    EntityHeader userToHeader(User user);
+    User headerToUser(EntityHeader header);
 }
