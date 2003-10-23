f3ca23294a6374c8ac1658d3df5454b8faf53e12
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-10-23 20:24:29 +0000

    Partial hibernate implementation of the ClientCertManager
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@3595 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/identity/cert/CertEntryRow.java
@@ -0,0 +1,51 @@
+package com.l7tech.identity.cert;
+
+/**
+ * LAYER 7 TECHNOLOGIES, INC
+ *
+ * User: flascell
+ * Date: Oct 23, 2003
+ * Time: 11:04:50 AM
+ * $Id$
+ *
+ * This bean class is meant to be used by hibernate only.
+ * (because hibernate needs to map a class type to a table)
+ */
+public class CertEntryRow {
+    public long getProviderId() {
+        return providerId;
+    }
+
+    public void setProviderId(long providerId) {
+        this.providerId = providerId;
+    }
+
+    public String getUserLogin() {
+        return userLogin;
+    }
+
+    public void setUserLogin(String userLogin) {
+        this.userLogin = userLogin;
+    }
+
+    public String getCert() {
+        return cert;
+    }
+
+    public void setCert(String cert) {
+        this.cert = cert;
+    }
+
+    public long getResetCounter() {
+        return resetCounter;
+    }
+
+    public void setResetCounter(long resetCounter) {
+        this.resetCounter = resetCounter;
+    }
+
+    private long providerId;
+    private String userLogin;
+    private String cert;
+    private long resetCounter;
+}
