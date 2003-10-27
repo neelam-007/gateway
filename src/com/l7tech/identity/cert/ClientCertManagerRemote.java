9bcd95b183dfdeeee1ef8f24c67cb0a58f1b1258
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-10-27 18:45:57 +0000

    client side impl for ClientCertManager
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@3643 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/identity/cert/ClientCertManagerRemote.java
@@ -0,0 +1,75 @@
+package com.l7tech.identity.cert;
+
+import com.l7tech.identity.User;
+import com.l7tech.identity.IdentityAdmin;
+import com.l7tech.objectmodel.UpdateException;
+import com.l7tech.objectmodel.FindException;
+import com.l7tech.common.util.Locator;
+
+import java.security.cert.Certificate;
+import java.security.cert.CertificateEncodingException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.CertificateException;
+import java.rmi.RemoteException;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+/**
+ * LAYER 7 TECHNOLOGIES, INC
+ *
+ * User: flascell
+ * Date: Oct 27, 2003
+ * Time: 10:32:13 AM
+ * $Id$
+ *
+ * Client side implementation of the ClientCertManager - the internal CA
+ */
+public class ClientCertManagerRemote implements ClientCertManager {
+
+    public Certificate getUserCert(User user) throws FindException {
+        try {
+            String certstr = getStub().getUserCert(user);
+            if (certstr == null) return null;
+            sun.misc.BASE64Decoder base64decoder = new sun.misc.BASE64Decoder();
+            byte[] certbytes = base64decoder.decodeBuffer(certstr);
+            return CertificateFactory.getInstance("X.509").
+                    generateCertificate(new ByteArrayInputStream(certbytes));
+        } catch (RemoteException e) {
+            throw new FindException("could not resolve remote interface", e);
+        } catch (CertificateEncodingException e) {
+            throw new FindException("could not encode cert", e);
+        } catch (IOException e) {
+            throw new FindException("could not decode cert", e);
+        } catch (CertificateException e) {
+            throw new FindException("could not generate cert", e);
+        }
+    }
+
+    public void revokeUserCert(User user) throws UpdateException {
+        try {
+            getStub().revokeCert(user);
+        } catch (RemoteException e) {
+            throw new UpdateException("could not resolve remote interface", e);
+        }
+    }
+
+    public void rememberCertWasUsedSuccessfully(User user) throws UpdateException {
+        throw new RuntimeException("not implemented in stub - this should not be invoked on client side");
+    }
+
+    public boolean userCanGenCert(User user) {
+        throw new RuntimeException("not implemented in stub - this should not be invoked on client side");
+    }
+
+    public void recordNewUserCert(User user, Certificate cert) throws UpdateException {
+        throw new RuntimeException("not implemented in stub - this should not be invoked on client side");
+    }
+
+    protected IdentityAdmin getStub() throws RemoteException {
+        IdentityAdmin svc = (IdentityAdmin)Locator.getDefault().lookup(IdentityAdmin.class);
+        if (svc == null) {
+            throw new RemoteException("Cannot obtain the identity service");
+        }
+        return svc;
+    }
+}
