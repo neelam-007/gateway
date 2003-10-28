77b0e1af4f89e1034df9f61e4c24d4841efadd90
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-10-28 01:00:35 +0000

    added flush, a little documentation
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@3667 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/objectmodel/PersistenceContext.java
+++ src/com/l7tech/objectmodel/PersistenceContext.java
@@ -3,9 +3,21 @@ package com.l7tech.objectmodel;
 import java.sql.SQLException;
 
 /**
+ * Manages transactions and sessions.
+ *
  * @author alex
  */
 public abstract class PersistenceContext {
+    /**
+     * commits currently open session
+     * must be followed by a call to close() to release connection
+     * @throws ObjectModelException
+     */
+    public abstract void flush() throws ObjectModelException;
+
+    /**
+     * rollbacks existing transaction if exists and closes the connection
+     */
     public abstract void close();
     public abstract void beginTransaction() throws TransactionException;
     public abstract void commitTransaction() throws TransactionException;
