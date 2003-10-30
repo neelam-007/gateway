c89c6039aa01d591dd269ca22f431f2b7d73d00a
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-10-30 02:23:53 +0000

    Initial checkin.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@3707 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/policy/assertion/RoutingStatus.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2003 Layer 7 Technologies Inc.
+ *
+ * $Id$
+ */
+
+package com.l7tech.policy.assertion;
+
+/**
+ * @author alex
+ * @version $Revision$
+ */
+public class RoutingStatus {
+    public static final RoutingStatus UNKNOWN = new RoutingStatus( -1, "None" );
+    public static final RoutingStatus ATTEMPTED = new RoutingStatus( 0, "Attempted" );
+    public static final RoutingStatus ROUTED = new RoutingStatus( 1, "Routed" );
+
+    private RoutingStatus( int num, String name ) {
+        _num = num;
+        _name = name;
+    }
+
+    public String toString() {
+        return "<RoutingStatus num='" + _num + "' name='" + _name + "'/>";
+    }
+
+    private int _num;
+    private String _name;
+}
