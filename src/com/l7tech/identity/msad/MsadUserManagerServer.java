0b7e278c96be2556299dc1097bea22032c0bc04c
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-10-30 03:09:17 +0000

    Moved groupMembershipFilter to GroupManager
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@3744 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/identity/msad/MsadUserManagerServer.java
+++ src/com/l7tech/identity/msad/MsadUserManagerServer.java
@@ -8,10 +8,8 @@ package com.l7tech.identity.msad;
 
 import com.l7tech.identity.IdentityProviderConfig;
 import com.l7tech.identity.UserManager;
-import com.l7tech.identity.User;
 import com.l7tech.identity.ldap.AbstractLdapConstants;
 import com.l7tech.identity.ldap.AbstractLdapUserManagerServer;
-import com.l7tech.identity.ldap.LdapConfigSettings;
 
 /**
  * @author alex
@@ -24,18 +22,6 @@ public class MsadUserManagerServer extends AbstractLdapUserManagerServer impleme
 
     protected AbstractLdapConstants getConstants() {
         return _constants;
-    }
-
-    protected String doGetGroupMembershipFilter(User user) {
-        StringBuffer filter = new StringBuffer();
-        filter.append( "(&(objectClass=" );
-        filter.append( _constants.groupObjectClass() );
-        filter.append( ")(" );
-        filter.append( _constants.groupMemberAttribute() );
-        filter.append( "=" );
-        filter.append( user.getName() );
-        filter.append( "))" );
-        return filter.toString();
     }
 
     private MsadConstants _constants = new MsadConstants();
