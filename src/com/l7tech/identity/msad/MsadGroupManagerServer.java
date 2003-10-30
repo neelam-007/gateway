53e5df88572b86fff49f2d451a7d00302adf2fb5
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-10-30 03:09:00 +0000

    Updated groupMembershipFilter
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@3743 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/identity/msad/MsadGroupManagerServer.java
+++ src/com/l7tech/identity/msad/MsadGroupManagerServer.java
@@ -9,6 +9,7 @@ package com.l7tech.identity.msad;
 import com.l7tech.identity.*;
 import com.l7tech.identity.ldap.AbstractLdapConstants;
 import com.l7tech.identity.ldap.AbstractLdapGroupManagerServer;
+import com.l7tech.identity.ldap.LdapUser;
 import com.l7tech.objectmodel.FindException;
 
 
@@ -21,26 +22,17 @@ public class MsadGroupManagerServer extends AbstractLdapGroupManagerServer imple
         super( config );
     }
 
-    /*
-    alex, is this dead code?
-    protected String groupMemberToLogin( String member ) {
-        StringTokenizer stok = new StringTokenizer( member, "," );
-        String token;
-        String attr = _constants.groupMemberAttribute();
-        String name, value;
-        while ( stok.hasMoreTokens() ) {
-            token = stok.nextToken();
-            int epos = token.indexOf("=");
-            if ( epos >= 0 ) {
-                name = token.substring(0,epos);
-                value = token.substring(epos+1);
-
-                if ( name.equalsIgnoreCase( attr ) ) return value;
-            }
-        }
-
-        return "";
-    }*/
+    protected String doGetGroupMembershipFilter( LdapUser user) {
+        StringBuffer filter = new StringBuffer();
+        filter.append( "(&(objectClass=" );
+        filter.append( _constants.groupObjectClass() );
+        filter.append( ")(" );
+        filter.append( _constants.groupMemberAttribute() );
+        filter.append( "=" );
+        filter.append( user.getDn() );
+        filter.append( "))" );
+        return filter.toString();
+    }
 
     protected AbstractLdapConstants getConstants() {
         return _constants;
