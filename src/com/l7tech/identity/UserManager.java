c15494164c6998a8c6c092d884a869021e33fb81
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-10-30 02:33:49 +0000

    Delete save( UserBean ) & update( UserBean ), UserBean implements User.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@3720 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/identity/UserManager.java
+++ src/com/l7tech/identity/UserManager.java
@@ -11,9 +11,7 @@ public interface UserManager extends EntityManager {
     void delete( User user ) throws DeleteException;
     void delete( String identifier ) throws DeleteException;
     String save( User user ) throws SaveException;
-    String save( UserBean user ) throws SaveException;
     void update( User user ) throws UpdateException;
-    void update( UserBean user ) throws UpdateException;
 
     EntityHeader userToHeader(User user);
     User headerToUser(EntityHeader header);
