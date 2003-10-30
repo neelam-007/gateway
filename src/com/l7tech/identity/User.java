b2f8b3945d6c9c364e6e215ffe161057ea9a08c6
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-10-30 02:30:39 +0000

    The Great Refactoring begins...
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@3717 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/identity/User.java
+++ src/com/l7tech/identity/User.java
@@ -1,221 +1,42 @@
 package com.l7tech.identity;
 
-import com.l7tech.objectmodel.imp.NamedEntityImp;
-import com.l7tech.policy.assertion.credential.http.HttpDigest;
-
 import java.security.Principal;
-import java.util.*;
+import java.util.Set;
 
 /**
  * Layer 7 Technologies, inc.
  * User: flascelles
  * Date: Jun 24, 2003
  *
- * User either from the internal identity provider or a ldap directory.
- * In the case of ldap, the name property contains the dn.
+ * Read-only interface for a User either from the internal identity provider or a ldap directory.
+ * In the case of ldap, the uniqueIdentifier property contains the dn.
+ * In the case of internal, it's a String representation of the oid.
+ *
  * Password property is stored as HEX(MD5(login:L7SSGDigestRealm:password)). If you pass a clear text passwd in
  * setPassword, this encoding will be done ofr you (provided that login was set before).
  */
-public class User extends NamedEntityImp implements Principal {
-
-    public String getLogin() {
-        return _login;
-    }
-
-    public String getPassword() {
-        return _password;
-    }
-
-    public String getFirstName() {
-        return _firstName;
-    }
-
-    public String getLastName() {
-        return _lastName;
-    }
-
-    public String getEmail() {
-        return _email;
-    }
-
-    public String getTitle() {
-        return _title;
-    }
-
-    public String getDepartment() {
-        return _department;
-    }
-
-    public Set getGroups() {
-        return _groups;
-    }
-
-    public Set getGroupHeaders() {
-        return _groupHeaders;
-    }
-
-    public void setGroups( Set groups ) {
-        _groups = groups;
-    }
-
-    public void setGroupHeaders( Set groupHeaders ) {
-        _groupHeaders = groupHeaders;
-    }
-
-    public void setLogin(String login) {
-        _login = login;
-    }
-
+public interface User extends Principal {
     /**
-     * set the login before setting the password.
-     * if the password is not encoded, this will encode it.
+     * This method should return an identifier for this User that is unique within its IdentityProvider.
+     * For internal users, this can be a String representation of the OID.
+     * For LDAP, it should be the DN.
      */
-    public void setPassword(String password) throws  IllegalStateException {
-        if ( password != null && !isAlreadyEncoded(password)) {
-            if (_login == null) throw new IllegalStateException("login must be set prior to encoding the password");
-            _password = encodePasswd(_login, password);
-        }
-        else _password = password;
-    }
-
-    public void setFirstName(String firstName) {
-        _firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        _lastName = lastName;
-    }
-
-    public void setEmail(String email) {
-        _email = email;
-    }
-
-    public void setTitle(String title) {
-        _title = title;
-    }
-
-    public void setDepartment(String department) {
-        _department = department;
-    }
-
+    String getUniqueIdentifier();
     /**
      * this is not persisted, it is set at run time by the provider who creates the object
      */
-    public long getProviderId() {
-        return providerId;
-    }
+    long getProviderId();
+    void setProviderId(long providerId);
+    String getLogin();
+    String getPassword();
+    String getFirstName();
+    String getLastName();
+    String getEmail();
+    String getTitle();
+    String getDepartment();
 
-    /**
-     * this is not persisted, it is set at run time by the provider who creates the object
-     */
-    public void setProviderId(long providerId) {
-        this.providerId = providerId;
-    }
+    Set getGroups();
+    Set getGroupHeaders();
 
-    public String toString() {
-        return "com.l7tech.identity.User." +
-                "\n\tName=" + _name +
-                "\n\tFirst name=" + _firstName +
-                "\n\tLast name=" + _lastName +
-                "\n\tLogin=" + _login +
-                "\n\tPassword=" + _password +
-                "\n\tproviderId=" + providerId;
-    }
-
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof User)) return false;
-        final User userImp = (User) o;
-        if ( providerId != DEFAULT_OID ? !( providerId== userImp.providerId ) : userImp.providerId != DEFAULT_OID ) return false;
-        if (_login != null ? !_login.equals(userImp._login) : userImp._login != null) return false;
-        return true;
-    }
-
-    public int hashCode() {
-        if ( _oid > 0 ) return (int)_oid;
-        if ( _login == null ) return System.identityHashCode(this);
-
-        int hash = _login.hashCode();
-        hash += 29 * (int)providerId;
-        return hash;
-    }
-
-    private static String encodePasswd( String a1 ) {
-        // MD5 IT
-        java.security.MessageDigest md5Helper = null;
-        try {
-            md5Helper = java.security.MessageDigest.getInstance("MD5");
-        } catch (java.security.NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
-        byte[] digest = md5Helper.digest( a1.getBytes() );
-        // ENCODE IT
-        if (digest == null) return "";
-        char[] hexadecimal = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
-        if (digest.length != 16) return "";
-        char[] buffer = new char[32];
-
-        for (int i = 0; i < 16; i++) {
-            int low = (digest[i] & 0x0f);
-            int high = ((digest[i] & 0xf0) >> 4);
-            buffer[i*2] = hexadecimal[high];
-            buffer[i*2 + 1] = hexadecimal[low];
-        }
-        return new String(buffer);
-    }
-
-    public static String encodePasswd(String login, String passwd) {
-        String toEncode = login + ":" + HttpDigest.REALM + ":" + passwd;
-        return encodePasswd( toEncode );
-    }
-
-    public static String encodePasswd( String login, String passwd, String realm ) {
-        String toEncode = login + ":" + realm + ":" + passwd;
-        return encodePasswd( toEncode );
-    }
-
-    /**
-     * allows to set all properties from another object
-     */
-    public void copyFrom(User objToCopy) {
-        setOid(objToCopy.getOid());
-        setName(objToCopy.getName());
-        setProviderId(objToCopy.getProviderId());
-        setLogin(objToCopy.getLogin());
-        setDepartment(objToCopy.getDepartment());
-        setEmail(objToCopy.getEmail());
-        setFirstName(objToCopy.getFirstName());
-        setGroups(objToCopy.getGroups());
-        setLastName(objToCopy.getLastName());
-        setTitle(objToCopy.getTitle());
-        _password = objToCopy.getPassword();
-    }
-
-    // ************************************************
-    // PRIVATES
-    // ************************************************
-
-    private boolean isAlreadyEncoded(String arg) {
-        if (arg == null || arg.length() != 32) return false;
-        String hexmembers = "0123456789abcdef";
-        for (int i = 0; i < arg.length(); i++) {
-            char toto = arg.charAt(i);
-            if (hexmembers.indexOf(toto) == -1) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private String _login;
-    private String _password;
-    private String _firstName;
-    private String _lastName;
-    private String _email;
-    private String _title;
-    private String _department;
-
-    private Set _groups = new HashSet();
-    private Set _groupHeaders = new HashSet();
-    private long providerId = IdProvConfManagerServer.INTERNALPROVIDER_SPECIAL_OID;
+    UserBean getUserBean();
 }
