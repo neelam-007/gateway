68fc0ef3ad9eec2d1ab223b433d11cd55055f245
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-10-30 02:24:52 +0000

    Get parsers from MessageProcessor
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@3709 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/message/XmlMessageAdapter.java
+++ src/com/l7tech/message/XmlMessageAdapter.java
@@ -6,14 +6,20 @@
 
 package com.l7tech.message;
 
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
-import org.xml.sax.InputSource;
-import org.apache.xml.serialize.XMLSerializer;
+import com.l7tech.server.MessageProcessor;
 import org.apache.xml.serialize.OutputFormat;
+import org.apache.xml.serialize.XMLSerializer;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
 
-import javax.xml.parsers.*;
-import java.io.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
 
 /**
  * @author alex
@@ -22,17 +28,22 @@ import java.io.*;
 public abstract class XmlMessageAdapter extends MessageAdapter implements XmlMessage {
     public XmlMessageAdapter( TransportMetadata tm ) {
         super(tm);
-        _dbf.setNamespaceAware(true);
     }
 
     synchronized void parse( String xml ) throws SAXException, IOException {
         try {
             // TODO: Ensure this is a lazy parser
-            DocumentBuilder parser = _dbf.newDocumentBuilder();
+            DocumentBuilder parser = MessageProcessor.getInstance().getDomParser();
             _document = parser.parse( new InputSource( new StringReader( xml ) ) );
         } catch ( ParserConfigurationException pce ) {
             throw new SAXException( pce );
         }
+    }
+
+    public synchronized XmlPullParser pullParser( String xml ) throws XmlPullParserException {
+        XmlPullParser xpp = MessageProcessor.getInstance().getPullParser();
+        xpp.setInput( new StringReader( xml ) );
+        return xpp;
     }
 
     protected String serializeDoc(Document doc) throws IOException {
@@ -47,5 +58,6 @@ public abstract class XmlMessageAdapter extends MessageAdapter implements XmlMes
     }
 
     protected Document _document;
-    protected DocumentBuilderFactory _dbf = DocumentBuilderFactory.newInstance();
+
+
 }
