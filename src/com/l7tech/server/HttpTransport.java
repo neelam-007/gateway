fa73aefe5d1253a351a67f241190dfd6c3086c2e
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-11-26 22:58:27 +0000

    Quick n' dirty
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@4004 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/server/HttpTransport.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2003 Layer 7 Technologies Inc.
+ *
+ * $Id$
+ */
+
+package com.l7tech.server;
+
+import com.l7tech.message.TransportProtocol;
+
+import java.net.InetAddress;
+
+/**
+ * @author alex
+ * @version $Revision$
+ */
+public class HttpTransport implements Transport {
+
+    public HttpTransport() {
+    }
+
+    public static class IpProtocolPort {
+        public IpProtocolPort( InetAddress ip, TransportProtocol protocol, int port ) {
+            _ip = ip;
+            _protocol = protocol;
+            _port = port;
+        }
+
+        public TransportProtocol getProtocol() {
+            return _protocol;
+        }
+
+        public int getPort() {
+            return _port;
+        }
+
+        public InetAddress getIp() {
+            return _ip;
+        }
+
+        private InetAddress _ip;
+        private TransportProtocol _protocol;
+        private int _port;
+    }
+}
---
+++ src/com/l7tech/server/Transport.java
@@ -0,0 +1,8 @@
+package com.l7tech.server;
+
+/**
+ * @author alex
+ * @version $Revision$
+ */
+public interface Transport {
+}
