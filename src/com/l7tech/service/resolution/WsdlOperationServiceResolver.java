88734e8e8931ad69bc3bc198c406583dff4bb67b
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-11-27 23:23:08 +0000

    Added Wsdl.getBindingOperations()
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@4029 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/service/resolution/WsdlOperationServiceResolver.java
+++ src/com/l7tech/service/resolution/WsdlOperationServiceResolver.java
@@ -25,18 +25,12 @@ public abstract class WsdlOperationServiceResolver extends NameValueServiceResol
 
         try {
             Wsdl wsdl = service.parsedWsdl();
-            Iterator bindings = wsdl.getBindings().iterator();
-            Binding binding;
-            Iterator operations;
-            while ( bindings.hasNext() ) {
-                binding = (Binding)bindings.next();
-                operations = binding.getBindingOperations().iterator();
-                BindingOperation operation;
-                while ( operations.hasNext() ) {
-                    operation = (BindingOperation)operations.next();
-                    String value = getTargetValue( wsdl.getDefinition(), operation );
-                    if ( value != null ) values.add( value );
-                }
+            Iterator operations = wsdl.getBindingOperations().iterator();
+            BindingOperation operation;
+            while ( operations.hasNext() ) {
+                operation = (BindingOperation)operations.next();
+                String value = getTargetValue( wsdl.getDefinition(), operation );
+                if ( value != null ) values.add( value );
             }
         } catch ( WSDLException we ) {
             logger.log(Level.SEVERE, null, we);
