d54e5f12557b005dbbc2283749c3f164a3527769
emil <emil@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-11-28 00:33:56 +0000

    Initial revision
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@4044 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/console/action/SamlPropertiesAction.java
@@ -0,0 +1,81 @@
+package com.l7tech.console.action;
+
+import com.l7tech.console.tree.SslTransportNode;
+import com.l7tech.console.tree.policy.SslAssertionTreeNode;
+import com.l7tech.console.tree.policy.SamlTreeNode;
+import com.l7tech.console.util.ComponentRegistry;
+import com.l7tech.console.util.Registry;
+import com.l7tech.console.panels.SamlPropertiesDialog;
+import com.l7tech.policy.assertion.SslAssertion;
+import com.l7tech.common.gui.util.Utilities;
+
+import javax.swing.*;
+import javax.swing.tree.DefaultTreeModel;
+import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.awt.*;
+
+/**
+ * The <code>SamlPropertiesAction</code> edits the SAML assertion
+ * properties.
+ * 
+ * @author <a href="mailto:emarceta@layer7-tech.com">Emil Marceta</a>
+ * @version 1.0
+ */
+public class SamlPropertiesAction extends BaseAction {
+    static final Logger log = Logger.getLogger(SamlPropertiesAction.class.getName());
+    SamlTreeNode assertion;
+
+    public SamlPropertiesAction(SamlTreeNode node) {
+        assertion = node;
+    }
+
+    /**
+     * @return the action name
+     */
+    public String getName() {
+        return "SAML properties";
+    }
+
+    /**
+     * @return the aciton description
+     */
+    public String getDescription() {
+        return "View/edit SAML properties";
+    }
+
+    /**
+     * specify the resource name for this action
+     */
+    protected String iconResource() {
+        return "com/l7tech/console/resources/Properties16.gif";
+    }
+
+    /**
+     * Actually perform the action.
+     * This is the method which should be called programmatically.
+     * <p/>
+     * note on threading usage: do not access GUI components
+     * without explicitly asking for the AWT event thread!
+     */
+    public void performAction() {
+        Frame f = Registry.getDefault().getComponentRegistry().getMainWindow();
+        SamlPropertiesDialog pw = new SamlPropertiesDialog(f, assertion);
+        pw.pack();
+        Utilities.centerOnScreen(pw);
+        pw.show();
+        assertionChanged();
+    }
+
+    public void assertionChanged() {
+        JTree tree =
+          (JTree)ComponentRegistry.getInstance().getPolicyTree();
+        if (tree != null) {
+            DefaultTreeModel model = (DefaultTreeModel)tree.getModel();
+            model.nodeChanged(assertion);
+        } else {
+            log.log(Level.WARNING, "Unable to reach the palette tree.");
+        }
+    }
+}
---
+++ src/com/l7tech/console/panels/SamlPropertiesDialog.java
@@ -0,0 +1,136 @@
+package com.l7tech.console.panels;
+
+import com.l7tech.console.tree.policy.SamlTreeNode;
+import com.l7tech.policy.assertion.xmlsec.SamlSecurity;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+/**
+ * Class <code>SamlPropertiesPanel</code> edits the SAML properties.
+ * 
+ * @author <a href="mailto:emarceta@layer7-tech.com">Emil Marceta</a>
+ */
+public class SamlPropertiesDialog extends JDialog {
+    private JPanel mainPanel;
+    private JCheckBox requireTimeValidity;
+    private JCheckBox requireSignature;
+    private JCheckBox requireEncryption;
+
+    private JButton helpButton;
+    private JButton okButton;
+    private JButton cancelButton;
+    private SamlTreeNode samlTreeNode;
+    private SamlSecurity samlSecurity;
+
+    public SamlPropertiesDialog(Frame owner, SamlTreeNode n) {
+        super(owner, false);
+        setTitle("SAML properties");
+        samlTreeNode = n;
+        samlSecurity = (SamlSecurity)samlTreeNode.asAssertion();
+        initialize();
+    }
+
+    private void initialize() {
+        setContentPane(mainPanel);
+        requireEncryption.setSelected(samlSecurity.isEncryption());
+        requireSignature.setSelected(samlSecurity.isValidateSignature());
+        requireTimeValidity.setSelected(samlSecurity.isValidateValidityPeriod());
+
+        okButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                samlSecurity.setEncryption(requireEncryption.isSelected());
+                samlSecurity.setValidateSignature(requireSignature.isSelected());
+                samlSecurity.setValidateValidityPeriod(requireTimeValidity.isSelected());
+                SamlPropertiesDialog.this.dispose();
+            }
+        });
+
+        cancelButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                SamlPropertiesDialog.this.dispose();
+
+            }
+        });
+    }
+
+    {
+// do not edit this generated initializer!!! do not add your code here!!!
+        $$$setupUI$$$();
+    }
+
+    /**
+     * generated code, do not edit or call this method manually !!!
+     */
+    private void $$$setupUI$$$() {
+        final JPanel _1;
+        _1 = new JPanel();
+        mainPanel = _1;
+        _1.setLayout(new com.intellij.uiDesigner.core.GridLayoutManager(4, 1, new Insets(5, 5, 5, 5), -1, -1));
+        final JPanel _2;
+        _2 = new JPanel();
+        _2.setLayout(new com.intellij.uiDesigner.core.GridLayoutManager(1, 1, new Insets(10, 0, 0, 0), -1, -1));
+        _1.add(_2, new com.intellij.uiDesigner.core.GridConstraints(0, 0, 1, 1, 1, 1, 3, 3, new Dimension(-1, -1), new Dimension(-1, -1), new Dimension(-1, -1)));
+        _2.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), null));
+        final JLabel _3;
+        _3 = new JLabel();
+        _3.setText("SAML constraints");
+        _2.add(_3, new com.intellij.uiDesigner.core.GridConstraints(0, 0, 1, 1, 8, 0, 0, 0, new Dimension(-1, -1), new Dimension(-1, -1), new Dimension(-1, -1)));
+        final JPanel _4;
+        _4 = new JPanel();
+        _4.setLayout(new com.intellij.uiDesigner.core.GridLayoutManager(1, 4, new Insets(5, 0, 0, 0), -1, -1));
+        _1.add(_4, new com.intellij.uiDesigner.core.GridConstraints(3, 0, 1, 1, 0, 3, 3, 3, new Dimension(-1, -1), new Dimension(-1, -1), new Dimension(-1, -1)));
+        final JButton _5;
+        _5 = new JButton();
+        helpButton = _5;
+        _5.setText("Help");
+        _4.add(_5, new com.intellij.uiDesigner.core.GridConstraints(0, 3, 1, 1, 0, 1, 3, 0, new Dimension(-1, -1), new Dimension(-1, -1), new Dimension(-1, -1)));
+        final JButton _6;
+        _6 = new JButton();
+        cancelButton = _6;
+        _6.setText("Cancel");
+        _4.add(_6, new com.intellij.uiDesigner.core.GridConstraints(0, 2, 1, 1, 0, 1, 3, 0, new Dimension(-1, -1), new Dimension(-1, -1), new Dimension(-1, -1)));
+        final JButton _7;
+        _7 = new JButton();
+        okButton = _7;
+        _7.setText("Ok");
+        _4.add(_7, new com.intellij.uiDesigner.core.GridConstraints(0, 1, 1, 1, 0, 1, 3, 0, new Dimension(-1, -1), new Dimension(-1, -1), new Dimension(-1, -1)));
+        final com.intellij.uiDesigner.core.Spacer _8;
+        _8 = new com.intellij.uiDesigner.core.Spacer();
+        _4.add(_8, new com.intellij.uiDesigner.core.GridConstraints(0, 0, 1, 1, 0, 1, 6, 1, new Dimension(-1, -1), new Dimension(-1, -1), new Dimension(-1, -1)));
+        final JPanel _9;
+        _9 = new JPanel();
+        _9.setLayout(new com.intellij.uiDesigner.core.GridLayoutManager(4, 3, new Insets(0, 0, 0, 0), -1, -1));
+        _1.add(_9, new com.intellij.uiDesigner.core.GridConstraints(1, 0, 1, 1, 0, 3, 3, 3, new Dimension(-1, -1), new Dimension(-1, -1), new Dimension(-1, -1)));
+        final JCheckBox _10;
+        _10 = new JCheckBox();
+        requireTimeValidity = _10;
+        _10.setText("Check time and date on ticket");
+        _10.setHorizontalAlignment(10);
+        _10.setHorizontalTextPosition(11);
+        _9.add(_10, new com.intellij.uiDesigner.core.GridConstraints(1, 1, 1, 1, 8, 0, 3, 0, new Dimension(-1, -1), new Dimension(-1, -1), new Dimension(-1, -1)));
+        final JCheckBox _11;
+        _11 = new JCheckBox();
+        requireEncryption = _11;
+        _11.setText("Require encrypted ticket");
+        _11.setHorizontalAlignment(10);
+        _11.setHorizontalTextPosition(11);
+        _9.add(_11, new com.intellij.uiDesigner.core.GridConstraints(2, 1, 1, 1, 8, 0, 3, 0, new Dimension(-1, -1), new Dimension(-1, -1), new Dimension(-1, -1)));
+        final JCheckBox _12;
+        _12 = new JCheckBox();
+        requireSignature = _12;
+        _12.setText("Require signed ticket");
+        _12.setHorizontalTextPosition(11);
+        _9.add(_12, new com.intellij.uiDesigner.core.GridConstraints(3, 1, 1, 1, 8, 0, 3, 0, new Dimension(-1, -1), new Dimension(-1, -1), new Dimension(-1, -1)));
+        final com.intellij.uiDesigner.core.Spacer _13;
+        _13 = new com.intellij.uiDesigner.core.Spacer();
+        _9.add(_13, new com.intellij.uiDesigner.core.GridConstraints(1, 2, 1, 1, 0, 1, 6, 1, new Dimension(100, -1), new Dimension(100, -1), new Dimension(-1, -1)));
+        final com.intellij.uiDesigner.core.Spacer _14;
+        _14 = new com.intellij.uiDesigner.core.Spacer();
+        _1.add(_14, new com.intellij.uiDesigner.core.GridConstraints(2, 0, 1, 1, 0, 2, 1, 6, new Dimension(-1, -1), new Dimension(-1, -1), new Dimension(-1, -1)));
+    }
+
+
+}
