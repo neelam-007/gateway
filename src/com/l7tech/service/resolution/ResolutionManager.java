55977613f778934d232c42595cf0ff76da6c49d3
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-12-02 00:29:50 +0000

    partial implementation
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@4080 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/service/resolution/ResolutionManager.java
+++ src/com/l7tech/service/resolution/ResolutionManager.java
@@ -1,7 +1,19 @@
 package com.l7tech.service.resolution;
 
+import cirrus.hibernate.HibernateException;
+import cirrus.hibernate.Query;
+import cirrus.hibernate.Session;
+import com.l7tech.logging.LogManager;
 import com.l7tech.objectmodel.DuplicateObjectException;
+import com.l7tech.objectmodel.HibernatePersistenceContext;
+import com.l7tech.objectmodel.PersistenceContext;
+import com.l7tech.objectmodel.UpdateException;
 import com.l7tech.service.PublishedService;
+
+import java.sql.SQLException;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * LAYER 7 TECHNOLOGIES, INC
@@ -26,13 +38,111 @@ public class ResolutionManager {
      * If those resolution parameters conflict with resolution parameters of another service, this
      * will throw a DuplicateObjectException exception.
      *
-     * This sould be called by service manager when saving and updating services.
+     * This sould be called by service manager when saving and updating services. If this throws, a rollback
+     * should occur.
      *
      * @param service the service whose resolution parameters should be recorded
      * @throws DuplicateObjectException this is thrown when there is a conflict between the resolution parameters of
-     * the passed service and the ones of another service.
+     * the passed service and the ones of another service. should rollback at that point.
+     * @throws UpdateException something went wrong, should rollback at that point
      */
-    public void recordResolutionParameters(PublishedService service) throws DuplicateObjectException {
-        // todo
+    public void recordResolutionParameters(PublishedService service) throws DuplicateObjectException, UpdateException {
+        Collection distinctItemsToSave = getDistinct(service);
+        Collection existingParameters = existingResolutionParameters(service);
+
+        // get the hibernate session
+        HibernatePersistenceContext pc = null;
+        Session session = null;
+        try {
+            pc = (HibernatePersistenceContext)PersistenceContext.getCurrent();
+            session = pc.getSession();
+        } catch (SQLException e) {
+            String msg = "cannot get hibernate session";
+            logger.log(Level.WARNING, msg, e);
+            throw new UpdateException(msg, e);
+        } catch (HibernateException e) {
+            String msg = "cannot get hibernate session";
+            logger.log(Level.WARNING, msg, e);
+            throw new UpdateException(msg, e);
+        }
+
+        // remove all trace of resolution parameters for this service
+        try {
+            for (Iterator i = existingParameters.iterator(); i.hasNext();) {
+                ResolutionParameters todelete = (ResolutionParameters)i.next();
+                session.delete(todelete);
+            }
+        } catch (SQLException e) {
+            String msg = "error deleting exsiting resolution parameters";
+            logger.log(Level.WARNING, msg, e);
+            throw new UpdateException(msg, e);
+        } catch (HibernateException e) {
+            String msg = "error deleting exsiting resolution parameters";
+            logger.log(Level.WARNING, msg, e);
+            throw new UpdateException(msg, e);
+        }
+
+        // insert these new ones
+        try {
+            for (Iterator i = distinctItemsToSave.iterator(); i.hasNext();) {
+                ResolutionParameters toadd = (ResolutionParameters)i.next();
+                session.save(toadd);
+            }
+        } catch (SQLException e) {
+            String msg = "error adding resolution parameters. throwing duplicate exception";
+            logger.log(Level.WARNING, msg, e);
+            throw new DuplicateObjectException(msg, e);
+        } catch (HibernateException e) {
+            String msg = "error adding resolution parameters. throwing duplicate exception";
+            logger.log(Level.WARNING, msg, e);
+            throw new DuplicateObjectException(msg, e);
+        }
     }
+
+    private Collection getDistinct(PublishedService service) {
+        ArrayList listOfParameters = new ArrayList();
+        SoapActionResolver soapresolver = new SoapActionResolver();
+        UrnResolver urnresolver = new UrnResolver();
+        Set soapactions = soapresolver.getDistinctParameters(service);
+        for (Iterator i = soapactions.iterator(); i.hasNext();) {
+            Set urns = urnresolver.getDistinctParameters(service);
+            String soapaction = (String)i.next();
+            for (Iterator j = urns.iterator(); j.hasNext();) {
+                ResolutionParameters parameters = new ResolutionParameters();
+                parameters.setSoapaction(soapaction);
+                parameters.setUrn((String)j.next());
+                listOfParameters.add(parameters);
+            }
+        }
+        return listOfParameters;
+    }
+
+    private Collection existingResolutionParameters(PublishedService service) {
+        String query = "from " + TABLE_NAME + " in class " + ResolutionParameters.class.getName() +
+                       " where " + TABLE_NAME + "." + SVCID_COLUMN + " = ?";
+
+        List hibResults = null;
+        HibernatePersistenceContext context = null;
+        try {
+            context = (HibernatePersistenceContext)PersistenceContext.getCurrent();
+            Query q = context.getSession().createQuery(query);
+            q.setLong(0, service.getOid());
+            hibResults = q.list();
+        } catch (SQLException e) {
+            hibResults = Collections.EMPTY_LIST;
+            logger.log(Level.WARNING, "hibernate error finding resolution parameters for " + service.getOid(), e);
+        }  catch (HibernateException e) {
+            hibResults = Collections.EMPTY_LIST;
+            logger.log(Level.WARNING, "hibernate error finding resolution parameters for " + service.getOid(), e);
+        }
+
+        return hibResults;
+    }
+
+    private static final String TABLE_NAME = "service_resolution";
+    private static final String SVCID_COLUMN = "serviceid";
+    private static final String SOAPACTION_COLUMN = "soapaction";
+    private static final String URN_COLUMN = "urn";
+
+    protected Logger logger = LogManager.getInstance().getSystemLogger();
 }
