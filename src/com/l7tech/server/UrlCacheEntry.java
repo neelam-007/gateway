4d857cc48f6d23e36bb705cc9176ce654180b7ff
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-12-03 21:54:30 +0000

    Initial checkin
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@4119 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/server/UrlCacheEntry.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2003 Layer 7 Technologies Inc.
+ *
+ * $Id$
+ */
+
+package com.l7tech.server;
+
+import com.l7tech.objectmodel.imp.EntityImp;
+
+/**
+ * @author alex
+ * @version $Revision$
+ */
+public class UrlCacheEntry extends EntityImp {
+    public UrlCacheEntry() {
+        super();
+    }
+
+    public String getContent() {
+        return _content;
+    }
+
+    public void setContent(String content) {
+        _content = content;
+    }
+
+    public String getUrl() {
+        return _url;
+    }
+
+    public void setUrl(String url) {
+        _url = url;
+    }
+
+    public int getSize() {
+        return _size;
+    }
+
+    public void setSize(int size) {
+        _size = size;
+    }
+
+    public long getTimestamp() {
+        return _timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        _timestamp = timestamp;
+    }
+
+    private String _content;
+    private String _url;
+    private int _size;
+    private long _timestamp;
+}
