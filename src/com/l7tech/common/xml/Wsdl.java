8336b09d14e56d0e713c64f78979169d546e9c99
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-12-03 22:15:19 +0000

    Don't find the same namespace URI more than once
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@4123 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/common/xml/Wsdl.java
+++ src/com/l7tech/common/xml/Wsdl.java
@@ -373,7 +373,7 @@ public class Wsdl {
                 if (ee instanceof SOAPBody) {
                     SOAPBody body = (SOAPBody)ee;
                     String uri = body.getNamespaceURI();
-                    if ( uri != null ) {
+                    if ( uri != null && !result.containsValue(uri) ) {
                         result.put( NS + ns++, uri );
                     }
                 }
