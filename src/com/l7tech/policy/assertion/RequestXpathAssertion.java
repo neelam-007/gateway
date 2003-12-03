c3348cf78809abffbd0f9b45c1866ed94994d966
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-12-03 21:53:01 +0000

    Added validate()
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@4117 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/policy/assertion/RequestXpathAssertion.java
+++ src/com/l7tech/policy/assertion/RequestXpathAssertion.java
@@ -7,8 +7,15 @@
 package com.l7tech.policy.assertion;
 
 import com.l7tech.policy.assertion.composite.CompositeAssertion;
+import com.l7tech.policy.PolicyValidatorResult;
+import com.l7tech.logging.LogManager;
 
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.jaxen.dom.DOMXPath;
+import org.jaxen.JaxenException;
 
 /**
  * @author alex
@@ -39,6 +46,24 @@ public class RequestXpathAssertion extends Assertion {
         _namespaceMap = namespaceMap;
     }
 
+    public void validate( PolicyValidatorResult result ) {
+        if ( _pattern == null ) {
+            result.addError( new PolicyValidatorResult.Error( this, "XPath pattern is missing", null ) );
+            _logger.warning( "XPath pattern is missing" );
+            return;
+        } else {
+            try {
+                DOMXPath temp = new DOMXPath(_pattern);
+            } catch (JaxenException e) {
+                result.addError( new PolicyValidatorResult.Error( this, "XPath pattern is not valid", e ) );
+                _logger.log( Level.WARNING, "XPath pattern is invalid", e );
+                return;
+            }
+        }
+
+        _logger.fine( "XPath pattern validated." );
+    }
+
     public String toString() {
         StringBuffer sb = new StringBuffer(super.toString());
         if (_namespaceMap != null)
@@ -50,4 +75,5 @@ public class RequestXpathAssertion extends Assertion {
 
     private String _pattern;
     private Map _namespaceMap;
+    private transient Logger _logger = LogManager.getInstance().getSystemLogger();
 }
