78031ab6dd9b5886ca603bb78c3fc9794be1d263
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-12-03 00:38:54 +0000

    Done with Jaxen--doesn't suck too badly
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@4101 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/server/policy/assertion/ServerRequestXpathAssertion.java
+++ src/com/l7tech/server/policy/assertion/ServerRequestXpathAssertion.java
@@ -13,80 +13,45 @@ import com.l7tech.message.XmlRequest;
 import com.l7tech.policy.assertion.AssertionStatus;
 import com.l7tech.policy.assertion.PolicyAssertionException;
 import com.l7tech.policy.assertion.RequestXpathAssertion;
-import com.l7tech.service.PublishedService;
-import org.apache.xerces.dom.DocumentImpl;
-import org.apache.xml.utils.PrefixResolver;
-import org.apache.xpath.CachedXPathAPI;
-import org.apache.xpath.XPathContext;
+import org.jaxen.dom.DOMXPath;
+import org.jaxen.JaxenException;
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.traversal.NodeIterator;
 import org.xml.sax.SAXException;
 
-import javax.wsdl.WSDLException;
-import javax.xml.transform.TransformerException;
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.List;
+import java.util.Map;
+import java.util.Iterator;
 
 /**
  * @author alex
  * @version $Revision$
  */
 public class ServerRequestXpathAssertion implements ServerAssertion {
-    private class L7PrefixResolver implements PrefixResolver {
-        public L7PrefixResolver( Map namespaceMap ) {
-            _map = namespaceMap;
-        }
-
-        public String getNamespaceForPrefix(String prefix) {
-            return (String)_map.get(prefix);
-        }
-
-        public String getNamespaceForPrefix(String prefix, Node context) {
-            return getNamespaceForPrefix(prefix);
-        }
-
-        public String getBaseIdentifier() {
-            return null;
-        }
-
-        public boolean handlesNullPrefixes() {
-            return false;
-        }
-
-        private Map _map;
-    }
 
     public ServerRequestXpathAssertion( RequestXpathAssertion data ) {
         _data = data;
-        _cachedXpathAPI = new CachedXPathAPI();
-
-        /*
-        Map namespaceMap = _data.getNamespaceMap();
-        if ( namespaceMap != null && !namespaceMap.isEmpty() ) {
-            _namespaceNode = getNamespaceNode( namespaceMap );
-            XPathContext context = _cachedXpathAPI.getXPathContext();
-            context.pushNamespaceContext( new L7PrefixResolver( namespaceMap ) );
-            _havePrefixResolver = true;
-        }
-        */
     }
 
-    private Node getNamespaceNode( Map namespaceMap ) {
-        DocumentImpl doc = new DocumentImpl();
-        Element el = doc.createElement("namespaces");
-        doc.appendChild(el);
-        for (Iterator i = namespaceMap.keySet().iterator(); i.hasNext();) {
-            String prefix = (String) i.next();
-            String uri = (String)namespaceMap.get(prefix);
-            el.setAttribute( "xmlns:" + prefix, uri );
+    private DOMXPath getDOMXpath() throws JaxenException {
+        if ( _domXpath == null ) {
+
+            String pattern = _data.getPattern();
+
+            _domXpath = new DOMXPath(pattern);
+
+            Map namespaceMap = _data.getNamespaceMap();
+
+            for (Iterator i = namespaceMap.keySet().iterator(); i.hasNext();) {
+                String key = (String) i.next();
+                String uri = (String)namespaceMap.get(key);
+                _domXpath.addNamespace( key, uri );
+            }
         }
 
-        return doc;
+        return _domXpath;
     }
 
     public AssertionStatus checkRequest(Request request, Response response) throws IOException, PolicyAssertionException {
@@ -95,37 +60,23 @@ public class ServerRequestXpathAssertion implements ServerAssertion {
                 XmlRequest xreq = (XmlRequest)request;
                 Document doc = xreq.getDocument();
 
-                String pattern = _data.getPattern();
+                List result = getDOMXpath().selectNodes(doc);
 
-                /*
-                if ( !_havePrefixResolver ) {
-                    PublishedService service = (PublishedService)request.getParameter( Request.PARAM_SERVICE );
-                    synchronized( _cachedXpathAPI ) {
-                        XPathContext context = _cachedXpathAPI.getXPathContext();
-                        Map namespaceMap = service.parsedWsdl().getNamespaces();
-                        context.pushNamespaceContext( new L7PrefixResolver( namespaceMap ) );
-                        _havePrefixResolver = true;
-                        _namespaceNode = getNamespaceNode( namespaceMap );
-                    }
-                }
-                */
-
-                NodeIterator i = _cachedXpathAPI.selectNodeIterator( doc, pattern );
-                if ( i.nextNode() != null ) {
-                    _logger.fine( "XPath pattern " + pattern + " matched request" );
+                if ( result != null && result.size() > 0 ) {
+                    _logger.fine( "XPath pattern " + _data.getPattern() + " matched request" );
                     return AssertionStatus.NONE;
                 } else {
-                    _logger.info( "XPath pattern " + pattern + " didn't match request!" );
+                    _logger.info( "XPath pattern " + _data.getPattern()  + " didn't match request!" );
                     return AssertionStatus.FALSIFIED;
                 }
-            } catch ( TransformerException txe ) {
-                _logger.log( Level.WARNING, "Caught TransformerException during XPath query", txe );
-                return AssertionStatus.SERVER_ERROR;
             } catch (SAXException e) {
                 _logger.log( Level.WARNING, "Caught SAXException during XPath query", e );
                 return AssertionStatus.SERVER_ERROR;
             } catch (IOException e) {
                 _logger.log( Level.WARNING, "Caught IOException during XPath query", e );
+                return AssertionStatus.SERVER_ERROR;
+            } catch (JaxenException e) {
+                _logger.log( Level.WARNING, "Caught JaxenException during XPath query", e );
                 return AssertionStatus.SERVER_ERROR;
             } /*catch (WSDLException e) {
                 _logger.log( Level.SEVERE, "Caught WSDLException during XPath namespace resolution", e );
@@ -136,9 +87,7 @@ public class ServerRequestXpathAssertion implements ServerAssertion {
         return AssertionStatus.FALSIFIED;
     }
 
-    private Logger _logger = LogManager.getInstance().getSystemLogger();
-    private RequestXpathAssertion _data;
-    private CachedXPathAPI _cachedXpathAPI = new CachedXPathAPI();
-    private boolean _havePrefixResolver = false;
-    private Node _namespaceNode;
+    private final RequestXpathAssertion _data;
+    private final Logger _logger = LogManager.getInstance().getSystemLogger();
+    private DOMXPath _domXpath;
 }
