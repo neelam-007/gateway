9e7ff87161a8e58f32766cf5fca79ad792600565
emil <emil@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-12-04 07:40:26 +0000

    Security element check
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@4142 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/server/policy/assertion/xmlsec/ServerSamlSecurity.java
+++ src/com/l7tech/server/policy/assertion/xmlsec/ServerSamlSecurity.java
@@ -1,25 +1,22 @@
 package com.l7tech.server.policy.assertion.xmlsec;
 
-import com.l7tech.server.policy.assertion.ServerAssertion;
-import com.l7tech.server.util.ServerSoapUtil;
-import com.l7tech.policy.assertion.AssertionStatus;
-import com.l7tech.policy.assertion.PolicyAssertionException;
-import com.l7tech.policy.assertion.xmlsec.SamlSecurity;
+import com.l7tech.logging.LogManager;
 import com.l7tech.message.Request;
 import com.l7tech.message.Response;
 import com.l7tech.message.SoapRequest;
-import com.l7tech.logging.LogManager;
+import com.l7tech.policy.assertion.AssertionStatus;
+import com.l7tech.policy.assertion.PolicyAssertionException;
+import com.l7tech.policy.assertion.xmlsec.SamlSecurity;
+import com.l7tech.server.policy.assertion.ServerAssertion;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import java.io.IOException;
-import java.io.StringReader;
-import java.util.logging.Logger;
+import java.util.NoSuchElementException;
 import java.util.logging.Level;
-
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
-import org.apache.xmlbeans.impl.common.ReaderInputStream;
-
-import javax.xml.soap.*;
+import java.util.logging.Logger;
 
 /**
  * Class <code>ServerSamlSecurity</code> represents the server side saml
@@ -29,9 +26,15 @@ import javax.xml.soap.*;
  */
 public class ServerSamlSecurity implements ServerAssertion {
     private SamlSecurity assertion;
+    public static final String HEADER_EL_NAME = "Header";
+    public static final String SECURITY_EL_NAME = "Security";
+    public static final String SECURITY_NAMESPACE_PREFIX = "wsse";
+    public static final String SECURITY_NAMESPACE = "http://schemas.xmlsoap.org/ws/2002/xx/secext";
+    public static final String SECURITY_NAMESPACE2 = "http://schemas.xmlsoap.org/ws/2002/12/secext";
 
     /**
      * Create the server side saml security policy element
+     * 
      * @param sa the saml
      */
     public ServerSamlSecurity(SamlSecurity sa) {
@@ -43,35 +46,48 @@ public class ServerSamlSecurity implements ServerAssertion {
 
     /**
      * SSG Server-side processing of the given request.
-     *
+     * 
      * @param request  (In/Out) The request to check.  May be modified by processing.
      * @param response (Out) The response to send back.  May be replaced during processing.
      * @return AssertionStatus.NONE if this Assertion did its business successfully; otherwise, some error code
      * @throws com.l7tech.policy.assertion.PolicyAssertionException
      *          something is wrong in the policy dont throw this if there is an issue with the request or the response
      */
-    public AssertionStatus checkRequest(Request request, Response response) throws IOException, PolicyAssertionException {
-        // GET THE DOCUMENT
-             try {
-                 SoapRequest sr = (SoapRequest)request;
-                 ReaderInputStream ri = new ReaderInputStream(new StringReader(sr.getRequestXml()), "UTF-8");
-                 SOAPMessage message = MessageFactory.newInstance().createMessage(new MimeHeaders(), ri);
-                 if (!hasSecuritryHeader(message)) {
-                    logger.log(Level.WARNING, "No security header present - returning 'falsified'");
-                     return AssertionStatus.FALSIFIED;
-                 }
-             } catch (SOAPException e) {
-                 logger.log(Level.SEVERE, "error getting the xml document", e);
-                 return AssertionStatus.FALSIFIED;
-             }
-
-        return AssertionStatus.FALSIFIED;
+    public AssertionStatus checkRequest(Request request, Response response)
+      throws IOException, PolicyAssertionException {
+        try {
+            SoapRequest sr = (SoapRequest)request;
+            Document document = sr.getDocument();
+            Element securityElement = getSecurityElement(document);
+        } catch (NoSuchElementException e) {
+            logger.log(Level.SEVERE, "Security header missing", e);
+            return AssertionStatus.FALSIFIED;
+        } catch (SAXException e) {
+            logger.log(Level.SEVERE, "error getting the xml document", e);
+            return AssertionStatus.FALSIFIED;
+        }
+        return AssertionStatus.NONE;
     }
 
-    private boolean hasSecuritryHeader(SOAPMessage sm) throws SOAPException {
-        SOAPHeader sh = sm.getSOAPPart().getEnvelope().getHeader();
-        return false;
-    }
+    /**
+     * Returns the Security element from the header of a soap message. If the
+     * message does not have a header throwsNoSuchElementException
+     * 
+     * @param document DOM document containing the soap message
+     * @return the security element (never null)
+     */
+    private Element getSecurityElement(Document document)
+      throws NoSuchElementException {
+        NodeList listSecurityElements = document.getElementsByTagNameNS(SECURITY_NAMESPACE, SECURITY_EL_NAME);
+        if (listSecurityElements.getLength() > 0) {
+            return (Element)listSecurityElements.item(0);
+        }
+        listSecurityElements = document.getElementsByTagNameNS(SECURITY_NAMESPACE2, SECURITY_EL_NAME);
 
+        if (listSecurityElements.getLength() > 0) {
+            return (Element)listSecurityElements.item(0);
+        }
+        throw new NoSuchElementException("Could not find security header in "+document);
+    }
     private Logger logger = LogManager.getInstance().getSystemLogger();
 }
