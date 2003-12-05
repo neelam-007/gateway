173c2239f8334e97626a7e434865c2a7f8fe487b
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-12-05 00:42:49 +0000

    new tool that checks jars
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@4171 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ tools/com/l7tech/tools/JarChecker.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2003 Layer 7 Technologies Inc.
+ *
+ * $Id$
+ */
+
+package com.l7tech.tools;
+
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.jar.JarFile;
+import java.util.zip.ZipEntry;
+
+/**
+ * Using the current VM's class path, attempt to load every class in the specified jarfile.
+ *
+ * @author mike
+ * @version 1.0
+ */
+public class JarChecker {
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.err.println("Usage: JarChecker jarfile.jar");
+            System.exit(1);
+        }
+
+        String jar = args[0];
+        try {
+            checkJar(jar);
+        } catch (Exception e) {
+            fatalErr(e);
+        }
+    }
+
+    private static void checkJar(String jar) throws IOException {
+        JarFile jarFile = new JarFile(jar);
+        Enumeration entries = jarFile.entries();
+        while (entries.hasMoreElements()) {
+            ZipEntry entry = (ZipEntry) entries.nextElement();
+            String name = entry.getName();
+            if (name.endsWith(".class")) {
+                String truncName = name.substring(0, name.length() - ".class".length());
+                String className = truncName.replace('/', '.').replace('\\', '.');
+                if (className.indexOf('$') > -1) {
+                    log("Skipping inner class " + name);
+                } else {
+                    log("Loading class: " + className);
+                    try {
+                        Class.forName(className);
+                    } catch (ClassNotFoundException e) {
+                        System.err.println("ERROR: ClassNotFoundException: " + e.getMessage());
+                    }
+                }
+            }
+        }
+    }
+
+    private static void log(String s) {
+        System.out.println(s);
+    }
+
+    private static void fatalErr(Throwable e) {
+        System.err.println("Fatal error: " + e.getMessage());
+        e.printStackTrace(System.err);
+    }
+}
