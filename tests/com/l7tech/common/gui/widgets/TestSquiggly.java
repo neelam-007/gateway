31c053258f8a0dd490931e9300ad97fd43e06771
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-12-09 18:11:26 +0000

    Moved stuff here from skunkworks
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@4256 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ tests/com/l7tech/common/gui/widgets/TestSquiggly.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2003 Layer 7 Technologies Inc.
+ *
+ * $Id$
+ */
+
+package com.l7tech.common.gui.widgets;
+
+import com.l7tech.common.gui.widgets.SquigglyTextField;
+
+import javax.swing.JFrame;
+
+/**
+ * @author alex
+ * @version $Revision$
+ */
+public class TestSquiggly extends JFrame {
+    public TestSquiggly() {
+        super( "Test squiggly" );
+        setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        SquigglyTextField text = new SquigglyTextField(80);
+        getContentPane().add(text);
+        pack();
+        show();
+    }
+
+    public static void main(String[] args) {
+        TestSquiggly me = new TestSquiggly();
+    }
+}
