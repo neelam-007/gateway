a9f5a5fd741665c7fc639b294dcf2b090dd7cfc5
emil <emil@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-12-10 06:29:32 +0000

    Test that creates an empty saml assertion with dummy data
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@4289 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- tests/com/l7tech/common/security/saml/XmlBeansSamlTest.java
+++ tests/com/l7tech/common/security/saml/XmlBeansSamlTest.java
@@ -5,13 +5,12 @@ import junit.framework.TestSuite;
 import junit.framework.Test;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Calendar;
+import java.util.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.FileNotFoundException;
+import java.io.StringWriter;
+import java.math.BigInteger;
 
 import org.apache.xmlbeans.XmlOptions;
 import org.apache.xmlbeans.XmlError;
@@ -20,6 +19,7 @@ import org.xml.sax.SAXException;
 import x0Assertion.oasisNamesTcSAML1.AssertionDocument;
 import x0Assertion.oasisNamesTcSAML1.AssertionType;
 import x0Assertion.oasisNamesTcSAML1.ConditionsType;
+import x0Assertion.oasisNamesTcSAML1.SubjectStatementAbstractType;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -29,18 +29,18 @@ import javax.xml.parsers.DocumentBuilderFactory;
  */
 public class XmlBeansSamlTest extends TestCase {
     public XmlBeansSamlTest(String name) throws Exception {
-           super(name);
-       }
+        super(name);
+    }
 
-       /**
-        * create the <code>TestSuite</code> containing the XmlBeansSamlTest
-        * <p/>
-        * Add new tests at the bottom of the list.
-        */
-       public static Test suite() {
-           return new TestSuite(XmlBeansSamlTest.class);
+    /**
+     * create the <code>TestSuite</code> containing the XmlBeansSamlTest
+     * <p/>
+     * Add new tests at the bottom of the list.
+     */
+    public static Test suite() {
+        return new TestSuite(XmlBeansSamlTest.class);
 
-       }
+    }
 
 
     public void testParseAssertionFromFile() throws Exception {
@@ -55,18 +55,18 @@ public class XmlBeansSamlTest extends TestCase {
         xo = new XmlOptions();
         Collection errors = new ArrayList();
         xo.setErrorListener(errors);
-        System.out.println("The document is "+(doc.validate(xo) ? "valid" : "invalid"));
+        System.out.println("The document is " + (doc.validate(xo) ? "valid" : "invalid"));
         for (Iterator iterator = errors.iterator(); iterator.hasNext();) {
-            XmlError xerr = (XmlError) iterator.next();
+            XmlError xerr = (XmlError)iterator.next();
             System.out.println(xerr);
         }
 
         AssertionType at = doc.getAssertion();
         ConditionsType type = at.getConditions();
-        Calendar notBefore  = type.getNotBefore();
-        Calendar notAfter   = type.getNotOnOrAfter();
-        System.out.println("The not before is: "+notBefore);
-        System.out.println("The not after is: "+notAfter);
+        Calendar notBefore = type.getNotBefore();
+        Calendar notAfter = type.getNotOnOrAfter();
+        System.out.println("The not before is: " + notBefore);
+        System.out.println("The not after is: " + notAfter);
     }
 
     public void testParseAssertionFromNode() throws Exception {
@@ -77,31 +77,65 @@ public class XmlBeansSamlTest extends TestCase {
         xo = new XmlOptions();
         Collection errors = new ArrayList();
         xo.setErrorListener(errors);
-        System.out.println("The document is "+(assertionDoc.validate(xo) ? "valid" : "invalid"));
+        System.out.println("The document is " + (assertionDoc.validate(xo) ? "valid" : "invalid"));
         for (Iterator iterator = errors.iterator(); iterator.hasNext();) {
-            XmlError xerr = (XmlError) iterator.next();
+            XmlError xerr = (XmlError)iterator.next();
             System.out.println(xerr);
         }
 
         AssertionType at = assertionDoc.getAssertion();
         ConditionsType type = at.getConditions();
-        Calendar notBefore  = type.getNotBefore();
-        Calendar notAfter   = type.getNotOnOrAfter();
-        System.out.println("The not before is: "+notBefore);
-        System.out.println("The not after is: "+notAfter);
+        Calendar notBefore = type.getNotBefore();
+        Calendar notAfter = type.getNotOnOrAfter();
+        System.out.println("The not before is: " + notBefore);
+        System.out.println("The not after is: " + notAfter);
+    }
+
+    public void testCreateEmptyAssertion() throws Exception {
+        AssertionDocument adoc = AssertionDocument.Factory.newInstance();
+        AssertionType assertion = AssertionType.Factory.newInstance();
+
+        assertion.setMinorVersion(new BigInteger("0"));
+        assertion.setMajorVersion(new BigInteger("1"));
+        assertion.setAssertionID(Long.toHexString(System.currentTimeMillis()));
+        assertion.setIssuer("ssg");
+        assertion.setIssueInstant(Calendar.getInstance());
+        ConditionsType ct = ConditionsType.Factory.newInstance();
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        ct.setNotBefore(calendar);
+        Calendar c2 = (Calendar)calendar.clone();
+        c2.roll(Calendar.MINUTE, 5);
+        ct.setNotOnOrAfter(c2);
+        assertion.setConditions(ct);
+        SubjectStatementAbstractType ss = assertion.addNewSubjectStatement();
+        StringWriter sw = new StringWriter();
+        adoc.setAssertion(assertion);
+
+        XmlOptions xo = new XmlOptions();
+        Map namespaces = new HashMap();
+        namespaces.put("saml", "urn:oasis:names:tc:SAML:1.0:assertion");
+        xo.setSaveImplicitNamespaces(namespaces);
+        xo.setSavePrettyPrint();
+        xo.setSavePrettyPrintIndent(2);
+        xo.setLoadLineNumbers();
+
+        adoc.save(sw, xo);
+        System.out.println("The document is: " + sw.toString());
     }
 
     private Document getDocument(String resourceName)
-         throws IOException, ParserConfigurationException, SAXException {
-           ClassLoader cl = getClass().getClassLoader();
-           InputStream is = cl.getResourceAsStream(resourceName);
-           if (is == null) {
-               throw new FileNotFoundException(resourceName);
-           }
-           DocumentBuilderFactory df = DocumentBuilderFactory.newInstance();
-           df.setNamespaceAware(true);
-           return df.newDocumentBuilder().parse(is);
-       }
+      throws IOException, ParserConfigurationException, SAXException {
+        ClassLoader cl = getClass().getClassLoader();
+        InputStream is = cl.getResourceAsStream(resourceName);
+        if (is == null) {
+            throw new FileNotFoundException(resourceName);
+        }
+        DocumentBuilderFactory df = DocumentBuilderFactory.newInstance();
+        df.setNamespaceAware(true);
+        return df.newDocumentBuilder().parse(is);
+    }
 
     /**
      * Test <code>KeysTest</code> main.
