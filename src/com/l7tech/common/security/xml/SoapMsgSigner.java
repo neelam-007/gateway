8e70be6c226d0d43b716d192f6178854c248fd7a
emil <emil@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-12-12 19:04:38 +0000

    Element signing
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@4331 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/common/security/xml/SoapMsgSigner.java
+++ src/com/l7tech/common/security/xml/SoapMsgSigner.java
@@ -1,24 +1,22 @@
 package com.l7tech.common.security.xml;
 
+import com.ibm.xml.dsig.*;
+import com.ibm.xml.dsig.util.AdHocIDResolver;
+import com.l7tech.common.util.SoapUtil;
 import org.w3c.dom.*;
 
 import java.security.PrivateKey;
 import java.security.PublicKey;
-import java.security.interfaces.RSAPrivateKey;
-import java.security.interfaces.DSAPrivateKey;
 import java.security.cert.X509Certificate;
-import com.ibm.xml.dsig.util.AdHocIDResolver;
-import com.ibm.xml.dsig.*;
-import com.l7tech.common.util.SoapUtil;
-import com.l7tech.common.security.xml.InvalidSignatureException;
-import com.l7tech.common.security.xml.SignatureNotFoundException;
+import java.security.interfaces.DSAPrivateKey;
+import java.security.interfaces.RSAPrivateKey;
 
 /**
  * User: flascell
  * Date: Aug 19, 2003
  * Time: 2:44:50 PM
  * $Id$
- *
+ * <p/>
  * Signs soap messages.
  * Meant to be used by both server-side and proxy-side dsig assertions
  */
@@ -26,52 +24,79 @@ public class SoapMsgSigner {
 
     /**
      * Appends a soap message with a digital signature of it's entire envelope.
-     *
+     * <p/>
      * If the envelope already has as Id attribute, it's value will be used to refer to the envelope within the
      * SignedInfo element. Otherwise, an Id of value DEF_ENV_TAG will be used.
-     *
-     * @param soapMsg the xml document containing the soap message expected to contain at least a soapenvelope element.
-     * this document contains the signature when at return time.
-     *
+     * 
+     * @param soapMsg    the xml document containing the soap message expected to contain at least a soapenvelope element.
+     *                   this document contains the signature when at return time.
      * @param privateKey the private key of the signer if imlpements RSAPrivateKey signature method will be
-     * http://www.w3.org/2000/09/xmldsig#rsa-sha1, if privateKey implements DSAPrivateKey, signature method will be
-     * http://www.w3.org/2000/09/xmldsig#dsa-sha1.
-     *
-     * @param cert the signer's cert
-     *
+     *                   http://www.w3.org/2000/09/xmldsig#rsa-sha1, if privateKey implements DSAPrivateKey, signature method will be
+     *                   http://www.w3.org/2000/09/xmldsig#dsa-sha1.
+     * @param cert       the signer's cert
      * @throws com.ibm.xml.dsig.SignatureStructureException
+     *          
      * @throws com.ibm.xml.dsig.XSignatureException
+     *          
      */
-    public void signEnvelope(Document soapMsg, PrivateKey privateKey, X509Certificate cert) throws SignatureStructureException, XSignatureException {
+    public void signEnvelope(Document soapMsg, PrivateKey privateKey, X509Certificate cert)
+      throws SignatureStructureException, XSignatureException {
         // is the envelope already ided?
         String id = soapMsg.getDocumentElement().getAttribute("Id");
+
         if (id == null || id.length() < 1) {
             id = DEF_ENV_TAG;
-            soapMsg.getDocumentElement().setAttribute("Id", id);
+        }
+        signElement(soapMsg, soapMsg.getDocumentElement(), id, privateKey, cert);
+    }
+
+    /**
+     * Sign the document element using the private key and Embed the <code>X509Certificate</code> into
+     * the XML document.
+     * 
+     * @param document    the xml document containing the element to sign.
+     * @param elem        the document element
+     * @param referenceId the signature reference ID attreibute value
+     * @param privateKey  the private key of the signer if imlpements RSAPrivateKey signature method will be
+     *                    http://www.w3.org/2000/09/xmldsig#rsa-sha1, if privateKey implements DSAPrivateKey, signature method will be
+     *                    http://www.w3.org/2000/09/xmldsig#dsa-sha1.
+     * @param cert        the signer's cert
+     * @throws com.ibm.xml.dsig.SignatureStructureException
+     *                                  
+     * @throws com.ibm.xml.dsig.XSignatureException
+     *                                  
+     * @throws IllegalArgumentException if any of the parameters i <b>null</b>
+     */
+    public void signElement(Document document, Element elem, String referenceId, PrivateKey privateKey, X509Certificate cert)
+      throws SignatureStructureException, XSignatureException {
+
+        if (document == null || elem == null | referenceId == null ||
+          privateKey == null || cert == null) {
+            throw new IllegalArgumentException();
+        }
+
+        String id = elem.getAttribute(referenceId);
+        if (id == null || "".equals(id)) {
+            id = referenceId;
+            elem.setAttribute("Id", referenceId);
         }
 
         // set the appropriate signature method
-        String signaturemethod = "";
-        if (privateKey instanceof RSAPrivateKey) signaturemethod = SignatureMethod.RSA;
-        else if (privateKey instanceof DSAPrivateKey) signaturemethod = SignatureMethod.DSA;
-        else {
-            throw new IllegalArgumentException("Unsupported private key type: " + privateKey.getClass().getName());
-        }
-
-        // get or create security element
-        Element securityEl = SoapUtil.getOrMakeSecurityElement(soapMsg);
+        String signaturemethod = getSignatureMethod(privateKey);
 
         // Create signature template and populate with appropriate transforms. Reference is to SOAP Envelope
-        TemplateGenerator template = new TemplateGenerator(soapMsg, XSignature.SHA1, Canonicalizer.W3C2, signaturemethod);
+        TemplateGenerator template = new TemplateGenerator(document, XSignature.SHA1, Canonicalizer.W3C2, signaturemethod);
         template.setPrefix(DS_PREFIX);
         Reference ref = template.createReference("#" + id);
         ref.addTransform(Transform.ENVELOPED);
         ref.addTransform(Transform.W3CC14N2);
         template.addReference(ref);
-        Element sigEl = template.getSignatureElement();
+        Element emptySignatureElement = template.getSignatureElement();
 
         // Signature is inserted in Header/Security, as per WS-S
-        Element envelopedSigEl = (Element) securityEl.appendChild(sigEl);
+        Element securityHeaderElement = SoapUtil.getOrMakeSecurityElement(document);
+
+        Element signatureElement = (Element)securityHeaderElement.appendChild(emptySignatureElement);
 
         // Include KeyInfo element in signature and embed cert into subordinate X509Data element
         KeyInfo keyInfo = new KeyInfo();
@@ -79,28 +104,48 @@ public class SoapMsgSigner {
         x509Data.setCertificate(cert);
         x509Data.setParameters(cert, true, true, true);
         keyInfo.setX509Data(new KeyInfo.X509Data[]{x509Data});
-        keyInfo.insertTo(envelopedSigEl, DS_PREFIX);
+        keyInfo.insertTo(signatureElement, DS_PREFIX);
 
-        normalizeDoc(soapMsg);
+        normalizeDoc(document);
 
         // Setup context and sign document
         SignatureContext sigContext = new SignatureContext();
-        AdHocIDResolver idResolver = new AdHocIDResolver(soapMsg);
+        AdHocIDResolver idResolver = new AdHocIDResolver(document);
         sigContext.setIDResolver(idResolver);
 
-        sigContext.sign(envelopedSigEl, privateKey);
+        sigContext.sign(signatureElement, privateKey);
     }
 
     /**
-     * Verify that a valid signature is included and that the entire envelope is signed.
+       * Verify that a valid signature is included and that the entire envelope is signed.
+       * The validity of the signer's cert is NOT verified against the local root authority.
+       *
+       * @param soapMsg the soap message that potentially contains a digital signature
+       * @return the cert used as part of the message's signature (not checked against any authority) never null
+       * @throws com.l7tech.common.security.xml.SignatureNotFoundException
+       *          if no signature is found in document
+       * @throws com.l7tech.common.security.xml.InvalidSignatureException
+       *          if the signature is invalid, not in an expected format or is missing information
+       */
+      public X509Certificate validateSignature(Document soapMsg)
+        throws SignatureNotFoundException, InvalidSignatureException {
+        return validateSignature(soapMsg, soapMsg.getDocumentElement());
+    }
+
+    /**
+     * Verify that a valid signature is included and that the element is signed.
      * The validity of the signer's cert is NOT verified against the local root authority.
-     *
+     * 
      * @param soapMsg the soap message that potentially contains a digital signature
+     * @param element the signed element
      * @return the cert used as part of the message's signature (not checked against any authority) never null
-     * @throws com.l7tech.common.security.xml.SignatureNotFoundException if no signature is found in document
-     * @throws com.l7tech.common.security.xml.InvalidSignatureException if the signature is invalid, not in an expected format or is missing information
+     * @throws com.l7tech.common.security.xml.SignatureNotFoundException
+     *          if no signature is found in document
+     * @throws com.l7tech.common.security.xml.InvalidSignatureException
+     *          if the signature is invalid, not in an expected format or is missing information
      */
-    public X509Certificate validateSignature(Document soapMsg) throws SignatureNotFoundException, InvalidSignatureException {
+    public X509Certificate validateSignature(Document soapMsg, Element element)
+      throws SignatureNotFoundException, InvalidSignatureException {
         normalizeDoc(soapMsg);
 
         // find signature element
@@ -148,7 +193,7 @@ public class SoapMsgSigner {
         }
 
         // verify that the entire envelope is signed
-        String refid = soapMsg.getDocumentElement().getAttribute("Id");
+        String refid = element.getAttribute("Id");
         if (refid == null || refid.length() < 1) {
             throw new InvalidSignatureException("No reference id on envelope");
         }
@@ -168,6 +213,7 @@ public class SoapMsgSigner {
         throw new InvalidSignatureException("No reference to envelope was verified.");
     }
 
+
     public Element getSignatureHeaderElement(Document doc) {
         // find signature element
         NodeList tmpNodeList = doc.getElementsByTagNameNS(XSignature.XMLDSIG_NAMESPACE, "Signature");
@@ -176,6 +222,19 @@ public class SoapMsgSigner {
         }
         return (Element)tmpNodeList.item(0);
     }
+
+    private String getSignatureMethod(PrivateKey privateKey) {
+        String signaturemethod;
+        if (privateKey instanceof RSAPrivateKey)
+            signaturemethod = SignatureMethod.RSA;
+        else if (privateKey instanceof DSAPrivateKey)
+            signaturemethod = SignatureMethod.DSA;
+        else {
+            throw new IllegalArgumentException("Unsupported private key type: " + privateKey.getClass().getName());
+        }
+        return signaturemethod;
+    }
+
 
     /**
      * Document normalization is necessary because the serialization / deserialization process between the
@@ -211,8 +270,7 @@ public class SoapMsgSigner {
                 } else {
                     normalizeTextNodeValue((Text)node);
                 }
-            }
-            else if (node.getNodeType() == Node.ELEMENT_NODE) {
+            } else if (node.getNodeType() == Node.ELEMENT_NODE) {
                 filterOutEmptyTextNodesAndNormalizeStrings((Element)node);
             }
         }
@@ -228,7 +286,7 @@ public class SoapMsgSigner {
             if (isCharSpace(c)) {
                 int begin = i;
                 int end = -1; // the first non space char
-                for (int j = i+1; j < originalVal.length(); j++) {
+                for (int j = i + 1; j < originalVal.length(); j++) {
                     char c2 = originalVal.charAt(j);
                     if (!isCharSpace(c2)) {
                         end = j;
@@ -240,7 +298,7 @@ public class SoapMsgSigner {
                     originalVal = originalVal.substring(0, begin);
                     // we're done
                     break;
-                } else if ((end-begin) > 1) {
+                } else if ((end - begin) > 1) {
                     // either a space prefix
                     if (begin == 0) {
                         originalVal = originalVal.substring(end);
@@ -251,7 +309,8 @@ public class SoapMsgSigner {
                 }
                 // this is a lonely space
                 // check if it's first thing
-                if (begin == 0) originalVal = originalVal.substring(1);
+                if (begin == 0)
+                    originalVal = originalVal.substring(1);
                 // make sure it hasn't been replaced by a \n or something like that
                 else if (c != ' ') originalVal = originalVal.replace(c, ' ');
             }
