4031be878d56ac491d79f5789e9f15bf21b31936
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-12-16 00:17:09 +0000

    New Rsa crypto driver
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@4367 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/common/security/prov/rsa/RsaJceProviderEngine.java
@@ -0,0 +1,222 @@
+/*
+ * Copyright (C) 2003 Layer 7 Technologies Inc.
+ *
+ * $Id$
+ */
+
+package com.l7tech.common.security.prov.rsa;
+
+import com.l7tech.common.security.CertificateRequest;
+import com.l7tech.common.security.JceProviderEngine;
+import com.l7tech.common.security.RsaSignerEngine;
+import com.l7tech.common.util.HexUtils;
+import com.rsa.certj.cert.CertificateException;
+import com.rsa.certj.cert.PKCS10CertRequest;
+import com.rsa.certj.cert.X500Name;
+import com.rsa.jsafe.JSAFE_PrivateKey;
+import com.rsa.jsafe.JSAFE_PublicKey;
+import com.rsa.jsafe.JSAFE_UnimplementedException;
+
+import java.math.BigInteger;
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.Provider;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.Security;
+import java.security.SignatureException;
+import java.security.interfaces.RSAPublicKey;
+
+/**
+ *
+ * @author mike
+ * @version 1.0
+ */
+public class RsaJceProviderEngine implements JceProviderEngine {
+    private static final Provider PROVIDER = new com.rsa.jsafe.provider.JsafeJCE();
+    public static final String DEVICE = "Native/Java";
+
+    public RsaJceProviderEngine() {
+        Security.insertProviderAt(PROVIDER, 0);
+    }
+
+    private static class RsaCertificateRequest extends CertificateRequest {
+        private final com.rsa.certj.cert.PKCS10CertRequest csr;
+
+        private RsaCertificateRequest(com.rsa.certj.cert.PKCS10CertRequest csr) {
+            this.csr = csr;
+        }
+
+        public String getSubjectAsString() {
+            return csr.getSubjectName().toString();
+        }
+
+        public byte[] getEncoded() {
+            byte[] certificateRequest = new byte[csr.getDERLen(0)];
+            try {
+                csr.getDEREncoding (certificateRequest, 0, 0);
+            } catch (CertificateException e) {
+                throw new RuntimeException("Unable to encode certificate request", e);
+            }
+            return certificateRequest;
+        }
+
+        public PublicKey getPublicKey() throws InvalidKeyException, NoSuchProviderException, NoSuchAlgorithmException {
+            try {
+                return new RsaRsaPublicKey(csr.getSubjectPublicKey(DEVICE));
+            } catch (CertificateException e) {
+                throw new RuntimeException("Unable to get certificate request public key", e);
+            }
+        }
+    }
+
+    private static class RsaRsaPublicKey implements RSAPublicKey {
+        private JSAFE_PublicKey pk;
+
+        private RsaRsaPublicKey(JSAFE_PublicKey pk) {
+            this.pk = pk;
+        }
+
+        public String getAlgorithm() {
+            return "RSA";
+        }
+
+        public String getFormat() {
+            return "X.509";
+        }
+
+        public byte[] getEncoded() {
+            try {
+                byte[][] data = pk.getKeyData("RSAPublicKeyBER");
+                return data[0];
+            } catch (JSAFE_UnimplementedException e) {
+                throw new RuntimeException("Unable to get public key as RSA subjectPublicKeyInfo", e);
+            }
+        }
+
+        public BigInteger getModulus() {
+            try {
+                byte[][] data = pk.getKeyData("RSAPublicKey");
+                return new BigInteger(1, data[0]);
+            } catch (JSAFE_UnimplementedException e) {
+                throw new RuntimeException("Unable to get public key modulus", e);
+            }
+        }
+
+        public BigInteger getPublicExponent() {
+            try {
+                byte[][] data = pk.getKeyData("RSAPublicKey");
+                return new BigInteger(1, data[1]);
+            } catch (JSAFE_UnimplementedException e) {
+                throw new RuntimeException("Unable to get public key public exponent", e);
+            }
+        }
+
+        public String toString() {
+            byte[][] data;
+            try {
+                data = pk.getKeyData("RSAPublicKey");
+            } catch (JSAFE_UnimplementedException e) {
+                throw new RuntimeException("Unable to get RSA public key data", e);
+            }
+
+            return "RsaJceProviderEngine RSA public key: \n" +
+                    "  public exponent:\n" +
+                    "    " + HexUtils.hexDump(data[1]) +
+                    "\n  modulus:\n" +
+                    "    " + HexUtils.hexDump(data[0]);
+        }
+    }
+
+    private static class CausedSignatureException extends SignatureException {
+        CausedSignatureException(Throwable t) {
+            super();
+            initCause(t);
+        }
+    }
+
+    /**
+     * Get the Provider.
+     * @return the JCE Provider
+     */
+    public Provider getProvider() {
+        return PROVIDER;
+    }
+
+    /**
+     * Create an RsaSignerEngine that uses the current crypto API.
+     *
+     * @param keyStorePath
+     * @param storePass
+     * @param privateKeyAlias
+     * @param privateKeyPass
+     * @return
+     */
+    public RsaSignerEngine createRsaSignerEngine(String keyStorePath, String storePass, String privateKeyAlias, String privateKeyPass) {
+        return new RsaRsaSignerEngine(keyStorePath, storePass, privateKeyAlias, privateKeyPass);
+    }
+
+    /**
+     * Generate an RSA public key / private key pair using the current Crypto provider.
+     *
+     * @return
+     */
+    public KeyPair generateRsaKeyPair() {
+        KeyPairGenerator kpg;
+        try {
+            kpg = KeyPairGenerator.getInstance("RSA");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e); // can't happen
+        }
+
+        kpg.initialize(RSA_KEY_LENGTH);
+        return kpg.genKeyPair();
+    }
+
+    /**
+     * Generate a CertificateRequest using the current Crypto provider.
+     *
+     * @param username  the username, ie "lyonsm"
+     * @param keyPair  the public and private keys
+     * @return
+     */
+    public CertificateRequest makeCsr(String username, KeyPair keyPair) throws InvalidKeyException, SignatureException
+    {
+
+        X500Name subject = null;
+        try {
+            subject = new X500Name("CN=" + username);
+        } catch (com.rsa.certj.cert.NameException e) {
+            throw new RuntimeException(e); // can happen if username is no good
+        }
+
+        PKCS10CertRequest csr = new PKCS10CertRequest();
+        try {
+            csr.setSubjectName(subject);
+        } catch (CertificateException e) {
+            throw new RuntimeException(e);  // might happen if username is no good
+        }
+
+        try {
+            csr.setSubjectPublicKey(keyPair.getPublic().getEncoded(), 0);
+        } catch (CertificateException e) {
+            throw new RuntimeException(e); // might happen if BER encoding is no good
+        }
+
+
+        JSAFE_PrivateKey privateKey = null;
+        try {
+            privateKey = JSAFE_PrivateKey.getInstance(keyPair.getPrivate().getEncoded(), 0, "Native/Java");
+            csr.signCertRequest("SHA1/RSA/PKCS1Block01Pad", "Java", privateKey, new SecureRandom());
+        } catch (JSAFE_UnimplementedException e) {
+            throw new RuntimeException(e); // can't heppen
+        } catch (CertificateException e) {
+            throw new CausedSignatureException(e); // shouldn't happen
+        }
+
+        return new RsaCertificateRequest(csr);
+    }
+}
---
+++ src/com/l7tech/common/security/prov/rsa/RsaRsaSignerEngine.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright (C) 2003 Layer 7 Technologies Inc.
+ *
+ * $Id$
+ */
+
+package com.l7tech.common.security.prov.rsa;
+
+import com.l7tech.common.security.RsaSignerEngine;
+import com.l7tech.logging.LogManager;
+import com.rsa.certj.cert.PKCS10CertRequest;
+import com.rsa.certj.cert.X500Name;
+import com.rsa.jsafe.JSAFE_PrivateKey;
+import sun.security.x509.X509CertImpl;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.SecureRandom;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author mike
+ * @version 1.0
+ */
+public class RsaRsaSignerEngine implements RsaSignerEngine {
+    private String keyStorePath;
+    private String storePass;
+    private String privateKeyPassString;
+    private String privateKeyAlias;
+    private PrivateKey privateKey;
+    private X509Certificate caCert;
+    private SecureRandom random;
+
+    public RsaRsaSignerEngine(String keyStorePath, String storePass, String privateKeyAlias, String privateKeyPass) {
+        this.keyStorePath = keyStorePath;
+        this.storePass = storePass;
+        this.privateKeyAlias = privateKeyAlias;
+        this.privateKeyPassString = privateKeyPass;
+
+        try {
+            init();
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, e.getMessage(), e);
+            throw new IllegalArgumentException(e.getMessage());
+        }
+    }
+
+    private void init() throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException, UnrecoverableKeyException {
+        KeyStore keyStore = KeyStore.getInstance("JKS");
+        InputStream is = new FileInputStream(keyStorePath);
+
+        if (storePass == null)
+            throw new IllegalArgumentException("A CA keystore passphrase must be provided");
+        char[] keyStorePass = storePass.toCharArray();
+        keyStore.load(is, keyStorePass);
+
+        if (privateKeyPassString == null)
+            throw new IllegalArgumentException("A CA private key passphrase must be provided");
+        char[] privateKeyPass = privateKeyPassString.toCharArray();
+
+        privateKey = (PrivateKey) keyStore.getKey(privateKeyAlias, privateKeyPass);
+        if (privateKey == null) {
+            logger.severe("Cannot load key with alias '" + privateKeyAlias + "' from keystore '" + keyStorePath + "'");
+            throw new IllegalArgumentException("Cannot load key with alias '" + privateKeyAlias + "' from keystore '" + keyStorePath + "'");
+        }
+        Certificate[] certchain = keyStore.getCertificateChain(privateKeyAlias);
+        if (certchain.length < 1) {
+            logger.severe("Cannot load certificate chain with alias '" + privateKeyAlias + "' from keystore '" + keyStorePath + "'");
+            throw new IllegalArgumentException("Cannot load certificate chain with alias '" + privateKeyAlias + "' from keystore '" + keyStorePath + "'");
+        }
+        // We only support a ca hierarchy with depth 2.
+        caCert = (X509Certificate) certchain[0];
+
+        // Use this random ONLY for serial numbers
+        random = new SecureRandom();
+        long seed = (new Date().getTime()) + this.hashCode();
+        random.setSeed(seed);
+    }
+
+    /**
+     * Create a certificate from the given PKCS10 Certificate Request.
+     *
+     * @param pkcs10req  the PKCS10 certificate signing request, expressed in binary form.
+     * @return a signed X509 client certificate
+     * @throws Exception if something bad happens
+     */
+    public Certificate createCertificate(byte[] pkcs10req) throws Exception {
+        PKCS10CertRequest csr = new PKCS10CertRequest(pkcs10req, 0, 0);
+        JSAFE_PrivateKey caKey = JSAFE_PrivateKey.getInstance(privateKey.getEncoded(), 0, "Native/Java");
+        com.rsa.certj.cert.X509Certificate cert = new com.rsa.certj.cert.X509Certificate();
+        cert.setVersion(com.rsa.certj.cert.X509Certificate.X509_VERSION_3);
+
+        byte[] serialBytes = new byte[8];
+        random.nextBytes(serialBytes);
+        cert.setSerialNumber(serialBytes, 0, serialBytes.length);
+
+        GregorianCalendar cal = new GregorianCalendar();
+        Date notBefore = cal.getTime();
+        cal.add(Calendar.DAY_OF_MONTH,  CERT_DAYS_VALID);
+        Date notAfter = cal.getTime();
+        cert.setValidity(notBefore, notAfter);
+
+        cert.setIssuerName(new X500Name(caCert.getSubjectDN().getName().toString()));
+        cert.setSubjectName(csr.getSubjectName());
+        cert.setSubjectPublicKey(csr.getSubjectPublicKey("Native/Java"));
+
+        cert.signCertificate ("SHA1/RSA/PKCS1Block01Pad", "Java", caKey, new SecureRandom());
+        byte[] encoded = new byte[cert.getDERLen(0)];
+        cert.getDEREncoding(encoded, 0, encoded.length);
+        return new X509CertImpl(encoded);
+    }
+
+    private Logger logger = LogManager.getInstance().getSystemLogger();
+}
