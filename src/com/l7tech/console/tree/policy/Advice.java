f3629eeaf1b7ef5e012d986fe024ae5e397349b6
emil <emil@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-12-16 18:57:13 +0000

    Mike was here
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@4389 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/console/tree/policy/Advice.java
+++ src/com/l7tech/console/tree/policy/Advice.java
@@ -5,28 +5,30 @@ package com.l7tech.console.tree.policy;
  * to the next advice in the chain (or the policy if the end of the chain
  * has been reached) by calling the <code>PolicyChange.proceed()</code>
  * method.
- * Implementations may modify argument values, throw or catch exceptions, 
+ * Implementations may modify argument values, throw or catch exceptions,
  * etc. The following is an example of a debugging
  * advice that prints the policy change:
- * 
+ * <p/>
  * <pre>
  * public class DebuggingAdvice implements Advice {
- * 
+ * <p/>
  *     public void proceed(PolicyChange pc)
  *             throws Throwable {
  *         System.out.println(pc);
  *         pc.proceed();
  *     }
- * 
+ * <p/>
  * }
  * </pre>
+ * 
  * @author <a href="mailto:emarceta@layer7tech.com>Emil Marceta</a>
  * @version 1.0
  */
 public interface Advice {
     /**
      * Intercepts a policy change.
+     * 
      * @param pc The policy change.
      */
-  public void proceed(PolicyChange pc) throws PolicyException;
+    void proceed(PolicyChange pc) throws PolicyException;
 }
