18093d37b7c2a71dafc4b7aafcc8db832f4017dc
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-12-16 01:37:51 +0000

    Factored non-Gui-specific code out of CertificatePanel
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@4382 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/common/gui/widgets/CertificatePanel.java
+++ src/com/l7tech/common/gui/widgets/CertificatePanel.java
@@ -6,17 +6,14 @@
 
 package com.l7tech.common.gui.widgets;
 
+import com.l7tech.common.util.CertUtils;
+
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.security.PublicKey;
 import java.security.cert.CertificateEncodingException;
 import java.security.cert.X509Certificate;
-import java.security.interfaces.DSAParams;
-import java.security.interfaces.DSAPublicKey;
-import java.security.interfaces.RSAPublicKey;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -72,7 +69,7 @@ public class CertificatePanel extends JPanel {
     }
 
     private void loadCertificateInfo() throws CertificateEncodingException, NoSuchAlgorithmException {
-        ArrayList props = getCertProperties();
+        ArrayList props = CertUtils.getCertProperties(cert);
         int y = 0;
         cp.removeAll();
         for (Iterator i = props.iterator(); i.hasNext();) {
@@ -95,79 +92,6 @@ public class CertificatePanel extends JPanel {
         invalidate();
     }
 
-    /** Returns a properties instance filled out with info about the certificate. */
-    private ArrayList getCertProperties()
-      throws CertificateEncodingException, NoSuchAlgorithmException {
-        ArrayList l = new ArrayList();
-        if (cert == null) return l;
-
-        // l.add(new String[]{"Revocation date", new Date().toString()});
-        l.add(new String[]{"Creation date", nullNa(cert.getNotBefore())});
-        l.add(new String[]{"Expiry date", nullNa(cert.getNotAfter())});
-        l.add(new String[]{"Issued to", nullNa(cert.getSubjectDN())});
-        l.add(new String[]{"Serial number", nullNa(cert.getSerialNumber())});
-        l.add(new String[]{"Issuer", nullNa(cert.getIssuerDN())});
-
-        l.add(new String[]{"SHA-1 fingerprint", getCertificateFingerprint(cert, "SHA1")});
-        l.add(new String[]{"MD5 fingerprint", getCertificateFingerprint(cert, "MD5")});
-
-        PublicKey publicKey = cert.getPublicKey();
-        l.add(new String[]{"Key type", nullNa(publicKey.getAlgorithm())});
-
-        if (publicKey != null && publicKey instanceof RSAPublicKey) {
-            RSAPublicKey rsaKey = (RSAPublicKey) publicKey;
-            String modulus = rsaKey.getModulus().toString(16);
-            l.add(new String[]{"RSA strength", (modulus.length() * 4) + " bits"});
-            //l.add(new String[]{"RSA modulus", modulus});
-            l.add(new String[]{"RSA public exponent", rsaKey.getPublicExponent().toString(16)});
-        } else if (publicKey != null && publicKey instanceof DSAPublicKey) {
-            DSAPublicKey dsaKey = (DSAPublicKey) publicKey;
-            DSAParams params = dsaKey.getParams();
-            l.add(new String[]{"DSA prime (P)", params.getP().toString(16)});
-            l.add(new String[]{"DSA subprime (P)", params.getQ().toString(16)});
-            l.add(new String[]{"DSA base (P)", params.getG().toString(16)});
-        }
-
-        return l;
-    }
-
-    /**
-     * The method creates the fingerprint and returns it in a
-     * String to the caller.
-     *
-     * @param cert      the certificate
-     * @param algorithm the alghorithm (MD5 or SHA1)
-     * @return the certificate fingerprint as a String
-     * @exception java.security.NoSuchAlgorithmException
-     *                      if the algorithm is not available.
-     * @exception java.security.cert.CertificateEncodingException
-     *                      thrown whenever an error occurs while attempting to
-     *                      encode a certificate.
-     */
-    private String getCertificateFingerprint(X509Certificate cert, String algorithm)
-      throws NoSuchAlgorithmException, CertificateEncodingException {
-        if (cert == null) {
-            throw new NullPointerException("cert");
-        }
-        StringBuffer buff = new StringBuffer();
-        byte[] fingers = cert.getEncoded();
-
-        MessageDigest md = MessageDigest.getInstance(algorithm);
-        md.update(fingers);
-        byte[] digest = md.digest();
-        // the algorithm
-        buff.append(algorithm + ":");
-
-        for (int i = 0; i < digest.length; i++) {
-            if (i != 0) buff.append(":");
-            int b = digest[i] & 0xff;
-            String hex = Integer.toHexString(b);
-            if (hex.length() == 1) buff.append("0");
-            buff.append(hex.toUpperCase());
-        }
-        return buff.toString();
-    }
-
 
     /** Update the certificate information. */
     public void setCertificate(X509Certificate newCert) throws CertificateEncodingException, NoSuchAlgorithmException {
@@ -175,8 +99,4 @@ public class CertificatePanel extends JPanel {
         loadCertificateInfo();
     }
 
-    /** Convert a null object into "N/A", otherwise toString */
-    private String nullNa(Object o) {
-        return o == null ? "N/A" : o.toString();
-    }
 }
