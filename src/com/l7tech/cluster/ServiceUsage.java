073c04f3502ac841031061a1c928b7098cf21d53
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-12-22 22:02:40 +0000

    Bean representation of a row in the service_usage table. This table is used to record usage statistics for a particular service on a particular node.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@4447 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/cluster/ServiceUsage.java
@@ -0,0 +1,108 @@
+package com.l7tech.cluster;
+
+import java.io.Serializable;
+
+/**
+ * Bean representation of a row in the service_usage table.
+ *
+ * This table is used to record usage statistics for a particular service on a particular node.
+ *
+ * <br/><br/>
+ * LAYER 7 TECHNOLOGIES, INC<br/>
+ * User: flascell<br/>
+ * Date: Dec 22, 2003<br/>
+ * $Id$<br/>
+ *
+ */
+public class ServiceUsage implements Serializable {
+
+    /**
+     * id of the service this stat is applicable to
+     */
+    public long getServiceid() {
+        return serviceid;
+    }
+
+    /**
+     * id of the service this stat is applicable to
+     */
+    public void setServiceid(long serviceid) {
+        this.serviceid = serviceid;
+    }
+
+    /**
+     * id of the node this stat is applicable to
+     */
+    public String getNodeid() {
+        return nodeid;
+    }
+
+    /**
+     * id of the node this stat is applicable to
+     */
+    public void setNodeid(String nodeid) {
+        this.nodeid = nodeid;
+    }
+
+    /**
+     * the total number of requests for this service handled for this service
+     */
+    public long getRequests() {
+        return requests;
+    }
+
+    /**
+     * the total number of requests for this service handled for this service
+     */
+    public void setRequests(long requests) {
+        this.requests = requests;
+    }
+
+    /**
+     * the number of requests that were not subject to an authorization failure
+     */
+    public long getAuthorized() {
+        return authorized;
+    }
+
+    /**
+     * the number of requests that were not subject to an authorization failure
+     */
+    public void setAuthorized(long authorized) {
+        this.authorized = authorized;
+    }
+
+    /**
+     * the number of requests that were completly processed by the ssg without any error
+     */
+    public long getCompleted() {
+        return completed;
+    }
+
+    /**
+     * the number of requests that were completly processed by the ssg without any error
+     */
+    public void setCompleted(long completed) {
+        this.completed = completed;
+    }
+
+    /**
+     * this must be overriden (hibernate requirement for composite id classes)
+     */
+    public boolean equals(Object obj) {
+        if (!(obj instanceof ServiceUsage)) return false;
+        ServiceUsage jghmx = (ServiceUsage)obj;
+        if (serviceid != jghmx.serviceid) return false;
+        if (!nodeid.equals(jghmx.nodeid)) return false;
+        if (requests != jghmx.requests) return false;
+        if (authorized != jghmx.authorized) return false;
+        if (completed != jghmx.completed) return false;
+        return true;
+    }
+
+    private long serviceid;
+    private String nodeid;
+    private long requests;
+    private long authorized;
+    private long completed;
+}
