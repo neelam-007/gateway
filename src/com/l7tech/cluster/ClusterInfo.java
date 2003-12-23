20bb02db5578410d9ed6d94740d23d40c2773a33
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2003-12-23 00:09:27 +0000

    javadoced all properties
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@4455 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/cluster/ClusterInfo.java
+++ src/com/l7tech/cluster/ClusterInfo.java
@@ -14,59 +14,101 @@ package com.l7tech.cluster;
  * 
  */
 public class ClusterInfo {
-    
+
+    /**
+     * mac address of the node that uniquely identifies this node in the cluster
+     */
     public String getMac() {
         return mac;
     }
 
+    /**
+     * mac address of the node that uniquely identifies this node in the cluster
+     */
     public void setMac(String mac) {
         this.mac = mac;
     }
 
+    /**
+     * direct ip address of this node
+     */
     public String getAddress() {
         return address;
     }
 
+    /**
+     * direct ip address of this node
+     */
     public void setAddress(String address) {
         this.address = address;
     }
 
+    /**
+     * whether or not this node has access to the CA key necessary for signing CSRs
+     */
     public boolean getIsMaster() {
         return isMaster;
     }
 
+    /**
+     * whether or not this node has access to the CA key necessary for signing CSRs
+     */
     public void setIsMaster(boolean master) {
         isMaster = master;
     }
 
+    /**
+     * the timestamp of when this node last booted
+     */
     public long getUptime() {
         return uptime;
     }
 
+    /**
+     * the timestamp of when this node last booted
+     */
     public void setUptime(long uptime) {
         this.uptime = uptime;
     }
 
+    /**
+     * the avg load of this node for the last minute
+     */
     public double getAvgLoad() {
         return avgLoad;
     }
 
+    /**
+     * the avg load of this node for the last minute
+     */
     public void setAvgLoad(double avgLoad) {
         this.avgLoad = avgLoad;
     }
 
+    /**
+     * the timestamp of when the avg load was last updated
+     */
     public long getLastUpdateTimeStamp() {
         return lastUpdateTimeStamp;
     }
 
+    /**
+     * the timestamp of when the avg load was last updated
+     */
     public void setLastUpdateTimeStamp(long lastUpdateTimeStamp) {
         this.lastUpdateTimeStamp = lastUpdateTimeStamp;
     }
 
+    /**
+     * name of the node (set at config time)
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * name of the node (set at config time)
+     */
     public void setName(String name) {
         this.name = name;
     }
