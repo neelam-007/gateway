d6cb53e1d1b15d812b2b39403231af1ddfc6c37e
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-01-05 23:15:49 +0000

    Add unimplemented ClientSamlSecurity
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@4517 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/proxy/policy/assertion/xmlsec/ClientSamlSecurity.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2003 Layer 7 Technologies Inc.
+ *
+ * $Id$
+ */
+
+package com.l7tech.proxy.policy.assertion.xmlsec;
+
+import com.l7tech.proxy.policy.assertion.UnimplementedClientAssertion;
+import com.l7tech.policy.assertion.xmlsec.SamlSecurity;
+
+/**
+ * Unimplemented on the client-side.
+ * @author mike
+ * @version 1.0
+ */
+public class ClientSamlSecurity extends UnimplementedClientAssertion {
+    public ClientSamlSecurity(SamlSecurity data) {
+    }
+}
