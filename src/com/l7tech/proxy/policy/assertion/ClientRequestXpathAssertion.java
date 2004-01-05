92f51f8e3340b7e8f06c88eedd7d06a2370f3afa
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-01-05 23:03:47 +0000

    Client should interpret XPath as a no-op until policy filters it out
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@4513 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/proxy/policy/assertion/ClientRequestXpathAssertion.java
+++ src/com/l7tech/proxy/policy/assertion/ClientRequestXpathAssertion.java
@@ -7,13 +7,44 @@
 package com.l7tech.proxy.policy.assertion;
 
 import com.l7tech.policy.assertion.RequestXpathAssertion;
+import com.l7tech.policy.assertion.AssertionStatus;
+import com.l7tech.proxy.datamodel.PendingRequest;
+import com.l7tech.proxy.datamodel.SsgResponse;
+import com.l7tech.proxy.datamodel.exceptions.*;
+
+import java.security.GeneralSecurityException;
+import java.io.IOException;
+
+import org.xml.sax.SAXException;
 
 /**
  * Client side support for RequestXpathAssertion.
  * @author mike
  * @version 1.0
  */
-public class ClientRequestXpathAssertion extends UnimplementedClientAssertion {
+public class ClientRequestXpathAssertion extends ClientAssertion {
     public ClientRequestXpathAssertion(RequestXpathAssertion requestXpathAssertion) {
+    }
+
+    public AssertionStatus decorateRequest(PendingRequest request)
+            throws BadCredentialsException, OperationCanceledException, GeneralSecurityException,
+            ClientCertificateException, IOException, SAXException, KeyStoreCorruptException, HttpChallengeRequiredException, PolicyRetryableException {
+        // No action required.
+        return AssertionStatus.NONE;
+    }
+
+    public AssertionStatus unDecorateReply(PendingRequest request, SsgResponse response)
+            throws BadCredentialsException, OperationCanceledException, GeneralSecurityException, IOException,
+            SAXException, ResponseValidationException, KeyStoreCorruptException {
+        // No action required.
+        return AssertionStatus.NONE;
+    }
+
+    public String getName() {
+        return "Server-enforced Xpath match";
+    }
+
+    public String iconResource(boolean open) {
+        return "com/l7tech/proxy/resources/tree/xmlsignature.gif";
     }
 }
