fe99d05bb015acbfab56f1b6cf7e58a0ebdea37f
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-01-06 23:21:07 +0000

    A servlet that sends status information for use by failover.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@4560 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/server/StatusPingServlet.java
@@ -0,0 +1,82 @@
+package com.l7tech.server;
+
+import com.l7tech.cluster.ClusterInfoManager;
+import com.l7tech.cluster.ClusterNodeInfo;
+import com.l7tech.objectmodel.FindException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collection;
+import java.util.Iterator;
+
+/**
+ * A servlet that sends status information for use by failover.
+ *
+ * This servlet returns an xml file that contains server status information. The
+ * failover code queries this servlet to assess whether the server is up and running
+ * ok.
+ *
+ * <br/><br/>
+ * LAYER 7 TECHNOLOGIES, INC<br/>
+ * User: flascell<br/>
+ * Date: Jan 6, 2004<br/>
+ * $Id$
+ * 
+ */
+public class StatusPingServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        Collection allStatuses = null;
+        try {
+            allStatuses = ClusterInfoManager.getInstance().retrieveClusterStatus();
+        } catch (FindException e) {
+            outputError(res, e.getMessage());
+        }
+        if (allStatuses == null || allStatuses.isEmpty()) {
+            outputError(res, "can't get server status");
+        }
+
+        outputStatuses(res, allStatuses);
+    }
+
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        doGet(req, res);
+    }
+
+    private void outputStatuses(HttpServletResponse res, Collection statuses) throws IOException {
+        res.setContentType("text/xml");
+        res.setStatus(HttpServletResponse.SC_OK);
+
+        PrintWriter out = res.getWriter();
+        out.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+        out.println("<status>");
+        for (Iterator i = statuses.iterator(); i.hasNext();) {
+            ClusterNodeInfo nodeInfo = (ClusterNodeInfo)i.next();
+            out.println("\t<server>");
+            out.println("\t\t<id>" + nodeInfo.getMac() + "</id>");
+            out.println("\t\t<address>" + nodeInfo.getAddress() + "</address>");
+            out.println("\t\t<name>" + nodeInfo.getName() + "</name>");
+            out.println("\t\t<uptime>" + nodeInfo.getUptime() + "</uptime>");
+            out.println("\t\t<avgload>" + nodeInfo.getAvgLoad() + "</avgload>");
+            out.println("\t\t<lastUpdated>" + nodeInfo.getLastUpdateTimeStamp() + "</lastUpdated>");
+            out.println("\t</server>");
+        }
+        out.println("</status>");
+        out.close();
+    }
+
+    private void outputError(HttpServletResponse res, String error) throws IOException {
+        res.setContentType("text/xml");
+        res.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        PrintWriter out = res.getWriter();
+        out.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+        out.println("<status>");
+        out.println("\t" + error);
+        out.println("</status>");
+        out.close();
+    }
+}
