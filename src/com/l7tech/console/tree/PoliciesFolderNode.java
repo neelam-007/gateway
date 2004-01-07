718e169a102e6b728a0766e4c32b691866468953
emil <emil@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-01-07 19:26:43 +0000

    Bugzilla 512, sorting in identity providers to be case-insensitive
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@4582 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/console/tree/PoliciesFolderNode.java
+++ src/com/l7tech/console/tree/PoliciesFolderNode.java
@@ -7,6 +7,10 @@ import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.logging.Level;
+import java.util.TreeSet;
+import java.util.Set;
+import java.util.Comparator;
+import java.util.Arrays;
 
 
 /**
@@ -52,16 +56,24 @@ public class PoliciesFolderNode extends AbstractTreeNode {
     protected void loadChildren() {
         try {
             File[] files = listPolicies();
+
+            Set sorted = new TreeSet(new Comparator() {
+                public int compare(Object o1, Object o2) {
+                    File f1 = (File)o1;
+                    File f2 = (File)o2;
+                    return f1.getName().compareTo(f2.getName());
+                }
+            });
+            sorted.addAll(Arrays.asList(files));
+            files = (File[])sorted.toArray(new File[] {});
             int index = 0;
             children = null;
-            for (int i = files.length - 1; i >= 0; i--) {
+            for (int i = 0; i < files.length; i++) {
                 File file = files[i];
                 insert((MutableTreeNode) new PolicyTemplateNode(file), index++);
             }
         } catch (IOException e) {
-            ErrorManager.
-              getDefault().
-              notify(Level.WARNING, e, "There was an error loading policy templates.");
+            ErrorManager.getDefault().notify(Level.WARNING, e, "There was an error loading policy templates.");
 
         }
     }
