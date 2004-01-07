df3f3bf1f3e103b751b7cabe5212007b10cb8ae2
emil <emil@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-01-07 01:16:32 +0000

    Bugzilla 427, Username sort order doesn't look correct in console
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@4575 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/objectmodel/EntityHeaderComparator.java
+++ src/com/l7tech/objectmodel/EntityHeaderComparator.java
@@ -17,6 +17,6 @@ public class EntityHeaderComparator implements Comparator {
         if (h1.getName() == null && h2.getName() == null) return 0;
         else if (h1.getName() == null) return 1;
         else if (h2.getName() == null) return -1;
-        else return h1.getName().compareTo(h2.getName());
+        else return h1.getName().compareToIgnoreCase(h2.getName());
     }
 }
