ee37a9b3a2b6f802d94085fcd84dfb971c18865c
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-01-13 22:23:14 +0000

    tester for new class RequestId
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@4662 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ tests/com/l7tech/common/RequestIdTest.java
@@ -0,0 +1,40 @@
+package com.l7tech.common;
+
+import junit.framework.TestCase;
+
+/**
+ * [class_desc]
+ *
+ * <br/><br/>
+ * LAYER 7 TECHNOLOGIES, INC<br/>
+ * User: flascell<br/>
+ * Date: Jan 13, 2004<br/>
+ * $Id$<br/>
+ *
+ */
+public class RequestIdTest extends TestCase {
+    public void testParsing() {
+        try {
+            RequestId redid1 = new RequestId(123, 456);
+            RequestId redid2 = new RequestId(redid1.toString());
+            assertTrue(redid1.equals(redid2));
+            redid1 = new RequestId(0, 0);
+            redid2 = new RequestId(redid1.toString());
+            assertTrue(redid1.equals(redid2));
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+            assertTrue(e.getMessage(), false);
+        }
+    }
+
+    public void testExtremeParsing() {
+        try {
+            RequestId redid1 = new RequestId(Long.MIN_VALUE, Long.MAX_VALUE);
+            RequestId redid2 = new RequestId(redid1.toString());
+            assertTrue(redid1.equals(redid2));
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+            assertTrue(e.getMessage(), false);
+        }
+    }
+}
