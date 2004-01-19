f1c46a65ea76618bb53387b04c80ae1d1f6e63c8
emil <emil@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-01-19 16:35:37 +0000

    Initial revision
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@4733 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/policy/assertion/xmlsec/ElementSecurity.java
@@ -0,0 +1,153 @@
+package com.l7tech.policy.assertion.xmlsec;
+
+/**
+ * Class <code>ElementSecurity</code> contains the security properties
+ * for an XML document portion (document element).
+ * It contains properties such as:
+ * <ul>
+ * <li>XPath expression (may be null)
+ * <li>required, indicating wheter the security element is required or not
+ * <li> the encryption toggle, whether the document is signed only or signed and encrypted
+ * <li>the encryption cypher name
+ * <li>the encryption key length
+ * </ul>
+ *
+ * @author <a href="mailto:emarceta@layer7-tech.com">Emil Marceta</a>
+ * @version 1.0
+ */
+public class ElementSecurity {
+    /**
+     * The default cipher to use when encryption is enabled.
+     */
+    public static final String DEFAULT_CIPHER = "AES";
+
+    /**
+     * The default key length in bits to use when encryption is enabled.
+     */
+    public static final int DEFAULT_KEYBITS = 128;
+
+    private String xpathExpression;
+    private boolean required;
+    private boolean encryption;
+    private String cipher = DEFAULT_CIPHER;
+    private int keyLength = DEFAULT_KEYBITS;
+
+    /**
+     * Default constructor. mainly for xml serialization
+     */
+    public ElementSecurity() {
+    }
+
+    /**
+     * Full constructor, instantiate the instances with all properties.
+     *
+     * @param encryption      whether the element is encrypted or signed only
+     * @param cipher          the cipher name for encryption, may not be null if encryption
+     *                        is requested
+     * @param keyLength       the key length, required if the encyption is requested
+     * @param xpathExpression the xpath expression that is evaluated in the context
+     *                        may be null
+     */
+    public ElementSecurity(boolean encryption, String cipher, int keyLength, String xpathExpression) {
+        this.encryption = encryption;
+        this.cipher = cipher;
+        this.keyLength = keyLength;
+        this.xpathExpression = xpathExpression;
+    }
+
+    /**
+     * The xpath expression that is
+     *
+     * @return the xpath expression, may be null
+     */
+    public String getXpathExpression() {
+        return xpathExpression;
+    }
+
+    /**
+     * Set the xpath expression
+     *
+     * @param xpathExpression the xpath expression value (or null)
+     */
+    public void setXpathExpression(String xpathExpression) {
+        this.xpathExpression = xpathExpression;
+    }
+
+    /**
+     * Return the required value. This is context dependent property that
+     * typically indicates whether the security element is required or not.
+     *
+     * @return true if required, false otherwise
+     */
+    public boolean isRequired() {
+        return required;
+    }
+
+    /**
+     * Set the required property for this security element.
+     *
+     * @param required the boolean required value
+     */
+    public void setRequired(boolean required) {
+        this.required = required;
+    }
+
+    /**
+     * This property describes whether or not the encryption is set on the assertion.
+     * The exact meaning of the encryption depends of the implementing class.
+     * <p/>
+     * In the body should be encrypted as opposed to only signed
+     *
+     * @return whether the encryption is used or not
+     */
+    public boolean isEncryption() {
+        return encryption;
+    }
+
+    /**
+     * Enable or disable the encryption on the assertion. The exact meaning of the
+     * encryption flag depends on the implementing class.
+     *
+     * @param b toggle the encryption on the assertion
+     */
+    public void setEncryption(boolean b) {
+        encryption = b;
+    }
+
+    /**
+     * Set the cipher to use when encryption is enabled.
+     *
+     * @param cipherName the cipher to use, ie "AES"
+     */
+    public void setCipher(String cipherName) {
+        this.cipher = cipherName;
+    }
+
+    /**
+     * Get the cipher to use when encryption is enabled.
+     *
+     * @return the cipher to use, ie "AES"
+     */
+    public String getCipher() {
+        return cipher;
+    }
+
+    /**
+     * Set the symmetric key length to use when encryption is enabled.
+     *
+     * @param keyBits the size of the key in bits, ie 128
+     */
+    public void setKeyLength(int keyBits) {
+        this.keyLength = keyBits;
+    }
+
+    /**
+     * Get the symmetric key length to use when encryption is enabled.
+     *
+     * @return the size of the key in bits, ie 128
+     */
+    public int getKeyLength() {
+        return keyLength;
+    }
+
+}
