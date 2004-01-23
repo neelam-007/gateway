57a6b055b02d519ad34784fc76f61aa6894dac7e
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-01-23 00:21:13 +0000

    implemented new ldap group manager
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@4811 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/identity/ldap/LdapGroupManager.java
+++ src/com/l7tech/identity/ldap/LdapGroupManager.java
@@ -1,13 +1,16 @@
 package com.l7tech.identity.ldap;
 
-import com.l7tech.identity.GroupManager;
 import com.l7tech.identity.Group;
+import com.l7tech.identity.GroupManager;
 import com.l7tech.identity.User;
-import com.l7tech.objectmodel.*;
 import com.l7tech.logging.LogManager;
+import com.l7tech.objectmodel.*;
 
-import java.util.Set;
-import java.util.Collection;
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.directory.*;
+import java.util.*;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -26,99 +29,301 @@ public class LdapGroupManager implements GroupManager {
         this.parent = daddy;
     }
 
-    public Group findByPrimaryKey(String identifier) throws FindException {
-        return null;
+    public Group findByPrimaryKey(String dn) throws FindException {
+        DirContext context = null;
+        try {
+            try {
+                context = parent.getBrowseContext(cfg);
+                Attributes attributes = context.getAttributes(dn);
+
+                GroupMappingConfig[] groupTypes = cfg.getGroupMappings();
+                Attribute objectclasses = attributes.get("objectclass");
+                for (int i = 0; i < groupTypes.length; i ++) {
+                    String grpclass = groupTypes[i].getObjClass();
+                    if (LdapIdentityProvider.attrContainsCaseIndependent(objectclasses, grpclass)) {
+                        LdapGroup out = new LdapGroup();
+                        out.setProviderId(cfg.getOid());
+                        out.setDn(dn);
+                        Object tmp = LdapIdentityProvider.extractOneAttributeValue(attributes,
+                                       groupTypes[i].getNameAttrName());
+                        if (tmp != null) out.setCn(tmp.toString());
+                        return out;
+                    }
+                }
+                return null;
+            } finally {
+                if ( context != null ) context.close();
+            }
+        } catch (NamingException e) {
+            logger.log(Level.WARNING, "error building group", e);
+            throw new FindException("naming exception", e);
+        }
     }
 
     public Group findByName(String name) throws FindException {
-        return null;
+        Collection res = parent.search(new EntityType[] {EntityType.GROUP}, name);
+        switch (res.size()) {
+            case 0:
+                logger.finest("no group found with name " + name);
+                return null;
+            case 1:
+                EntityHeader header = (EntityHeader)res.iterator().next();
+                return findByPrimaryKey(header.getStrId());
+            default:
+                // return 1st one
+                logger.warning("search on group name returned more than one " +
+                               "result. returning 1st one");
+                header = (EntityHeader)res.iterator().next();
+                return findByPrimaryKey(header.getStrId());
+        }
     }
 
     public void delete(Group group) throws DeleteException, ObjectNotFoundException {
+        throw new UnsupportedOperationException();
     }
 
     public void delete(String identifier) throws DeleteException, ObjectNotFoundException {
+        throw new UnsupportedOperationException();
     }
 
     public String save(Group group) throws SaveException {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     public void update(Group group) throws UpdateException, ObjectNotFoundException {
+        throw new UnsupportedOperationException();
     }
 
     public String save(Group group, Set userHeaders) throws SaveException {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     public void update(Group group, Set userHeaders) throws UpdateException, ObjectNotFoundException {
+        throw new UnsupportedOperationException();
     }
 
     public boolean isMember(User user, Group group) throws FindException {
+        Set userHeaders = getUserHeaders( group );
+        for (Iterator i = userHeaders.iterator(); i.hasNext();) {
+            EntityHeader header = (EntityHeader) i.next();
+            String login = header.getName();
+            if ( login != null && login.equals( user.getLogin() ) ) return true;
+        }
         return false;
     }
 
     public void addUsers(Group group, Set users) throws FindException, UpdateException {
+        throw new UnsupportedOperationException();
     }
 
     public void removeUsers(Group group, Set users) throws FindException, UpdateException {
+        throw new UnsupportedOperationException();
     }
 
     public void addUser(User user, Set groups) throws FindException, UpdateException {
+        throw new UnsupportedOperationException();
     }
 
     public void removeUser(User user, Set groups) throws FindException, UpdateException {
+        throw new UnsupportedOperationException();
     }
 
     public void addUser(User user, Group group) throws FindException, UpdateException {
+        throw new UnsupportedOperationException();
     }
 
     public void removeUser(User user, Group group) throws FindException, UpdateException {
+        throw new UnsupportedOperationException();
     }
 
     public Set getGroupHeaders(User user) throws FindException {
-        return null;
+        Collection allgroups = findAll();
+        Set output = new HashSet();
+        for (Iterator i = allgroups.iterator(); i.hasNext();) {
+            Group agrp = (Group)i.next();
+            if (isMember(user, agrp)) {
+                output.add(new EntityHeader(agrp.getUniqueIdentifier(), EntityType.GROUP, agrp.getName(), agrp.getDescription()));
+            }
+        }
+        return output;
     }
 
     public Set getGroupHeaders(String userId) throws FindException {
-        return null;
+        return getGroupHeaders( getUserManager().findByPrimaryKey( userId ) );
     }
 
     public void setGroupHeaders(User user, Set groupHeaders) throws FindException, UpdateException {
+        throw new UnsupportedOperationException();
     }
 
     public void setGroupHeaders(String userId, Set groupHeaders) throws FindException, UpdateException {
+        throw new UnsupportedOperationException();
     }
 
     public Set getUserHeaders(Group group) throws FindException {
-        return null;
+        NamingEnumeration answer = null;
+        DirContext context = null;
+
+        try {
+            Set headers = new HashSet();
+
+            LdapGroup groupImp = (LdapGroup)group;
+            String dn = groupImp.getDn();
+            context = parent.getBrowseContext(cfg);
+            Attributes attributes = context.getAttributes(dn);
+
+            GroupMappingConfig[] groupTypes = cfg.getGroupMappings();
+            Attribute objectclasses = attributes.get("objectclass");
+            for (int i = 0; i < groupTypes.length; i ++) {
+                String grpclass = groupTypes[i].getObjClass();
+                if (LdapIdentityProvider.attrContainsCaseIndependent(objectclasses, grpclass)) {
+                    if (groupTypes[i].getMemberStrategy().equals(MemberStrategy.MEMBERS_BY_OU)) {
+                        collectOUGroupMembers(context, dn, headers);
+                    } else if (groupTypes[i].getMemberStrategy().equals(MemberStrategy.MEMBERS_ARE_LOGIN)) {
+                        Attribute memberAttribute = attributes.get(groupTypes[i].getMemberAttrName());
+                        if (memberAttribute != null) {
+                            for (int ii = 0; ii < memberAttribute.size(); ii++) {
+                                Object val = memberAttribute.get(ii);
+                                if (val != null) {
+                                    String memberlogin = val.toString();
+                                    User u = getUserManager().findByLogin(memberlogin);
+                                    if (u != null) {
+                                        headers.add(new EntityHeader(u.getName(), EntityType.USER, u.getLogin(), null));
+                                    }
+                                }
+                            }
+                        }
+                    } else {
+                        Attribute memberAttribute = attributes.get(groupTypes[i].getMemberAttrName());
+                        if (memberAttribute != null) {
+                            for (int ii = 0; ii < memberAttribute.size(); ii++) {
+                                Object val = memberAttribute.get(ii);
+                                if (val != null) {
+                                    // nv pairs or dn. which is it?
+                                    String memberhint = val.toString();
+                                    // try to get user through dn
+                                    boolean done = false;
+                                    try {
+                                        User u = getUserManager().findByPrimaryKey(memberhint);
+                                        if (u != null) {
+                                            headers.add(new EntityHeader(u.getName(), EntityType.USER, u.getLogin(), null));
+                                            done = true;
+                                        }
+                                    } catch (FindException e) {
+                                        logger.log(Level.FINEST, "cannot resolve user through dn, try nv" +
+                                                                    "pair method", e);
+                                    }
+                                    // if that dont work, try search assuming nv pair
+                                    if (!done) {
+                                        nvSearchForUser(context, memberhint, headers);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return headers;
+        } catch ( NamingException ne ) {
+            logger.log(Level.SEVERE, null, ne);
+            throw new FindException( ne.getMessage(), ne );
+        } finally {
+            try {
+                if ( answer != null ) answer.close();
+                if ( context != null ) context.close();
+            } catch ( NamingException ne ) {
+                throw new FindException( ne.getMessage(), ne );
+            }
+        }
     }
 
     public Set getUserHeaders(String groupId) throws FindException {
-        return null;
+        return getUserHeaders( findByPrimaryKey( groupId ) );
     }
 
     public void setUserHeaders(Group group, Set groupHeaders) throws FindException, UpdateException {
+        throw new UnsupportedOperationException();
     }
 
     public void setUserHeaders(String groupId, Set groupHeaders) throws FindException, UpdateException {
+        throw new UnsupportedOperationException();
     }
 
     public Collection findAllHeaders() throws FindException {
-        return null;
+        return parent.search(new EntityType[] {EntityType.GROUP}, "*");
     }
 
     public Collection findAllHeaders(int offset, int windowSize) throws FindException {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     public Collection findAll() throws FindException {
-        return null;
+        Collection headers = findAllHeaders();
+        Collection output = new ArrayList();
+        Iterator i = headers.iterator();
+        while (i.hasNext()) {
+            EntityHeader header = (EntityHeader)i.next();
+            output.add(findByPrimaryKey(header.getStrId()));
+        }
+        return output;
     }
 
     public Collection findAll(int offset, int windowSize) throws FindException {
-        return null;
+        Collection headers = findAllHeaders(offset, windowSize);
+        Collection output = new ArrayList();
+        Iterator i = headers.iterator();
+        while (i.hasNext()) {
+            EntityHeader header = (EntityHeader)i.next();
+            output.add(findByPrimaryKey(header.getStrId()));
+        }
+        return output;
     }
+
+    protected LdapUserManager getUserManager() {
+        return (LdapUserManager)parent.getUserManager();
+    }
+
+    private void collectOUGroupMembers( DirContext context, String dn, Set memberHeaders) throws NamingException {
+        // build group memberships
+        NamingEnumeration answer = null;
+        String filter = parent.userSearchFilterWithParam("*");
+        SearchControls sc = new SearchControls();
+        sc.setSearchScope(SearchControls.SUBTREE_SCOPE);
+        // use dn of group as base
+        answer = context.search(dn, filter, sc);
+        while (answer.hasMore()) {
+            String userdn = null;
+            SearchResult sr = (SearchResult)answer.next();
+            userdn = sr.getName() + "," + dn;
+            memberHeaders.add(parent.searchResultToHeader(sr, userdn));
+        }
+
+        if (answer != null) answer.close();
+    }
+
+    private void nvSearchForUser(DirContext context, String nvhint, Set memberHeaders) throws NamingException {
+        StringBuffer filter = new StringBuffer("(|");
+        UserMappingConfig[] userTypes = cfg.getUserMappings();
+        for (int i = 0; i < userTypes.length; i++) {
+            filter.append("(&" +
+                            "(objectClass=" + userTypes[i].getObjClass() + ")" +
+                            "(" + nvhint+ ")" +
+                          ")");
+        }
+        filter.append(")");
+
+        SearchControls sc = new SearchControls();
+        sc.setSearchScope(SearchControls.SUBTREE_SCOPE);
+        NamingEnumeration answer = null;
+        answer = context.search(cfg.getSearchBase(), filter.toString(), sc);
+        while (answer.hasMore()) {
+            String userdn = null;
+            SearchResult sr = (SearchResult)answer.next();
+            userdn = sr.getName() + "," + cfg.getSearchBase();
+            memberHeaders.add(parent.searchResultToHeader(sr, userdn));
+        }
+    }
+
 
     private LdapIdentityProviderConfig cfg;
     private final Logger logger = LogManager.getInstance().getSystemLogger();
