7f1b85911b13e5c03b561e855ad8791ab1d52892
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-01-23 00:17:25 +0000

    load templates from template files instead of hardcoded templates
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@4806 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/identity/ldap/LdapConfigTemplateManager.java
+++ src/com/l7tech/identity/ldap/LdapConfigTemplateManager.java
@@ -1,16 +1,21 @@
 package com.l7tech.identity.ldap;
 
+import com.l7tech.server.ServerConfig;
+import com.l7tech.logging.LogManager;
+import com.l7tech.common.util.HexUtils;
+
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Set;
-import java.io.IOException;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+import java.io.*;
 
 /**
  * Provides partially configured LdapIdenityProviderConfig to simplify the addition of a new connector.
  *
  * Once the template is chosen, it can be used to create a new config object.
  *
- * Todo, read and persist those templates to database, allow to add new templates
  *
  * <br/><br/>
  * LAYER 7 TECHNOLOGIES, INC<br/>
@@ -22,7 +27,8 @@ import java.io.IOException;
 public class LdapConfigTemplateManager {
 
     public LdapConfigTemplateManager() {
-        buildHardCodedTemplates();
+        populateTemplatesFromFile();
+        // buildHardCodedTemplates();
     }
 
     /**
@@ -40,14 +46,58 @@ public class LdapConfigTemplateManager {
     }
 
     /**
-     * get a template, given its name
+     * get a template, given its name. the actual template is not returned but a new config object
+     * based on this template.
      * @return the config object if found, null if not found
      */
-    public final LdapIdentityProviderConfig getTemplate(String templateName) throws IOException {
+    public LdapIdentityProviderConfig getTemplate(String templateName) throws IOException {
+        // clone the template, so it's not affected by whatever the user is doing with it
         return new LdapIdentityProviderConfig((LdapIdentityProviderConfig)templates.get(templateName));
     }
 
-    private void buildHardCodedTemplates() {
+    private void populateTemplatesFromFile() {
+        String rootPath = ServerConfig.getInstance().getLdapTemplatesPath();
+        File rootFile = new File(rootPath);
+        if (!rootFile.exists()) {
+            logger.warning("templates not available!");
+            return;
+        }
+        String[] output = rootFile.list(new FilenameFilter() {
+            public boolean accept(File dir, String name) {
+                int length = name.length();
+                if (length < 4) return false;
+                String extension = name.substring(length-4, length);
+                if (!extension.equals(".xml")) return false;
+                return true;
+            }
+        });
+        for (int i = 0; i < output.length; i++) {
+            LdapIdentityProviderConfig template = new LdapIdentityProviderConfig();
+            template.setName(output[i].substring(0, output[i].length()-4));
+            String fulltemplatefilename = rootPath + File.separatorChar + output[i];
+            FileInputStream fis = null;
+            try {
+                fis = new FileInputStream(fulltemplatefilename);
+            } catch (FileNotFoundException e) {
+                logger.log(Level.WARNING, "could not open file " + fulltemplatefilename, e);
+                fis = null;
+            }
+            if (fis != null) {
+                String properties = null;
+                try {
+                    byte[] data = HexUtils.slurpStream(fis, 32768);
+                    properties = new String(data);
+                } catch (IOException e) {
+                    logger.log(Level.WARNING, "cannot slurp file " + fulltemplatefilename, e);
+                }
+                template.setSerializedProps(properties);
+                templates.put(template.getName(), template);
+                logger.finest("added template " + template.getName());
+            }
+        }
+    }
+
+    /*private void buildHardCodedTemplates() {
         // build the standard LDAP template
         LdapIdentityProviderConfig template = new LdapIdentityProviderConfig();
         template.setName("Standard LDAP Template");
@@ -63,32 +113,34 @@ public class LdapConfigTemplateManager {
         usrMap.setNameAttrName("cn");
         usrMap.setObjClass("inetOrgPerson");
         usrMap.setPasswdAttrName("userPassword");
-        usrMap.setPasswdType(UserMappingConfig.CLEAR);
+        usrMap.setPasswdType(PasswdStrategy.CLEAR);
         template.setUserMapping(usrMap);
         // add the posixGroup
         GroupMappingConfig grpConfig = new GroupMappingConfig();
         grpConfig.setMemberAttrName("memberUid");
-        grpConfig.setMemberStrategy(GroupMappingConfig.MEMBERS_ARE_LOGIN);
+        grpConfig.setMemberStrategy(MemberStrategy.MEMBERS_ARE_LOGIN);
         grpConfig.setNameAttrName("cn");
         grpConfig.setObjClass("posixGroup");
         template.setGroupMapping(grpConfig);
         // add the groupOfUniqueNames
         grpConfig = new GroupMappingConfig();
         grpConfig.setMemberAttrName("uniqueMember");
-        grpConfig.setMemberStrategy(GroupMappingConfig.MEMBERS_ARE_NVPAIR);
+        grpConfig.setMemberStrategy(MemberStrategy.MEMBERS_ARE_NVPAIR);
         grpConfig.setNameAttrName("cn");
         grpConfig.setObjClass("groupOfUniqueNames");
         template.setGroupMapping(grpConfig);
         // add the organizationalUnit
         grpConfig = new GroupMappingConfig();
         grpConfig.setMemberAttrName("");
-        grpConfig.setMemberStrategy(GroupMappingConfig.MEMBERS_BY_OU);
+        grpConfig.setMemberStrategy(MemberStrategy.MEMBERS_BY_OU);
         grpConfig.setNameAttrName("ou");
         grpConfig.setObjClass("organizationalUnit");
         template.setGroupMapping(grpConfig);
 
         templates.put(template.getName(), template);
-    }
+
+    }*/
 
     private final HashMap templates = new HashMap();
+    private final Logger logger = LogManager.getInstance().getSystemLogger();
 }
