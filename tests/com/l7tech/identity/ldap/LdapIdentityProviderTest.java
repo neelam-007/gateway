b0670a8759b7ff63d13f6f7e93c1efb8a659c863
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-01-23 01:33:16 +0000

    added tests for msad provider
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@4818 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- tests/com/l7tech/identity/ldap/LdapIdentityProviderTest.java
+++ tests/com/l7tech/identity/ldap/LdapIdentityProviderTest.java
@@ -32,14 +32,31 @@ public class LdapIdentityProviderTest extends TestCase {
         return spockTemplate;
     }
 
+    private LdapIdentityProviderConfig getConfigForMSAD() throws IOException {
+        LdapConfigTemplateManager templateManager = new LdapConfigTemplateManager();
+        LdapIdentityProviderConfig msadTemplate = templateManager.getTemplate("MSAD");
+        //msadTemplate.setLdapUrl("ldap://localhost:3899");
+        msadTemplate.setLdapUrl("ldap://mail.l7tech.com:3268");
+        msadTemplate.setSearchBase("ou=Layer 7 Users,dc=L7TECH,dc=LOCAL");
+        msadTemplate.setBindDN("browse");
+        msadTemplate.setBindPasswd("password");
+        return msadTemplate;
+    }
+
     private LdapIdentityProvider getSpockProvider() throws IOException {
         LdapIdentityProvider spock =  new LdapIdentityProvider();
         spock.initialize(getConfigForSpock());
         return spock;
     }
 
+    private LdapIdentityProvider getMSADProvider() throws IOException {
+        LdapIdentityProvider spock =  new LdapIdentityProvider();
+        spock.initialize(getConfigForMSAD());
+        return spock;
+    }
+
     public void testGetUsers() throws Exception {
-        Collection users = getSpockProvider().getUserManager().findAll();
+        Collection users = localProvider.getUserManager().findAll();
         for (Iterator i = users.iterator(); i.hasNext(); ) {
             LdapUser user = (LdapUser)i.next();
             System.out.println("found user " + user);
@@ -47,7 +64,7 @@ public class LdapIdentityProviderTest extends TestCase {
     }
 
     public void testGetGroupsAndMembers() throws Exception {
-        GroupManager manager = getSpockProvider().getGroupManager();
+        GroupManager manager = localProvider.getGroupManager();
         Collection groups = manager.findAll();
         for (Iterator i = groups.iterator(); i.hasNext(); ) {
             LdapGroup grp = (LdapGroup)i.next();
@@ -61,17 +78,23 @@ public class LdapIdentityProviderTest extends TestCase {
 
     public void testAuthenticate() throws Exception {
         try {
-        User notauthenticated = getSpockProvider().getUserManager().findByLogin("flascelles");
-        if (notauthenticated == null) {
-            System.out.println("user not found");
-            return;
-        } else {
-            System.out.println("user found " + notauthenticated.getUniqueIdentifier());
-        }
-        User authenticated = getSpockProvider().authenticate(new LoginCredentials(notauthenticated.getLogin(), "rockclimbing".getBytes(), CredentialFormat.CLEARTEXT, null, null));
-        if (authenticated != null) {
-            System.out.println("user authenticated " + authenticated);
-        }
+            User notauthenticated = localProvider.getUserManager().findByLogin("flascelles");
+            if (notauthenticated == null) {
+                System.out.println("user not found");
+                return;
+            } else {
+                System.out.println("user found " + notauthenticated.getUniqueIdentifier());
+            }
+            User authenticated = null;
+            try {
+                authenticated = localProvider.authenticate(new LoginCredentials(notauthenticated.getLogin(),
+                        "rockclimbing".getBytes(), CredentialFormat.CLEARTEXT, null, null));
+            } catch (Exception e) {
+                System.out.println("creds do not authenticate.");
+            }
+            if (authenticated != null) {
+                System.out.println("user authenticated " + authenticated);
+            }
         } catch (Exception e) {
             e.printStackTrace(System.err);
             throw e;
@@ -80,8 +103,14 @@ public class LdapIdentityProviderTest extends TestCase {
 
     public static void main(String[] args) throws Exception {
         LdapIdentityProviderTest me = new LdapIdentityProviderTest();
+
+        //me.localProvider = me.getSpockProvider();
+        me.localProvider = me.getMSADProvider();
+
         me.testAuthenticate();
         me.testGetUsers();
         me.testGetGroupsAndMembers();
     }
+
+    private LdapIdentityProvider localProvider;
 }
