cc405ff9143a8a23a52ce2d4c3125a3a123f33a5
fpang <fpang@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-01-23 18:58:13 +0000

    Updated the test stub with adding the data required.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@4837 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- tests/com/l7tech/cluster/ClusterStatusAdminStub.java
+++ tests/com/l7tech/cluster/ClusterStatusAdminStub.java
@@ -25,14 +25,14 @@ public class ClusterStatusAdminStub implements ClusterStatusAdmin{
         ClusterNodeInfo c7 = new ClusterNodeInfo();
         ClusterNodeInfo c8 = new ClusterNodeInfo();
 
-        c1.setName("SSG1"); c1.setAddress("192.128.1.100"); c1.setAvgLoad(1.5); c1.setUptime(1072746384);
-        c2.setName("SSG2"); c2.setAddress("192.128.1.101"); c2.setAvgLoad(1.8); c2.setUptime(1072656394);
-        c3.setName("SSG3"); c3.setAddress("192.128.1.102"); c3.setAvgLoad(0); c3.setUptime(1072746404);
-        c4.setName("SSG4"); c4.setAddress("192.128.2.10"); c4.setAvgLoad(1.1); c4.setUptime(1072776414);
-        c5.setName("SSG5"); c5.setAddress("192.128.2.11"); c5.setAvgLoad(2.1); c5.setUptime(1072746484);
-        c6.setName("SSG6"); c6.setAddress("192.128.3.1"); c6.setAvgLoad(0.8); c6.setUptime(1072736464);
-        c7.setName("SSG7"); c7.setAddress("192.128.3.2"); c7.setAvgLoad(0); c7.setUptime(1072808010);
-        c8.setName("SSG8"); c8.setAddress("192.128.3.3"); c8.setAvgLoad(0); c8.setUptime(1072808325);
+        c1.setMac("00:0c:11:f0:43:01"); c1.setName("SSG1"); c1.setAddress("192.128.1.100"); c1.setAvgLoad(1.5); c1.setUptime(1072746384);
+        c2.setMac("00:0c:11:f0:43:02");c2.setName("SSG2"); c2.setAddress("192.128.1.101"); c2.setAvgLoad(1.8); c2.setUptime(1072656394);
+        c3.setMac("00:0c:11:f0:43:03");c3.setName("SSG3"); c3.setAddress("192.128.1.102"); c3.setAvgLoad(0); c3.setUptime(1072746404);
+        c4.setMac("00:0c:11:f0:43:04");c4.setName("SSG4"); c4.setAddress("192.128.2.10"); c4.setAvgLoad(1.1); c4.setUptime(1072776414);
+        c5.setMac("00:0c:11:f0:43:05");c5.setName("SSG5"); c5.setAddress("192.128.2.11"); c5.setAvgLoad(2.1); c5.setUptime(1072746484);
+        c6.setMac("00:0c:11:f0:43:06");c6.setName("SSG6"); c6.setAddress("192.128.3.1"); c6.setAvgLoad(0.8); c6.setUptime(1072736464);
+        c7.setMac("00:0c:11:f0:43:07");c7.setName("SSG7"); c7.setAddress("192.128.3.2"); c7.setAvgLoad(0); c7.setUptime(1072808010);
+        c8.setMac("00:0c:11:f0:43:08");c8.setName("SSG8"); c8.setAddress("192.128.3.3"); c8.setAvgLoad(0); c8.setUptime(1072808325);
 
         cluster[0] = c1;
         cluster[1] = c2;
@@ -59,16 +59,16 @@ public class ClusterStatusAdminStub implements ClusterStatusAdmin{
         ServiceUsage s9 = new ServiceUsage();
         ServiceUsage s10 = new ServiceUsage();
 
-        s1.setServiceid(1234); s1.setNodeid("SSG1"); s1.setRequests(1000); s1.setAuthorized(998); s1.setCompleted(998);
-        s2.setServiceid(1235); s2.setNodeid("SSG1"); s2.setRequests(500); s2.setAuthorized(497); s2.setCompleted(497);
-        s3.setServiceid(1234); s3.setNodeid("SSG2"); s3.setRequests(1100); s3.setAuthorized(1008); s3.setCompleted(1008);
-        s4.setServiceid(1235); s4.setNodeid("SSG2"); s4.setRequests(600); s4.setAuthorized(567); s4.setCompleted(567);
-        s5.setServiceid(1234); s5.setNodeid("SSG4"); s5.setRequests(1200); s5.setAuthorized(1158); s5.setCompleted(1158);
-        s6.setServiceid(1235); s6.setNodeid("SSG4"); s6.setRequests(700); s6.setAuthorized(689); s6.setCompleted(689);
-        s7.setServiceid(1234); s7.setNodeid("SSG5"); s7.setRequests(1300); s7.setAuthorized(1230); s7.setCompleted(1230);
-        s8.setServiceid(1235); s8.setNodeid("SSG5"); s8.setRequests(800); s8.setAuthorized(755); s8.setCompleted(755);
-        s9.setServiceid(1234); s9.setNodeid("SSG6"); s9.setRequests(1400); s9.setAuthorized(1298); s9.setCompleted(1298);
-        s10.setServiceid(1235); s10.setNodeid("SSG6"); s10.setRequests(900); s10.setAuthorized(905); s10.setCompleted(905);
+        s1.setServiceid(1234); s1.setNodeid("00:0c:11:f0:43:01"); s1.setRequests(1000); s1.setAuthorized(998); s1.setCompleted(998);
+        s2.setServiceid(1235); s2.setNodeid("00:0c:11:f0:43:01"); s2.setRequests(500); s2.setAuthorized(497); s2.setCompleted(497);
+        s3.setServiceid(1234); s3.setNodeid("00:0c:11:f0:43:02"); s3.setRequests(1100); s3.setAuthorized(1008); s3.setCompleted(1008);
+        s4.setServiceid(1235); s4.setNodeid("00:0c:11:f0:43:02"); s4.setRequests(600); s4.setAuthorized(567); s4.setCompleted(567);
+        s5.setServiceid(1234); s5.setNodeid("00:0c:11:f0:43:04"); s5.setRequests(1200); s5.setAuthorized(1158); s5.setCompleted(1158);
+        s6.setServiceid(1235); s6.setNodeid("00:0c:11:f0:43:04"); s6.setRequests(700); s6.setAuthorized(689); s6.setCompleted(689);
+        s7.setServiceid(1234); s7.setNodeid("00:0c:11:f0:43:05"); s7.setRequests(1300); s7.setAuthorized(1230); s7.setCompleted(1230);
+        s8.setServiceid(1235); s8.setNodeid("00:0c:11:f0:43:05"); s8.setRequests(800); s8.setAuthorized(755); s8.setCompleted(755);
+        s9.setServiceid(1234); s9.setNodeid("00:0c:11:f0:43:06"); s9.setRequests(1400); s9.setAuthorized(1298); s9.setCompleted(1298);
+        s10.setServiceid(1235); s10.setNodeid("00:0c:11:f0:43:06"); s10.setRequests(900); s10.setAuthorized(905); s10.setCompleted(905);
 
         serviceUsage[0] = s1;
         serviceUsage[1] = s2;
