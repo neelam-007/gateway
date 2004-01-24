6861bea06a7c1d397a953cbc07e9d85d7e929f2b
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-01-24 01:48:17 +0000

    Initial checkin of common JMS stuff
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@4858 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/jms/JmsAdmin.java
@@ -0,0 +1,21 @@
+package com.l7tech.jms;
+
+import com.l7tech.objectmodel.*;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.Collection;
+
+/**
+ * @author alex
+ * @version $Revision$
+ */
+public interface JmsAdmin extends Remote {
+    Collection findAllProviders() throws RemoteException, FindException;
+    JmsProvider findProviderByPrimaryKey( long oid ) throws RemoteException, FindException;
+    long saveProvider( JmsProvider provider ) throws RemoteException, UpdateException, SaveException, VersionException;
+    void deleteProvider( long providerOid ) throws RemoteException, DeleteException;
+
+    long saveDestination( JmsDestination destination ) throws RemoteException, UpdateException, SaveException, VersionException;
+    void deleteDestination( long destinationOid ) throws RemoteException, DeleteException;
+}
