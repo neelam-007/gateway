67209391437041750a4f1a96b2a01577897a39a8
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-01-29 01:42:16 +0000

    Moved here from a stupider place
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@4934 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/common/transport/jms/JmsDestination.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2003-2004 Layer 7 Technologies Inc.
+ *
+ * $Id$
+ */
+
+package com.l7tech.common.transport.jms;
+
+import com.l7tech.objectmodel.imp.NamedEntityImp;
+
+import java.io.Serializable;
+
+/**
+ * A reference to a preconfigured JMS Destination (i.e. a Queue or Topic).
+ *
+ * Persistent.
+ *
+ * @author alex
+ * @version $Revision$
+ */
+public class JmsDestination extends NamedEntityImp implements Serializable {
+    private JmsProvider _provider;
+    private String _destinationName;
+    private JmsReplyType _replyType;
+
+    /** Optional, set only if {@link #_replyType} is {@link com.l7tech.common.transport.jms.JmsReplyType#REPLY_TO_OTHER} */
+    private JmsDestination _replyDestination;
+
+    /** Optional */
+    private JmsDestination _failureDestination;
+
+    public JmsProvider getProvider() {
+        return _provider;
+    }
+
+    public void setProvider(JmsProvider provider) {
+        _provider = provider;
+    }
+
+    public String getDestinationName() {
+        return _destinationName;
+    }
+
+    public void setDestinationName(String name) {
+        _destinationName = name;
+    }
+
+    public JmsReplyType getReplyType() {
+        return _replyType;
+    }
+
+    public void setReplyType(JmsReplyType replyType) {
+        _replyType = replyType;
+    }
+
+    public JmsDestination getReplyDestination() {
+        return _replyDestination;
+    }
+
+    public void setReplyDestination(JmsDestination replyTo) {
+        _replyDestination = replyTo;
+    }
+
+    public JmsDestination getFailureDestination() {
+        return _failureDestination;
+    }
+
+    public void setFailureDestination(JmsDestination failureDestination) {
+        _failureDestination = failureDestination;
+    }
+}
---
+++ src/com/l7tech/common/transport/jms/JmsReplyType.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2003-2004 Layer 7 Technologies Inc.
+ *
+ * $Id$
+ */
+
+package com.l7tech.common.transport.jms;
+
+import net.sf.hibernate.PersistentEnum;
+
+import java.io.Serializable;
+import java.io.ObjectStreamException;
+
+/**
+ * @author alex
+ * @version $Revision$
+ */
+public class JmsReplyType implements Serializable, PersistentEnum {
+    public static final JmsReplyType AUTOMATIC = new JmsReplyType( 0, "Automatic" );
+    public static final JmsReplyType NO_REPLY = new JmsReplyType( 1, "No reply" );
+    public static final JmsReplyType REPLY_TO_OTHER = new JmsReplyType( 2, "Reply to other" );
+    public static final JmsReplyType REPLY_TO_SAME = new JmsReplyType( 3, "Reply to same destination" );
+    private static final JmsReplyType[] VALUES = { AUTOMATIC, NO_REPLY, REPLY_TO_OTHER, REPLY_TO_SAME };
+
+    public int getNum() { return _num; }
+    public String getName() { return _name; }
+    public String toString() {
+        return "<JmsReplyType num=\"" + _num + "\" name=\"" + _name + "\"/>";
+    }
+
+    private JmsReplyType( int num, String name ) {
+        _num = num;
+        _name = name;
+    }
+
+    private Object readResolve() throws ObjectStreamException {
+        return VALUES[_num];
+    }
+
+    private final int _num;
+    private final String _name;
+
+    public int toInt() {
+        return _num;
+    }
+}
