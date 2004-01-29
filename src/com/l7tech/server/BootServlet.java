9fc9edfd89518ad3a5569f485943f582867f6b05
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-01-29 01:29:12 +0000

    Refactored startup code to BootProcess
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@4926 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/server/BootServlet.java
+++ src/com/l7tech/server/BootServlet.java
@@ -6,19 +6,7 @@
 
 package com.l7tech.server;
 
-import com.l7tech.common.BuildInfo;
-import com.l7tech.common.util.Locator;
 import com.l7tech.logging.LogManager;
-import com.l7tech.logging.ServerLogManager;
-import com.l7tech.objectmodel.HibernatePersistenceManager;
-import com.l7tech.objectmodel.PersistenceContext;
-import com.l7tech.objectmodel.UpdateException;
-import com.l7tech.remote.jini.Services;
-import com.l7tech.remote.jini.export.RemoteService;
-import com.l7tech.service.ServiceManager;
-import com.l7tech.service.ServiceManagerImp;
-import com.l7tech.cluster.ClusterInfoManager;
-import com.l7tech.cluster.StatusUpdater;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -27,13 +15,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.File;
-import java.io.FileInputStream;
-import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.Properties;
-import java.util.Iterator;
 
 /**
  * @author alex
@@ -42,72 +25,29 @@ import java.util.Iterator;
 public class BootServlet extends HttpServlet {
     // private final Logger logger = Logger.getLogger(BootServlet.class.getName());
     private final Logger logger = LogManager.getInstance().getSystemLogger();
-    private final ServerConfig _config = ServerConfig.getInstance();
+    private final ServerComponentLifecycle _boot = new BootProcess();
 
     public void init( ServletConfig sc ) throws ServletException {
         super.init( sc );
-
         boolean failure = false;
+
         try {
-            setSystemProperties();
-
-            initializeAdminServices();
-            HibernatePersistenceManager.initialize();
-            // make sure the ServiceManager is available. this will also build the service cache
-            if (Locator.getDefault().lookup(ServiceManager.class) == null) {
-                logger.severe("Could not instantiate the ServiceManager");
-            }
-
-            PersistenceContext.getCurrent().beginTransaction();
-            // initialize the log dumper
-            LogManager logManager = LogManager.getInstance();
-            if (logManager instanceof ServerLogManager) {
-                ((ServerLogManager)logManager).suscribeDBHandler();
-            }
-            // initialize the process that updates the cluster status info
-            initializeClusterStatusUpdate();
-            PersistenceContext.getCurrent().commitTransaction();
-            PersistenceContext.getCurrent().close();
-
-
-        } catch (SQLException e) {
-            logger.log(Level.SEVERE, "SQL ERROR IN BOOT SERVLET", e);
-            failure = true;
-            throw new ServletException(e);
-        } catch (Throwable e) {
+            _boot.init();
+            _boot.start();
+        } catch ( LifecycleException e ) {
             logger.log(Level.SEVERE, "ERROR IN BOOT SERVLET", e);
             failure = true;
             throw new ServletException(e);
         } finally {
             if (failure) {
-                destroy();
+                try {
+                    _boot.close();
+                } catch ( LifecycleException e ) {
+                    throw new ServletException(e);
+                }
             }
         }
 
-        logger.info( BuildInfo.getLongBuildString() );
-        logger.info("Boot servlet complete.");
-    }
-
-    private void setSystemProperties() throws IOException {
-        // Set system properties
-        String sysPropsPath = _config.getSystemPropertiesPath();
-        File propsFile = new File( sysPropsPath );
-        Properties props = new Properties();
-
-        // Set default properties
-        props.setProperty("com.sun.jndi.ldap.connect.pool.timeout", new Integer( 30 * 1000 ).toString() );
-        props.setProperty( ServerConfig.PROP_HOSTNAME, _config.getHostname() );
-
-        if ( propsFile.exists() ) {
-            FileInputStream fis = new FileInputStream( propsFile );
-            props.load(fis);
-        }
-
-        for (Iterator i = props.keySet().iterator(); i.hasNext();) {
-            String name = (String)i.next();
-            String value = (String)props.get(name);
-            System.setProperty( name, value );
-        }
     }
 
     public void doGet( HttpServletRequest request, HttpServletResponse response ) throws IOException, ServletException {
@@ -121,34 +61,13 @@ public class BootServlet extends HttpServlet {
      *
      */
     public void destroy() {
-        logger.info("Stopping admin services.");
-        RemoteService.unexportAll();
-        // stop cache integrity process if necessary
-        ServiceManager serviceManager = (ServiceManager)Locator.getDefault().lookup(ServiceManager.class);
-        if (serviceManager != null && serviceManager instanceof ServiceManagerImp) {
-            ((ServiceManagerImp)serviceManager).destroy();
-        }
-        // if we were updating cluster status, stop doing it
-        StatusUpdater.stopUpdater();
-    }
-
-    protected void initializeAdminServices() {
         try {
-            Services.getInstance().start();
-        } catch (Exception e) {
-            logger.log(Level.WARNING,
-              "There was an error in initalizing admin services.\n" +
-              " The admin services may not be available.", e);
+            _boot.stop();
+            _boot.close();
+        } catch ( LifecycleException e ) {
+            logger.warning( "Caught exception during server shutdown" );
         }
     }
 
-    protected void initializeClusterStatusUpdate() {
-        ClusterInfoManager man = ClusterInfoManager.getInstance();
-        try {
-            man.updateSelfUptime();
-        } catch (UpdateException e) {
-            logger.log(Level.WARNING, "error updating boot time of node.", e);
-        }
-        StatusUpdater.initialize();
-    }
+
 }
