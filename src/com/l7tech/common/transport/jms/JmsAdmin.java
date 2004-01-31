b638130bcad7288317649669a164fc2727ee0609
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-01-31 04:22:57 +0000

    document saveConnection()
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@4989 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/common/transport/jms/JmsAdmin.java
+++ src/com/l7tech/common/transport/jms/JmsAdmin.java
@@ -12,8 +12,17 @@ import java.rmi.RemoteException;
  * @version $Revision$
  */
 public interface JmsAdmin extends Remote {
+    JmsProvider[] getProviderList() throws RemoteException;
     EntityHeader[] findAllConnections() throws RemoteException, FindException;
     JmsConnection findConnectionByPrimaryKey( long oid ) throws RemoteException, FindException;
+
+    /**
+     * Save the specified JmsConnection, which may or may not have been newly created by the caller, to the database.
+     *
+     * @param connection the JmsConnection to save
+     * @return the OID assigned to the saved JmsConnection.
+     */
     long saveConnection( JmsConnection connection ) throws RemoteException, UpdateException, SaveException, VersionException;
+
     void deleteConnection( long connectionOid ) throws RemoteException, DeleteException;
 }
