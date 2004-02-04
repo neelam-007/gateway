8a8a7b08d14f99effcfccdcd66070e046ea70d76
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-02-04 01:45:57 +0000

    Initial checkin (not much)
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@5012 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/server/transport/jms/JmsSoapRequest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2003-2004 Layer 7 Technologies Inc.
+ *
+ * $Id$
+ */
+
+package com.l7tech.server.transport.jms;
+
+import com.l7tech.message.SoapRequest;
+import com.l7tech.logging.LogManager;
+
+import javax.jms.Message;
+import javax.jms.TextMessage;
+import javax.jms.JMSException;
+import java.io.Reader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.logging.Logger;
+
+/**
+ * @author alex
+ * @version $Revision$
+ */
+public class JmsSoapRequest extends SoapRequest {
+    private Logger _logger = LogManager.getInstance().getSystemLogger();
+
+    public JmsSoapRequest( JmsTransportMetadata mt ) {
+        super( mt );
+        _jms = mt;
+    }
+
+    protected Reader doGetRequestReader() throws IOException {
+        Message request = _jms.getRequest();
+        if ( request instanceof TextMessage ) {
+            TextMessage treq = (TextMessage)request;
+            try {
+                return new StringReader( treq.getText() );
+            } catch (JMSException e) {
+                throw new IOException( e.toString() );
+            }
+        } else {
+            _logger.warning( "Can't get a reader for a non-text message!" );
+            return new StringReader("");
+        }
+    }
+
+    private JmsTransportMetadata _jms;
+}
---
+++ src/com/l7tech/server/transport/jms/JmsSoapResponse.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2003-2004 Layer 7 Technologies Inc.
+ *
+ * $Id$
+ */
+
+package com.l7tech.server.transport.jms;
+
+import com.l7tech.message.SoapResponse;
+
+/**
+ * @author alex
+ * @version $Revision$
+ */
+public class JmsSoapResponse extends SoapResponse {
+    public JmsSoapResponse( JmsTransportMetadata tm ) {
+        super( tm );
+    }
+}
