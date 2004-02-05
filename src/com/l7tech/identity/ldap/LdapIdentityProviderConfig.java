fc817e5053d19f983fab93a3b3a44529114915cc
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-02-05 00:20:48 +0000

    documented the class and added methods to allow for overriding of all user and group object mappings at once.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@5030 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/identity/ldap/LdapIdentityProviderConfig.java
+++ src/com/l7tech/identity/ldap/LdapIdentityProviderConfig.java
@@ -34,38 +34,70 @@ public class LdapIdentityProviderConfig extends IdentityProviderConfig implement
         super(IdentityProviderType.LDAP);
     }
 
+    /**
+     * the ldap url for connecting to the directory.
+     */
     public String getLdapUrl() {
         return (String)props.get(URL);
     }
 
+    /**
+     * the ldap url for connecting to the directory.
+     */
     public void setLdapUrl(String ldapUrl) {
         props.put(URL, ldapUrl);
     }
 
+    /**
+     * the search base for users and groups
+     */
     public String getSearchBase() {
         return (String)props.get(SEARCH_BASE);
     }
 
+    /**
+     * the search base for users and groups
+     */
     public void setSearchBase(String searchBase) {
         props.put(SEARCH_BASE, searchBase);
     }
 
+    /**
+     * overrides the search filter for group objects
+     * currently unused
+     */
     public String getCustomGrpFilter() {
         return (String)props.get(CUSTOM_GROUP_SEARCH_FILTER);
     }
 
+    /**
+     * overrides the search filter for group objects
+     * currently unused
+     */
     public void setCustomGrpFilter(String filter) {
         props.put(CUSTOM_GROUP_SEARCH_FILTER, filter);
     }
 
+    /**
+     * overrides the search filter for user objects
+     * currently unused
+     */
     public void setCustomUsrFilter(String filter) {
         props.put(CUSTOM_USER_SEARCH_FILTER, filter);
     }
 
+    /**
+     * overrides the search filter for user objects
+     * currently unused
+     */
     public String getCustomUsrFilter() {
         return (String)props.get(CUSTOM_USER_SEARCH_FILTER);
     }
 
+    /**
+     * get the mapping for a specified group object class
+     * returns null if there is no mapping declared for the passed object class
+     */
     public GroupMappingConfig getGroupMapping(String objClass) {
         HashMap grpMap = (HashMap)props.get(GROUP_MAPPINGS);
         if (grpMap == null) return null;
@@ -73,6 +105,9 @@ public class LdapIdentityProviderConfig extends IdentityProviderConfig implement
         return output;
     }
 
+    /**
+     * @return all existing group mappings
+     */
     public GroupMappingConfig[] getGroupMappings() {
         HashMap grpMap = (HashMap)props.get(GROUP_MAPPINGS);
         if (grpMap == null) return new GroupMappingConfig[0];
@@ -86,6 +121,9 @@ public class LdapIdentityProviderConfig extends IdentityProviderConfig implement
         return output;
     }
 
+    /**
+     * add or overrides the mapping for a specific group object class
+     */
     public void setGroupMapping(GroupMappingConfig cfg) {
         HashMap grpMap = (HashMap)props.get(GROUP_MAPPINGS);
         if (grpMap == null) {
@@ -95,6 +133,26 @@ public class LdapIdentityProviderConfig extends IdentityProviderConfig implement
         grpMap.put(cfg.getObjClass().toLowerCase(), cfg);
     }
 
+    /**
+     * overrides all group class mappings at once
+     */
+    public void setGroupMappings(GroupMappingConfig[] cfgs) {
+        HashMap grpMap = (HashMap)props.get(GROUP_MAPPINGS);
+        if (grpMap == null) {
+            grpMap = new HashMap();
+            props.put(GROUP_MAPPINGS, grpMap);
+        } else {
+            grpMap.clear();
+        }
+        for (int i = 0; i < cfgs.length; i++) {
+            grpMap.put(cfgs[i].getObjClass().toLowerCase(), cfgs[i]);
+        }
+    }
+
+    /**
+     * get the mapping for a specified user object class
+     * returns null if there is no mapping declared for the passed object class
+     */
     public UserMappingConfig getUserMapping(String objClass) {
         HashMap usrMap = (HashMap)props.get(USER_MAPPINGS);
         if (usrMap == null) return null;
@@ -102,6 +160,9 @@ public class LdapIdentityProviderConfig extends IdentityProviderConfig implement
         return output;
     }
 
+    /**
+     * @return all existing user mappings
+     */
     public UserMappingConfig[] getUserMappings() {
         HashMap usrMap = (HashMap)props.get(USER_MAPPINGS);
         if (usrMap == null) return new UserMappingConfig[0];
@@ -116,6 +177,9 @@ public class LdapIdentityProviderConfig extends IdentityProviderConfig implement
         return output;
     }
 
+    /**
+     * add or overrides the mapping for a specific user object class
+     */
     public void setUserMapping(UserMappingConfig cfg) {
         HashMap usrMap = (HashMap)props.get(USER_MAPPINGS);
         if (usrMap == null) {
@@ -125,26 +189,60 @@ public class LdapIdentityProviderConfig extends IdentityProviderConfig implement
         usrMap.put(cfg.getObjClass().toLowerCase(), cfg);
     }
 
+    /**
+     * overrides all user class mappings at once
+     */
+    public void setUserMappings(UserMappingConfig[] cfgs) {
+        HashMap usrMap = (HashMap)props.get(USER_MAPPINGS);
+        if (usrMap == null) {
+            usrMap = new HashMap();
+            props.put(USER_MAPPINGS, usrMap);
+        } else {
+            usrMap.clear();
+        }
+        for (int i = 0; i < cfgs.length; i++) {
+            usrMap.put(cfgs[i].getObjClass().toLowerCase(), cfgs[i]);
+        }
+    }
+
+    /**
+     * the bind dn for searching
+     */
     public String getBindDN() {
         return (String)props.get(BIND_DN);
     }
 
+    /**
+     * the bind dn for searching
+     */
     public void setBindDN(String binddn) {
         props.put(BIND_DN, binddn);
     }
 
+    /**
+     * the bind passwd for searching
+     */
     public String getBindPasswd() {
         return (String)props.get(BIND_PASS);
     }
 
+    /**
+     * the bind passwd for searching
+     */
     public void setBindPasswd(String bindpasswd) {
         props.put(BIND_PASS, bindpasswd);
     }
 
+    /**
+     * set by the template manager. dont override this value.
+     */
     public String getTemplateName() {
         return (String)props.get(BASE_TEMPLATE);
     }
 
+    /**
+     * set by the template manager. dont override this value.
+     */
     public void setTemplateName(String name) {
         props.put(BASE_TEMPLATE, name);
     }
