0f1e4f141972b487105a9e4569ae2cf1c66f9f77
fpang <fpang@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-02-06 01:39:29 +0000

    Updated the panel to show the data for editing.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@5049 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/console/panels/InternalIdentityProviderConfigPanel.java
+++ src/com/l7tech/console/panels/InternalIdentityProviderConfigPanel.java
@@ -1,6 +1,13 @@
 package com.l7tech.console.panels;
 
+import com.l7tech.identity.ldap.LdapIdentityProviderConfig;
+import com.l7tech.identity.IdentityProviderConfig;
+
 import javax.swing.*;
+import java.awt.*;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 
 /*
  * Copyright (C) 2003 Layer 7 Technologies Inc.
@@ -8,37 +15,184 @@ import javax.swing.*;
  * $Id$
  */
 
-public class InternalIdentityProviderConfigPanel extends WizardStepPanel{
+public class InternalIdentityProviderConfigPanel extends WizardStepPanel {
 
-        public InternalIdentityProviderConfigPanel(WizardStepPanel next, boolean showProviderType) {
+    public InternalIdentityProviderConfigPanel(WizardStepPanel next, boolean showProviderType) {
         super(next);
         this.showProviderType = showProviderType;
-        //initResources();
-        //initComponents();
-        //providerSettingsPanel = getLdapPanel(null);
-       // add(providerSettingsPanel);
+        initResources();
+        initComponents();
     }
 
     /** populate the form from the provider beans */
     public void readSettings(Object settings) throws IllegalArgumentException {
-        {
-            // kludge, we add the internal provider, as itmay show only in
-            // edit dsabled mode
-            providerTypesCombo.addItem("Internal Provider");
+        if (settings != null) {
 
-            for (int i = providerTypesCombo.getModel().getSize() - 1; i >= 0; i--) {
-                Object toto = providerTypesCombo.getModel().getElementAt(i);
-                if (toto instanceof String) {
-                    if (toto.equals("Internal Provider")) {
-                        providerTypesCombo.setSelectedIndex(i);
-                        break;
-                    }
+            if (settings instanceof IdentityProviderConfig) {
+
+                IdentityProviderConfig iProviderConfig = (IdentityProviderConfig) settings;
+
+                if (iProviderConfig.getOid() != -1) {
+
+                    getProviderNameTextField().setText(iProviderConfig.getName());
                 }
             }
         }
         providerTypesCombo.setEnabled(false);
     }
 
+    /** @return the wizard step label    */
+    public String getStepLabel() {
+        return "Provider Configuration";
+    }
+
+    /**
+     * Loads locale-specific resources: strings  etc
+     */
+    private void initResources() {
+        Locale locale = Locale.getDefault();
+        //todo: change the property file from IdentityProviderDialog to LdapIdentityProviderConfigPanel
+        resources = ResourceBundle.getBundle("com.l7tech.console.resources.IdentityProviderDialog", locale);
+    }
+
+    private void initComponents() {
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridBagLayout());
+
+        GridBagConstraints constraints;
+
+        int rowIndex = 0;
+
+        // provider types
+        JLabel providerTypesLabel = new JLabel();
+        providerTypesLabel.setToolTipText(resources.getString("providerTypeTextField.tooltip"));
+        providerTypesLabel.setText(resources.getString("providerTypeTextField.label"));
+        constraints = new GridBagConstraints();
+        constraints.gridx = 0;
+        constraints.gridy = rowIndex;
+        constraints.gridwidth = 1;
+        constraints.fill = GridBagConstraints.NONE;
+        constraints.anchor = GridBagConstraints.WEST;
+        constraints.weightx = 0.0;
+        constraints.insets = new Insets(12, 12, 0, 0);
+        panel.add(providerTypesLabel, constraints);
+
+        constraints = new GridBagConstraints();
+        constraints.gridx = 1;
+        constraints.gridy = rowIndex++;
+        constraints.gridwidth = 1;
+        constraints.fill = GridBagConstraints.NONE;
+        constraints.anchor = GridBagConstraints.WEST;
+        constraints.weightx = 0.0;
+        constraints.insets = new Insets(12, 7, 0, 0);
+        panel.add(getProviderTypes(), constraints);
+
+          if(!showProviderType) {
+              providerTypesLabel.setVisible(false);
+              getProviderTypes().setVisible(false);
+          }
+
+        // Provider ID label
+        JLabel providerNameLabel = new JLabel();
+        providerNameLabel.setToolTipText(resources.getString("providerNameTextField.tooltip"));
+        providerNameLabel.setText(resources.getString("providerNameTextField.label"));
+
+        constraints = new GridBagConstraints();
+        constraints.gridx = 0;
+        constraints.gridy = rowIndex;
+        constraints.weightx = 0.0;
+        constraints.gridwidth = 1;
+        constraints.fill = GridBagConstraints.NONE;
+        constraints.anchor = GridBagConstraints.WEST;
+        constraints.insets = new Insets(12, 12, 0, 0);
+        panel.add(providerNameLabel, constraints);
+
+        // Provider ID text field
+        constraints = new GridBagConstraints();
+        constraints.gridx = 1;
+        constraints.gridy = rowIndex++;
+        constraints.weightx = 0.0;
+        constraints.gridwidth = 1;
+        constraints.fill = GridBagConstraints.NONE;
+        constraints.anchor = GridBagConstraints.WEST;
+        constraints.insets = new Insets(12, 7, 0, 11);
+        panel.add(getProviderNameTextField(), constraints);
+
+        add(panel);
+
+    }
+
+    /**
+     * A method that returns a JCheckBox that indicates
+     * wether the user wishes to define additional properties
+     * of the entity
+     *
+     * @return the CheckBox component
+     */
+    private JComboBox getProviderTypes() {
+        if (providerTypesCombo == null) {
+
+            providerTypesCombo = new JComboBox();
+            providerTypesCombo.addItem("Internal Provider");
+            providerTypesCombo.setRenderer(providerTypeRenderer);
+            providerTypesCombo.setToolTipText(resources.getString("providerTypeTextField.tooltip"));
+        }
+        return providerTypesCombo;
+    }
+
+    /**
+     * A method that returns a JTextField containing provider information
+     *
+     * @return the ID textfield
+     */
+    public JTextField getProviderNameTextField() {
+        if (providerNameTextField != null) return providerNameTextField;
+
+        providerNameTextField = new JTextField();
+        providerNameTextField.setPreferredSize(new Dimension(217, 20));
+        providerNameTextField.setMinimumSize(new Dimension(217, 20));
+        providerNameTextField.setToolTipText(resources.getString("providerNameTextField.tooltip"));
+
+        return providerNameTextField;
+    }
+
+    private ListCellRenderer
+            providerTypeRenderer = new DefaultListCellRenderer() {
+                /**
+                 * Return a component that has been configured to display the identity provider
+                 * type value.
+                 *
+                 * @param list The JList we're painting.
+                 * @param value The value returned by list.getModel().getElementAt(index).
+                 * @param index The cells index.
+                 * @param isSelected True if the specified cell was selected.
+                 * @param cellHasFocus True if the specified cell has the focus.
+                 * @return A component whose paint() method will render the specified value.
+                 *
+                 * @see JList
+                 * @see ListSelectionModel
+                 * @see ListModel
+                 */
+                public Component getListCellRendererComponent(JList list,
+                                                              Object value,
+                                                              int index,
+                                                              boolean isSelected, boolean cellHasFocus) {
+                    super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+                    if (!(value instanceof LdapIdentityProviderConfig)) {
+                        setText(value.toString());
+                    } else {
+                        LdapIdentityProviderConfig type = (LdapIdentityProviderConfig) value;
+                        setText(type.getName());
+                    }
+
+                    return this;
+                }
+
+            };
+
+    private ResourceBundle resources = null;
     private boolean showProviderType;
     private JComboBox providerTypesCombo = null;
+    private JTextField providerNameTextField = null;
 }
