dfc13e80079464767fec485da0e52b834ba7eaab
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-02-09 18:55:02 +0000

    Invoked when a schema validation assertion is dropped in the policy tree. Prevents the instertion of a schema validation assertion with no schema defined.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@5092 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/console/tree/policy/advice/AddSchemaValidationAssertionAdvice.java
@@ -0,0 +1,48 @@
+package com.l7tech.console.tree.policy.advice;
+
+import com.l7tech.common.gui.util.Utilities;
+import com.l7tech.console.MainWindow;
+import com.l7tech.console.panels.SchemaValidationPropertiesDialog;
+import com.l7tech.console.tree.policy.PolicyChange;
+import com.l7tech.console.tree.policy.PolicyException;
+import com.l7tech.console.util.Registry;
+import com.l7tech.policy.assertion.Assertion;
+import com.l7tech.policy.assertion.xml.SchemaValidation;
+
+import java.util.logging.Logger;
+
+/**
+ * Invoked when a schema validation assertion is dropped in the policy tree.
+ * Prevents the instertion of a schema validation assertion with no schema defined.
+ *
+ * <br/><br/>
+ * LAYER 7 TECHNOLOGIES, INC<br/>
+ * User: flascell<br/>
+ * Date: Feb 9, 2004<br/>
+ * $Id$<br/>
+ *
+ */
+public class AddSchemaValidationAssertionAdvice implements Advice {
+    public void proceed(PolicyChange pc) throws PolicyException {
+        Assertion[] assertions = pc.getEvent().getChildren();
+        if (assertions == null || assertions.length != 1 || !(assertions[0] instanceof SchemaValidation)) {
+            throw new IllegalArgumentException();
+        }
+        SchemaValidation assertion = (SchemaValidation)assertions[0];
+        final MainWindow mw = Registry.getDefault().getComponentRegistry().getMainWindow();
+        //SchemaValidationTreeNode fakenode = new SchemaValidationTreeNode(assertion);
+        SchemaValidationPropertiesDialog dlg = new SchemaValidationPropertiesDialog(mw, assertion, pc.getService());
+        // show the dialog
+        dlg.pack();
+        Utilities.centerOnScreen(dlg);
+        dlg.show();
+        // make sure a schema was entered
+        if (assertion.getSchema() != null && assertion.getSchema().length() > 0) {
+            pc.proceed();
+        } else {
+            log.info("schema validation must have been canceled " + assertion.getSchema());
+        }
+    }
+
+    private final Logger log = Logger.getLogger(getClass().getName());
+}
