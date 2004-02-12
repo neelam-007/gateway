ffef58f437a0fdd228713d6ef6e2c5bbb75485f8
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-02-12 01:12:34 +0000

    Only start a receiver for endpoints that are marked isMessageSource = true
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@5147 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/server/transport/jms/JmsBootProcess.java
+++ src/com/l7tech/server/transport/jms/JmsBootProcess.java
@@ -6,9 +6,8 @@
 
 package com.l7tech.server.transport.jms;
 
-import com.l7tech.common.transport.jms.JmsEndpoint;
 import com.l7tech.common.transport.jms.JmsConnection;
-import com.l7tech.common.transport.jms.JmsReplyType;
+import com.l7tech.common.transport.jms.JmsEndpoint;
 import com.l7tech.logging.LogManager;
 import com.l7tech.objectmodel.FindException;
 import com.l7tech.server.LifecycleException;
@@ -20,13 +19,12 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.logging.Logger;
-import java.util.logging.Level;
 
 /**
  * @author alex
  * @version $Revision$
  */
-public class JmsBootProcess implements ServerComponentLifecycle {
+public class JmsBootProcess implements ServerComponentLifecycle, JmsCrudListener {
     public JmsBootProcess() {
         _manager = new JmsManager();
     }
@@ -39,65 +37,188 @@ public class JmsBootProcess implements ServerComponentLifecycle {
         if ( _booted ) throw new LifecycleException( "Can't boot JmsBootProcess twice!" );
 
         try {
-            Collection connections = _manager.findAll();
-            for (Iterator i = connections.iterator(); i.hasNext();) {
-                JmsConnection conn = (JmsConnection)i.next();
-                _logger.info( "Initializing JMS receiver '" + conn.getName() + "'..." );
-                Set endpoints = conn.getEndpoints();
+            Collection endpoints = _manager.findMessageSourceEndpoints();
+            for ( Iterator i = endpoints.iterator(); i.hasNext(); ) {
+                JmsEndpoint requestEnd = (JmsEndpoint) i.next();
+                JmsConnection conn = requestEnd.getConnection();
 
-                Iterator j = endpoints.iterator();
-                if ( !j.hasNext() ) {
-                    _logger.warning( "No JMS endpoints could be found for connection '" + conn.getName() + "'" );
-                    continue;
-                }
+                _logger.info( "Initializing JMS receiver for '" + conn.getName() + "/" + requestEnd.getName() + "'" );
+                JmsReceiver receiver = makeReceiver( requestEnd );
 
-                while ( j.hasNext() ) {
-                    JmsEndpoint requestEnd = (JmsEndpoint) j.next();
-                    JmsEndpoint replyToEnd = requestEnd.getReplyEndpoint();
-                    JmsEndpoint failureEnd = requestEnd.getFailureEndpoint();
-
-                    _logger.info( "Initializing JMS receiver for '" + conn.getName() + "/" + requestEnd.getName() + "'" );
-                    JmsReceiver receiver = new JmsReceiver(
-                            conn,
-                            JmsReplyType.AUTOMATIC, // TODO Hardcoded
-                            requestEnd, replyToEnd, failureEnd );
-
-                    try {
-                        receiver.init( config );
-                        _receivers.add( receiver );
-                    } catch ( LifecycleException e ) {
-                        _logger.log( Level.WARNING, "Receiver " + receiver + " failed to initialize!", e );
-                    }
-                }
+                init( receiver );
+                _receivers.add( receiver );
             }
+
+            _manager.addCrudListener( this );
+
             _booted = true;
         } catch (FindException e) {
             throw new LifecycleException( e.toString(), e );
         }
     }
 
-    public void start() throws LifecycleException {
+    /**
+     * Attempts to start all JMS receivers.
+     * <p/>
+     * Any exception that is thrown in a JmsReceiver's start() method will be logged but not propagated.
+     */
+    public void start() {
         for (Iterator i = _receivers.iterator(); i.hasNext();) {
             JmsReceiver receiver = (JmsReceiver) i.next();
             _logger.info( "Starting JMS receiver '" + receiver.toString() + "'..." );
-            receiver.start();
+            start( receiver );
         }
     }
 
-    public void stop() throws LifecycleException {
+    /**
+     * Attempts to stop all JMS receivers.
+     */
+    public void stop() {
         for (Iterator i = _receivers.iterator(); i.hasNext();) {
             JmsReceiver receiver = (JmsReceiver) i.next();
             _logger.info( "Stopping JMS receiver '" + receiver.toString() + "'" );
-            receiver.stop();
+            stop( receiver );
         }
     }
 
-    public void close() throws LifecycleException {
+    /**
+     * Attempts to close all JMS receivers.
+     */
+    public void close() {
         for (Iterator i = _receivers.iterator(); i.hasNext();) {
             JmsReceiver receiver = (JmsReceiver) i.next();
             _logger.info( "Closing JMS receiver '" + receiver.toString() + "'" );
-            receiver.close();
+            close(receiver);
         }
+    }
+
+    /**
+     * Handles the event fired by the deletion of a JmsConnection.
+     */
+    public void connectionDeleted( JmsConnection connection ) {
+        for ( Iterator i = _receivers.iterator(); i.hasNext(); ) {
+            JmsReceiver receiver = (JmsReceiver) i.next();
+            if ( receiver.getConnection().getOid() == connection.getOid() ) {
+                stop( receiver );
+                close( receiver );
+            }
+        }
+    }
+
+    /**
+     * Stops a JmsReceiver. Logs but does not propagate any exception that might be thrown.
+     */
+    private void stop( JmsReceiver component ) {
+        try {
+            component.stop();
+        } catch ( LifecycleException e ) {
+            _logger.warning( "Exception while stopping " + component );
+        }
+    }
+
+    /**
+     * Closes a JmsReceiver.  Logs but does not propagate any exception that might be thrown.
+     */
+    private void close( JmsReceiver component ) {
+        try {
+            component.close();
+        } catch ( LifecycleException e ) {
+            _logger.warning( "Exception while closing " + component );
+        }
+    }
+
+    /**
+     * Initializes a JmsReceiver. Logs but does not propagate any exception that might be thrown.
+     */
+    private void init( JmsReceiver component ) {
+        try {
+            component.init( ServerConfig.getInstance() );
+        } catch ( LifecycleException e ) {
+            _logger.warning( "Exception while initializing " + component );
+        }
+    }
+
+    /**
+     * Starts a JmsReceiver. Logs but does not propagate any exception that might be thrown.
+     */
+    private void start( JmsReceiver component ) {
+        try {
+            component.start();
+        } catch ( LifecycleException e ) {
+            _logger.warning( "Exception while initializing " + component );
+        }
+    }
+
+    /**
+     * Handles the event fired by the update of a JmsConnection.
+     */
+    public synchronized void connectionUpdated( JmsConnection updatedConnection ) {
+        for ( Iterator i = _receivers.iterator(); i.hasNext(); ) {
+            JmsReceiver receiver = (JmsReceiver) i.next();
+            stop(receiver);
+            close(receiver);
+            _receivers.remove(receiver);
+        }
+
+        for ( Iterator i = updatedConnection.getEndpoints().iterator(); i.hasNext(); ) {
+            JmsEndpoint endpoint = (JmsEndpoint)i.next();
+            JmsReceiver receiver = makeReceiver( endpoint );
+            try {
+                receiver.init( ServerConfig.getInstance() );
+                receiver.start();
+                _receivers.add( receiver );
+            } catch ( LifecycleException e ) {
+                _logger.warning( "Exception while initializing " + receiver );
+            }
+        }
+    }
+
+    /**
+     * Handles the event fired by the deletion of a JmsEndpoint.
+     */
+    public synchronized void endpointDeleted( JmsEndpoint deletedEndpoint ) {
+        for ( Iterator i = _receivers.iterator(); i.hasNext(); ) {
+            JmsReceiver receiver = (JmsReceiver) i.next();
+            JmsEndpoint existingEndpoint = receiver.getInboundRequestEndpoint();
+            if ( existingEndpoint.getOid() == deletedEndpoint.getOid() ) {
+                stop(receiver);
+                close(receiver);
+            }
+        }
+    }
+
+    /**
+     * Handles the event fired by the update of a JmsEndpoint.
+     */
+    public synchronized void endpointUpdated( JmsEndpoint updatedEndpoint ) {
+        for ( Iterator i = _receivers.iterator(); i.hasNext(); ) {
+            JmsReceiver receiver = (JmsReceiver) i.next();
+            JmsEndpoint originalEndpoint = receiver.getInboundRequestEndpoint();
+            if ( originalEndpoint.getOid() == updatedEndpoint.getOid() ) {
+                stop(receiver);
+                close(receiver);
+                _receivers.remove(receiver);
+            }
+        }
+
+        if ( updatedEndpoint.isMessageSource() ) {
+            JmsReceiver receiver = makeReceiver( updatedEndpoint );
+            try {
+                receiver.init( ServerConfig.getInstance() );
+                receiver.start();
+                _receivers.add( receiver );
+            } catch ( LifecycleException e ) {
+                _logger.warning( "Exception while initializing " + receiver );
+            }
+        }
+    }
+
+    private JmsReceiver makeReceiver( JmsEndpoint endpoint ) {
+        JmsReceiver receiver = new JmsReceiver( endpoint,
+                                                endpoint.getReplyType(),
+                                                endpoint.getReplyEndpoint(),
+                                                endpoint.getFailureEndpoint() );
+        return receiver;
     }
 
     private Logger _logger = LogManager.getInstance().getSystemLogger();
