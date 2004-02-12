c97e496841c763fda3a121f2ab5e5a3ef64468cf
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-02-12 00:19:33 +0000

    The tree node in the assertion palette corresponding to the XSL Transformation Assertion
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@5136 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/console/tree/XslTransformationPaletteNode.java
@@ -0,0 +1,73 @@
+package com.l7tech.console.tree;
+
+import com.l7tech.policy.assertion.Assertion;
+import com.l7tech.policy.assertion.xml.XslTransformation;
+
+import javax.swing.*;
+
+/**
+ * The tree node in the assertion palette corresponding to the XSL Transformation Assertion.
+ *
+ * <br/><br/>
+ * LAYER 7 TECHNOLOGIES, INC<br/>
+ * User: flascell<br/>
+ * Date: Feb 11, 2004<br/>
+ * $Id$<br/>
+ *
+ */
+public class XslTransformationPaletteNode extends AbstractTreeNode {
+    public XslTransformationPaletteNode() {
+        super(null);
+    }
+
+    /**
+     * Get the set of actions associated with this node.
+     * This may be used e.g. in constructing a context menu.
+     *
+     * @return actions appropriate to the node
+     */
+    public Action[] getActions() {
+        return new Action[]{};
+    }
+
+    /**
+     * Return assertion representation of the node
+     * or <b>null</b> if the node cannot be an assertion
+     *
+     * @return the popup menu
+     */
+    public Assertion asAssertion() {
+        return new XslTransformation();
+    }
+
+    /**
+     * Returns true if the receiver is a leaf.
+     *
+     * @return true if leaf, false otherwise
+     */
+    public boolean isLeaf() {
+        return true;
+    }
+
+    /**
+     * Returns true if the receiver allows children.
+     */
+    public boolean getAllowsChildren() {
+        return false;
+    }
+
+    protected void loadChildren() {}
+
+    public String getName() {
+        return "XSL Transformation";
+    }
+
+    /**
+     * subclasses override this method specifying the resource name
+     *
+     * @param open for nodes that can be opened, can have children
+     */
+    protected String iconResource(boolean open) {
+        return "com/l7tech/console/resources/xmlsignature.gif";
+    }
+}
