060227f8e6646f6a3e89fddad20741297386c2c6
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-02-12 23:07:42 +0000

    fixed performance issues with specific user processing by removing unnecessary calls to id provider
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@5155 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/server/policy/assertion/identity/ServerSpecificUser.java
+++ src/com/l7tech/server/policy/assertion/identity/ServerSpecificUser.java
@@ -6,15 +6,12 @@
 
 package com.l7tech.server.policy.assertion.identity;
 
-import com.l7tech.server.policy.assertion.ServerAssertion;
-import com.l7tech.policy.assertion.identity.SpecificUser;
-import com.l7tech.policy.assertion.AssertionStatus;
 import com.l7tech.identity.User;
-import com.l7tech.identity.UserManager;
-import com.l7tech.objectmodel.FindException;
 import com.l7tech.logging.LogManager;
+import com.l7tech.policy.assertion.AssertionStatus;
+import com.l7tech.policy.assertion.identity.SpecificUser;
+import com.l7tech.server.policy.assertion.ServerAssertion;
 
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -28,36 +25,18 @@ public class ServerSpecificUser extends ServerIdentityAssertion implements Serve
     }
 
     /**
-     * Attempts to resolve a <code>User</code> based on this Assertion's <code>userLogin</code>
-     * property.
-     * @return a <code>User</code> that matches the login, or null if none could be found.
-     * @throws com.l7tech.objectmodel.FindException if
-     */
-    protected User getUser() throws FindException {
-        UserManager uman = getIdentityProvider().getUserManager();
-        return uman.findByLogin( _data.getUserLogin() );
-    }
-
-    /**
      * Verifies that the authenticated <code>User</code> matches the <code>User</code>
      * corresponding to this Assertion's <code>userLogin</code> property.
      * @param requestingUser the <code>User</code> to check
      * @return <code>AssertionStatus.NONE</code> if the <code>User</code> matches.
      */
     public AssertionStatus checkUser(User requestingUser) {
-        try {
-            User specifiedUser = getUser();
-            if (specifiedUser == null || !requestingUser.equals(specifiedUser)) {
-                logger.log( Level.FINE, "Requesting user " + requestingUser.getLogin() +
-                                         " does not match specified user " + _data.getUserLogin());
-                return AssertionStatus.UNAUTHORIZED;
-            }
-            else
+        if (requestingUser.getLogin().equals(_data.getUserLogin())) {
+            if (requestingUser.getProviderId() == _data.getIdentityProviderOid()) {
                 return AssertionStatus.NONE;
-        } catch (FindException e) {
-            logger.log(Level.SEVERE, "Exception getting user from provider", e);
-            return AssertionStatus.FAILED;
+            }
         }
+        return AssertionStatus.UNAUTHORIZED;
     }
 
     protected SpecificUser _data;
