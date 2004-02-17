4208264c5b5c0012d36cefc27ce3d9c868bbc037
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-02-17 00:51:33 +0000

    added day of week panel
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@5188 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/console/panels/TimeRangePropertiesDialog.java
+++ src/com/l7tech/console/panels/TimeRangePropertiesDialog.java
@@ -35,6 +35,18 @@ public class TimeRangePropertiesDialog extends JDialog {
     }
 
     /**
+     * enable disable day of week controls
+     */
+    private void toggleDayOfWeek() {
+        boolean enable = false;
+        if (enableDayOfWeek.isSelected()) {
+            enable = true;
+        }
+        startDay.setEnabled(enable);
+        endDay.setEnabled(enable);
+    }
+
+    /**
      * enable disable time of day controls
      */
     private void toggleTimeOfDay() {
@@ -82,10 +94,16 @@ public class TimeRangePropertiesDialog extends JDialog {
                 toggleTimeOfDay();
             }
         });
+
+        enableDayOfWeek.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                toggleDayOfWeek();
+            }
+        });
     }
 
     private JComponent makeTabPanel() {
-        JTabbedPane tabbedPane = new JTabbedPane();
+        JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP, JTabbedPane.SCROLL_TAB_LAYOUT);
         tabbedPane.addTab("Time of day restriction", makeTimeOfDayPanel());
         tabbedPane.addTab("Day of week restriction", makeDayOfWeekPanel());
         return wrapAroundBorders(tabbedPane, CONTROL_SPACING, CONTROL_SPACING, 0, BORDER_PADDING);
@@ -130,19 +148,15 @@ public class TimeRangePropertiesDialog extends JDialog {
         JPanel startTimePanel = new JPanel();
         startTimePanel.setLayout(new FlowLayout(FlowLayout.LEADING, CONTROL_SPACING, 0));
         startTimePanel.add(new JLabel("between"));
-
         startHr = new JSpinner(new SpinnerNumberModel(8, 0, 23, 1));
         startTimePanel.add(startHr);
         startTimePanel.add(new JLabel("hr"));
-
         startMin = new JSpinner(new SpinnerNumberModel(0, 0, 59, 1));
         startTimePanel.add(startMin);
         startTimePanel.add(new JLabel("min"));
-
         startSec = new JSpinner(new SpinnerNumberModel(0, 0, 59, 1));
         startTimePanel.add(startSec);
         startTimePanel.add(new JLabel("sec"));
-
         return startTimePanel;
     }
 
@@ -150,29 +164,57 @@ public class TimeRangePropertiesDialog extends JDialog {
         JPanel endTimePanel = new JPanel();
         endTimePanel.setLayout(new FlowLayout(FlowLayout.LEADING, CONTROL_SPACING, 0));
         endTimePanel.add(new JLabel("and"));
-
         endHr = new JSpinner(new SpinnerNumberModel(17, 0, 23, 1));
         endTimePanel.add(endHr);
         endTimePanel.add(new JLabel("hr"));
-
         endMin = new JSpinner(new SpinnerNumberModel(0, 0, 59, 1));
         endTimePanel.add(endMin);
         endTimePanel.add(new JLabel("min"));
-
         endSec = new JSpinner(new SpinnerNumberModel(0, 0, 59, 1));
         endTimePanel.add(endSec);
         endTimePanel.add(new JLabel("sec"));
-
         return endTimePanel;
+    }
+
+    private JPanel startDayPanel() {
+        JPanel startDayPanel = new JPanel();
+        startDayPanel.setLayout(new FlowLayout(FlowLayout.LEADING, CONTROL_SPACING, 0));
+        startDayPanel.add(new JLabel("between"));
+        startDay = new JSpinner(weekModel());
+        startDayPanel.add(startDay);
+        return startDayPanel;
+    }
+
+    private JPanel endDayPanel() {
+        JPanel endDayPanel = new JPanel();
+        endDayPanel.setLayout(new FlowLayout(FlowLayout.LEADING, CONTROL_SPACING, 0));
+        endDayPanel.add(new JLabel("and"));
+        endDay = new JSpinner(weekModel());
+        endDayPanel.add(endDay);
+        return endDayPanel;
     }
 
     private JComponent makeDayOfWeekPanel() {
         JPanel dayOfWeekPanel = new JPanel();
-        dayOfWeekPanel.setLayout(new BoxLayout(dayOfWeekPanel, BoxLayout.Y_AXIS));
+        dayOfWeekPanel.setLayout(new GridLayout(3, 1, CONTROL_SPACING, CONTROL_SPACING));
+        JPanel titlePanel = new JPanel();
+        titlePanel.setLayout(new FlowLayout(FlowLayout.LEADING, CONTROL_SPACING, 0));
         enableDayOfWeek = new JCheckBox("Restrict day of week");
-        dayOfWeekPanel.add(enableDayOfWeek);
-        // todo
-        return wrapAroundBorders(dayOfWeekPanel, CONTROL_SPACING, CONTROL_SPACING, CONTROL_SPACING, CONTROL_SPACING);
+        titlePanel.add(enableDayOfWeek);
+        enableDayOfWeek.setSelected(true);
+        dayOfWeekPanel.add(titlePanel);
+        dayOfWeekPanel.add(startDayPanel());
+        dayOfWeekPanel.add(endDayPanel());
+        // wrapper
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.anchor = GridBagConstraints.NORTHWEST;
+        constraints.weightx = 1.0;
+        constraints.weighty = 1.0;
+        constraints.insets = new Insets(CONTROL_SPACING, CONTROL_SPACING, CONTROL_SPACING, CONTROL_SPACING);
+        JPanel bordered = new JPanel();
+        bordered.setLayout(new GridBagLayout());
+        bordered.add(dayOfWeekPanel, constraints);
+        return bordered;
     }
 
     private JPanel makeBottomButtonsPanel() {
@@ -204,6 +246,12 @@ public class TimeRangePropertiesDialog extends JDialog {
         return output;
     }
 
+    private SpinnerListModel weekModel() {
+        SpinnerListModel model = new SpinnerListModel(week);
+        model.setValue(week[3]);
+        return model;
+    }
+
     /**
      * for dev purposes only, to view dlg's layout
      */
@@ -218,13 +266,20 @@ public class TimeRangePropertiesDialog extends JDialog {
     private JButton okButton;
     private JButton cancelButton;
     private JCheckBox enableTimeOfDay;
-    private JCheckBox enableDayOfWeek;
     private JSpinner startHr;
     private JSpinner endHr;
     private JSpinner startMin;
     private JSpinner endMin;
     private JSpinner startSec;
     private JSpinner endSec;
+
+    private JCheckBox enableDayOfWeek;
+    private JSpinner startDay;
+    private JSpinner endDay;
+    private static final String[] week = new String[] {"Sunday", "Monday",
+                                                       "Tuesday", "Wednesday",
+                                                       "Thursday", "Friday",
+                                                       "Saturday"};
 
     private final static int BORDER_PADDING = 20;
     private final static int CONTROL_SPACING = 5;
