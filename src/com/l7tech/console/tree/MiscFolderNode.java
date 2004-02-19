9fa94afa15109c27e406152e168a32505df5f89d
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-02-19 23:47:00 +0000

    new assertion type folder for palette "misc"
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@5234 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/console/tree/MiscFolderNode.java
@@ -0,0 +1,64 @@
+package com.l7tech.console.tree;
+
+/**
+ * The class represents an gui node element in the TreeModel that
+ * represents the misc assertions folder.
+ *
+ * <br/><br/>
+ * LAYER 7 TECHNOLOGIES, INC<br/>
+ * User: flascell<br/>
+ * Date: Feb 19, 2004<br/>
+ * $Id$
+ */
+public class MiscFolderNode extends AbstractTreeNode {
+    /**
+     * construct the <CODE>ProvidersFolderNode</CODE> instance.
+     */
+    public MiscFolderNode() {
+        super(null);
+    }
+
+    /**
+     * Returns true if the receiver is a leaf.
+     *
+     * @return true if leaf, false otherwise
+     */
+    public boolean isLeaf() {
+        return false;
+    }
+
+
+    /**
+     * subclasses override this method
+     */
+    protected void loadChildren() {
+        int index = 0;
+        children = null;
+        insert( new TimeRangePaletteNode(), index++ );
+    }
+
+    /**
+     * Returns the node name.
+     * Gui nodes have name to facilitate handling in
+     * hierarchical gui components such as JTree.
+     *
+     * @return the FQ name as a String
+     */
+    public String getName() {
+        return "Miscellaneous";
+    }
+
+    /**
+     * subclasses override this method specifying the resource name
+     *
+     * @param open for nodes that can be opened, can have children
+     */
+    protected String iconResource(boolean open) {
+        if (open)
+            return "com/l7tech/console/resources/folderOpen.gif";
+
+        return "com/l7tech/console/resources/folder.gif";
+
+    }
+
+}
