390d81d31f56c627d669745080267001ec47835b
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-02-19 23:58:33 +0000

    changed palette name to "Time And Day Availability"
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@5242 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/console/tree/TimeRangePaletteNode.java
+++ src/com/l7tech/console/tree/TimeRangePaletteNode.java
@@ -59,7 +59,7 @@ public class TimeRangePaletteNode extends AbstractTreeNode {
     protected void loadChildren() {}
 
     public String getName() {
-        return "Time Range";
+        return "Time And Day Availability";
     }
 
     /**
