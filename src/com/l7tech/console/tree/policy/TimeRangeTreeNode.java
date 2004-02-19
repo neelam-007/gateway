6ba03027f84eca29d21b70bae0f3c88aeed32de5
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-02-19 23:47:53 +0000

    Policy tree node for TimeRange assertion.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@5237 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/console/tree/policy/TimeRangeTreeNode.java
@@ -0,0 +1,63 @@
+package com.l7tech.console.tree.policy;
+
+import com.l7tech.policy.assertion.Assertion;
+import com.l7tech.policy.assertion.TimeRange;
+import com.l7tech.console.action.TimeRangePropertiesAction;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Policy tree node for TimeRange assertion.
+ *
+ * <br/><br/>
+ * LAYER 7 TECHNOLOGIES, INC<br/>
+ * User: flascell<br/>
+ * Date: Feb 19, 2004<br/>
+ * $Id$
+ * 
+ */
+public class TimeRangeTreeNode extends LeafAssertionTreeNode {
+    public TimeRangeTreeNode(Assertion assertion) {
+        super(assertion);
+        if (assertion instanceof TimeRange) {
+            nodeAssertion = (TimeRange)assertion;
+        } else
+            throw new IllegalArgumentException("assertion passed must be of type " + TimeRange.class.getName());
+    }
+    public String getName() {
+        return "Time And Day Availability";
+    }
+
+    protected String iconResource(boolean open) {
+        // todo, a special icon for this assertion?
+        return "com/l7tech/console/resources/time.gif";
+    }
+
+    /**
+     * Get the set of actions associated with this node.
+     * This may be used e.g. in constructing a context menu.
+     *
+     * @return actions appropriate to the node
+     */
+    public Action[] getActions() {
+        java.util.List list = new ArrayList();
+        list.add(getPreferredAction());
+        list.addAll(Arrays.asList(super.getActions()));
+        return (Action[])list.toArray(new Action[]{});
+    }
+
+    /**
+     * Gets the default action for this node.
+     *
+     * @return <code>null</code> indicating there should be none default action
+     */
+    public Action getPreferredAction() {
+        return new TimeRangePropertiesAction(this);
+    }
+
+    public TimeRange getTimeRange() {return nodeAssertion;}
+
+    private TimeRange nodeAssertion;
+}
