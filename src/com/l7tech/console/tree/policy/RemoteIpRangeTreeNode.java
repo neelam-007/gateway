e8588989d79b6828802f07889783cbd918f77c57
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-02-23 21:47:09 +0000

    Policy tree node for RemoteIpRange assertion.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@5281 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/console/tree/policy/RemoteIpRangeTreeNode.java
@@ -0,0 +1,56 @@
+package com.l7tech.console.tree.policy;
+
+import com.l7tech.policy.assertion.Assertion;
+import com.l7tech.policy.assertion.RemoteIpRange;
+import com.l7tech.console.action.RemoteIpRangePropertiesAction;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Policy tree node for RemoteIpRange assertion.
+ *
+ * <br/><br/>
+ * LAYER 7 TECHNOLOGIES, INC<br/>
+ * User: flascell<br/>
+ * Date: Feb 23, 2004<br/>
+ * $Id$<br/>
+ *
+ */
+public class RemoteIpRangeTreeNode extends LeafAssertionTreeNode {
+    public RemoteIpRangeTreeNode(Assertion assertion) {
+        super(assertion);
+        if (assertion instanceof RemoteIpRange) {
+            nodeAssertion = (RemoteIpRange)assertion;
+        } else
+            throw new IllegalArgumentException("assertion passed must be of type " +
+              RemoteIpRange.class.getName());
+    }
+    public String getName() {
+        return "IP address range";
+    }
+
+    protected String iconResource(boolean open) {
+        return "com/l7tech/console/resources/network.gif";
+    }
+
+    public Action getPreferredAction() {
+        return new RemoteIpRangePropertiesAction(this);
+    }
+
+    public Action[] getActions() {
+        java.util.List list = new ArrayList();
+        list.add(getPreferredAction());
+        list.addAll(Arrays.asList(super.getActions()));
+        return (Action[])list.toArray(new Action[]{});
+    }
+
+    public boolean canDelete() {
+        return true;
+    }
+
+    public RemoteIpRange getAssertion() {return nodeAssertion;}
+
+    private RemoteIpRange nodeAssertion;
+}
