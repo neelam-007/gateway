8e2451601169c700a2b60ba929e6152f6813ebc6
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-02-23 22:12:08 +0000

    Fix boneheaded double use of list, causing CME
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@5283 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/server/transport/jms/JmsAdminImpl.java
+++ src/com/l7tech/server/transport/jms/JmsAdminImpl.java
@@ -40,13 +40,13 @@ public class JmsAdminImpl extends RemoteService implements JmsAdmin {
         Collection found = getConnectionManager().findAll();
         if ( found == null || found.size() < 1 ) return new JmsConnection[0];
 
+        List results = new ArrayList();
         for (Iterator i = found.iterator(); i.hasNext();) {
-            EntityHeader entityHeader = (EntityHeader) i.next();
-            JmsConnection conn = getConnectionManager().findConnectionByPrimaryKey( entityHeader.getOid() );
-            found.add( conn );
+            JmsConnection conn = (JmsConnection)i.next();
+            results.add( conn );
         }
 
-        return (JmsConnection[])found.toArray( new JmsConnection[0] );
+        return (JmsConnection[])results.toArray( new JmsConnection[0] );
     }
 
     public JmsConnection findConnectionByPrimaryKey( long oid ) throws RemoteException, FindException {
@@ -61,49 +61,6 @@ public class JmsAdminImpl extends RemoteService implements JmsAdmin {
             list.add( endpoint.toEntityHeader() );
         }
         return (EntityHeader[])list.toArray( new EntityHeader[0] );
-    }
-
-    public void saveAllMonitoredEndpoints( long[] newSourceOids ) throws RemoteException, FindException, UpdateException {
-        _logger.info( "Saving monitored endpoint list" );
-        HibernatePersistenceContext context = null;
-        try {
-            final JmsEndpointManager endManager = getEndpointManager();
-            context = (HibernatePersistenceContext)PersistenceContext.getCurrent();
-            context.beginTransaction();
-
-            // TODO make us smart! This algorithm is slow!
-
-            // Stop all endpoints that were previously message sources that are no longer
-            Collection oldSourceEnds = endManager.findMessageSourceEndpoints();
-            for ( Iterator i = oldSourceEnds.iterator(); i.hasNext(); ) {
-                final JmsEndpoint oldSourceEndpoint = (JmsEndpoint) i.next();
-                boolean wasMessageSource = false;
-                for ( int j = 0; j < newSourceOids.length; j++ ) {
-                    long oid = newSourceOids[j];
-                    if ( oid == oldSourceEndpoint.getOid() ) wasMessageSource = true;
-                }
-                if ( !wasMessageSource ) {
-                    oldSourceEndpoint.setMessageSource( false );
-                }
-            }
-
-            // Start all endpoints that are newly message sources
-            for ( int i = 0; i < newSourceOids.length; i++ ) {
-                long oid = newSourceOids[i];
-                final JmsEndpoint oldEndpoint = endManager.findByPrimaryKey( oid );
-                if ( oldEndpoint != null && !oldEndpoint.isMessageSource() ) {
-                    oldEndpoint.setMessageSource(true);
-                }
-            }
-
-            context.commitTransaction();
-        } catch ( SQLException e ) {
-            throw new FindException( e.toString(), e );
-        } catch ( TransactionException e ) {
-            throw new FindException( e.toString(), e );
-        } finally {
-            if ( context != null ) context.close();
-        }
     }
 
     public long saveConnection( JmsConnection connection ) throws RemoteException, UpdateException,
