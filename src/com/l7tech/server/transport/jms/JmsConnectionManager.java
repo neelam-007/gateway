67446092e1e03172dfdcb593ded6018e34ab22fe
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-02-24 18:20:27 +0000

    Overrode delete(oid) to cascade-delete endpoints, changed default provider list to OpenJMS
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@5306 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/server/transport/jms/JmsConnectionManager.java
+++ src/com/l7tech/server/transport/jms/JmsConnectionManager.java
@@ -9,11 +9,14 @@ package com.l7tech.server.transport.jms;
 import com.l7tech.common.transport.jms.JmsConnection;
 import com.l7tech.common.transport.jms.JmsProvider;
 import com.l7tech.common.util.Locator;
-import com.l7tech.objectmodel.*;
 import com.l7tech.logging.LogManager;
+import com.l7tech.objectmodel.*;
 
 import java.sql.SQLException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -29,7 +32,7 @@ public class JmsConnectionManager extends HibernateEntityManager {
     public Collection findAllProviders() throws FindException {
         // TODO make this real, eh?!!
         if ( _allProviders == null ) {
-            JmsProvider mqseries = new JmsProvider( "IBM MQSeries", "com.ibm.SomethingOrOther", "QueueConnectionFactory" );
+            JmsProvider mqseries = new JmsProvider( "OpenJMS", "org.exolab.jms.jndi.InitialContextFactory", "QueueConnectionFactory" );
             List list = new ArrayList();
             list.add( mqseries );
             _allProviders = list;
@@ -54,13 +57,36 @@ public class JmsConnectionManager extends HibernateEntityManager {
         }
     }
 
-    public void update( final JmsConnection conn ) throws UpdateException {
+    public void update( final JmsConnection conn ) throws VersionException, UpdateException {
         _logger.info( "Updating JmsConnection " + conn );
 
+        JmsConnection original = null;
+        // check for original connection
         try {
-            PersistenceManager.update( getContext(), conn );
-        } catch (SQLException e) {
-            throw new UpdateException(e.toString(), e);
+            original = findConnectionByPrimaryKey(conn.getOid());
+        } catch (FindException e) {
+            throw new UpdateException("could not get original connection", e);
+        }
+
+        // check version
+        if (original.getVersion() != conn.getVersion()) {
+            logger.severe("db connection has version: " + original.getVersion() + ". requestor connection has version: "
+                          + conn.getVersion());
+            throw new VersionException("the connection you are trying to update is no longer valid.");
+        }
+
+        // update
+        PersistenceContext context = null;
+        try {
+            original.copyFrom(conn);
+
+            context = getContext();
+            PersistenceManager.update(context, original);
+            logger.info( "Updated JmsConnection #" + conn.getOid() );
+
+        } catch ( SQLException se ) {
+            logger.log( Level.SEVERE, se.toString(), se );
+            throw new UpdateException( se.toString(), se );
         }
     }
 
@@ -87,6 +113,16 @@ public class JmsConnectionManager extends HibernateEntityManager {
         } catch ( SQLException e ) {
             throw new DeleteException( e.toString(), e );
         }
+    }
+
+    /**
+     * Overridden to take care of dependent objects
+     * @param oid
+     * @throws DeleteException
+     * @throws FindException
+     */
+    public void delete( long oid ) throws DeleteException, FindException {
+        delete( findConnectionByPrimaryKey( oid ) );
     }
 
     private JmsEndpointManager getEndpointManager() {
