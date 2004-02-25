9d9e8c835784139909dd9e9a70a265028265929a
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-02-25 01:39:17 +0000

    Moved here from Agent inner class
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@5329 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/common/util/CausedIOException.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2003-2004 Layer 7 Technologies Inc.
+ *
+ * $Id$
+ */
+
+package com.l7tech.common.util;
+
+import java.io.IOException;
+
+/**
+ * @author mike
+ * @version $Revision$
+ */
+public class CausedIOException extends IOException {
+    CausedIOException(Throwable cause) {
+        super();
+        initCause(cause);
+    }
+}
