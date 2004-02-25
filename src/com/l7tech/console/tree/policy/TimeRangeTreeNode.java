15549bbdd31dd471cf724c38f6f82c353c8142c2
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-02-25 00:37:38 +0000

    bugfix for issue 603. node now displays the availability in the tree as part of its description
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@5327 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/console/tree/policy/TimeRangeTreeNode.java
+++ src/com/l7tech/console/tree/policy/TimeRangeTreeNode.java
@@ -2,11 +2,14 @@ package com.l7tech.console.tree.policy;
 
 import com.l7tech.policy.assertion.Assertion;
 import com.l7tech.policy.assertion.TimeRange;
+import com.l7tech.policy.assertion.TimeOfDay;
+import com.l7tech.policy.assertion.TimeOfDayRange;
 import com.l7tech.console.action.TimeRangePropertiesAction;
 
 import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 
 /**
  * Policy tree node for TimeRange assertion.
@@ -16,7 +19,7 @@ import java.util.Arrays;
  * User: flascell<br/>
  * Date: Feb 19, 2004<br/>
  * $Id$
- * 
+ *
  */
 public class TimeRangeTreeNode extends LeafAssertionTreeNode {
     public TimeRangeTreeNode(Assertion assertion) {
@@ -27,7 +30,23 @@ public class TimeRangeTreeNode extends LeafAssertionTreeNode {
             throw new IllegalArgumentException("assertion passed must be of type " + TimeRange.class.getName());
     }
     public String getName() {
-        return "Time And Day Availability";
+        if (nodeAssertion != null && (nodeAssertion.isControlDay() || nodeAssertion.isControlTime())) {
+            String nodeName = "Available ";
+            if (nodeAssertion.isControlDay()) {
+                nodeName += week[nodeAssertion.getStartDayOfWeek()-1] +
+                            " trough " + week[nodeAssertion.getEndDayOfWeek()-1] + " ";
+            }
+
+            if (nodeAssertion.isControlTime() && nodeAssertion.getTimeRange() != null) {
+                TimeOfDayRange tr = nodeAssertion.getTimeRange();
+
+                nodeName += "from " + timeToString(utcToLocalTime(tr.getFrom())) + " to " +
+                                      timeToString(utcToLocalTime(tr.getTo()));
+            }
+
+            return nodeName;
+        }
+        else return "No availability defined";
     }
 
     protected String iconResource(boolean open) {
@@ -61,7 +80,44 @@ public class TimeRangeTreeNode extends LeafAssertionTreeNode {
         return true;
     }
 
+    private String timeToString(TimeOfDay tod) {
+        return (tod.getHour() < 10 ? "0" : "") + tod.getHour() +
+               (tod.getMinute() < 10 ? ":0" : ":") + tod.getMinute() +
+               (tod.getSecond() < 10 ? ":0" : ":") + tod.getSecond();
+    }
+
+    private TimeOfDay utcToLocalTime(TimeOfDay utc) {
+        int totOffsetInMin = Calendar.getInstance().getTimeZone().getRawOffset() / (1000*60);
+        int hroffset = totOffsetInMin/60;
+        int minoffset = totOffsetInMin%60;
+        int utchr = utc.getHour() + hroffset;
+        int utcmin = utc.getMinute() + minoffset;
+        while (utcmin >= 60) {
+            ++utchr;
+            utcmin -= 60;
+        }
+        while (utchr >= 24) {
+            utchr -= 24;
+        }
+        while (utcmin < 0) {
+            --utchr;
+            utcmin += 60;
+        }
+        while (utchr < 0) {
+            utchr += 24;
+        }
+        return new TimeOfDay(utchr, utcmin, utc.getSecond());
+    }
+
     public TimeRange getTimeRange() {return nodeAssertion;}
 
     private TimeRange nodeAssertion;
+
+    private static final String[] week = {"Sunday",
+                                          "Monday",
+                                          "Tuesday",
+                                          "Wednesday",
+                                          "Thursday",
+                                          "Friday",
+                                          "Saturday"};
 }
