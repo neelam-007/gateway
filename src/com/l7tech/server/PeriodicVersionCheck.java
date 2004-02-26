18bf9b10f9dfb1226959eac78f7464fab7a9bbf6
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-02-26 00:08:22 +0000

    Clean up cah-razy formatting
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@5343 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/server/PeriodicVersionCheck.java
+++ src/com/l7tech/server/PeriodicVersionCheck.java
@@ -25,105 +25,102 @@ public abstract class PeriodicVersionCheck extends TimerTask {
     }
 
     public synchronized void run() {
+        PersistenceContext context = null;
+
         try {
-            PersistenceContext context = null;
+            context = PersistenceContext.getCurrent();
+        } catch (SQLException e) {
+            logger.log(Level.SEVERE, "error getting persistence context. " +
+                                     "this version check is stopping prematurely", e);
+            return;
+        }
+
+        try {
+            Map dbversions = null;
 
             try {
-                context = PersistenceContext.getCurrent();
-            } catch (SQLException e) {
-                logger.log(Level.SEVERE, "error getting persistence context. " +
+                context.beginTransaction();
+            } catch (TransactionException e) {
+                logger.log(Level.SEVERE, "error begining transaction. " +
                                          "this version check is stopping prematurely", e);
                 return;
             }
 
+            // get db versions
             try {
-                Map dbversions = null;
+                dbversions = _manager.findVersionMap();
+            } catch (FindException e) {
+                logger.log(Level.SEVERE, "error getting versions. " +
+                                         "this version check is stopping prematurely", e);
+                return;
+            }
 
-                try {
-                    context.beginTransaction();
-                } catch (TransactionException e) {
-                    logger.log(Level.SEVERE, "error begining transaction. " +
-                                             "this version check is stopping prematurely", e);
-                    return;
-                }
+            // actual check logic
+            List updatesAndAdditions = new ArrayList();
+            List deletions = new ArrayList();
 
-                // get db versions
-                try {
-                    dbversions = _manager.findVersionMap();
-                } catch (FindException e) {
-                    logger.log(Level.SEVERE, "error getting versions. " +
-                                             "this version check is stopping prematurely", e);
-                    return;
-                }
+            // 1. check that all that is in db is present in cache and that version is same
+            for (Iterator i = dbversions.keySet().iterator(); i.hasNext();) {
+                Long oid = (Long)i.next();
 
-                // actual check logic
-                List updatesAndAdditions = new ArrayList();
-                List deletions = new ArrayList();
+                // is it already in cache?
+                Integer cachedVersion = (Integer)_cachedVersionMap.get(oid);
 
-                // 1. check that all that is in db is present in cache and that version is same
-                for (Iterator i = dbversions.keySet().iterator(); i.hasNext();) {
-                    Long oid = (Long)i.next();
-
-                    // is it already in cache?
-                    Integer cachedVersion = (Integer)_cachedVersionMap.get(oid);
-
-                    if (cachedVersion == null) {
-                        logger.fine( "Entity " + oid + " is new." );
-                        updatesAndAdditions.add(oid);
-                    } else {
-                        // check actual version
-                        Integer dbversion = (Integer)dbversions.get(oid);
-
-                        if ( !dbversion.equals(cachedVersion) ) {
-                            updatesAndAdditions.add(oid);
-                            logger.fine("Entity " + oid + " has been updated.");
-                        }
-                    }
-                }
-
-                // 2. check for things in cache not in db (deletions)
-                for (Iterator i = _cachedVersionMap.keySet().iterator(); i.hasNext();) {
-                    Long oid = (Long)i.next();
-                    if (dbversions.get(oid) == null) {
-                        deletions.add(oid);
-                        logger.fine("Entity " + oid + " has been deleted.");
-                    }
-                }
-
-                // 3. make the updates
-                if (updatesAndAdditions.isEmpty() && deletions.isEmpty()) {
-                    // nothing to do. we're done
+                if (cachedVersion == null) {
+                    logger.fine( "Entity " + oid + " is new." );
+                    updatesAndAdditions.add(oid);
                 } else {
-                    for (Iterator i = updatesAndAdditions.iterator(); i.hasNext();) {
-                        Long updatedOid = (Long)i.next();
-                        Entity updatedEntity = null;
-                        try {
-                            updatedEntity = _manager.findEntity(updatedOid.longValue());
-                        } catch (FindException e) {
-                            updatedEntity = null;
-                            logger.log(Level.WARNING, "Entity that was updated or created " +
-                                                      "cannot be retrieved", e);
-                        }
-                        if (updatedEntity != null) {
-                            addOrUpdate(updatedEntity);
-                        } // otherwise, next version check shall delete this service from cache
-                    }
-                    for (Iterator i = deletions.iterator(); i.hasNext();) {
-                        Long key = (Long)i.next();
-                        remove(key);
-                    }
-                }
+                    // check actual version
+                    Integer dbversion = (Integer)dbversions.get(oid);
 
-                // close hib transaction
-                try {
-                    context.rollbackTransaction();
-                } catch (TransactionException e) {
-                    logger.log(Level.WARNING, "error rollbacking transaction", e);
+                    if ( !dbversion.equals(cachedVersion) ) {
+                        updatesAndAdditions.add(oid);
+                        logger.fine("Entity " + oid + " has been updated.");
+                    }
                 }
-            } finally {
-                context.close();
+            }
+
+            // 2. check for things in cache not in db (deletions)
+            for (Iterator i = _cachedVersionMap.keySet().iterator(); i.hasNext();) {
+                Long oid = (Long)i.next();
+                if (dbversions.get(oid) == null) {
+                    deletions.add(oid);
+                    logger.fine("Entity " + oid + " has been deleted.");
+                }
+            }
+
+            // 3. make the updates
+            if (updatesAndAdditions.isEmpty() && deletions.isEmpty()) {
+                // nothing to do. we're done
+            } else {
+                for (Iterator i = updatesAndAdditions.iterator(); i.hasNext();) {
+                    Long updatedOid = (Long)i.next();
+                    Entity updatedEntity = null;
+                    try {
+                        updatedEntity = _manager.findEntity(updatedOid.longValue());
+                    } catch (FindException e) {
+                        updatedEntity = null;
+                        logger.log(Level.WARNING, "Entity that was updated or created " +
+                                                  "cannot be retrieved", e);
+                    }
+                    if (updatedEntity != null) {
+                        addOrUpdate(updatedEntity);
+                    } // otherwise, next version check shall delete this service from cache
+                }
+                for (Iterator i = deletions.iterator(); i.hasNext();) {
+                    Long key = (Long)i.next();
+                    remove(key);
+                }
+            }
+
+            // close hib transaction
+            try {
+                context.rollbackTransaction();
+            } catch (TransactionException e) {
+                logger.log(Level.WARNING, "error rollbacking transaction", e);
             }
         } finally {
+            context.close();
         }
     }
 
