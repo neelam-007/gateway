b805db0c904a8b9c756bf53e2e3f0d0d0a63e0e2
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-02-27 22:39:26 +0000

    Added to avoid sending JMSExceptions to manager
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@5376 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/common/transport/jms/JmsTestException.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2003-2004 Layer 7 Technologies Inc.
+ *
+ * $Id$
+ */
+
+package com.l7tech.common.transport.jms;
+
+/**
+ * @author alex
+ * @version $Revision$
+ */
+public class JmsTestException extends Exception {
+    public JmsTestException() {
+        super();
+    }
+
+    public JmsTestException( String message ) {
+        super( message );
+    }
+}
