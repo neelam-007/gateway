8803b8e670bf2010f2962a7e38a21b3271c8848f
emil <emil@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-03-03 05:21:21 +0000

    Resolve policy implementation
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@5406 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- tests/com/l7tech/policy/assertion/ext/CustomAssertionsRegistrarStub.java
+++ tests/com/l7tech/policy/assertion/ext/CustomAssertionsRegistrarStub.java
@@ -1,11 +1,20 @@
 package com.l7tech.policy.assertion.ext;
 
 import com.l7tech.policy.assertion.CustomAssertionHolder;
+import com.l7tech.policy.assertion.Assertion;
+import com.l7tech.policy.wsp.WspReader;
+import com.l7tech.objectmodel.EntityHeader;
+import com.l7tech.objectmodel.ObjectNotFoundException;
+import com.l7tech.identity.StubDataStore;
+import com.l7tech.service.PublishedService;
 
 import java.rmi.RemoteException;
+import java.rmi.ServerException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.io.IOException;
 
 /**
  * @author emil
@@ -33,5 +42,20 @@ public class CustomAssertionsRegistrarStub implements CustomAssertionsRegistrar 
         list.add(ca);
         return list;
 
+    }
+
+    public Assertion resolvePolicy(EntityHeader eh) throws RemoteException, ObjectNotFoundException {
+        final Map publishedServices = StubDataStore.defaultStore().getPublishedServices();
+        PublishedService svc = (PublishedService)publishedServices.get(new Long(eh.getOid()));
+        if (svc == null) {
+            throw new ObjectNotFoundException("service "+eh);
+        }
+        try {
+            return WspReader.parse(svc.getPolicyXml());
+        } catch (IOException e) {
+            ServerException se = new  ServerException(e.getMessage());
+            se.initCause(e);
+            throw se;
+        }
     }
 }
