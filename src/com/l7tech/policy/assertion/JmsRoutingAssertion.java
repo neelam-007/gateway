bea08bdb8a152ad56cb395a4f685f677af6b138c
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-03-05 23:03:44 +0000

    Added responseTimeout property, defaulting to 10s
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@5436 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/policy/assertion/JmsRoutingAssertion.java
+++ src/com/l7tech/policy/assertion/JmsRoutingAssertion.java
@@ -6,14 +6,11 @@
 
 package com.l7tech.policy.assertion;
 
-import com.l7tech.objectmodel.Entity;
-
 /**
  * Holds information needed to route a message to an outbound JMS destination.
  */
 public class JmsRoutingAssertion extends RoutingAssertion {
-    private Long endpointOid = null;
-    private String endpointName = null;
+    public static final int DEFAULT_TIMEOUT = 10000;
 
     /**
      * @return the OID of the JMS endpoint, or null if there isn't one.
@@ -30,12 +27,45 @@ public class JmsRoutingAssertion extends RoutingAssertion {
         this.endpointOid = endpointOid;
     }
 
-    /** @return the name of this endpoint if known, for cosmetic purposes only. */
+    /**
+     * The name of the {@link com.l7tech.common.transport.jms.JmsEndpoint}.
+     * @return the name of this endpoint if known, for cosmetic purposes only. 
+     */
     public String getEndpointName() {
         return endpointName;
     }
 
+    /**
+     * The name of the {@link com.l7tech.common.transport.jms.JmsEndpoint}.
+     * @param endpointName the name of this endpoint if known, for cosmetic purposes only.
+     */
     public void setEndpointName(String endpointName) {
         this.endpointName = endpointName;
     }
+
+    /**
+     * The time, in milliseconds, that the SSG should wait for a response from the protected service.
+     * After this timeout has lapsed, the request fails.
+     *
+     * Defaults to {@link JmsRoutingAssertion.DEFAULT_TIMEOUT}.
+     * @return the response timeout (in milliseconds)
+     */
+    public int getResponseTimeout() {
+        return responseTimeout;
+    }
+
+    /**
+     * The time, in milliseconds, that the SSG should wait for a response from the protected service.
+     * After this timeout has lapsed, the request fails.
+     *
+     * Defaults to {@link JmsRoutingAssertion.DEFAULT_TIMEOUT}.
+     * @param responseTimeout the response timeout (in milliseconds)
+     */
+    public void setResponseTimeout( int responseTimeout ) {
+        this.responseTimeout = responseTimeout;
+    }
+
+    private Long endpointOid = null;
+    private String endpointName = null;
+    private int responseTimeout = DEFAULT_TIMEOUT;
 }
