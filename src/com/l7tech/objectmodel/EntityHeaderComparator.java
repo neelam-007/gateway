8c51ec8ecf137904e32eda415d0a3d52424847e6
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-03-12 00:09:53 +0000

    fix for issue #645 compare user and group header with same name
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@5485 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/objectmodel/EntityHeaderComparator.java
+++ src/com/l7tech/objectmodel/EntityHeaderComparator.java
@@ -14,13 +14,18 @@ public class EntityHeaderComparator implements Comparator {
     public int compare(Object o1, Object o2) {
         EntityHeader h1 = (EntityHeader)o1;
         EntityHeader h2 = (EntityHeader)o2;
-        if (h1.getName() == null && h2.getName() == null) return 0;
+        if (h1.getName() == null && h2.getName() == null) {
+            return h1.getType().getVal() - h2.getType().getVal();
+        }
         else if (h1.getName() == null) return 1;
         else if (h2.getName() == null) return -1;
         int res = h1.getName().compareToIgnoreCase(h2.getName());
         if (res == 0) {
             res = h1.getName().compareTo(h2.getName());
         }
-        return res;
+        if (res == 0) {
+            return h1.getType().getVal() - h2.getType().getVal();
+        }
+        else return res;
     }
 }
