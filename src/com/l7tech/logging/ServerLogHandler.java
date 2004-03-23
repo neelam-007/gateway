3e25d079579dde45bb3491f1e0f11ff1b9995faf
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-03-23 01:01:10 +0000

    bugfix for issue #678 1. no longer cleans old records from database 2. search on oid instead of sequence number
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@5567 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/logging/ServerLogHandler.java
+++ src/com/l7tech/logging/ServerLogHandler.java
@@ -108,14 +108,14 @@ public class ServerLogHandler extends Handler {
                                         " where " + TABLE_NAME + "." + NODEID_COLNAME + " = \'" +
                                         reqnode + "\'";
         if (lowMsgNumber < 0 && highMsgNumber >= 0) {
-            selStatement += " and " + TABLE_NAME + "." + SEQ_COLNAME + " < " + highMsgNumber;
+            selStatement += " and " + TABLE_NAME + "." + OID_COLNAME + " < " + highMsgNumber;
         } else if (lowMsgNumber >= 0 && highMsgNumber < 0) {
-            selStatement += " and " + TABLE_NAME + "." + SEQ_COLNAME + " > " + lowMsgNumber;
+            selStatement += " and " + TABLE_NAME + "." + OID_COLNAME + " > " + lowMsgNumber;
         } else if (lowMsgNumber >= 0 && highMsgNumber >= 0) {
-            selStatement += " and " + TABLE_NAME + "." + SEQ_COLNAME + " > " + lowMsgNumber +
-                            " and " + TABLE_NAME + "." + SEQ_COLNAME + " < " + highMsgNumber;
+            selStatement += " and " + TABLE_NAME + "." + OID_COLNAME + " > " + lowMsgNumber +
+                            " and " + TABLE_NAME + "." + OID_COLNAME + " < " + highMsgNumber;
         }
-        selStatement += " order by " + TABLE_NAME + "." + SEQ_COLNAME + " desc limit " + size;
+        selStatement += " order by " + TABLE_NAME + "." + OID_COLNAME + " desc limit " + size;
 
         List res = null;
         try {
@@ -186,7 +186,7 @@ public class ServerLogHandler extends Handler {
      * and flushing new cached log entries to database.
      * this method is responsible to manager its own persistence context.
      */
-    protected void cleanAndFlush(boolean fullClean) {
+    protected void cleanAndFlush(/*boolean fullClean*/) {
         // get the persistence context
         HibernatePersistenceContext context = null;
         Session session = null;
@@ -206,13 +206,13 @@ public class ServerLogHandler extends Handler {
         try {
             context.beginTransaction();
 
-            if (fullClean) {
+            /*if (fullClean) {
                 String deleteSQLStatement = "from " + TABLE_NAME + " in class " + SSGLogRecord.class.getName() +
                                             " where " + TABLE_NAME + "." + NODEID_COLNAME +
                                             " = \'" + nodeid + "\'";
                 session.delete(deleteSQLStatement);
                 session.flush();
-            } /*else {
+            } else {
                 // delete records older than 48 hrs
                 long deadline = System.currentTimeMillis();
                 deadline -= HOW_LONG_WE_KEEP_LOGS;
@@ -224,9 +224,9 @@ public class ServerLogHandler extends Handler {
             // flush new records
             flushtodb(session);
             context.commitTransaction();
-        } catch (HibernateException e) {
+        } /*catch (HibernateException e) {
             reportException("error deleting old records", e);
-        } catch(TransactionException e) {
+        }*/ catch(TransactionException e) {
             reportException("Exception with hibernate transaction", e);
         } finally {
             if ( context != null ) context.close();
@@ -259,7 +259,7 @@ public class ServerLogHandler extends Handler {
                                 "this log handler will stop dumping logs to db", null);
                 return;
             }
-            boolean fullclean = true;
+            //boolean fullclean = true;
             while (true) {
                 try {
                     sleep(FLUSH_FREQUENCY);
@@ -269,11 +269,11 @@ public class ServerLogHandler extends Handler {
                     break;
                 }
                 try {
-                    parent.cleanAndFlush(fullclean);
+                    parent.cleanAndFlush(/*fullclean*/);
                 } catch (Throwable e) {
                     reportException("unhandled exception", e);
                 }
-                fullclean = false;
+                //fullclean = false;
             }
         }
         ServerLogHandler parent = null;
@@ -288,10 +288,10 @@ public class ServerLogHandler extends Handler {
     private static final LogDumper flusherDeamon = new LogDumper();
     private static final long FLUSH_FREQUENCY = 6000;
     //private static final long FLUSH_FREQUENCY = 10;
-    private static final long HOW_LONG_WE_KEEP_LOGS = (48*3600*1000);
+    //private static final long HOW_LONG_WE_KEEP_LOGS = (48*3600*1000);
 
-    private static final String TIMESTAMP_COLNAME = "millis";
+    //private static final String TIMESTAMP_COLNAME = "millis";
     private static final String TABLE_NAME = "ssg_logs";
     private static final String NODEID_COLNAME = "nodeId";
-    private static final String SEQ_COLNAME = "sequenceNumber";
+    private static final String OID_COLNAME = "oid";
 }
