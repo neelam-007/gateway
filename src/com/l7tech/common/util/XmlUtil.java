7178dab1cbf35a013799a235ce4f72ee4c5a1474
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-04-01 00:02:42 +0000

    Added new XmlUtil findChild... methods
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@5659 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/common/util/XmlUtil.java
+++ src/com/l7tech/common/util/XmlUtil.java
@@ -10,6 +10,8 @@ import org.apache.xml.serialize.OutputFormat;
 import org.apache.xml.serialize.XMLSerializer;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -21,6 +23,8 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.StringWriter;
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  * Thread-local XML parsing and pretty-printing utilities.
@@ -86,5 +90,72 @@ public class XmlUtil {
         getXmlSerializer().setOutputCharStream(sw);
         getXmlSerializer().serialize(element);
         return sw.toString();
+    }
+
+    /**
+     * Finds the first child {@link Element} of a parent {@link Element}.
+     * @param parent the {@link Element} in which to search for children. Must be non-null.
+     * @return First child {@link Element} or null if the specified parent contains no elements
+     */
+    public static Element findFirstChildElement( Element parent ) {
+        NodeList children = parent.getChildNodes();
+        for ( int i = 0; i < children.getLength(); i++ ) {
+            Node n = children.item(i);
+            if ( n.getNodeType() == Node.ELEMENT_NODE ) return (Element)n;
+        }
+        return null;
+    }
+
+    /**
+     * Finds the first child {@link Element} of a parent {@link Element}
+     * with the specified name that is in the specified namespace.
+     *
+     * The parent must belong to a DOM produced by a namespace-aware parser,
+     * and the name must be undecorated.
+     *
+     * @param parent the {@link Element} in which to search for children. Must be non-null.
+     * @param nsuri the URI of the namespace to which the child must belong, NOT THE PREFIX!  Must be non-null.
+     * @param name the name of the element to find. Must be non-null.
+     * @return First matching child {@link Element} or null if the specified parent contains no matching elements
+     */
+    public static Element findFirstChildElementByName( Element parent, String nsuri, String name ) {
+        if ( nsuri == null || name == null ) throw new IllegalArgumentException( "nsuri and name must be non-null!" );
+        NodeList children = parent.getChildNodes();
+        for ( int i = 0; i < children.getLength(); i++ ) {
+            Node n = children.item(i);
+            if ( n.getNodeType() == Node.ELEMENT_NODE &&
+                 name.equals( n.getLocalName()) &&
+                 nsuri.equals( n.getNamespaceURI() ) )
+                return (Element)n;
+        }
+        return null;
+    }
+
+    /**
+     * Returns a list of all child {@link Element}s of a parent {@link Element}
+     * with the specified name that are in the specified namespace.
+     *
+     * The parent must belong to a DOM produced by a namespace-aware parser,
+     * and the name must be undecorated.
+     *
+     * @param parent the {@link Element} in which to search for children. Must be non-null.
+     * @param nsuri the URI of the namespace to which the children must belong, NOT THE PREFIX!  Must be non-null.
+     * @param name the name of the elements to find. Must be non-null.
+     * @return A {@link List} containing all matching child {@link Element}s. Will be empty if the specified parent contains no matching elements
+     */
+    public static List findChildElementsByName( Element parent, String nsuri, String name ) {
+        if ( nsuri == null || name == null ) throw new IllegalArgumentException( "nsuri and name must be non-null!" );
+        List found = new ArrayList();
+
+        NodeList children = parent.getChildNodes();
+        for ( int i = 0; i < children.getLength(); i++ ) {
+            Node n = children.item(i);
+            if ( n.getNodeType() == Node.ELEMENT_NODE &&
+                 name.equals( n.getLocalName()) &&
+                 nsuri.equals( n.getNamespaceURI() ) )
+                found.add( n );
+        }
+
+        return found;
     }
 }
