7f841f40bb2612fb89dfc85c0b614a77f652513c
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-04-30 17:08:55 +0000

    New helpset
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@6084 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/console/resources/helpset/SecureSpan_Manager_Help_System.hs
@@ -0,0 +1,33 @@
+<?xml version='1.0' encoding='ISO-8859-1' ?>
+<!DOCTYPE helpset PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 1.0//EN" "http://java.sun.com/products/javahelp/helpset_1_0.dtd">
+
+<helpset version="1.0">
+  <title>SecureSpan Manager Help System</title>
+   <maps>
+    <homeID>welcome_htm</homeID>
+    <mapref location="SecureSpan_Manager_Help_System_map.xml" />
+  </maps>
+
+  <view>
+    <name>TOC</name>
+    <label>Table of Contents</label>
+    <type>javax.help.TOCView</type>
+    <data>SecureSpan_Manager_Help_System_toc.xml</data>
+  </view>
+
+  <view>
+    <name>Index</name>
+    <label>Index</label>
+    <type>javax.help.IndexView</type>
+    <data>SecureSpan_Manager_Help_System_ndx.xml</data>
+  </view>
+
+  <view>
+    <name>Search</name>
+    <label>Search</label>
+    <type>javax.help.SearchView</type>
+    <data engine="com.sun.java.help.search.DefaultSearchEngine">SecureSpan_Manager_Help_System_JavaHelpSearch</data>
+  </view>
+
+
+</helpset>
