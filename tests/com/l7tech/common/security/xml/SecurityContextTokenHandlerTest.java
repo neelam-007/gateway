2e5edf0b204013b2f2cd93825d6a1d5de5f5a23d
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-05-10 23:58:19 +0000

    tester for new class SecurityCOntextTokenHandler
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@6133 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ tests/com/l7tech/common/security/xml/SecurityContextTokenHandlerTest.java
@@ -0,0 +1,67 @@
+package com.l7tech.common.security.xml;
+
+import junit.framework.TestCase;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.apache.xml.serialize.XMLSerializer;
+import org.apache.xml.serialize.OutputFormat;
+
+import javax.xml.parsers.DocumentBuilder;
+
+import com.ibm.xml.dsig.util.DOMParserNS;
+import com.ibm.xml.sax.StandardErrorHandler;
+
+import java.io.StringReader;
+import java.io.StringWriter;
+
+/**
+ * Tests for SecurityContextTokenHandler class.
+ *
+ * <p/>
+ * <br/><br/>
+ * LAYER 7 TECHNOLOGIES, INC<br/>
+ * User: flascell<br/>
+ * Date: May 10, 2004<br/>
+ * $Id$
+ */
+public class SecurityContextTokenHandlerTest extends TestCase {
+
+    public void testAppendAndReadSession() throws Exception {
+        Document doc = readDocFromString(simpleDoc);
+        System.out.println("Original doc");
+        System.out.println(serializeDocWithXMLSerializer(doc));
+        byte[] sessionid = SecurityContextTokenHandler.generateNewSessionId();
+        SecurityContextTokenHandler.appendSessionInfoToSoapMessage(doc, sessionid, 69);
+        System.out.println("Modified doc");
+        System.out.println(serializeDocWithXMLSerializer(doc));
+        // todo test that we can get sesionid out of this.
+    }
+
+    private Document readDocFromString(String docStr)  throws Exception {
+        DocumentBuilder builder = DOMParserNS.createBuilder();
+        builder.setErrorHandler(new StandardErrorHandler());
+        return builder.parse(new InputSource(new StringReader(docStr)));
+    }
+
+    public String serializeDocWithXMLSerializer(Document doc) throws Exception {
+        final StringWriter sw = new StringWriter();
+        XMLSerializer xmlSerializer = new XMLSerializer();
+        xmlSerializer.setOutputCharStream(sw);
+        try {
+            OutputFormat of = new OutputFormat();
+            of.setIndent(4);
+            xmlSerializer.setOutputFormat(of);
+            xmlSerializer.serialize(doc);
+        } catch (Exception e) {}
+        return sw.toString();
+    }
+
+    private String simpleDoc = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
+                                "<S:Envelope xmlns:S=\"http://www.w3.org/2001/12/soap-envelope\">" +
+                                    "<S:Body>" +
+                                        "<tru:StockSymbol xmlns:tru=\"http://fabrikam123.com/payloads\">" +
+                                            "QQQ" +
+                                        "</tru:StockSymbol>" +
+                                    "</S:Body>" +
+                                "</S:Envelope>";
+}
