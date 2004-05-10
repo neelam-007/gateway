6f3e2846b15883942d55ebe9e5d3f625cee20821
hchan <hchan@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-05-10 18:37:11 +0000

    made this into a service script
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@6128 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- etc/dbfaildetect.sh
+++ etc/dbfaildetect.sh
@@ -1,6 +1,11 @@
 #/bin/sh
 ###########################################################################
-# This program will detect if a MySQL DB cluster on IP address X.X.X.X is up.
+#
+# Startup script for SSG db fail over
+#
+# chkconfig: - 99 01
+# description: This program will detect if a MySQL DB cluster on 
+# IP address X.X.X.X is up.
 # If it fails to connect to this DB cluster after Y tries, this script 
 # will attempt to perform an IP takeover of address X.X.X.X
 # IP takeover involves:
@@ -14,19 +19,21 @@
 # by Jay Thorne - jthorne@layer7tech.com
 ###########################################################################
 
+. /etc/init.d/functions
+
 # programs used
 IFCONFIG="/sbin/ifconfig"
-GARP="/usr/local/bin/garp"
+GARP="/sbin/garp"
 WGET="/usr/bin/wget"
 NC="/usr/bin/nc"
 # globals and configuration
-
-EMAIL="support@layer7tech.com"
+PIDFILE=/var/run/dbfaildetect.pid
+EMAIL="hchan@layer7tech.com"
 
 DBHOST="10.7.7.77"
 # cluster IP addresss
 
-INTERFACE="eth0"
+INTERFACE="eth1"
 INTERFACE_ALIAS="$INTERFACE:MYSQLDB"
 # alias interface
 
@@ -98,5 +105,29 @@ doget() {
 	fi
 }
 
-# after all those definitions now call the entry point.
-doget
+
+
+
+# See how we were called.
+case "$1" in
+  start)
+	echo "Starting $0"
+	sh -c "$0 doget" &
+	echo $! > $PIDFILE
+	;;
+  stop)
+	echo "Stopping $0"
+	kill -9 `cat $PIDFILE`
+	;;
+  restart)
+	stop
+	start
+	;;
+  doget)
+	doget
+	;;
+  *)
+	echo "Usage: $0 {start|stop|restart}"
+	exit 1
+esac
+
