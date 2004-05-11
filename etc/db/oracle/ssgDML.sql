f013c735f131934dc0814701fbc5158b71eb709a
jay <jay@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-05-11 00:12:32 +0000

    Real working sql, including source data. Note that 255 chars is a LOT small for an xml policy.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@6136 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- etc/db/oracle/ssgDML.sql
+++
@@ -1,9 +0,0 @@
-/* this file should eventually be deprecated with
-Hibernate 2.2 custom sql statements
-i.e. <sql-insert>....</sql-insert> inside SSG.hbm.xml
-*/
-/* INSERT INTO hibernate_unique_key VALUES (70); */
-INSERT INTO internal_group VALUES (2,0,'Gateway Administrators','Admin console users having administration rights to the gateway');
-INSERT INTO internal_user VALUES (3,0,'admin','admin','a41306e4b1b5858d3e3d705dd2e738e2','fname','lname','email','title');
-INSERT INTO internal_user_group VALUES (3,2);
-
