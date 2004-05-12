d2e62d1753ffc034d4e2fc0bc0f65358f540abd6
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-05-12 00:14:44 +0000

    doced
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@6154 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/common/security/xml/SecurityContextTokenHandler.java
+++ src/com/l7tech/common/security/xml/SecurityContextTokenHandler.java
@@ -16,7 +16,7 @@ import java.util.Date;
 
 /**
  * Appends and parses out xml security session information in/out of soap messages.
- * The specifications used here are WS-Secure Conversation
+ * The specifications used here are WS-Secure Conversation and WS-Trust versions 1.1.
  *
  * This is not yet plugged in and will eventually phase out the class SecureConversationTokenHandler.
  *
