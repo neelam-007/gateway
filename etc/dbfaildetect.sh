cc299c949bc287aab35d27a74c221384caed50a9
jay <jay@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-05-12 23:13:48 +0000

    bug 877: check network connectivity before we assume the other node is dead
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@6162 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- etc/dbfaildetect.sh
+++ etc/dbfaildetect.sh
@@ -4,34 +4,33 @@
 # Startup script for SSG db fail over
 #
 # chkconfig: - 99 01
-# description: This program will detect if a MySQL DB cluster on 
-# IP address X.X.X.X is up.
-# If it fails to connect to this DB cluster after Y tries, this script 
-# will attempt to perform an IP takeover of address X.X.X.X
-# IP takeover involves:
-#   a) an ifconfig alias on a certain interface
-#   b) a gratuitous ARP call
-# 
-#
-#
 # Property of Layer 7 Technologies
 #
 # by Jay Thorne - jthorne@layer7tech.com
 ###########################################################################
 
-. /etc/init.d/functions
+#. /etc/init.d/functions
 
 # programs used
 IFCONFIG="/sbin/ifconfig"
 GARP="/usr/local/bin/garp"
 WGET="/usr/bin/wget"
 NC="/usr/bin/nc"
+PING="/bin/ping"
+
 # globals and configuration
 PIDFILE=/var/run/dbfaildetect.pid
 EMAIL="hchan@layer7tech.com"
 
 DBHOST="10.7.7.77"
 # cluster IP addresss
+
+DEFAULT_ROUTE=`/sbin/route -n | egrep ^0.0.0.0 | cut -c 17-32 | tr -d [:blank:]`
+
+if [ -z "$DEFAULT_ROUTE" ] ;  then
+     echo "No default route, Exiting"
+     exit
+fi
 
 INTERFACE="eth0"
 INTERFACE_ALIAS="$INTERFACE:1"
@@ -59,13 +58,47 @@ SLEEPRETRY=10
 #
 # Subroutines
 #
+desc() {
+tries=`expr $TRIES + 1`
+cat <<EOF
+
+This program will detect if a MySQL DB instance is available
+at $DBHOST / port $PORT.
+If it fails to connect to this DB after $tries tries, this script
+will attempt to perform an IP takeover of address $DBHOST
+if this machine has ICMP connectivity to its default route.
+
+If this machine has the alias already up, it will then test
+if connectivity to the default route has failed and if so
+relinquish the cluster address, assuming that the machine
+is disconnected from the network at the moment.
+
+IP takeover involves:
+  a) creating an alias on $INTERFACE_ALIAS
+  b) sending a gratuitous ARP packet to inform other machines
+     that the previous arp address is no longer valid
+
+
+EOF
+}
+
+selfmaster() {
+    iface=`/sbin/ifconfig | grep $INTERFACE_ALIAS`
+    if [ -z "$iface" ] ; then
+        echo "Alias Interface not found"
+        return 1
+    else
+        echo "Alias interface found"
+        return 0
+    fi
+}
 success() {
 	rm -f $TRYAGAINFILE
 	sleep $SLEEPRETRY
 }
 
 failure() {
-	echo "Failed get page"
+	echo "Failed get connection"
 	if ! [ -f $TRYAGAINFILE ] ; then
 		echo "Setting first failure"
 		echo
@@ -73,32 +106,73 @@ failure() {
 		sleep $RETEST
 	else
 		echo "Failed Twice. Time for action"
-		echo
-		doiptakeover
+		if  pinggw; then
+		    echo "Other node down, network is alive, taking over as master"
+		    doiptakeover
+		else
+		    echo "Network down, not attempting to take over as master"
+		fi
 		sleep $RETEST
 		rm $TRYAGAINFILE
 	fi
 }
 
 doiptakeover() {
-	# FIXME: currently Defanged
+    date
+    echo
 	echo "Taking over IP $DBHOST";
 	$IFCONFIG $INTERFACE_ALIAS $DBHOST
 	echo "Sending gratuitous arp"
 	$GARP -i $INTERFACE -a $DBHOST
-#	echo "echo 'SSG Failover' | mail -s 'SSG Failover' $EMAIL" 
+#	echo "echo 'SSG DB Failover' | mail -s 'SSG DB Failover' $EMAIL"
+}
+
+takedownalias() {
+    date
+    echo
+    echo "Network failure detected, relinquishing cluster IP"
+    $IFCONFIG $INTERFACE_ALIAS 99.99.99.99
+    # needed to make local machine not be able to reach a formerly
+    # configured address - linux bug(ette) workaround
+    $IFCONFIG $INTERFACE_ALIAS down
+	echo "Sending gratuitous arp"
+	$GARP -i $INTERFACE -a $DBHOST
+}
+
+pinggw() {
+    # 5 packets, 1 second apart, waiting 7 seconds
+    $PING -I $INTERFACE -n -c 5 -i 0.4 -q -w 7 $DEFAULT_ROUTE >/dev/null 2>&1
+    return $?
 }
 
 doget() {
-	# grab a file from the tomcat using wget
-	# quiet, few retries, short timeout, configurable location
-	#if $WGET  -q -t $TRIES -T $WAIT http://$DBHOST:$PORT/$FILE ;  then 
-	if echo " " | $NC -w $WAIT -o /tmp/foo $DBHOST $PORT > /tmp/bar;  then 
-		success
-	else 
-		RETVAL=$?
-		failure $RETVAL
-	fi
+    # first, lets see if we think we're cluster master.
+    # cluster master is defined as owning the cluster ip and alias.
+    if selfmaster; then
+        # wait a minute, I'm master.
+        # I should see if i'm still alive on the net
+        if pinggw; then
+            # yay, i'm alive, I'm master, so it looks like I should stay that way.
+            success
+        else
+            # duh, I'm dead to the world , and I think i'm master
+            # turn off that alias, so when i'm plugged back in again
+            # I won't be stupid and try to own it still
+            takedownalias
+            # now start the loop again to see if
+            # we've come alive again
+            success
+        fi
+    else
+        # we're not the master (any more)
+    	if echo " " | $NC -w $WAIT -o /tmp/foo $DBHOST $PORT > /tmp/bar;  then
+    		success
+    	else
+
+    		RETVAL=$?
+    		failure $RETVAL
+    	fi
+    fi
 }
 
 
@@ -120,8 +194,11 @@ case "$1" in
 	echo "Stopping $0"
 	kill -9 `cat $PIDFILE`
 	;;
+  desc)
+    desc
+    ;;
   *)
-	echo "Usage: $0 {start|stop}"
+	echo "Usage: $0 {start|stop|desc}"
 	exit 1
 esac
 
