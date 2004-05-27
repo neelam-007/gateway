fffc9f45e26c79c441701363dcc2c49f9d6ed724
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-05-27 18:43:00 +0000

    Initial checkin
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@6260 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/common/security/ExportKeys.java
@@ -0,0 +1,68 @@
+/*
+* Copyright (C) 2003-2004 Layer 7 Technologies Inc.
+*
+* $Id$
+*/
+
+package com.l7tech.common.security;
+
+import com.l7tech.common.util.HexUtils;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.security.Key;
+import java.security.KeyStore;
+import java.security.PublicKey;
+import java.util.Enumeration;
+
+/**
+ * @author alex
+ * @version $Revision$
+ */
+public class ExportKeys {
+    public static void main( String[] args ) throws Exception {
+        String keystorefile = args[0];
+        String storepassword = args[1];
+        String keypassword = args[2];
+        String privout = args[3];
+        String pubout = args[4];
+
+        KeyStore ks = KeyStore.getInstance("JKS");
+        ks.load( new FileInputStream( keystorefile ), storepassword.toCharArray()) ;
+        Enumeration enum = ks.aliases();
+        while (enum.hasMoreElements()) {
+            String alias = (String) enum.nextElement();
+            System.err.println("alias="+alias);
+
+            if ( ks.isKeyEntry(alias) ){
+                Key privkey = ks.getKey( alias, keypassword.toCharArray() );
+                PublicKey pubkey = (ks.getCertificate(alias)).getPublicKey();
+
+                System.err.println("private key format=" + privkey.getFormat());
+                FileOutputStream fos = null;
+                try {
+                    byte encoded[] = privkey.getEncoded();
+                    fos = new FileOutputStream(privout);
+                    fos.write("-----BEGIN PRIVATE KEY-----\n".getBytes());
+                    fos.write(HexUtils.encodeBase64(encoded).getBytes());
+                    fos.write("\n-----END PRIVATE KEY-----\n".getBytes());
+                    System.err.println( "Wrote " + privout);
+                } finally {
+                    if ( fos != null ) fos.close();
+                }
+
+                System.err.println("public key format=" + pubkey.getFormat());
+                try {
+                    byte encoded[] = pubkey.getEncoded();
+                    fos = new FileOutputStream(pubout);
+                    fos.write("-----BEGIN RSA PUBLIC KEY-----\n".getBytes());
+                    fos.write(HexUtils.encodeBase64(encoded).getBytes("UTF-8"));
+                    fos.write("\n-----END RSA PUBLIC KEY-----\n".getBytes());
+                    System.err.println( "Wrote " + pubout);
+                } finally {
+                    if ( fos != null ) fos.close();
+                }
+            }
+        }
+    }
+}
