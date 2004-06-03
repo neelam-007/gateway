4b36815dc58e8174c29e276dd07b26aff0479e3b
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-06-03 18:07:33 +0000

    always clear thread context in close by moving statement in finally clause instead of end of try (previously, the thread context was not cleaned if an exceptio occured)
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@6322 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/objectmodel/HibernatePersistenceContext.java
+++ src/com/l7tech/objectmodel/HibernatePersistenceContext.java
@@ -119,9 +119,10 @@ public class HibernatePersistenceContext extends PersistenceContext {
                 _session.close();
             }
             _session = null;
-            PersistenceContext.releaseContext();
         } catch (HibernateException e) {
             logger.log(Level.FINE, "error closing context", e);
+        } finally {
+            PersistenceContext.releaseContext();
         }
     }
 
