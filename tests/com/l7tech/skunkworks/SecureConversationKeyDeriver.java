3a89bf391d806a71fd592b70a1e55d9d3ee66067
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-06-09 16:52:31 +0000

    Finished P_SHA-1 implementation (algo for deriving keys according to WS-Secure Conversation)
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@6346 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- tests/com/l7tech/skunkworks/SecureConversationKeyDeriver.java
+++ tests/com/l7tech/skunkworks/SecureConversationKeyDeriver.java
@@ -3,6 +3,8 @@ package com.l7tech.skunkworks;
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.security.InvalidKeyException;
 
 /**
  * Implement mechanism described in WS-Secure Conversation to derive
@@ -28,14 +30,46 @@ public class SecureConversationKeyDeriver {
      *
      *  A(x) is defined as:
      *      A(0) = seed
-     *      A(i) = HMAC_hash(secret, A(i-1)
+     *      A(i) = HMAC_hash(secret, A(i-1))
      */
-    public byte[] pSHA1(byte[] secret, byte[] seed, int requiredlength) {
-        // todo, use hmacsha1 and implement algo in header
-        return null;
+    public byte[] pSHA1(byte[] secret, byte[] seed, int requiredlength) throws NoSuchAlgorithmException, InvalidKeyException {
+        // compute A(1)
+        byte[] ai = hmacsha1(secret, seed);
+        // compute A(1) + seed
+        byte[] aiPlusSeed = new byte[ai.length + seed.length];
+        System.arraycopy(ai, 0, aiPlusSeed, 0, ai.length);
+        System.arraycopy(seed, 0, aiPlusSeed, ai.length, seed.length);
+        // start the P_SHA-1
+        byte[] output = hmacsha1(secret, aiPlusSeed);
+        // continue until we get at least the desired length
+        while (output.length < requiredlength) {
+            ai = hmacsha1(secret, ai);
+            if (aiPlusSeed.length == (ai.length + seed.length)) {
+                System.arraycopy(ai, 0, aiPlusSeed, 0, ai.length);
+            } else {
+                aiPlusSeed = new byte[ai.length + seed.length];
+                System.arraycopy(ai, 0, aiPlusSeed, 0, ai.length);
+                System.arraycopy(seed, 0, aiPlusSeed, ai.length, seed.length);
+            }
+            byte[] bytesToAppend = hmacsha1(secret, aiPlusSeed);
+            byte[] newoutput = new byte[output.length + bytesToAppend.length];
+            System.arraycopy(output, 0, newoutput, 0, output.length);
+            System.arraycopy(bytesToAppend, 0, newoutput, output.length, bytesToAppend.length);
+            output = newoutput;
+        }
+        // trim to desired size if necessary
+        if (output.length > requiredlength) {
+            byte[] trimmedoutput = new byte[requiredlength];
+            System.arraycopy(output, 0, trimmedoutput, 0, requiredlength);
+            output = trimmedoutput;
+        }
+        return output;
     }
 
-    public byte[] hmacsha1(byte[] secret, byte[] input) throws Exception {
+    /**
+     * @param secret the key used to compute the sha1 mac
+     */
+    public byte[] hmacsha1(byte[] secret, byte[] input) throws NoSuchAlgorithmException, InvalidKeyException {
         // todo, property + constants
         Mac mac = Mac.getInstance("HMacSHA1");
         Key key = new SecretKeySpec(secret, "SHA1");
