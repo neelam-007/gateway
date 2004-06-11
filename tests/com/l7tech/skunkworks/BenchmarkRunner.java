bd080dc25396e100df8cf42b51f605c070b407b2
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-06-11 22:40:46 +0000

    Thread pool close timeout = 10000
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@6372 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- tests/com/l7tech/skunkworks/BenchmarkRunner.java
+++ tests/com/l7tech/skunkworks/BenchmarkRunner.java
@@ -100,7 +100,7 @@ public class BenchmarkRunner {
             synchronized (this) {
                 try {
                     this.wait();
-                    threadPool.close(1000);
+                    threadPool.close(10000);
                 } catch (InterruptedException e) {
                     // swallow
                 }
