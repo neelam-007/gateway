2782f4427da950f9be7881478e072f5f2ab6be9c
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-07-05 21:55:12 +0000

    processing placeholders
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@6507 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/common/security/xml/WssProcessorImpl.java
+++ src/com/l7tech/common/security/xml/WssProcessorImpl.java
@@ -2,10 +2,14 @@ package com.l7tech.common.security.xml;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
 import java.security.cert.X509Certificate;
 import java.security.PrivateKey;
 import java.util.logging.Logger;
+import java.util.Collection;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import com.l7tech.common.util.SoapUtil;
 
@@ -33,7 +37,14 @@ public class WssProcessorImpl implements WssProcessor {
     public WssProcessor.ProcessorResult undecorateMessage(Document soapMsg,
                                                           X509Certificate recipientCert,
                                                           PrivateKey recipientKey) throws WssProcessor.ProcessorException {
-        // look for security elements
+        // Reset all potential outputs
+        unprocessedDocument = soapMsg;
+        elementsThatWereSigned.clear();
+        elementsThatWereEncrypted.clear();
+        securityTokens.clear();
+        timestamp = null;
+
+        // Get relevent Security header
         Element[] securityHeaders = SoapUtil.getSecurityElements(soapMsg);
         Element releventSecurityHeader = null;
         // find the relevent security header. that is the one with no actor
@@ -48,31 +59,74 @@ public class WssProcessorImpl implements WssProcessor {
         // maybe there are no security headers at all in which case, there is nothing to process
         if (releventSecurityHeader == null) {
             logger.finer("No relevent security header found.");
-            return emptyResult(soapMsg);
+            return produceResult();
         }
-        // todo
-        return null;
+
+        // Process elements one by one
+        NodeList securityChildren = releventSecurityHeader.getElementsByTagName("*");
+        for (int i = 0; i < securityChildren.getLength(); i++) {
+            Element securityChildToProcess = (Element)securityChildren.item(i);
+
+            if (securityChildToProcess.getLocalName().equals("EncryptedKey")) {
+                logger.finest("Processing EncryptedKey");
+                // todo
+            } else if (securityChildToProcess.getLocalName().equals("Timestamp")) {
+                logger.finest("Processing Timestamp");
+                // todo
+            } else if (securityChildToProcess.getLocalName().equals("BinarySecurityToken")) {
+                logger.finest("Processing BinarySecurityToken");
+                // todo
+            } else if (securityChildToProcess.getLocalName().equals("Signature")) {
+                logger.finest("Processing Signature");
+                // todo
+            } else {
+                logger.finer("Unknown element in security header: " + securityChildToProcess.getNodeName());
+            }
+        }
+        return produceResult();
     }
 
-    private WssProcessor.ProcessorResult emptyResult(final Document original) {
+    private WssProcessor.ProcessorResult produceResult() {
         return new WssProcessor.ProcessorResult() {
             public Document getUndecoratedMessage() {
-                return original;
+                return unprocessedDocument;
             }
             public Element[] getElementsThatWereSigned() {
-                return new Element[0];
+                Element[] output = new Element[elementsThatWereSigned.size()];
+                Iterator iter = elementsThatWereSigned.iterator();
+                for (int i = 0; i < output.length; i++) {
+                    output[i] = (Element)iter.next();
+                }
+                return output;
             }
             public Element[] getElementsThatWereEncrypted() {
-                return new Element[0];
+                Element[] output = new Element[elementsThatWereEncrypted.size()];
+                Iterator iter = elementsThatWereEncrypted.iterator();
+                for (int i = 0; i < output.length; i++) {
+                    output[i] = (Element)iter.next();
+                }
+                return output;
             }
             public WssProcessor.SecurityToken[] getSecurityTokens() {
-                return new WssProcessor.SecurityToken[0];
+                WssProcessor.SecurityToken[] output = new WssProcessor.SecurityToken[securityTokens.size()];
+                Iterator iter = securityTokens.iterator();
+                for (int i = 0; i < output.length; i++) {
+                    output[i] = (WssProcessor.SecurityToken)iter.next();
+                }
+                return output;
             }
             public WssProcessor.Timestamp getTimestamp() {
-                return null;
+                return timestamp;
             }
         };
     }
 
     private final Logger logger = Logger.getLogger(WssProcessorImpl.class.getName());
+
+    private Document unprocessedDocument = null;
+    private final Collection elementsThatWereSigned = new ArrayList();
+    private final Collection elementsThatWereEncrypted = new ArrayList();
+    private final Collection securityTokens = new ArrayList();
+    private WssProcessor.Timestamp timestamp = null;
+
 }
