e8010d64640da73df88b03a5f57dc98b783e0252
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-07-07 00:16:01 +0000

    Javadoc, retrieve doesn't throw CertificateException
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@6569 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/common/security/TrustedCertAdmin.java
+++ src/com/l7tech/common/security/TrustedCertAdmin.java
@@ -11,7 +11,6 @@ import com.l7tech.objectmodel.*;
 import java.io.IOException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.List;
 
@@ -20,10 +19,52 @@ import java.util.List;
  * @version $Revision$
  */
 public interface TrustedCertAdmin extends Remote {
+
+    /**
+     * Retrieves all {@link TrustedCert}s from the database.
+     * @return a {@link List} of {@link TrustedCert}s
+     * @throws FindException
+     * @throws RemoteException
+     */
     public List findAllCerts() throws FindException, RemoteException;
+
+    /**
+     * Retrieves the {@link TrustedCert} with the specified oid.
+     * @param oid the oid of the {@link TrustedCert} to retrieve
+     * @return
+     * @throws FindException
+     * @throws RemoteException
+     */
     public TrustedCert findCertByPrimaryKey(long oid) throws FindException, RemoteException;
+
+    /**
+     * Saves a new or existing {@link TrustedCert} to the database.
+     * @param cert the {@link TrustedCert} to be saved
+     * @return
+     * @throws SaveException
+     * @throws UpdateException
+     * @throws VersionException
+     * @throws RemoteException
+     */
     public long saveCert(TrustedCert cert) throws SaveException, UpdateException, VersionException, RemoteException;
+
+    /**
+     * Removes the specified {@link TrustedCert} from the database.
+     * @param oid the oid of the {@link TrustedCert} to be deleted
+     * @throws FindException if the {@link TrustedCert} cannot be found
+     * @throws DeleteException if the {@link TrustedCert} cannot be deleted
+     * @throws RemoteException
+     */
     public void deleteCert(long oid) throws FindException, DeleteException, RemoteException;
-    public X509Certificate[] retrieveCertFromUrl(String url) throws CertificateException, RemoteException, IOException;
-    public X509Certificate[] retrieveCertFromUrl(String url, boolean ignoreHostname) throws CertificateException, RemoteException, IOException;
+
+    /**
+     * Retrieves the {@link X509Certificate} chain from the specified URL.
+     * @param url the url from which to retrieve the cert.
+     * @return an {@link X509Certificate} chain.
+     * @throws IOException if the certificate cannot be retrieved for whatever reason.
+     * @throws IllegalArgumentException if the URL does not start with "https://"
+     * @throws RemoteException
+     */
+    public X509Certificate[] retrieveCertFromUrl(String url) throws RemoteException, IOException;
+    public X509Certificate[] retrieveCertFromUrl(String url, boolean ignoreHostname) throws RemoteException, IOException;
 }
