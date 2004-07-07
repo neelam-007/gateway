8276fadb5ad6da33c3159f48182d8d9daa617fc9
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-07-07 22:51:41 +0000

    Add Z to end of date format pattern
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@6597 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/common/util/SoapUtil.java
+++ src/com/l7tech/common/util/SoapUtil.java
@@ -110,7 +110,7 @@ public class SoapUtil {
     public static final String SOAP_BODY_XPATH = SOAP_ENVELOPE_XPATH+"/"+NamespaceConstants.NSPREFIX_SOAP_ENVELOPE + ":Body";
 
     /** Timestamp date format */
-    public static final String DATE_FORMAT_PATTERN = "yyyy-MM-dd'T'HH:mm:ss";
+    public static final String DATE_FORMAT_PATTERN = "yyyy-MM-dd'T'HH:mm:ss'Z'";
     public static final TimeZone DATE_FORMAT_TIMEZONE = TimeZone.getTimeZone("UTC");
 
     /**
