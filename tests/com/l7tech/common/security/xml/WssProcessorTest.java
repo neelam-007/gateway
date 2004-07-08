553c9f86b083605bf0d2ca7be7f7ebfcc987af1f
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-07-08 00:22:13 +0000

    fix extra ','
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@6611 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- tests/com/l7tech/common/security/xml/WssProcessorTest.java
+++ tests/com/l7tech/common/security/xml/WssProcessorTest.java
@@ -120,7 +120,7 @@ public class WssProcessorTest extends TestCase {
         makeEttkTestDocument("ettk signed encrypted request", TestDocuments.ETTK_SIGNED_ENCRYPTED_REQUEST),
 
         makeDotNetTestDocument("request wrapped l7 actor", TestDocuments.WRAPED_L7ACTOR),
-        makeDotNetTestDocument("request multiple wrapped l7 actor", TestDocuments.MULTIPLE_WRAPED_L7ACTOR),
+        makeDotNetTestDocument("request multiple wrapped l7 actor", TestDocuments.MULTIPLE_WRAPED_L7ACTOR)
     };
 
     private TestDocument makeEttkTestDocument(String testname, String docname) {
