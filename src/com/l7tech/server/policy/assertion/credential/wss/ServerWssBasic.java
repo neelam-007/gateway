753b820e37c0046f4e7b18d9262d27b5f5d089e4
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-07-09 21:20:00 +0000

    refactored to reflect new WSS processing model
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@6696 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/server/policy/assertion/credential/wss/ServerWssBasic.java
+++ src/com/l7tech/server/policy/assertion/credential/wss/ServerWssBasic.java
@@ -6,24 +6,18 @@
 
 package com.l7tech.server.policy.assertion.credential.wss;
 
-import com.l7tech.common.util.SAXParsingCompleteException;
+import com.l7tech.common.security.xml.WssProcessor;
 import com.l7tech.message.Request;
 import com.l7tech.message.Response;
-import com.l7tech.message.XmlRequest;
+import com.l7tech.message.SoapRequest;
 import com.l7tech.policy.assertion.AssertionStatus;
 import com.l7tech.policy.assertion.PolicyAssertionException;
-import com.l7tech.policy.assertion.credential.CredentialFinderException;
 import com.l7tech.policy.assertion.credential.CredentialFormat;
 import com.l7tech.policy.assertion.credential.LoginCredentials;
 import com.l7tech.policy.assertion.credential.wss.WssBasic;
 import com.l7tech.server.policy.assertion.ServerAssertion;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-import org.xml.sax.XMLReader;
-import org.xml.sax.helpers.XMLReaderFactory;
 
 import java.io.IOException;
-import java.io.StringReader;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -33,109 +27,31 @@ import java.util.logging.Logger;
  */
 public class ServerWssBasic implements ServerAssertion {
     public ServerWssBasic(WssBasic data) {
-        _data = data;
+        // nothing interesting in here
     }
 
     public AssertionStatus checkRequest(Request request, Response response) throws IOException, PolicyAssertionException {
-        LoginCredentials creds = null;
-        try {
-            creds = findCredentials(request, response);
-        } catch (IOException e) {
-            logger.log(Level.SEVERE, "cannot find credentials", e);
-            return AssertionStatus.FALSIFIED;
-        } catch (CredentialFinderException e) {
-            logger.log(Level.SEVERE, "cannot find credentials", e);
-            return AssertionStatus.AUTH_REQUIRED;
+        if (!(request instanceof SoapRequest)) {
+            throw new PolicyAssertionException("This type of assertion is only supported with SOAP type of messages");
         }
-        if (creds == null) {
-            response.setPolicyViolated(true);
-            return AssertionStatus.FALSIFIED;
+        SoapRequest req = (SoapRequest)request;
+        WssProcessor.ProcessorResult wssResults = req.getWssProcessorOutput();
+        if (wssResults == null) {
+            throw new PolicyAssertionException("This request was not processed for WSS level security.");
         }
-        request.setPrincipalCredentials(creds);
-        return AssertionStatus.NONE;
+        WssProcessor.SecurityToken[] tokens = wssResults.getSecurityTokens();
+        for (int i = 0; i < tokens.length; i++) {
+            if (tokens[i].asObject() instanceof LoginCredentials) {
+                LoginCredentials creds = (LoginCredentials)tokens[i].asObject();
+                if (creds.getFormat() == CredentialFormat.CLEARTEXT) {
+                    request.setPrincipalCredentials(creds);
+                    return AssertionStatus.NONE;
+                }
+            }
+        }
+        logger.log(Level.SEVERE, "cannot find credentials");
+        return AssertionStatus.AUTH_REQUIRED;
     }
 
-    public LoginCredentials findCredentials( Request grequest, Response gresponse ) throws IOException, CredentialFinderException {
-        XmlRequest request;
-        if ( grequest instanceof XmlRequest )
-            request = (XmlRequest)grequest;
-        else
-            throw new CredentialFinderException( "Only XML requests and responses are supported!", AssertionStatus.FALSIFIED );
-
-        String xmlreq = null;
-        try {
-            xmlreq = request.getRequestXml();
-        } catch (IOException e) {
-            logger.log(Level.SEVERE, "Exception getting a xml request " + e.getMessage(), e);
-            throw new CredentialFinderException("Exception getting the xml request " + e.getMessage(), e);
-        }
-        // in order to get WSS credentials, we need a soap document
-        if (xmlreq == null || xmlreq.length() < 1) {
-            logger.log(Level.WARNING, "No xml request provided to extract wss element from");
-            return null;
-        }
-
-        // get xml parser
-        // todo, should this be set in some sort of startup class somewhere ?
-        System.setProperty("org.xml.sax.driver", "org.apache.xerces.parsers.SAXParser");
-        XMLReader reader = null;
-        try {
-            reader = XMLReaderFactory.createXMLReader();
-        } catch (SAXException e) {
-            logger.log(Level.SEVERE, "Exception getting an XMLReader " + e.getMessage(), e);
-            throw new CredentialFinderException("Exception getting an XMLReader " + e.getMessage(), e);
-        }
-        WsseBasicSaxHandler handler = new WsseBasicSaxHandler();
-        reader.setContentHandler(handler);
-
-        // parse the request, look for SAXParsingCompleteException
-        StringReader stringReader = new StringReader(xmlreq);
-        try {
-            reader.parse(new InputSource(stringReader));
-        } catch (SAXParsingCompleteException e) {
-            String passwd = handler.getParsedPassword();
-            // make sure we got a password (avoid a NPE)
-            if (passwd == null || passwd.length() < 1) {
-                logger.log(Level.WARNING, "parsing completed but no password available.");
-                return null;
-            }
-            // make sure we have a clear text passwd
-            if (!handler.DEFAULT_PASSWORD_TYPE.equals(handler.getPasswdType())) {
-                logger.warning("password is of wrong type: " + handler.getPasswdType());
-                return null;
-            }
-
-            // return the whole thing
-            // this is good, we got what we needed
-            logger.fine("Found credentials for user " + handler.getParsedUsername());
-            return new LoginCredentials( handler.getParsedUsername(), passwd.getBytes(), CredentialFormat.CLEARTEXT);
-        } catch (IOException e) {
-            logger.log(Level.SEVERE, "Exception parsing xml request " + e.getMessage(), e);
-            throw new CredentialFinderException("Exception parsing xml request " + e.getMessage(), e);
-        } catch (SAXException e) {
-            logger.log(Level.SEVERE, "Exception parsing xml request " + e.getMessage(), e);
-            throw new CredentialFinderException("Exception parsing xml request " + e.getMessage(), e);
-        } finally {
-            stringReader.close();
-        }
-        // note: the actual result is returned in the handling of the SAXParsingCompleteException
-        logger.warning("no credentials found");
-        return null;
-    }
-
-
-    /*public AssertionStatus checkCredentials(Request request, Response response) throws CredentialFinderException {
-        // this is only called once we have credentials
-        // there is nothing more to check here, if the creds were not in the right format,
-        // the WssBasicCredentialFinder would not have returned credentials
-
-        // (just to make sure)
-        LoginCredentials pc = request.getPrincipalCredentials();
-        // yes, we're good
-        if (pc != null) return AssertionStatus.NONE;
-        else return AssertionStatus.AUTH_REQUIRED;
-    }*/
-
-    protected WssBasic _data;
     private final Logger logger = Logger.getLogger(getClass().getName());
 }
