77662c81e645ba1fe0afbc2f2f3ab0161f32b4d2
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-07-09 20:09:58 +0000

    remove unused imports
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@6689 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/proxy/policy/assertion/xmlsec/ClientXmlRequestSecurity.java
+++ src/com/l7tech/proxy/policy/assertion/xmlsec/ClientXmlRequestSecurity.java
@@ -1,16 +1,9 @@
 package com.l7tech.proxy.policy.assertion.xmlsec;
 
 import com.l7tech.common.security.AesKey;
-import com.l7tech.common.security.xml.SecureConversationTokenHandler;
-import com.l7tech.common.security.xml.Session;
-import com.l7tech.common.security.xml.SignerInfo;
+import com.l7tech.common.security.xml.*;
+import com.l7tech.common.xml.MessageNotSoapException;
 import com.l7tech.policy.assertion.AssertionStatus;
-import com.l7tech.common.security.xml.ElementSecurity;
-import com.l7tech.common.security.xml.SecurityProcessor;
-import com.l7tech.common.security.xml.SecurityProcessorException;
-import com.l7tech.common.util.SoapUtil;
-import com.l7tech.common.xml.MessageNotSoapException;
-import com.l7tech.common.xml.MessageNotSoapException;
 import com.l7tech.policy.assertion.xmlsec.XmlRequestSecurity;
 import com.l7tech.proxy.datamodel.PendingRequest;
 import com.l7tech.proxy.datamodel.Ssg;
