f3efe64d4d3737688607c232c3e50d1636f9b1aa
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-07-12 19:00:16 +0000

    More tests
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@6736 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- tests/com/l7tech/skunkworks/xss4j/CanonicalizerBugRepro.java
+++ tests/com/l7tech/skunkworks/xss4j/CanonicalizerBugRepro.java
@@ -10,6 +10,7 @@ import com.ibm.xml.dsig.Canonicalizer;
 import com.ibm.xml.dsig.transform.ExclusiveC11r;
 import com.ibm.xml.dsig.transform.ExclusiveC11rWC;
 import com.ibm.xml.dsig.transform.W3CCanonicalizer;
+import com.l7tech.common.util.SoapUtil;
 import com.l7tech.common.xml.TestDocuments;
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -22,7 +23,8 @@ import java.util.logging.Logger;
 /**
  * Test case demonstrating that both xs4j "exclusive" canonicalizers fail for unknown reasons
  * when canonicalizing the "productid" element in our placeorder_cleartext sample message.
- * It seems that some canonicalizer can only canonalicalize full documents.
+ * Some canonicalizer (ExclusiveC11r and ExclusiveC11rWC) throw an exception when canonicalize
+ * certain elements.
  */
 public class CanonicalizerBugRepro extends TestCase {
     private static final Logger log = Logger.getLogger(CanonicalizerBugRepro.class.getName());
@@ -54,11 +56,18 @@ public class CanonicalizerBugRepro extends TestCase {
 
     private void useCanon(Canonicalizer canon) throws Exception {
         Document message = TestDocuments.getTestDocument(TestDocuments.PLACEORDER_CLEARTEXT);
-        Element productid = (Element)message.getElementsByTagNameNS("", "productid").item(0);
-        System.out.println("Using canon to serialize document");
+        Element body = SoapUtil.getBodyElement(message);
+        Element placeOrder = (Element)message.getElementsByTagNameNS("http://warehouse.acme.com/ws", "placeOrder").item(0);
+        Element productid = (Element)message.getElementsByTagName("productid").item(0);
+
         canon.canonicalize(message, System.out);
-        System.out.println("Document serialized ok. Trying to canon a node.");
+        assertTrue(body != null);
+        canon.canonicalize(body, System.out);
+        assertTrue(placeOrder != null);
+        canon.canonicalize(placeOrder, System.out);
+        assertTrue(productid != null);
         canon.canonicalize(productid, System.out);
+
         System.out.println("Node serialized ok.");
     }
 }
