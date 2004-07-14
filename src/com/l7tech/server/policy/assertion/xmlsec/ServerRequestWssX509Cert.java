dfb2a4f64b65dd932f85952978036c7ff2c011da
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-07-14 18:00:22 +0000

    change logic for more than one signature. changed the exception handling.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@6842 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/server/policy/assertion/xmlsec/ServerRequestWssX509Cert.java
+++ src/com/l7tech/server/policy/assertion/xmlsec/ServerRequestWssX509Cert.java
@@ -1,20 +1,20 @@
 package com.l7tech.server.policy.assertion.xmlsec;
 
-import com.l7tech.server.policy.assertion.ServerAssertion;
-import com.l7tech.policy.assertion.AssertionStatus;
-import com.l7tech.policy.assertion.PolicyAssertionException;
-import com.l7tech.policy.assertion.credential.LoginCredentials;
-import com.l7tech.policy.assertion.credential.CredentialFormat;
-import com.l7tech.policy.assertion.xmlsec.RequestWssX509Cert;
+import com.l7tech.common.security.xml.WssProcessor;
 import com.l7tech.message.Request;
 import com.l7tech.message.Response;
 import com.l7tech.message.SoapRequest;
-import com.l7tech.common.security.xml.WssProcessor;
+import com.l7tech.policy.assertion.AssertionStatus;
+import com.l7tech.policy.assertion.PolicyAssertionException;
+import com.l7tech.policy.assertion.credential.CredentialFormat;
+import com.l7tech.policy.assertion.credential.LoginCredentials;
+import com.l7tech.policy.assertion.xmlsec.RequestWssX509Cert;
+import com.l7tech.server.policy.assertion.ServerAssertion;
+import sun.security.x509.X500Name;
 
 import java.io.IOException;
 import java.util.logging.Logger;
-
-import sun.security.x509.X500Name;
+import java.security.cert.X509Certificate;
 
 /**
  * This assertion verifies that the message contained an
@@ -34,12 +34,13 @@ public class ServerRequestWssX509Cert implements ServerAssertion {
     }
     public AssertionStatus checkRequest(Request request, Response response) throws IOException, PolicyAssertionException {
         if (!(request instanceof SoapRequest)) {
-            throw new PolicyAssertionException("This type of assertion is only supported with SOAP type of messages");
+            logger.info("This type of assertion is only supported with SOAP type of messages");
+            return AssertionStatus.BAD_REQUEST;
         }
         SoapRequest soapreq = (SoapRequest)request;
         WssProcessor.ProcessorResult wssResults = soapreq.getWssProcessorOutput();
         if (wssResults == null) {
-            throw new PolicyAssertionException("This request was not processed for WSS level security.");
+            throw new IOException("This request was not processed for WSS level security.");
         }
 
         WssProcessor.SecurityToken[] tokens = wssResults.getSecurityTokens();
@@ -47,25 +48,35 @@ public class ServerRequestWssX509Cert implements ServerAssertion {
             logger.info("No tokens were processed from this request. Returning AUTH_REQUIRED.");
             return AssertionStatus.AUTH_REQUIRED;
         }
+        X509Certificate gotACertAlready = null;
         for (int i = 0; i < tokens.length; i++) {
             WssProcessor.SecurityToken tok = tokens[i];
             if (tok instanceof WssProcessor.X509SecurityToken) {
                 WssProcessor.X509SecurityToken x509Tok = (WssProcessor.X509SecurityToken)tok;
                 if (x509Tok.isPossessionProved()) {
-                    X500Name x500name = new X500Name(x509Tok.asX509Certificate().getSubjectX500Principal().getName());
-                    String certCN = x500name.getCommonName();
-                    request.setPrincipalCredentials(new LoginCredentials(certCN,
-                                                                         null,
-                                                                         CredentialFormat.CLIENTCERT,
-                                                                         null,
-                                                                         x509Tok.asX509Certificate()));
-                    logger.fine("Cert loaded as principal credential for CN:" + certCN);
+                    X509Certificate okCert = x509Tok.asX509Certificate();
                     // todo, it is possible that a request has more than one signature by more than one
                     // identity. we should refactor request.setPrincipalCredentials to be able to remember
                     // more than one proven identity.
-                    return AssertionStatus.NONE;
+                    if (gotACertAlready != null) {
+                        logger.severe("We got a request that presented more than one valid signature from" +
+                                      "more than one client cert. This is not yet supported");
+                        return AssertionStatus.NOT_YET_IMPLEMENTED;
+                    }
+                    gotACertAlready = okCert;
                 }
             }
+        }
+        if (gotACertAlready != null) {
+            X500Name x500name = new X500Name(gotACertAlready.getSubjectX500Principal().getName());
+            String certCN = x500name.getCommonName();
+            request.setPrincipalCredentials(new LoginCredentials(certCN,
+                                                                 null,
+                                                                 CredentialFormat.CLIENTCERT,
+                                                                 null,
+                                                                 gotACertAlready));
+            logger.fine("Cert loaded as principal credential for CN:" + certCN);
+            return AssertionStatus.NONE;
         }
         logger.info("This assertion did not find a proven x509 cert to use as credentials. Returning AUTH_REQUIRED.");
         return AssertionStatus.AUTH_REQUIRED;
