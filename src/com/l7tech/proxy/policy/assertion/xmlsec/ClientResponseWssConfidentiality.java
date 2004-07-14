6f8df8c5932392fd8d7f4bc5e42d6ddefcac687d
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-07-14 23:01:18 +0000

    fixed doc to explain what this really do.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@6873 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/proxy/policy/assertion/xmlsec/ClientResponseWssConfidentiality.java
+++ src/com/l7tech/proxy/policy/assertion/xmlsec/ClientResponseWssConfidentiality.java
@@ -23,12 +23,7 @@ import java.util.Iterator;
 import java.util.List;
 
 /**
- * XML Digital signature on the soap response sent from the ssg server to the requestor (probably proxy). May also enforce
- * xml encryption for the body element of the response.
- * <p/>
- * On the server side, this decorates a response with an xml d-sig and maybe xml-enc the response's body
- * On the proxy side, this verifies that the Soap Response contains a valid xml d-sig for the entire envelope and decrypts
- * the response's body if necessary.
+ * Verifies that a specific element in the response was encrypted by the ssg.
  * <p/>
  * <br/><br/>
  * User: flascell<br/>
@@ -45,17 +40,6 @@ public class ClientResponseWssConfidentiality extends ClientAssertion {
         }
     }
 
-    /**
-     * If this assertion includes xml-enc, the proxy will add a header to the request that tells the server
-     * which xml-enc session to use.
-     *
-     * @param request might receive a header containing the xml-enc session
-     * @return AssertionStatus.NONE if we are prepared to handle the eventual response
-     * @throws com.l7tech.proxy.datamodel.exceptions.ServerCertificateUntrustedException
-     *                                    if an updated SSG cert is needed
-     * @throws com.l7tech.proxy.datamodel.exceptions.OperationCanceledException if the user cancels the logon dialog
-     * @throws com.l7tech.proxy.datamodel.exceptions.BadCredentialsException    if the SSG rejects the SSG username/password when establishing the session
-     */
     public AssertionStatus decorateRequest(PendingRequest request)
             throws GeneralSecurityException,
             OperationCanceledException, BadCredentialsException,
@@ -70,13 +54,6 @@ public class ClientResponseWssConfidentiality extends ClientAssertion {
         return AssertionStatus.NONE;
     }
 
-    /**
-     * validate the signature of the response by the ssg server
-     *
-     * @param request
-     * @param response
-     * @return
-     */
     public AssertionStatus unDecorateReply(PendingRequest request, SsgResponse response)
       throws ServerCertificateUntrustedException, IOException, SAXException, ResponseValidationException, KeyStoreCorruptException {
         Document soapmsg = response.getResponseAsDocument();
