6c8d65ff4e44827f992f807bdae2c13018c2f661
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-07-14 22:19:53 +0000

    added constructors
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@6864 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/policy/assertion/xmlsec/RequestWssIntegrity.java
+++ src/com/l7tech/policy/assertion/xmlsec/RequestWssIntegrity.java
@@ -1,14 +1,34 @@
 package com.l7tech.policy.assertion.xmlsec;
 
-import com.l7tech.policy.assertion.Assertion;
-import com.l7tech.policy.assertion.XpathBasedAssertion;
 import com.l7tech.common.xml.XpathExpression;
+import com.l7tech.common.util.SoapUtil;
+import com.l7tech.policy.assertion.XpathBasedAssertion;
+
+import javax.xml.soap.SOAPConstants;
+import javax.xml.rpc.NamespaceConstants;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * Enforces the XML security on the message elements or entire message
- * 
- * @author flascell<br/>
- * @version Aug 27, 2003<br/>
+ * Enforces that a specific element in a request is signed.
+ * <p/>
+ * <br/><br/>
+ * LAYER 7 TECHNOLOGIES, INC<br/>
+ * User: flascell<br/>
+ * Date: July 14, 2004<br/>
  */
 public class RequestWssIntegrity extends XpathBasedAssertion {
+    public RequestWssIntegrity() {
+        // default constructor that requests a signature on the body
+        XpathExpression xpath = new XpathExpression();
+        xpath.setExpression(SoapUtil.SOAP_BODY_XPATH);
+        Map nss = new HashMap();
+        nss.put(NamespaceConstants.NSPREFIX_SOAP_ENVELOPE, SOAPConstants.URI_NS_SOAP_ENVELOPE);
+        xpath.setNamespaces(nss);
+        setXpathExpression(xpath);
+    }
+
+    public RequestWssIntegrity(XpathExpression xpath) {
+        setXpathExpression(xpath);
+    }
 }
