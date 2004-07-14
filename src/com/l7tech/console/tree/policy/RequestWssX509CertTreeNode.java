5b39b078706064619b8e6a54477ee54266909436
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-07-14 18:45:04 +0000

    This is the tree node corresponding to the RequestWssX509Cert assertion type.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@6849 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/console/tree/policy/RequestWssX509CertTreeNode.java
@@ -0,0 +1,26 @@
+package com.l7tech.console.tree.policy;
+
+import com.l7tech.policy.assertion.xmlsec.RequestWssX509Cert;
+
+/**
+ * This is the tree node corresponding to the RequestWssX509Cert assertion type.
+ * <p/>
+ * <br/><br/>
+ * LAYER 7 TECHNOLOGIES, INC<br/>
+ * User: flascell<br/>
+ * Date: Jul 14, 2004<br/>
+ * $Id$<br/>
+ */
+public class RequestWssX509CertTreeNode extends LeafAssertionTreeNode {
+    public RequestWssX509CertTreeNode(RequestWssX509Cert assertion) {
+        super(assertion);
+    }
+
+    public String getName() {
+        return "WSS sign SOAP request.";
+    }
+
+    protected String iconResource(boolean open) {
+        return "com/l7tech/console/resources/xmlencryption.gif";
+    }
+}
