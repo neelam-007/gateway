d74e1793f2eb52aba18bb121963622570ed1a7bd
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-07-16 20:58:43 +0000

    added wrapExportReferencesToPolicyDocument
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@6987 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/policy/exporter/PolicyExporter.java
+++ src/com/l7tech/policy/exporter/PolicyExporter.java
@@ -1,9 +1,11 @@
 package com.l7tech.policy.exporter;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
 import com.l7tech.policy.assertion.Assertion;
 import com.l7tech.policy.wsp.WspWriter;
+import com.l7tech.policy.wsp.WspConstants;
 import com.l7tech.common.util.XmlUtil;
 
 import java.io.File;
@@ -20,10 +22,12 @@ import java.io.IOException;
  * $Id$<br/>
  */
 public class PolicyExporter {
+
     public Document exportToDocument(Assertion rootAssertion) throws IOException, SAXException {
+        // do policy to xml
         Document policydoc = XmlUtil.stringToDocument(WspWriter.getPolicyXml(rootAssertion));
         // go through each assertion and list external dependencies
-        // do policy to xml
+        Element referencesEl = wrapExportReferencesToPolicyDocument(policydoc);
         // add external dependencies to document
         // todo
         return policydoc;
@@ -33,5 +37,20 @@ public class PolicyExporter {
         Document doc = exportToDocument(rootAssertion);
         // write doc to file
         // todo
+    }
+
+    private Element wrapExportReferencesToPolicyDocument(Document originalPolicy) {
+        Element exportRoot = originalPolicy.createElementNS(ExporterConstants.EXPORTED_POL_NS,
+                                                            ExporterConstants.EXPORTED_DOCROOT_ELNAME);
+        exportRoot.setAttribute("xmlns:" + ExporterConstants.EXPORTED_POL_PREFIX, ExporterConstants.EXPORTED_POL_NS);
+        exportRoot.setPrefix(ExporterConstants.EXPORTED_POL_PREFIX);
+        Element referencesEl = originalPolicy.createElementNS(ExporterConstants.EXPORTED_POL_NS,
+                                                              ExporterConstants.EXPORTED_REFERENCES_ELNAME);
+        referencesEl.setPrefix(ExporterConstants.EXPORTED_POL_PREFIX);
+        exportRoot.appendChild(referencesEl);
+        Element previousRoot = originalPolicy.getDocumentElement();
+        exportRoot.appendChild(previousRoot);
+        originalPolicy.appendChild(exportRoot);
+        return referencesEl;
     }
 }
