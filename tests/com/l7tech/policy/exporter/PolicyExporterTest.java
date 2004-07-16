bca3e0a5114c67dabd04a7caaafcc9d6df20694e
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-07-16 19:09:33 +0000

    Test for policy exporter
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@6965 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ tests/com/l7tech/policy/exporter/PolicyExporterTest.java
@@ -0,0 +1,43 @@
+package com.l7tech.policy.exporter;
+
+import junit.framework.TestCase;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import com.l7tech.policy.assertion.Assertion;
+import com.l7tech.policy.assertion.composite.AllAssertion;
+import com.l7tech.common.util.XmlUtil;
+import org.w3c.dom.Document;
+
+/**
+ * Test class for the policy exporter.
+ * <p/>
+ * <br/><br/>
+ * LAYER 7 TECHNOLOGIES, INC<br/>
+ * User: flascell<br/>
+ * Date: Jul 16, 2004<br/>
+ * $Id$<br/>
+ */
+public class PolicyExporterTest extends TestCase {
+
+    public void exportToDocumentTest() throws Exception  {
+        PolicyExporter exporter = new PolicyExporter();
+        Assertion testPolicy = createTestPolicy();
+        Document resultingExport = exporter.exportToDocument(testPolicy);
+        // visual inspection for now:
+        System.out.println(XmlUtil.nodeToString(resultingExport));
+    }
+
+    private Assertion createTestPolicy() {
+        AllAssertion root = new AllAssertion();
+        // todo, more
+        return root;
+    }
+
+    public static Test suite() {
+        return new TestSuite(PolicyExporterTest.class);
+    }
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+}
