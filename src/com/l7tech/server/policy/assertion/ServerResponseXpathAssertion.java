aaedcafdc94d2d4d1e0a9ca94e2cdc9f3712e1d8
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-07-16 19:36:44 +0000

    Server-side implementation of ResponseXpathAssertion
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@6969 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/server/policy/assertion/ServerResponseXpathAssertion.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright (C) 2003 Layer 7 Technologies Inc.
+ *
+ * $Id$
+ */
+
+package com.l7tech.server.policy.assertion;
+
+import com.l7tech.message.Request;
+import com.l7tech.message.Response;
+import com.l7tech.message.XmlResponse;
+import com.l7tech.policy.assertion.AssertionStatus;
+import com.l7tech.policy.assertion.PolicyAssertionException;
+import com.l7tech.policy.assertion.ResponseXpathAssertion;
+import org.jaxen.JaxenException;
+import org.jaxen.dom.DOMXPath;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Server-side processing for an assertion that verifies whether a response
+ * matches a specified XPath pattern.
+ *
+ * @see com.l7tech.policy.assertion.ResponseXpathAssertion
+ * @see com.l7tech.proxy.policy.assertion.ClientResponseXpathAssertion
+ * @author alex
+ * @version $Revision$
+ */
+public class ServerResponseXpathAssertion implements ServerAssertion {
+
+    public ServerResponseXpathAssertion( ResponseXpathAssertion data ) {
+        _data = data;
+    }
+
+    private synchronized DOMXPath getDOMXpath() throws JaxenException {
+        if ( _domXpath == null ) {
+            String pattern = _data.pattern();
+            Map namespaceMap = _data.namespaceMap();
+
+            if ( pattern != null ) {
+                _domXpath = new DOMXPath(pattern);
+
+                if ( namespaceMap != null ) {
+                    for (Iterator i = namespaceMap.keySet().iterator(); i.hasNext();) {
+                        String key = (String) i.next();
+                        String uri = (String)namespaceMap.get(key);
+                        _domXpath.addNamespace( key, uri );
+                    }
+                }
+            }
+        }
+
+        return _domXpath;
+    }
+
+    public AssertionStatus checkRequest(Request request, Response response) throws IOException, PolicyAssertionException {
+        if ( response instanceof XmlResponse ) {
+            try {
+                String pattern = _data.pattern();
+
+                if ( pattern == null || pattern.length() == 0 ) {
+                    _logger.warning( "XPath pattern cannot be null or empty!" );
+                    return AssertionStatus.FALSIFIED;
+                }
+
+                XmlResponse xres = (XmlResponse)response;
+                Document doc = xres.getDocument();
+
+                List result = null;
+                DOMXPath xp = getDOMXpath();
+                try {
+                    result = xp.selectNodes(doc);
+                } catch ( RuntimeException rte ) {
+                    _logger.log( Level.WARNING, "XPath processor threw Runtime Exception", rte );
+                    return AssertionStatus.FALSIFIED;
+                }
+
+                if ( result == null || result.size() == 0 ) {
+                    _logger.info( "XPath pattern " + pattern  + " didn't match response!" );
+                    return AssertionStatus.FALSIFIED;
+                } else {
+                    Object o = result.get(0);
+                    if ( o instanceof Boolean ) {
+                        if ( ((Boolean)o).booleanValue() ) {
+                            _logger.fine( "XPath pattern " + pattern + " returned true" );
+                            return AssertionStatus.NONE;
+                        } else {
+                            _logger.info( "XPath pattern " + pattern + " returned false" );
+                            return AssertionStatus.FALSIFIED;
+                        }
+                    } else if ( o instanceof Node ) {
+                        Node n = (Node)o;
+                        int type = n.getNodeType();
+                        switch( type ) {
+                            case Node.TEXT_NODE:
+                                _logger.fine( "XPath pattern " + pattern + " found a text node '" + n.getNodeValue() + "'" );
+                                return AssertionStatus.NONE;
+                            case Node.ELEMENT_NODE:
+                                _logger.fine( "XPath pattern " + pattern + " found an element '" + n.getNodeName() + "'" );
+                                return AssertionStatus.NONE;
+                            default:
+                                _logger.fine( "XPath pattern " + pattern + " found some other node '" + n.toString() + "'" );
+                                return AssertionStatus.NONE;
+                        }
+                    } else {
+                        _logger.fine( "XPath pattern " + pattern + " matched response" );
+                        return AssertionStatus.NONE;
+                    }
+                }
+            } catch (SAXException e) {
+                _logger.log( Level.WARNING, "Caught SAXException during XPath query", e );
+                return AssertionStatus.SERVER_ERROR;
+            } catch (IOException e) {
+                _logger.log( Level.WARNING, "Caught IOException during XPath query", e );
+                return AssertionStatus.SERVER_ERROR;
+            } catch (JaxenException e) {
+                _logger.log( Level.WARNING, "Caught JaxenException during XPath query", e );
+                return AssertionStatus.SERVER_ERROR;
+            }
+        }
+        _logger.warning( "ResponseXPathAssertion only works on XML responses!" );
+        return AssertionStatus.FALSIFIED;
+    }
+
+    private final ResponseXpathAssertion _data;
+    private final Logger _logger = Logger.getLogger(getClass().getName());
+    private DOMXPath _domXpath;
+}
