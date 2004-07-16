3a77e8de4aceea1013dd3a511009e88c8ff38c48
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-07-16 22:05:30 +0000

    A reference to an id provider.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@6993 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/policy/exporter/IdProviderReference.java
@@ -0,0 +1,87 @@
+package com.l7tech.policy.exporter;
+
+import org.w3c.dom.Element;
+import com.l7tech.common.util.Locator;
+import com.l7tech.identity.IdentityProviderConfigManager;
+import com.l7tech.identity.IdentityProviderConfig;
+import com.l7tech.objectmodel.FindException;
+
+import java.util.logging.Logger;
+import java.util.logging.Level;
+import java.io.IOException;
+
+/**
+ * A reference to an id provider.
+ * <p/>
+ * <br/><br/>
+ * LAYER 7 TECHNOLOGIES, INC<br/>
+ * User: flascell<br/>
+ * Date: Jul 16, 2004<br/>
+ * $Id$<br/>
+ */
+public class IdProviderReference extends ExternalReference {
+    /**
+     * Constructor meant to be used by the export process.
+     * This will actually retreive the id provider and remember its
+     * settings.
+     */
+    public IdProviderReference(long providerId) {
+        // try to retrieve this id provider to remember its settings
+        IdentityProviderConfigManager manager = (IdentityProviderConfigManager)
+                                                  Locator.getDefault().lookup(IdentityProviderConfigManager.class);
+        IdentityProviderConfig config = null;
+        if (manager != null) {
+            try {
+                manager.findByPrimaryKey(providerId);
+            } catch (FindException e) {
+                logger.log(Level.WARNING, "error finding id provider config", e);
+            }
+        }
+        if (config == null) {
+            logger.severe("This policy is referring to an Id provider that cannot be retrieved.");
+        } else {
+            try {
+                setIdProviderConfProps(config.getSerializedProps());
+            } catch (IOException e) {
+                logger.log(Level.WARNING, "Error getting properties from id provider", e);
+            }
+        }
+        setProviderId(providerId);
+    }
+
+    public void serializeToRefElement(Element referencesParentElement) {
+        // todo
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof IdProviderReference)) return false;
+        final IdProviderReference idProviderReference = (IdProviderReference)o;
+        if (providerId != idProviderReference.providerId) return false;
+        return true;
+    }
+
+    public int hashCode() {
+        return (int) (providerId ^ (providerId >>> 32));
+    }
+
+    public long getProviderId() {
+        return providerId;
+    }
+
+    public void setProviderId(long providerId) {
+        this.providerId = providerId;
+    }
+
+    public String getIdProviderConfProps() {
+        return idProviderConfProps;
+    }
+
+    public void setIdProviderConfProps(String idProviderConfProps) {
+        this.idProviderConfProps = idProviderConfProps;
+    }
+
+    private long providerId;
+    private String idProviderConfProps;
+    private final Logger logger = Logger.getLogger(IdProviderReference.class.getName());
+}
