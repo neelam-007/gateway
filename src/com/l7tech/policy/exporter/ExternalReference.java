e3ec7ba1f4c53d3a1161efdc1553149675ea5070
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-07-16 22:05:10 +0000

    An external reference used by an exported policy.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@6992 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/policy/exporter/ExternalReference.java
@@ -0,0 +1,20 @@
+package com.l7tech.policy.exporter;
+
+import org.w3c.dom.Element;
+
+/**
+ * An external reference used by an exported policy.
+ * <p/>
+ * <br/><br/>
+ * LAYER 7 TECHNOLOGIES, INC<br/>
+ * User: flascell<br/>
+ * Date: Jul 16, 2004<br/>
+ * $Id$<br/>
+ */
+public abstract class ExternalReference {
+    public abstract void serializeToRefElement(Element referencesParentElement);
+    public static ExternalReference fromElement(Element serializedReference) {
+        // todo
+        return null;
+    }
+}
