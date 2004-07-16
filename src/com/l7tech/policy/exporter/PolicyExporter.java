9a76939e10d39da16b878f96ff9090b498598f80
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-07-16 22:05:50 +0000

    added appendRelatedReferences
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@6994 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/policy/exporter/PolicyExporter.java
+++ src/com/l7tech/policy/exporter/PolicyExporter.java
@@ -1,15 +1,21 @@
 package com.l7tech.policy.exporter;
 
+import com.l7tech.common.util.XmlUtil;
+import com.l7tech.policy.assertion.Assertion;
+import com.l7tech.policy.assertion.identity.SpecificUser;
+import com.l7tech.policy.assertion.composite.CompositeAssertion;
+import com.l7tech.policy.wsp.WspWriter;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
-import com.l7tech.policy.assertion.Assertion;
-import com.l7tech.policy.wsp.WspWriter;
-import com.l7tech.policy.wsp.WspConstants;
-import com.l7tech.common.util.XmlUtil;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.FileOutputStream;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Collection;
+import java.util.ArrayList;
 
 /**
  * Exports a Policy to an XML file that contains details of all external
@@ -27,16 +33,57 @@ public class PolicyExporter {
         // do policy to xml
         Document policydoc = XmlUtil.stringToDocument(WspWriter.getPolicyXml(rootAssertion));
         // go through each assertion and list external dependencies
-        Element referencesEl = wrapExportReferencesToPolicyDocument(policydoc);
+        Collection refs = new ArrayList();
+        traverseAssertionTreeForReferences(rootAssertion, refs);
         // add external dependencies to document
-        // todo
+        Element referencesEl = wrapExportReferencesToPolicyDocument(policydoc);
+        serializeReferences(referencesEl, (ExternalReference[])refs.toArray(new ExternalReference[0]));
         return policydoc;
+    }
+
+    private void serializeReferences(Element referencesEl, ExternalReference[] references) {
+        for (int i = 0; i < references.length; i++) {
+            references[i].serializeToRefElement(referencesEl);
+        }
     }
 
     public void exportToFile(Assertion rootAssertion, File outputFile) throws IOException, SAXException {
         Document doc = exportToDocument(rootAssertion);
         // write doc to file
-        // todo
+        FileOutputStream fos = new FileOutputStream(outputFile);
+        fos.write(XmlUtil.nodeToString(doc).getBytes());
+        fos.close();
+    }
+
+    /**
+     * Recursively go through an assertion tree populating the references as necessary.
+     */
+    private void traverseAssertionTreeForReferences(Assertion rootAssertion, Collection refs) {
+        if (rootAssertion instanceof CompositeAssertion) {
+            CompositeAssertion ca = (CompositeAssertion)rootAssertion;
+            List children = ca.getChildren();
+            for (Iterator i = children.iterator(); i.hasNext();) {
+                Assertion child = (Assertion)i.next();
+                traverseAssertionTreeForReferences(child, refs);
+            }
+        } else {
+            appendRelatedReferences(rootAssertion, refs);
+        }
+    }
+
+    /**
+     * Adds ExternalReference instances to refs collestion in relation to the assertion
+     * if applicable
+     */
+    private void appendRelatedReferences(Assertion assertion, Collection refs) {
+        if (assertion instanceof SpecificUser) {
+            SpecificUser suassertion = (SpecificUser)assertion;
+            IdProviderReference ref = new IdProviderReference(suassertion.getIdentityProviderOid());
+            if (!refs.contains(ref)) {
+                refs.add(ref);
+            }
+        }
+        // todo, same for all assertion types
     }
 
     private Element wrapExportReferencesToPolicyDocument(Document originalPolicy) {
