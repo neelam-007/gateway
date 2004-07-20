f8cc87d2bb11829f9f3a18398c9ccdd3f7e8d6a7
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-07-20 17:06:50 +0000

    A reference to a CustomAssertion type. This reference is exported alongside a policy when a custom assertion of that type exists in the policy. At import time, the importer will verify that this type of custom assertion is installed before importing the policy.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@7025 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/policy/exporter/CustomAssertionReference.java
@@ -0,0 +1,51 @@
+package com.l7tech.policy.exporter;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.Text;
+
+/**
+ * A reference to a CustomAssertion type. This reference is exported alongside
+ * a policy when a custom assertion of that type exists in the policy. At import
+ * time, the importer will verify that this type of custom assertion is installed
+ * before importing the policy.
+ * <p/>
+ * <br/><br/>
+ * LAYER 7 TECHNOLOGIES, INC<br/>
+ * User: flascell<br/>
+ * Date: Jul 20, 2004<br/>
+ * $Id$<br/>
+ */
+public class CustomAssertionReference extends ExternalReference {
+    public CustomAssertionReference(String customAssertionName) {
+        this.customAssertionName = customAssertionName;
+    }
+
+    public void serializeToRefElement(Element referencesParentElement) {
+        Element refEl = referencesParentElement.getOwnerDocument().createElement("CustomAssertionReference");
+        refEl.setAttribute(ExporterConstants.REF_TYPE_ATTRNAME, IdProviderReference.class.getName());
+        referencesParentElement.appendChild(refEl);
+        Element nameEl = referencesParentElement.getOwnerDocument().createElement("CustomAssertionName");
+        if (customAssertionName != null) {
+            Text txt = referencesParentElement.getOwnerDocument().createTextNode(customAssertionName);
+            nameEl.appendChild(txt);
+        }
+        refEl.appendChild(nameEl);
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CustomAssertionReference)) return false;
+
+        final CustomAssertionReference customAssertionReference = (CustomAssertionReference) o;
+
+        if (customAssertionName != null ? !customAssertionName.equals(customAssertionReference.customAssertionName) : customAssertionReference.customAssertionName != null) return false;
+
+        return true;
+    }
+
+    public int hashCode() {
+        return (customAssertionName != null ? customAssertionName.hashCode() : 0);
+    }
+
+    private String customAssertionName;
+}
