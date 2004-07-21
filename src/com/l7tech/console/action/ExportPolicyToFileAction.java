dbf0c930a73004d16c2f274cd8aca2cd8e6b52b7
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-07-21 17:24:04 +0000

    The SSM action type that saves a policy to a file using the new policy export mechanism.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@7053 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/console/action/ExportPolicyToFileAction.java
@@ -0,0 +1,117 @@
+package com.l7tech.console.action;
+
+import com.l7tech.console.logging.ErrorManager;
+import com.l7tech.console.tree.PoliciesFolderNode;
+import com.l7tech.console.tree.policy.AssertionTreeNode;
+import com.l7tech.console.util.Preferences;
+import com.l7tech.console.util.TopComponents;
+import com.l7tech.policy.exporter.PolicyExporter;
+import org.xml.sax.SAXException;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+/**
+ * The SSM action type that saves a policy to a file using the policy export mechanism.
+ *
+ * <p/>
+ * <br/><br/>
+ * LAYER 7 TECHNOLOGIES, INC<br/>
+ * User: flascell<br/>
+ * Date: Jul 21, 2004<br/>
+ */
+public class ExportPolicyToFileAction extends BaseAction {
+    static final Logger log = Logger.getLogger(ExportPolicyToFileAction.class.getName());
+    protected AssertionTreeNode node;
+
+    public ExportPolicyToFileAction() {
+    }
+
+    public ExportPolicyToFileAction(AssertionTreeNode node) {
+        if (node == null) {
+            throw new IllegalArgumentException();
+        }
+        this.node = node;
+    }
+
+    /**
+     * @return the action name
+     */
+    public String getName() {
+        return "Export to File";
+    }
+
+    /**
+     * @return the aciton description
+     */
+    public String getDescription() {
+        return "Exports the policy to a file along with external references.";
+    }
+
+    /**
+     * subclasses override this method specifying the resource name
+     */
+    protected String iconResource() {
+        return "com/l7tech/console/resources/saveTemplate.gif";
+    }
+
+    /** Actually perform the action.
+     * This is the method which should be called programmatically.
+
+     * note on threading usage: do not access GUI components
+     * without explicitly asking for the AWT event thread!
+     */
+    public void performAction() {
+        if (node == null) {
+            throw new IllegalStateException("no node specified");
+        }
+
+        File templateDir = null;
+        try {
+            templateDir = new File(Preferences.getPreferences().getHomePath() +
+                                   File.separator + PoliciesFolderNode.TEMPLATES_DIR);
+            if (!templateDir.exists()) {
+                if (!templateDir.mkdir()) {
+                    throw new IOException("Cannot create " + templateDir.getPath());
+                }
+            }
+        } catch (IOException e) {
+            ErrorManager.getDefault().notify(Level.WARNING,
+                                             e,
+                                             "The system reported problem in accessing or creating" +
+                                             "the policy template directory " + templateDir.getPath() + "\n" +
+                                             "The policy template is not saved.");
+            return;
+        }
+
+        JFileChooser chooser = new JFileChooser(templateDir);
+        chooser.setDialogTitle("Export to ...");
+        // Allow single selection only
+        chooser.setMultiSelectionEnabled(false);
+        int ret = chooser.showSaveDialog(TopComponents.getInstance().getMainWindow());
+        if (JFileChooser.APPROVE_OPTION != ret) return;
+        String name = chooser.getSelectedFile().getPath();
+        int overwrite = JOptionPane.YES_OPTION;
+        File policyFile = new File(name);
+        final boolean policyFileExists = policyFile.exists();
+        if (policyFileExists) {
+            overwrite = JOptionPane.showConfirmDialog(TopComponents.getInstance().getMainWindow(),
+                                                      "Overwrite " + name + "?",
+                                                      "Warning",
+                                                      JOptionPane.YES_NO_OPTION);
+        }
+        if (overwrite != JOptionPane.YES_OPTION) return;
+        PolicyExporter exporter = new PolicyExporter();
+        try {
+            exporter.exportToFile(node.asAssertion(), policyFile);
+        } catch (IOException e) {
+            ErrorManager.getDefault().notify(Level.WARNING, e, "Cannot export policy to file " + name);
+        } catch (SAXException e) {
+            ErrorManager.getDefault().notify(Level.WARNING, e, "Cannot export policy to file " + name);
+        }
+    }
+}
