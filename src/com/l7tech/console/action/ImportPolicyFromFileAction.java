c361496c792fd53cbc761877edd850805f205e5f
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-07-21 22:47:53 +0000

    The SSM action type that imports a policy from a file.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@7065 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/console/action/ImportPolicyFromFileAction.java
@@ -0,0 +1,64 @@
+package com.l7tech.console.action;
+
+import com.l7tech.service.PublishedService;
+
+import java.util.logging.Logger;
+
+
+/**
+ * The SSM action type that imports a policy from a file.
+ *
+ * <p/>
+ * <br/><br/>
+ * LAYER 7 TECHNOLOGIES, INC<br/>
+ * User: flascell<br/>
+ * Date: Jul 21, 2004<br/>
+ */
+public class ImportPolicyFromFileAction extends BaseAction {
+    static final Logger log = Logger.getLogger(ImportPolicyFromFileAction.class.getName());
+    protected PublishedService service;
+
+    public ImportPolicyFromFileAction() {
+    }
+
+    public ImportPolicyFromFileAction(PublishedService svc) {
+        if (svc == null) {
+            throw new IllegalArgumentException();
+        }
+        this.service = svc;
+    }
+
+    /**
+     * @return the action name
+     */
+    public String getName() {
+        return "Import Policy";
+    }
+
+    /**
+     * @return the aciton description
+     */
+    public String getDescription() {
+        return "Import a policy from a file along with external references.";
+    }
+
+    /**
+     * subclasses override this method specifying the resource name
+     */
+    protected String iconResource() {
+        return "com/l7tech/console/resources/saveTemplate.gif";
+    }
+
+    /** Actually perform the action.
+     * This is the method which should be called programmatically.
+
+     * note on threading usage: do not access GUI components
+     * without explicitly asking for the AWT event thread!
+     */
+    public void performAction() {
+        if (service == null) {
+            throw new IllegalStateException("no service specified");
+        }
+        // todo
+    }
+}
