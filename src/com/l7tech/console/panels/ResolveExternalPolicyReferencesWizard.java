81c9df0d55c48c4ccf4d56d4339e14e388bdeaaf
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-07-26 20:46:52 +0000

    Link panels together, add title.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@7180 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/console/panels/ResolveExternalPolicyReferencesWizard.java
+++ src/com/l7tech/console/panels/ResolveExternalPolicyReferencesWizard.java
@@ -1,6 +1,7 @@
 package com.l7tech.console.panels;
 
 import com.l7tech.policy.exporter.ExternalReference;
+import com.l7tech.policy.exporter.IdProviderReference;
 
 import java.awt.*;
 
@@ -20,14 +21,33 @@ public class ResolveExternalPolicyReferencesWizard extends Wizard {
      * Convenient method to create a wizard based on a number of problematic external references.
      * @param refs the external references that could not be automatically resolved.
      */
-    public static ResolveExternalPolicyReferencesWizard fromReferences(ExternalReference[] refs) {
+    public static ResolveExternalPolicyReferencesWizard fromReferences(Frame parent, ExternalReference[] refs) {
         if (refs == null || refs.length < 1) {
             throw new IllegalArgumentException("cannot create wizard without references.");
         }
-        // todo
-        return null;
+        WizardStepPanel previousPanel = null;
+        WizardStepPanel firstPanel = null;
+        for (int i = 0; i < refs.length; i++) {
+            WizardStepPanel panel = null;
+            if (refs[i] instanceof IdProviderReference) {
+                panel = new ResolveForeignIdentityProviderPanel(null, (IdProviderReference)(refs[i]));
+            } else {
+                // todo, other types of panels
+            }
+            if (panel != null) {
+                if (firstPanel == null) {
+                    firstPanel = panel;
+                }
+                if (previousPanel != null) {
+                    previousPanel.setNextPanel(panel);
+                }
+                previousPanel = panel;
+            }
+        }
+        return new ResolveExternalPolicyReferencesWizard(parent, firstPanel);
     }
     public ResolveExternalPolicyReferencesWizard(Frame parent, WizardStepPanel panel) {
         super(parent, panel);
+        setTitle("Resolve external dependencies from exported policy document");
     }
 }
