1b892a7d7e9f5a4bc2f02b0576dc5b5171fb3d8b
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-07-31 00:11:31 +0000

    JAVADOCKED!
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@7325 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/identity/IdentityProvider.java
+++ src/com/l7tech/identity/IdentityProvider.java
@@ -11,14 +11,35 @@ import java.util.Collection;
  * @author alex
  */
 public interface IdentityProvider {
-    IdentityProviderConfig getConfig();
-    UserManager getUserManager();
-    GroupManager getGroupManager();
-
     /**
-     * @return a {@link User}. Will never be null.
+     * Called by {@link com.l7tech.server.policy.assertion.identity.ServerIdentityAssertion} to try to
+     * identify a {@link User} based on a {@link LoginCredentials} that was previously attached to the
+     * message by a {@link com.l7tech.policy.assertion.credential.CredentialSourceAssertion}.
+     * <p>
+     * If this method returns a User, the authentication was successful, but whether that user is
+     * authorized is then left up to the implementations of ServerIdentityAssertion, namely
+     * {@link com.l7tech.server.policy.assertion.identity.ServerSpecificUser} and
+     * {@link com.l7tech.server.policy.assertion.identity.ServerMemberOfGroup}.
+     *
+     * @param pc an identity and a set of credentials.
+     * @return a {@link User}. Must never be null.
      */
     User authenticate( LoginCredentials pc ) throws AuthenticationException, FindException, IOException;
+
+    /**
+     * @return the {@link IdentityProviderConfig} for this IdentityProvider.
+     */
+    IdentityProviderConfig getConfig();
+
+    /**
+     * @return the {@link UserManager} for this IdentityProvider. Currently should never be null, but might be in future.
+     */
+    UserManager getUserManager();
+
+    /**
+     * @return the {@link GroupManager} for this IdentityProvider. Currently should never be null, but might be in future.
+     */
+    GroupManager getGroupManager();
 
     /**
      * searches for users and groups whose name match the pattern described in searchString
