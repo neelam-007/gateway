51c7f0192bc979a324eab9de7c906128671f4a44
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-08-03 15:31:48 +0000

    A secure conversation session used between a client and the ssg.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@7329 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/server/secureconversation/SecureConversationSession.java
@@ -0,0 +1,62 @@
+package com.l7tech.server.secureconversation;
+
+import com.l7tech.identity.User;
+
+import javax.crypto.SecretKey;
+
+/**
+ * A secure conversation session used between a client and the ssg.
+ * <p/>
+ * <br/><br/>
+ * LAYER 7 TECHNOLOGIES, INC<br/>
+ * User: flascell<br/>
+ * Date: Aug 3, 2004<br/>
+ * $Id$<br/>
+ */
+public class SecureConversationSession {
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public SecretKey getSharedSecret() {
+        return sharedSecret;
+    }
+
+    public void setSharedSecret(SecretKey sharedSecret) {
+        this.sharedSecret = sharedSecret;
+    }
+
+    public long getExpiration() {
+        return expiration;
+    }
+
+    public void setExpiration(long expiration) {
+        this.expiration = expiration;
+    }
+
+    public long getCreation() {
+        return creation;
+    }
+
+    public void setCreation(long creation) {
+        this.creation = creation;
+    }
+
+    public User getUsedBy() {
+        return usedBy;
+    }
+
+    public void setUsedBy(User usedBy) {
+        this.usedBy = usedBy;
+    }
+
+    private String identifier;
+    private SecretKey sharedSecret;
+    private long expiration;
+    private long creation;
+    private User usedBy;
+}
