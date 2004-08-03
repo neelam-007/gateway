2f5ee7666ef600331d8f32c37cf7ec0914e8c64a
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-08-03 15:50:29 +0000

    changed name of SecretKey accessor
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@7332 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/common/security/xml/WssProcessor.java
+++ src/com/l7tech/common/security/xml/WssProcessor.java
@@ -52,7 +52,7 @@ public interface WssProcessor {
     }
 
     public interface SecurityContext {
-        SecretKey getSecretKey();
+        SecretKey getSharedSecret();
     }
 
     public interface SecurityContextFinder {
