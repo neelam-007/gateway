be854baac5f8b979647ca5ab9d4c8ada0deb2f30
fpang <fpang@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-08-03 22:54:38 +0000

    Initial creation.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@7366 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/console/panels/VirtualGroupDetailsPanel.java
@@ -0,0 +1,56 @@
+package com.l7tech.console.panels;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * <p> Copyright (C) 2004 Layer 7 Technologies Inc.</p>
+ * <p> @author fpang </p>
+ * $Id$
+ */
+public class VirtualGroupDetailsPanel extends JPanel {
+
+    private JPanel mainPanel;
+    private JTextField groupDescTextField;
+    private JTextField x509SubjectDNTextField;
+    private JTextField emailTextField;
+
+    /**
+     *  Constructor
+     */
+    public VirtualGroupDetailsPanel() {
+        initComponents();
+    }
+
+    /**
+     * This method is called from within the constructor to
+     * initialize the dialog.
+     */
+    private void initComponents() {
+        this.add(mainPanel);
+    }
+
+    public JTextField getGroupDescTextField() {
+        return groupDescTextField;
+    }
+
+    public void setGroupDescTextField(JTextField groupDescTextField) {
+        this.groupDescTextField = groupDescTextField;
+    }
+
+    public JTextField getX509SubjectDNTextField() {
+        return x509SubjectDNTextField;
+    }
+
+    public void setX509SubjectDNTextField(JTextField x509SubjectDNTextField) {
+        this.x509SubjectDNTextField = x509SubjectDNTextField;
+    }
+
+    public JTextField getEmailTextField() {
+        return emailTextField;
+    }
+
+    public void setEmailTextField(JTextField emailTextField) {
+        this.emailTextField = emailTextField;
+    }
+}
---
+++ src/com/l7tech/console/panels/VirtualGroupMembershipPanel.java
@@ -0,0 +1,29 @@
+package com.l7tech.console.panels;
+
+import javax.swing.*;
+
+/**
+ * <p> Copyright (C) 2004 Layer 7 Technologies Inc.</p>
+ * <p> @author fpang </p>
+ * $Id$
+ */
+public class VirtualGroupMembershipPanel extends JPanel {
+
+    private JPanel mainPanel;
+
+    /**
+     *  Constructor
+     */
+    public VirtualGroupMembershipPanel() {
+            initComponents();
+        }
+
+    /**
+      * This method is called from within the constructor to
+      * initialize the dialog.
+      */
+     private void initComponents() {
+         this.add(mainPanel);
+     }
+
+}
