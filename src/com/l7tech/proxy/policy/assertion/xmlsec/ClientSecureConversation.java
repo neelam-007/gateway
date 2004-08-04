d6987a519fe13d8e4e0c5f219cb8692f0d3b1715
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-08-04 18:53:51 +0000

    The client side processing of the SecureConversation assertion.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@7413 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/proxy/policy/assertion/xmlsec/ClientSecureConversation.java
@@ -0,0 +1,50 @@
+package com.l7tech.proxy.policy.assertion.xmlsec;
+
+import com.l7tech.proxy.policy.assertion.ClientAssertion;
+import com.l7tech.proxy.datamodel.PendingRequest;
+import com.l7tech.proxy.datamodel.SsgResponse;
+import com.l7tech.proxy.datamodel.exceptions.*;
+import com.l7tech.policy.assertion.AssertionStatus;
+import com.l7tech.policy.assertion.PolicyAssertionException;
+import com.l7tech.policy.assertion.xmlsec.SecureConversation;
+import com.l7tech.common.xml.InvalidDocumentFormatException;
+
+import java.security.GeneralSecurityException;
+import java.io.IOException;
+
+import org.xml.sax.SAXException;
+
+/**
+ * The client side processing of the SecureConversation assertion.
+ * <p/>
+ * <br/><br/>
+ * LAYER 7 TECHNOLOGIES, INC<br/>
+ * User: flascell<br/>
+ * Date: Aug 4, 2004<br/>
+ * $Id$<br/>
+ */
+public class ClientSecureConversation extends ClientAssertion {
+    public ClientSecureConversation(SecureConversation assertion) {
+        // nothing in assertion we need to remember
+    }
+
+    public AssertionStatus unDecorateReply(PendingRequest request, SsgResponse response) throws BadCredentialsException, OperationCanceledException, GeneralSecurityException, IOException, SAXException, ResponseValidationException, KeyStoreCorruptException, PolicyAssertionException, InvalidDocumentFormatException {
+        // todo
+        return null;
+    }
+
+    public String getName() {
+        // todo
+        return null;
+    }
+
+    public String iconResource(boolean open) {
+        // todo
+        return null;
+    }
+
+    public AssertionStatus decorateRequest(PendingRequest request) throws BadCredentialsException, OperationCanceledException, GeneralSecurityException, ClientCertificateException, IOException, SAXException, KeyStoreCorruptException, HttpChallengeRequiredException, PolicyRetryableException, PolicyAssertionException, InvalidDocumentFormatException {
+        // todo
+        return null;
+    }
+}
