792d4f5825dabd72aa28b5aa99f552e82f85ecbc
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-08-04 20:33:21 +0000

    implemented checkRequest
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@7422 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/server/policy/assertion/xmlsec/ServerSecureConversation.java
+++ src/com/l7tech/server/policy/assertion/xmlsec/ServerSecureConversation.java
@@ -1,13 +1,19 @@
 package com.l7tech.server.policy.assertion.xmlsec;
 
 import com.l7tech.server.policy.assertion.ServerAssertion;
+import com.l7tech.server.secureconversation.SecureConversationContextManager;
+import com.l7tech.server.secureconversation.SecureConversationSession;
 import com.l7tech.policy.assertion.AssertionStatus;
 import com.l7tech.policy.assertion.PolicyAssertionException;
 import com.l7tech.policy.assertion.xmlsec.SecureConversation;
 import com.l7tech.message.Request;
 import com.l7tech.message.Response;
+import com.l7tech.message.SoapRequest;
+import com.l7tech.common.security.xml.WssProcessor;
+import com.l7tech.identity.User;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 /**
  * The SSG-side processing of the SecureConversation assertion.
@@ -24,7 +30,30 @@ public class ServerSecureConversation implements ServerAssertion {
         // nothing to remember from the passed assertion
     }
     public AssertionStatus checkRequest(Request request, Response response) throws IOException, PolicyAssertionException {
-        // todo
-        return null;
+        if (!(request instanceof SoapRequest)) {
+            logger.info("This type of assertion is only supported with SOAP type of messages");
+            return AssertionStatus.BAD_REQUEST;
+        }
+        SoapRequest soapreq = (SoapRequest)request;
+        WssProcessor.ProcessorResult wssResults = soapreq.getWssProcessorOutput();
+        if (wssResults == null) {
+            throw new IOException("This request was not processed for WSS message level security.");
+        }
+        WssProcessor.SecurityToken[] tokens = wssResults.getSecurityTokens();
+        for (int i = 0; i < tokens.length; i++) {
+            WssProcessor.SecurityToken token = tokens[i];
+            if (token instanceof WssProcessor.SecurityContextToken) {
+                WssProcessor.SecurityContextToken secConTok = (WssProcessor.SecurityContextToken)token;
+                String contextId = secConTok.getContextIdentifier();
+                SecureConversationSession session = SecureConversationContextManager.getInstance().getSession(contextId);
+                User authenticatedUser = session.getUsedBy();
+                request.setAuthenticated(true);
+                request.setUser(authenticatedUser);
+                return AssertionStatus.NONE;
+            }
+        }
+        return AssertionStatus.AUTH_REQUIRED;
     }
+
+    private final Logger logger = Logger.getLogger(getClass().getName());
 }
