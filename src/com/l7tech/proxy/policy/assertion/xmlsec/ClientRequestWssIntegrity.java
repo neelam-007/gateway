827de616de3805f70d57bee6d9066b4db7e7977b
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-08-05 00:09:53 +0000

    Signing requests does not require a client cert.  They can be signed using a client cert or a ws-sc session; the encryption assertion should not care.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@7454 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/proxy/policy/assertion/xmlsec/ClientRequestWssIntegrity.java
+++ src/com/l7tech/proxy/policy/assertion/xmlsec/ClientRequestWssIntegrity.java
@@ -7,21 +7,17 @@ import com.l7tech.policy.assertion.AssertionStatus;
 import com.l7tech.policy.assertion.PolicyAssertionException;
 import com.l7tech.policy.assertion.xmlsec.RequestWssIntegrity;
 import com.l7tech.proxy.datamodel.PendingRequest;
-import com.l7tech.proxy.datamodel.Ssg;
-import com.l7tech.proxy.datamodel.SsgKeyStoreManager;
 import com.l7tech.proxy.datamodel.SsgResponse;
 import com.l7tech.proxy.datamodel.exceptions.*;
 import com.l7tech.proxy.policy.assertion.ClientAssertion;
 import com.l7tech.proxy.policy.assertion.ClientDecorator;
 import com.l7tech.proxy.policy.assertion.credential.http.ClientHttpClientCert;
-import java.util.logging.Logger;
 import org.jaxen.JaxenException;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
-import java.security.PrivateKey;
-import java.security.cert.X509Certificate;
 import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * XML Digital signature on the soap request sent from the proxy to the ssg server. Also does XML
@@ -58,16 +54,6 @@ public class ClientRequestWssIntegrity extends ClientAssertion {
                    GeneralSecurityException, IOException, KeyStoreCorruptException, HttpChallengeRequiredException,
                    PolicyRetryableException, ClientCertificateException
     {
-        request.getCredentials();
-        request.prepareClientCertificate();
-
-        // get the client cert and private key
-        // We must have credentials to get the private key
-        final Ssg ssg = request.getSsg();
-        final X509Certificate userCert = SsgKeyStoreManager.getClientCert(ssg);
-        final PrivateKey userPrivateKey = SsgKeyStoreManager.getClientCertPrivateKey(ssg);
-        final X509Certificate ssgCert = SsgKeyStoreManager.getServerCert(ssg);
-
         // add a pending decoration that will be applied only if the rest of this policy branch succeeds
         request.getPendingDecorations().put(this, new ClientDecorator() {
             public AssertionStatus decorateRequest(PendingRequest request) throws PolicyAssertionException {
@@ -86,9 +72,6 @@ public class ClientRequestWssIntegrity extends ClientAssertion {
                     // get the client cert and private key
                     // We must have credentials to get the private key
                     WssDecorator.DecorationRequirements wssReqs = request.getWssRequirements();
-                    wssReqs.setRecipientCertificate(ssgCert);
-                    wssReqs.setSenderCertificate(userCert);
-                    wssReqs.setSenderPrivateKey(userPrivateKey);
                     wssReqs.getElementsToSign().addAll(elements);
                     return AssertionStatus.NONE;
                 } catch (JaxenException e) {
