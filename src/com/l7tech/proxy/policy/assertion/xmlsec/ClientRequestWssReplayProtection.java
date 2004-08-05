622ee94e9298a598fa444a8cb78941edda1dac85
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-08-05 00:07:28 +0000

    Replay protection should not require a client cert.  It requires that the timestamp be signed, but doesn't care whether it is signed with a client cert or with a secure conversation session.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@7452 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/proxy/policy/assertion/xmlsec/ClientRequestWssReplayProtection.java
+++ src/com/l7tech/proxy/policy/assertion/xmlsec/ClientRequestWssReplayProtection.java
@@ -5,8 +5,6 @@ import com.l7tech.policy.assertion.AssertionStatus;
 import com.l7tech.policy.assertion.PolicyAssertionException;
 import com.l7tech.policy.assertion.xmlsec.RequestWssReplayProtection;
 import com.l7tech.proxy.datamodel.PendingRequest;
-import com.l7tech.proxy.datamodel.Ssg;
-import com.l7tech.proxy.datamodel.SsgKeyStoreManager;
 import com.l7tech.proxy.datamodel.SsgResponse;
 import com.l7tech.proxy.datamodel.exceptions.*;
 import com.l7tech.proxy.policy.assertion.ClientAssertion;
@@ -14,8 +12,6 @@ import com.l7tech.proxy.policy.assertion.ClientDecorator;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
-import java.security.PrivateKey;
-import java.security.cert.X509Certificate;
 import java.util.logging.Logger;
 
 /**
@@ -45,23 +41,12 @@ public class ClientRequestWssReplayProtection extends ClientAssertion {
             GeneralSecurityException, IOException, KeyStoreCorruptException, HttpChallengeRequiredException,
             PolicyRetryableException, ClientCertificateException
     {
-        request.getCredentials();
-        request.prepareClientCertificate();
-
-        // get the client cert and private key
-        // We must have credentials to get the private key
-        final Ssg ssg = request.getSsg();
-        final X509Certificate userCert = SsgKeyStoreManager.getClientCert(ssg);
-        final PrivateKey userPrivateKey = SsgKeyStoreManager.getClientCertPrivateKey(ssg);
-
         // add a pending decoration that will be applied only if the rest of this policy branch succeeds
         request.getPendingDecorations().put(this, new ClientDecorator() {
             public AssertionStatus decorateRequest(PendingRequest request) throws PolicyAssertionException {
                 // get the client cert and private key
                 // We must have credentials to get the private key
                 WssDecorator.DecorationRequirements wssReqs = request.getWssRequirements();
-                wssReqs.setSenderCertificate(userCert);
-                wssReqs.setSenderPrivateKey(userPrivateKey);
                 wssReqs.setSignTimestamp(true);
                 return AssertionStatus.NONE;
             }
