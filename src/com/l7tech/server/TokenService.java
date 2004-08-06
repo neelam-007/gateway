5b45e69106893d920d87c63c1b941599bec3898b
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-08-06 22:43:51 +0000

    isValidRequestForSecureConversationContext
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@7517 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/server/TokenService.java
+++ src/com/l7tech/server/TokenService.java
@@ -3,12 +3,15 @@ package com.l7tech.server;
 import com.l7tech.common.security.xml.WssProcessor;
 import com.l7tech.common.security.xml.WssProcessorImpl;
 import com.l7tech.common.util.KeystoreUtils;
+import com.l7tech.common.util.SoapUtil;
+import com.l7tech.common.util.XmlUtil;
 import com.l7tech.common.xml.InvalidDocumentFormatException;
 import com.l7tech.identity.User;
 import com.l7tech.policy.assertion.credential.CredentialFormat;
 import com.l7tech.policy.assertion.credential.LoginCredentials;
 import com.l7tech.server.secureconversation.SecureConversationContextManager;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import sun.security.x509.X500Name;
 
 import java.io.ByteArrayInputStream;
@@ -131,9 +134,52 @@ public class TokenService {
         return null;
     }
 
-    private boolean isValidRequestForSecureConversationContext(Document request, WssProcessor.ProcessorResult wssOutput) {
-        // todo
-        return true;
+    private boolean isValidRequestForSecureConversationContext(Document request,
+                                                               WssProcessor.ProcessorResult wssOutput)
+                                                                        throws InvalidDocumentFormatException {
+        Element body = SoapUtil.getBodyElement(request);
+        // body must include wst:RequestSecurityToken element
+        Element maybeRSTEl = XmlUtil.findFirstChildElement(body);
+        if (!maybeRSTEl.getLocalName().equals(RST_ELNAME)) {
+            logger.fine("Body's child does not seem to be a RST (" + maybeRSTEl.getLocalName() + ")");
+            return false;
+        }
+        if (!maybeRSTEl.getNamespaceURI().equals(SoapUtil.WST_NAMESPACE)) {
+            logger.fine("Trust namespace not recognized (" + maybeRSTEl.getNamespaceURI() + ")");
+            return false;
+        }
+        // validate <wst:TokenType>http://schemas.xmlsoap.org/ws/2004/04/sct</wst:TokenType>
+        Element tokenTypeEl = XmlUtil.findOnlyOneChildElementByName(maybeRSTEl, SoapUtil.WST_NAMESPACE, TOKTYPE_ELNAME);
+        if (tokenTypeEl == null) {
+            logger.warning("Token type not specified. This is not supported.");
+            return false;
+        }
+        String value = XmlUtil.getTextValue(tokenTypeEl);
+        if (!value.equals("http://schemas.xmlsoap.org/ws/2004/04/sct")) {
+            logger.warning("TokenType not supported." + value);
+            return false;
+        }
+        // validate <wst:RequestType>http://schemas.xmlsoap.org/ws/2004/04/security/trust/Issue</wst:RequestType>
+        Element reqTypeEl = XmlUtil.findOnlyOneChildElementByName(maybeRSTEl, SoapUtil.WST_NAMESPACE, REQTYPE_ELNAME);
+        if (reqTypeEl == null) {
+            logger.warning("Request type not specified. This is not supported.");
+            return false;
+        }
+        value = XmlUtil.getTextValue(reqTypeEl);
+        if (!value.equals("http://schemas.xmlsoap.org/ws/2004/04/security/trust/Issue")) {
+            logger.warning("RequestType not supported." + value);
+            return false;
+        }
+        // make sure body was signed
+        WssProcessor.SignedElement[] signedElements = wssOutput.getElementsThatWereSigned();
+        for (int i = 0; i < signedElements.length; i++) {
+            WssProcessor.SignedElement signedElement = signedElements[i];
+            if (signedElement.asElement() == body) {
+                return true;
+            }
+        }
+        logger.warning("Seems like the body was not signed.");
+        return false;
     }
 
     private boolean isValidRequestForSAMLToken(Document request, WssProcessor.ProcessorResult wssOutput) {
@@ -161,4 +207,7 @@ public class TokenService {
     private X509Certificate serverCert = null;
 
     private final Logger logger = Logger.getLogger(getClass().getName());
+    private final static String RST_ELNAME = "RequestSecurityToken";
+    private final static String TOKTYPE_ELNAME = "TokenType";
+    private final static String REQTYPE_ELNAME = "RequestType";
 }
