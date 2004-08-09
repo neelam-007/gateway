14779899105c28480e52cb1298dbecd69b7ae472
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-08-09 21:29:35 +0000

    test with more than one broken external reference
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@7547 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- tests/com/l7tech/policy/exporter/ResolveForeignIdentityProviderPanelTest.java
+++ tests/com/l7tech/policy/exporter/ResolveForeignIdentityProviderPanelTest.java
@@ -1,8 +1,7 @@
 package com.l7tech.policy.exporter;
 
 import com.l7tech.common.gui.util.Utilities;
-import com.l7tech.console.panels.ResolveForeignIdentityProviderPanel;
-import com.l7tech.console.panels.Wizard;
+import com.l7tech.console.panels.ResolveExternalPolicyReferencesWizard;
 
 /**
  * Test the look and feel of the panel.
@@ -20,10 +19,15 @@ public class ResolveForeignIdentityProviderPanelTest {
         ref.setProviderName("MS International Directory");
         ref.setIdProviderTypeVal(2);
 
-        ResolveForeignIdentityProviderPanel panel = new ResolveForeignIdentityProviderPanel(null, ref);
-        Wizard wiz = new Wizard(null, panel);
+        IdProviderReference ref2 = new IdProviderReference(5);
+        ref2.setProviderName("IBM dir");
+        ref2.setIdProviderTypeVal(2);
+
+        //ResolveForeignIdentityProviderPanel panel = new ResolveForeignIdentityProviderPanel(null, ref);
+        //Wizard wiz = new Wizard(null, panel);
+        ResolveExternalPolicyReferencesWizard wiz = ResolveExternalPolicyReferencesWizard.fromReferences(null, new ExternalReference[]{ref, ref2});
         wiz.pack();
-        wiz.setSize(900, 500);
+        wiz.setSize(1000, 500);
         Utilities.centerOnScreen(wiz);
         wiz.setModal(true);
         wiz.setVisible(true);
