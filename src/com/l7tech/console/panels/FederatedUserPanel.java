789886eb5762cc170080ef340b0d9ee0513b5ff6
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-08-10 01:41:45 +0000

    Set max length of X.509 subject dn field to 255
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@7556 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/console/panels/FederatedUserPanel.java
+++ src/com/l7tech/console/panels/FederatedUserPanel.java
@@ -482,7 +482,7 @@ public class FederatedUserPanel extends UserPanel {
             x509SubjectNameTextField.setMinimumSize(new Dimension(200, 20));
             x509SubjectNameTextField.setPreferredSize(new Dimension(200, 20));
             x509SubjectNameTextField.setEditable(true);
-            x509SubjectNameTextField.setDocument(new MaxLengthDocument(50));
+            x509SubjectNameTextField.setDocument(new MaxLengthDocument(255));
             // Register listeners
             x509SubjectNameTextField.getDocument().addDocumentListener(documentListener);
         }
