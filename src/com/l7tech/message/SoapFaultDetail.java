acb9c2fd4feb6202854759a9081d1ea6b8878d9b
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-08-16 21:32:48 +0000

    Some error or exception that can be translated into a SOAP fault.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@7668 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/message/SoapFaultDetail.java
@@ -0,0 +1,16 @@
+package com.l7tech.message;
+
+/**
+ * Some error or exception that can be translated into a SOAP fault.
+ * <p/>
+ * <br/><br/>
+ * LAYER 7 TECHNOLOGIES, INC<br/>
+ * User: flascell<br/>
+ * Date: Aug 16, 2004<br/>
+ * $Id$
+ */
+public interface SoapFaultDetail {
+    String getFaultCode();
+    String getFaultString();
+    String getFaultDetails();
+}
