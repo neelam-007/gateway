5b25a0e700c887356dd584b2de5c24f226096868
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-08-16 23:56:03 +0000

    Util stuff to generate soap faults.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@7683 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/common/util/SoapFaultUtils.java
@@ -0,0 +1,55 @@
+package com.l7tech.common.util;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+import com.l7tech.message.SoapFaultDetail;
+
+import java.io.IOException;
+
+/**
+ * Util stuff to generate soap faults.
+ * <p/>
+ * <br/><br/>
+ * LAYER 7 TECHNOLOGIES, INC<br/>
+ * User: flascell<br/>
+ * Date: Aug 16, 2004<br/>
+ * $Id$
+ */
+public class SoapFaultUtils {
+    public static Document generateSoapFault(SoapFaultDetail soapFaultDetail, String faultActor) throws IOException, SAXException {
+        return generateSoapFault(soapFaultDetail.getFaultCode(),
+                                 soapFaultDetail.getFaultString(),
+                                 soapFaultDetail.getFaultDetails(),
+                                 faultActor);
+    }
+
+    public static Document generateSoapFault(String faultCode,
+                                             String faultString,
+                                             String faultDetails,
+                                             String faultActor) throws IOException, SAXException {
+        String rawXml = generateRawSoapFault(faultCode, faultString, faultDetails, faultActor);
+        return XmlUtil.stringToDocument(rawXml);
+    }
+    
+    public static String generateRawSoapFault(String faultCode,
+                                              String faultString,
+                                              String faultDetails,
+                                              String faultActor) {
+        if (faultActor == null) {
+            faultActor = "";
+        }
+        return  "<soapenv:Envelope" +
+                " xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\"" +
+                " xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"" +
+                " xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\n" +
+                " <soapenv:Body>\n" +
+                "  <soapenv:Fault>\n" +
+                "   <faultcode>" + faultCode + "</faultcode>\n" +
+                "   <faultstring>" + faultString + "</faultstring>\n" +
+                "   <faultactor>" + faultActor + "</faultactor>\n" +
+                "   <detail>" + faultDetails + "</detail>\n" +
+                "  </soapenv:Fault>\n" +
+                " </soapenv:Body>\n" +
+                "</soapenv:Envelope>";
+    }
+}
