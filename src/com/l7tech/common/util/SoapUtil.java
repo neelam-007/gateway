8754bfb36cfded50635b2d344ae896b986b5f0b3
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-08-19 21:44:21 +0000

    Fixed VALUETYPE_SECURECONV
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@7739 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/common/util/SoapUtil.java
+++ src/com/l7tech/common/util/SoapUtil.java
@@ -124,7 +124,7 @@ public class SoapUtil {
     public static final String VALUETYPE_SKI = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-x509-token-profile-1.0#X509SubjectKeyIdentifier";
     public static final String VALUETYPE_X509 = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-x509-token-profile-1.0#X509v3";
     public static final String VALUETYPE_DERIVEDKEY = "http://schemas.xmlsoap.org/ws/2004/04/security/sc/dk";
-    public static final String VALUETYPE_SECURECONV = "http://schemas.xmlsoap.org/ws/2004/04/sc";
+    public static final String VALUETYPE_SECURECONV = "http://schemas.xmlsoap.org/ws/2004/04/security/sc/sct";
     public static final String ENCODINGTYPE_BASE64BINARY = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary";
     public static final String ALGORITHM_PSHA = "http://schemas.xmlsoap.org/ws/2004/04/security/sc/dk/p_sha1";
 
