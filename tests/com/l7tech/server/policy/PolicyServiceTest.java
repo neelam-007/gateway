1f95920e2975a8bfb77b3c174135cccc51ed7642
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-08-24 21:58:31 +0000

    added more complex test
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@7813 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- tests/com/l7tech/server/policy/PolicyServiceTest.java
+++ tests/com/l7tech/server/policy/PolicyServiceTest.java
@@ -12,9 +12,12 @@ import com.l7tech.message.SoapRequest;
 import com.l7tech.message.SoapResponse;
 import com.l7tech.message.TestSoapRequest;
 import com.l7tech.message.TestSoapResponse;
-import com.l7tech.policy.assertion.composite.AllAssertion;
 import com.l7tech.policy.assertion.Assertion;
 import com.l7tech.policy.assertion.TrueAssertion;
+import com.l7tech.policy.assertion.identity.SpecificUser;
+import com.l7tech.policy.assertion.credential.http.HttpBasic;
+import com.l7tech.policy.assertion.composite.AllAssertion;
+import com.l7tech.policy.assertion.composite.OneOrMoreAssertion;
 import com.l7tech.proxy.util.PolicyServiceClient;
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -41,7 +44,7 @@ public class PolicyServiceTest extends TestCase {
         junit.textui.TestRunner.run(suite());
     }
 
-    public void testPolicyService() throws Exception {
+    private void testPolicy(final Assertion policyToTest) throws Exception {
         Document request = PolicyServiceClient.createGetPolicyRequest("123",
                                                                       TestDocuments.getEttkClientCertificate(),
                                                                       TestDocuments.getEttkClientPrivateKey());
@@ -54,13 +57,25 @@ public class PolicyServiceTest extends TestCase {
                                              TestDocuments.getDotNetServerCertificate());
         PolicyService.PolicyGetter policyGetter = new PolicyService.PolicyGetter() {
             public Assertion getPolicy(String serviceId) {
-                return new TrueAssertion();
+                return policyToTest;
             }
         };
 
         ps.respondToPolicyDownloadRequest(soapReq, soapRes, policyGetter);
         Document response = soapRes.getDocument();
         log.info("Response (pretty-printed:" + XmlUtil.nodeToFormattedString(response));
+    }
 
+    public void testSimplePolicyService() throws Exception {
+        testPolicy(new TrueAssertion());
+    }
+
+    public void testWithIdentities() throws Exception {
+        AllAssertion root = new AllAssertion();
+        root.getChildren().add(new HttpBasic());
+        OneOrMoreAssertion or = new OneOrMoreAssertion();
+        root.getChildren().add(or);
+        or.getChildren().add(new SpecificUser(-2, "franco", "666", "franco"));
+        testPolicy(root);
     }
 }
