17cd36c69d281722973531a1044951ec3aae003d
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-08-24 18:23:56 +0000

    added another entry point for non-soap requests
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@7787 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/server/policy/PolicyService.java
+++ src/com/l7tech/server/policy/PolicyService.java
@@ -4,19 +4,36 @@ import com.l7tech.policy.assertion.composite.AllAssertion;
 import com.l7tech.policy.assertion.composite.OneOrMoreAssertion;
 import com.l7tech.policy.assertion.composite.CompositeAssertion;
 import com.l7tech.policy.assertion.Assertion;
+import com.l7tech.policy.assertion.PolicyAssertionException;
+import com.l7tech.policy.assertion.AssertionStatus;
 import com.l7tech.policy.assertion.credential.CredentialSourceAssertion;
 import com.l7tech.policy.assertion.identity.IdentityAssertion;
+import com.l7tech.message.SoapRequest;
+import com.l7tech.message.SoapResponse;
+import com.l7tech.common.security.xml.WssProcessor;
+import com.l7tech.common.security.xml.WssProcessorImpl;
+import com.l7tech.common.security.xml.ProcessorException;
+import com.l7tech.common.xml.InvalidDocumentFormatException;
+import com.l7tech.server.secureconversation.SecureConversationContextManager;
+import com.l7tech.server.policy.assertion.ServerAssertion;
+import com.l7tech.identity.User;
 import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Iterator;
+import java.security.PrivateKey;
+import java.security.GeneralSecurityException;
+import java.security.cert.X509Certificate;
+import java.io.IOException;
 
 /**
  * This is the service that lets a client download a policy for consuming a web service.
  * Whether or not a requestor is allowed to download a policy depends on whether or not he is capable
  * of consuming it. This is determined by looking at the identity assertions inside the target policy
  * comparing them with the identity resuling from the authentication of the policy download request.
+ * Document format expected is <sp:Body><PolicyDiscoveryRequest serviceOid="132"/></sp:Body>
  *
  * <p/>
  * <br/><br/>
@@ -27,24 +44,111 @@ import java.util.Iterator;
  */
 public class PolicyService {
 
-    protected PolicyService() {
+    interface PolicyGetter {
+        AllAssertion getPolicy(String serviceId);
+    }
+
+    public PolicyService(PrivateKey privateServerKey, X509Certificate serverCert) {
         // populate all possible credentials sources
         allCredentialAssertions = new ArrayList();
         for (int i = 0; i < CredentialSourceAssertion.ALL_CREDENTIAL_ASSERTIONS_TYPES.length; i++) {
             CredentialSourceAssertion assertion = CredentialSourceAssertion.ALL_CREDENTIAL_ASSERTIONS_TYPES[i];
             allCredentialAssertions.add(assertion);
         }
+        if (privateServerKey == null || serverCert == null) {
+            throw new IllegalArgumentException("Server key and server cert must be provided to create a TokenService");
+        }
+        this.privateServerKey = privateServerKey;
+        this.serverCert = serverCert;
     }
 
-    public Document respondToPolicyDownloadRequest(Document request) {
-        // todo, all of below
-        // inspect document
-        // see which policy is requested
-        // get the policy
-        // make policy-policy
-        // execute it
-        // construct the response
+    /**
+     * @return the filtered policy
+     */
+    public Document respondToPolicyDownloadRequest(String policyId,
+                                                   User preAuthenticatedUser,
+                                                   PolicyGetter policyGetter) {
+        // todo
         return null;
+    }
+
+
+    /**
+     * The filtered policy is contained in the soapResponse
+     */
+    public void respondToPolicyDownloadRequest(SoapRequest request,
+                                               SoapResponse response,
+                                               PolicyGetter policyGetter) {
+        // We need a Document
+        Document requestDoc = null;
+        try {
+            requestDoc = request.getDocument();
+        } catch (SAXException e) {
+            throw new IllegalArgumentException("Request must contain an xml document");
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Request must contain an xml document");
+        }
+
+        // Process request for message level security stuff
+        WssProcessor.ProcessorResult wssOutput = null;
+        try {
+            wssOutput = processMessageLevelSecurity(requestDoc);
+        } catch (ProcessorException e) {
+            exceptionToFault(e, response);
+            return;
+        } catch (InvalidDocumentFormatException e) {
+            exceptionToFault(e, response);
+            return;
+        } catch (GeneralSecurityException e) {
+            exceptionToFault(e, response);
+            return;
+        } catch (WssProcessor.BadContextException e) {
+            exceptionToFault(e, response);
+            return;
+        }
+        if (wssOutput.getUndecoratedMessage() != null) {
+            request.setDocument(wssOutput.getUndecoratedMessage());
+        }
+        request.setWssProcessorOutput(wssOutput);
+
+        // Which policy is requested?
+        String policyId = getRequestedPolicyId(wssOutput.getUndecoratedMessage());
+
+        // Run the policy-policy
+        AllAssertion targetPolicy = policyGetter.getPolicy(policyId);
+        ServerAssertion policyPolicy = constructPolicyPolicy(targetPolicy);
+        AssertionStatus status = null;
+        try {
+            status = policyPolicy.checkRequest(request, response);
+        } catch (IOException e) {
+            exceptionToFault(e, response);
+        } catch (PolicyAssertionException e) {
+            exceptionToFault(e, response);
+        }
+
+        // todo
+        
+        return;
+    }
+
+    private String getRequestedPolicyId(Document requestDoc) {
+        // todo
+        return null;
+    }
+
+    private SoapResponse exceptionToFault(Exception e, SoapResponse response) {
+        // todo
+        return null;
+    }
+
+    private WssProcessor.ProcessorResult processMessageLevelSecurity(Document request)
+                                            throws ProcessorException, InvalidDocumentFormatException,
+                                                   GeneralSecurityException, WssProcessor.BadContextException {
+        WssProcessor trogdor = new WssProcessorImpl();
+        return trogdor.undecorateMessage(request,
+                                         serverCert,
+                                         privateServerKey,
+                                         SecureConversationContextManager.getInstance());
     }
 
     /**
@@ -52,13 +156,13 @@ public class PolicyService {
      * @param targetPolicy the policy targeted by a requestor.
      * @return the policy that should be validated by the policy download request for the passed target
      */
-    AllAssertion constructPolicyPolicy(AllAssertion targetPolicy) {
+    ServerAssertion constructPolicyPolicy(AllAssertion targetPolicy) {
         AllAssertion base = new AllAssertion();
         base.getChildren().add(new OneOrMoreAssertion(allCredentialAssertions));
         List allTargetIdentities = new ArrayList();
         addIdAssertionToList(targetPolicy, allTargetIdentities);
         base.getChildren().add(new OneOrMoreAssertion(allTargetIdentities));
-        return base;
+        return ServerPolicyFactory.getInstance().makeServerPolicy(base);
     }
 
     private void addIdAssertionToList(CompositeAssertion composite, List receptacle) {
@@ -73,4 +177,6 @@ public class PolicyService {
     }
 
     private final List allCredentialAssertions;
+    private PrivateKey privateServerKey = null;
+    private X509Certificate serverCert = null;
 }
