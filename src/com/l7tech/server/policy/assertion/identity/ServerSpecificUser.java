452ea1c7aeb38e282a29638a69e6144e2c36b554
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-08-30 19:03:50 +0000

    Renamed 'specified' => 'required'
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@7903 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/server/policy/assertion/identity/ServerSpecificUser.java
+++ src/com/l7tech/server/policy/assertion/identity/ServerSpecificUser.java
@@ -30,21 +30,23 @@ public class ServerSpecificUser extends ServerIdentityAssertion implements Serve
      * @return <code>AssertionStatus.NONE</code> if the <code>User</code> matches.
      */
     public AssertionStatus checkUser(User requestingUser) {
-        String specifiedLogin = specificUser.getUserLogin();
-        String specifiedUid = specificUser.getUserUid();
-        if (specificUser == null || specifiedLogin == null || specifiedUid == null ) {
+        String requiredLogin = specificUser.getUserLogin();
+        String requiredUid = specificUser.getUserUid();
+        long requiredProvider = specificUser.getIdentityProviderOid();
+
+        if (specificUser == null || requiredLogin == null || requiredUid == null ) {
             String msg = "null assertion or SpecificUser has null login and uid.";
             logger.warning(msg);
             return AssertionStatus.SERVER_ERROR;
         }
 
         // check provider id and user login (start with provider as it's cheaper)
-        if (requestingUser.getProviderId() == specificUser.getIdentityProviderOid()) {
+        if (requestingUser.getProviderId() == requiredProvider) {
             String requestingUserUid = requestingUser.getUniqueIdentifier();
             // Check uid first if present
-            if ( specifiedUid == null || specifiedUid.equals(requestingUserUid) ) {
+            if ( requiredUid == null || requiredUid.equals(requestingUserUid) ) {
                 // They can't both be null (already checked) so this is safe
-                if ( specifiedLogin == null || specifiedLogin.equals(requestingUser.getLogin()) ) {
+                if ( requiredLogin == null || requiredLogin.equals(requestingUser.getLogin()) ) {
                     return AssertionStatus.NONE;
                 }
             }
