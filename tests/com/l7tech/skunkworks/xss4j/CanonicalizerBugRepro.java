85b17e45fd2a20a6f1fbf6fcf8fe5559c403eb00
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-08-31 17:49:01 +0000

    Replaced with a standalone repro that uses no external code other than xss4j.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@7922 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- tests/com/l7tech/skunkworks/xss4j/CanonicalizerBugRepro.java
+++ tests/com/l7tech/skunkworks/xss4j/CanonicalizerBugRepro.java
@@ -11,15 +11,12 @@ import com.ibm.xml.dsig.transform.ExclusiveC11r;
 import com.ibm.xml.dsig.transform.ExclusiveC11rWC;
 import com.ibm.xml.dsig.transform.W3CCanonicalizer;
 import com.ibm.xml.dsig.transform.W3CCanonicalizer2WC;
-import com.l7tech.common.util.SoapUtil;
-import com.l7tech.common.xml.TestDocuments;
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
-import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import java.io.ByteArrayInputStream;
 
 /**
  * Test case demonstrating that both xs4j "exclusive" canonicalizers fail for unknown reasons
@@ -27,52 +24,69 @@ import java.util.logging.Logger;
  * Some canonicalizer (ExclusiveC11r and ExclusiveC11rWC) throw an exception when canonicalize
  * certain elements.
  */
-public class CanonicalizerBugRepro extends TestCase {
-    private static final Logger log = Logger.getLogger(CanonicalizerBugRepro.class.getName());
-
-    public CanonicalizerBugRepro(String name) {
-        super(name);
-    }
-
-    public static Test suite() {
-        TestSuite suite = new TestSuite(CanonicalizerBugRepro.class);
-        return suite;
-    }
+public class CanonicalizerBugRepro {
+    private static final String SOAP_NS = "http://schemas.xmlsoap.org/soap/envelope/";
+    private static final String APP_NS = "http://warehouse.acme.com/ws";
+    public static final String TEST_DOC = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+            "<soapenv:Envelope\n" +
+            "    xmlns:soapenv=\"" + SOAP_NS + "\"\n" +
+            "    xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\n" +
+            "    <soapenv:Body>\n" +
+            "        <ns1:placeOrder\n" +
+            "            soapenv:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\" xmlns:ns1=\"" + APP_NS + "\">\n" +
+            "            <productid xsi:type=\"xsd:long\">-9206260647417300294</productid>\n" +
+            "            <amount xsi:type=\"xsd:long\">1</amount>\n" +
+            "            <price xsi:type=\"xsd:float\">5.0</price>\n" +
+            "            <accountid xsi:type=\"xsd:long\">228</accountid>\n" +
+            "        </ns1:placeOrder>\n" +
+            "    </soapenv:Body>\n" +
+            "</soapenv:Envelope>";
 
     public static void main(String[] args) {
-        junit.textui.TestRunner.run(suite());
+        Canonicalizer[] c = new Canonicalizer[] {
+            new W3CCanonicalizer(),
+            new W3CCanonicalizer2WC(),
+            new ExclusiveC11r(),
+            new ExclusiveC11rWC(),
+        };
+
+        for (int i = 0; i < c.length; i++) {
+            Canonicalizer canonicalizer = c[i];
+            try {
+                System.out.println("\n*** Testing: " + canonicalizer.getClass() + "\n");
+                useCanon(canonicalizer);
+                System.out.println("\n\n*** Test succeeded: " + canonicalizer.getClass() + "\n");
+            } catch (Exception e) {
+                System.out.println("\n");
+                e.printStackTrace(System.out);
+                System.out.println("\n*** Test failed: " + canonicalizer.getClass() + "\n");
+            }
+        }
     }
 
-    public void testExclusiveCanonicalizer() throws Exception {
-        useCanon(new ExclusiveC11r());
-    }
-
-    public void testExclusiveWithCommentsCanonicalizer() throws Exception {
-        useCanon(new ExclusiveC11rWC());
-    }
-
-    public void testW3CCanonicalizer() throws Exception {
-        useCanon(new W3CCanonicalizer());
-    }
-
-    public void testW3CCanonicalizer2WC() throws Exception {
-        useCanon(new W3CCanonicalizer2WC());
-    }
-
-    private void useCanon(Canonicalizer canon) throws Exception {
-        Document message = TestDocuments.getTestDocument(TestDocuments.PLACEORDER_CLEARTEXT);
-        Element body = SoapUtil.getBodyElement(message);
-        Element placeOrder = (Element)message.getElementsByTagNameNS("http://warehouse.acme.com/ws", "placeOrder").item(0);
+    private static void useCanon(Canonicalizer canon) throws Exception {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        dbf.setNamespaceAware(true);
+        DocumentBuilder builder = dbf.newDocumentBuilder();
+        ByteArrayInputStream bais = new ByteArrayInputStream(TEST_DOC.getBytes());
+        Document message = builder.parse(bais);
+        assertNotNull(message);
+        Element body = (Element)message.getElementsByTagNameNS(SOAP_NS, "Body").item(0);
+        assertNotNull(body);
+        Element placeOrder = (Element)message.getElementsByTagNameNS(APP_NS, "placeOrder").item(0);
+        assertNotNull(placeOrder);
         Element productid = (Element)message.getElementsByTagName("productid").item(0);
+        assertNotNull(productid);
 
         canon.canonicalize(message, System.out);
-        assertTrue(body != null);
         canon.canonicalize(body, System.out);
-        assertTrue(placeOrder != null);
         canon.canonicalize(placeOrder, System.out);
-        assertTrue(productid != null);
-        canon.canonicalize(productid, System.out);
+        canon.canonicalize(productid, System.out);  // NPE here if using exclusive c11r
 
         System.out.println("Node serialized ok.");
+    }
+
+    private static void assertNotNull(Object o) {
+        if (o == null) throw new NullPointerException("assertion failed");
     }
 }
