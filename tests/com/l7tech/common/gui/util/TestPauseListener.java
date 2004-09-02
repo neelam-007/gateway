ca4d00b72761c5f0a578d75a0e5c467b3bb18363
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-09-02 00:48:19 +0000

    Now validates URLs!
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@7973 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- tests/com/l7tech/common/gui/util/TestPauseListener.java
+++ tests/com/l7tech/common/gui/util/TestPauseListener.java
@@ -9,6 +9,9 @@ package com.l7tech.common.gui.util;
 import javax.swing.*;
 import javax.swing.text.JTextComponent;
 import java.awt.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 /**
  * @author alex
@@ -26,6 +29,7 @@ public class TestPauseListener extends JFrame implements PauseListener {
         cp.setLayout(new GridBagLayout());
         cp.add( _textField );
         cp.add( _label );
+        cp.add(statusLabel,new GridBagConstraints(0,1,2,1,0,0,GridBagConstraints.NORTH,0,new Insets(0,0,0,0),0,0));
         pack();
         show();
     }
@@ -36,12 +40,32 @@ public class TestPauseListener extends JFrame implements PauseListener {
 
     public void textEntryPaused( JTextComponent component, long msecs ) {
         _label.setText( "paused" );
+        final String value = component.getText();
+        if (value != null && value.length() > 0 && !value.equals(oldvalue)) {
+            InputStream is = null;
+            try {
+                URL url = new URL(value);
+                is = url.openStream();
+                statusLabel.setText("OK");
+            } catch ( IOException e ) {
+                statusLabel.setText(e.toString());
+            } finally {
+                try {
+                    if (is != null) is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        oldvalue = value;
     }
 
     public void textEntryResumed(JTextComponent component) {
         _label.setText( "resumed" );
     }
 
+    private String oldvalue = null;
     private JTextField _textField = new JTextField(80);
     private JLabel _label = new JLabel("initial value");
+    private JLabel statusLabel = new JLabel(" ");
 }
