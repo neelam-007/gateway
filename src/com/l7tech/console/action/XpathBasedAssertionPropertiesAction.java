71315149f91fb4225ef3816ab22bdf97e1418f8b
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-09-15 18:17:43 +0000

    Patched xpath editor to support non-soap (for non-wss xpath assertions).
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@8163 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/console/action/XpathBasedAssertionPropertiesAction.java
+++ src/com/l7tech/console/action/XpathBasedAssertionPropertiesAction.java
@@ -1,29 +1,45 @@
 package com.l7tech.console.action;
 
 import com.l7tech.common.gui.util.Utilities;
+import com.l7tech.common.xml.XpathExpression;
+import com.l7tech.common.xml.XpathEvaluator;
+import com.l7tech.common.util.XmlUtil;
 import com.l7tech.console.MainWindow;
 import com.l7tech.console.panels.XpathBasedAssertionPropertiesDialog;
-import com.l7tech.console.tree.policy.AssertionTreeNode;
-import com.l7tech.console.tree.policy.PolicyTreeModel;
-import com.l7tech.console.tree.policy.XpathBasedAssertionTreeNode;
+import com.l7tech.console.tree.policy.*;
 import com.l7tech.console.util.TopComponents;
 import com.l7tech.objectmodel.FindException;
+import com.l7tech.policy.assertion.XpathBasedAssertion;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.HashMap;
 import java.rmi.RemoteException;
+import java.io.IOException;
+
+import org.jaxen.JaxenException;
+import org.xml.sax.SAXException;
 
 /**
  * Action for editing XML security assertion properties
  */
 public class XpathBasedAssertionPropertiesAction extends NodeAction {
     static final Logger log = Logger.getLogger(XpathBasedAssertionPropertiesAction.class.getName());
+    private XpathEvaluator testEvaluator;
 
     public XpathBasedAssertionPropertiesAction(XpathBasedAssertionTreeNode node) {
         super(node);
+        try {
+            testEvaluator = XpathEvaluator.newEvaluator(XmlUtil.stringToDocument("<blah xmlns=\"http://bzzt.com\"/>"),
+                                                        new HashMap());
+        } catch (IOException e) {
+            logger.log(Level.WARNING, "cannot create evaluator", e); // cannot happen
+        } catch (SAXException e) {
+            logger.log(Level.WARNING, "cannot create evaluator", e); // cannot happen
+        }
     }
 
     /**
@@ -64,8 +80,17 @@ public class XpathBasedAssertionPropertiesAction extends NodeAction {
                 Utilities.centerOnScreen(dialog);
                 dialog.show();
             } else {
-                JOptionPane.showMessageDialog(mw, "Cannot edit this assertion because it is not configurable " +
-                                                  "on non-soap services.");
+                if (n instanceof RequestXpathPolicyTreeNode || n instanceof ResponseXpathPolicyTreeNode) {
+                    XpathBasedAssertion xmlSecAssertion = (XpathBasedAssertion)node.asAssertion();
+                    String res = JOptionPane.showInputDialog("Please provide xpath value.",
+                                                             xmlSecAssertion.getXpathExpression().getExpression());
+                    if (isValidValue(res)) {
+                        xmlSecAssertion.setXpathExpression(new XpathExpression(res));
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(mw, "Cannot edit this assertion because it is not configurable " +
+                                                      "on non-soap services.");
+                }
             }
         } catch (FindException e) {
             logger.log(Level.WARNING, "cannot get associated service", e);
@@ -90,4 +115,20 @@ public class XpathBasedAssertionPropertiesAction extends NodeAction {
             });
         }
     };
+
+    private boolean isValidValue(String xpath) {
+        if (xpath == null) return false;
+        xpath = xpath.trim();
+        if (xpath.length() < 1) return false;
+         try {
+            testEvaluator.evaluate(xpath);
+        } catch (JaxenException e) {
+            log.fine(e.getMessage());
+            return false;
+        } catch (NullPointerException e) {
+            log.fine(e.getMessage());
+            return false;
+        }
+        return true;
+    }
 }
