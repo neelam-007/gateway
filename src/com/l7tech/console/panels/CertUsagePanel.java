ba53810b64c5861f7a91b73cc646ee5474e2bdea
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-09-21 18:25:09 +0000

    Fixed description refresh bug introduced by [Bugzilla #1117].
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@8276 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/console/panels/CertUsagePanel.java
+++ src/com/l7tech/console/panels/CertUsagePanel.java
@@ -1,23 +1,15 @@
 package com.l7tech.console.panels;
 
 import com.l7tech.common.security.TrustedCert;
-import com.l7tech.common.util.CertUtils;
-import com.intellij.uiDesigner.core.GridLayoutManager;
-import com.intellij.uiDesigner.core.GridConstraints;
-import com.intellij.uiDesigner.core.Spacer;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.FocusAdapter;
-import java.awt.event.FocusEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.Locale;
 import java.util.ResourceBundle;
 import java.util.logging.Logger;
-import java.security.cert.X509Certificate;
-import java.security.cert.CertificateException;
-import java.io.IOException;
 
 /**
  * <p> Copyright (C) 2004 Layer 7 Technologies Inc.</p>
@@ -42,56 +34,6 @@ public class CertUsagePanel extends WizardStepPanel {
     private void initialize() {
         setLayout(new BorderLayout());
         add(mainPanel);
-
-        signingClientCertCheckBox.addFocusListener(new FocusAdapter() {
-            public void focusGained( FocusEvent e ) { setClientDesc(); }
-        });
-        signingClientCertCheckBox.addMouseListener(new MouseAdapter() {
-            public void mouseEntered( MouseEvent e ) { setClientDesc(); }
-        });
-
-        signingServerCertCheckBox.addFocusListener(new FocusAdapter() {
-            public void focusGained( FocusEvent e ) { setServerDesc(); }
-        });
-        signingServerCertCheckBox.addMouseListener(new MouseAdapter() {
-            public void mouseEntered( MouseEvent e ) { setServerDesc(); }
-        });
-
-        signingSAMLTokenCheckBox.addFocusListener(new FocusAdapter() {
-            public void focusGained( FocusEvent e ) { setSamlDesc(); }
-        });
-        signingSAMLTokenCheckBox.addMouseListener(new MouseAdapter() {
-            public void mouseEntered( MouseEvent e ) { setSamlDesc(); }
-        });
-
-        outboundSSLConnCheckBox.addFocusListener(new FocusAdapter() {
-            public void focusGained( FocusEvent e ) { setSslDesc(); }
-        });
-        outboundSSLConnCheckBox.addMouseListener(new MouseAdapter() {
-            public void mouseEntered( MouseEvent e ) { setSslDesc(); }
-        });
-    }
-
-    private String currentDescription = DEFAULT_DESC;
-
-    private void setSslDesc() {
-        currentDescription = resources.getString("usage.desc.ssl");
-        notifyListeners();
-    }
-
-    private void setSamlDesc() {
-        currentDescription = resources.getString("usage.desc.saml");
-        notifyListeners();
-    }
-
-    private void setServerDesc() {
-        currentDescription = resources.getString("usage.desc.server");
-        notifyListeners();
-    }
-
-    private void setClientDesc() {
-        currentDescription = resources.getString("usage.desc.client");
-        notifyListeners();
     }
 
     /**
@@ -142,16 +84,6 @@ public class CertUsagePanel extends WizardStepPanel {
                                            resources.getString("save.error.title"),
                                            JOptionPane.ERROR_MESSAGE);
         }
-
-        // diasble the cert options that are not allowed based on the key usage specified in the cert
-/*        boolean [] keyUsageArray = cert.getKeyUsage();
-
-
-        if(keyUsageArray != null && !keyUsageArray[CertUtils.KeyUsage.keyCertSign]) {
-            signingServerCertCheckBox.setEnabled(false);
-            signingSAMLTokenCheckBox.setEnabled(false);
-            signingClientCertCheckBox.setEnabled(false);
-        }*/
     }
 
     /**
@@ -167,26 +99,25 @@ public class CertUsagePanel extends WizardStepPanel {
      * @return  String  The descritpion of the step.
      */
     public String getDescription() {
-        //return currentDescription;
         return "Select one or more certificate usage options:\n" +
                 "\n" +
-                "Outbound SSL Connections: Using HTTPS, the SecureSpan Gateway can connect to \n" +
+                "<p>Outbound SSL Connections: Using HTTPS, the SecureSpan Gateway can connect to \n" +
                 "protected Web services hosted on SSL servers that use this certificate. If a \n" +
                 "Web service's SSL uses a self-signed certificate, then that certificate must \n" +
                 "be imported with this option.\n" +
                 "\n" +
-                "Signing Certificates for Outbound SSL Connections: Using HTTPS, the \n" +
+                "<p>Signing Certificates for Outbound SSL Connections: Using HTTPS, the \n" +
                 "SecureSpan Gateway can connect to protected Web services hosted on SSL servers \n" +
                 "whose certificates were signed by this certificate authority. If several \n" +
                 "protected Web services' SSL server certificates are signed using an in-house \n" +
                 "certificate authority, then the SecureSpan Gateway can connect to all of them \n" +
                 "if the certificate authority's certificate is imported with this option.\n" +
                 "\n" +
-                "Signing Client Certificates: A Federated Identity Provider can be configured \n" +
+                "<p>Signing Client Certificates: A Federated Identity Provider can be configured \n" +
                 "to authorize identities whose X.509 certificates were signed by this \n" +
                 "certificate authority certificate.\n" +
                 "\n" +
-                "Signing SAML Tokens: A Federated Identity Provider can be configured to \n" +
+                "<p>Signing SAML Tokens: A Federated Identity Provider can be configured to \n" +
                 "authorize identities whose SAML Tokens were signed by this certificate.";
     }
 
@@ -198,7 +129,4 @@ public class CertUsagePanel extends WizardStepPanel {
      public boolean canFinish() {
         return true;
     }
-
-    private static final String DEFAULT_DESC = "This panel allows you to select the the usage options for the certificate. Any combination of the options in the list is allowed.";
-
 }
