78c36789347d342c0d6975bdb627cfb00a2abbbf
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-10-01 18:37:20 +0000

    Server impl
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@8473 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/server/policy/assertion/ServerAuditAssertion.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2004 Layer 7 Technologies Inc.
+ *
+ * $Id$
+ */
+
+package com.l7tech.server.policy.assertion;
+
+import com.l7tech.policy.assertion.AssertionStatus;
+import com.l7tech.policy.assertion.PolicyAssertionException;
+import com.l7tech.policy.assertion.AuditAssertion;
+import com.l7tech.message.Request;
+import com.l7tech.message.Response;
+
+import java.io.IOException;
+import java.util.logging.Level;
+
+/**
+ * @author mike
+ */
+public class ServerAuditAssertion implements ServerAssertion {
+    private AuditAssertion data;
+    private Level level;
+
+    public ServerAuditAssertion(AuditAssertion data) {
+        this.data = data;
+        this.level = Level.parse(data.getLevel());
+    }
+
+    public AssertionStatus checkRequest(Request request, Response response) throws IOException, PolicyAssertionException {
+        return AssertionStatus.NOT_YET_IMPLEMENTED;
+    }
+}
