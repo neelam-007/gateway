968a0b5db183d84c170a98136a5e8557fecc7504
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-10-05 20:12:01 +0000

    More validation rules [bugzilla #1076].
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@8528 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/server/policy/validator/ServerPolicyValidator.java
+++ src/com/l7tech/server/policy/validator/ServerPolicyValidator.java
@@ -10,6 +10,7 @@ import com.l7tech.policy.assertion.identity.IdentityAssertion;
 import com.l7tech.policy.assertion.identity.SpecificUser;
 import com.l7tech.policy.assertion.identity.MemberOfGroup;
 import com.l7tech.policy.assertion.xmlsec.SamlSecurity;
+import com.l7tech.policy.assertion.xmlsec.RequestWssX509Cert;
 import com.l7tech.server.identity.IdentityProviderFactory;
 import com.l7tech.server.transport.jms.JmsEndpointManager;
 import com.l7tech.identity.IdentityProvider;
@@ -19,8 +20,7 @@ import com.l7tech.objectmodel.FindException;
 import com.l7tech.common.util.Locator;
 import com.l7tech.common.transport.jms.JmsEndpoint;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.logging.Logger;
 import java.util.logging.Level;
 
@@ -31,8 +31,8 @@ import java.util.logging.Level;
  *
  *   1.     for each id assertion, check that the corresponding id exists
  *
- *   2.     for each id assertion that is saml only, make sure that no
- *          credential assertion (other than saml) preceeds the assertion
+ *   2.     for each id assertion that is from a fip. make sure the appropritate
+ *          credential source type is in same path
  *
  *   3.     for each JMS routing assertion, make sure referenced endpoint exists
  *
@@ -55,21 +55,63 @@ public class ServerPolicyValidator extends PolicyValidator {
     private void validateAssertion(Assertion a, PathContext pathContext, PolicyValidatorResult r, AssertionPath ap) {
         if (a instanceof IdentityAssertion) {
             int idStatus = getIdStatus((IdentityAssertion)a);
-            if (idStatus == 0) {
-                r.addError(new PolicyValidatorResult.Error(a, ap, "The corresponding identity cannot be found. " +
-                                                                  "Please remove the assertion from the policy.",
-                                                                  null));
-            } else if (idStatus == 2) {
-                if (pathContext.seenCredCredAssertionOtherThanSaml) {
-                    r.addError(new PolicyValidatorResult.Error(a, ap, "This identity can only authenticate with a SAML " +
-                                                                      "token but another type of credential source is " +
-                                                                      "specified.", null));
-                }
+            switch (idStatus) {
+                case ID_NOT_EXIST:
+                    r.addError(new PolicyValidatorResult.Error(a,
+                                                               ap,
+                                                               "The corresponding identity cannot be found. " +
+                                                               "Please remove the assertion from the policy.",
+                                                               null));
+                    break;
+                case ID_FIP:
+                    for (Iterator iterator = pathContext.credentialSources.iterator(); iterator.hasNext();) {
+                        CredentialSourceAssertion credSrc = (CredentialSourceAssertion) iterator.next();
+                        if (credSrc instanceof SamlSecurity || credSrc instanceof RequestWssX509Cert);
+                        else {
+                            r.addError(new PolicyValidatorResult.Error(a,
+                                                                       ap,
+                                                                       "This identity can only authenticate with " +
+                                                                       "a SAML token or an X509 Binary Security " +
+                                                                       "Token but another type of credential " +
+                                                                       "source is specified.",
+                                                                       null));
+                            break;
+                        }
+                    }
+                    break;
+                case ID_SAMLONLY:
+                    for (Iterator iterator = pathContext.credentialSources.iterator(); iterator.hasNext();) {
+                        CredentialSourceAssertion credSrc = (CredentialSourceAssertion) iterator.next();
+                        if (!(credSrc instanceof SamlSecurity)) {
+                            r.addError(new PolicyValidatorResult.Error(a,
+                                                                       ap,
+                                                                       "This identity can only authenticate with " +
+                                                                       "a SAML token " +
+                                                                       "but another type of credential " +
+                                                                       "source is specified.",
+                                                                       null));
+                            break;
+                        }
+                    }
+                    break;
+                case ID_X509ONLY:
+                    for (Iterator iterator = pathContext.credentialSources.iterator(); iterator.hasNext();) {
+                        CredentialSourceAssertion credSrc = (CredentialSourceAssertion) iterator.next();
+                        if (!(credSrc instanceof RequestWssX509Cert)) {
+                            r.addError(new PolicyValidatorResult.Error(a,
+                                                                       ap,
+                                                                       "This identity can only authenticate with " +
+                                                                       "an X509 Binary Security " +
+                                                                       "Token but another type of credential " +
+                                                                       "source is specified.",
+                                                                       null));
+                            break;
+                        }
+                    }
+                    break;
             }
         } else if (a instanceof CredentialSourceAssertion) {
-            if (!(a instanceof SamlSecurity)) {
-                pathContext.seenCredCredAssertionOtherThanSaml = true;
-            }
+            pathContext.credentialSources.add(a);
         } else if (a instanceof JmsRoutingAssertion) {
             JmsRoutingAssertion jmsass = (JmsRoutingAssertion)a;
             if (jmsass.getEndpointOid() != null) {
@@ -106,16 +148,9 @@ public class ServerPolicyValidator extends PolicyValidator {
             try {
                 // get provider
                 IdentityProvider prov = IdentityProviderFactory.getProvider(identityAssertion.getIdentityProviderOid());
-                // check if it's a fip
-                boolean samlonly = false;
-                if (prov.getConfig().getTypeVal() == IdentityProviderType.FEDERATED.toVal()) {
-                    FederatedIdentityProviderConfig cfg = (FederatedIdentityProviderConfig)prov.getConfig();
-                    if (cfg.isSamlSupported() && !cfg.isX509Supported()) {
-                        samlonly = true;
-                    }
-                }
-                boolean idexists = false;
+
                 // check if user or group exists
+                boolean idexists = false;
                 if (identityAssertion instanceof SpecificUser) {
                     SpecificUser su = (SpecificUser)identityAssertion;
                     if (prov.getUserManager().findByPrimaryKey(su.getUserUid()) != null) {
@@ -129,11 +164,22 @@ public class ServerPolicyValidator extends PolicyValidator {
                 } else {
                     throw new RuntimeException("Type not supported " + identityAssertion.getClass().getName());
                 }
-                int val = -1;
-                if (!idexists) val = ID_NOT_EXIST;
-                else if (samlonly) val = ID_EXIST_BUT_SAMLONLY;
-                else val = ID_EXIST;
-                output = new Integer(val);
+                if (!idexists) {
+                    output = new Integer(ID_NOT_EXIST);
+                } else {
+                    // check for special fip values
+                    int val = ID_EXIST;
+                    if (prov.getConfig().getTypeVal() == IdentityProviderType.FEDERATED.toVal()) {
+                        val = ID_FIP;
+                        FederatedIdentityProviderConfig cfg = (FederatedIdentityProviderConfig)prov.getConfig();
+                        if (cfg.isSamlSupported() && !cfg.isX509Supported()) {
+                            val = ID_SAMLONLY;
+                        } else if (!cfg.isSamlSupported() && cfg.isX509Supported()) {
+                            val = ID_X509ONLY;
+                        }
+                    }
+                    output = new Integer(val);
+                }
                 idAssertionStatus.put(identityAssertion, output);
             } catch (FindException e) {
                 logger.log(Level.WARNING, "problem retrieving identity", e);
@@ -145,21 +191,23 @@ public class ServerPolicyValidator extends PolicyValidator {
     }
 
     private static final int ID_NOT_EXIST = 0; // the corresponding id does not exist
-    private static final int ID_EXIST = 1; // the corresponding id exists and does not belong to saml only fip
-    private static final int ID_EXIST_BUT_SAMLONLY = 2; // the corresponding id exists but belongs to saml only fip
+    private static final int ID_EXIST = 1; // the corresponding id exists and is not fip
+    private static final int ID_FIP = 2; // the corresponding id exists but in a fip provider (saml and wss authen only)
+    private static final int ID_SAMLONLY = 3; // the corresponding id exists but belongs to saml only fip
+    private static final int ID_X509ONLY = 4; // the corresponding id exists but belongs to X509 only fip
 
     // A new validator is instantiated once per policy validation
     // so it's ok to cache these here. This cache will expire at
     // end of each policy validation.
     /**
      * key is IdentityAssertion object
-     * value Integer (ID_NOT_EXIST, ID_EXIST, or ID_EXIST_BUT_SAMLONLY)
+     * value Integer (ID_NOT_EXIST, ID_EXIST, ID_FIP, ID_SAMLONLY, or ID_X509ONLY)
      */
     private Map idAssertionStatus = new HashMap();
 
     private final Logger logger = Logger.getLogger(ServerPolicyValidator.class.getName());
 
     class PathContext {
-        boolean seenCredCredAssertionOtherThanSaml = false;
+        Collection credentialSources = new ArrayList();
     }
 }
