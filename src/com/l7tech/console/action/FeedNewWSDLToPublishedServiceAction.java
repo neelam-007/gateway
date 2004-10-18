a66460163c4908c0e261e1f0d3c7f9741475d81d
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-10-18 23:11:26 +0000

    This action resets the wsdl of an already published web service (for example if the downstream service's wsdl has changed since publication).
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@8722 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/console/action/FeedNewWSDLToPublishedServiceAction.java
@@ -0,0 +1,124 @@
+package com.l7tech.console.action;
+
+import com.l7tech.service.PublishedService;
+import com.l7tech.service.ResolutionParameterTooLongException;
+import com.l7tech.console.tree.ServiceNode;
+import com.l7tech.console.tree.ServicesTree;
+import com.l7tech.console.util.Registry;
+import com.l7tech.console.util.TopComponents;
+import com.l7tech.console.MainWindow;
+import com.l7tech.common.xml.Wsdl;
+import com.l7tech.objectmodel.FindException;
+import com.l7tech.objectmodel.UpdateException;
+import com.l7tech.objectmodel.SaveException;
+import com.l7tech.objectmodel.VersionException;
+
+import javax.swing.*;
+import javax.swing.tree.DefaultTreeModel;
+import javax.wsdl.WSDLException;
+import java.io.StringReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.rmi.RemoteException;
+import java.util.logging.Level;
+
+/**
+ * This action resets the wsdl of an already published web service (for example if the downstream
+ * service's wsdl has changed since publication).
+ * <p/>
+ * <br/><br/>
+ * LAYER 7 TECHNOLOGIES, INC<br/>
+ * <p/>
+ * User: flascell<br/>
+ * Date: Oct 18, 2004<br/>
+ */
+public class FeedNewWSDLToPublishedServiceAction extends NodeAction {
+
+    public FeedNewWSDLToPublishedServiceAction(ServiceNode node) {
+        super(node);
+    }
+
+    public String getName() {
+        return "Reset WSDL";
+    }
+
+    public String getDescription() {
+        return "Reset the WSDL property of this service.";
+    }
+
+    protected String iconResource() {
+        return "com/l7tech/console/resources/Edit16.gif";
+    }
+
+    protected void performAction() {
+        final MainWindow mw = TopComponents.getInstance().getMainWindow();
+        PublishedService svc = null;
+        try {
+            svc = ((ServiceNode)node).getPublishedService();
+        } catch (FindException e) {
+            logger.log(Level.WARNING, "error retrieving service", e);
+            JOptionPane.showMessageDialog(mw, "Cannot retrieve published service.");
+            return;
+        } catch (RemoteException e) {
+            logger.log(Level.WARNING, "error retrieving service", e);
+            JOptionPane.showMessageDialog(mw, "Cannot retrieve published service.");
+            return;
+        }
+        String existingURL = svc.getWsdlUrl();
+        if (existingURL == null) existingURL = "";
+        String response = JOptionPane.showInputDialog(mw, "Enter URL for new WSDL", existingURL);
+        if (response == null) return;
+        String newWSDL = null;
+        try {
+            try {
+                new URL(response);
+            } catch (MalformedURLException e) {
+                logger.log(Level.WARNING, "bad url " + response, e);
+                JOptionPane.showMessageDialog(mw, response + " is not a valid url.");
+                return;
+            }
+            newWSDL = Registry.getDefault().getServiceManager().resolveWsdlTarget(response);
+            if (newWSDL != null && newWSDL.length() > 0) {
+                Wsdl.newInstance(null, new StringReader(newWSDL));
+            }
+        } catch (WSDLException e) {
+            logger.log(Level.WARNING, "invalid wsdl", e);
+            JOptionPane.showMessageDialog(mw, "Invalid WSDL. Consult log for more information.");
+            return;
+        } catch (RemoteException e) {
+            logger.log(Level.WARNING, "cannot resolve wsdl", e);
+            JOptionPane.showMessageDialog(mw, "Error resolving WSDL. Consult log for more information.");
+            return;
+        }
+        try {
+            svc.setWsdlUrl(response);
+            svc.setWsdlXml(newWSDL);
+            Registry.getDefault().getServiceManager().savePublishedService(svc);
+            JTree tree = (JTree)TopComponents.getInstance().getComponent(ServicesTree.NAME);
+            if (tree != null) {
+                DefaultTreeModel model = (DefaultTreeModel)tree.getModel();
+                model.nodeChanged(node);
+            }
+        } catch (MalformedURLException e) {
+            logger.log(Level.WARNING, "invalid url", e);
+            JOptionPane.showMessageDialog(mw, "Invalid URL");
+        } catch (RemoteException e) {
+            logger.log(Level.WARNING, "cannot change wsdl", e);
+            JOptionPane.showMessageDialog(mw, "Error Changing WSDL. Consult log for more information.");
+        } catch (UpdateException e) {
+            logger.log(Level.WARNING, "cannot change wsdl", e);
+            JOptionPane.showMessageDialog(mw, "Error Changing WSDL. Consult log for more information.");
+        } catch (SaveException e) {
+            logger.log(Level.WARNING, "cannot change wsdl", e);
+            JOptionPane.showMessageDialog(mw, "Error Changing WSDL. Consult log for more information.");
+        } catch (VersionException e) {
+            logger.log(Level.WARNING, "version mismatch", e);
+            JOptionPane.showMessageDialog(mw, "The service's version number is no longer valid. Perhaps " +
+                                              "another administrator has changed the service since you loaded it?");
+        } catch (ResolutionParameterTooLongException e) {
+            logger.log(Level.WARNING, "resolution parameter too long", e);
+            JOptionPane.showMessageDialog(mw, "The WSDL contains resolution parameters that are too long. " +
+                                              "Consult log for more information.");
+        }
+    }
+}
