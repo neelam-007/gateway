9e1d011e2e08dd9ba24e04033889803832debafd
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-10-21 18:42:17 +0000

    Usage, error checking
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@8791 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/common/security/ExportKeys.java
+++ src/com/l7tech/common/security/ExportKeys.java
@@ -8,6 +8,7 @@ package com.l7tech.common.security;
 
 import com.l7tech.common.util.HexUtils;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.security.Key;
@@ -16,36 +17,50 @@ import java.security.cert.Certificate;
 import java.util.Enumeration;
 
 /**
+ * Exports a JKS keystore to a PKCS#8 keypair and certificate in separate files, both PEM-encoded
+ *
  * @author alex
  * @version $Revision$
  */
 public class ExportKeys {
     public static void main( String[] args ) throws Exception {
-        String keystorefile = args[0];
-        String storepassword = args[1];
-        String keypassword = args[2];
-        String privout = args[3];
-        String pubout = args[4];
+        if (args.length < 5) {
+            System.err.println("Usage: java " + ExportKeys.class.getName() +
+                               " jksKeystoreFile keystorePassword keyPassword exportKeyFile exportCertFile" );
+            System.exit(1);
+        }
+
+        String keystoreFilename = args[0];
+        String keystorePassword = args[1];
+        String keyPassword = args[2];
+        String exportKeyFilename = args[3];
+        String certFilename = args[4];
+
+        File keystoreFile = new File(keystoreFilename);
+        if (!(keystoreFile.exists() && keystoreFile.isFile() && keystoreFile.canRead())) {
+            System.err.println("Keystore File '" + keystoreFilename + "' does not exist or is not a readable file");
+            System.exit(2);
+        }
 
         KeyStore ks = KeyStore.getInstance("JKS");
-        ks.load( new FileInputStream( keystorefile ), storepassword.toCharArray()) ;
+        ks.load( new FileInputStream( keystoreFile ), keystorePassword.toCharArray()) ;
         Enumeration e = ks.aliases();
         while (e.hasMoreElements()) {
             String alias = (String) e.nextElement();
             System.err.println("alias="+alias);
 
             if ( ks.isKeyEntry(alias) ){
-                Key privkey = ks.getKey( alias, keypassword.toCharArray() );
+                Key privkey = ks.getKey( alias, keyPassword.toCharArray() );
 
                 System.err.println("private key format=" + privkey.getFormat());
                 FileOutputStream fos = null;
                 try {
                     byte encoded[] = privkey.getEncoded();
-                    fos = new FileOutputStream(privout);
+                    fos = new FileOutputStream(exportKeyFilename);
                     fos.write("-----BEGIN PRIVATE KEY-----\n".getBytes());
                     fos.write(HexUtils.encodeBase64(encoded).getBytes());
                     fos.write("\n-----END PRIVATE KEY-----\n".getBytes());
-                    System.err.println( "Wrote " + privout);
+                    System.err.println( "Wrote " + exportKeyFilename);
                 } finally {
                     if ( fos != null ) fos.close();
                 }
@@ -53,11 +68,11 @@ public class ExportKeys {
                 Certificate cert = ks.getCertificate(alias);
                 try {
                     byte encoded[] = cert.getEncoded();
-                    fos = new FileOutputStream(pubout);
+                    fos = new FileOutputStream(certFilename);
                     fos.write("-----BEGIN TRUSTED CERTIFICATE-----\n".getBytes());
                     fos.write(HexUtils.encodeBase64(encoded).getBytes("UTF-8"));
                     fos.write("\n-----END TRUSTED CERTIFICATE-----\n".getBytes());
-                    System.err.println( "Wrote " + pubout);
+                    System.err.println( "Wrote " + certFilename);
                 } finally {
                     if ( fos != null ) fos.close();
                 }
