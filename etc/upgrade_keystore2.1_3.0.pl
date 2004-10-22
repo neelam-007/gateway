4abaa3bcebc86bd4d940045b67e16ee7194ef3fc
jay <jay@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-10-22 17:21:21 +0000

    Upgrade keystore format and the definitions.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@8824 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ etc/upgrade_keystore2.1_3.0.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+use strict;
+#
+# Upgrade Keystores $Id$
+# Take the existing keys, hide the originals, convert to the other format
+# 
+
+my $keyproperties="/ssg/etc/conf/keystore.properties";
+
+# No user serviceable parts after this
+
+my %props=();
+open ( PROPS, $keyproperties);
+while (<PROPS>) {
+	if ( m/(.*)=(.*)/) {
+		$props{$1}=$2;
+	}
+}
+
+fix_keystore("$props{keystoredir}/$props{sslkstorename}",$props{sslkspasswd});
+fix_keystore("$props{keystoredir}/$props{rootcakstorename}",$props{rootcakspasswd});
+
+
+sub fix_keystore($$) {
+	my $keystore = shift;
+	my $keypass = shift;
+	if ( $keystore && -e $keystore) {
+		print "Keystore File: $keystore\n";
+	} else { 
+		print "Keystore $keystore not found\n";
+		return 0;
+	}
+	unlink "/tmp/working.p12";
+	unlink "/tmp/working.ks";
+	`cp $keystore /tmp/working.ks`;
+	`java -classpath /ssg/tomcat/webapps/ROOT/WEB-INF/classes com.l7tech.common.security.CopyKeystore /tmp/working.ks JKS /tmp/working.p12 BCPKCS12 $keypass`;
+
+	`mv $keystore $keystore.old_format`;
+	`cp /tmp/working.p12 $keystore`;
+}
