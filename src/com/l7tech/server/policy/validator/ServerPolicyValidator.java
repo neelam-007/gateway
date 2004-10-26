536af9d0a34e9b994e3f6991f40b6d673e9eaa67
emil <emil@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-10-26 19:41:37 +0000

    Bugzilla 1285 - LDAP identity with HTTP auth produces warning
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@8860 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/server/policy/validator/ServerPolicyValidator.java
+++ src/com/l7tech/server/policy/validator/ServerPolicyValidator.java
@@ -7,6 +7,7 @@ import com.l7tech.policy.assertion.Assertion;
 import com.l7tech.policy.assertion.JmsRoutingAssertion;
 import com.l7tech.policy.assertion.credential.CredentialSourceAssertion;
 import com.l7tech.policy.assertion.credential.http.HttpClientCert;
+import com.l7tech.policy.assertion.credential.http.HttpDigest;
 import com.l7tech.policy.assertion.identity.IdentityAssertion;
 import com.l7tech.policy.assertion.identity.SpecificUser;
 import com.l7tech.policy.assertion.identity.MemberOfGroup;
@@ -29,16 +30,16 @@ import java.util.logging.Level;
 
 /**
  * Performs server side policy validation.
- *
+ * <p/>
  * Rules checked:
- *
- *   1.     for each id assertion, check that the corresponding id exists
- *
- *   2.     for each id assertion that is from a fip. make sure the appropritate
- *          credential source type is in same path
- *
- *   3.     for each JMS routing assertion, make sure referenced endpoint exists
- *
+ * <p/>
+ * 1.     for each id assertion, check that the corresponding id exists
+ * <p/>
+ * 2.     for each id assertion that is from a fip. make sure the appropritate
+ * credential source type is in same path
+ * <p/>
+ * 3.     for each JMS routing assertion, make sure referenced endpoint exists
+ * <p/>
  * <p/>
  * <br/><br/>
  * LAYER 7 TECHNOLOGIES, INC<br/>
@@ -57,59 +58,80 @@ public class ServerPolicyValidator extends PolicyValidator {
 
     private void validateAssertion(Assertion a, PathContext pathContext, PolicyValidatorResult r, AssertionPath ap) {
         if (a instanceof IdentityAssertion) {
-            int idStatus = getIdStatus((IdentityAssertion)a);
+            final IdentityAssertion identityAssertion = (IdentityAssertion)a;
+            int idStatus = getIdentityStatus(identityAssertion);
             switch (idStatus) {
+                case PROVIDER_NOT_EXIST:
+                    r.addError(new PolicyValidatorResult.Error(a,
+                      ap,
+                      "The corresponding identity provider does not exist any more. " +
+                      "Please remove the assertion from the policy.",
+                      null));
+                    break;
                 case ID_NOT_EXIST:
                     r.addError(new PolicyValidatorResult.Error(a,
-                                                               ap,
-                                                               "The corresponding identity cannot be found. " +
-                                                               "Please remove the assertion from the policy.",
-                                                               null));
+                      ap,
+                      "The corresponding identity cannot be found. " +
+                      "Please remove the assertion from the policy.",
+                      null));
                     break;
                 case ID_FIP:
                     for (Iterator iterator = pathContext.credentialSources.iterator(); iterator.hasNext();) {
-                        CredentialSourceAssertion credSrc = (CredentialSourceAssertion) iterator.next();
+                        CredentialSourceAssertion credSrc = (CredentialSourceAssertion)iterator.next();
                         if (credSrc instanceof SamlSecurity || credSrc instanceof RequestWssX509Cert ||
-                            credSrc instanceof SecureConversation || credSrc instanceof HttpClientCert);
+                          credSrc instanceof SecureConversation || credSrc instanceof HttpClientCert)
+                            ;
                         else {
                             r.addError(new PolicyValidatorResult.Error(a,
-                                                                       ap,
-                                                                       "This identity cannot authenticate with the " +
-                                                                       "type of credential " +
-                                                                       "source specified.",
-                                                                       null));
+                              ap,
+                              "This identity cannot authenticate with the " +
+                              "type of credential " +
+                              "source specified.",
+                              null));
                             break;
                         }
                     }
                     break;
                 case ID_SAMLONLY:
                     for (Iterator iterator = pathContext.credentialSources.iterator(); iterator.hasNext();) {
-                        CredentialSourceAssertion credSrc = (CredentialSourceAssertion) iterator.next();
+                        CredentialSourceAssertion credSrc = (CredentialSourceAssertion)iterator.next();
                         if (!(credSrc instanceof SamlSecurity)) {
                             r.addError(new PolicyValidatorResult.Error(a,
-                                                                       ap,
-                                                                       "This identity can only authenticate with " +
-                                                                       "a SAML token " +
-                                                                       "but another type of credential " +
-                                                                       "source is specified.",
-                                                                       null));
+                              ap,
+                              "This identity can only authenticate with " +
+                              "a SAML token " +
+                              "but another type of credential " +
+                              "source is specified.",
+                              null));
                             break;
                         }
                     }
                     break;
                 case ID_X509ONLY:
                     for (Iterator iterator = pathContext.credentialSources.iterator(); iterator.hasNext();) {
-                        CredentialSourceAssertion credSrc = (CredentialSourceAssertion) iterator.next();
+                        CredentialSourceAssertion credSrc = (CredentialSourceAssertion)iterator.next();
                         if (!(credSrc instanceof RequestWssX509Cert || credSrc instanceof SecureConversation ||
-                              credSrc instanceof HttpClientCert)) {
+                          credSrc instanceof HttpClientCert)) {
                             r.addError(new PolicyValidatorResult.Error(a,
-                                                                       ap,
-                                                                       "This identity can only authenticate using " +
-                                                                       "its client cert. " +
-                                                                       "The specified type of credential " +
-                                                                       "source is not supported by that user.",
-                                                                       null));
+                              ap,
+                              "This identity can only authenticate using " +
+                              "its client cert. " +
+                              "The specified type of credential " +
+                              "source is not supported by that user.",
+                              null));
                             break;
+                        }
+                    }
+                    break;
+                case ID_LDAP:
+                    if (identityAssertion instanceof SpecificUser) {
+                        if (pathContext.contains(HttpDigest.class)) {
+                            r.addWarning(new PolicyValidatorResult.Warning(a,
+                              ap,
+                              "This identity may not be able to authenticate with the " +
+                              "type of credential " +
+                              "source specified.",
+                              null));
                         }
                     }
                     break;
@@ -130,10 +152,10 @@ public class ServerPolicyValidator extends PolicyValidator {
                 }
                 if (!jmsEndpointDefinedOk) {
                     r.addError(new PolicyValidatorResult.Error(a,
-                                                               ap,
-                                                               "This routing assertion refers to a JMS " +
-                                                               "endpoint that cannot be found on this system.",
-                                                               null));
+                      ap,
+                      "This routing assertion refers to a JMS " +
+                      "endpoint that cannot be found on this system.",
+                      null));
                 }
             }
         }
@@ -142,18 +164,22 @@ public class ServerPolicyValidator extends PolicyValidator {
     /**
      * This is synchronized just in case, i dont expect this to be invoked from multiple simultaneous threads.
      * Add more return values as needed.
+     * todo: ocnsider refactoring into some sort of status class 'IdentityStatus' instead of returning codes. - em
      *
      * @return see ID_NOT_EXIST, ID_EXIST, or ID_EXIST_BUT_SAMLONLY
      */
-    private synchronized int getIdStatus(IdentityAssertion identityAssertion) {
+    private synchronized int getIdentityStatus(IdentityAssertion identityAssertion) {
         // look in cache first
-        Integer output = (Integer)idAssertionStatus.get(identityAssertion);
+        Integer output = (Integer)idAssertionStatusCache.get(identityAssertion);
         if (output == null) {
             try {
-                // get provider
+// get provider
                 IdentityProvider prov = IdentityProviderFactory.getProvider(identityAssertion.getIdentityProviderOid());
-
-                // check if user or group exists
+                if (prov == null) {
+                    idAssertionStatusCache.put(identityAssertion, new Integer(PROVIDER_NOT_EXIST));
+                    return PROVIDER_NOT_EXIST;
+                }
+// check if user or group exists
                 boolean idexists = false;
                 if (identityAssertion instanceof SpecificUser) {
                     SpecificUser su = (SpecificUser)identityAssertion;
@@ -171,9 +197,9 @@ public class ServerPolicyValidator extends PolicyValidator {
                 if (!idexists) {
                     output = new Integer(ID_NOT_EXIST);
                 } else {
-                    // check for special fip values
+// check for special fip values
                     int val = ID_EXIST;
-                    if (prov.getConfig().getTypeVal() == IdentityProviderType.FEDERATED.toVal()) {
+                    if (IdentityProviderType.is(prov, IdentityProviderType.FEDERATED)) {
                         val = ID_FIP;
                         FederatedIdentityProviderConfig cfg = (FederatedIdentityProviderConfig)prov.getConfig();
                         if (cfg.isSamlSupported() && !cfg.isX509Supported()) {
@@ -181,14 +207,16 @@ public class ServerPolicyValidator extends PolicyValidator {
                         } else if (!cfg.isSamlSupported() && cfg.isX509Supported()) {
                             val = ID_X509ONLY;
                         }
+                    } else if (IdentityProviderType.is(prov, IdentityProviderType.LDAP)) {
+                        val = ID_LDAP;
                     }
                     output = new Integer(val);
                 }
-                idAssertionStatus.put(identityAssertion, output);
+                idAssertionStatusCache.put(identityAssertion, output);
             } catch (FindException e) {
                 logger.log(Level.WARNING, "problem retrieving identity", e);
                 output = new Integer(ID_NOT_EXIST);
-                idAssertionStatus.put(identityAssertion, output);
+                idAssertionStatusCache.put(identityAssertion, output);
             }
         }
         return output.intValue();
@@ -199,19 +227,31 @@ public class ServerPolicyValidator extends PolicyValidator {
     private static final int ID_FIP = 2; // the corresponding id exists but in a fip provider (saml and wss authen only)
     private static final int ID_SAMLONLY = 3; // the corresponding id exists but belongs to saml only fip
     private static final int ID_X509ONLY = 4; // the corresponding id exists but belongs to X509 only fip
+    private static final int ID_LDAP = 5; // the corresponding id exists in a ldap provider
+    private static final int PROVIDER_NOT_EXIST = 6; // the corresponding provider does not exist any more
 
-    // A new validator is instantiated once per policy validation
-    // so it's ok to cache these here. This cache will expire at
-    // end of each policy validation.
+// A new validator is instantiated once per policy validation
+// so it's ok to cache these here. This cache will expire at
+// end of each policy validation.
     /**
      * key is IdentityAssertion object
      * value Integer (ID_NOT_EXIST, ID_EXIST, ID_FIP, ID_SAMLONLY, or ID_X509ONLY)
      */
-    private Map idAssertionStatus = new HashMap();
+    private Map idAssertionStatusCache = new HashMap();
 
     private final Logger logger = Logger.getLogger(ServerPolicyValidator.class.getName());
 
     class PathContext {
         Collection credentialSources = new ArrayList();
+
+        boolean contains(Class clz) {
+            for (Iterator iterator = credentialSources.iterator(); iterator.hasNext();) {
+                Object o = iterator.next();
+                if (o.getClass().equals(clz)) {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
