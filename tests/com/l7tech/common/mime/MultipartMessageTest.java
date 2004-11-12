aff7d6a64549304cfc33445031be28e378a780c7
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-11-12 01:53:35 +0000

    Added failing test with no preamble
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@9066 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- tests/com/l7tech/common/mime/MultipartMessageTest.java
+++ tests/com/l7tech/common/mime/MultipartMessageTest.java
@@ -36,7 +36,7 @@ public class MultipartMessageTest extends TestCase {
     }
 
     public void testSimple() throws Exception {
-        MultipartMessage mm = makeRubyMessage();
+        MultipartMessage mm = makeMessage(MESS, CT);
 
         PartInfo rubyPart = mm.getPart(1);
         InputStream rubyStream = rubyPart.getInputStream(true);
@@ -68,9 +68,43 @@ public class MultipartMessageTest extends TestCase {
         }
     }
 
-    private MultipartMessage makeRubyMessage() throws IOException, NoSuchPartException {
-        InputStream mess = new ByteArrayInputStream(MESS.getBytes());
-        ContentTypeHeader mr = ContentTypeHeader.parseValue("multipart/related; type=\"text/xml\"; boundary=\"----=Part_-763936460.407197826076299\"; start=\"-76394136.15558\"");
+    public void testSimple2() throws Exception {
+        MultipartMessage mm = makeMessage(MESS2, CT2);
+
+        PartInfo rubyPart = mm.getPart(1);
+        InputStream rubyStream = rubyPart.getInputStream(true);
+        ByteArrayOutputStream bo = new ByteArrayOutputStream();
+        HexUtils.copyStream(rubyStream, bo);
+        log.info("Ruby part retrieved " + bo.toByteArray().length + " bytes: \n" + new String(bo.toByteArray()));
+
+        PartInfo soapPart = mm.getPart(0);
+        InputStream soapStream = soapPart.getInputStream(true);
+        bo = new ByteArrayOutputStream();
+        HexUtils.copyStream(soapStream, bo);
+        log.info("Soap part retrieved " + bo.toByteArray().length + " bytes: \n" + new String(bo.toByteArray()));
+
+        // We read them out of order, so this part will have been stashed
+        soapStream = soapPart.getInputStream(false);
+
+        try {
+            rubyStream = rubyPart.getInputStream(false);
+            fail("Ruby part was read destructively, and is last part, but was stashed anyway");
+        } catch (IOException e) {
+            log.info("The Ruby part was consumed successfully: " + e.getMessage());
+        }
+
+        try {
+            PartInfo bogusPart = mm.getPart(2);
+            fail("Failed to get expected exception");
+        } catch (NoSuchPartException e) {
+            log.info("Got proper exception on trying to get a nonexistent MIME part: " + e.getMessage());
+        }
+    }
+
+
+    private MultipartMessage makeMessage(String message, String contentTypeValue) throws IOException, NoSuchPartException {
+        InputStream mess = new ByteArrayInputStream(message.getBytes());
+        ContentTypeHeader mr = ContentTypeHeader.parseValue(contentTypeValue);
         StashManager sm = new ByteArrayStashManager();
         MultipartMessage mm = MultipartMessage.createMultipartMessage(sm, mr, mess);
         return mm;
@@ -81,13 +115,13 @@ public class MultipartMessageTest extends TestCase {
     }
 
     public void testStreamAll() throws Exception {
-        MultipartMessage mm = makeRubyMessage();
+        MultipartMessage mm = makeMessage(MESS, CT);
 
         // TODO
     }
 
     public void testLookupsByCid() throws Exception {
-        MultipartMessage mm = makeRubyMessage();
+        MultipartMessage mm = makeMessage(MESS, CT);
         // TODO
     }
 
@@ -138,4 +172,50 @@ public class MultipartMessageTest extends TestCase {
              RUBY +
             "\r\n" +
             "------=Part_-763936460.407197826076299--";
+
+    public static final String MESS2 = "------=Part_-763936460.00306951464153826\r\n" +
+            "Content-Transfer-Encoding: 8bit\r\n" +
+            "Content-Type: text/xml; charset=utf-8\r\n" +
+            "Content-ID: -76394136.13454\r\n" +
+            "\r\n" +
+            "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n" +
+            "<env:Envelope xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"\n" +
+            "    xmlns:env=\"http://schemas.xmlsoap.org/soap/envelope/\"\n" +
+            "    xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\n" +
+            "  <env:Body>\n" +
+            "    <n1:echoOne xmlns:n1=\"urn:EchoAttachmentsService\"\n" +
+            "        env:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\">\n" +
+            "      <file href=\"cid:-76392836.13454\"></file>\n" +
+            "    </n1:echoOne>\n" +
+            "  </env:Body>\n" +
+            "</env:Envelope>\n" +
+            "\r\n" +
+            "------=Part_-763936460.00306951464153826\r\n" +
+            "Content-Transfer-Encoding: 8bit\r\n" +
+            "Content-Type: application/octet-stream\r\n" +
+            "Content-ID: <-76392836.13454>\r\n" +
+            "\r\n" +
+            "require 'soap/rpc/driver'\n" +
+            "require 'soap/attachment'\n" +
+            "\n" +
+            "attachment = ARGV.shift || __FILE__\n" +
+            "\n" +
+            "#server = 'http://localhost:7700/'\n" +
+            "server = 'http://data.l7tech.com:80/'\n" +
+            "\n" +
+            "driver = SOAP::RPC::Driver.new(server, 'urn:EchoAttachmentsService')\n" +
+            "driver.wiredump_dev = STDERR\n" +
+            "driver.add_method('echoOne', 'file')\n" +
+            "\n" +
+            "File.open(attachment)  do |fin|\n" +
+            "  File.open('attachment.out', 'w') do |fout|\n" +
+            ".fout << driver.echoOne(SOAP::Attachment.new(fin))\n" +
+            "  end      \n" +
+            "end\n" +
+            "\n" +
+            "\n" +
+            "\r\n" +
+            "------=Part_-763936460.00306951464153826--\r\n";
+    public static final String CT = "multipart/related; type=\"text/xml\"; boundary=\"----=Part_-763936460.407197826076299\"; start=\"-76394136.15558\"";
+    public static final String CT2 = "multipart/related; type=\"text/xml\"; boundary=\"----=Part_-763936460.00306951464153826\"; start=\"-76394136.13454\"";
 }
