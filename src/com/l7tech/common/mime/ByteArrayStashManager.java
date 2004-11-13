c31931a8958ed8799141a3b6fecd811af3717e65
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-11-13 01:00:09 +0000

    Fix comment, add way to stash byte array directly
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@9075 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/common/mime/ByteArrayStashManager.java
+++ src/com/l7tech/common/mime/ByteArrayStashManager.java
@@ -18,7 +18,7 @@ import java.util.ArrayList;
  * A StashManager that can only be used for a single request, and which always buffers all InputStreams in memory.
  */
 public class ByteArrayStashManager implements StashManager {
-    private ArrayList stashed = new ArrayList(); // an array of ByteArrayOutputStreams
+    private ArrayList stashed = new ArrayList(); // contains byte arrays
 
     public ByteArrayStashManager() {
     }
@@ -26,9 +26,20 @@ public class ByteArrayStashManager implements StashManager {
     public void stash(int ordinal, InputStream in) throws IOException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream(4096);
         HexUtils.copyStream(in, baos);
+        stash(ordinal, baos.toByteArray());
+    }
+
+    /**
+     * For callers that already possess the entire InputStream as a byte array, this method can be used
+     * to stash the byte array directly without needing to copy it again.
+     *
+     * @param ordinal a small non-negative integer to identify this stream within the stash
+     * @param in      the byte array to stash.  May be zero-length but must be non-null.
+     */
+    public void stash(int ordinal, byte[] in) {
         while (stashed.size() <= ordinal)
             stashed.add(null);
-        stashed.set(ordinal, baos.toByteArray());
+        stashed.set(ordinal, in);
     }
 
     public void unstash(int ordinal) {
