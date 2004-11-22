9cc025031544faf05df4093b6afc8e46207a43b1
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-11-22 23:45:28 +0000

    Javadoc
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@9207 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/common/audit/AuditRecord.java
+++ src/com/l7tech/common/audit/AuditRecord.java
@@ -11,6 +11,11 @@ import com.l7tech.logging.SSGLogRecord;
 import java.util.logging.Level;
 
 /**
+ * Abstract superclass of all of the different types of audit record.
+ *
+ * Note that audit records should be treated as immutable, although they still need non-final fields and setters
+ * for persistence purposes.
+ *
  * @author alex
  * @version $Revision$
  */
@@ -19,16 +24,32 @@ public abstract class AuditRecord extends SSGLogRecord {
     protected AuditRecord() {
     }
 
-    public AuditRecord(Level level, String nodeId, String ipAddress, String name, String message) {
+    /**
+     * Fills in the fields that are common to all types of AuditRecord
+     * @param level the {@link Level} of this record.
+     * @param nodeId the ID of the cluster node from which this AuditRecord originates (see {@link com.l7tech.cluster.ClusterStatusAdmin#getClusterStatus()})
+     * @param ipAddress the IP address of the entity that caused this AuditRecord to be created.  It could be that of a cluster node, an administrative workstation or a web service requestor.
+     * @param name the name of the service or system affected by event that generated the AuditRecord
+     * @param message a short description of the event that generated the AuditRecord
+     */
+    protected AuditRecord(Level level, String nodeId, String ipAddress, String name, String message) {
         super(level, nodeId, message);
         this.name = name;
         this.ipAddress = ipAddress;
     }
 
+    /**
+     * Gets the IP address of the entity that caused this AuditRecord to be created.  It could be that of a cluster node, an administrative workstation or a web service requestor.
+     * @return the IP address of the entity that caused this AuditRecord to be created.
+     */
     public String getIpAddress() {
         return ipAddress;
     }
 
+    /**
+     * Gets the name of the service or system affected by event that generated the AuditRecord
+     * @return the name of the service or system affected by event that generated the AuditRecord
+     */
     public String getName() {
         return name;
     }
