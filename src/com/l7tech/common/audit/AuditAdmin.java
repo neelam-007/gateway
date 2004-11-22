a52467bfbe30f6d5521adb95195997985b3191b8
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-11-22 19:55:22 +0000

    Javadoc
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@9181 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/common/audit/AuditAdmin.java
+++ src/com/l7tech/common/audit/AuditAdmin.java
@@ -16,35 +16,48 @@ import java.util.Collection;
 import java.util.logging.Level;
 
 /**
+ * The API for administering the SecureSpan Gateway's audit system.
+ *
+ * Note that the API does not permit the modification of audit records in any way,
+ * and only permits their deletion using a process that generates a permanent audit record.
+ *
  * @author alex
  * @version $Revision$
  */
 public interface AuditAdmin extends GenericLogAdmin {
     /**
-     * Retrieves the {@link AuditRecord} with the given oid.
+     * Retrieves the {@link AuditRecord} with the given oid, or null if no such record exists.
+     * @param oid the numeric object identifier of the record to retrieve
+     * @return the {@link AuditRecord} with the given oid, or null if no such record exists.
+     * @throws FindException if there was a problem retrieving Audit records from the database
+     * @throws RemoteException if there was a problem communicating with the Gateway
      */
     AuditRecord findByPrimaryKey(long oid) throws FindException, RemoteException;
 
     /**
-     * Retrieves a collection of {@link AuditRecord}s matching the provided criteria.
-     */ 
+     * Retrieves a collection of {@link AuditRecord}s matching the provided criteria.  May be empty, but never null.
+     * @param criteria an {@link AuditSearchCriteria} describing the search criteria.  Must not be null.
+     * @throws FindException if there was a problem retrieving Audit records from the database
+     * @throws RemoteException if there was a problem communicating with the Gateway
+     */
     Collection find(AuditSearchCriteria criteria) throws FindException, RemoteException;
 
     /**
-     * Get the level below which the server will not record audit events of type message.
-     * @return the level currently applicable
-     * @throws RemoteException
+     * Get the level below which the server will not record audit events of type {@link MessageSummaryAuditRecord}.
+     * @return the level currently applicable. Never null.
+     * @throws RemoteException if there was a problem communicating with the Gateway
      */
     Level serverMessageAuditThreshold() throws RemoteException;
 
     /**
-     * Delete all sub-SEVERE AuditRecords that are more than 168 hours old (by default), while producing new audit
-     * record documenting that this action has been performed (and by which administrator).
+     * Delete all sub-SEVERE AuditRecords that are more than 168 hours old (by default), while producing a new audit
+     * record at {@link Level#SEVERE} documenting that this action has been performed (and by which administrator).
+     * @throws RemoteException if there was a problem communicating with the Gateway
      */
     void deleteOldAuditRecords() throws RemoteException;
 
     /**
-     * Create a context for downloading audit IDs.  The context will expire after ten minutes of inactivity.
+     * Create a context for downloading audit records.  The context will expire after ten minutes of inactivity.
      * @param chunkSizeInBytes number of bytes per download chunk.  If zero, default of 8192 will be used.
      * @return a OpaqueId for passing to downloadNextChunk().  never null.
      * @throws RemoteException if there is a problem preparing the download context.
@@ -65,17 +78,17 @@ public interface AuditAdmin extends GenericLogAdmin {
     }
 
     /**
-     * Download the next chunk of audit IDs.
-     * @param context
-     * @return
-     * @throws RemoteException
+     * Download the next chunk of audit records.
+     * @param context the {@link OpaqueId} that was returned by a previous call to {@link #downloadAllAudits(int)}
+     * @return the next {@link DownloadChunk}, or null if the last chunk has already been sent.
+     * @throws RemoteException if there is a problem preparing the chunk.
      */
     DownloadChunk downloadNextChunk(OpaqueId context) throws RemoteException;
 
     /**
      * The minimum age (in hours) that an Audit record must be before it can be purged.
-     * @return the number of hours old that an AuditRecord can be before it can be purged.
-     * @throws RemoteException
+     * @return the minimum number of hours old that an AuditRecord must be before it can be purged.
+     * @throws RemoteException if there was a problem communicating with the Gateway
      */
     int serverMinimumPurgeAge() throws RemoteException;
 }
--- src/com/l7tech/common/audit/AuditSearchCriteria.java
+++ src/com/l7tech/common/audit/AuditSearchCriteria.java
@@ -11,20 +11,28 @@ import java.util.Date;
 import java.util.logging.Level;
 
 /**
+ * A set of criteria that can be used to search for {@link AuditRecord}s using the {@link AuditAdmin} interface.
+ *
  * @author mike
  */
 public final class AuditSearchCriteria implements Serializable {
     /**
-     * @param fromTime the time of the earliest record to retrieve (null = one day ago)
-     * @param toTime the time of the latest record to retrieve (null = now)
-     * @param fromLevel the level of the least severe record to retrieve (null = {@link java.util.logging.Level#INFO})
-     * @param toLevel the level of the most severe record to retrieve (null = {@link java.util.logging.Level#SEVERE})
-     * @param nodeId the ID (usually a MAC address) of the cluster node to search (null = any)
-     * @param startMessageNumber the minimum OID to find
-     * @param endMessageNumber the maximum OID to find
+     * Constructs a new AuditSearchCriteria with all possible search parameters
+     *
+     * @param fromTime the time of the earliest record to retrieve, inclusive (null = one day ago)
+     * @param toTime the time of the latest record to retrieve, inclusive (null = now)
+     * @param fromLevel the level of the least severe record to retrieve, inclusive (null = {@link java.util.logging.Level#INFO})
+     * @param toLevel the level of the most severe record to retrieve, inclusive (null = {@link java.util.logging.Level#SEVERE})
+     * @param recordClass the type of {@link AuditRecord}s to retrieve; must be AuditRecord.class or one of its subclasses (null = AuditRecord.class)
+     * @param nodeId the ID (usually a MAC address) of the cluster node to search (null = any, see {@link com.l7tech.cluster.ClusterStatusAdmin#getClusterStatus()})
+     * @param startMessageNumber the minimum OID to find, inclusive (0 = don't care)
+     * @param endMessageNumber the maximum OID to find, inclusive (0 = don't care)
      * @param maxRecords the maximum number of records to retrieve (0 = 4096);
      */
-    public AuditSearchCriteria(Date fromTime, Date toTime, Level fromLevel, Level toLevel, Class recordClass, String nodeId, long startMessageNumber, long endMessageNumber, int maxRecords) {
+    public AuditSearchCriteria(Date fromTime, Date toTime, Level fromLevel, Level toLevel,
+                               Class recordClass, String nodeId, long startMessageNumber, long endMessageNumber,
+                               int maxRecords)
+    {
         this.fromTime = fromTime;
         this.toTime = toTime;
         this.fromLevel = fromLevel;
@@ -37,22 +45,39 @@ public final class AuditSearchCriteria implements Serializable {
     }
 
     /**
-     * @param start the minimum OID to find
-     * @param end the maximum OID to find
+     * Constructs a simple AuditSearchCriteria
+     *
+     * @param nodeId the ID (usually a MAC address) of the cluster node to search (null = any, see {@link com.l7tech.cluster.ClusterStatusAdmin#getClusterStatus()})
+     * @param start the minimum OID to find, inclusive (0 = any)
+     * @param end the maximum OID to find, inclusive (0 = any)
      * @param max the maximum number of records to retrieve (0 = 4096);
      */
     public AuditSearchCriteria(String nodeId, long start, long end, int max) {
         this(null, null, null, null, null, nodeId, start, end, max);
     }
 
+    /** The time of the earliest record to retrieve (null = 24 hours ago) */
     public final Date fromTime;
+    /** The time of the latest record to retrieve (null = now) */
     public final Date toTime;
+
+    /** the level of the least severe record to retrieve (null = {@link java.util.logging.Level#INFO}) */
     public final Level fromLevel;
+    /** the level of the most severe record to retrieve (null = {@link java.util.logging.Level#SEVERE}) */
     public final Level toLevel;
+
+    /** the type of {@link AuditRecord}s to retrieve; must be AuditRecord.class or one of its subclasses */
     public final Class recordClass;
+
+    /** the ID (usually a MAC address) of the cluster node to search (null = any, see  {@link com.l7tech.cluster.ClusterStatusAdmin#getClusterStatus()}) */
     public final String nodeId;
 
+    /** the minimum OID to find */
     public final long startMessageNumber;
+
+    /** the maximum OID to find */
     public final long endMessageNumber;
+
+    /** the maximum number of records to retrieve (0 = 4096) */
     public final int maxRecords;
 }
