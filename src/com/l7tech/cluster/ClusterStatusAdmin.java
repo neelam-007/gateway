48cafd9aef8d3b6cb70315b9c7596cb61478d381
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-11-22 19:45:25 +0000

    polished javadoc
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@9178 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/cluster/ClusterStatusAdmin.java
+++ src/com/l7tech/cluster/ClusterStatusAdmin.java
@@ -8,7 +8,8 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 /**
- * Admin interface for the cluster status panel of the SSM.
+ * Remote interface for getting the status of nodes in a gateway cluster.
+ * This is used by the cluster status panel of the SSM.
  *
  * <br/><br/>
  * LAYER 7 TECHNOLOGIES, INC<br/>
@@ -19,44 +20,51 @@ import java.rmi.RemoteException;
  */
 public interface ClusterStatusAdmin extends Remote {
     /**
-     * get status for all nodes recorded as part of the cluster.
+     * Get status for all nodes recorded as part of the cluster.
+     * @return An array of ClusterNodeInfo (one for each node)
+     * @throws FindException if there was a server-side problem accessing the requested information
      */
     ClusterNodeInfo[] getClusterStatus() throws RemoteException, FindException;
 
     /**
-     * get service usage as currently recorded in database.
+     * Get some usage statistics on a 'per-published-service' basis.
+     * @return an array of ServiceUsage (one per published service). never null
+     * @throws FindException if there was a server-side problem accessing the requested information
      */
     ServiceUsage[] getServiceUsage() throws RemoteException, FindException;
 
     /**
-     * lets the administrator change the human readable name of a node part of the cluster. these
+     * Allows the administrator change the human readable name of a node part of the cluster. these
      * names are originally automatically set by the server when they join the cluster.
      *
      * @param nodeid the mac of the node for which we want to change the name
      * @param newName the new name of the node (must not be null)
+     * @throws UpdateException if there was a server-side problem updating the requested node
      */
     void changeNodeName(String nodeid, String newName) throws RemoteException, UpdateException;
 
     /**
-     * lets the administrator remove a node that is no longer part of the cluster. after this is called,
+     * Allows the administrator remove a node that is no longer part of the cluster. When a
+     * node is removed from the cluster, the other nodes might think that the node is no longer
+     * responding. After this is called,
      * the getClusterStatus calls will no longer refer to this node. this operation will not be permitted
-     * unless this node's status has not been updated for a while.
+     * unless this node's status has not been updated for a while. Tables potentially affected by this
+     * call are cluster_info, service_usage and ssg_logs
      *
-     * tables potentially affected by this call are cluster_info, service_usage and ssg_logs
-     *
-     * @param nodeid the mac of the stale node to remove
+     * @param nodeid the mac of the stale node to remove as defined in the ClusterNodeInfo object
+     * @throws DeleteException if there was a server-side problem deleting the requested node
      */
     void removeStaleNode(String nodeid) throws RemoteException, DeleteException;
 
     /**
-     * gets the current system time
+     * Return the current system time on the gateway.
      *
      * @return java.util.Date  The current system time
      */
     java.util.Date getCurrentClusterSystemTime() throws RemoteException;
 
     /**
-     * gets the name of node that handles the admin request.
+     * Gets the name of node that handles the admin request.
      *
      * @return String  The node name
      */
