de333298776dfaf8efd8e592fb61bf87ec0c2d87
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-11-23 00:05:26 +0000

    Javadoc
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@9210 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/common/audit/MessageSummaryAuditRecord.java
+++ src/com/l7tech/common/audit/MessageSummaryAuditRecord.java
@@ -15,7 +15,10 @@ import java.util.logging.Level;
 /**
  * An {@link AuditRecord} that describes the processing of a single message.
  * <p>
- * By default, one of these will be created and logged with every message that gets processed.
+ * By default, these are not saved unless the {@link com.l7tech.policy.assertion.AuditAssertion} is used or the
+ * SSG's message processing audit threshold is set to {@link Level#INFO} or lower.
+ *
+ * @see {@link com.l7tech.server.ServerConfig#PARAM_AUDIT_MESSAGE_THRESHOLD}
  *
  * @author alex
  * @version $Revision$
@@ -25,13 +28,32 @@ public class MessageSummaryAuditRecord extends AuditRecord {
     public MessageSummaryAuditRecord() {
     }
 
+    /**
+     *
+     * @param level the java.util.logging.Level of this record.
+     * @param nodeId the ID of the cluster node from which this AuditRecord originates (see com.l7tech.cluster.ClusterStatusAdmin.getClusterStatus())
+     * @param clientAddr the IP address of the client that made the request
+     * @param serviceName the name of the service that received the request that generated the AuditRecord
+     * @param requestId the unique ID of the request
+     * @param status the {@link AssertionStatus} resulting from applying the service policy to this request
+     * @param requestXml the text of the request received from the client. Not saved by default.
+     * @param requestContentLength the length of the request, in bytes.
+     * @param responseXml the text of the response sent to the client. Not saved by default.
+     * @param responseContentLength the length of the response, in bytes.
+     * @param serviceOid the OID of the {@link PublishedService} this request was resolved to, or {@link PublishedService#DEFAULT_OID} if it could not be resolved.
+     * @param serviceName the name of the {@link PublishedService} this request was resolved to, or null if it could not be resolved.
+     * @param authenticated true if the request was authenticated, false otherwise
+     * @param identityProviderOid the OID of the {@link IdentityProviderConfig IdentityProvider} against which the user authenticated, or {@link IdentityProviderConfig#DEFAULT_OID} if the request was not authenticated.
+     * @param userName the name or login of the user who was authenticated, or null if the request was not authenticated.
+     * @param userId the OID or DN of the user who was authenticated, or null if the request was not authenticated.
+     */
     public MessageSummaryAuditRecord(Level level, String nodeId, String requestId, AssertionStatus status, String clientAddr,
                                      String requestXml, int requestContentLength,
                                      String responseXml, int responseContentLength,
                                      long serviceOid, String serviceName,
                                      boolean authenticated, long identityProviderOid, String userName, String userId)
     {
-        super(level, nodeId, clientAddr, null, null);
+        super(level, nodeId, clientAddr, serviceName, null);
         StringBuffer msg = new StringBuffer("Message ");
         if (status == AssertionStatus.NONE) {
             msg.append("processed successfully");
@@ -40,7 +62,6 @@ public class MessageSummaryAuditRecord extends AuditRecord {
             msg.append(status.getMessage());
             msg.append(" (").append(status.getNumeric()).append(")");
         }
-        this.name = serviceName;
         this.requestId = requestId;
         this.setMessage(msg.toString());
         this.status = status.getNumeric();
@@ -55,42 +76,82 @@ public class MessageSummaryAuditRecord extends AuditRecord {
         this.userId = userId;
     }
 
+    /**
+     * Gets the {@link AssertionStatus} resulting from applying the service policy to this request
+     * @return the {@link AssertionStatus} resulting from applying the service policy to this request
+     */
     public int getStatus() {
         return status;
     }
 
+    /**
+     * Gets the OID of the {@link PublishedService} this request was resolved to, or {@link PublishedService#DEFAULT_OID} if it could not be resolved.
+     * @return the OID of the {@link PublishedService} this request was resolved to, or {@link PublishedService#DEFAULT_OID} if it could not be resolved.
+     */
     public long getServiceOid() {
         return serviceOid;
     }
 
+    /**
+     * Gets the OID of the {@link IdentityProviderConfig IdentityProvider} against which the user authenticated, or {@link IdentityProviderConfig#DEFAULT_OID} if the request was not authenticated.
+     * @return the OID of the {@link IdentityProviderConfig IdentityProvider} against which the user authenticated, or {@link IdentityProviderConfig#DEFAULT_OID} if the request was not authenticated.
+     */
     public long getIdentityProviderOid() {
         return identityProviderOid;
     }
 
+    /**
+     * Gets the name or login of the user who was authenticated, or null if the request was not authenticated.
+     * @return the name or login of the user who was authenticated, or null if the request was not authenticated.
+     */
     public String getUserName() {
         return userName;
     }
 
+    /**
+     * Gets the OID or DN of the user who was authenticated, or null if the request was not authenticated.
+     * @return the OID or DN of the user who was authenticated, or null if the request was not authenticated.
+     */
     public String getUserId() {
         return userId;
     }
 
+    /**
+     * Gets the text of the request received from the client.
+     * @return the text of the request received from the client.
+     */
     public String getRequestXml() {
         return requestXml;
     }
 
+    /**
+     * Gets the text of the response sent to the client.
+     * @return the text of the response sent to the client.
+     */
     public String getResponseXml() {
         return responseXml;
     }
 
+    /**
+     * Returns true if the request was authenticated, false otherwise
+     * @return true if the request was authenticated, false otherwise
+     */
     public boolean isAuthenticated() {
         return authenticated;
     }
 
+    /**
+     * Gets the length of the request received from the client, in bytes.
+     * @return the length of the request, in bytes.
+     */
     public int getRequestContentLength() {
         return requestContentLength;
     }
 
+    /**
+     * Gets the length of the response sent to the client, in bytes.
+     * @return the length of the response, in bytes.
+     */
     public int getResponseContentLength() {
         return responseContentLength;
     }
--- src/com/l7tech/common/audit/SystemAuditRecord.java
+++ src/com/l7tech/common/audit/SystemAuditRecord.java
@@ -11,6 +11,9 @@ import com.l7tech.common.Component;
 import java.util.logging.Level;
 
 /**
+ * SystemAuditRecords are generated for system-level events that are not necessarily triggered by a particular
+ * administrator or as a result of a client request.
+ *
  * @author alex
  * @version $Revision$
  */
@@ -19,6 +22,15 @@ public class SystemAuditRecord extends AuditRecord {
     protected SystemAuditRecord() {
     }
 
+    /**
+     * Constructs a new SystemAuditRecord.
+     *
+     * @param level the java.util.logging.Level of this record.
+     * @param nodeId the ID of the cluster node from which this AuditRecord originates (see com.l7tech.cluster.ClusterStatusAdmin.getClusterStatus())
+     * @param ip the IP address of the entity that caused this AuditRecord to be created. It could be that of a cluster node, an administrative workstation or a web service requestor.
+     * @param component the {@link Component} that was involved in the event
+     * @param action a short description of the action that was happening when the event was generated
+     */
     public SystemAuditRecord(Level level, String nodeId, Component component, String action, String ip) {
         super(level, nodeId, ip, component.getName(), component.getName() + " " + action);
         this.component = component.getCode();
@@ -33,6 +45,10 @@ public class SystemAuditRecord extends AuditRecord {
         return component;
     }
 
+    /**
+     * Gets a short description of the action that was happening when the event was generated
+     * @return a short description of the action that was happening when the event was generated
+     */
     public String getAction() {
         return action;
     }
