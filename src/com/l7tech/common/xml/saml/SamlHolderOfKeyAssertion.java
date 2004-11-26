8efe81f880f39bb776dfab5c931097812b8dd99f
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-11-26 22:14:25 +0000

    dead imports
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@9269 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/common/xml/saml/SamlHolderOfKeyAssertion.java
+++ src/com/l7tech/common/xml/saml/SamlHolderOfKeyAssertion.java
@@ -6,31 +6,10 @@
 
 package com.l7tech.common.xml.saml;
 
-import com.ibm.xml.dsig.IDResolver;
-import com.ibm.xml.dsig.SignatureContext;
-import com.ibm.xml.dsig.Validity;
-import com.l7tech.common.util.CertUtils;
-import com.l7tech.common.util.HexUtils;
-import com.l7tech.common.util.SoapUtil;
-import com.l7tech.common.util.XmlUtil;
-import com.l7tech.common.xml.TooManyChildElementsException;
-import org.apache.xmlbeans.XmlException;
-import org.w3.x2000.x09.xmldsig.KeyInfoType;
-import org.w3.x2000.x09.xmldsig.X509DataType;
-import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
-import x0Assertion.oasisNamesTcSAML1.*;
 
-import java.io.IOException;
-import java.security.PublicKey;
-import java.security.SignatureException;
-import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.Calendar;
-import java.util.TimeZone;
 
 /**
  * @author mike
