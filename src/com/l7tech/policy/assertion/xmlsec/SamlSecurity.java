f62979471fc351e219d9114395b77889f636f688
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-11-26 17:56:08 +0000

    extended SamlSecurity to allow different types of ConfirmationMethods
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@9259 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/policy/assertion/xmlsec/SamlSecurity.java
+++ src/com/l7tech/policy/assertion/xmlsec/SamlSecurity.java
@@ -7,4 +7,38 @@ import com.l7tech.policy.assertion.credential.CredentialSourceAssertion;
  * assertion signed by a recognized token server.
  */
 public class SamlSecurity extends CredentialSourceAssertion {
+    public static final int CONFIRMATION_METHOD_HOLDER_OF_KEY = 0;
+    public static final int CONFIRMATION_METHOD_SENDER_VOUCHES = 1;
+    public static final int CONFIRMATION_METHOD_WHATEVER = 2;
+
+    /**
+     * Default constructor (sets confirmation method type as whatever)
+     */
+    public SamlSecurity() {
+        this.confirmationMethodType = CONFIRMATION_METHOD_WHATEVER;
+    }
+
+    /**
+     * Constructor that lets you spec the confirmation method
+     * @param confirmationMethodType CONFIRMATION_METHOD_HOLDER_OF_KEY or CONFIRMATION_METHOD_SENDER_VOUCHES
+     *                               or CONFIRMATION_METHOD_WHATEVER
+     */
+    public SamlSecurity(int confirmationMethodType) {
+        if (confirmationMethodType < CONFIRMATION_METHOD_HOLDER_OF_KEY ||
+            confirmationMethodType > CONFIRMATION_METHOD_WHATEVER) {
+            throw new IllegalArgumentException("confirmationMethodType value of " + confirmationMethodType +
+                                               " is invalid");
+        }
+        this.confirmationMethodType = confirmationMethodType;
+    }
+
+    public int getConfirmationMethodType() {
+        return confirmationMethodType;
+    }
+
+    public void setConfirmationMethodType(int confirmationMethodType) {
+        this.confirmationMethodType = confirmationMethodType;
+    }
+
+    private int confirmationMethodType;
 }
