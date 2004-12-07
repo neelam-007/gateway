b792bf7cdd559755e9da9d809f6ff99eadb6b113
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-12-07 22:46:00 +0000

    Add test for XencUtils, specifically padSymmetricKeyForRsaEncryption
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@9307 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ tests/com/l7tech/common/security/xml/XencUtilTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2004 Layer 7 Technologies Inc.
+ *
+ * $Id$
+ */
+
+package com.l7tech.common.security.xml;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import java.util.logging.Logger;
+import java.util.Random;
+import java.security.cert.X509Certificate;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.interfaces.RSAPublicKey;
+
+import com.l7tech.common.xml.TestDocuments;
+import com.l7tech.common.util.HexUtils;
+
+/**
+ * @author mike
+ */
+public class XencUtilTest extends TestCase {
+    private static Logger log = Logger.getLogger(XencUtilTest.class.getName());
+
+    public XencUtilTest(String name) {
+        super(name);
+    }
+
+    public static Test suite() {
+        return new TestSuite(XencUtilTest.class);
+    }
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public void testPadKey() throws Exception {
+        SecureRandom rand = new SecureRandom();
+        X509Certificate recipientCert = TestDocuments.getDotNetServerCertificate();
+        RSAPublicKey publicKey = (RSAPublicKey)recipientCert.getPublicKey();
+
+        byte[] keyBytes = HexUtils.unHexDump("954daf423cea7911cc5cb9b664d4c38d");
+
+        final int modulusLength = publicKey.getModulus().toByteArray().length;
+        assertTrue(modulusLength <= 128);
+
+        byte[] paddedKeyBytes = XencUtil.padSymmetricKeyForRsaEncryption(keyBytes, modulusLength, rand);
+
+        assertTrue(paddedKeyBytes.length <= modulusLength);
+        assertEquals(128, paddedKeyBytes.length);
+    }
+}
