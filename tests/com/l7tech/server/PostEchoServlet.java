baac30ab5fc25bdf474fe82a2a1cf6d38b0732cc
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-12-14 00:52:45 +0000

    For Jay
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@9357 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ tests/com/l7tech/server/PostEchoServlet.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2004 Layer 7 Technologies Inc.
+ *
+ * $Id$
+ */
+
+package com.l7tech.server;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * @author mike
+ */
+public class PostEchoServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest servletRequest, HttpServletResponse servletResponse) throws ServletException, IOException {
+        servletResponse.setContentType(servletRequest.getContentType());
+        servletResponse.setStatus(HttpServletResponse.SC_OK);
+        InputStream in = servletRequest.getInputStream();
+        OutputStream out = servletResponse.getOutputStream();
+        byte[] buf = new byte[8192];
+        int got;
+        while ((got = in.read(buf)) > 0) {
+            out.write(buf, 0, got);
+        }
+        in.close();
+        out.close();
+    }
+}
