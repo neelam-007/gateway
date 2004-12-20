978c6b7eddf4a01b7fcf3d7d1b77021883db7fd9
cyeung <cyeung@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-12-20 19:44:55 +0000

    Bug 1420-1,2(changed matching pattern of url,username,password),6; 1422; 1423; 1424-1; add one more matching pattern for servier-id to cover already uncommented ones
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@9373 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- etc/install.pl
+++ etc/install.pl
@@ -6,31 +6,52 @@
 
 use strict;
 my @list=(
-	cluster   => "Node is in a cluster       ",
-	firstnode => "This is first Node         ",
-	master_n  => "First node of cluster      ",
-	hostname  => "Hostname for this Node     ",
-	dbserver  => "Node is a DB Server        ",
-	db_otherip=> "IP of Other DB Server      ",
-	db_user   => "Database Username          ",
-	db_pass   => "Database Password          ",
-	dbclip    => "Database cluster IP        ",
-	clusternm => "Cluster Host Name          ",
-	def_rt    => "IP of Load Balancer Backend",
-	front_ip  => "Node Public IP             ",
-	front_mask=> "Public Network Mask        ",
-	back_ip   => "Node Private IP            ",
-	back_net  => "Node private Network       ",
-	back_mask => "Private Network Mask       ",
-	back_rt   => "Back End Router            ",
+	hostname      => "Hostname for this node (eg. hostname.company.com)                                                         ",
+	gc_cluster    => "Node is part of a cluster gateway (y/n)                                                                   ",
+	gc_firstnode  => "Cluster gateway env, First node in gateway cluster (y/n)                                                  ",
+	gc_masternip  => "Cluster gateway env, IP (reachable by this host) of the first node (eg. 10.0.0.2)                         ",
+        gc_clusternm  => "Cluster gateway env, Cluster host name (eg. cluster_hostname.company.com)                                 ",
+	dc_cluster    => "Node is to be connected to a cluster database environemnt (y/n)                                           ",
+	dc_dbserver   => "Cluster DB env, Node is a DB Server (y/n)                                                                 ",
+	dc_firstnode  => "Cluster DB env, First node in DB cluster (y/n)                                                            ",
+        dc_dbip       => "Cluster DB env, DB cluster IP (eg. 10.0.0.10)                                                             ",
+	dblocal       => "Non cluster DB env, DB is on the localhost (database must able to login as localhost) (y/n)               ",
+	dbhostname    => "Non cluster DB env, DB hostname (must be reach-able by this node) if DB is not on localhost (eg. dbserver)",
+	dbuser        => "Database Username (eg. dbuser)                                                                            ",
+	dbpass        => "Database Password (eg. dbpassword)                                                                        ",
+	net_def_rt    => "IP of Load Balancer between bridge/client and gateway (eg. 192.168.1.1)                                   ",
+	net_front_ip  => "Network configuration, Node Public IP (eg. 192.168.1.8)                                                   ",
+	net_front_mask=> "Network configuration, Public Network Mask (eg. 255.255.255.0)                                            ",
+	net_back_ip   => "Network configruation, Node Private IP (eg. 10.0.0.8)                                                     ",
+	net_back_net  => "Network configuration, Node private Network for /etc/init.d/back_route (eg. 224.0.0.0)                    ",
+	net_back_mask => "Network configuration, Private Network Mask (eg. 255.255.255.0)                                           ",
+	net_back_rt   => "Network configuration, Back End Router (eg. 10.0.0.1)                                                     ",
 );
 my %Conf=();
 
-my $SAVE_FILE="/home/jay/SSG_INSTALL";
+my $SAVE_FILE="/var/log/SSG_INSTALL";
+
+my $cnf_src="/ssg/bin/my.cnf";
+my $cnf_target="/etc/my.cnf";
+
 # then change the local files as appropriate
 if ( -e $SAVE_FILE ) {
 	if ( $ARGV[0] eq "-reinstall" ) {
 		readconfig();
+LOOP:
+		getparams();
+		print display();
+		print "Is this correct?(y/n)";
+		my $ans=<STDIN>;
+		chomp $ans;
+		$ans = trimwhitespace($ans);
+		if ($ans eq 'y') {
+        		print "Writing Config file $SAVE_FILE\n";
+        		writefile();
+        		change_os_config();
+		} else {
+        		goto LOOP;
+		}
 	} elsif ($ARGV[0] eq '-apply' ) {
 		readconfig();
 		change_os_config();
@@ -41,30 +62,15 @@ if ( -e $SAVE_FILE ) {
 		die "Already Installed";
 	}
 } 
-	
-
-LOOP:
-getparams();
-print display();
-print "Is this correct?(y/N)";
-my $ans=<STDIN>;
-chomp $ans;
-if ($ans eq 'y') {
-	print "Writing Config file $SAVE_FILE\n";
-	writefile();
-	change_os_config();
-} else {
-	goto LOOP;
-}
 
 
 sub change_os_config {
 	# lets start small
 	# 1. the front network config
-	print "Starting Network config: ";
+	print "Starting Network config: \n";
 	my $front_conf = "/etc/sysconfig/network-scripts/ifcfg-eth1";
 	
-	if ($Conf{"front_ip"} ) {
+	if ($Conf{"net_front_ip"} ) {
 		if ( -e $front_conf ) {
 			`mv $front_conf $front_conf.save`;
 		}
@@ -73,21 +79,21 @@ sub change_os_config {
 DEVICE=eth1
 ONBOOT=yes
 BOOTPROTO=static
-IPADDR=$Conf{"front_ip"}
-NETMASK=$Conf{"front_mask"}
-GATEWAY=$Conf{"def_rt"}
+IPADDR=$Conf{"net_front_ip"}
+NETMASK=$Conf{"net_front_mask"}
+GATEWAY=$Conf{"net_def_rt"}
 EOF
 		close OUT;
 		print "Front: Eth1 ";	
 	} else {
-		print "WARNING: No front end network defined";
+		print "WARNING: No front end network defined\n";
 	}
 	
 	# 2. the back network config
 
 	my $back_conf = "/etc/sysconfig/network-scripts/ifcfg-eth0";
 	
-	if ($Conf{"back_ip"} ) {
+	if ($Conf{"net_back_ip"} ) {
 		if ( -e $back_conf ) {
 			`mv $back_conf $back_conf.save`;
 		}
@@ -96,30 +102,30 @@ EOF
 DEVICE=eth0
 ONBOOT=yes
 BOOTPROTO=static
-IPADDR=$Conf{"back_ip"}
-NETMASK=$Conf{"back_mask"}
+IPADDR=$Conf{"net_back_ip"}
+NETMASK=$Conf{"net_back_mask"}
 EOF
 		close OUT;
 		print "Back: Eth0 ";	
 	} else {
-		print "WARNING: No back end network defined ";
+		print "WARNING: No back end network defined\n";
 	}
 	print " ... Done\n";
 
 	# cluster stuff
 	# 3 . hostname
 	print "Cluster Hostname: ";
-	if ($Conf{clusternm} && lc($Conf{cluster}) eq "y") {
+	if ($Conf{gc_clusternm} && lc($Conf{gc_cluster}) eq "y") {
 		open (OUT, ">/ssg/etc/conf/cluster_hostname") or die "Can't open cluster hostname file";
-		print OUT "$Conf{clusternm}\n";
+		print OUT "$Conf{gc_clusternm}\n";
 		close OUT;
-		print "$Conf{clusternm}\n";
+		print "$Conf{gc_clusternm}\n";
 		open (HOST, ">/etc/hosts") or die "Can't open /etc/hosts!";
 		print HOST <<EOF;
 # Modified by /ssg/bin/install.pl
 # Will be replaced if you rerun!
 127.0.0.1	localhost localhost.localdomain
-$Conf{back_ip}	$Conf{clusternm} $Conf{hostname}
+$Conf{net_back_ip}	$Conf{gc_clusternm} $Conf{hostname}
 
 EOF
 		close HOST;
@@ -133,70 +139,116 @@ EOF
 	# 4. local config of db connection
 	# Only works with a distro hib properties, it has placeholders
 	#
-	print "DB Connection Parameters: ";
-	if ($Conf{db_user} ) {
+	print "\nINFO: DB Connection Parameters...\n";
+
+	# 4a. db url, username, password configuration to /ssg/etc/conf/hibernate.properties
+	my $db_url;
+        # non cluster DB
+        if ($Conf{dc_cluster} eq "n") {
+                if ($Conf{dblocal} eq "y") {
+                        $db_url = "localhost";
+                } elsif ($Conf{dbhostname}) {
+                        $db_url = $Conf{dbhostname};
+                } else {
+                        print "WARNING: DB url not set, as database hostname not provided\n";
+                }
+        } else {  # cluster DB
+                if ($Conf{dc_dbip}) {
+                        $db_url = $Conf{dc_dbip};
+                } else {
+                        print "WARNING: DB url not set, as cluster database IP not provided\n";
+                }
+        } 
+
+	if ($Conf{dbuser}) {
+		print "INFO: Now update /ssg/etc/conf/hibernate.properties\n";
 		my $dbconfig="/ssg/etc/conf/hibernate.properties";
 		rename ($dbconfig,"$dbconfig.orig");
 		open (IN, "<$dbconfig.orig");
 		open (OUT,">$dbconfig");
 		while (<IN>) {
-			s/DB_USER/$Conf{db_user}/;
-			s/DB_PASSWORD/$Conf{db_pass}/;
+			if ($db_url) {
+				if ($_ =~ m/url(.*)\/\/(.*)\//) {
+					s/$2/$db_url/;
+				}
+			}
+			s/username(.*)$/username = $Conf{dbuser}/;
+			s/password(.*)$/password = $Conf{dbpass}/;
 			print OUT;
 		}
 		close IN;
 		close OUT;
-		my $sql = "grant all on ssg.* to $Conf{db_user}\@'%' identified by '$Conf{db_pass}';\n\n";
-		$sql .="grant all on ssg.* to $Conf{db_user}\@'localhost' identified by '$Conf{db_pass}';\n\n";
+	} else {
+		print "WARNING: /ssg/etc/conf/hibernate.properties not set";
+	}
+
+	# 4b. grant user to local database
+	if (($Conf{dbuser}) && (($Conf{dc_dbserver} eq "y") || ($Conf{dblocal} eq "y"))) {
+		# grant user to local database
+		print "INFO: Now grant username & password to local database\n";
+		my $sql = "grant all on ssg.* to $Conf{dbuser}\@'%' identified by '$Conf{dbpass}';\n\n";
+		$sql .="grant all on ssg.* to $Conf{dbuser}\@'localhost' identified by '$Conf{dbpass}';\n\n";
 		open (TMP, ">/tmp/sql.grants");
 		print TMP $sql;
 		close TMP;
+		print "INFO: Now run following SQL... \n$sql\n";
 		my $success=`mysql -u root </tmp/sql.grants`;
 		# unlink "/tmp/sql.grants";
-		print "Set \n";
 	} else {
-		print "WARNING: Db connection not set\n";
+		print "WARNING: DB username/password not granted to local database\n";
 	}
-	# 4. do the config for db cluster id?
-	print "DB Cluster Parameters: ";
-	if ( $Conf{dbserver} eq "y" ) {
+ 
+	# 4c. copy $cnf_src to $cnf_target 
+	print "INFO: Now copy $cnf_src to $cnf_target\n";
+        if (-e $cnf_target) {
+		print "WARNING: $cnf_target existed, now renaming $cnf_target to $cnf_target.orig\n";
+	        rename ($cnf_target,"$cnf_target.orig");
+        }
+	print "INFO: Copying $cnf_src to $cnf_target\n";
+       `cp -f $cnf_src $cnf_target`;
+ 
+	# 4d. do the config for db cluster id?
+	print "INFO: Now update $cnf_target\n";
+	if ( ($Conf{dc_cluster} eq "y") && ($Conf{dc_dbserver} eq "y") ) {
 		my $s_id=2;
-		if ($Conf{firstnode} eq "y" ) {
-			$s_id=1;
-		}
-	
-		my $cnf="/etc/my.cnf";
-				
-		rename ($cnf,"$cnf.orig");
-		open (IN,"$cnf.orig");
-		open(CNF, ">$cnf");
+                if ($Conf{dc_firstnode} eq "y" ) {
+                        $s_id=1;
+                }
+		open (IN,"$cnf_target");
+		open(CNF, ">$cnf_target");
 		while(<IN>) {
-			s/^#server-id=1$/server-id=$s_id/;
+                        s/^#server-id=1$/server-id=$s_id/;   #uncomment it and set server id value if commented out
+			s/^server-id=(.*)$/server-id=$s_id/; #set server id value if already uncomment, also purge any server-id were set to other values
 			print CNF;
 		}		
 		close CNF;
 		# server id is set. Need to run the sync; then set the master/slave relationship
 		# perhaps this is best left to the user.
-		print "cluster id: $s_id\n";
+		print "INFO: Server id is set - need to manually runt the sync, then set the master/slave relationship\n";
+		print "INFO: $cnf_target updated with cluster server id: $s_id\n";
 	} else {
-		print "WARNING: Db cluster id not set\n";
+		print "WARNING: DB cluster server id not set\n";
 	}
-		
 	
-	# 5. gen keys?
-	if ($Conf{cluster} ) {
-		if ( $Conf{firstnode} ) {
-			# gen the keys, otherwise copy them
-			`su - gateway -c /ssg/bin/setkeys.sh`;
-		} else {
-			print "supply gateway user password of first node\n";
-			`scp gateway\@$Conf{master_n}/ssg/etc/keys/* /ssg/etc/keys`;
-			`chmod -R gateway.gateway /ssg/etc`;
-		}
+	# 5. gen keys? 
+	if (($Conf{gc_cluster} eq "n") || ($Conf{gc_cluster} eq "y" && $Conf{gc_firstnode} eq "y")) {
+		# gen the keys, otherwise copy them
+		print "Invoke /ssg/bin/setkeys.sh script to generate keys\n";
+		system("su - gateway -c /ssg/bin/setkeys.sh");
+
+	} else {
+		print "Copy keys & keys configuration files from the first node...\n";
+		print "Supply gateway user password of first node - " + $Conf{gc_masternip} + "\n";
+                print "Now to copy /ssg/etc/keys/*:\n"; 
+		`cp gateway\@$Conf{gc_masternip}/ssg/etc/keys/* /ssg/etc/keys`;
+		print "Now to copy /ssg/tomcat/conf/server.xml:\n";
+		`scp gateway\@$Conf{gc_masternip}/ssg/tomcat/conf/server.xml /ssg/tomcat/conf/server.xml`;
+		print "Now to copy /ssg/etc/conf/keystore.properties:\n";
+		`scp gateway\@$Conf{gc_masternip}/ssg/etc/conf/keystore.properties /ssg/etc/conf/keystore.properties`;
+		`chmod -R gateway.gateway /ssg/etc`;
+		`chmod gateway.gateway /ssg/tomcat/conf/server.xml`;
+		`chmod gateway.gateway /ssg/etc/conf/keystore.properties`;
 	}
-	# 
-	# 
-		
 }
 	
 
@@ -224,6 +276,7 @@ EOF
 		print "$c{$f} ($Conf{$f}):";
 		my $newval=<STDIN>;
 		chomp $newval;
+		$newval = trimwhitespace($newval);
 		if ($newval ne "") {
 			$Conf{$f}=$newval;
 		}
@@ -264,4 +317,12 @@ $0: [-reinstall] [-apply] [-usage]
 
 EOF
 
+}
+
+sub trimwhitespace($) {
+# Remove whitespace from the start and end of the string
+	my $string = shift;
+	$string =~ s/^\s+//;
+	$string =~ s/\s+$//;
+	return $string;
 }
