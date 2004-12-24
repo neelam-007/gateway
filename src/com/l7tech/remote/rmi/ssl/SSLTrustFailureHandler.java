ad382b98e848050e23ee55b2e0fd445e47be3b57
emil <emil@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2004-12-24 19:01:22 +0000

    Restore the SSM client cert download, and fix the wrong IdProvCOnfigManager obtaining in PolicyServiette
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@9410 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/remote/rmi/ssl/SSLTrustFailureHandler.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2003-2004 Layer 7 Technologies Inc.
+ *
+ * $Id$
+ */
+package com.l7tech.remote.rmi.ssl;
+
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+/**
+ * Implementations are notified during ssl handskhake that the trust could
+ * not be established because of certificate related issues. The peer certificate
+ * missing from the trust store for example.
+ * @author emil
+ * @version Dec 20, 2004
+ * @see SslRMIClientSocketFactory#setTrustFailureHandler(SSLTrustFailureHandler) 
+ */
+public interface SSLTrustFailureHandler {
+    /**
+     * HAndle the trust failure and return whether the connection should proceed or
+     * fail.
+     *
+     * @param e the exception that caused the trust failure
+     * @param chain the peer certificate chain
+     * @param authType the authentication type that was requested
+     * @return true to proceed with the ssl connection, false otherwise
+     */
+    boolean handle(CertificateException e, X509Certificate[] chain, String authType);
+}
