024a6566df58902ac26cef291c1662c38a25c681
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-01-04 22:58:28 +0000

    Imports
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@9422 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/server/BootServlet.java
+++ src/com/l7tech/server/BootServlet.java
@@ -6,12 +6,9 @@
 
 package com.l7tech.server;
 
-import org.springframework.web.context.support.WebApplicationContextUtils;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.ContextLoaderServlet;
 import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
