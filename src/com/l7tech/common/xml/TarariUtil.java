67ad4ac2462654dc65e7e26ca473c0dc0f42ad13
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-01-11 20:42:36 +0000

    Now Tarari board is only probed if the system property com.l7tech.common.xml.tarari.enable is true.  Otherwise, attempts even to probe the card throw an uncatchable link error on Windows the instant the XPathCompiler class is initialized.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@9498 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/common/xml/TarariUtil.java
+++ src/com/l7tech/common/xml/TarariUtil.java
@@ -6,20 +6,31 @@
 
 package com.l7tech.common.xml;
 
-import com.tarari.xml.xpath.XPathCompiler;
+import java.lang.reflect.Method;
 
 /**
  * @author alex
  * @version $Revision$
  */
 public class TarariUtil {
+    public static final String ENABLE_PROPERTY = "com.l7tech.common.xml.tarari.enable";
+    public static final String XPATH_COMPILER_CLASSNAME = "com.tarari.xml.xpath.XPathCompiler";
     private static Boolean present = null;
     public static boolean isTarariPresent() {
         if (present == null) {
-            try {
-                XPathCompiler.reset();
-                present = Boolean.TRUE;
-            } catch (Throwable t) {
+            if (Boolean.getBoolean(ENABLE_PROPERTY)) {
+                try {
+                    Class xpathCompilerClass = Class.forName(XPATH_COMPILER_CLASSNAME, false, TarariUtil.class.getClassLoader());
+                    Method resetMethod = xpathCompilerClass.getMethod("reset", new Class[0]);
+                    resetMethod.invoke(null, new Object[0]);
+                    present = Boolean.TRUE;
+                } catch (UnsatisfiedLinkError e) {
+                    present = Boolean.FALSE;
+                } catch (Throwable t) {
+                    present = Boolean.FALSE;
+                }
+            } else {
+                // Disabled -- skip the probe
                 present = Boolean.FALSE;
             }
         }
