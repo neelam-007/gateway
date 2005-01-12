9aa103509060923ba2bce4cf99532f7aeabfbd6f
cyeung <cyeung@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-01-12 22:52:38 +0000

    Bug 1435: no more input prompts, detect from/to release, and invoke specific upgrade script accordingly
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@9523 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- etc/upgrade.sh
+++ etc/upgrade.sh
@@ -1,66 +1,52 @@
-#!/bin/sh
+#/bin/sh
 
+if [ ! $# = 1 ]; then
+  echo "ERROR: Usage - $0 [getFromRelease|doUpgrade]"
+  exit 0
+fi
+
+option="$1"
 ssgDir="/ssg/bin"
 ssgDBDir="/ssg/etc/sql"
-ssgDBName="ssg"
-geometryTable="/ssg/bin/upgrade.txt"
 
-echo "Please base on the release upgrade geometry (from release x to release y)"
-echo "to figure out the upgrade ssg and database script required to be run on this RPM upgrade."
-echo "If you have existed from this $0 script, you may invoke it manually, which locates at /ssg/bin/ directory."
-echo "\n"
-if [ -e $geometryTable ]; then
-  cat /ssg/bin/upgrade.txt | more
-else
-  echo "ERROR: $geometryTable NOT found - the content of this file contains a release geometry table on assisting you to figure out the upgrade ssg and database script required to be run on this RPM upgrade"
-  echo "$0 exits"
-  exit
+releaseFromFile="/tmp/releaseFrom.txt"
+
+hibernateFile="/ssg/etc/conf/hibernate.properties"
+
+if [ "$option" = "getFromRelease" ]; then
+  # record RPM distribution, assumption - returns ssg-x.y.z-n pattern
+  rpm -qa | grep -i ^ssg- > $releaseFromFile
 fi
-echo "Does this upgrade require any \"ssg upgrade script\" to be run? (y/n)"
-read inputReqSsgUp
-if [ "$inputReqSsgUp" = "y" ]; then
-  echo "Name of \"ssg upgrade script\"? (eg. upgrade_<releaseX>-<releaseY>.sh)"
-  read inputNameSsgUp
-  if [ -e $ssgDir/$inputNameSsgUp -a -x $ssgDir/$inputNameSsgUp -a ! -d $ssgDir/$inputNameSsgUp ]; then
-    echo "INFO: Start running script \"$ssgDir/$inputNameSsgUp\"..."
-    $ssgDir/$inputNameSsgUp
-  else
-    echo "ERROR: $ssgDir/$inputNameSsgUp does not exist and/or not executable, so $ssgDir/$inputNameSsgUp did NOT run"
+
+if [ "$option" = "doUpgrade" ]; then
+  # extract x.y.z from ssg-x.y.z-n pattern from $releaseFromFile (taken before rpm distribution)
+  releaseFromVersion=`cat $releaseFromFile | sed -e 's/ssg-//' | sed -e 's/-.*//'`
+  # extract x.y.z from ssg-x.y.z-n pattern from ssg version taken after rpm distribution 
+  releaseToVersion=`rpm -qa | grep ssg | sed -e 's/ssg-//' | sed -e 's/-.*//'`
+  echo "INFO: Detected upgrade is from <$releaseFromVersion> to <$releaseToVersion>"
+  # assumption - version specific upgrade file of naming convention upgrade_<releaseFromVersion>-<releaseToVersion>.sh
+  upgradeSSGFile=$ssgDir/upgrade_$releaseFromVersion-$releaseToVersion.sh
+  # attempt to upgrade ssg
+  if [ -e $upgradeSSGFile -a -x $upgradeSSGFile -a ! -d $upgradeSSGFile ]; then
+    echo "INFO: Stating sgg upgrade script $upgradeSSGFile"
+    $upgradeSSGFile
+  else 
+    echo "WARNING: SSG upgrade script $upgradeSSGFile NOT found/run, or may not be necessary"
   fi
-fi
-echo "Does this upgrade require any \"database upgrade script\" to be run? (y/n)" 
-read inputReqDBUp
-if [ "$inputReqDBUp" = "y" ]; then
-  echo "Name of \"database upgrade script\"? (eg. upgrade_<releaseX>-<releaseY>.sql)"
-  read inputNameDBUp
-  if [ -e $ssgDBDir/$inputNameDBUp -a ! -d $ssgDBDir/$inputNameDBUp ]; then
-    echo "What is the database vendor? (Select by entering a number which represent a database vendor, and press <Enter> key)"
-    select dbVendor in "MySQL" "Postgres" "Oracle"; do
-      break
-    done
-    echo "You have selected $dbVendor"
-    case $dbVendor in 
-      MySQL)
-        echo "INFO: Start running $ssgDBDir/$inputNameDBUp script on $dbVendor database $ssgDBName - \"mysql $ssgDBName < $ssgDBDir/$inputNameDBUp\"..."
-        mysql $ssgDBName < $ssgDBDir/$inputNameDBUp
-    ;;
-      Postgres)
-        echo "INFO: Start running $ssgDBDir/$inputNameDBUp script on $dbVendor database $ssgDBName - \"psql $ssgDBName -f $ssgDBDir/$inputNameDBUp\"..."
-        psql $ssgDBName -f $ssgDBDir/$inputNameDBUp
-    ;;
-      Oracle)
-        echo "Database connection username?"
-        read dbuser
-        echo "Above user's password?"
-        read dbpass
-        echo "INFO: Start running $ssgDBDir/$inputNameDBUp script on $dbVendor database $ssgDBName - \"sqlplus $dbuser/$dbpass @$ssgDBDir/$inputNameDBUp\"..."
-        sqlplus $dbuser/$dbpass @$ssgDBDir/$inputNameDBUp
-    ;;
-      *)
-        echo "ERROR: Bad vendor selection, database upgrade script is NOT run"
-    ;;
-    esac
+  # check for database vendor of mysql and database name of ssg from $hibernateFile
+  dbVendor=`grep -i ^hibernate.connection.url.*jdbc:mysql.*\/ssg\? $hibernateFile`
+  if [ ! "$dbVendor" = "" ]; then
+    echo "INFO: SSG is running with MySQL of database name ssg - <$dbVendor> from <$hibernateFile>"
+    # assumption - version specific upgrade file of naming convention upgrade_<releaseFromVersion>-<releaseToVersion>.sql
+    upgradeDBFile=$ssgDBDir/upgrade_$releaseFromVersion-$releaseToVersion.sql
+    # attempt to upgrade database if it's running MySQL
+    if [ -e $upgradeDBFile -a ! -d $upgradeDBFile ]; then 
+      echo "INFO: Invoking database upgrade with command \"mysql ssg < $upgradeDBFile\""
+      mysql ssg < $upgradeDBFile
+    else
+      echo "WARNING: SSG database upgrade script $upgradeDBFile NOT found/run, or may not be necessary" 
+    fi
   else
-    echo "ERROR: $ssgDBDir/$inputNameDBUp does not exist, so $ssgDBDir/$inputNameDBUp did NOT run"
+    echo "MANUAL TASK: If the database vendor is not MySQL and/or the SSG database name is not \"ssg\", database requires update manually if database upgrade applicable"
   fi
 fi
