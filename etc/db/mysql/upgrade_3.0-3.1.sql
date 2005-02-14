e68ca830f8abfe75398dd274215a1a276590d7ef
jay <jay@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-02-14 20:00:02 +0000

    Fix alter table statement to work (at all) Bugz: 1510
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@9897 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- etc/db/mysql/upgrade_3.0-3.1.sql
+++ etc/db/mysql/upgrade_3.0-3.1.sql
@@ -1,4 +1,4 @@
 alter table internal_user add column expiration bigint(20);
 update internal_user set expiration=-1;
 alter table internal_user modify column expiration bigint(20) not null;
-alter TABLE audit_main modify column ip_address null;
+alter TABLE audit_main change ip_address ip_address varchar(32) null;
