acf26db60cf29a79d56090de54eb7692c11da072
cyeung <cyeung@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-02-17 20:25:15 +0000

    Bug 1530 - better message report if SSG version specific upgrade script is not applicable; sql script needs to be run manually with message to inform users
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@9950 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- etc/upgrade.sh
+++ etc/upgrade.sh
@@ -55,38 +55,35 @@ if [ "$option" = "getFromRelease" -o "$option" = "getToRelease" ]; then
   buildNumber=`/bin/ls /ssg/dist/ROOT-b*.war | sed -e 's/^.*ROOT-b//' | sed -e 's/\.war//'`
   # verify $mapFile contains only unique record for the $buildNumber
   grepCount=`grep -c ^.*$buildNumber.*= $mapFile | sed -e 's/^.*=//'`
+  echo $grepCount
   if [ ! "$grepCount" = 1 ]; then 
-    echo "ERROR: <$mapFile> contains more than one record (total of <$grepCount>) for buildNumber <$buildNumber>"
+    echo "WARNING: Not applicable if there's no SSG version specific upgrade srcipt to be 
+run - <$mapFile> contains more than one record (total of <$grepCount>) for buildNumber <$buildNumber>"
     echo `grep ^.*$buildNumber.*= $mapFile`
-    echo "ERROR: <$0 $option> exits"
     exit 1
   fi
   # get version number base on build number from $mapFile
   versionNumber=`grep ^.*$buildNumber.*= $mapFile | sed -e 's/^.*=//'`
-  if [ ! "$versionNumber" = "" ]; then
-    if [ "$option" = "getFromRelease" ]; then
-      echo "INFO: Save fromRelease version number <$versionNumber> to <$releaseFromFile>"
-      echo $versionNumber > $releaseFromFile
-    elif [ "$option" = "getToRelease" ]; then
-      echo "INFO: Save toRelesae version number <$versionNumber> to <$releaseToFile>"
-      echo $versionNumber > $releaseToFile
-    fi
-  else
-    echo "ERROR: <$mapFile> missing map record for buildNumber <$buildNumber>"
-    echo "ERROR: <$0 $option> exits"
-    exit 1
+  if [ "$option" = "getFromRelease" ]; then
+    echo "INFO: Save fromRelease version number <$versionNumber> to <$releaseFromFile>"
+    echo $versionNumber > $releaseFromFile
+  elif [ "$option" = "getToRelease" ]; then
+    echo "INFO: Save toRelesae version number <$versionNumber> to <$releaseToFile>"
+    echo $versionNumber > $releaseToFile
   fi
 fi
 
 if [ "$option" = "doUpgrade" ]; then
+  # SSG version specific upgrade
   echo "INFO: Performing Upgrade"
   # get $releaseToFile
   $0 getToRelease
-  # verify the existence of $releaseFromFile and $releaseToFile
+  # verify the existence of $releaseFromFile and $releaseToFile, if they do not exist means no records from $mapFile 
   if [ ! -e $releaseFromFile -o -d $releaseFromFile -o ! -e $releaseToFile -o -d $releaseToFile ]; then
-    echo "ERROR: Missing <$releaseFromFile> or/and <$releaseToFile> - unable to derive the SSG fromVersion and toVeresion number"
-    echo "ERROR: <$0 $option> exits"
-    exit 1
+    echo "WARNING: <$releaseFromFile>/<$releaseToFile> NOT found/run therefore SSG version specific upgrade script can not be deduced - SSG version specific upgrade not applicable"
+    # inform user to upgrade database if necessary
+    echo "MANUAL TASK: Please update database by running sql script (located at $ssgDBDir/) manually if applicable"
+    exit 0
   fi
   releaseFromVersion=`cat $releaseFromFile`
   releaseToVersion=`cat $releaseToFile`
@@ -98,22 +95,8 @@ if [ "$option" = "doUpgrade" ]; then
     echo "INFO: Stating sgg upgrade script <$upgradeSSGFile>"
     $upgradeSSGFile
   else 
-    echo "WARNING: SSG upgrade script <$upgradeSSGFile> NOT found/run, or may not be necessary"
-  fi
-  # check for database vendor of mysql and database name of ssg from $hibernateFile
-  dbVendor=`grep -i ^hibernate.connection.url.*jdbc:mysql.*\/ssg\? $hibernateFile`
-  if [ ! "$dbVendor" = "" ]; then
-    echo "INFO: SSG is running with MySQL of database name ssg - <$dbVendor> from <$hibernateFile>"
-    # assumption - version specific upgrade file of naming convention upgrade_<releaseFromVersion>-<releaseToVersion>.sql
-    upgradeDBFile=$ssgDBDir/upgrade_$releaseFromVersion-$releaseToVersion.sql
-    # attempt to upgrade database if it's running MySQL
-    if [ -e $upgradeDBFile -a ! -d $upgradeDBFile ]; then 
-      echo "INFO: Invoking database upgrade with command \"mysql ssg < $upgradeDBFile\""
-      mysql ssg < $upgradeDBFile
-    else
-      echo "WARNING: SSG database upgrade script <$upgradeDBFile> NOT found/run, or may not be necessary" 
-    fi
-  else
-    echo "MANUAL TASK: If the database vendor is not MySQL and/or the SSG database name is not \"ssg\", database requires update manually if database upgrade applicable"
+    echo "WARNING: SSG upgrade script <$upgradeSSGFile> NOT found/run - SSG version specific upgrade not applicable"
+    # inform user to upgrade database if necessary
+    echo "MANUAL TASK: Please update database by running sql script (located at $ssgDBDir/) manually if applicable"
   fi
 fi
