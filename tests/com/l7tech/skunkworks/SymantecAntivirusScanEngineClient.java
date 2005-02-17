43dfe8df47fe46680440f7de247356609f97038a
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-02-17 18:52:45 +0000

    Fixed reading from server
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@9946 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- tests/com/l7tech/skunkworks/SymantecAntivirusScanEngineClient.java
+++ tests/com/l7tech/skunkworks/SymantecAntivirusScanEngineClient.java
@@ -15,51 +15,77 @@ import java.io.IOException;
  * $Id$
  */
 public class SymantecAntivirusScanEngineClient {
+    private static final byte[] SCAN_REQ = "RESPMOD icap://127.0.0.1:7777/AVSCAN?action=SCAN ICAP/1.0\r\n".getBytes();
 
+    private static final byte[] VIRUS = {0x58, 0x35, 0x4f, 0x21, 0x50, 0x25,
+                                         0x40, 0x41, 0x50, 0x5b, 0x34, 0x5c, 0x50, 0x5a,
+                                         0x58, 0x35, 0x34, 0x28, 0x50, 0x5e, 0x29, 0x37,
+                                         0x43, 0x43, 0x29, 0x37, 0x7d, 0x24, 0x45, 0x49,
+                                         0x43, 0x41, 0x52, 0x2d, 0x53, 0x54, 0x41, 0x4e,
+                                         0x44, 0x41, 0x52, 0x44, 0x2d, 0x41, 0x4e, 0x54,
+                                         0x49, 0x56, 0x49, 0x52, 0x55, 0x53, 0x2d, 0x54,
+                                         0x45, 0x53, 0x54, 0x2d, 0x46, 0x49, 0x4c, 0x45,
+                                         0x21, 0x24, 0x48, 0x2b, 0x48, 0x2a};
     /**
      * do the thing this class does
      * todo, a return value or exception that indicates if the content is infected
      */
-    public void scan(String scanEngineHostname, int scanEnginePort, byte[] stuffToScan, String filename) throws IOException, UnknownHostException {
-        String get = "GET http://scapi.symantec.com/" + filename + " HTTP/1.1\n\n";
-        String header = "HTTP/1.1 200 OK\n" +
-                        "Transfer-Encoding: chunked\n\n";
+    public String scan(String scanEngineHostname, int scanEnginePort, byte[] stuffToScan, String filename) throws IOException, UnknownHostException {
+        String get = "GET http://scapi.symantec.com/" + filename + " HTTP/1.0\r\n\r\n";
+        String header = "HTTP/1.0 200 OK\r\n\r\n";
 
         long bodylength = stuffToScan.length;
 
         int reshdr = get.length();
         int resbodylngt = reshdr + header.length();
 
-        String icapRequest = "OPTIONS icap://savse.com/avscan ICAP/1.0\n\n" +
-                             "RESPMOD icap://127.0.0.1:7777/AVSCAN?action=SCAN ICAP/1.0\n" +
-                             "Host: 127.0.0.1:7777\n" +
-                             "Allow: 204\n" +
-                             "Encapsulated: req-hdr=0, res-hdr=" + reshdr + ", res-body=" + resbodylngt + "\n\n" +
-                             get + header + Long.toHexString(bodylength) + "\n";
+        String icapRequest = "Host: " + scanEngineHostname + ":" + scanEnginePort + "\r\n" +
+                             "Allow: 204\r\n" +
+                             "Encapsulated: req-hdr=0, res-hdr=" + reshdr + ", res-body=" + resbodylngt + "\r\n\r\n" +
+                             get + header + Long.toHexString(bodylength) + "\r\n";
 
-        Socket socket = new Socket("localhost", 7777);
+        Socket socket = new Socket(scanEngineHostname, scanEnginePort);
+        socket.getOutputStream().write(SCAN_REQ);
         socket.getOutputStream().write(icapRequest.getBytes());
         socket.getOutputStream().write(stuffToScan);
-        socket.getOutputStream().write("\n0".getBytes());
+        socket.getOutputStream().write("\r\n0\r\n\r\n".getBytes());
 
-        byte[] returnedfromsav = new byte[2048];
-        int read = socket.getInputStream().read(returnedfromsav);
-        if (read > 0) {
-            System.out.println(new String(returnedfromsav, 0, read));
+        byte[] returnedfromsav = new byte[4096];
+        int read = 0;
+        read = socket.getInputStream().read(returnedfromsav);
+        StringBuffer output = new StringBuffer();
+        while (read > 0) {
+            output.append(new String(returnedfromsav, 0, read));
+            if (socket.getInputStream().available() <= 0) break;
+            read = socket.getInputStream().read(returnedfromsav);
         }
         socket.close();
+        return output.toString();
+
+    }
+
+    public String getSavScanEngineOptions(String scanEngineHostname, int scanEnginePort) throws IOException, UnknownHostException {
+        String req = "OPTIONS icap://savse.com/avscan ICAP/1.0\r\n\r\n";
+        Socket socket = new Socket(scanEngineHostname, scanEnginePort);
+        socket.getOutputStream().write(req.getBytes());
+        byte[] returnedfromsav = new byte[4096];
+        int read = 0;
+        read = socket.getInputStream().read(returnedfromsav);
+        StringBuffer output = new StringBuffer();
+        while (read > 0) {
+            output.append(new String(returnedfromsav, 0, read));
+            if (socket.getInputStream().available() <= 0) break;
+            read = socket.getInputStream().read(returnedfromsav);
+        }
+        socket.close();
+        return output.toString();
     }
 
     public static void main(String[] args) throws Exception {
         SymantecAntivirusScanEngineClient me = new SymantecAntivirusScanEngineClient();
-        String something = "blahblahblahblahblahblahblahblahblahblahblah" +
-                           "blahblahblahblahblahblahblahblahblahblahblah" +
-                           "blahblahblahblahblahblahblahblahblahblahblah" +
-                           "blahblahblahblahblahblahblahblahblahblahblah" +
-                           "blahblahblahblahblahblahblahblahblahblahblah" +
-                           "blahblahblahblahblahblahblahblahblahblahblah" +
-                           "blahblahblahblahblahblahblahblah";
-        String eicarVirusTest = "X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*";
-        me.scan("localhost", 7777, something.getBytes(), "blah94");
+        //String res = me.getSavScanEngineOptions("localhost", 7777);
+        String res = me.scan("localhost", 7777, VIRUS, "blah2092302392");
+        //String res = me.scan("localhost", 7777, "blahblahblah".getBytes(), "blah2092302392");
+        System.out.println("----------------VIRUS SCAN RESULT----------------\n" + res);
     }
 }
