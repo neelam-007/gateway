c1920f430173baaed5901fe705b4f2e841bd3c18
jay <jay@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-02-17 21:34:56 +0000

    Add in Bugz: 1416, restore adding tables for 3.1
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@9953 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- etc/db/mysql/upgrade_3.0-3.1.sql
+++ etc/db/mysql/upgrade_3.0-3.1.sql
@@ -1,4 +1,33 @@
 alter table internal_user add column expiration bigint(20);
 update internal_user set expiration=-1;
 alter table internal_user modify column expiration bigint(20) not null;
-alter TABLE audit_main change ip_address ip_address varchar(32) null;
+alter table audit_main change ip_address ip_address varchar(32) null;
+alter table client_cert change login login varchar(255) null;
+
+DROP TABLE IF EXISTS audit_detail;
+CREATE TABLE audit_detail (
+   objectid bigint(20) NOT NULL,
+   audit_oid bigint(20) NOT NULL,
+   time bigint(20) NOT NULL,
+   component_id integer,
+   ordinal integer,
+   message_id integer NOT NULL,
+   exception MEDIUMTEXT,
+   PRIMARY KEY (objectid),
+   KEY idx_component_id (component_id),
+   KEY idx_audit_oid (audit_oid)
+) TYPE=InnoDB;
+
+DROP TABLE IF EXISTS audit_detail_params;
+CREATE TABLE audit_detail_params (
+   audit_detail_oid bigint(20) NOT NULL,
+   position integer NOT NULL,
+   value varchar(255) NOT NULL,
+   PRIMARY KEY (audit_detail_oid, position)
+) Type=InnoDB;
+
+
+ALTER TABLE audit_system ADD component_id integer;
+UPDATE audit_system SET component_id = 1000000 WHERE component = 'GS';
+ALTER TABLE audit_system DROP component;
+CREATE INDEX idx_component_id ON audit_system (component_id);
