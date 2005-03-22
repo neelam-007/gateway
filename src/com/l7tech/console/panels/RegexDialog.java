d1425218cec34323d53bba4512051396b77e406a
emil <emil@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-03-22 15:57:28 +0000

    Regex UI From initial checkin
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@10278 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/console/panels/RegexDialog.java
@@ -0,0 +1,23 @@
+package com.l7tech.console.panels;
+
+import javax.swing.*;
+
+/**
+ * @author emil
+ * @version 22-Mar-2005
+ */
+public class RegexDialog extends JDialog {
+    private JPanel mainPanel;
+    private JButton cancelButton;
+    private JButton okButton;
+    private JTextArea regexTextArea;
+    private JScrollPane regexTextAreaScrollPane;
+    private JPanel testPanel;
+    private JTextArea replaceTextArea;
+    private JScrollPane replaceTextAreaScrollPane;
+    private JTextArea testInputTextArea;
+    private JScrollPane testInputTextAreaScrollPane;
+    private JButton testButton;
+    private JTextArea testResultTextArea;
+    private JScrollPane testResultTextAreaScrollPane;
+}
