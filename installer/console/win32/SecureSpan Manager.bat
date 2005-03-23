4444ceb73beaf173721bfca74d1b07c4a86059dd
cyeung <cyeung@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-03-23 01:07:51 +0000

    Bug 1599: jre 1.4 to 1.5
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@10294 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- installer/console/win32/SecureSpan Manager.bat
+++ installer/console/win32/SecureSpan Manager.bat
@@ -1,3 +1,3 @@
-@j2re1.4.2\bin\java -jar Manager.jar
+@jre1.5.0_02\bin\java -jar Manager.jar
 @echo Layer 7 SecureSpan Manager has finished.  Its final log messages are shown above.
 @pause
--- installer/proxy/win32/SecureSpan Bridge in Text Mode.bat
+++ installer/proxy/win32/SecureSpan Bridge in Text Mode.bat
@@ -1,1 +1,1 @@
-@j2re1.4.2\bin\java -classpath Bridge.jar com.l7tech.proxy.Main
+@jre1.5.0_02\bin\java -classpath Bridge.jar com.l7tech.proxy.Main
