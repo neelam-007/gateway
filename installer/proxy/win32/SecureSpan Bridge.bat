ad6c5438d6958ab856a46c1a3c2f75c05fdd81df
cyeung <cyeung@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-03-23 01:03:30 +0000

    Bug 1599 - jre 1.4 to 1.5
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@10293 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- installer/proxy/win32/SecureSpan Bridge.bat
+++ installer/proxy/win32/SecureSpan Bridge.bat
@@ -1,3 +1,3 @@
-@j2re1.4.2\bin\java -server -Dcom.l7tech.proxy.listener.maxthreads=300 -jar Bridge.jar
+@jre1.5.0_02\bin\java -Dcom.l7tech.proxy.listener.maxthreads=300 -jar Bridge.jar
 @echo Layer 7 SecureSpan Bridge has finished.  Its final log messages are shown above.
 @pause
