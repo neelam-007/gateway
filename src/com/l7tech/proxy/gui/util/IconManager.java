6a3a98ef21651191d9782ebe73f5db4aa62551f2
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-04-05 04:17:23 +0000

    Rolled back change to onscreen diagrams -- the new images weren't ready to use yet.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@10369 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/proxy/gui/util/IconManager.java
+++ src/com/l7tech/proxy/gui/util/IconManager.java
@@ -40,19 +40,23 @@ public class IconManager {
 
     public static ImageIcon getTrustedSsgDiagram() {
         if (trustedSsgDiagram == null)
-            trustedSsgDiagram = loadImageIcon(Gui.RESOURCE_PATH + "/dia_trusted_ssg.gif");
+            trustedSsgDiagram = loadImageIcon(Gui.RESOURCE_PATH + "/dia_trusted_ssg.png");
         return trustedSsgDiagram;
     }
 
     public static ImageIcon getFederatedSsgDiagram() {
         if (federatedSsgDiagram == null)
-            federatedSsgDiagram = loadImageIcon(Gui.RESOURCE_PATH + "/dia_federated_ssg.gif");
+            federatedSsgDiagram = loadImageIcon(Gui.RESOURCE_PATH + "/dia_federated_ssg.png");
         return federatedSsgDiagram;
     }
 
     public static ImageIcon getFederatedSsgWithTokenServiceDiagram() {
-        if (federatedSsgWithTokenServiceDiagram == null)
-            federatedSsgWithTokenServiceDiagram = loadImageIcon(Gui.RESOURCE_PATH + "/dia_federated_ssg_with_tokenservice.gif");
+        if (federatedSsgWithTokenServiceDiagram == null) {
+            String path = "/dia_federated_ssg_with_tokenservice.png";
+            if (Boolean.getBoolean("noibm") || Boolean.getBoolean("interoperability") || Boolean.getBoolean("interop"))
+                path = "/dia_federated_ssg_with_tokenservice_noibm.png";
+            federatedSsgWithTokenServiceDiagram = loadImageIcon(Gui.RESOURCE_PATH + path);
+        }
         return federatedSsgWithTokenServiceDiagram;
     }
 
