fb3ae4b37b25b8ecb9f5781a4f2b9bb8fdef320f
emil <emil@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-04-07 20:27:06 +0000

    Merged from rel3_1 branch
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@10433 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/spring/remoting/rmi/AdminSessionInvocationFactory.java
+++ src/com/l7tech/spring/remoting/rmi/AdminSessionInvocationFactory.java
@@ -5,16 +5,20 @@
  */
 package com.l7tech.spring.remoting.rmi;
 
-import org.springframework.remoting.support.RemoteInvocationFactory;
-import org.springframework.remoting.support.RemoteInvocation;
 import org.aopalliance.intercept.MethodInvocation;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.remoting.support.RemoteInvocation;
+import org.springframework.remoting.support.RemoteInvocationFactory;
 
 import javax.security.auth.Subject;
-import java.security.SecureRandom;
-import java.security.AccessController;
 import java.io.Serializable;
+import java.security.AccessController;
+import java.security.Principal;
+import java.util.Set;
+import java.util.Collection;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * @author emil
@@ -24,8 +28,28 @@ final class AdminSessionInvocationFactory implements RemoteInvocationFactory, Se
     protected final Log logger = LogFactory.getLog(getClass());
 
     public RemoteInvocation createRemoteInvocation(MethodInvocation methodInvocation) {
-        logger.info("createRemoteInvocation");
-        return new AdminSessionRemoteInvocation(methodInvocation, Subject.getSubject(AccessController.getContext()));
+        logger.debug("createRemoteInvocation for method: '"+methodInvocation.getMethod().getName()+"'");
+        final Subject subject = Subject.getSubject(AccessController.getContext());
+        if (subject == null) {
+            logger.debug("No Subject in the current calling context (subject == null)");
+        } else {
+            logger.debug("Using Subject(s) : "+getPrincipalNames(subject.getPrincipals()));
+        }
+        return new AdminSessionRemoteInvocation(methodInvocation, subject);
+    }
+
+    private String getPrincipalNames(Set principals) {
+        Collection principalNames = new ArrayList();
+        for (Iterator iterator = principals.iterator(); iterator.hasNext();) {
+            Object o = (Object)iterator.next();
+            if (o instanceof Principal) {
+                Principal principal = (Principal)o;
+                principalNames.add(principal.getName());
+            } else {
+                principalNames.add(o.toString());
+            }
+        }
+        return principalNames.toString();
     }
 
 }
