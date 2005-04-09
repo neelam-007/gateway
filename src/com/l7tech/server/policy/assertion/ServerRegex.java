dfcf715dcf9d82c385b4fb1a61b7c44da6e84b25
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-04-09 00:38:13 +0000

    Make replacement in specified MIME part (merged from FFIC branch)
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@10463 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/server/policy/assertion/ServerRegex.java
+++ src/com/l7tech/server/policy/assertion/ServerRegex.java
@@ -1,26 +1,33 @@
 /*
  * Copyright (C) 2003 Layer 7 Technologies Inc.
  *
+<<<<<<< ServerRegex.java
  * $Id$
+=======
+ * $Id$
+>>>>>>> 1.5.4.3
  */
 package com.l7tech.server.policy.assertion;
 
-import com.l7tech.policy.assertion.*;
-import com.l7tech.policy.ExpandVariables;
-import com.l7tech.server.message.PolicyEnforcementContext;
 import com.l7tech.common.audit.Auditor;
 import com.l7tech.common.audit.AssertionMessages;
-import com.l7tech.common.mime.PartInfo;
 import com.l7tech.common.mime.NoSuchPartException;
+import com.l7tech.common.mime.PartInfo;
 import com.l7tech.common.util.HexUtils;
+import com.l7tech.policy.assertion.AssertionStatus;
+import com.l7tech.policy.assertion.PolicyAssertionException;
+import com.l7tech.policy.assertion.Regex;
+import com.l7tech.policy.assertion.RoutingStatus;
+import com.l7tech.policy.ExpandVariables;
+import com.l7tech.server.message.PolicyEnforcementContext;
+import org.springframework.context.ApplicationContext;
 
 import java.io.IOException;
-import java.util.logging.Logger;
+import java.io.InputStream;
 import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
-
-import org.springframework.context.ApplicationContext;
 
 /**
  * The Server side Regex Assertion
@@ -64,29 +71,36 @@ public class ServerRegex implements ServerAssertion {
             return AssertionStatus.FALSIFIED;
         }
 
+        int whichPart = regexAssertion.getMimePart();
         try {
-            PartInfo firstPart = isPostRouting(context) ?
-              context.getResponse().getMimeKnob().getFirstPart() :
-              context.getRequest().getMimeKnob().getFirstPart();
+
+            PartInfo messagePart = isPostRouting(context) ?
+              context.getResponse().getMimeKnob().getPart(whichPart) :
+              context.getRequest().getMimeKnob().getPart(whichPart);
 
             String replacement = regexAssertion.getReplacement();
             final boolean isReplacement = regexAssertion.isReplace();
 
             // replacement set and replacement null
             if (isReplacement && replacement == null) {
-                final String msg = "Replace requested, and no replace string specified (null).";
-                logger.log(Level.FINE, msg);
-                throw new PolicyAssertionException(msg);
+                auditor.logAndAudit(AssertionMessages.REGEX_NO_REPLACEMENT);
+                throw new PolicyAssertionException(AssertionMessages.REGEX_NO_REPLACEMENT.getMessage());
             }
 
-            byte[] message = HexUtils.slurpStream(firstPart.getInputStream(isReplacement));
-            final String encoding = firstPart.getContentType().getEncoding();
+            InputStream is = messagePart.getInputStream(false);
+            byte[] message = HexUtils.slurpStream(is, Regex.MAX_LENGTH);
+            if (message.length == Regex.MAX_LENGTH) {
+                auditor.logAndAudit(AssertionMessages.REGEX_TOO_BIG);
+                return AssertionStatus.FAILED;
+            }
+
+            final String encoding = messagePart.getContentType().getEncoding();
             Matcher matcher = regexPattern.matcher(new String(message, encoding));
             if (isReplacement) {
                 logger.log(Level.FINE, "Replace requested: Match pattern '{0}', replace pattern '{1}'", new Object[]{regexAssertion.getRegex(), replacement});
                 replacement = expandVariables.process(replacement, context.getVariables());
                 String result = matcher.replaceAll(replacement);
-                firstPart.setBodyBytes(result.getBytes(encoding));
+                messagePart.setBodyBytes(result.getBytes(encoding));
             } else {
                 logger.fine("Verifying match for pattern " + regexAssertion.getRegex());
                 if (!matcher.find()) {
@@ -96,7 +110,7 @@ public class ServerRegex implements ServerAssertion {
             }
             return AssertionStatus.NONE;
         } catch (NoSuchPartException e) {
-            auditor.logAndAudit(AssertionMessages.EXCEPTION_SEVERE, null, e);
+            auditor.logAndAudit(AssertionMessages.REGEX_NO_SUCH_PART, new String[] { Integer.toString(whichPart) });
             return AssertionStatus.FAILED;
         } catch (ExpandVariables.VariableNotFoundException e) {
             auditor.logAndAudit(AssertionMessages.EXCEPTION_SEVERE, null, e);
