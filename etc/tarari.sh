a40484910cce048177d26cc83b96dd798e2dc14e
jay <jay@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-04-13 22:16:12 +0000

    Config tarari if installed
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@10512 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ etc/tarari.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+if [ -e  /usr/local/Tarari ]; then
+	export TARARIROOT=/usr/local/Tarari
+	export PATH=$TARARIROOT/bin:$PATH
+	export LD_LIBRARY_PATH=$TARARIROOT/lib:$LD_LIBRARY_PATH
+	export KERNELSOURCE=/usr/src/linux
+	export JAVA_OPTS="-Dcom.l7tech.common.xml.tarari.enable=true $JAVA_OPTS"fi
