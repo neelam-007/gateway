c00962aec7d01e54f068e9fd33aac70dce410a33
jay <jay@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-04-13 22:31:17 +0000

    Line wrap oogly
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@10517 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- etc/tarari.sh
+++ etc/tarari.sh
@@ -4,4 +4,5 @@ if [ -e  /usr/local/Tarari ]; then
 	export PATH=$TARARIROOT/bin:$PATH
 	export LD_LIBRARY_PATH=$TARARIROOT/lib:$LD_LIBRARY_PATH
 	export KERNELSOURCE=/usr/src/linux
-	export JAVA_OPTS="-Dcom.l7tech.common.xml.tarari.enable=true $JAVA_OPTS"fi
+	export JAVA_OPTS="-Dcom.l7tech.common.xml.tarari.enable=true $JAVA_OPTS"
+fi
