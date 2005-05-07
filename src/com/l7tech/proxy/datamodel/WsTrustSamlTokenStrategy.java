11fe005658a43fb8c9d19a0c0283eb52d5b11d66
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-05-07 02:25:46 +0000

    Switching request type to ISSUE from VALIDATE when getting a SAML token from a FIM server.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@10839 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/proxy/datamodel/WsTrustSamlTokenStrategy.java
+++ src/com/l7tech/proxy/datamodel/WsTrustSamlTokenStrategy.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2004 Layer 7 Technologies Inc.
  *
- * $Id$
+ * 
  */
 
 package com.l7tech.proxy.datamodel;
@@ -107,7 +107,7 @@ public class WsTrustSamlTokenStrategy extends AbstractSamlTokenStrategy implemen
                                                    null, // not overriding timestamp created date
                                                    null, // no client cert (not signing message)
                                                    null, // no client private key (not signing message)
-                                                   WsTrustRequestType.VALIDATE,
+                                                   WsTrustRequestType.ISSUE,
                                                    null, // no token type (FIM doesn't like it)
                                                    usernameToken,
                                                    getAppliesTo(),
