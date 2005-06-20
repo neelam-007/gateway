d56a7b0e868b3747d84709bf1a3acbe38f416cca
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-06-20 17:56:06 +0000

    DB schema update script for upgrade 3.1 to 3.2 [bugzilla #1862].
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@11101 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ etc/db/mysql/upgrade_3.1-3.2.sql
@@ -0,0 +1,18 @@
+ALTER TABLE audit_message ADD COLUMN operation_name varchar(255);
+ALTER TABLE audit_message ADD COLUMN response_status int(11);
+ALTER TABLE audit_message ADD COLUMN routing_latency int(11);
+
+DROP TABLE IF EXISTS counters;
+CREATE TABLE counters (
+  counterid bigint(20) NOT NULL,
+  userid varchar(128),
+  providerid bigint(20) NOT NULL,
+  countername varchar(128) NOT NULL,
+  cnt_sec bigint(20) default 0,
+  cnt_hr bigint(20) default 0,
+  cnt_day bigint(20) default 0,
+  cnt_mnt bigint(20) default 0,
+  last_update bigint(20) default 0,
+  unique(userid, providerid, countername),
+  PRIMARY KEY (counterid)
+) TYPE=InnoDB;
