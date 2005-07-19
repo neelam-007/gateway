b67a372388872834d8617c59747a47c97fabbad8
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-07-19 00:12:49 +0000

    first call to findAll populates table with default soapenv schema is table is empty
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@11225 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/server/communityschemas/CommunitySchemaManager.java
+++ src/com/l7tech/server/communityschemas/CommunitySchemaManager.java
@@ -9,7 +9,9 @@ package com.l7tech.server.communityschemas;
 import org.springframework.orm.hibernate.support.HibernateDaoSupport;
 
 import java.util.Collection;
+import java.util.ArrayList;
 import java.util.logging.Logger;
+import java.util.logging.Level;
 
 import com.l7tech.objectmodel.FindException;
 import com.l7tech.objectmodel.SaveException;
@@ -30,6 +32,18 @@ public class CommunitySchemaManager extends HibernateDaoSupport {
     public Collection findAll() throws FindException {
         String queryall = "from " + TABLE_NAME + " in class " + CommunitySchemaEntry.class.getName();
         Collection output = getHibernateTemplate().find(queryall);
+        if (output.isEmpty()) {
+            CommunitySchemaEntry defaultEntry = new CommunitySchemaEntry();
+            defaultEntry.setSchema(SOAP_SCHEMA);
+            try {
+                save(defaultEntry);
+            } catch (SaveException e) {
+                logger.log(Level.WARNING, "cannot save default soap xsd", e);
+            }
+            output = new ArrayList();
+            output.add(defaultEntry);
+        }
+
         return output;
     }
 
@@ -43,4 +57,80 @@ public class CommunitySchemaManager extends HibernateDaoSupport {
 
     private static final String TABLE_NAME = "community_schema";
     private final Logger logger = Logger.getLogger(CommunitySchemaManager.class.getName());
+    private static final String SOAP_SCHEMA = "<?xml version='1.0' encoding='UTF-8' ?>\n" +
+            "<xs:schema xmlns:xs=\"http://www.w3.org/2001/XMLSchema\"\n" +
+            "           xmlns:tns=\"http://schemas.xmlsoap.org/soap/envelope/\"\n" +
+            "           targetNamespace=\"http://schemas.xmlsoap.org/soap/envelope/\" >\n" +
+            "  <!-- Envelope, header and body -->\n" +
+            "  <xs:element name=\"Envelope\" type=\"tns:Envelope\" />\n" +
+            "  <xs:complexType name=\"Envelope\" >\n" +
+            "    <xs:sequence>\n" +
+            "      <xs:element ref=\"tns:Header\" minOccurs=\"0\" />\n" +
+            "      <xs:element ref=\"tns:Body\" minOccurs=\"1\" />\n" +
+            "      <xs:any namespace=\"##other\" minOccurs=\"0\" maxOccurs=\"unbounded\" processContents=\"lax\" />\n" +
+            "    </xs:sequence>\n" +
+            "    <xs:anyAttribute namespace=\"##other\" processContents=\"lax\" />\n" +
+            "  </xs:complexType>\n" +
+            "  <xs:element name=\"Header\" type=\"tns:Header\" />\n" +
+            "  <xs:complexType name=\"Header\" >\n" +
+            "    <xs:sequence>\n" +
+            "      <xs:any namespace=\"##other\" minOccurs=\"0\" maxOccurs=\"unbounded\" processContents=\"lax\" />\n" +
+            "    </xs:sequence>\n" +
+            "    <xs:anyAttribute namespace=\"##other\" processContents=\"lax\" />\n" +
+            "  </xs:complexType>\n" +
+            "  <xs:element name=\"Body\" type=\"tns:Body\" />\n" +
+            "  <xs:complexType name=\"Body\" >\n" +
+            "    <xs:sequence>\n" +
+            "      <xs:any namespace=\"##any\" minOccurs=\"0\" maxOccurs=\"unbounded\" processContents=\"lax\" />\n" +
+            "    </xs:sequence>\n" +
+            "    <xs:anyAttribute namespace=\"##any\" processContents=\"lax\" >\n" +
+            "          <xs:annotation>\n" +
+            "            <xs:documentation>\n" +
+            "                  Prose in the spec does not specify that attributes are allowed on the Body element\n" +
+            "                </xs:documentation>\n" +
+            "          </xs:annotation>\n" +
+            "        </xs:anyAttribute>\n" +
+            "  </xs:complexType>\n" +
+            "  <!-- Global Attributes.  The following attributes are intended to be usable via qualified attribute names on any complex type referencing them.  -->\n" +
+            "  <xs:attribute name=\"mustUnderstand\" >\n" +
+            "     <xs:simpleType>\n" +
+            "     <xs:restriction base='xs:boolean'>\n" +
+            "           <xs:pattern value='0|1' />\n" +
+            "         </xs:restriction>\n" +
+            "   </xs:simpleType>\n" +
+            "  </xs:attribute>\n" +
+            "  <xs:attribute name=\"actor\" type=\"xs:anyURI\" />\n" +
+            "  <xs:simpleType name=\"encodingStyle\" >\n" +
+            "    <xs:annotation>\n" +
+            "          <xs:documentation>\n" +
+            "            'encodingStyle' indicates any canonicalization conventions followed in the contents of the containing element.  For example, the value 'http://schemas.xmlsoap.org/soap/encoding/' indicates the pattern described in SOAP specification\n" +
+            "          </xs:documentation>\n" +
+            "        </xs:annotation>\n" +
+            "    <xs:list itemType=\"xs:anyURI\" />\n" +
+            "  </xs:simpleType>\n" +
+            "  <xs:attribute name=\"encodingStyle\" type=\"tns:encodingStyle\" />\n" +
+            "  <xs:attributeGroup name=\"encodingStyle\" >\n" +
+            "    <xs:attribute ref=\"tns:encodingStyle\" />\n" +
+            "  </xs:attributeGroup>" +
+            "  <xs:element name=\"Fault\" type=\"tns:Fault\" />\n" +
+            "  <xs:complexType name=\"Fault\" final=\"extension\" >\n" +
+            "    <xs:annotation>\n" +
+            "          <xs:documentation>\n" +
+            "            Fault reporting structure\n" +
+            "          </xs:documentation>\n" +
+            "        </xs:annotation>\n" +
+            "    <xs:sequence>\n" +
+            "      <xs:element name=\"faultcode\" type=\"xs:QName\" />\n" +
+            "      <xs:element name=\"faultstring\" type=\"xs:string\" />\n" +
+            "      <xs:element name=\"faultactor\" type=\"xs:anyURI\" minOccurs=\"0\" />\n" +
+            "      <xs:element name=\"detail\" type=\"tns:detail\" minOccurs=\"0\" />\n" +
+            "    </xs:sequence>\n" +
+            "  </xs:complexType>\n" +
+            "  <xs:complexType name=\"detail\">\n" +
+            "    <xs:sequence>\n" +
+            "      <xs:any namespace=\"##any\" minOccurs=\"0\" maxOccurs=\"unbounded\" processContents=\"lax\" />\n" +
+            "    </xs:sequence>\n" +
+            "    <xs:anyAttribute namespace=\"##any\" processContents=\"lax\" />\n" +
+            "  </xs:complexType>\n" +
+            "</xs:schema>";
 }
