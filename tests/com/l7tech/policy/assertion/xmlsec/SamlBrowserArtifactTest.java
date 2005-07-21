d563a1864c9136b1cdb26499d6e93c006f26cd21
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-07-21 23:34:25 +0000

    SamlBrowserArtifactTest
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@11239 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ tests/com/l7tech/policy/assertion/xmlsec/SamlBrowserArtifactTest.java
@@ -0,0 +1,53 @@
+package com.l7tech.policy.assertion.xmlsec;
+
+import com.l7tech.common.ApplicationContexts;
+import com.l7tech.common.http.GenericHttpClient;
+import com.l7tech.common.message.Message;
+import com.l7tech.policy.assertion.credential.LoginCredentials;
+import com.l7tech.policy.assertion.credential.http.HttpBasic;
+import com.l7tech.server.message.PolicyEnforcementContext;
+import com.l7tech.server.policy.assertion.xmlsec.ServerSamlBrowserArtifact;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.springframework.context.ApplicationContext;
+
+import java.util.logging.Logger;
+
+/**
+ * Test Assertion/CompositeAssertion data structure management.
+ */
+public class SamlBrowserArtifactTest extends TestCase {
+    private static Logger log = Logger.getLogger(SamlBrowserArtifactTest.class.getName());
+
+    public SamlBrowserArtifactTest(String name) {
+        super(name);
+    }
+
+    public static Test suite() {
+        return new TestSuite(SamlBrowserArtifactTest.class);
+    }
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public void testStuff() throws Exception {
+        SamlBrowserArtifact sba = new SamlBrowserArtifact();
+            sba.setSsoEndpointUrl("https://redroom.l7tech.com/FIM/sps/l7fed/saml/login?TARGET=https://redroom.l7tech.com/FIM/sps/l7fed-sp/saml/login");
+//        HashMap extraFields = new HashMap();
+//        extraFields.put("login-form-type", "pwd");
+//        sba.setExtraFields(extraFields);
+//        sba.setUsernameFieldname("username");
+//        sba.setPasswordFieldname("password");
+//        sba.setMethod(GenericHttpClient.METHOD_GET);
+
+        ApplicationContext spring = ApplicationContexts.getTestApplicationContext();
+        ServerSamlBrowserArtifact ssba = new ServerSamlBrowserArtifact(sba, spring);
+
+        PolicyEnforcementContext pec = new PolicyEnforcementContext(new Message(), new Message());
+        pec.setCredentials(LoginCredentials.makePasswordCredentials("testuser", "passw0rd".toCharArray(), HttpBasic.class));
+        ssba.checkRequest(pec);
+    }
+
+}
