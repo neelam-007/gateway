c0f935749bebe3fa2f786ce3ad39838968c6af8e
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-08-11 23:21:23 +0000

    An org.apache.catalina.Valve implementation that allows servlets to tell the container to kill a connection without sending standard response including http status, etc.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@11384 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/server/tomcat/valve/ResponseKillerValve.java
@@ -0,0 +1,62 @@
+/**
+ * LAYER 7 TECHNOLOGIES, INC<br/>
+ *
+ * User: flascell<br/>
+ * Date: Aug 11, 2005<br/>
+ */
+package com.l7tech.server.tomcat.valve;
+
+import org.apache.catalina.valves.ValveBase;
+import org.apache.catalina.connector.Request;
+import org.apache.catalina.connector.Response;
+import org.apache.coyote.http11.InternalOutputBuffer;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+/**
+ * <p>
+ * An org.apache.catalina.Valve implementation that allows servlets to tell the container to
+ * kill a connection without sending standard response including http status, etc.
+ * <p>
+ * A servlet tells this valve to kill the response connection by adding a special attribute to teh request object:
+ * HttpServletRequest.setAttribute(ResponseKillerValve.ATTRIBUTE_FLAG_NAME, "foo");
+ * <p>
+ * This package should be jared and dropped in $TOMCAT_HOME/server/lib
+ * Server.xml should refer to this valve by adding the following element as a child of Host:
+ * &lt;Valve className="com.l7tech.server.tomcat.valve.ResponseKillerValve"/&gt;
+ *
+ * @author flascelles@layer7-tech.com, acruise@layer7-tech.com
+ */
+public class ResponseKillerValve extends ValveBase {
+    public static final String ATTRIBUTE_FLAG_NAME = "killmenow";
+    private final Logger logger = Logger.getLogger(ResponseKillerValve.class.getName());
+
+    public void invoke(Request req, Response res) throws IOException, ServletException {
+        InternalOutputBuffer iob = null;
+        try {
+            iob = (InternalOutputBuffer)res.getCoyoteResponse().getOutputBuffer();
+        } catch (Exception e) {
+            logger.log(Level.SEVERE,
+                       "Unexpected type returned by res.getCoyoteResponse().getOutputBuffer()",
+                       e);
+        }
+
+        // Let servlet do it's thing
+        getNext().invoke(req, res);
+
+        // Check for special flag
+        if (req.getAttribute(ATTRIBUTE_FLAG_NAME) != null && iob != null) {
+            logger.info("Killing response");
+            try {
+                iob.getOutputStream().close();
+            } catch (Exception e) {
+                logger.log(Level.SEVERE,
+                       "Error closing response output buffer",
+                       e);
+            }
+        }
+    }
+}
