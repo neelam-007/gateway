addeb6791f4f73f06dab3902fbe20ced0b98b694
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-08-11 19:02:53 +0000

    How to close the response output stream without any output
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@11379 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- tests/com/l7tech/skunkworks/FooValve.java
+++ tests/com/l7tech/skunkworks/FooValve.java
@@ -8,6 +8,13 @@ import org.apache.coyote.http11.InternalOutputBuffer;
 import javax.servlet.ServletException;
 import java.io.IOException;
 
+/**
+ * This is a proof-of-concept Tomcat valve that kills every request without sending any response. :)
+ * <p>
+ * Valve classes have to be in $TOMCAT_HOME/server/classes or $TOMCAT_HOME/server/lib.
+ * <p>
+ * Install by adding &lt;Valve className="com.l7tech.skunkworks.FooValve"/&gt; as a child of //Host in $TOMCAT_HOME/conf/server.xml.
+ */
 public class FooValve extends ValveBase {
     public void invoke(Request request, Response response) throws IOException, ServletException {
         InternalOutputBuffer iob = (InternalOutputBuffer)response.getCoyoteResponse().getOutputBuffer();
