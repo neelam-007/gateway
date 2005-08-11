f664e77550417dd58f208f41ce0a6d7642ac6c68
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-08-11 23:01:16 +0000

    added new table for global schemas
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@11383 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- etc/db/mysql/upgrade_3.2-3.3.sql
+++ etc/db/mysql/upgrade_3.2-3.3.sql
@@ -4,3 +4,11 @@
 --- Layer 7 Technologies, inc
 
 ALTER TABLE trusted_cert ADD COLUMN trusted_as_saml_attesting_entity tinyint(1) default '0';
+
+CREATE TABLE community_schemas (
+  objectid bigint(20) NOT NULL,
+  name varchar(128) default '',
+  tns varchar(128) default '',
+  schema mediumtext default '',
+  PRIMARY KEY (objectid)
+) TYPE=InnoDB;
