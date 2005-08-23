d530d483c0ebf985f715f55d0ddb6c8439ad4766
rmak <rmak@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-08-23 20:19:38 +0000

    Added new command batch file for Windows XP/Server 2003 equivalence to ssgruntimedefs.sh.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@11459 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ etc/ssgruntimedefs.cmd
@@ -0,0 +1,71 @@
+:: Copyright (C) 2005 Layer 7 Technologies Inc.
+::
+:: $Id$
+::
+:: Compatibility: Windows XP, Windows Server 2003.
+
+:: This file must not be moved from its location in <SSG home folder>\bin
+:: because runtime determination of the SSG home folder depends on its assumed
+:: location.
+
+:: Compute the SSG home folder absolute path (without trailing backslash!).
+pushd %~dp0\..
+set SSG_HOME=%CD%
+popd
+
+set JAVA_HOME=%SSG_HOME%\jdk
+set TOMCAT_HOME=%SSG_HOME%\tomcat
+
+:: Unset environment variables that we don't want to inherit from outside.
+set CATALINA_HOME=
+set CATALINA_BASE=
+set CATALINA_OPTS=
+set CATALINA_TMPDIR=
+set JSSE_HOME=
+set JPDA_TRANSPORT=
+set JPDA_ADDRESS=
+
+:: Compute system physical RAM amount for tunning JVM.
+for /F "delims=: tokens=1,2" %%i in ('systeminfo') do if "%%i"=="Total Physical Memory" set tmpstr1=%%j
+for /F "tokens=1" %%i in ("%tmpstr1%") do set tmpstr2=%%i
+set system_ram=%tmpstr2:,=%
+set /a java_ram=%system_ram%*2/3
+set /a maxnewsize=%java_ram%/2
+
+
+set JAVA_OPTS=^
+-Dcom.l7tech.server.home="%SSG_HOME%" ^
+-Dfile.encoding=UTF-8 ^
+-Dnetworkaddress.cache.ttl=30 ^
+-Xms%java_ram%M ^
+-Xmx%java_ram%M ^
+-Xss256k ^
+-server ^
+-Djava.awt.headless=true ^
+-XX:CompileThreshold=1500 ^
+-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.Jdk14Logger ^
+-XX:NewSize=%maxnewsize%M ^
+-XX:MaxNewSize=%maxnewsize%M ^
+-XX:+DisableExplicitGC 
+
+:: Tune for single or multi processor machine.
+if %NUMBER_OF_PROCESSORS%==1 (
+    set JAVA_OPTS=%JAVA_OPTS% -XX:+DisableExplicitGC
+) else (
+    set JAVA_OPTS=%JAVA_OPTS% -XX:+DisableExplicitGC -XX:+UseParallelGC
+)
+
+:: Set the fully qualified host name of the RMI server.
+for /F "tokens=1" %%i in ('hostname') do set short_hostname=%%i
+for /F "tokens=1,2" %%i in ('nslookup %short_hostname%') do if "%%i"=="Name:" set full_hostname=%%j
+if exist "%SSG_HOME%\etc\conf\cluster_hostname" (
+    for /F "usebackq tokens=1" %%i in ("%SSG_HOME%\etc\conf\cluster_hostname") do set cluster_hostname=%%i
+)
+if exist "%SSG_HOME%\etc\conf\cluster_hostname" (
+    set JAVA_OPTS=%JAVA_OPTS% -Djava.rmi.server.hostname=%cluster_hostname%
+) else (
+    set JAVA_OPTS=%JAVA_OPTS% -Djava.rmi.server.hostname=%full_hostname%
+)
+
+:: Append to the system search path.
+set PATH=%PATH%;%JAVA_HOME%\bin;%SSG_HOME%\bin
