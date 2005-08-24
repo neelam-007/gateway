08e1e24d8e5c3ae936cebe814c43aa99f93171cb
rmak <rmak@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-08-24 20:41:17 +0000

    Added timestamp to stdout messages.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@11479 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- etc/ssg.cmd
+++ etc/ssg.cmd
@@ -1,7 +1,5 @@
 :: Copyright (C) 2005 Layer 7 Technologies Inc.
 ::
-:: $Id$
-::
 :: Compatibility: Windows XP, Windows Server 2003.
 
 :: Startup/Shutdown script for the SecureSpan Gateway server.
@@ -9,6 +7,10 @@
 @echo off
 
 setlocal
+
+for /f "tokens=2-4 delims=/ " %%i in ('date /t') do set month=%%i&set day=%%j&set year=%%k
+for /f "tokens=1-3 delims=: " %%i in ('time /t') do set hour=%%i&set minute=%%j&set ampm=%%k
+set dateTime=%year%-%month%-%day% %hour%:%minute% %ampm%
 
 if ""%1"" == ""start""   goto doStart
 if ""%1"" == ""stop""    goto doStop
@@ -23,36 +25,36 @@ goto end
 call "%~dp0\ssgruntimedefs.cmd"
 set ssgRunning=0
 pushd "%SSG_HOME%"
-for /F "tokens=*" %%i in ('"bin\process.exe --quiet --fields=exeFull"') do if "%%i"=="%JAVA_HOME%\bin\java.exe" set ssgRunning=1
+for /f "tokens=*" %%i in ('"bin\process.exe --quiet --fields=exeFull"') do if "%%i"=="%JAVA_HOME%\bin\java.exe" set ssgRunning=1
 popd
 if %ssgRunning%==1 goto dontStart
-echo Starting SSG...
+echo [%dateTime%] Starting SSG...
 pushd "%TOMCAT_HOME%\bin"
 call startup.bat
 popd
 goto end
 
 :dontStart
-echo SSG Already Running
+echo [%dateTime%] SSG Already Running
 goto end
 
 :doStop
 call "%~dp0\ssgruntimedefs.cmd"
 set ssgRunning=0
 pushd "%SSG_HOME%"
-for /F "tokens=*" %%i in ('"bin\process.exe --quiet --fields=exeFull"') do if "%%i"=="%JAVA_HOME%\bin\java.exe" set ssgRunning=1
+for /f "tokens=*" %%i in ('"bin\process.exe --quiet --fields=exeFull"') do if "%%i"=="%JAVA_HOME%\bin\java.exe" set ssgRunning=1
 popd
 if %ssgRunning%==0 goto dontStop
-echo Stopping SSG...
+echo [%dateTime%] Stopping SSG...
 pushd "%TOMCAT_HOME%\bin"
 call shutdown.bat
-:: If that doesn't stop in 10 seconds, force a kill.
-"%SSG_HOME%\bin\killproc.exe" --wait=10000 --ignoreCase "%JAVA_HOME%\bin\java.exe"
+:: If that doesn't stop in 12 seconds, force a kill.
+"%SSG_HOME%\bin\killproc.exe" --wait=12000 --ignoreCase "%JAVA_HOME%\bin\java.exe"
 popd
 goto end
 
 :dontStop
-echo SSG Not Running
+echo [%dateTime%] SSG Not Running
 goto end
 
 :end
