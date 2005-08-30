ec7e4d62a5a14f6b3d941d13b4049e3f2ec3cf2f
megery <megery@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-08-30 23:18:28 +0000

    new Handler which logs everything to three in memory lists
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@11553 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/server/config/ListHandler.java
@@ -0,0 +1,56 @@
+package com.l7tech.server.config;
+
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+import java.util.logging.Level;
+import java.util.ArrayList;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: megery
+ * Date: Aug 30, 2005
+ * Time: 2:36:07 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ListHandler extends Handler {
+    private static ArrayList severeLogList = new ArrayList();
+    private static ArrayList warningLogList = new ArrayList();
+    private static ArrayList infoLogList = new ArrayList();
+
+
+    public synchronized void publish(LogRecord logRecord) {
+        if (logRecord != null) {
+            Level level = logRecord.getLevel();
+            if (level == Level.SEVERE) {
+                severeLogList.add(level + ": " + logRecord.getMessage());
+            }
+            else if (level == Level.WARNING) {
+                warningLogList.add(level + ": " + logRecord.getMessage());
+            }
+            else { //(level == Level.INFO) or other wise
+                infoLogList.add(level + ": " + logRecord.getMessage());
+            }
+        }
+    }
+
+    public void flush() {
+        severeLogList.clear();
+        warningLogList.clear();
+        infoLogList.clear();
+    }
+
+    public void close() throws SecurityException {
+    }
+
+    public static ArrayList getSevereLogList() {
+        return severeLogList;
+    }
+
+    public static ArrayList getWarningLogList() {
+        return warningLogList;
+    }
+
+    public static ArrayList getInfoLogList() {
+        return infoLogList;
+    }
+}
