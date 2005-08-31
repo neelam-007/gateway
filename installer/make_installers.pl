3264ab25fea376595499f7b2eaca52c3a19e334c
cyeung <cyeung@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-08-31 00:00:19 +0000

    rollback rsync to copy, only use rsync for windows exe build on linux file server
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@11567 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- installer/make_installers.pl
+++ installer/make_installers.pl
@@ -152,9 +152,13 @@ sub build_installer {
 
 sub docopy {
 	my ($src, $dst) = @_;
-        # use resync instead of copy to be flexible to perform cross platform build - rsync provides modify-window options to work around NT and FAT don't store time to 1 second precision, making it it believe a source file (on Windows) and a destination file (on UNIX) had the same date
-        print "Rsync'ing(copying) $src -> $dst\n";
-        system("rsync --modify-window=2 $src $dst") or die "Unable to rsync $src to $dst: $!";
+        print "Copying $src -> $dst\n"; 
+        copy($src, $dst) or die "Unable to copy $src to $dst: $!";
+        # for building NSIS exe on window, 
+        # please comment out copy, and replace with rsync
+        # use resync instead of copy to be flexible to perform cross platform build - rsync provides modify-window options to work around NT and FAT don't store time to 1 second precision, making it it believe a source file (on Windows) and a destination file (on UNIX) had the same date 
+        # print "Rsync'ing(copying) $src -> $dst\n"; 
+        # system("rsync --modify-window=2 $src $dst") or die "Unable to rsync $src to $dst: $!"; 
 }
 
 sub create_file {
