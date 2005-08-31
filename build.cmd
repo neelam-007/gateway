4abb3bbf1ff19fb6bc25c9e22dc1bcd7a9ed7a71
rmak <rmak@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-08-31 22:48:50 +0000

    Fixed handling of path with spaces.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@11604 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- build.cmd
+++ build.cmd
@@ -3,9 +3,9 @@ rem -------
 rem Build Script for UneasyRooster 
 rem -------
 setlocal
-if .%SRC_ROOT% == . goto srcRootMissing
-if .%JAVA_HOME% == . goto javaHomeMissing
-if .%TOMCAT_HOME% == . goto tomcatHomeMissing
+if "%SRC_ROOT%" == "" goto srcRootMissing
+if "%JAVA_HOME%" == "" goto javaHomeMissing
+if "%TOMCAT_HOME%" == "" goto tomcatHomeMissing
 rem - JDK classes -
 set JDK_CLASSES=%JAVA_HOME%\jre\lib\rt.jar;%JAVA_HOME%\lib\tools.jar
 
@@ -18,7 +18,7 @@ set CLASSPATH=%SRC_ROOT%\lib\xml-apis.jar;%CLASSPATH%
 set CLASSPATH=%SRC_ROOT%\lib\junit.jar;%CLASSPATH%
 rem echo %CLASSPATH%
 rem Execute ANT to perform the requested build target
-java org.apache.tools.ant.Main -Dsrc.root=%SRC_ROOT% -Dtomcat.home=%TOMCAT_HOME% %1 %2 %3 %4 %5
+java org.apache.tools.ant.Main -Dsrc.root="%SRC_ROOT%" -Dtomcat.home="%TOMCAT_HOME%" %1 %2 %3 %4 %5
 goto end
 :javaHomeMissing
 @echo The environment variable JAVA_HOME is not present or invalid. Must point to the
