ae0893440c5c06f5c25af9cfea4ff1a08081d256
megery <megery@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-08-31 00:32:04 +0000

    log errors while backing up configuration
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@11575 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/server/config/commands/BaseConfigurationCommand.java
+++ src/com/l7tech/server/config/commands/BaseConfigurationCommand.java
@@ -3,10 +3,7 @@ package com.l7tech.server.config.commands;
 import com.l7tech.server.config.OSSpecificFunctions;
 import com.l7tech.server.config.beans.ConfigurationBean;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -47,33 +44,44 @@ public abstract class BaseConfigurationCommand implements ConfigurationCommand {
             currentTime = Calendar.getInstance().getTime();
         }
 
+        String backupFileName = backupName + "_" + formatter.format(currentTime);
+        String fullBackupPath = osFunctions.getSsgInstallRoot() + backupFileName + ".zip";
         if (files != null && files.length > 0) {
-            String backupFileName = backupName + "_" + formatter.format(currentTime);
-            String fullBackupPath = osFunctions.getSsgInstallRoot() + backupFileName + ".zip";
             logger.info("creating ZIP file: " + fullBackupPath);
 
-            ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(fullBackupPath));
+            try {
+                ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(fullBackupPath));
 
-            File origFile;
-            FileInputStream fis;
-            for (int i = 0; i < files.length; i++) {
-                origFile = files[i];
-                if (origFile != null && origFile.exists()) {
-                    zos.putNextEntry(new ZipEntry(origFile.getAbsolutePath()));
+                File origFile;
+                FileInputStream fis;
+                for (int i = 0; i < files.length; i++) {
+                    origFile = files[i];
+                    if (origFile != null && origFile.exists()) {
+                        zos.putNextEntry(new ZipEntry(origFile.getAbsolutePath()));
 
-                    fis = new FileInputStream(origFile);
+                        fis = new FileInputStream(origFile);
 
-                    byte[] buf = new byte[10240];
-                    int len = 0;
-                    while ((len = fis.read(buf)) > 0) {
-                        zos.write(buf, 0, len);
+                        byte[] buf = new byte[10240];
+                        int len = 0;
+                        while ((len = fis.read(buf)) > 0) {
+                            zos.write(buf, 0, len);
+                        }
+                        zos.closeEntry();
+                        fis.close();
                     }
-                    zos.closeEntry();
-                    fis.close();
                 }
+                zos.finish();
+                zos.close();
+                zos = null;
+            } catch (FileNotFoundException e) {
+                logger.warning("unable to create backup zip file: " + fullBackupPath);
+                logger.warning(e.getMessage());
+                throw e;
+            } catch (IOException e) {
+                logger.warning("error while writing the backup file: " + fullBackupPath);
+                logger.warning(e.getMessage());
+                throw e;
             }
-            zos.finish();
-            zos.close();
         }
     }
 }
