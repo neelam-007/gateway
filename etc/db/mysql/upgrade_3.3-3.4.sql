46066e254f15398774e630db03cbdfd99baf6ac9
jay <jay@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-09-23 20:47:12 +0000

    Db changes for properties table
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@11806 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- etc/db/mysql/upgrade_3.3-3.4.sql
+++ etc/db/mysql/upgrade_3.3-3.4.sql
@@ -9,3 +9,10 @@ ALTER TABLE client_cert ADD INDEX i_ski (ski);
 
 ALTER TABLE trusted_cert ADD COLUMN ski VARCHAR(64);
 ALTER TABLE trusted_cert ADD INDEX i_ski (ski);
+
+
+alter table  cluster_properties add column new_value mediumtext;
+update cluster_properties set new_value=propvalue;
+alter table  cluster_properties drop column propvalue;
+alter table cluster_properties change new_value propvalue mediumtext;
+
