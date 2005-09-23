9c6bffd6a600e61dde53e0e4ff8003ffd9fddab8
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-09-23 04:02:22 +0000

    Added new Component so audit records can refer to the License Manager as an event source.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@11799 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/common/Component.java
+++ src/com/l7tech/common/Component.java
@@ -29,6 +29,7 @@ public class Component {
     public static final Component     GW_POLICY_SERVICE   = new Component(  20000, "Policy Service", GW_SERVER);
     public static final Component     GW_TOKEN_SERVICE    = new Component(  30000, "Security Token Service", GW_SERVER);
     public static final Component     GW_AUDIT_SYSTEM     = new Component(  40000, "Audit System", GW_SERVER);
+    public static final Component     GW_LICENSE_MANAGER  = new Component(  50000, "License Manager", GW_SERVER);
     public static final Component     GW_CLUSTER          = new Component( 300000, "Cluster", GATEWAY);
     public static final Component     GW_DATABASE         = new Component( 400000, "Database", GATEWAY);
 
