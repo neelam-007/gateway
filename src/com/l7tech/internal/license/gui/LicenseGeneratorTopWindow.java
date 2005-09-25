2015a1d0ae9a27c859454976f2eac9914e41db95
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-09-25 09:39:32 +0000

    License generator GUI is finished.  Currently requires manual setup of keystore/alias/pass phrase via system properties, but I think that's actually more than good enough for now.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@11836 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/internal/license/gui/LicenseGeneratorTopWindow.java
+++ src/com/l7tech/internal/license/gui/LicenseGeneratorTopWindow.java
@@ -7,38 +7,40 @@ package com.l7tech.internal.license.gui;
 
 import com.japisoft.xmlpad.XMLContainer;
 import com.l7tech.common.License;
-import com.l7tech.common.security.xml.DsigUtil;
 import com.l7tech.common.gui.widgets.LicensePanel;
+import com.l7tech.common.security.xml.DsigUtil;
 import com.l7tech.common.util.Background;
 import com.l7tech.common.util.ExceptionUtils;
 import com.l7tech.common.util.HexUtils;
 import com.l7tech.common.util.XmlUtil;
-import com.l7tech.internal.license.LicenseSpec;
 import com.l7tech.internal.license.LicenseGenerator;
+import com.l7tech.internal.license.LicenseSpec;
 import com.l7tech.proxy.gui.util.IconManager;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 import javax.swing.*;
-import javax.swing.filechooser.FileFilter;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import javax.swing.filechooser.FileFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.io.*;
 import java.math.BigInteger;
+import java.security.*;
+import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.security.interfaces.RSAPrivateKey;
-import java.security.PrivateKey;
+import java.util.Enumeration;
+import java.util.Properties;
 import java.util.TimerTask;
-import java.util.logging.Logger;
 import java.util.logging.Level;
-import java.beans.PropertyChangeListener;
-import java.beans.PropertyChangeEvent;
-
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
+import java.util.logging.Logger;
 
 /**
  * @author mike
@@ -48,12 +50,15 @@ public class LicenseGeneratorTopWindow extends JFrame {
     private static final long INACTIVITY_UPDATE_MILLIS = 500; // we'll fire an update when they've made a change then been idle for half a second
 
     public static final String PROPERTY_KEYSTORE_PATH = "licenseGenerator.keystorePath";
+    public static final String PROPERTY_KEYSTORE_PASSWORD = "licenseGenerator.keystorePass";
     public static final String PROPERTY_KEYSTORE_TYPE = "licenseGenerator.keystoreType";
     public static final String PROPERTY_KEYSTORE_ALIAS = "licenseGenerator.keystoreAlias";
+    public static final String PROPERTY_KEYSTORE_ALIAS_PASSWORD = "licenseGenerator.keystoreAliasPass";
     public static final String KEYSTORE_MESSAGE = "No keystore is configured.  Please set these system properties:\n" +
             "\n  " + PROPERTY_KEYSTORE_PATH +
-            "\n  " + PROPERTY_KEYSTORE_TYPE +
+            "\n  " + PROPERTY_KEYSTORE_PASSWORD +
             "\n  " + PROPERTY_KEYSTORE_ALIAS;
+    public static final String DEFAULT_KEYSTORE_TYPE = "PKCS12";
 
     private LicensePanel licensePanel = new LicensePanel("Building License");
     private LicenseSpecPanel specPanel = new LicenseSpecPanel();
@@ -80,6 +85,8 @@ public class LicenseGeneratorTopWindow extends JFrame {
     private Action openAction;
     private Action newAction;
     private Action quitAction;
+    private Action createNewKeystoreAction;
+    private Action configureKeystoreAction;
 
     public LicenseGeneratorTopWindow() throws HeadlessException {
         setContentPane(rootPanel);
@@ -173,7 +180,16 @@ public class LicenseGeneratorTopWindow extends JFrame {
         JMenuBar menuBar = new JMenuBar();
         menuBar.add(makeFileMenu());
         menuBar.add(makeEditMenu());
+        //menuBar.add(makeSetupMenu());
         return menuBar;
+    }
+
+    private JMenu makeSetupMenu() {
+        JMenu menu = new JMenu("Setup");
+        menu.add(new JMenuItem(getConfigureKeystoreAction()));
+        menu.add(new JMenuItem(getCreateNewKeystoreAction()));
+
+        return menu;
     }
 
     private JMenu makeEditMenu() {
@@ -192,6 +208,32 @@ public class LicenseGeneratorTopWindow extends JFrame {
         menu.add(new JMenuItem(getQuitAction()));
 
         return menu;
+    }
+
+    private Action getCreateNewKeystoreAction() {
+        if (createNewKeystoreAction != null) return createNewKeystoreAction;
+        Action action = new AbstractAction("Create New Key Store") {
+            public void actionPerformed(ActionEvent e) {
+                // TODO
+            }
+        };
+        action.setEnabled(false); // TODO enable after it's written
+        action.putValue(Action.SHORT_DESCRIPTION, "Create a new signing certificate and key store.");
+        action.putValue(Action.MNEMONIC_KEY, new Integer(KeyEvent.VK_C));
+        return createNewKeystoreAction = action;
+    }
+
+    private Action getConfigureKeystoreAction() {
+        if (configureKeystoreAction != null) return configureKeystoreAction;
+        Action action = new AbstractAction("Configure Existing Key Store") {
+            public void actionPerformed(ActionEvent e) {
+                // TODO
+            }
+        };
+        action.setEnabled(false); // TODO enable after it's written
+        action.putValue(Action.SHORT_DESCRIPTION, "Configure the License Generator to point at an existing key store.");
+        action.putValue(Action.MNEMONIC_KEY, new Integer(KeyEvent.VK_K));
+        return configureKeystoreAction = action;
     }
 
     private Action getSignLicenseAction() {
@@ -225,6 +267,8 @@ public class LicenseGeneratorTopWindow extends JFrame {
                 }
             }
         };
+        action.putValue(Action.SHORT_DESCRIPTION, "Sign this license with the currently-configured key store.");
+        action.putValue(Action.MNEMONIC_KEY, new Integer(KeyEvent.VK_S));
         return signLicenseAction = action;
     }
 
@@ -261,6 +305,8 @@ public class LicenseGeneratorTopWindow extends JFrame {
                 }
             }
         };
+        action.putValue(Action.SHORT_DESCRIPTION, "Remove any ds:Signature second-level elements from the current XML.");
+        action.putValue(Action.MNEMONIC_KEY, new Integer(KeyEvent.VK_G));
         return stripSignaturesAction = action;
     }
 
@@ -308,7 +354,7 @@ public class LicenseGeneratorTopWindow extends JFrame {
             }
         };
         action.putValue(Action.SHORT_DESCRIPTION, "Save the current license");
-        action.putValue(Action.MNEMONIC_KEY, new Integer(KeyEvent.VK_X));
+        action.putValue(Action.MNEMONIC_KEY, new Integer(KeyEvent.VK_S));
         return saveAsAction = action;
     }
 
@@ -345,7 +391,7 @@ public class LicenseGeneratorTopWindow extends JFrame {
             }
         };
         action.putValue(Action.SHORT_DESCRIPTION, "Open an existing license");
-        action.putValue(Action.MNEMONIC_KEY, new Integer(KeyEvent.VK_X));
+        action.putValue(Action.MNEMONIC_KEY, new Integer(KeyEvent.VK_O));
         return openAction = action;
     }
 
@@ -357,7 +403,7 @@ public class LicenseGeneratorTopWindow extends JFrame {
             }
         };
         action.putValue(Action.SHORT_DESCRIPTION, "Start from a blank license document");
-        action.putValue(Action.MNEMONIC_KEY, new Integer(KeyEvent.VK_X));
+        action.putValue(Action.MNEMONIC_KEY, new Integer(KeyEvent.VK_N));
         return newAction = action;
     }
 
@@ -413,15 +459,19 @@ public class LicenseGeneratorTopWindow extends JFrame {
     }
 
     private X509Certificate[] getTrustedIssuers() {
+        X509Certificate cert = null;
         try {
-            X509Certificate cert = getSignerCert();
-            if (cert == null) return null;
-            return new X509Certificate[] { cert };
-        } catch (CertificateException e) {
+            cert = getSignerCert();
+        } catch (Exception e) {
             // Ignore here -- we'll deal with it when the user goes to sign something
-            return null;
-        } catch (IOException e) {
-            // Ignore here -- we'll deal with it when the user goes to sign something
+        }
+        try {
+            if (cert == null)
+                return new X509Certificate[] { getDemoSignerCert() };
+            else
+                return new X509Certificate[] { getDemoSignerCert(), cert };
+        } catch (Exception e) {
+            // Did the best we could.  Anyway this can't happen unless someone changes the hardcoded demo cert
             return null;
         }
     }
@@ -467,8 +517,106 @@ public class LicenseGeneratorTopWindow extends JFrame {
         return s;
     }
 
-    private PrivateKey getSignerKey() {
-        RSAPrivateKey privKey = new RSAPrivateKey() {
+    private KeyStore keyStore = null;
+    private PrivateKey privateKey = null;
+    private X509Certificate signerCert = null;
+
+    private void loadKeyStore() throws GeneralSecurityException, IOException
+    {
+        String storeType = System.getProperty(PROPERTY_KEYSTORE_TYPE, DEFAULT_KEYSTORE_TYPE);
+        if (storeType == null || storeType.length() < 1) storeType = DEFAULT_KEYSTORE_TYPE; // not needed
+
+        String storePath = System.getProperty(PROPERTY_KEYSTORE_PATH);
+        if (storePath == null || storePath.length() < 1)
+            return;
+
+        String storePass = System.getProperty(PROPERTY_KEYSTORE_PASSWORD, "");
+        String keyPass = System.getProperty(PROPERTY_KEYSTORE_ALIAS_PASSWORD, storePass);
+
+        KeyStore ks = KeyStore.getInstance(storeType);
+        final FileInputStream fis = new FileInputStream(storePath);
+        try {
+            ks.load(fis, storePass.toCharArray());
+        } catch (IOException e) {
+            throw (IOException)new IOException("Unable to load key store " + storePath + ": " + ExceptionUtils.getMessage(e)).initCause(e);
+        } finally {
+            //noinspection EmptyCatchBlock
+            try { fis.close(); } catch (IOException e) {}
+        }
+
+        String alias = System.getProperty(PROPERTY_KEYSTORE_ALIAS);
+        if (alias == null || alias.length() < 1) {
+            Enumeration aliases = ks.aliases();
+            while (aliases.hasMoreElements()) {
+                String a = (String)aliases.nextElement();
+                if (alias == null) {
+                    try {
+                        if (ks.isKeyEntry(alias))
+                            alias = a;
+                    } catch (NullPointerException npe) {
+                        // Screw you, com.sun.net.ssl.internal.ssl.PKCS12KeyStore.engineIsKeyEntry
+                    }
+                }
+            }
+        }
+
+        if (alias == null)
+            throw new KeyStoreException("No alias configured: please set the system property " + PROPERTY_KEYSTORE_ALIAS +
+                    "\nto the alias of the signing cert (using key store: " + storePath + ")");
+
+        Key key = ks.getKey(alias, keyPass.toCharArray());
+        if (key == null)
+            throw new KeyStoreException("The alias " + alias + " was not found in the key store " + storePath + ".");
+
+        if (!(key instanceof PrivateKey))
+            throw new KeyStoreException("The alias " + alias + " is not a private key (using key store: " + storePath + ".");
+
+        this.privateKey = (PrivateKey)key;
+
+        Certificate[] chain = ks.getCertificateChain(alias);
+        if (chain == null || chain.length < 1 || chain[0] == null)
+            throw new KeyStoreException("The alias " + alias + " has no certificate chain (using key store: " + storePath + ".");
+
+        if (!(chain[0] instanceof X509Certificate))
+            throw new KeyStoreException("The alias " + alias + " certificate isn't X.509 (using key store: " + storePath + ".");
+
+        this.signerCert = (X509Certificate)chain[0];
+    }
+
+    private X509Certificate getSignerCert() throws GeneralSecurityException, IOException
+    {
+        if (keyStore == null) loadKeyStore();
+        return signerCert;
+    }
+
+    private PrivateKey getSignerKey() throws GeneralSecurityException, IOException
+    {
+        if (keyStore == null) loadKeyStore();
+        return privateKey;
+    }
+
+    private void mergeAndSaveProperties() throws IOException {
+        Properties props = LicenseGeneratorMain.getProperties();
+        String[] propsToMerge = {
+                PROPERTY_KEYSTORE_PATH,
+                PROPERTY_KEYSTORE_PASSWORD,
+                PROPERTY_KEYSTORE_TYPE,
+                PROPERTY_KEYSTORE_ALIAS,
+                PROPERTY_KEYSTORE_ALIAS_PASSWORD,
+        };
+
+        for (int i = 0; i < propsToMerge.length; i++) {
+            String key = propsToMerge[i];
+            String sysVal = System.getProperty(key);
+            if (sysVal != null && sysVal.length() > 0)
+                props.setProperty(key, sysVal);
+        }
+
+        LicenseGeneratorMain.saveProperties();
+    }
+
+    private PrivateKey getDemoSignerKey() {
+        return new RSAPrivateKey() {
             public BigInteger getPrivateExponent() {
                 return new BigInteger(KEY_PRIVATE_EXPONENT);
             }
@@ -493,29 +641,13 @@ public class LicenseGeneratorTopWindow extends JFrame {
                 return new BigInteger(KEY_MODULUS);
             }
         };
-        return privKey;
     }
 
-    private X509Certificate getSignerCert() throws CertificateException, IOException {
+    private X509Certificate getDemoSignerCert() throws CertificateException, IOException {
         CertificateFactory cfac = CertificateFactory.getInstance("X.509");
-        X509Certificate signerCert = (X509Certificate)cfac.generateCertificate(
+        return (X509Certificate)cfac.generateCertificate(
                 new ByteArrayInputStream(HexUtils.unHexDump(EXAMPLE_CERT)));
-        return signerCert;
     }
-
-
-    public static final String SAMPLE_UNSIGNED =
-            "<license Id=\"1001\" xmlns=\"http://l7tech.com/license\">\n" +
-                    "    <description>Layer 7 Internal Developer License</description>\n" +
-                    "    <valid>2005-09-24T06:54:01.718Z</valid>\n" +
-                    "    <expires>2101-01-02T07:54:01.718Z</expires>\n" +
-                    "    <host name=\"*\"/>\n" +
-                    "    <ip address=\"*\"/>\n" +
-                    "    <product name=\"Layer 7 SecureSpan Suite\">\n" +
-                    "        <version major=\"3\" minor=\"4\"/>\n" +
-                    "    </product>\n" +
-                    "    <licensee contactEmail=\"developers@layer7tech.com\" name=\"Layer 7 Developer\"/>\n" +
-                    "</license>";
 
     public static final String EXAMPLE_CERT = "308202063082016fa003020102020829b38eab36c17550300d06092a864886f70d01010505003015311330110603550403130a726f6f742e72696b6572301e170d3034303332343233303231315a170d3036303332343233313231315a3010310e300c0603550403130572696b657230819f300d06092a864886f70d010101050003818d0030818902818100ba38aabf7cc4009ec3792509d9f50a93062995a671900395807aec6a9132f788b2ef58c021772509b02bc0cdcd52d73678c953d992413d7a64cb788317b6df1a7bd51e8d60303ec560448478cc089c8980a48923bca1d721398c60a76b9165bce4e4ea447a04ced066df7648ad764dbce48f7235aac92c2f45cb0387dcaaba7d0203010001a3643062300f0603551d130101ff04053003010100300f0603551d0f0101ff0405030307a000301d0603551d0e041604147562fcfd17554344f77cc1c74c03ffcd24913ef0301f0603551d230418301680141fa3c0db54eb527fca4fbe5a876f4db42b06ec76300d06092a864886f70d0101050500038181001e7f5b2a886fb40ba2de576e1f8c225ad3c285ed8c7c27a913eb6fc624c4045d861e2107eeb7db230b0eff7445e660e359176752f177f961f0f68d1654857ab0a0ffab2dc77c486d4e1e4f7596f70b3ca212fb35a687816dd8aa8e1ee9aab12794fd3447d6e82404744df60197c3cfa1cbcd675c8f41a7aa9df2c4e49e9dfd93";
     public static final String KEY_ENCODED = "30820277020100300d06092a864886f70d0101010500048202613082025d02010002818100ba38aabf7cc4009ec3792509d9f50a93062995a671900395807aec6a9132f788b2ef58c021772509b02bc0cdcd52d73678c953d992413d7a64cb788317b6df1a7bd51e8d60303ec560448478cc089c8980a48923bca1d721398c60a76b9165bce4e4ea447a04ced066df7648ad764dbce48f7235aac92c2f45cb0387dcaaba7d02030100010281804ab6ad9b023dc959e967537aee5da80e70ec8244334fea8032fa1e9c6b011ddb549f3ee66706dc6f54a55947b8d741cd730ca37da9764f6a29c290e957bc612a19c9e3972473f956659d9037e644cedc1441fef16cc7ee4e943e13de3d7d04e67c0317272eb4f409686cf83d2d79ed20519921ca308198a682b62c9d7f404a81024100dce1d6618cd548cd5fb6697f3f115c95de12607f1649ca6de2e37e7b0346614b2d327b975133deefe3471c187ad2763199b743673fbcf6d75e577bf968055fd1024100d7d417f7b7c0a0768acebc269ab9c8e8f4149c3c3e319eda73e186dd972b6e06e276408b56751e6473e0a0584b2f00dc8fdbe870d01236c88481bb3321f826ed024100b60ad7020ced179453e6e5e9be93d3879cbfee91af4fdfab530c858862b995a43cbad78b6d9c5f87bbfc3656a29b64581ac524a32aafd58cc8af3778e5575091024005e36773feb366ad554426a5f6fab29c886c3786fd3b6556186b42beb0ed226755ae5c3c70f3690cdc8c78537059abf0588c6b5f088e36a869d5104268ded851024100d5ae95211764367ecabfb5fb0ec4a4e3527b14d1697aa643613cbad880e0d6172b8dea22e6e2d168099641fbe764c22d2745dc820c23dcbbf35bd010790f61ac";
