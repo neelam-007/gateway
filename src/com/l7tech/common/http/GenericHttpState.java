0c8feaa116154aca0209b2604b3bdcda1d50d67a
steve <steve@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-09-28 19:37:51 +0000

    Container for provider specific state object.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@11870 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/common/http/GenericHttpState.java
@@ -0,0 +1,27 @@
+package com.l7tech.common.http;
+
+/**
+ * User: steve
+ * Date: Sep 21, 2005
+ * Time: 2:02:49 PM
+ * $Id$
+ */
+public class GenericHttpState {
+
+    //- PUBLIC
+
+    public GenericHttpState() {
+    }
+
+    public Object getStateObject() {
+        return state;
+    }
+
+    public void setStateObject(Object state) {
+        this.state = state;
+    }
+
+    //- PRIVATE
+
+    private Object state;
+}
