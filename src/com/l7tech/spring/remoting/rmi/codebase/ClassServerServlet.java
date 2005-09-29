dcb6d47a31cd1a23f8c6666d01cc7c83eb5c810e
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-09-29 22:51:37 +0000

    Reinstated the Class Server, but introduced a whitelist of URI regexps that will be served.  Anything not matching a regex on the whitelist will be rejected.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@11928 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/spring/remoting/rmi/codebase/ClassServerServlet.java
+++ src/com/l7tech/spring/remoting/rmi/codebase/ClassServerServlet.java
@@ -7,15 +7,20 @@
 package com.l7tech.spring.remoting.rmi.codebase;
 
 import com.l7tech.common.util.HexUtils;
+import com.l7tech.common.util.ExceptionUtils;
+import com.l7tech.policy.assertion.Regex;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.logging.Logger;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * The servlet that works as a class server.
@@ -25,20 +30,74 @@ import java.util.logging.Logger;
  */
 public class ClassServerServlet extends HttpServlet {
     private static final Logger logger = Logger.getLogger(ClassServerServlet.class.getName());
+    private static final String DEFAULT_WHITELIST = "defaultWhitelist.dat";
+    public static final String PARAM_WHITELIST = "WhitelistFile";
+
+    private Pattern[] uriMatchers = null;
 
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
+        String whitelist = config.getInitParameter(PARAM_WHITELIST);
+        if (whitelist == null || whitelist.trim().length() < 1)
+            whitelist = DEFAULT_WHITELIST;
+
+        List patternList = new ArrayList();
+        InputStream is = getClass().getClassLoader().getResourceAsStream(whitelist);
+        try {
+            if (is == null)
+                is = new FileInputStream(whitelist);
+            BufferedReader r = new BufferedReader(new InputStreamReader(is));
+            String line;
+            while ((line = r.readLine()) != null) {
+                String trimmed = line.trim();
+                if (trimmed.length() < 1 || trimmed.startsWith("#"))
+                    continue;
+                try {
+                    Pattern pattern = Pattern.compile(trimmed);
+                    patternList.add(pattern);
+                } catch (PatternSyntaxException e) {
+                    throw new ServletException("Class server disabled: syntax error in URI whitelist: " + ExceptionUtils.getMessage(e), e);
+                }
+            }
+            uriMatchers = (Pattern[])patternList.toArray(new Pattern[0]);
+        } catch (IOException e) {
+            throw new ServletException("Class server disabled: unable to read URI whitelist: " + ExceptionUtils.getMessage(e), e);
+        } finally {
+            if (is != null) //noinspection EmptyCatchBlock
+                try { is.close(); } catch (IOException e) {}
+        }
     }
 
+
     public void service(HttpServletRequest request, HttpServletResponse response)
-      throws IOException, ServletException {
+            throws IOException, ServletException
+    {
         final String requestURI = request.getRequestURI();
         logger.fine("The request URI is " + requestURI);
+
+        if (uriMatchers == null || uriMatchers.length < 1)
+            throw new ServletException("Class server disabled: no URI whitelist has been loaded");
 
         if (!requestURI.startsWith(URI_PREFIX)) {
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             return;
         }
+
+        boolean approved = false;
+        for (int i = 0; i < uriMatchers.length; i++) {
+            Pattern uriMatcher = uriMatchers[i];
+            if (uriMatcher.matcher(requestURI).matches()) {
+                approved = true;
+                break;
+            }
+        }
+
+        if (!approved) {
+            logger.severe("Class Server blocked attempt to access non-whitelisted URI: " + requestURI);
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
         byte[] bytes;
         InputStream in = null;
         try {
