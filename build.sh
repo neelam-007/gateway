138e72cc517aa660c177e50bc66b5ebd33a89de4
megery <megery@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-09-30 20:12:51 +0000

    now use the AntPerformanceListener when building to collect stats.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@11950 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- build.sh
+++ build.sh
@@ -62,7 +62,7 @@ if [ ! -e $SRC_ROOT/build.sh ]; then
     exit 1
 fi
 
-ANT_JARS="$SRC_ROOT/lib/ant.jar:$SRC_ROOT/lib/optional.jar:$SRC_ROOT/lib/xercesImpl.jar:$SRC_ROOT/lib/xml-apis.jar:$SRC_ROOT/lib/junit.jar"
+ANT_JARS="$SRC_ROOT/lib/ant.jar:$SRC_ROOT/lib/optional.jar:$SRC_ROOT/lib/xercesImpl.jar:$SRC_ROOT/lib/xml-apis.jar:$SRC_ROOT/lib/junit.jar:$SRC_ROOT/lib/ant-contrib.jar"
 CLASSPATH="${JDK_CLASSES}:${ANT_JARS}"
 # For Cygwin, switch paths to Windows format before running java
 if $cygwin; then
@@ -71,4 +71,4 @@ if $cygwin; then
   TOMCAT_HOME=`cygpath --path --windows "$TOMCAT_HOME"`
 fi
 export CLASSPATH
-$JAVA_HOME/bin/java $JAVA_OPTS org.apache.tools.ant.Main "-Dtomcat.home=$TOMCAT_HOME" "-Dsrc.root=$SRC_ROOT" $@
+$JAVA_HOME/bin/java $JAVA_OPTS org.apache.tools.ant.Main -listener net.sf.antcontrib.perf.AntPerformanceListener "-Dtomcat.home=$TOMCAT_HOME" "-Dsrc.root=$SRC_ROOT" $@
