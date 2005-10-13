ed33f1769053d9f4e70678f218b56cc4518ef953
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-10-13 21:41:08 +0000

    fix actions on bridge routing properties [bugzilla #2072]
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@11977 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/console/tree/policy/BridgeRoutingAssertionTreeNode.java
+++ src/com/l7tech/console/tree/policy/BridgeRoutingAssertionTreeNode.java
@@ -7,9 +7,13 @@
 package com.l7tech.console.tree.policy;
 
 import com.l7tech.console.action.BridgeRoutingAssertionPropertiesAction;
+import com.l7tech.console.action.HttpRoutingAssertionPropertiesAction;
 import com.l7tech.policy.assertion.RoutingAssertion;
 
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 
 /**
  * Represents a policy tree node for a BridgeRoutingAssertion.
@@ -25,5 +29,18 @@ public class BridgeRoutingAssertionTreeNode extends HttpRoutingAssertionTreeNode
 
     public Action getPreferredAction() {
         return new BridgeRoutingAssertionPropertiesAction(this);
+    }
+
+    public Action[] getActions() {
+        java.util.List list = new ArrayList(Arrays.asList(super.getActions()));
+        for (Iterator iterator = list.iterator(); iterator.hasNext();) {
+            Action action = (Action) iterator.next();
+            if (action instanceof HttpRoutingAssertionPropertiesAction) {
+                list.remove(action);
+                break;
+            }
+        }
+        list.add(0, getPreferredAction());
+        return (Action[])list.toArray(new Action[]{});
     }
 }
