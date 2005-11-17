fbcdf296db58fa4ad13b1125a4462969e9667e3a
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2005-11-17 01:17:32 +0000

    Added some javadoc
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@12106 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/server/admin/AdminSessionManager.java
+++ src/com/l7tech/server/admin/AdminSessionManager.java
@@ -5,10 +5,25 @@ import com.l7tech.common.util.HexUtils;
 
 import java.security.SecureRandom;
 
+/**
+ * This class keeps track of admin sessions that have already authenticated.
+ * The cookie is used to look up the username that authenticated with it.  Anyone who can steal a cookie can
+ * resume an admin session as that user; thus, the cookies must be sent over SSL, never written to disk by
+ * either client or server, and not kept longer than necessary.
+ */
 public class AdminSessionManager {
+    // TODO expire old sessions rather than wait for them to fall out of the LRU map.
     private final LRUMap sessionMap = new LRUMap(1000);
     private final SecureRandom random = new SecureRandom();
 
+    /**
+     * Record a successful authentication for the specified login and return a cookie that can be used
+     * to resume the session from now on.
+     *
+     * @param login  the login to register.  Must not be null.
+     * @return a cookie string that can be used with {@link #getLogin} later to recover the username.  Never null or empty.
+     *         Always contains at least 16 bytes of entropy.
+     */
     public synchronized String login(String login) {
         if (login == null) throw new NullPointerException();
 
@@ -20,6 +35,12 @@ public class AdminSessionManager {
         return cookie;
     }
 
+    /**
+     * Attempt to resume a session for a previously-authenticated user.
+     *
+     * @param session  the session ID that was originally returned from {@link #login}.  Must not be null or empty.
+     * @return the login name associated with this session ID, or null if the session doesn't exist or has expired.
+     */
     public synchronized String getLogin(String session) {
         if (session == null) throw new NullPointerException();
         return (String)sessionMap.get(session);
