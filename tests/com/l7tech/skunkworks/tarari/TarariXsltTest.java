c69a61d8e6b03820da9bc915fe7bea588da15a0c
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2006-03-11 02:32:35 +0000

    Try to log and stop all if there's a card problem
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@12664 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- tests/com/l7tech/skunkworks/tarari/TarariXsltTest.java
+++ tests/com/l7tech/skunkworks/tarari/TarariXsltTest.java
@@ -5,19 +5,21 @@
 
 package com.l7tech.skunkworks.tarari;
 
-import com.l7tech.common.xml.TestDocuments;
 import com.l7tech.common.io.EmptyInputStream;
 import com.l7tech.common.io.NullOutputStream;
+import com.l7tech.common.message.TarariMessageContextFactory;
 import com.l7tech.common.util.HexUtils;
+import com.l7tech.common.xml.TarariLoader;
+import com.l7tech.common.xml.TestDocuments;
+import com.l7tech.common.xml.tarari.GlobalTarariContextImpl;
+import com.l7tech.common.xml.tarari.TarariMessageContextImpl;
 import com.l7tech.skunkworks.BenchmarkRunner;
-import com.tarari.xml.XmlSource;
-import com.tarari.xml.XmlConfigException;
 import com.tarari.xml.XmlResult;
+import com.tarari.xml.XmlSource;
 import com.tarari.xml.rax.RaxDocument;
 import com.tarari.xml.xslt11.Stylesheet;
 
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.io.ByteArrayOutputStream;
 
 /**
@@ -29,6 +31,8 @@ public class TarariXsltTest {
             return new XmlSource(new EmptyInputStream());
         }
     };
+    private static BenchmarkRunner br1;
+    private static BenchmarkRunner br2;
 
     public static void main(String[] args) throws Exception {
         final String reqStr = TestDocuments.getTestDocumentAsXml(TestDocuments.DIR + "xslt/DocSearchResponse.xml");
@@ -38,11 +42,16 @@ public class TarariXsltTest {
         final byte[] requestBytes = reqStr.getBytes();
         final Stylesheet master = Stylesheet.create(new XmlSource(xsltBytes));
 
+        final GlobalTarariContextImpl gtci = (GlobalTarariContextImpl)TarariLoader.getGlobalContext();
+        gtci.compileAllXpaths();
+        final TarariMessageContextFactory cfac = TarariLoader.getMessageContextFactory();
+
         // Make a test that simulates what the SSG has to do
         Runnable xsltTestSsg = new Runnable() {
             public void run() {
                 try {
-                    RaxDocument doc = RaxDocument.createDocument(new XmlSource(new ByteArrayInputStream(requestBytes)));
+                    TarariMessageContextImpl ctx = (TarariMessageContextImpl)cfac.makeMessageContext(new ByteArrayInputStream(requestBytes));
+                    RaxDocument doc = ctx.getRaxDocument();
                     //final XPathProcessor xpathProcessor = new XPathProcessor(doc);
                     //XPathResult xpathResult = xpathProcessor.processXPaths();
                     XmlSource source = (XmlSource)xmlSource.get();
@@ -54,9 +63,9 @@ public class TarariXsltTest {
                     transformer.transform(source, result);
                     byte[] transformedmessage = output.toByteArray();
                     HexUtils.copyStream(new ByteArrayInputStream(transformedmessage), new NullOutputStream());
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                } catch (XmlConfigException e) {
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    stopAll();
                     throw new RuntimeException(e);
                 }
             }
@@ -73,20 +82,25 @@ public class TarariXsltTest {
                     transformer.setValidate(false);
                     XmlResult result = new XmlResult(new NullOutputStream());
                     transformer.transform(fastSource, result);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                } catch (XmlConfigException e) {
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    stopAll();
                     throw new RuntimeException(e);
                 }
             }
         };
 
-        BenchmarkRunner br1 = new BenchmarkRunner(xsltTestSsg, 10000, "xsltTestSsg");
+        br1 = new BenchmarkRunner(xsltTestSsg, 10000, "xsltTestSsg");
         br1.setThreadCount(10);
         br1.run();
 
-        BenchmarkRunner br2 = new BenchmarkRunner(xsltTestFast, 10000, "xsltTestFast");
+        br2 = new BenchmarkRunner(xsltTestFast, 10000, "xsltTestFast");
         br2.setThreadCount(10);
         br2.run();
+    }
+
+    private static void stopAll() {
+        if (br1 != null) br1.stopAll();
+        if (br2 != null) br2.stopAll();
     }
 }
