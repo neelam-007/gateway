5b5a7be8852663b40ece1fe403f2996121fed40c
jay <jay@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2006-03-28 22:37:39 +0000

    Bugz: 2338: add some memory config
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@12865 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- installer/console/win32/SecureSpan Manager.bat
+++ installer/console/win32/SecureSpan Manager.bat
@@ -1,3 +1,3 @@
-@jre1.5.0_02\bin\java -Dfile.encoding=UTF-8  -Dsun.net.inetaddr.ttl=10 -Dnetworkaddress.cache.ttl=10 -jar Manager.jar
+@jre1.5.0_02\bin\java  -Xms128M -Xmx256M -Xss256k -server -Dfile.encoding=UTF-8  -Dsun.net.inetaddr.ttl=10 -Dnetworkaddress.cache.ttl=10 -jar Manager.jar
 @echo Layer 7 SecureSpan Manager has finished.  Its final log messages are shown above.
 @pause
