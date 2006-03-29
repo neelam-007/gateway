33d5e84cd6ba5b31363f488d6be0dc898701908e
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2006-03-29 00:58:03 +0000

    Initial commit
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@12870 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ tests/com/l7tech/skunkworks/IHQL.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright (C) 2006 Layer 7 Technologies Inc.
+ */
+package com.l7tech.skunkworks;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Iterator;
+
+/**
+ * @author alex
+ */
+public class IHQL {
+    private final SessionFactory sessionFactory;
+
+    private IHQL() {
+        Configuration config = new Configuration();
+        config.addFile("etc/db/SSG.hbm.xml");
+        config.addFile("etc/db/audit.hbm.xml");
+        this.sessionFactory = config.buildSessionFactory();
+    }
+
+    private void doIt() throws IOException {
+        String line;
+        BufferedReader sin = new BufferedReader(new InputStreamReader(System.in));
+        while ((line = sin.readLine()) != null) {
+            System.out.print("HSQL> ");
+            if ("q".equalsIgnoreCase(line)) break;
+            try {
+                Session s = sessionFactory.openSession();
+                Query q = s.createQuery(line);
+                int spos = line.indexOf(" ");
+                if (spos == -1) {
+                    System.out.println("Invalid command");
+                    continue;
+                }
+                String word = line.substring(0, spos);
+                if ("delete".equalsIgnoreCase(word) || "update".equalsIgnoreCase(word)) {
+                    int num = q.executeUpdate();
+                    System.out.println(num + " record(s) affected.");
+                } else {
+                    int i = 0;
+                    for (Iterator j = q.iterate(); j.hasNext();) {
+                        Object o = j.next();
+                        System.out.print(++i + ": ");
+                        if (o instanceof Object[]) {
+                            Object[] objects = (Object[])o;
+                            System.out.println("{");
+                            for (int k = 0; k < objects.length; k++) {
+                                Object object = objects[k];
+                                System.out.println("    " + (k+1) + ": " + object.toString());
+                            }
+                            System.out.println("}");
+                        } else {
+                            System.out.println(o.toString());
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace(System.err);
+            }
+
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        IHQL me = new IHQL();
+        me.doIt();
+    }
+}
