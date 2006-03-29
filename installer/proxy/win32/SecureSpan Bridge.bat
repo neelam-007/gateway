32f5b890276e438fb4371ccf9f5b4d836398a13e
jay <jay@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2006-03-29 20:48:28 +0000

    Bugz: 2338 "Memories, my memories, how long can they stay, to haunt my days" - Freddy Mercury
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@12884 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- installer/proxy/win32/SecureSpan Bridge.bat
+++ installer/proxy/win32/SecureSpan Bridge.bat
@@ -1,3 +1,3 @@
-@jre1.5.0_02\bin\java -Dfile.encoding=UTF-8  -Xms128M -Xmx256M -Xss256k -server -Dsun.net.inetaddr.ttl=10 -Dnetworkaddress.cache.ttl=10 -Dcom.l7tech.proxy.listener.maxthreads=300 -jar Bridge.jar
+@jre1.5.0_02\bin\java -Dfile.encoding=UTF-8  -Xms128M -Xmx256M -Xss256k -Dsun.net.inetaddr.ttl=10 -Dnetworkaddress.cache.ttl=10 -Dcom.l7tech.proxy.listener.maxthreads=300 -jar Bridge.jar
 @echo Layer 7 SecureSpan Bridge has finished.  Its final log messages are shown above.
 @pause
