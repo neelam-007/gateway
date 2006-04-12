da505153fdd56dbe781044bb8d524115136a4aec
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2006-04-12 00:41:46 +0000

    Merge from rel3_5: saner BenchmarkRunner
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@13044 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- tests/com/l7tech/skunkworks/BenchmarkRunner.java
+++ tests/com/l7tech/skunkworks/BenchmarkRunner.java
@@ -2,7 +2,6 @@ package com.l7tech.skunkworks;
 
 import EDU.oswego.cs.dl.util.concurrent.BrokenBarrierException;
 import EDU.oswego.cs.dl.util.concurrent.Rendezvous;
-import com.l7tech.common.util.ThreadPool;
 
 import java.util.*;
 import java.util.logging.Level;
@@ -12,7 +11,7 @@ import java.util.logging.Logger;
  * The <code>BenchmarkRunner</code> is simple bechmark tool, that accepts
  * the <code>Runnable</code> and executes it specified number of times.
  * The <code>Runnable</code> executions are distributed over the number
- * of threads up to {@link BenchmarkRunner.REQUESTS_PER_THREAD}.
+ * of threads up to {@link BenchmarkRunner#REQUESTS_PER_THREAD}.
  * <p/>
  * The 'Barrier' synchronization model to ensure all the test threads
  * are started at the same time.
@@ -29,14 +28,15 @@ public class BenchmarkRunner {
     /** an arbitray number of requests per thread that the test uses */
     public static final int REQUESTS_PER_THREAD = 15;
 
+    private final Set runnerResults = new HashSet();
+
     private Rendezvous rendezvous;
     private int threadCount;
-    private Set runnerResults = new HashSet();
-    private final ThreadPool threadPool = new ThreadPool("test", MAX_THREAD_COUNT);
+    private Thread[] threads;
     private Runnable runnable;
     private int runCount;
     private boolean running;
-    private volatile boolean runnerDone = false;
+    private volatile boolean allThreadsCompletedNormally = false;
 
     /**
      * Test constructor
@@ -45,11 +45,6 @@ public class BenchmarkRunner {
         this.runnable = r;
         this.runCount = times;
         this.name = name;
-
-        // block indefinitely for an available thread
-        // and threads never exit
-        threadPool.setTimeout(-1);
-        threadPool.setIdleTimeout(-1);
     }
 
     public BenchmarkRunner(Runnable r, int times) {
@@ -80,74 +75,104 @@ public class BenchmarkRunner {
             throw new IllegalArgumentException();
         }
         if (running) {
-            throw new IllegalStateException();
+            throw new IllegalStateException("already running");
         }
         this.threadCount = threadCount;
+    }
+
+    private void killAllThreads() {
+        if (threads == null || threads.length < 1) return;
+        for (int i = 0; i < threads.length; i++) {
+            Thread thread = threads[i];
+            if (thread != null) {
+                threads[i] = null;
+                thread.interrupt();
+            }
+        }
+        threads = null;
     }
 
     /**
      * crete the test, and invoke the rendezvous (Barrier) that will
      * release waiting threads.
-     * 
+     *
      * @throws InterruptedException
      */
     public final void run() throws InterruptedException {
+        // Make sure any old threads get killed
+        killAllThreads();
+        allThreadsCompletedNormally = false;
         running = true;
+        synchronized (runnerResults) {
+            runnerResults.clear();
+        }
 
         try {
             prepareRunnables();
             // last rendezvois signals the release
             rendezvous.rendezvous(new Object());
-            synchronized (this) {
-                while (!runnerDone) {
-                    try {
-                        this.wait(200);
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
-                        break;
-                    }
-                }
-                threadPool.close();
+
+            // Wait for all test threads to die
+            for (int i = 0; i < threads.length; i++) {
+                Thread thread = threads[i];
+                thread.join();
             }
+
         } finally {
+            killAllThreads();
             running = false;
         }
+
+        if (allThreadsCompletedNormally) {
+            log.info( name + ": all results collected.");
+        } else {
+            log.severe( name = ": WARNING: at least one thread died before finishing: results are incomplete");
+        }
+        printResults(allThreadsCompletedNormally);
+
     }
 
     /**
-     * prepare the agents authenticaiton test. Load every Bridge, and
-     * distribute the Bridge auth request to threads.
-     * 
-     * @throws InterruptedException
+     * prepare the test.  Instantiates and starts every thread, waiting only for run to unblock them.
      */
-    private void prepareRunnables() throws InterruptedException {
+    private void prepareRunnables() {
         if (runCount <= 0) {
             log.warning( name +": Invalid runnable count "+runCount);
             return;
         }
 
         int runnableCount = runCount;
-        int adjustedThreads = threadCount;
         // if not set
         if (threadCount == 0) {
+            int adjustedThreads = threadCount;
             threadCount = estimateThreads(runnableCount);
             // adjust if reminder
             int r = (runnableCount % threadCount) == 0 ? 0 : 1;
             adjustedThreads = threadCount + r;
+            threadCount = adjustedThreads;
         }
 
-        log.info( name + ": Runnables = " + runnableCount + " threads to use = " + adjustedThreads);
+        if (threadCount < 1) throw new IllegalStateException("Invalid thread count: " + threadCount);
+        int perThread = runCount / threadCount;
+        int remainder = runCount % threadCount;
 
-        rendezvous = new Rendezvous(adjustedThreads + 1);
+        log.info( name + ": Runnables = " + runnableCount + " threads to use = " + threadCount + "  (about " + perThread + " iterations per thread)");
 
-        ThreadRunner arun = null;
+        rendezvous = new Rendezvous(threadCount + 1);
+        // Make sure any old threads get killed
+        killAllThreads();
+        this.threads = new Thread[threadCount];
 
-        for (int i = 0; i < runnableCount; i++) {
-            if ((i % (runnableCount / threadCount)) == 0) {
-                arun = new ThreadRunner(rendezvous);
-                threadPool.start(arun);
+        for (int i = 0; i < threadCount; ++i) {
+            int num = perThread;
+            if (remainder > 0) {
+                // Distribute any remainder among the first few threads
+                num++;
+                remainder--;
             }
-            arun.addRunnable(runnable);
+            ThreadRunner arun = new ThreadRunner(rendezvous, num, runnable);
+            threads[i] = new Thread(arun);
+            threads[i].start();
         }
     }
 
@@ -157,13 +182,12 @@ public class BenchmarkRunner {
      * Start with MAX_THREAD_COUNT threads and decrease the number of threads
      * until there are less then REQUESTS_PER_THREAD clients per thread.
      *
-     * @param requests the number of Bridge instances to process
-     * @return the estimated number of threads that will process the
-     *         auth request.
+     * @param requests the number of runnables to process
+     * @return the estimated number of threads that will be used
      */
     private int estimateThreads(int requests) {
         if (requests <= 0) {
-            throw new IllegalArgumentException("number of Bridges " + requests);
+            throw new IllegalArgumentException("number of runnables must be positive " + requests);
         }
         int nThreads = MAX_THREAD_COUNT;
         while (nThreads > 1 && (requests / nThreads) < REQUESTS_PER_THREAD) {
@@ -175,7 +199,7 @@ public class BenchmarkRunner {
 
     /**
      * collect authentication results for the current thread.
-     * 
+     *
      * @param result the results that were produced by the thread
      *               reporting the results.
      */
@@ -186,25 +210,32 @@ public class BenchmarkRunner {
             currentSize = runnerResults.size();
         }
         if (currentSize == threadCount) {
-            log.info( name + ": all results collected.");
-
-            long start = Long.MAX_VALUE - 1;
-            long end = 0;
-
-            Iterator it = runnerResults.iterator();
-            while (it.hasNext()) {
-                ThreadRunner.Result res = (ThreadRunner.Result)it.next();
-                //log.info("thread = " + res.thread + " time " + (res.end - res.start));
-                if (end < res.end) end = res.end;
-                if (start > res.start) start = res.start;
-            }
-            log.info( name + ": total time = " + (end - start) + "ms");
-            // notify done
             synchronized (this) {
-                runnerDone = true;
-                this.notify();
+                allThreadsCompletedNormally = true;
+                this.notifyAll();
             }
         }
+    }
+
+    private void printResults(boolean allResultsReady) {
+        if (allResultsReady)
+            log.info( name + ": all results collected.");
+        else
+            log.severe( name + ": SEVERE: at least one thread died before finishing: results are probably incomplete");
+
+        long start = Long.MAX_VALUE - 1;
+        long end = 0;
+
+        Iterator it = runnerResults.iterator();
+        while (it.hasNext()) {
+            ThreadRunner.Result res = (ThreadRunner.Result)it.next();
+            //log.info("thread = " + res.thread + " time " + (res.end - res.start));
+            if (end < res.end) end = res.end;
+            if (start > res.start) start = res.start;
+        }
+        long t = (end - start);
+        if (t == 0) t = 1;
+        log.info( name + ": total time = " + t + "ms (" + runCount / (t / 1000f) + "/s)" );
     }
 
 
@@ -216,42 +247,31 @@ public class BenchmarkRunner {
          * the class contains the results for one ThreadRunner
          */
         class Result {
-            private Thread thread;
             long start;
             long end;
 
-            public Result(long start, long end, Thread th) {
+            public Result(long start, long end) {
                 this.start = start;
                 this.end = end;
-                this.thread = th;
             }
         }
 
         /** barrier */
-        private Rendezvous rzvs;
-        /** the request queue */
-        private List runnables = new ArrayList();
-
-        private long start;
-        private long end;
-        private Thread th; // thread on which requests are executing
+        private final Rendezvous rzvs;
+        private final Runnable runnable;
+        private final int iterations;
 
         /**
          * constructor
-         * 
+         *
          * @param rzvs rendezvous 'Barrier' synchronization variation
+         * @param iterations number of times to repeat the work
+         * @param runnable  the work to do
          */
-        public ThreadRunner(Rendezvous rzvs) {
+        public ThreadRunner(Rendezvous rzvs, int iterations, Runnable runnable) {
             this.rzvs = rzvs;
-        }
-
-        /**
-         * add the runnable to the runner
-         * 
-         * @param r the runnable
-         */
-        public void addRunnable(Runnable r) {
-            runnables.add(r);
+            this.iterations = iterations;
+            this.runnable = runnable;
         }
 
         /**
@@ -259,41 +279,25 @@ public class BenchmarkRunner {
          */
         public void run() {
             try {
-                th = Thread.currentThread();
-
                 rzvs.rendezvous(new Object()); // join barrier
 
-                start = System.currentTimeMillis();
+                final long start = System.currentTimeMillis();
+                for (int i = 0; i < iterations; ++i)
+                    runnable.run();
+                final long end = System.currentTimeMillis();
 
-                Iterator it = runnables.iterator();
-                while (it.hasNext()) {
-                    Runnable r = (Runnable)it.next();
-                    r.run();
-                }
-                submitResults();
+                collectResults(new Result(start, end));
+
             } catch (InterruptedException e) {
                 log.severe( name + ": thread interrupted");
                 Thread.currentThread().interrupt();
             } catch (BrokenBarrierException e) {
                 log.log(Level.SEVERE, name + ": barrier synchronziation exception", e);
+            } catch (Throwable t) {
+                log.log(Level.SEVERE, name + ": SEVERE: uncaught exception", t);
             }
         }
 
-        /**
-         * submit the results for this AuthLogonCompletionHandlerImpl
-         */
-        private void submitResults() {
-            end = System.currentTimeMillis();
-            collectResults(new Result(start, end, th));
-        }
-    }
-
-    public synchronized void stopAll() {
-        Thread[] t = threadPool.getAllThreads();
-        for (int i = 0; i < t.length; i++) {
-            Thread thread = t[i];
-            if (thread != null) thread.interrupt();
-        }
     }
 
     private String name;
