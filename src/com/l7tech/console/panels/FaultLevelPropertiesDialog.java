58d8c2afaa6e5da2466eebdeff43b1d677dfd59e
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2006-05-05 03:30:00 +0000

    added level combo controls, populate and action listeners in FaultLevelPropertiesDialog
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@13341 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/console/panels/FaultLevelPropertiesDialog.java
+++ src/com/l7tech/console/panels/FaultLevelPropertiesDialog.java
@@ -2,9 +2,12 @@ package com.l7tech.console.panels;
 
 import com.l7tech.policy.assertion.FaultLevel;
 import com.l7tech.common.gui.util.Utilities;
+import com.l7tech.common.xml.SoapFaultLevel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 /**
  * Dialog box to edit the properties of a FaultLevel assertion
@@ -23,6 +26,7 @@ public class FaultLevelPropertiesDialog extends JDialog {
     private JPanel mainPanel;
     private JButton okButton;
     private JButton cancelButton;
+    private JComboBox levelBox;
 
     public FaultLevelPropertiesDialog(Frame owner, FaultLevel subject) {
         super(owner, TITLE, true);
@@ -33,5 +37,71 @@ public class FaultLevelPropertiesDialog extends JDialog {
     private void initialize() {
         setContentPane(mainPanel);
         Utilities.equalizeButtonSizes(new AbstractButton[] { okButton, cancelButton });
+
+        // populate the combo box with the possible levels
+        levelBox.setModel(new DefaultComboBoxModel(new LevelComboItems[] {
+                            new LevelComboItems(SoapFaultLevel.DROP_CONNECTION, "Drop Connection Completly"),
+                            new LevelComboItems(SoapFaultLevel.GENERIC_FAULT, "Generic SOAP Fault (no detail on failure)"),
+                            new LevelComboItems(SoapFaultLevel.MEDIUM_DETAIL_FAULT, "Medium Detail (SOAP fault with failure details)"),
+                            new LevelComboItems(SoapFaultLevel.FULL_TRACE_FAULT, "Full Detail (SOAP fault with complete policy evaluation details)"),
+                            new LevelComboItems(SoapFaultLevel.TEMPLATE_FAULT, "Template Fault (provide your own xml to return)")
+                          }));
+
+        okButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                ok();
+            }
+        });
+
+        cancelButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                cancel();
+            }
+        });
+
+        /* todo, add help button
+        helpButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                help();
+            }
+        });
+        */
+
+        Utilities.setEnterAction(this, new AbstractAction() {
+            public void actionPerformed(ActionEvent e) {
+                ok();
+            }
+        });
+        Utilities.setEscAction(this, new AbstractAction() {
+            public void actionPerformed(ActionEvent e) {
+                cancel();
+            }
+        });
+
+        // todo, other controls
+
+        // todo, set initial data
+    }
+
+    private void ok() {
+        // todo, some sort of validation
+        cancel();
+    }
+
+    private void cancel() {
+        dispose();
+    }
+
+    private class LevelComboItems {
+        private int level;
+        private String levelname;
+        public LevelComboItems(int level, String levelname) {
+            assert(levelname != null);
+            this.level = level;
+            this.levelname = levelname;
+        }
+        public String toString() {
+            return levelname;
+        }
     }
 }
