14bdbacf9ce1be683e1a2ca3cf75051786cbcc26
steve <steve@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2006-06-07 21:33:35 +0000

    Update Tomcat to 5.5.17. Modify the way client certs are trusted by the SSL connectors.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@13531 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/server/tomcat/SsgSSLImplementation.java
+++ src/com/l7tech/server/tomcat/SsgSSLImplementation.java
@@ -1,6 +1,17 @@
 package com.l7tech.server.tomcat;
 
 import java.net.Socket;
+import java.util.Properties;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+import java.io.File;
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.FileInputStream;
+import java.security.Security;
+import java.security.Provider;
 
 import org.apache.tomcat.util.net.SSLImplementation;
 import org.apache.tomcat.util.net.SSLSupport;
@@ -8,6 +19,11 @@ import org.apache.tomcat.util.net.ServerSocketFactory;
 
 /**
  * SSL implementation that wraps the default SSL factory in an SsgServerSocketFactory.
+ *
+ * <p>If the conf directory contains a properties file (SsgSSLImplementation.properties)
+ * with any attributes then these are set on any created socket factories.</p>
+ *
+ * <p>This will also register our crypto provider if it is available.</p>
  *
  * @author Steve Jones, $Author$
  * @version $Revision$
@@ -23,7 +39,15 @@ public class SsgSSLImplementation extends SSLImplementation {
      */
     public SsgSSLImplementation() {
         try {
-            delegate = getInstance();
+            String providerClass = "com.l7tech.server.tomcat.ClientTrustingProvider";
+            Security.addProvider((Provider) Class.forName(providerClass).newInstance());
+        }
+        catch(Exception e) {
+            // log as fine since the provider is optional.
+            logger.log(Level.FINE, "Could not configured ClientTrustingProvider", e);
+        }
+        try {
+              delegate = getInstance();
         }
         catch(Exception e) {
             throw (IllegalStateException) new IllegalStateException("Cannot create default SSLImplementation").initCause(e);
@@ -49,6 +73,30 @@ public class SsgSSLImplementation extends SSLImplementation {
      */
     public ServerSocketFactory getServerSocketFactory() {
         ServerSocketFactory ssf = delegate.getServerSocketFactory();
+
+        File propFile = new File("conf/SsgSSLImplementation.properties");
+        if (propFile.exists()) {
+            Properties props = new Properties();
+            InputStream in = null;
+            try {
+                in = new FileInputStream(propFile);
+                props.load(in);
+                for (Iterator propIter=props.entrySet().iterator(); propIter.hasNext();) {
+                    Map.Entry entry = (Map.Entry) propIter.next();
+                    ssf.setAttribute((String)entry.getKey(), entry.getValue());
+                }
+            }
+            catch(IOException ioe) {
+                logger.log(Level.WARNING, "Error reading connector properties.", ioe);
+            }
+            finally {
+                if (in != null) try {in.close();} catch(IOException ioe){}
+            }
+        }
+        else {
+            logger.info("No SSgSSLImplementation properties found ["+propFile.getAbsolutePath()+"]");
+        }
+
         return new SsgServerSocketFactory(ssf);
     }
 
@@ -60,6 +108,8 @@ public class SsgSSLImplementation extends SSLImplementation {
     }
 
     //- PRIVATE
+
+    private static final Logger logger = Logger.getLogger(SsgSSLImplementation.class.getName());
 
     private final SSLImplementation delegate;
 }
