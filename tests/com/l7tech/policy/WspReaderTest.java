e77e3778303065de1175faf330e4971ea19e43d1
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2006-06-26 21:07:13 +0000

    undid previous change (was wrong)
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@13644 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- tests/com/l7tech/policy/WspReaderTest.java
+++ tests/com/l7tech/policy/WspReaderTest.java
@@ -429,7 +429,7 @@ public class WspReaderTest extends TestCase {
         xslt.setResourceInfo(rinfo);
         String policy = WspWriter.getPolicyXml(xslt);
         XslTransformation newXslt = (XslTransformation) WspReader.parseStrictly(policy);
-        assertTrue(newXslt.getResourceInfo().equals(xslt.getResourceInfo()));
+        assertTrue(newXslt.equals(xslt));
     }
 
     public static void main(String[] args) {
