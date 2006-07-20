c9df9962f5789aa60f360b7c3b066ef3aae395e9
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2006-07-20 19:12:01 +0000

    Javadoc
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@13823 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/server/event/MessageProcessingEvent.java
+++ src/com/l7tech/server/event/MessageProcessingEvent.java
@@ -7,11 +7,12 @@ import org.springframework.context.ApplicationEvent;
 import com.l7tech.server.message.PolicyEnforcementContext;
 
 /**
- * Instances are created and fired by {@link MessageProcessor} and/or {@link ServerAssertion}s to
- * notify components of the status of message processing.
+ * Instances are created and fired by {@link com.l7tech.server.MessageProcessor} and/or
+ * {@link com.l7tech.server.policy.assertion.ServerAssertion}s to notify components of the status
+ * of message processing.
  * <p/>
  * In order to receive events, components must register as a Spring bean (e.g. in <code>webApplicationContext.xml</code>)
- * and implement {@link ApplicationListener}.
+ * and implement {@link org.springframework.context.ApplicationListener}.
  */
 public abstract class MessageProcessingEvent extends ApplicationEvent {
     protected final PolicyEnforcementContext context;
