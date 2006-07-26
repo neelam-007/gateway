c0ab5a81693dcefd6960d106480db55df4d6deaa
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2006-07-26 21:35:33 +0000

    AssertionStatus now provides a service to translate an int into an instance
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@13860 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/policy/assertion/AssertionStatus.java
+++ src/com/l7tech/policy/assertion/AssertionStatus.java
@@ -1,8 +1,10 @@
 package com.l7tech.policy.assertion;
 
-import java.util.logging.Level;
-
 import com.l7tech.common.util.SoapFaultUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
 
 /**
  * Typesafe enum with values for lots of conceivable error conditions (including NONE)
@@ -17,32 +19,34 @@ public class AssertionStatus {
     static final int POLICY = 600;
     static final int MISC   = 1000;
 
-    public static final AssertionStatus UNDEFINED         = make( -1, Level.INFO, "Undefined" );
+    private static final Map statuses = new HashMap();
+
+    public static final AssertionStatus UNDEFINED         = make( statuses, -1, Level.INFO, "Undefined" );
 
     /** Assertion finished successfully */
-    public static final AssertionStatus NONE              = make(          0, Level.FINE, "No Error" );
-    public static final AssertionStatus BAD_REQUEST       = make( CLIENT + 0, Level.INFO, "Bad Request" );
+    public static final AssertionStatus NONE              = make( statuses,          0, Level.FINE, "No Error" );
+    public static final AssertionStatus BAD_REQUEST       = make( statuses, CLIENT + 0, Level.INFO, "Bad Request" );
     /** Credentials required but missing */
-    public static final AssertionStatus AUTH_REQUIRED     = make( CLIENT + 1, Level.INFO, "Authentication Required", true );
+    public static final AssertionStatus AUTH_REQUIRED     = make( statuses, CLIENT + 1, Level.INFO, "Authentication Required", true );
     /** Credentials present but erroneous */
-    public static final AssertionStatus AUTH_FAILED       = make( CLIENT + 2, Level.WARNING, "Authentication Failed", true );
-    public static final AssertionStatus UNAUTHORIZED      = make( CLIENT + 2, Level.INFO, "Unauthorized", true );
-    public static final AssertionStatus SERVICE_DISABLED  = make( CLIENT + 3, Level.INFO, "Service Disabled" );
+    public static final AssertionStatus AUTH_FAILED       = make( statuses, CLIENT + 2, Level.WARNING, "Authentication Failed", true );
+    public static final AssertionStatus UNAUTHORIZED      = make( statuses, CLIENT + 2, Level.INFO, "Unauthorized", true );
+    public static final AssertionStatus SERVICE_DISABLED  = make( statuses, CLIENT + 3, Level.INFO, "Service Disabled" );
     /** Couldn't resolve a service for this request */
-    public static final AssertionStatus SERVICE_NOT_FOUND = make( CLIENT + 4, Level.INFO, "Service Not Found" );
+    public static final AssertionStatus SERVICE_NOT_FOUND = make( statuses, CLIENT + 4, Level.INFO, "Service Not Found" );
 
     /** Assertion not yet implemented. */
-    public static final AssertionStatus NOT_YET_IMPLEMENTED = make( MISC + 0, Level.INFO, "Not yet implemented!" );
-    public static final AssertionStatus NOT_APPLICABLE      = make( MISC + 1, Level.INFO, "Not applicable in this context" );
+    public static final AssertionStatus NOT_YET_IMPLEMENTED = make( statuses, MISC + 0, Level.INFO, "Not yet implemented!" );
+    public static final AssertionStatus NOT_APPLICABLE      = make( statuses, MISC + 1, Level.INFO, "Not applicable in this context" );
 
     /** Generic catch-all status */
-    public static final AssertionStatus SERVER_ERROR        = make( SERVER + 0, Level.INFO, "Internal Server Error" );
-    public static final AssertionStatus SERVER_AUTH_FAILED  = make( SERVER + 1, Level.INFO, "Access Denied by Protected Service" );
-    public static final AssertionStatus BAD_RESPONSE        = make( SERVER + 2, Level.INFO, "Bad Response from Protected Service" );
+    public static final AssertionStatus SERVER_ERROR        = make( statuses, SERVER + 0, Level.INFO, "Internal Server Error" );
+    public static final AssertionStatus SERVER_AUTH_FAILED  = make( statuses, SERVER + 1, Level.INFO, "Access Denied by Protected Service" );
+    public static final AssertionStatus BAD_RESPONSE        = make( statuses, SERVER + 2, Level.INFO, "Bad Response from Protected Service" );
 
     /** A generic negative result */
-    public static final AssertionStatus FALSIFIED = make( POLICY + 0, Level.INFO, "Assertion Falsified" );
-    public static final AssertionStatus FAILED    = make( POLICY + 1, Level.INFO, "Error in Assertion Processing" );
+    public static final AssertionStatus FALSIFIED = make( statuses, POLICY + 0, Level.INFO, "Assertion Falsified" );
+    public static final AssertionStatus FAILED    = make( statuses, POLICY + 1, Level.INFO, "Error in Assertion Processing" );
 
     public int getNumeric() {
         return _numeric;
@@ -67,19 +71,25 @@ public class AssertionStatus {
             return SoapFaultUtils.FC_SERVER;
     }
 
-    private static AssertionStatus make( int numeric, Level level, String message, boolean auth ) {
-        return new AssertionStatus( numeric, level, message, auth );
+    private static AssertionStatus make( Map registry, int numeric, Level level, String message, boolean auth ) {
+        return new AssertionStatus( registry, numeric, level, message, auth );
     }
 
-    private static AssertionStatus make( int numeric, Level level, String message ) {
-        return make( numeric, level, message, false );
+    private static AssertionStatus make( Map registry, int numeric, Level level, String message ) {
+        return make( registry, numeric, level, message, false );
     }
 
-    private AssertionStatus( int numeric, Level level, String message, boolean auth ) {
+    private AssertionStatus( Map registry, int numeric, Level level, String message, boolean auth ) {
         _numeric = numeric;
         _level = level;
         _message = message;
         _authProblem = auth;
+        registry.put(new Integer(numeric), this);
+    }
+
+    /** @return assertion status for this integer, or null. */
+    public static AssertionStatus fromInt(int status) {
+        return (AssertionStatus)statuses.get(new Integer(status));
     }
 
     public String toString() {
