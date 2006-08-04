9b194ac68680e43ca6ec8ae6c6fde21eef33f049
steve <steve@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2006-08-04 20:13:42 +0000

    Fix inverse http form post.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@13971 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/server/policy/assertion/ServerInverseHttpFormPost.java
+++ src/com/l7tech/server/policy/assertion/ServerInverseHttpFormPost.java
@@ -45,39 +45,42 @@ public class ServerInverseHttpFormPost extends AbstractServerAssertion implement
     public AssertionStatus checkRequest(PolicyEnforcementContext context) throws IOException, PolicyAssertionException {
         Message request = context.getRequest();
         MimeKnob reqMime = request.getMimeKnob();
-        BufferPoolByteArrayOutputStream baos = new BufferPoolByteArrayOutputStream(512);
-        for (int i = 0; i < assertion.getFieldNames().length; i++) {
-            String fieldName = assertion.getFieldNames()[i];
-            try {
-                PartInfo partInfo = reqMime.getPart(i);
-                ContentTypeHeader ctype = partInfo.getContentType();
-                InputStream partStream = partInfo.getInputStream(true);
-                byte[] partBytes = HexUtils.slurpStreamLocalBuffer(partStream);
-                if (partBytes.length >= HexUtils.LOCAL_BUFFER_SIZE) {
-                    auditor.logAndAudit(AssertionMessages.INVERSE_HTTPFORM_TOO_BIG, new String[] { Integer.toString(i) });
-                    baos.close();
+        BufferPoolByteArrayOutputStream baos = null;
+        try {
+            baos = new BufferPoolByteArrayOutputStream(512);
+            for (int i = 0; i < assertion.getFieldNames().length; i++) {
+                String fieldName = assertion.getFieldNames()[i];
+                try {
+                    PartInfo partInfo = reqMime.getPart(i);
+                    ContentTypeHeader ctype = partInfo.getContentType();
+                    InputStream partStream = partInfo.getInputStream(true);
+                    byte[] partBytes = HexUtils.slurpStreamLocalBuffer(partStream);
+                    if (partBytes.length >= HexUtils.LOCAL_BUFFER_SIZE) {
+                        auditor.logAndAudit(AssertionMessages.INVERSE_HTTPFORM_TOO_BIG, new String[] { Integer.toString(i) });
+                        return AssertionStatus.FAILED;
+                    }
+
+                    baos.write(fieldName.getBytes(ENCODING));
+                    baos.write("=".getBytes());
+                    baos.write(URLEncoder.encode(new String(partBytes, ctype.getEncoding()), ENCODING).getBytes(ENCODING));
+                    if (assertion.getFieldNames().length < i) baos.write("&".getBytes());
+                } catch (NoSuchPartException e) {
+                    auditor.logAndAudit(AssertionMessages.INVERSE_HTTPFORM_NO_SUCH_PART, new String[] { Integer.toString(i) });
                     return AssertionStatus.FAILED;
                 }
+            }
 
-                baos.write(fieldName.getBytes(ENCODING));
-                baos.write("=".getBytes());
-                baos.write(URLEncoder.encode(new String(partBytes, ctype.getEncoding()), ENCODING).getBytes(ENCODING));
-                if (assertion.getFieldNames().length < i) baos.write("&".getBytes());
+            try {
+                request.initialize(StashManagerFactory.createStashManager(),
+                        ContentTypeHeader.parseValue("application/" + HttpFormPost.X_WWW_FORM_URLENCODED),
+                        new ByteArrayInputStream(baos.toByteArray()));
+                return AssertionStatus.NONE;
             } catch (NoSuchPartException e) {
-                auditor.logAndAudit(AssertionMessages.INVERSE_HTTPFORM_NO_SUCH_PART, new String[] { Integer.toString(i) });
-                baos.close();
-                return AssertionStatus.FAILED;
+                throw new PolicyAssertionException(assertion, e);
             }
         }
-        baos.close();
-
-        try {
-            request.initialize(StashManagerFactory.createStashManager(),
-                    ContentTypeHeader.parseValue("application/" + HttpFormPost.X_WWW_FORM_URLENCODED),
-                    new ByteArrayInputStream(baos.toByteArray()));
-            return AssertionStatus.NONE;
-        } catch (NoSuchPartException e) {
-            throw new PolicyAssertionException(assertion, e);
+        finally {
+            if (baos != null) baos.close();
         }
     }
 }
