047dfd4a1fa2da769c9b60fc386422d191b34e7b
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2006-08-19 01:34:28 +0000

    Add placeholder for apparently-missing class so the tree will compile
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@14079 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/common/security/rbac/RbacUtilities.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2005 Layer 7 Technologies Inc.
+ *
+ */
+
+package com.l7tech.common.security.rbac;
+
+/**
+ * Placeholder for apparently-uncommitted class.  Replace with the real one on Monday.
+ */
+public class RbacUtilities {
+    public static boolean isEnableRoleEditing() {
+        // TODO need the real impl of this method
+        return false;
+    }
+}
