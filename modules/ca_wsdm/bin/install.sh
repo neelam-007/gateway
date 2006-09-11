bf85a62e86304bc314dbe906ac809a3387dca577
megery <megery@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2006-09-11 23:23:17 +0000

    fixed third part connector installers so they no longer chown the entire /ssg tree to gateway.gateway. they now only chown the files or directories that they create or copy.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@14270 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/ca_wsdm/bin/install.sh
+++ modules/ca_wsdm/bin/install.sh
@@ -49,30 +49,45 @@ fi
 if [ -e WsdmSOMMA_Basic.properties.EDIT ]; then rm WsdmSOMMA_Basic.properties.EDIT; fi
 sed "s|^log.file.path=.*|log.file.path=${SSG_HOME}/logs/ca_wsdm_observer|" ssg/tomcat/webapps/ROOT/WEB-INF/classes/WsdmSOMMA_Basic.properties > WsdmSOMMA_Basic.properties.EDIT
 mv WsdmSOMMA_Basic.properties.EDIT ssg/tomcat/webapps/ROOT/WEB-INF/classes/WsdmSOMMA_Basic.properties
+chown -f gateway.gateway ssg/tomcat/webapps/ROOT/WEB-INF/classes/WsdmSOMMA_Basic.properties
 
 # Copy files with forced overwrite.
 cp -f ssg/tomcat/webapps/ROOT/WEB-INF/CaWsdmObserverContext.xml "${SSG_HOME}/tomcat/webapps/ROOT/WEB-INF/"
+chown -f gateway.gateway "${SSG_HOME}/tomcat/webapps/ROOT/WEB-INF/CaWsdmObserverContext.xml"
+
 cp -f ssg/tomcat/webapps/ROOT/WEB-INF/lib/axis-1.3.jar "${SSG_HOME}/tomcat/webapps/ROOT/WEB-INF/lib/"
+chown -f gateway.gateway "${SSG_HOME}/tomcat/webapps/ROOT/WEB-INF/lib/axis-1.3.jar"
+
 cp -f ssg/tomcat/webapps/ROOT/WEB-INF/lib/ca_wsdm_observer.jar "${SSG_HOME}/tomcat/webapps/ROOT/WEB-INF/lib/"
+chown -f gateway.gateway "${SSG_HOME}/tomcat/webapps/ROOT/WEB-INF/lib/ca_wsdm_observer.jar"
+
 cp -f ssg/tomcat/webapps/ROOT/WEB-INF/lib/ca_wsdm-3.50-core.jar "${SSG_HOME}/tomcat/webapps/ROOT/WEB-INF/lib/"
+chown -f gateway.gateway "${SSG_HOME}/tomcat/webapps/ROOT/WEB-INF/lib/ca_wsdm-3.50-core.jar"
+
 cp -f ssg/tomcat/webapps/ROOT/WEB-INF/lib/ca_wsdm-3.50-handler_common.jar "${SSG_HOME}/tomcat/webapps/ROOT/WEB-INF/lib/"
+chown -f gateway.gateway "${SSG_HOME}/tomcat/webapps/ROOT/WEB-INF/lib/ca_wsdm-3.50-handler_common.jar"
+
 cp -f ssg/tomcat/webapps/ROOT/WEB-INF/lib/ca_wsdm-3.50-wsdm35mmi-axis-stubskel.jar "${SSG_HOME}/tomcat/webapps/ROOT/WEB-INF/lib/"
+chown -f gateway.gateway "${SSG_HOME}/tomcat/webapps/ROOT/WEB-INF/lib/ca_wsdm-3.50-wsdm35mmi-axis-stubskel.jar"
+
 cp -f ssg/tomcat/webapps/ROOT/WEB-INF/lib/tmxmltoolkit.jar "${SSG_HOME}/tomcat/webapps/ROOT/WEB-INF/lib/"
+chown -f gateway.gateway "${SSG_HOME}/tomcat/webapps/ROOT/WEB-INF/lib/tmxmltoolkit.jar"
 
 # Copy files without forced overwrite.
 if [ -e "${SSG_HOME}/tomcat/webapps/ROOT/WEB-INF/classes/WsdmSOMMA_Basic.properties" ]; then
     echo "The file ${SSG_HOME}/tomcat/webapps/ROOT/WEB-INF/classes/WsdmSOMMA_Basic.properties already exists. It will not be overwritten."
 else
     cp ssg/tomcat/webapps/ROOT/WEB-INF/classes/WsdmSOMMA_Basic.properties "${SSG_HOME}/tomcat/webapps/ROOT/WEB-INF/classes/"
+    chown -f gateway.gateway "${SSG_HOME}/tomcat/webapps/ROOT/WEB-INF/classes/WsdmSOMMA_Basic.properties"
 fi
 
 if [ -e "${SSG_HOME}/etc/conf/ca_wsdm_observer.properties" ]; then
     echo "The file ${SSG_HOME}/etc/conf/ca_wsdm_observer.properties already exists. It will not be overwritten."
 else
     cp ssg/etc/conf/ca_wsdm_observer.properties "${SSG_HOME}/etc/conf/"
+    chown -f gateway.gateway "${SSG_HOME}/etc/conf/ca_wsdm_observer.properties"
 fi
 
-chown -R gateway.gateway /ssg/*
 
 echo "Installation of Observer for CA Unicenter WSDM complete."
 echo "You may need to review and update the configuration in:"
