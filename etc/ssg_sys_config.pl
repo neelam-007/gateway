3e390fc77288991389693e6fb05fa34b080f25e3
megery <megery@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2006-09-12 23:45:22 +0000

    bugz 2899: paths have been corrected and logging for the daemon has been added. Now with much more WORKING.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@14281 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- etc/ssg_sys_config.pl
+++ etc/ssg_sys_config.pl
@@ -3,18 +3,28 @@
 use strict;
 use FileHandle;
 
+my $timestamp = localtime time;
+my $logFile = new FileHandle;
+if ($logFile->open(">>/ssg/sysconfigwizard/ssgsysconfig.log")) {
+	$logFile->print("\n-------------------------------------------------------------------\n");
+	$logFile->print("$timestamp: System Configuration Started\n");
+	$logFile->print("-------------------------------------------------------------------\n");
+} else {
+ 	print("Could not open log file. Will not proceed with system configuration\n");
+}
+
 my @commandArray;
-my $hostnameToUse = "";
-my $ntpServerToUse = "";
+my $hostnameToUse = undef;
+my $ntpServerToUse = undef;
 my @filesToDelete;
 
 my $netConfigCommand = "/usr/sbin/netconfig";
-my $netConfigPattern = "configfiles/netconfig_*";
+my $netConfigPattern = "/ssg/sysconfigwizard/configfiles/netconfig_*";
 
 my $inputFh = new FileHandle;
 my %inputFiles = (
-    "HOSTNAMEFILE" => "configfiles/hostname",
-    "NTPFILE" => "configfiles/ntpconfig"
+    "HOSTNAMEFILE" => "/ssg/sysconfigwizard/configfiles/hostname",
+    "NTPFILE" => "/ssg/sysconfigwizard/configfiles/ntpconfig"
 );
 
 my $outputFh = new FileHandle;
@@ -23,7 +33,6 @@ my %outputFiles = (
     "NTPCONFFILE" => "/etc/ntp.conf",
     "STEPTICKERSFILE" => "/etc/ntp/step-tickers"
 );
-
 
 
 my @netConfigFiles = glob($netConfigPattern);
@@ -45,7 +54,10 @@ if ($inputFh->open("<$inputFiles{'HOSTNAMEFILE'}")) {
 	$hostnameToUse = $hostnames[0];
 	chomp($hostnameToUse);
 	push (@filesToDelete, $inputFiles{'HOSTNAMEFILE'});
+	$logFile->print("$timestamp: hostname found: $hostnameToUse\n");
 	$inputFh->close();
+} else {
+	$logFile->print("$timestamp: $inputFiles{'HOSTNAMEFILE'} not found. The hostname will not be changed.\n");
 }
 
 if ($inputFh->open("<$inputFiles{'NTPFILE'}")) {
@@ -53,34 +65,62 @@ if ($inputFh->open("<$inputFiles{'NTPFILE'}")) {
 	$ntpServerToUse = $serverNames[0];
 	chomp($ntpServerToUse);
 	push (@filesToDelete, $inputFiles{'NTPFILE'});
+	$logFile->print("$timestamp: NTP server found: $ntpServerToUse\n");
 	$inputFh->close();
+} else {
+	$logFile->print("$timestamp: $inputFiles{'NTPFILE'} not found. The NTP configuration will not be changed.\n");
 }
 
 #configure the network interfaces
 for my $command (@commandArray) {
+	$logFile->print("$timestamp: executing network configuration command:\n");
+	$logFile->print("\t$netConfigCommand $command\n");
 	`$netConfigCommand $command`;
 }
 
 #enable networking and setup hostname
-if ($outputFh->open(">$outputFiles{'NETFILE'}")) {
-	$outputFh->print("NETWORKING=yes\n");
-	$outputFh->print("HOSTNAME=$hostnameToUse\n");
-	$outputFh->close();
+if (defined($hostnameToUse)) {
+	if ($outputFh->open(">$outputFiles{'NETFILE'}")) {
+	   $outputFh->print("NETWORKING=yes\n");
+	   $outputFh->print("HOSTNAME=$hostnameToUse\n");
+	   $outputFh->close();
+
+	   system("hostname $hostnameToUse");
+
+	   $logFile->print("$timestamp: Wrote $outputFiles{'NETFILE'} with:\n");
+	   $logFile->print("\tNETWORKING=yes\n");
+	   $logFile->print("\tHOSTNAME=$hostnameToUse\n");
+	} else {
+		$logFile->print("$timestamp: Couldn't open $outputFiles{'NETFILE'}. Skipping Network configuration\n");
+	}
 }
 
 #configure NTP
-if ($outputFh->open(">$outputFiles{'NTPCONFFILE'}")) {
-	$outputFh->print("server $ntpServerToUse\n");
-	$outputFh->print("restrict $ntpServerToUse mask 255.255.255.255 nomodify notrap noquery\n");
-	$outputFh->close();
+if (defined($ntpServerToUse)) {
+	if ($outputFh->open(">$outputFiles{'NTPCONFFILE'}")) {
+	    $outputFh->print("server $ntpServerToUse\n");
+	    $outputFh->print("restrict $ntpServerToUse mask 255.255.255.255 nomodify notrap noquery\n");
+	    $outputFh->close();
+
+	    $logFile->print("$timestamp: Wrote $outputFiles{'NTPCONFFILE'} with:\n");
+	    $logFile->print("\tserver $ntpServerToUse\n");
+	    $logFile->print("\trestrict $ntpServerToUse mask 255.255.255.255 nomodify notrap noquery\n");
+	} else {
+		$logFile->print("$timestamp: Couldn't open $outputFiles{'NTPCONFGILE'}. Skipping NTP configuration\n");
+	}
+
+	if ($outputFh->open(">$outputFiles{'STEPTICKERSFILE'}")) {
+    		$outputFh->print("$ntpServerToUse\n");
+    		$outputFh->close();
+
+    		$logFile->print("$timestamp: Wrote $outputFiles{'STEPTICKERSFILE'} with:\n");
+    		$logFile->print("\t$ntpServerToUse\n");
+	} else {
+		$logFile->print("$timestamp: Couldn't open $outputFiles{'STEPTICKERSFILE'}. Skipping step tickers configuration\n");
+	}
 }
 
-if ($outputFh->open(">$outputFiles{'STEPTICKERSFILE'}")) {
-    $outputFh->print("$ntpServerToUse\n");
-    $outputFh->close();
-}
-
-my $backupFilename = "configfiles/sysconfigsettings.tgz";
+#my $backupFilename = "configfiles/sysconfigsettings.tgz";
 unlink(@filesToDelete);
-
+$logFile->close();
 
