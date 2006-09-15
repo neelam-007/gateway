278b56babb04a4a60a12a7b6ad3163123732a773
steve <steve@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2006-09-15 23:16:15 +0000

    Fix for bug 2919 (LogAndAudit causes performance loss)
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@14300 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/server/audit/ThreadLocalAuditContext.java
@@ -0,0 +1,65 @@
+package com.l7tech.server.audit;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.beans.BeansException;
+
+import com.l7tech.common.audit.AuditContext;
+import com.l7tech.common.audit.AuditDetail;
+import com.l7tech.common.audit.AuditRecord;
+
+/**
+ * AuditContext implementation that delegates to a thread-local copy.
+ *
+ * <p>You need to set the targetId of the bean to delegate to. Be sure that it
+ * is not a singleton!</p>
+ *
+ * @author Steve Jones, $Author$
+ * @version $Revision$
+ */
+public class ThreadLocalAuditContext implements AuditContext, ApplicationContextAware {
+
+    //- PUBLIC
+
+    public void setTargetId(final String id) {
+        this.targetId = id;
+    }
+
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    public void addDetail(AuditDetail detail, Object source) {
+        threadLocalDelegate.get().addDetail(detail, source);
+    }
+
+    public void flush() {
+        threadLocalDelegate.get().flush();
+    }
+
+    public Map<Object, List<AuditDetail>> getDetails() {
+        return threadLocalDelegate.get().getDetails();
+    }
+
+    public Set getHints() {
+        return threadLocalDelegate.get().getHints();
+    }
+
+    public void setCurrentRecord(AuditRecord record) {
+        threadLocalDelegate.get().setCurrentRecord(record);
+    }
+
+    //- PRIVATE
+
+    private String targetId;
+    private ApplicationContext applicationContext;
+    private ThreadLocal<AuditContext> threadLocalDelegate = new ThreadLocal<AuditContext>(){
+        protected AuditContext initialValue() {
+            return (AuditContext) applicationContext.getBean(targetId, AuditContext.class);
+        }
+    };
+}
