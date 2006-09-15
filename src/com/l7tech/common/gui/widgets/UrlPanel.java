8bad056e43bf1bcc1c02f3a8f82743071359907b
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2006-09-15 20:43:42 +0000

    dont try to validate url when it contains variables [bugzilla #2921, #2915]
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@14295 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/common/gui/widgets/UrlPanel.java
+++ src/com/l7tech/common/gui/widgets/UrlPanel.java
@@ -4,6 +4,7 @@
 package com.l7tech.common.gui.widgets;
 
 import com.l7tech.common.util.ExceptionUtils;
+import com.l7tech.policy.variable.ExpandVariables;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -29,6 +30,11 @@ public class UrlPanel extends TextEntryPanel {
 
     protected String getSemanticError(Object model) {
         if (model == null || model.toString().length() == 0) return null;
+        // if the URL contains context variable, you just can't check semantic
+        String[] tmp = ExpandVariables.getReferencedNames(model.toString());
+        if (tmp != null && tmp.length > 0) {
+            return null;
+        }
         InputStream readme = null;
         try {
             URLConnection conn = new URL(model.toString()).openConnection();
@@ -48,6 +54,11 @@ public class UrlPanel extends TextEntryPanel {
 
     protected String getSyntaxError(Object model) {
         if (model == null || model.toString().length() == 0) return null;
+        // if the URL contains context variable, you just can't check syntax
+        String[] tmp = ExpandVariables.getReferencedNames(model.toString());
+        if (tmp != null && tmp.length > 0) {
+            return null;
+        }
         try {
             URL url = new URL(model.toString());
             if (url.getHost() == null || url.getHost().length() == 0) {
