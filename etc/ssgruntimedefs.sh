42c33e8404bd31b1d2f60fad06e352e1e75dbd9d
jay <jay@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2006-09-25 18:05:36 +0000

    Bugz 2958: GC interval for RMI
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@14369 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- etc/ssgruntimedefs.sh
+++ etc/ssgruntimedefs.sh
@@ -30,20 +30,21 @@ system_ram=`grep MemTotal /proc/meminfo |cut -c 15-23`
 multiplier="2/3"
 
 let java_ram="$system_ram*$multiplier" 
-if [ `expr $java_ram \> 2274412` == 1 ]; then
+if [ `expr $java_ram \> 2074412` == 1 ]; then
 	# we have more ram than java can use
 	# FIXME: when we start running 64 bit JVM
-	java_ram=2274412;
+	java_ram=2074412;
 fi
 let maxnewsize="$java_ram*75/100"
 
-default_java_opts="-Dcom.l7tech.common.http.prov.apache.CommonsHttpClient.maxConnectionsPerHost=750 -Djava.net.preferIPv4Stack=true "
+default_java_opts="-server -Dcom.l7tech.common.http.prov.apache.CommonsHttpClient.maxConnectionsPerHost=750 -Djava.net.preferIPv4Stack=true "
+default_java_opts="$default_java_opts -Dsun.rmi.dgc.server.gcInterval=3600000 -Dsun.rmi.dgc.client.gcInterval=3600000 "
 default_java_opts="$default_java_opts -Dcom.l7tech.common.http.prov.apache.CommonsHttpClient.maxTotalConnections=7500"
 default_java_opts="$default_java_opts -Djavax.xml.transform.TransformerFactory=org.apache.xalan.processor.TransformerFactoryImpl"
 default_java_opts="$default_java_opts -Dfile.encoding=UTF-8 -Dsun.net.inetaddr.ttl=30 -Dnetworkaddress.cache.ttl=30"
-default_java_opts="$default_java_opts -server -Djava.awt.headless=true"
+default_java_opts="$default_java_opts -Djava.awt.headless=true -XX:CompileThreshold=1500 "
 default_java_opts="$default_java_opts -Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.Jdk14Logger"
-default_java_opts="$default_java_opts -Xmx${java_ram}k -Xms${java_ram}k -Xmn${maxnewsize}k -Xss192k "
+default_java_opts="$default_java_opts -Xmx${java_ram}k -Xss192k "
 
 unset system_ram
 unset multiplier
