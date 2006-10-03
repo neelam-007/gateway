2c5b6894d3c992a28c2fb45276c36f150bd86cd3
steve <steve@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2006-10-03 21:21:37 +0000

    Build modifications.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@14485 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ etc/Bridge.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# Bridge Startup script for *nix systems
+
+
+if [ -z "$JAVA_OPTS" ]; then
+	# we don't have java opts, so we set them ourselves
+	JAVA_OPTS=" -Xms128M -Xmx256M -Xss256k -server ";
+elif [ `expr "$JAVA_OPTS" : ".*headless.*"` != 0 ]; then
+       # We look in $JAVA_OPTS ... if java.awt.headless mode is there
+       # then we've likely got the default options for SSG and it would prevent a gui
+       # from coming up. So we over-write them with the following
+       JAVA_OPTS=" -Xms128M -Xmx256M -Xss256k -server ";
+fi
+
+# set current dir to where this script is
+cd `dirname $0`
+
+# include startup options 
+extra="-server -Dcom.l7tech.proxy.listener.maxthreads=300  -Dsun.net.inetaddr.ttl=10 -Dnetworkaddress.cache.ttl=10 -Dfile.encoding=UTF-8"
+
+if [ "$1" = "-bd" ]; then
+	run="-classpath Bridge.jar com.l7tech.proxy.Main"
+elif [ "$1" = "-config" ]; then
+    run="-classpath Bridge.jar com.l7tech.proxy.cli.Main"
+else
+	run="-jar Bridge.jar"
+fi
+
+
+
+if [ -z "$JAVA_HOME" ]; then
+        echo "No JAVA_HOME set, exiting"
+        exit;
+fi
+       
+$JAVA_HOME/bin/java $JAVA_OPTS $extra $run
---
+++ etc/Manager.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# Manager Startup script for *nix systems
+
+
+if [ -z "$JAVA_OPTS" ]; then
+	# we don't have java opts, so we set them ourselves
+	JAVA_OPTS=" -Xms128M -Xmx256M -Xss256k -server ";
+elif [ `expr "$JAVA_OPTS" : ".*headless.*"` != 0 ]; then
+       # We look in $JAVA_OPTS ... if java.awt.headless mode is there
+       # then we've likely got the default options for SSG and it would prevent a gui
+       # from coming up. So we over-write them with the following
+       JAVA_OPTS=" -Xms128M -Xmx256M -Xss256k -server ";
+fi
+
+# set current dir to where this script is
+cd `dirname $0`
+
+# include startup options 
+extra="-server -Dcom.l7tech.proxy.listener.maxthreads=300  -Dsun.net.inetaddr.ttl=10 -Dnetworkaddress.cache.ttl=10 -Dfile.encoding=UTF-8"
+
+run="-jar Manager.jar";
+
+
+
+if [ -z "$JAVA_HOME" ]; then
+        echo "No JAVA_HOME set, exiting"
+        exit;
+fi
+       
+$JAVA_HOME/bin/java $JAVA_OPTS $extra $run
