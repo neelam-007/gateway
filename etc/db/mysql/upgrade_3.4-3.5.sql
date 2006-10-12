7b208cc6d1109c9ee14437129b825779ecb2ba66
megery <megery@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2006-10-12 23:30:40 +0000

    merged with the version from 3.5 since somehow they got out of sync. The version of this script in the 3.6 build and later was not correct and was missing the upgrade to the cluster_properties table ... this causes problems when upgrading to 3.6 from something prior to 3.5 since the 3.6 upgrade script adds content to the cluster_properties table that won't work because the table is not the correct structure
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@14525 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- etc/db/mysql/upgrade_3.4-3.5.sql
+++ etc/db/mysql/upgrade_3.4-3.5.sql
@@ -20,8 +20,8 @@ CREATE TABLE service_metrics (
   resolution INTEGER NOT NULL,
   period_start BIGINT(20) NOT NULL,
   start_time BIGINT(20) NOT NULL,
-  end_time BIGINT(20) NOT NULL,
   interval_size INTEGER NOT NULL,
+  end_time BIGINT(20) NOT NULL,
   attempted INTEGER NOT NULL,
   authorized INTEGER NOT NULL,
   completed INTEGER NOT NULL,
@@ -31,5 +31,35 @@ CREATE TABLE service_metrics (
   front_min INTEGER NOT NULL,
   front_max INTEGER NOT NULL,
   front_sum INTEGER NOT NULL,
+  INDEX i_sm_nodeid (nodeid),
+  INDEX i_sm_serviceoid (published_service_oid),
+  INDEX i_sm_resolution (resolution),
+  INDEX i_sm_pstart (period_start),
   PRIMARY KEY (nodeid, published_service_oid, resolution, period_start)
 ) TYPE=InnoDB;
+
+------------------------
+-- Cluster Properties --
+------------------------
+
+alter table cluster_properties drop primary key;
+
+alter table cluster_properties add objectid bigint(20) not null primary key auto_increment;
+alter table cluster_properties add version integer not null;
+
+-- Removes auto_increment but still primary key
+alter table cluster_properties change objectid objectid bigint(20) not null;
+
+create unique index i_cp_propkey on cluster_properties (propkey);
+
+-----------------------
+-- Community Schemas --
+-----------------------
+
+alter table community_schemas add version integer not null;
+alter table community_schemas change `schema` schema_xml mediumtext default '';
+
+------------------------------------
+-- GLOBAL COUNTERS IN CLUSTER FIX --
+------------------------------------
+alter table counters modify column userid varchar(128) NOT NULL;
