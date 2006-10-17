2df1773c8fdc37c7fa9c3265ca33b6a97d7d2d2a
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2006-10-17 22:48:24 +0000

    jdoc implications of this interface
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@14540 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/policy/assertion/UsesVariables.java
+++ src/com/l7tech/policy/assertion/UsesVariables.java
@@ -7,6 +7,9 @@ package com.l7tech.policy.assertion;
  * Indicates that this assertion can make use of variables.  {@link #getVariablesUsed()}
  * should return the list of variables that are actually known to be required by this
  * assertion in its current configuration.
+ *
+ * Warning. This is a hard contract: if your assertion does not implement this interface and
+ * tries to access a variable at runtime, it may not be able to access it.
  */
 public interface UsesVariables {
     String[] getVariablesUsed();
