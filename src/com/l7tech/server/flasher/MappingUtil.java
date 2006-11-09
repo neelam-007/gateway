81e7d9deb6cde50dd3da09b38054f42e3a33d891
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2006-11-09 05:14:02 +0000

    wrote code creating template mapping file
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@14630 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/server/flasher/MappingUtil.java
+++ src/com/l7tech/server/flasher/MappingUtil.java
@@ -1,5 +1,18 @@
 package com.l7tech.server.flasher;
 
+import com.l7tech.common.util.XmlUtil;
+
+import java.sql.*;
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.io.IOException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
 /**
  * Methods facilitating the mapping of ip addresses between staging SSG instances.
  * <p/>
@@ -10,7 +23,123 @@ package com.l7tech.server.flasher;
  * Date: Nov 8, 2006<br/>
  */
 public class MappingUtil {
+
+    private static final Pattern ipaddresspattern = Pattern.compile("\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}");
+
     public static void produceTemplateMappingFileFromDump(String dumpFilePath, String outputTemplatePath) {
         // todo, regexes!
+    }
+
+    public static void produceTemplateMappingFileFromDatabaseConnection(String dburl, String dbuser,
+                                                                        String dbpasswd, String outputTemplatePath) throws SQLException, SAXException, IOException {
+        // create connection to database
+        com.mysql.jdbc.Driver driver = new com.mysql.jdbc.Driver();
+        Properties props = new Properties();
+        props.put("user", "gateway");
+        props.put("password", "7layer");
+        Connection c = driver.connect("jdbc:mysql://localhost/ssg?failOverReadOnly=false&autoReconnect=false&socketTimeout=120000&useNewIO=true&characterEncoding=UTF8&characterSetResults=UTF8", props);
+
+        // go through the cluster properties
+        HashMap<String, String> mapOfClusterProperties = new HashMap<String, String>();
+        Statement s = c.createStatement();
+        ResultSet rs = s.executeQuery("select propkey, propvalue from cluster_properties;");
+        while (rs.next()) {
+            String value = rs.getString(2);
+            String key = rs.getString(1);
+            if (!key.equals("license")) mapOfClusterProperties.put(key, value);
+        }
+        rs.close();
+
+        // go through the policies
+        ArrayList<String> ipaddressesInRoutingAssertions = new ArrayList<String>();
+        rs = s.executeQuery("select policy_xml from published_service;");
+        while (rs.next()) {
+            String xml = rs.getString(1);
+            Document doc = XmlUtil.stringToDocument(xml);
+            NodeList nl = doc.getElementsByTagNameNS("http://www.layer7tech.com/ws/policy", "HttpRoutingAssertion");
+            for (int i = 0; i < nl.getLength(); i++) {
+                Element ra = (Element)nl.item(i);
+                Element cia = XmlUtil.findFirstChildElementByName(ra, "http://www.layer7tech.com/ws/policy",
+                                                                      "CustomIpAddresses");
+                List listofitems = XmlUtil.findChildElementsByName(cia, "http://www.layer7tech.com/ws/policy", "item");
+                for (Iterator iterator = listofitems.iterator(); iterator.hasNext();) {
+                    Element el =  (Element)iterator.next();
+                    ipaddressesInRoutingAssertions.add(el.getAttribute("stringValue"));
+                }
+            }
+        }
+        rs.close();
+
+        s.close();
+        c.close();
+        Document outputdoc = XmlUtil.createEmptyDocument("ssgimportmapping", "L7flash",
+                                                         "http://www.layer7tech.com/flashing/stagingmapping");
+        Element backendipmappingEl = XmlUtil.createAndAppendElementNS(outputdoc.getDocumentElement(),
+                                                                      "backendipmapping",
+                                                                      "http://www.layer7tech.com/flashing/stagingmapping",
+                                                                      "L7flash");
+        for (String routingip : ipaddressesInRoutingAssertions) {
+            Element ipmapel = XmlUtil.createAndAppendElementNS(backendipmappingEl, "ipmap",
+                    "http://www.layer7tech.com/flashing/stagingmapping",
+                    "L7flash");
+            ipmapel.setAttribute("source", routingip);
+            ipmapel.setAttribute("target", "__add_your_value__");
+
+        }
+
+        Element globalvarmappingEl = XmlUtil.createAndAppendElementNS(outputdoc.getDocumentElement(),
+                                                                      "globalvarmapping",
+                                                                      "http://www.layer7tech.com/flashing/stagingmapping",
+                                                                      "L7flash");
+
+        for (String propKey: mapOfClusterProperties.keySet()) {
+            Element varmapEl = XmlUtil.createAndAppendElementNS(globalvarmappingEl, "varmap",
+                    "http://www.layer7tech.com/flashing/stagingmapping",
+                    "L7flash");
+            varmapEl.setAttribute("name", propKey);
+            varmapEl.setAttribute("value", mapOfClusterProperties.get(propKey));
+        }
+
+        System.out.println(XmlUtil.nodeToFormattedString(outputdoc));
+    }
+
+    public static String extractIpAddressFromString(String input) {
+        Matcher m = ipaddresspattern.matcher(input);
+        if (m.find()) {
+            return input.substring(m.start(), m.end());
+        } else {
+            return null;
+        }
+    }
+
+    /*
+    NOTES
+
+    in a sql dump,
+
+    an ip address inside a cluster property has the following pattern: ",'192.168.19.5')"
+    two ip address inside a routing assertions has the following pattern: "<L7p:CustomIpAddresses stringArrayValue=\"included\">\n                <L7p:item stringValue=\"192.168.19.19\"/>\n                <L7p:item stringValue=\"192.168.19.20\"/>\n            </L7p:CustomIpAddresses>\n"
+
+
+    in policy xml format, ip addresses added to a routing assertion look like:
+           <L7p:HttpRoutingAssertion>
+                <L7p:ProtectedServiceUrl stringValue="http://localhost:8080/parlayx/services/SendSms"/>
+                <L7p:CustomIpAddresses stringArrayValue="included">
+                    <L7p:item stringValue="192.168.19.1"/>
+                    <L7p:item stringValue="192.168.19.2"/>
+                </L7p:CustomIpAddresses>
+            </L7p:HttpRoutingAssertion>
+
+     otherwise, the policy xml is like
+            <L7p:HttpRoutingAssertion>
+                <L7p:ProtectedServiceUrl stringValue="http://localhost:8080/parlayx/services/SendSms"/>
+            </L7p:HttpRoutingAssertion>
+
+    */
+
+    // for testing purposes only
+    public static void main(String[] args) throws Exception {
+        produceTemplateMappingFileFromDatabaseConnection("jdbc:mysql://localhost/ssg?failOverReadOnly=false&autoReconnect=false&socketTimeout=120000&useNewIO=true&characterEncoding=UTF8&characterSetResults=UTF8",
+                "gateway", "7layer", "~/foo");
     }
 }
