23c58afed392216ede9e53b9c8d74e5095790721
jay <jay@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2006-11-17 21:50:01 +0000

    Bugz: 2956
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@14704 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- etc/ssgruntimedefs.sh
+++ etc/ssgruntimedefs.sh
@@ -41,10 +41,10 @@ default_java_opts="-server -Dcom.l7tech.common.http.prov.apache.CommonsHttpClien
 default_java_opts="$default_java_opts -Dsun.rmi.dgc.server.gcInterval=3600000 -Dsun.rmi.dgc.client.gcInterval=3600000 "
 default_java_opts="$default_java_opts -Dcom.l7tech.common.http.prov.apache.CommonsHttpClient.maxTotalConnections=7500"
 default_java_opts="$default_java_opts -Djavax.xml.transform.TransformerFactory=org.apache.xalan.processor.TransformerFactoryImpl"
-default_java_opts="$default_java_opts -Dfile.encoding=UTF-8 -Dsun.net.inetaddr.ttl=30 -Dnetworkaddress.cache.ttl=30"
+default_java_opts="$default_java_opts -Dfile.encoding=UTF-8 -Dsun.net.inetaddr.ttl=30 "
 default_java_opts="$default_java_opts -Djava.awt.headless=true -XX:CompileThreshold=1500 "
 default_java_opts="$default_java_opts -Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.Jdk14Logger"
-default_java_opts="$default_java_opts -Xmx${java_ram}k -Xss192k "
+default_java_opts="$default_java_opts -Xmx${java_ram}k -Xss256k "
 
 unset system_ram
 unset multiplier
