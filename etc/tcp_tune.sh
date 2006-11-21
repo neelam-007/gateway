1a9f733cbf960ab383cf63d806ab75e649f06c96
jay <jay@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2006-11-21 20:45:22 +0000

    Thanks to the solaris documentation, I've got more data on tcp tuning. This reflects better values for a few more fields
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@14736 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- etc/tcp_tune.sh
+++ etc/tcp_tune.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Network Startup config
 #
-# chkconfig: - 99 01
+# chkconfig: 2345 99 01
 # description: Layer7's Secure Span Gateway TCP protection
 # processname: tomcat
 # pidfile: /var/run/ssg.pid
@@ -23,10 +23,10 @@ start() {
 	echo "Turning off timestamps"
 	echo 0 > /proc/sys/net/ipv4/tcp_timestamps
 	echo "Lowering keepalive time"
-	echo 1200 > /proc/sys/net/ipv4/tcp_keepalive_time
+	echo 2400 > /proc/sys/net/ipv4/tcp_keepalive_time
 	echo 5 > /proc/sys/net/ipv4/tcp_keepalive_intvl
 	echo "Lowering FIN timeout"
-	echo 10 > /proc/sys/net/ipv4/tcp_fin_timeout
+	echo 5 > /proc/sys/net/ipv4/tcp_fin_timeout
 	echo "Turning On Window scaling"
 	echo 1 > /proc/sys/net/ipv4/tcp_window_scaling
 	echo "Turning On Selective Acknowledgement"
@@ -44,7 +44,7 @@ start() {
 	echo 8388608 > /proc/sys/net/core/rmem_max
 	echo "4096 87380 4194304" > /proc/sys/net/ipv4/tcp_rmem
 	echo "4096 65536 4194304" > /proc/sys/net/ipv4/tcp_wmem
-	echo "4000000 4500000 5000000" > /proc/sys/net/ipv4/tcp_mem
+	echo "8000000 9000000 10000000" > /proc/sys/net/ipv4/tcp_mem
 	echo "Turning on TIME_WAIT recyle and reuse"
 	echo 1 > /proc/sys/net/ipv4/tcp_tw_recycle
 	echo 1 > /proc/sys/net/ipv4/tcp_tw_reuse
@@ -52,8 +52,8 @@ start() {
 	echo 360000 > /proc/sys/net/ipv4/tcp_max_tw_buckets
 	echo "Turning on syncookie protection from Denial of Service (DOS) attacks"
 	echo 1 > /proc/sys/net/ipv4/tcp_syncookies
-        echo "Done"
-        return 0
+    echo "Done"
+    return 0
 }
 stop() {
 	echo 
