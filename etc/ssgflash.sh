c939ce137f5612f4e43657a63eaf1892d637dda1
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2006-11-22 23:21:42 +0000

    require flasher utility to run as root to enable os level changes reset proper user permissions after ssg flash fix broken dependencies not caught by testpackage
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@14756 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- etc/ssgflash.sh
+++ etc/ssgflash.sh
@@ -7,13 +7,23 @@
 # -----------------------------------------------------------------------------
 #
 
-# set the current working directory to where this script lives
-cd `dirname $0`
-# assume we're running from SSG_ROOT/flasher and deduct SSG_ROOT from there
-pushd .. > /dev/null
-SSG_ROOT=`pwd`
-popd > /dev/null
-# assume location of the jdk
-JAVA_HOME=${SSG_ROOT}/jdk
-# invoke flasher
-${JAVA_HOME}/bin/java -Dcom.l7tech.server.home=${SSG_ROOT} -jar SSGFlasher.jar $*
+if [ ! "$SSG_USER" ]; then
+    SSG_USER=gateway.gateway
+fi
+
+if [ $UID -eq 0 ]; then
+    # set the current working directory to where this script lives
+    cd `dirname $0`
+    # assume we're running from SSG_ROOT/flasher and deduct SSG_ROOT from there
+    pushd .. > /dev/null
+    SSG_ROOT=`pwd`
+    popd > /dev/null
+    # assume location of the jdk
+    JAVA_HOME=${SSG_ROOT}/jdk
+    # invoke flasher
+    ${JAVA_HOME}/bin/java -Dcom.l7tech.server.home=${SSG_ROOT} -jar SSGFlasher.jar $*
+    # chown all files that could potentially have been overwritten
+    chown -R $SSG_USER ..
+else
+    echo "Must be root to invoke ssgflash.sh"
+fi
