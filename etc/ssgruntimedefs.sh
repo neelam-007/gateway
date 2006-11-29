9a6e7016b5a92c943b6ec16742a51ea408efc351
jay <jay@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2006-11-29 22:51:59 +0000

    Rearrange runtime so we can extract per-partition changes easily
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@14838 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- etc/ssgruntimedefs.sh
+++ etc/ssgruntimedefs.sh
@@ -2,40 +2,11 @@
 # /etc/profile.d/ssgruntimedefs.sh
 # LAYER 7 TECHNOLOGIES
 # Defines JAVA_HOME, TOMCAT_HOME, etc
-# and tunes the JVM for whatever is installed
-
-release=`cat /etc/redhat-release`
-# historical compatibility
-# stuff 
-
-if [ "$release" = "Red Hat Linux release 8.0 (Psyche)" ]; then
-	old_rel=1;
-fi
-
-if [ "$release" = "Red Hat Linux release 9 (Shrike)" ]; then
-	old_rel=1;
-fi
-
-unset release
-
-if [ "$old_rel" = 1 ]; then
-	export LD_ASSUME_KERNEL="2.2.5"
-fi
-unset old_rel
-
-# non-portable to non-english meminfo of which I don't know of any
 # at the moment
-system_ram=`grep MemTotal /proc/meminfo |cut -c 15-23`
 
-multiplier="2/3"
+SSG_HOME=/ssg
 
-let java_ram="$system_ram*$multiplier" 
-if [ `expr $java_ram \> 2074412` == 1 ]; then
-	# we have more ram than java can use
-	# FIXME: when we start running 64 bit JVM
-	java_ram=2074412;
-fi
-let maxnewsize="$java_ram*75/100"
+TOMCAT_HOME=/ssg/tomcat
 
 default_java_opts="-server -Dcom.l7tech.common.http.prov.apache.CommonsHttpClient.maxConnectionsPerHost=750 -Djava.net.preferIPv4Stack=true "
 default_java_opts="$default_java_opts -Dsun.rmi.dgc.server.gcInterval=3600000 -Dsun.rmi.dgc.client.gcInterval=3600000 "
@@ -44,12 +15,6 @@ default_java_opts="$default_java_opts -Djavax.xml.transform.TransformerFactory=o
 default_java_opts="$default_java_opts -Dfile.encoding=UTF-8 -Dsun.net.inetaddr.ttl=30 "
 default_java_opts="$default_java_opts -Djava.awt.headless=true -XX:CompileThreshold=1500 "
 default_java_opts="$default_java_opts -Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.Jdk14Logger"
-default_java_opts="$default_java_opts -Xmx${java_ram}k -Xss256k "
-
-unset system_ram
-unset multiplier
-unset java_ram
-unset maxnewsize
 
 
 if [ -e /ssg/etc/conf/JVM ]; then
@@ -60,27 +25,13 @@ else
 	JAVA_HOME="/ssg/jdk"
 fi
 
-
-#
-# Sun's tuning works fine for 1.5.0_05 and above. No need to play with it.
-#
-
-
 ulimit -s 2048
-
-SSG_HOME=/ssg
-TOMCAT_HOME=/ssg/tomcat
-CATALINA_PID=/ssg/etc/conf/ssg.pid
-
-# Under cygwin?.
-cygwin=false;
-case "`uname`" in
-  CYGWIN*) cygwin=true ;;
-esac
 
 # add to path
 PATH=$PATH:$JAVA_HOME/bin:$SSG_HOME/bin
-# Set Java system properties
+
+# Set rmi hostname for cluster correctness
+
 if  [ -e "/ssg/etc/conf/cluster_hostname" ];
 then
         default_java_opts="$default_java_opts -Djava.rmi.server.hostname=`cat /ssg/etc/conf/cluster_hostname`"
@@ -88,43 +39,12 @@ else
         default_java_opts="$default_java_opts -Djava.rmi.server.hostname=`hostname -f`"
 fi
 
-# May have to restore something like this for OS other than linux
-# if $cygwin; then
-#    mac=''
-#else
-#    mac=`/sbin/ifconfig eth0 |awk '/HWaddr/ {print $5}'`
-#fi
-#
-#if [ ! -z $mac ]; then
-#        default_java_opts="$default_java_opts -Dcom.l7tech.cluster.macAddress=$mac"
-#fi
 
 # aliases to start and stop ssg
 alias startssg='/etc/rc.d/init.d/ssg start'
 alias stopssg='/etc/rc.d/init.d/ssg stop'
 
-JAVA_OPTS=$default_java_opts;
-unset default_java_opts
 
-export SSG_HOME
-export JAVA_HOME
-export TOMCAT_HOME
-export JAVA_OPTS
-export CATALINA_PID
-
-# define tomcat home
-if $cygwin; then
-    export PATH=$PATH:$SSG_HOME/lib
-    SSG_HOME=`cygpath --path --unix "$SSG_HOME"`
-    JAVA_HOME=`cygpath --path --windows "$JAVA_HOME"`
-    TOMCAT_HOME=`cygpath --path --windows "$TOMCAT_HOME"`
-else
-    export PATH
-    #add to the ld path (shared native libraries)
-    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SSG_HOME/lib
-fi
-
-unset cygwin;
 
 if [ -e  /usr/local/Tarari ]; then
 	export TARARIROOT=/usr/local/Tarari
@@ -140,4 +60,50 @@ if [ -e "/opt/oracle" ] ; then
 	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ORACLE_HOME/lib
 	export NLS_LANG=AMERICAN_AMERICA.US7ASCII
 fi
+#########################################################################
+# Per-Partition
+#
+system_ram=`grep MemTotal /proc/meminfo |cut -c 15-23`
+# Maximum amount of RAM for _SINGLE_ partition
+multiplier="2/3"
+# 
+let java_ram="$system_ram*$multiplier" 
+if [ `expr $java_ram \> 2074412` == 1 ]; then
+	# we have more ram than java can use
+	# FIXME: when we start running 64 bit JVM
+	java_ram=2074412;
+	# CAP at 2 gigs
+fi
+
+if [ "${this_is_a_partition}" == "true" && ${number_of_partitions} > 1 ]; then
+	let java_ram ="$system_ram*$multiplier/$number_of_partitions"
+fi
+
+CATALINA_PID=/ssg/etc/conf/ssg.pid
+
+
+default_java_opts="$default_java_opts -Xmx${java_ram}k -Xss256k "
+
+unset system_ram
+unset multiplier
+unset java_ram
+
+# End Per-Partition
+#########################################################################
+
+JAVA_OPTS=$default_java_opts;
+
+unset default_java_opts
+
+
+export SSG_HOME
+export JAVA_HOME
+export TOMCAT_HOME
+export JAVA_OPTS
+export CATALINA_PID
+
+# define tomcat home
+export PATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SSG_HOME/lib
+
 
