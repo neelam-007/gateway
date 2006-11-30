24eac727b71f49e68196df303980a4df453ebeae
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2006-11-30 00:17:32 +0000

    ssg flashing utility feature complete (pending further creep)
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@14847 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/server/flasher/OSConfigManager.java
+++ src/com/l7tech/server/flasher/OSConfigManager.java
@@ -83,16 +83,16 @@ public class OSConfigManager {
         logger.info("Listing files that potentially need restore");
         ArrayList<String> listofosfiles = listDir(SYSTMP_PATH);
         if (listofosfiles != null) for (String osfiletorestore : listofosfiles) {
-            String tmptarget;
+            String realtarget;
             if (osfiletorestore.startsWith(SYSTMP_PATH)) {
-                tmptarget = osfiletorestore.substring(SYSTMP_PATH.length());
+                realtarget = osfiletorestore.substring(SYSTMP_PATH.length());
             } else {
                 // if this happens, it's a bug
                 throw new RuntimeException("unexpected path for " + osfiletorestore);
             }
-            logger.info("Restoring " + osfiletorestore + " into " + tmptarget);
+            logger.info("Restoring " + osfiletorestore + " into " + realtarget);
             File fromFile = new File(osfiletorestore);
-            File toFile = new File(tmptarget);
+            File toFile = new File(realtarget);
             if (toFile.canWrite()) {
                 if (toFile.exists()) {
                     toFile.delete();
@@ -100,7 +100,7 @@ public class OSConfigManager {
                 FileUtils.ensurePath(toFile.getParentFile());
                 FileUtils.copyFile(fromFile, toFile);
             } else {
-                logger.severe("cannot restore " + tmptarget + " because i dont have necessary permissions. perhaps " +
+                logger.severe("cannot restore " + realtarget + " because i dont have necessary permissions. perhaps " +
                               "this was incorrectly invoked at non-root user");
             }
         }
