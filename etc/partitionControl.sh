73b959c2071923a41363f7facca7ebcabc7af0ed
megery <megery@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2006-11-30 22:14:26 +0000

    this is the control script for partitions on linux. It's a wrapper for catalina.sh that passes in the correct arguments to get a partition to use a different conifuration from the default. this will be invoked by service ssg start|stop to start each partition on a system or it can be invoked individually. this file will be located in the partition root for a given partition (ex. /ssg/etc/conf/partitions/<partitionName>/partitionControl.sh
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@14867 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ etc/partitionControl.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+##################################################################################
+#Control script to start or stop an individual partition. 
+#Run from the partitions root directory (SSG_ROOT/etc/conf/partitions/<partition>)
+##################################################################################
+usage() {
+	echo "Usage: partitionControl.sh [start|run|stop|usage]"
+	echo "  start - start the given partition and detach from the console"
+	echo "  run - start the given partition but do not detach from the console"
+	echo "  stop - stop the given partition"
+	echo "  usage - show this message"
+}
+
+COMMAND=${1}
+if [ -z "${COMMAND}" ] ; then
+	echo "Please specify a command."
+	usage
+	exit 1;
+fi
+
+if [ ${COMMAND} == "usage" ] ; then
+	usage;
+	exit 0;
+fi
+
+cd `dirname $0`
+pushd ../../../../ > /dev/null
+SSG_ROOT=`pwd`
+popd > /dev/null
+
+PARTITION_ROOT_DIR=${PWD}
+CONFIG_FILE="${PARTITION_ROOT_DIR}/server.xml"
+PARTITION_NAME=`basename ${PWD}`
+export CATALINA_OPTS=-Dcom.l7tech.server.partitionName=${PARTITION_NAME}
+(/ssg/tomcat/bin/catalina.sh ${COMMAND} -config ${CONFIG_FILE})
