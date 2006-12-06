f054e8fe2cd56a14c1e854667831ed6c33dfbae9
megery <megery@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2006-12-06 23:59:22 +0000

    bugz 3173: now only need the one partitionControl.sh in /ssg/bin to start and stop partitions. It's in the path too so there's no need to enter the full path. It checks to see if it's root and, if so, switches to gateway. Otherwise, just run the partition as the current user.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@14984 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- etc/partitionControl.sh
+++ etc/partitionControl.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
 ##################################################################################
-#Control script to start or stop an individual partition. 
-#Run from the partitions root directory (SSG_ROOT/etc/conf/partitions/<partition>)
+#Control script to start or stop an individual partition.
 ##################################################################################
+
+. /etc/profile.d/ssgruntimedefs.sh
+USER=`whoami`
+SSGUSER="gateway"
+
 usage() {
-	echo "Usage: partitionControl.sh [start|run|stop|usage]"
+	echo "Usage: partitionControl.sh [start|run|stop|usage] partition-name"
 	echo "  start - start the given partition and detach from the console"
 	echo "  run - start the given partition but do not detach from the console"
 	echo "  stop - stop the given partition"
 	echo "  usage - show this message"
+	echo "  partition-name - the name of the partition to control"
 }
 
 COMMAND=${1}
+PARTITION_NAME=${2}
+
 if [ -z "${COMMAND}" ] ; then
 	echo "Please specify a command."
+	usage
+	exit 1;
+fi
+
+if [ -z "${PARTITION_NAME}" ] ; then
+	echo "No partition name specified."
 	usage
 	exit 1;
 fi
@@ -24,13 +37,25 @@ if [ ${COMMAND} == "usage" ] ; then
 	exit 0;
 fi
 
-cd `dirname $0`
-pushd ../../../../ > /dev/null
-SSG_ROOT=`pwd`
-popd > /dev/null
+if [ ! -d "${SSG_HOME}" ] ; then
+    if [ -z "${SSG_HOME}" ] ; then
+        echo "SSG_HOME is not set! Please set SSG_HOME to the installation root of the SecureSpan Gateway (ex. /ssg)"
+        exit 1;
+    fi
+    echo "Invalid SSG_HOME! Please set SSG_HOME to the installation root of the SecureSpan Gateway (ex. /ssg)"
+    exit 1;
+fi
 
-PARTITION_ROOT_DIR=${PWD}
-CONFIG_FILE="${PARTITION_ROOT_DIR}/server.xml"
-PARTITION_NAME=`basename ${PWD}`
+PARTITION_DIR=${SSG_HOME}/etc/conf/partitions/${PARTITION_NAME}
+CONFIG_FILE="${PARTITION_DIR}/server.xml"
+
+. ${SSG_HOME}/bin/partition_defs.sh true "${PARTITION_COUNT}"
+export JAVA_OPTS="${ORIGINAL_JAVA_OPTS} ${partition_opts}"
 export CATALINA_OPTS=-Dcom.l7tech.server.partitionName=${PARTITION_NAME}
-(${SSG_ROOT}/tomcat/bin/catalina.sh ${COMMAND} -config ${CONFIG_FILE})
+
+if [ "$USER" = "root" ]; then
+    (su $SSGUSER -c "${SSG_HOME}/tomcat/bin/catalina.sh ${COMMAND} -config ${CONFIG_FILE}")
+else
+    (${SSG_HOME}/tomcat/bin/catalina.sh ${COMMAND} -config ${CONFIG_FILE})
+fi
+
