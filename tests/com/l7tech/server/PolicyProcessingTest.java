327d409a14ea2357563192b878f0083b40e97e1c
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2007-01-10 19:13:25 +0000

    PolicyProcessingTest all good again
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@15339 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- tests/com/l7tech/server/PolicyProcessingTest.java
+++ tests/com/l7tech/server/PolicyProcessingTest.java
@@ -291,16 +291,16 @@ public class PolicyProcessingTest extends TestCase {
 
         Result result = processMessage("/httproutecookie", requestMessage1, 0);
 
-        // assertTrue("Outbound request cookie missing", headerExists(mockClient.getParams().getExtraHeaders(), "Cookie", "cookie=invalue"));
-        // assertTrue("Outbound response cookie missing", cookieExists(result.context.getCookies(),"cookie", "outvalue"));
+        assertTrue("Outbound request cookie missing", headerExists(mockClient.getParams().getExtraHeaders(), "Cookie", "cookie=invalue"));
+        assertTrue("Outbound response cookie missing", cookieExists(result.context.getCookies(),"cookie", "outvalue"));
 
         MockGenericHttpClient mockClient2 = buildMockHttpClient(responseHeaders, responseMessage1);
         testingHttpClientFactory.setMockHttpClient(mockClient2);
 
         Result result2 = processMessage("/httproutenocookie", requestMessage1, 0);
 
-        // assertFalse("Outbound request cookie present", headerExists(mockClient2.getParams().getExtraHeaders(), "Cookie", "cookie=invalue"));
-        // assertFalse("Outbound response cookie present", cookieExists(result2.context.getCookies(),"cookie", "outvalue"));
+        assertFalse("Outbound request cookie present", headerExists(mockClient2.getParams().getExtraHeaders(), "Cookie", "cookie=invalue"));
+        assertFalse("Outbound response cookie present", cookieExists(result2.context.getCookies(),"cookie", "outvalue"));
     }
 
     /**
