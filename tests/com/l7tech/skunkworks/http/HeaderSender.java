46318a843653b008de6284a2b9e3f81bd34c3807
flascell <flascell@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2007-01-15 21:07:36 +0000

    a servlet which will return information about incoming requests specifically, the http headers and parameters received
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@15395 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ tests/com/l7tech/skunkworks/http/HeaderSender.java
@@ -0,0 +1,27 @@
+package com.l7tech.skunkworks.http;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.PostMethod;
+
+
+/**
+ * A simple http client that sends http headers and displays response's payload
+ * <p/>
+ * <p/>
+ * <br/><br/>
+ * LAYER 7 TECHNOLOGIES, INC<br/>
+ * User: flascell<br/>
+ * Date: Jan 15, 2007<br/>
+ */
+public class HeaderSender {
+    public static final String URL = "http://soong:8080/test/blahness?param1=value1";
+    public static void main(String[] args) throws Exception {
+        HttpClient client = new HttpClient();
+        PostMethod post = new PostMethod(URL);
+        post.addRequestHeader("header1", "header1value");
+
+        int res = client.executeMethod(post);
+        System.out.println("Post retulted in status " + res);
+        System.out.println("Response payload: " + new String(post.getResponseBody()));
+    }
+}
