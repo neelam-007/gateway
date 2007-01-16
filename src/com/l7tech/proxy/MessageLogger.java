011074fe074badca4cb4a282f5e128f15ae8bc0d
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2007-01-16 00:18:50 +0000

    SSB MessageLogger now logs request and response message bodies at FINE instead of INFO (so it no longer logs all traffic by default).  Also fixed response logging, which has been broken (logging something like com.l7tech.common.message.Message@1a3ca10, instead of the actual response body, since probably version 3.0).
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@15403 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/proxy/MessageLogger.java
+++ src/com/l7tech/proxy/MessageLogger.java
@@ -6,21 +6,26 @@
 
 package com.l7tech.proxy;
 
-import com.l7tech.common.util.XmlUtil;
+import com.l7tech.common.io.BufferPoolByteArrayOutputStream;
+import com.l7tech.common.message.MimeKnob;
+import com.l7tech.common.mime.NoSuchPartException;
+import com.l7tech.common.mime.PartInfo;
 import com.l7tech.common.util.ExceptionUtils;
+import com.l7tech.common.util.HexUtils;
+import com.l7tech.common.util.XmlUtil;
 import com.l7tech.proxy.datamodel.Policy;
 import com.l7tech.proxy.datamodel.PolicyAttachmentKey;
 import com.l7tech.proxy.datamodel.Ssg;
 import com.l7tech.proxy.message.PolicyApplicationContext;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
  * RequestInterceptor that logs all messages in and out.
- * User: mike
- * Date: Jun 30, 2003
- * Time: 10:52:55 AM
+ * @author mike
  */
 public class MessageLogger implements RequestInterceptor {
     private final static Logger log = Logger.getLogger(MessageLogger.class.getName());
@@ -32,19 +37,36 @@ public class MessageLogger implements RequestInterceptor {
      * Fired when a message is received from a client, after it is parsed.
      */
     public void onReceiveMessage(PolicyApplicationContext context) {
+        if (!log.isLoggable(Level.FINE)) return;
         try {
-            log.info("Received client request: " + XmlUtil.nodeToString(context.getRequest().getXmlKnob().getOriginalDocument()));
+            log.fine("Received client request: " + XmlUtil.nodeToString(context.getRequest().getXmlKnob().getOriginalDocument()));
         } catch (Exception e) {
             log.log(Level.SEVERE, "Error examining client request", e);
         }
     }
 
-    /**
-     * Fired when a reply is read from the SSG, after it is parsed.
-     * @param context
-     */
     public void onReceiveReply(PolicyApplicationContext context) {
-        log.info("Received server response: " + context.getResponse());
+        if (!log.isLoggable(Level.FINE)) return;
+        String responseStr = null;
+        MimeKnob mk = context.getResponse().getMimeKnob();
+        InputStream is = null;
+        BufferPoolByteArrayOutputStream baos = null;
+        try {
+            if (mk != null) {
+                is = mk.getFirstPart().getInputStream(false);
+                baos = new BufferPoolByteArrayOutputStream();
+                HexUtils.copyStream(is, baos);
+                responseStr = baos.toString();
+            }
+        } catch (IOException e) {
+            responseStr = "<IOException: " + ExceptionUtils.getMessage(e) + ">";
+        } catch (NoSuchPartException e) {
+            responseStr = "<NoSuchPartException: " + ExceptionUtils.getMessage(e) + ">";
+        } finally {
+            if (is != null) try { is.close(); } catch (IOException e) { /* too late to care */ }
+            if (baos != null) baos.close();
+        }
+        log.fine("Received server response: " + responseStr == null ? "<null>" : responseStr);
     }
 
     /**
@@ -52,6 +74,7 @@ public class MessageLogger implements RequestInterceptor {
      * @param t The error that occurred during the request.
      */
     public void onMessageError(Throwable t) {
+        if (!log.isLoggable(Level.WARNING)) return;
         log.warning("Error while processing request from client: " + t);
     }
 
@@ -60,19 +83,17 @@ public class MessageLogger implements RequestInterceptor {
      * @param t The error that occurred during the request.
      */
     public void onReplyError(Throwable t) {
+        if (!log.isLoggable(Level.INFO)) return;
         log.info("Error while processing response from server: " + t);
     }
 
-    /**
-     * Fired when a policy is updated.
-     * @param binding
-     * @param policy
-     */
     public void onPolicyUpdated(Ssg ssg, PolicyAttachmentKey binding, Policy policy) {
+        if (!log.isLoggable(Level.INFO)) return;
         log.info("Policy updated for Gateway: " + ssg);
     }
 
     public void onPolicyError(Ssg ssg, PolicyAttachmentKey binding, Throwable error) {
+        if (!log.isLoggable(Level.WARNING)) return;
         log.warning("Policy download failed for Gateway: " + ssg + ": " + ExceptionUtils.getMessage(error));
     }
 }
