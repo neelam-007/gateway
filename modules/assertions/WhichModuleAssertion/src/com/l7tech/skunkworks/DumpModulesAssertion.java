635580757a47293ccab1c8b3ee27b8ea9213a791
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2007-02-21 01:47:59 +0000

    Added a skunkworks module containing diagnostic assertions (DumpModules and WhichModule).
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@15700 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ modules/assertions/WhichModuleAssertion/src/com/l7tech/skunkworks/DumpModulesAssertion.java
@@ -0,0 +1,78 @@
+package com.l7tech.skunkworks;
+
+import com.l7tech.policy.assertion.*;
+import com.l7tech.policy.variable.DataType;
+import com.l7tech.policy.variable.VariableMetadata;
+import com.l7tech.server.message.PolicyEnforcementContext;
+import com.l7tech.server.policy.AssertionModule;
+import com.l7tech.server.policy.ServerAssertionRegistry;
+import com.l7tech.server.policy.assertion.AbstractServerAssertion;
+import org.springframework.context.ApplicationContext;
+
+import java.io.IOException;
+import java.util.Set;
+
+/**
+ * Set a variable with XML-encoded information about all loaded modules.
+ */
+public class DumpModulesAssertion extends Assertion implements SetsVariables {
+    private static final String OUTVAR = "modules";
+
+    public VariableMetadata[] getVariablesSet() {
+        return new VariableMetadata[] {
+                new VariableMetadata(OUTVAR, false, true, null, false, DataType.STRING),
+        };
+    }
+
+    public AssertionMetadata meta() {
+        DefaultAssertionMetadata meta = defaultMeta();
+        meta.put(AssertionMetadata.LONG_NAME, "Gets info about all currently loaded modules");
+        meta.put(AssertionMetadata.PALETTE_NODE_ICON, "com/l7tech/console/resources/ServerRegistry.gif");
+        meta.put(AssertionMetadata.PALETTE_FOLDERS, new String[] { "policyLogic" });
+        meta.put(AssertionMetadata.SERVER_ASSERTION_CLASSNAME, getClass().getName() + "$ServerImpl");
+        meta.put(AssertionMetadata.PROPERTIES_EDITOR_CLASSNAME, getClass().getName() + "$PropDialog");
+        meta.put(AssertionMetadata.POLICY_VALIDATOR_CLASSNAME, getClass().getName() + "$Validator");
+        return meta;
+    }
+
+    public static class ServerImpl extends AbstractServerAssertion<DumpModulesAssertion> {
+        private final ServerAssertionRegistry assertionRegistry;
+        private final String[] simpleProps = new String[] {
+                AssertionMetadata.SHORT_NAME,
+                AssertionMetadata.LONG_NAME,
+                AssertionMetadata.DESCRIPTION,
+                AssertionMetadata.PALETTE_NODE_NAME,
+                AssertionMetadata.USED_BY_CLIENT,
+        };
+
+        public ServerImpl(DumpModulesAssertion assertion, ApplicationContext context) {
+            super(assertion);
+            this.assertionRegistry = (ServerAssertionRegistry)context.getBean("assertionRegistry", ServerAssertionRegistry.class);
+        }
+
+        public AssertionStatus checkRequest(PolicyEnforcementContext context) throws IOException, PolicyAssertionException {
+
+            Set<AssertionModule> mods = assertionRegistry.getLoadedModules();
+            StringBuffer sb = new StringBuffer();
+            for (AssertionModule mod : mods) {
+
+                sb.append("<module>\n");
+                sb.append("    <name>").append(mod.getName()).append("</name>\n");
+                sb.append("    <sha1>").append(mod.getSha1()).append("</sha1>\n");
+                for (Assertion proto : mod.getAssertionPrototypes()) {
+                    final AssertionMetadata meta = proto.meta();
+                    sb.append("    <assertion classname=\"").append(proto.getClass().getName()).append("\">\n");
+                    for (String prop : simpleProps) {
+                        sb.append("        <").append(prop).append(">").append(meta.get(prop)).append("</").append(prop).append(">\n");
+                    }
+                    sb.append("    </assertion>\n");
+                }
+                sb.append("</module>\n");
+            }
+            context.setVariable(OUTVAR, sb.toString());
+
+            return AssertionStatus.NONE;
+        }
+    }
+}
+
