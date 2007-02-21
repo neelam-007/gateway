86024922d78442a0c53df50dd747eeca053ef9ff
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2007-02-21 05:21:03 +0000

    Trying degenerify to work around build failure I just can't reproduce locally
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@15709 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/assertions/WhichModuleAssertion/src/com/l7tech/external/assertions/whichmodule/server/ServerHelloWorldAssertion.java
+++ modules/assertions/WhichModuleAssertion/src/com/l7tech/external/assertions/whichmodule/server/ServerHelloWorldAssertion.java
@@ -1,24 +1,27 @@
 package com.l7tech.external.assertions.whichmodule.server;
 
 import com.l7tech.common.mime.ContentTypeHeader;
+import com.l7tech.external.assertions.whichmodule.HelloWorldAssertion;
 import com.l7tech.policy.assertion.AssertionStatus;
 import com.l7tech.policy.assertion.PolicyAssertionException;
 import com.l7tech.policy.assertion.RoutingStatus;
 import com.l7tech.server.message.PolicyEnforcementContext;
 import com.l7tech.server.policy.assertion.ServerRoutingAssertion;
-import com.l7tech.external.assertions.whichmodule.HelloWorldAssertion;
 import org.springframework.context.ApplicationContext;
 
 import java.io.IOException;
 
-public class ServerHelloWorldAssertion extends ServerRoutingAssertion<HelloWorldAssertion> {
+/** @noinspection unchecked*/
+public class ServerHelloWorldAssertion extends ServerRoutingAssertion {
+    private final byte[] greetBytes;
+
     public ServerHelloWorldAssertion(HelloWorldAssertion assertion, ApplicationContext context) {
         super(assertion, context);
+        greetBytes = ("<greeting>Hello, " + assertion.getNameToGreet() + "!</greeting>").getBytes();
     }
 
     public AssertionStatus checkRequest(PolicyEnforcementContext context) throws IOException, PolicyAssertionException {
-        context.getResponse().initialize(ContentTypeHeader.XML_DEFAULT,
-                                         ("<greeting>Hello, " + getAssertion().getNameToGreet() + "!</greeting>").getBytes());
+        context.getResponse().initialize(ContentTypeHeader.XML_DEFAULT, greetBytes);
         context.setRoutingStatus(RoutingStatus.ROUTED);
         return AssertionStatus.NONE;
     }
