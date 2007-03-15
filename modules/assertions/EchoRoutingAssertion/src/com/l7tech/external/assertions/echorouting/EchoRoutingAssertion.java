06d12de930553922fec70b02c73a19fec5c371fc
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2007-03-15 18:48:37 +0000

    Change EchoRoutingAssertion AAR so it uses the same WSP name as the pre-3.7 versions (Bug #3605)
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@15914 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/assertions/EchoRoutingAssertion/src/com/l7tech/external/assertions/echorouting/EchoRoutingAssertion.java
+++ modules/assertions/EchoRoutingAssertion/src/com/l7tech/external/assertions/echorouting/EchoRoutingAssertion.java
@@ -4,6 +4,9 @@ import static com.l7tech.policy.assertion.AssertionMetadata.*;
 import com.l7tech.policy.assertion.RoutingAssertion;
 import com.l7tech.policy.assertion.AssertionMetadata;
 import com.l7tech.policy.assertion.DefaultAssertionMetadata;
+import com.l7tech.policy.wsp.TypeMapping;
+
+import java.util.HashMap;
 
 /**
  * <code>EchoRoutingAssertion</code> is an assertion that copies the request content into the response.
@@ -31,6 +34,12 @@ public class EchoRoutingAssertion extends RoutingAssertion {
 
         meta.put(POLICY_NODE_NAME, "Echo request to response");
         meta.put(POLICY_ADVICE_CLASSNAME, "auto");
+
+        meta.put(WSP_EXTERNAL_NAME, "EchoRoutingAssertion"); // keep same WSP name as pre-3.7 (Bug #3605)
+        final TypeMapping typeMapping = (TypeMapping)meta.get(WSP_TYPE_MAPPING_INSTANCE);
+        meta.put(AssertionMetadata.WSP_COMPATIBILITY_MAPPINGS, new HashMap<String, TypeMapping>() {{
+            put("EchoRouting", typeMapping);
+        }});
 
         // request default feature set name for our class name, since we are a known optional module
         // that is, we want our required feature set to be "assertion:EchoRouting" rather than "set:modularAssertions"
