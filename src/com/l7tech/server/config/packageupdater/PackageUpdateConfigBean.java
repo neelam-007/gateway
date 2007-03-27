f361fe6ba33b31f975d1ae11ceec9f002f722561
megery <megery@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2007-03-27 00:04:53 +0000

    more progress on update installer
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@15972 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/server/config/packageupdater/PackageUpdateConfigBean.java
+++ src/com/l7tech/server/config/packageupdater/PackageUpdateConfigBean.java
@@ -8,25 +8,53 @@ import com.l7tech.server.config.beans.BaseConfigurationBean;
  * Time: 4:16:00 PM
  */
 public class PackageUpdateConfigBean extends BaseConfigurationBean {
-    private String packageLocation;
+    public static final String PACKAGE_UPDATE_EXTENSION = "sup";
+    public static String DESCRIPTION_FILENAME = "description.txt";
+    public static String INSTALLER_JAR_FILENAME = "UpdateInstaller.jar";
+
+    private UpdatePackageInfo packageInfo;
 
     public PackageUpdateConfigBean(String name, String description) {
         super(name, description);
     }
 
-    public void setPackageLocation(String location) {
-        this.packageLocation = location;
-    }
-
-
-    public String getPackageLocation() {
-        return packageLocation;
-    }
-
     public void reset() {
-        packageLocation ="";
+        packageInfo = null;
     }
 
     protected void populateExplanations() {
+    }
+
+    public void setPackageInformation(UpdatePackageInfo updatePackageInfo) {
+        this.packageInfo = updatePackageInfo;
+    }
+
+    public UpdatePackageInfo getPackageInfo() {
+        return packageInfo;
+    }
+
+    public static class UpdatePackageInfo {
+        private String description;
+        private String pathLocation;
+
+
+        public UpdatePackageInfo() {
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public String getPathLocation() {
+            return pathLocation;
+        }
+
+        public void setPathLocation(String pathLocation) {
+            this.pathLocation = pathLocation;
+        }
     }
 }
--- src/com/l7tech/server/config/packageupdater/UpdateWizardPackageQuestionsStep.java
+++ src/com/l7tech/server/config/packageupdater/UpdateWizardPackageQuestionsStep.java
@@ -1,13 +1,21 @@
 package com.l7tech.server.config.packageupdater;
 
+import com.l7tech.common.util.FileUtils;
+import com.l7tech.common.util.ResourceUtils;
 import com.l7tech.server.config.exceptions.WizardNavigationException;
 import com.l7tech.server.config.ui.console.BaseConsoleStep;
 import com.l7tech.server.config.ui.console.ConfigurationWizard;
 import org.apache.commons.lang.StringUtils;
 
-import java.io.IOException;
-import java.io.File;
+import java.io.*;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.logging.Logger;
 import java.util.regex.Pattern;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 /**
  * User: megery
@@ -15,9 +23,14 @@ import java.util.regex.Pattern;
  * Time: 2:00:39 PM
  */
 public class UpdateWizardPackageQuestionsStep extends BaseConsoleStep {
+    private static final Logger logger = Logger.getLogger(UpdateWizardPackageQuestionsStep.class.getName());
+
     private static final String TITLE = "SecureSpan Gateway Appliance Update Wizard - Locate Update Packages";
 
     PackageUpdateConfigBean configBean;
+
+    List<PackageUpdateConfigBean.UpdatePackageInfo> updatePackages;
+    private String updateLocation;
 
     public UpdateWizardPackageQuestionsStep(ConfigurationWizard parentWiz) {
         super(parentWiz);
@@ -25,61 +38,219 @@ public class UpdateWizardPackageQuestionsStep extends BaseConsoleStep {
     }
 
     public boolean validateStep() {
-        boolean isValid = false;
-        try {
-            checkValidLocation();
-            checkValidUpdatePackage();
-            isValid = true;
-        } catch (UpdateWizardException e) {
-            printText("*** " + e.getMessage() + " ***");
+        return isOnePackageSelected();
+    }
+
+    private boolean isOnePackageSelected() {
+        switch (getUpdatePackages().size()) {
+            case 0:
+                printText("No Update Packages are selected");
+                return false;
+            case 1:
+                return true;
+            default:
+                printText("More than one update packages is available to be installed. Please select one");
+                return false;
         }
-        return isValid;
     }
 
-    private void checkValidUpdatePackage() {
-
-    }
-
-    private void checkValidLocation() throws UpdateWizardException {
-        String path = configBean.getPackageLocation();
+    private void checkUpdateLocation(String path) throws UpdateWizardException {
         if (StringUtils.isEmpty(path)) {
             throw new UpdateWizardException("A valid path to the update package was not specified.");
         }
 
         File updatePath = new File(path);
         if (updatePath.exists()) {
-            if (updatePath.isFile()) {
-                checkZipFile(updatePath);
-            }
+            checkUpdateFile(updatePath);
         } else {
-            throw new UpdateWizardException("No valid update package was found at \"" + path + "\"");
+            throw new UpdateWizardException("The path \"" + path + "\" does not exist");
         }
     }
 
-    private void checkZipFile(File updatePath) {
-        if (updatePath == null) {
-            
+    private boolean checkValidSinglePackage(PackageUpdateConfigBean.UpdatePackageInfo updatePackage) {
+        boolean isValid = false;
+        return isValid;
+    }
+
+    private void checkUpdateFile(File updatePath) throws UpdateWizardException {
+        getUpdatePackages().clear();
+        if (updatePath.isDirectory()) {
+            File[] updateFiles = updatePath.listFiles(new FilenameFilter() {
+                public boolean accept(File dir, String name) {
+                    return name.endsWith(PackageUpdateConfigBean.PACKAGE_UPDATE_EXTENSION);
+                }
+            });
+
+            if (updateFiles == null || updateFiles.length == 0) {
+                throw new UpdateWizardException("No SecureSpan Update Packages could be found at the path specified \"" + updatePath.getAbsolutePath() + "\".");
+            }
+
+            for (File updateFile : updateFiles) {
+                PackageUpdateConfigBean.UpdatePackageInfo info = getUpdateInfo(updateFile);
+                if (info != null) getUpdatePackages().add(info);
+            }
+        } else {
+            if (!updatePath.getName().endsWith(PackageUpdateConfigBean.PACKAGE_UPDATE_EXTENSION)) {
+                 throw new UpdateWizardException("Could not find a valid SecureSpan Update package using: \"" + updatePath.getAbsolutePath() + "\"");
+            }
+
+            PackageUpdateConfigBean.UpdatePackageInfo info = getUpdateInfo(updatePath);
+            if (info != null) getUpdatePackages().add(info);
         }
+    }
+
+    private List<PackageUpdateConfigBean.UpdatePackageInfo> getUpdatePackages() {
+        if (updatePackages == null)
+            updatePackages = new ArrayList<PackageUpdateConfigBean.UpdatePackageInfo>();
+        return updatePackages;
+    }
+
+    private PackageUpdateConfigBean.UpdatePackageInfo getUpdateInfo(File updateFile) {
+        PackageUpdateConfigBean.UpdatePackageInfo info = null;
+        File zipOutputDir = new File("updatepackages",  updateFile.getName()+"expanded");
+        BufferedOutputStream dest = null;
+        try {
+            ZipFile zipFile = new ZipFile(updateFile);
+            if (zipFile.size() == 0) {
+                logger.warning(MessageFormat.format("Update package {0} is empty. This is not a valid SecureSpan update package.", updateFile.getAbsolutePath()));
+                return null;
+            }
+
+            if (zipOutputDir.exists()) {
+                FileUtils.deleteDir(zipOutputDir);
+            }
+            zipOutputDir.mkdirs();
+
+            Enumeration e = zipFile.entries();
+            while (e.hasMoreElements()) {
+                ZipEntry ze = (ZipEntry) e.nextElement();
+                String name = ze.getName();
+                logger.info("Extracting " + name);
+
+                BufferedInputStream bis = null;
+                bis = new BufferedInputStream(zipFile.getInputStream(ze));
+
+                File destFile = new File(zipOutputDir, name);
+                if (ze.isDirectory()) {
+                    destFile.mkdirs();
+                } else {
+                    dest = new BufferedOutputStream(new FileOutputStream(destFile));
+
+                    byte[] buf = new byte[512];
+                    int len = 0;
+                    while ((len = bis.read(buf)) != -1) {
+                        dest.write(buf, 0, len);
+                    }
+                    dest.flush();
+                    dest.close();
+                }
+                bis.close();
+            }
+
+            //now check to see if things we expect are there.
+            File fileToCheck = new File(zipOutputDir, PackageUpdateConfigBean.INSTALLER_JAR_FILENAME);
+            if (!fileToCheck.exists()) {
+                logger.warning(MessageFormat.format("Could not find a {0} in the update package {1}. This is not a valid SecureSpan update package.", PackageUpdateConfigBean.INSTALLER_JAR_FILENAME, updateFile.getAbsolutePath()));
+                return null;
+            }
+
+            fileToCheck = new File(zipOutputDir, PackageUpdateConfigBean.DESCRIPTION_FILENAME);
+            if (!fileToCheck.exists()) {
+                logger.warning(MessageFormat.format("Could not find a {0} in the update package {1}. This is not a valid SecureSpan update package.", PackageUpdateConfigBean.DESCRIPTION_FILENAME, updateFile.getAbsolutePath()));
+                return null;
+            }
+            String description = getDescription(fileToCheck);
+
+            info = new PackageUpdateConfigBean.UpdatePackageInfo();
+            info.setDescription(description);
+        } catch (FileNotFoundException e) {
+            logger.warning(MessageFormat.format("Could not read update package from file \"{0}\". ({1})", updateFile.getAbsolutePath(), e.getMessage()));
+        } catch (IOException e) {
+            logger.warning(MessageFormat.format("Could not read update package from file \"{0}\". ({1})", updateFile.getAbsolutePath(), e.getMessage()));
+        } catch (UpdateWizardException e) {
+            logger.warning(MessageFormat.format("Could not read update package from file \"{0}\". ({1})", updateFile.getAbsolutePath(), e.getMessage()));
+        } finally {
+            ResourceUtils.closeQuietly(dest);
+        }
+        return info;
+    }
+
+    private String getDescription(File descriptionFile) throws UpdateWizardException {
+        StringBuilder sb = new StringBuilder();
+        BufferedInputStream bis = null;
+        try {
+            bis = new BufferedInputStream(new FileInputStream(descriptionFile));
+            int len = 0;
+            byte[] buf = new byte[512];
+            while ((len = bis.read(buf)) != -1) {
+                sb.append(buf);
+            }
+        } catch (FileNotFoundException e) {
+            String message = "No description file found. This is not a valid SecureSpan Update package.";
+            logger.warning(message);
+            throw new UpdateWizardException(message);
+        } catch (IOException e) {
+            String message = MessageFormat.format("Error while reading description file. Cannot proceed ({0}).", e.getMessage());
+            logger.warning(message);
+            throw new UpdateWizardException(message);
+        } finally{
+            ResourceUtils.closeQuietly(bis);
+        }
+        return sb.toString();
     }
 
     public void doUserInterview(boolean validated) throws WizardNavigationException {
-
         try {
-            String location = getData(
-                    new String[] {
-                        "Enter the path to the update package you wish to install:"
-                    },
-                    "",
-                    (Pattern) null,
-                    "Invalid Path");
-            configBean.setPackageLocation(location);
-
+            doSpecifyPackageQuestions();
+            storeInput();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    private void doSpecifyPackageQuestions() throws IOException, WizardNavigationException {
+        updateLocation = getData(
+                new String[] {
+                    "Enter the path to the update package you wish to install:"
+                },
+                "",
+                (Pattern) null,
+                "Invalid Path");
+
+        try {
+            checkUpdateLocation(updateLocation);
+            boolean isValid = false;
+            if (getUpdatePackages().isEmpty()) {
+                printText("No upgrade package was found at this location. Please specify the package you wish to install." + getEolChar());
+                logger.warning("No upgrade packages found.");
+                doSpecifyPackageQuestions();
+            } else if (getUpdatePackages().size() > 1) {
+                printText("Multiple upgrade packages found at this location. Please specify the package you wish to install." + getEolChar());
+                logger.warning("Multiple upgrade packages found.");
+                doSpecifyPackageQuestions();
+            } else {
+                if (!checkValidSinglePackage(getUpdatePackages().iterator().next())) {
+                    doSpecifyPackageQuestions();    
+                }
+            }
+
+            if (!getConfirmationFromUser("The following update package will be installed. Is this correct?")) {
+                doSpecifyPackageQuestions();
+            }
+
+        } catch (UpdateWizardException e) {
+            printText("*** " + e.getMessage() + " ***" + getEolChar());
+            getUpdatePackages().clear();
+        }
+    }
+
     public String getTitle() {
         return TITLE;
+    }
+
+    protected void storeInput() {
+        if (!getUpdatePackages().isEmpty()) {
+            configBean.setPackageInformation(getUpdatePackages().iterator().next());
+        }
     }
 }
