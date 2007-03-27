4b6fccee96c3f3064d36435fcb4c2613f07da4d6
megery <megery@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2007-03-27 21:57:01 +0000

    Changes to the launching of the configwizard from the ssgconfig menu. Now, if there's a DISPLAY set, it will just use it. Otherwise, it will go all console mode on you.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@15982 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- etc/ssgconfig.sh
+++ etc/ssgconfig.sh
@@ -13,28 +13,17 @@ launchtype=${1}
 launch_wizard(){
 	#check if we're root
 	if [ "$USER" != "ssgconfig" ]; then
-        	su ssgconfig -c "${JAVA_HOME}/bin/java -Djava.library.path=${SSG_ROOT}/lib -Dcom.l7tech.server.home=${SSG_ROOT} -jar ConfigWizard.jar $*"
+        	su -m ssgconfig -c "${JAVA_HOME}/bin/java -Djava.library.path=${SSG_ROOT}/lib -Dcom.l7tech.server.home=${SSG_ROOT} -jar ConfigWizard.jar $*"
     	else
         	${JAVA_HOME}/bin/java -Djava.library.path=${SSG_ROOT}/lib -Dcom.l7tech.server.home=${SSG_ROOT} -jar ConfigWizard.jar $*
     	fi
 }
 
 if [ -z "${launchtype}" ] || [ ${launchtype} == "-graphical" ]; then
-    #check if xhost exists
-    which xhost &> /dev/null
-    if [ $? -eq 0 ]; then
-        #set the display to local 0.0 if there isn't already one
-        if [ -z "${DISPLAY}" ]; then
-            export DISPLAY=:0.0
-        fi
-        xhost +local: > /dev/null
-    else
-        #if there's no xhosts, then there's no X so force the console mode
-	    echo "No graphical environment found, running in console only mode"
-	    launchtype="-console"
+    if [ -z "${DISPLAY}" ]; then
+        echo "No DISPLAY variable set, running in console only mode"
+        launchtype="-console"
     fi
-    launch_wizard "${launchtype}"
-else 
-	launch_wizard "${launchtype}"
 fi
+launch_wizard "${launchtype}"
 
