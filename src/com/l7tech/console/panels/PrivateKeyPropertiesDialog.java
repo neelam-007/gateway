c439c827a17ba72bc9c3fa57f7f9e241ff652618
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2007-05-24 18:39:07 +0000

    Mockup of private key properties dialog
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@16280 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ src/com/l7tech/console/panels/PrivateKeyPropertiesDialog.java
@@ -0,0 +1,15 @@
+package com.l7tech.console.panels;
+
+import javax.swing.*;
+
+/**
+ *
+ */
+public class PrivateKeyPropertiesDialog {
+    private JList list1;
+    private JButton destroyPrivateKeyButton;
+    private JButton viewCertificateButton;
+    private JButton generateCSRButton;
+    private JButton closeButton;
+    private JButton replaceCertificateChainButton;
+}
