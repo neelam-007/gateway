adc80ef898e24f60e366da3cfa0040f36ef375b9
jay <jay@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2007-05-29 00:10:33 +0000

    Local Net configuration
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@16312 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ etc/solaris_build/startup/ssg_sys_config.pl
@@ -0,0 +1,226 @@
+#!/usr/bin/perl -w
+
+use strict;
+use FileHandle;
+
+my $timestamp = localtime time;
+my $logFile = new FileHandle;
+if ($logFile->open(">>ssgsysconfig.log")) {
+	$logFile->print("\n-------------------------------------------------------------------\n");
+	$logFile->print("$timestamp: System Configuration Started\n");
+	$logFile->print("-------------------------------------------------------------------\n");
+} else {
+ 	print("Could not open log file. Will not proceed with system configuration\n");
+}
+
+my %hostnameInfo = ();
+my $ntpServerToUse = undef;
+my @filesToDelete;
+my ($hostname, $domain) = ($hostnameInfo{'hostname'}, $hostnameInfo{'domain'});
+
+my $netConfigPattern = "netconfig_*";
+
+my $inputFh = new FileHandle;
+my %inputFiles = (
+	"HOSTNAMEFILE" => "hostname",
+	"NTPFILE" => "ntpconfig",
+	"TZFILE" => "timezone"
+);
+
+my $outputFh = new FileHandle;
+my %outputFiles = (
+	"NTP" 		=> "/etc/ntp.conf",
+	"TZ" 		=> "/etc/default/init",
+	"GATEWAY" 	=> "/etc/defaultrouter",
+	"RESOLV" 	=> "/etc/resolv.conf",
+	"HOSTNAME" 	=> "/etc/nodename",
+	"DOMAIN" 	=> "/etc/defaultdomain",
+	"ETC" 		=> "/etc/"
+);
+
+##########################http://xkcd.com/c208.html#####################
+
+#Setup hostname, domain name
+if ($inputFh->open("<$inputFiles{'HOSTNAMEFILE'}")) {
+	my $fileContent = do { local( $/ ) ; <$inputFh> } ;
+	%hostnameInfo = $fileContent =~ /^(\w+)=(.+)$/mg ;
+	my $hostnameToUse = $hostnameInfo{'hostname'};
+	if ($hostnameToUse ne "") {
+		$logFile->print("$timestamp: hostname found: $hostnameToUse\n");
+		if (defined($hostnameInfo{'hostname'}) && $hostnameInfo{'hostname'} ne "") {
+			($hostname, $domain) = ($hostnameInfo{'hostname'}, $hostnameInfo{'domain'});
+		
+			if ($outputFh->open(">$outputFiles{'HOSTNAME'}")) {
+				$outputFh->print("$hostname\n");
+				$outputFh->close();
+				$logFile->print("$timestamp: Wrote $outputFiles{'HOSTNAME'} with:\n");
+				$logFile->print("\t$hostname\n");
+			} else {
+				$logFile->print("$timestamp: Couldn't open $outputFiles{'HOSTNAME'}. Unable to set nodename (hostname)\n");
+			}
+			if ($outputFh->open(">$outputFiles{'DOMAIN'}")) {
+				$outputFh->print("$domain\n");
+				$outputFh->close();
+				$logFile->print("$timestamp: Wrote $outputFiles{'DOMAIN'} with:\n");
+				$logFile->print("\t$domain\n");
+			} else {
+				$logFile->print("$timestamp: Couldn't open $outputFiles{'DOMAIN'}. Unable to set domain name\n");
+			}
+		}
+	} else {
+		$logFile->print("$timestamp: no hostname found. Hostname will not be set\n");
+		%hostnameInfo = undef;
+	}
+	$inputFh->close();
+	push (@filesToDelete, $inputFiles{'HOSTNAMEFILE'});
+} else {
+	$logFile->print("$timestamp: $inputFiles{'HOSTNAMEFILE'} not found. The hostname will not be changed.\n");
+}
+
+#Setup Interfaces & Routes
+my @netConfigFiles = glob($netConfigPattern);
+for my $configFile(@netConfigFiles) {
+	if($inputFh->open("<$configFile")) {
+		my (undef, $ifName) = split("_", $configFile);
+		my $params = " ";
+		while(my $line = <$inputFh>) {
+			chomp($line);
+			$params = $params."$line ";
+		}
+
+		my %opts;
+		while ($params =~ /--([^=\s]+)(?:=|\s+)(\S+)/g) {
+			my ($option, $value) = ($1, $2);
+			if ($option eq "nameserver") {
+				push @{ $opts{$option} }, $value;
+			} else {
+				$opts{$option} = $value;
+			}
+		}
+
+	#DHCP is easy, assuming it works on boot, just create the file and that's it.
+		if ($opts{bootproto} eq "dhcp") {
+			unlink("$outputFiles{'ETC'}hostname.$opts{device}");
+			#open my $touchy, ">", "$outputFiles{'ETC'}dhcp.$opts{device}";
+			#close $touchy;
+			if($outputFh->open(">$outputFiles{'ETC'}dhcp.$opts{device}")) {
+				$outputFh->print("inet $hostname\n");
+				$outputFh->close();
+			} else {
+				print "Unable to configure DHCP for $opts{device}, weird.\n";
+			}
+		} elsif ($opts{bootproto} eq "static") {
+	#Static config is a bit of a nightmare, mostly because you need to manually edit /etc/netmasks to
+	#correspond with reality; set /etc/hostname.interface, /etc/defaultrouter, /etc/resolv.conf...
+	#Note that we're not touching /etc/hosts... which is the proper way to setup interfaces, but that
+	#much perl-based config editing could get hairy in a hurry.
+
+			my $Tip = pack "C4", split (/\./, $opts{ip}), shift;
+			my $Tmask = pack "C4", split (/\./, $opts{netmask}), shift;
+			$opts{network} = join ".", unpack "C4", ($Tip & $Tmask);
+			unlink("$outputFiles{'ETC'}dhcp.$opts{device}");
+
+			if ($outputFh->open(">$outputFiles{'ETC'}hostname.$opts{device}")) {
+				$outputFh->print("inet $opts{ip}\n");
+				$outputFh->close();
+			}
+
+			if ($outputFh->open(">$outputFiles{'ETC'}defaultrouter")) {
+				$outputFh->print($opts{gateway} . "\n");
+				$outputFh->close();
+			}
+
+			if ($outputFh->open(">$outputFiles{'ETC'}resolv.conf")) {
+				#$outputFh->print("search $hostnameInfo{'domain'}\n");
+				my $ns;
+				foreach $ns (@{$opts{nameserver}}) {
+					print "$ns\n";
+					$outputFh->print("nameserver $ns\n");
+				}
+				$outputFh->close();
+			}
+
+			##### Edit /etc/netmasks here.
+
+			#Read in the existing netmasks file
+			my @raw_data;
+			open(DAT, "$outputFiles{'ETC'}netmasks") || die("Could not open file! Your Solaris Installation is Damaged!\n");
+			@raw_data=<DAT>;
+			close(DAT);
+
+			#Write out the file with the changed TZ
+			if ($outputFh->open(">$outputFiles{'ETC'}netmasks")) {
+				my $maskline;
+				my $flag = "L7flag.$opts{device}";
+				foreach $maskline (@raw_data) {
+					if ($maskline =~ /$flag/) {
+						print "flag $flag\n";
+					} else {
+						$outputFh->print("$maskline");
+					}
+				}
+				$outputFh->print("$opts{network} $opts{netmask} #$flag\n");
+				$outputFh->close();
+			} else {
+					print "Couldn\'t open $outputFiles{'ETC'}netmasks, unable to set timezone.\n";
+			}
+
+			print "Static device $opts{device}, IP $opts{ip}, mask $opts{netmask}, net $opts{network}, gw $opts{gateway}, nameservers @{$opts{nameserver}}\n";
+		}
+		push (@filesToDelete, $configFile);
+		$inputFh->close();
+	}
+
+	#$logFile->print("$timestamp: executing network configuration command:\n");
+}
+
+########################################################################
+
+
+#Setup ntp.conf
+if ($inputFh->open("<$inputFiles{'NTPFILE'}")) {
+	my @serverNames = $inputFh->getlines();
+	$ntpServerToUse = $serverNames[0];
+	chomp($ntpServerToUse);
+	$inputFh->close();
+
+	if ($outputFh->open(">$outputFiles{'NTP'}")) {
+		$outputFh->print("server $ntpServerToUse\n");
+		$outputFh->close();
+
+		push (@filesToDelete, $inputFiles{'NTPFILE'});
+		$logFile->print("$timestamp: NTP server found: $ntpServerToUse\n");
+	} else {
+		$logFile->print("$timestamp: $inputFiles{'NTPFILE'} not writable. The NTP configuration will not be changed.\n");
+	}
+} else {
+	$logFile->print("$timestamp: $inputFiles{'NTPFILE'} not found. The NTP configuration will not be changed.\n");
+}
+
+#Setup Timezone (which is actually embedded in a common config file. Sigh)
+if($inputFh->open("<$inputFiles{'TZFILE'}")) {
+#Get the TZ, only read first line.
+	my $TZ = <$inputFh>;
+	$inputFh->close();
+	chomp($TZ);
+	#print "TZ src: $TZ\n\n";
+
+	#Read in the existing init file
+	my @raw_data;
+	open(DAT, $outputFiles{'TZ'}) || die("Could not open file! Your Solaris Installation is Damaged!\n");
+	@raw_data=<DAT>;
+	close(DAT);
+
+	#Write out the file with the changed TZ
+	if ($outputFh->open(">$outputFiles{'TZ'}")) {
+		my $initline;
+		foreach $initline (@raw_data) {
+			$initline =~ s/TZ=.*/TZ=$TZ/;
+			$outputFh->print("$initline");
+		}
+		$outputFh->close();
+	} else {
+		print "Couldn't open $outputFiles{'TZ'}, unable to set timezone.\n";
+	}
+}
+
