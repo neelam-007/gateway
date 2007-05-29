2509424a3a0a1aba1d753ecc979b53f377c0bfc4
jay <jay@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2007-05-29 00:47:39 +0000

    Username and group for SSG dirs aregateway.gateway and ssgconfig.gateway
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@16314 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- etc/solaris_build/build.sh
+++ etc/solaris_build/build.sh
@@ -77,11 +77,11 @@ echo "i depend" >> ../pkgbuild/Prototype
 echo "i copyright" >> ../pkgbuild/Prototype 
 
 ### Export/home stuff!
-du -a ./export/home/gateway | awk '{print $2}' | pkgproto | sed -e "s/build staff/gateway ssg/" >> ../pkgbuild/Prototype
-du -a ./export/home/ssgconfig | awk '{print $2}' | pkgproto | sed -e "s/build staff/ssgconfig ssg/" >> ../pkgbuild/Prototype
+du -a ./export/home/gateway | awk '{print $2}' | pkgproto | sed -e "s/build staff/gateway gateway/" >> ../pkgbuild/Prototype
+du -a ./export/home/ssgconfig | awk '{print $2}' | pkgproto | sed -e "s/build staff/ssgconfig gateway/" >> ../pkgbuild/Prototype
 
 ### Main /ssg
-du -a ./ssg | awk '{print $2}' | pkgproto | sed -e "s/build staff/gateway ssg/" >> ../pkgbuild/Prototype
+du -a ./ssg | awk '{print $2}' | pkgproto | sed -e "s/build staff/gateway gateway/" >> ../pkgbuild/Prototype
 
 ### Startups
 du -a ./etc | awk '{print $2}' | pkgproto | sed -e "s/build staff/root sys/" >> ../pkgbuild/Prototype
