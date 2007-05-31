3a3d47fcbd0b7387dd486686d7f11a884b014105
jay <jay@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2007-05-31 00:30:00 +0000

    Replacement ssgconfig without overwrite of Java Home
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@16363 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ etc/solaris_build/startup/ssgconfig.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#set the current working directory to where this script lives
+cd `dirname $0`
+pushd .. > /dev/null
+SSG_ROOT=`pwd`
+USER=${LOGNAME}
+popd > /dev/null
+
+launchtype=${1}
+
+OPTIONS="-Djava.library.path=${SSG_ROOT}/lib -Dcom.l7tech.server.home=${SSG_ROOT}"
+
+check_options(){
+    ENABLE_HSM="yesfornow"
+    if [ -n "${ENABLE_HSM}" ]; then
+        OPTIONS="$OPTIONS -Dcom.l7tech.server.keystore.enablehsm=true"
+    fi
+}
+
+launch_wizard(){
+	check_options
+	#check if we're root
+	if [ "$USER" != "ssgconfig" ]; then
+        	su -m ssgconfig -c "${JAVA_HOME}/bin/java ${OPTIONS} -jar ConfigWizard.jar $*"
+    	else
+        	${JAVA_HOME}/bin/java ${OPTIONS} -jar ConfigWizard.jar $*
+    	fi
+}
+
+if [ -z "${launchtype}" ] || [ ${launchtype} == "-graphical" ]; then
+    if [ -z "${DISPLAY}" ]; then
+        echo "No DISPLAY variable set, running in console only mode"
+        launchtype="-console"
+    fi
+fi
+launch_wizard "${launchtype}"
+
