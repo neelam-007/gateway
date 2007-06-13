ba213815193806b6acca7b40b3f86006288ff63d
megery <megery@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2007-06-13 22:59:47 +0000

    when selecting the timezone, the available timezones are displayed in batches of 10
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@16602 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/server/config/systemconfig/SystemConfigWizardNtpStep.java
+++ src/com/l7tech/server/config/systemconfig/SystemConfigWizardNtpStep.java
@@ -22,6 +22,7 @@ public class SystemConfigWizardNtpStep extends BaseConsoleStep {
 
     private static final String TITLE = "Configure Time Synchronization (NTP)";
     private NtpConfigurationBean ntpBean;
+    public static final int TIMEZONES_PER_PAGE = 10; 
 
 
     public SystemConfigWizardNtpStep(ConfigurationWizard parentWizard) {
@@ -62,42 +63,75 @@ public class SystemConfigWizardNtpStep extends BaseConsoleStep {
     }
 
     private File doTzConfigurationPrompts(File dir) throws IOException, WizardNavigationException {
-        if (dir != null) {
-            if (!dir.isDirectory()) {
-                return dir;
-            }
-        }
-        List<String> prompts = new ArrayList<String>();
-        prompts.add("Select a timezone from the following list " + getEolChar());
-        if (dir.isDirectory())
-            prompts.add("[" + dir.getName() + "]" + getEolChar());
+        if (dir == null)
+            return null;
 
-        File f = null;
+        if (!dir.isDirectory())
+            return dir;
+
+        File selectedTimeZone = null;
         if (!dir.exists()) {
             printText("*** " + "Could not determine available timezones. Timezone directory \"" + dir.getAbsolutePath() + "\" does not exist" + " ***" + getEolChar());
             logger.warning("Could not determine available timezones. Timezone directory \"" + dir.getAbsolutePath() + "\" does not exist");
         } else {
-            File[] files = dir.listFiles();
+            File[] allTimeZones = dir.listFiles();
+            File[] timezonesToDisplay = null;
+            int howManyTimeZones = allTimeZones.length;
 
-            int x = 1;
-            for (File file : files) {
-                String indexStr = String.valueOf(x++);
-                String prompt = indexStr + ") " + file.getName() + (file.isDirectory()?"[more choices]":"");
-                prompts.add(prompt + getEolChar());
+            int whichIndex = 0;
+            if (howManyTimeZones > TIMEZONES_PER_PAGE) {
+                //try paging the view
+                int displayedCount = 0;
+
+                timezonesToDisplay = new File[TIMEZONES_PER_PAGE];
+                System.arraycopy(allTimeZones, 0, timezonesToDisplay, 0, TIMEZONES_PER_PAGE);
+                while (displayedCount < howManyTimeZones && selectedTimeZone == null) {
+                    whichIndex = showTimeZones(timezonesToDisplay, dir.equals(new File(osFunctions.getTimeZonesDir()))?null:dir, displayedCount + 1);
+                    displayedCount += TIMEZONES_PER_PAGE;
+                    if (whichIndex > 0) {
+                        selectedTimeZone = allTimeZones[whichIndex];
+                    } else {
+                        System.arraycopy(allTimeZones, displayedCount, timezonesToDisplay, 0, TIMEZONES_PER_PAGE);
+                    }
+                }
+            } else {
+                whichIndex = showTimeZones(allTimeZones, dir, 1);
+                selectedTimeZone = allTimeZones[whichIndex];
             }
-            
-            prompts.add("Please make a selection [1]: ");
-            String[] allowedEntries = new String[x];
-            for (int index=1; index <= x; ++index) {
-                allowedEntries[index-1] = String.valueOf(index);
-            }
 
-            String tzSelection = getData(prompts, "1", allowedEntries);
-            int whichChoice = Integer.parseInt(tzSelection);
-
-            f = doTzConfigurationPrompts(files[whichChoice -1]);
+            selectedTimeZone = doTzConfigurationPrompts(selectedTimeZone);
         }
-        return f;
+        return selectedTimeZone;
+    }
+
+    private int showTimeZones(File[] timezones, File baseDir, int startingIndex) throws IOException, WizardNavigationException {
+        List<String> prompts = new ArrayList<String>();
+        prompts.add("Select a timezone from the following list " + getEolChar());
+
+        if (baseDir != null && baseDir.isDirectory())
+            prompts.add("[" + baseDir.getName() + "]" + getEolChar());
+
+        int x = startingIndex;
+        for (File file : timezones) {
+            String indexStr = String.valueOf(x++);
+            String prompt = indexStr + ") " + file.getName() + (file.isDirectory()?"[more choices]":"");
+            prompts.add(prompt + getEolChar());
+        }
+
+        prompts.add("Please make a selection [Enter for next page]: ");
+        String[] allowedEntries = new String[x+1];
+        for (int index=1; index <= x; ++index) {
+            allowedEntries[index-1] = String.valueOf(index);
+        }
+        String defaultValue = "";
+        allowedEntries[allowedEntries.length -1] = defaultValue;
+
+        String tzSelection = getData(prompts, "", allowedEntries);
+        if (tzSelection.equals(defaultValue)) {
+            return -1;
+        }
+        int whichChoice = Integer.parseInt(tzSelection);
+        return whichChoice - 1;
     }
 
     private void doNtpConfig() throws IOException, WizardNavigationException {
