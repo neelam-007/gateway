f284b0fffc730da6192549c363668f750ab08d36
jay <jay@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2007-06-25 20:28:47 +0000

    z: TOOL-56 Redundant file spec in creation of overlay on target system
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@16695 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- etc/solaris_build/make_overlay_tarball.sh
+++ etc/solaris_build/make_overlay_tarball.sh
@@ -2,6 +2,6 @@
 rm -rf ssg
 mkdir -p ssg/bin
 cp startup/* ssg/bin
-tar -cvf solaris_ssg_bin.tar ssg/bin/* ssg/migration/cfg/*
+tar -cvf solaris_ssg_bin.tar ssg/bin/*
 gzip solaris_ssg_bin.tar
 rm -rf ssg
