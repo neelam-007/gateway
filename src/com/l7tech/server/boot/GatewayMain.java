28522f0acee787cccb2b52069df5e5f487e1d5e0
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2007-07-24 21:06:58 +0000

    Changed top level catchall to catch Throwable instead of just LifecycleException.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@16869 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/server/boot/GatewayMain.java
+++ src/com/l7tech/server/boot/GatewayMain.java
@@ -38,7 +38,7 @@ public class GatewayMain {
     public static void main(String[] args) {
         try {
             new GatewayBoot(getServerHome()).runUntilShutdown();            
-        } catch (LifecycleException e) {
+        } catch (Throwable e) {
             e.printStackTrace(System.err);
             System.err.println("\n\n\n**** Unable to start the server: " + ExceptionUtils.getMessage(e) + "\n\n\n");
             System.exit(77);
