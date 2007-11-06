696a812b419d572bb30ad04763b49d0c52987e2e
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2007-11-06 18:10:48 +0000

    Fixed swapping of action with message in JMSEvent (#SSG-2005)
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@17520 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/server/event/system/JMSEvent.java
+++ src/com/l7tech/server/event/system/JMSEvent.java
@@ -6,9 +6,6 @@ import com.l7tech.common.Component;
 
 /**
  * Event class for use by JMS receivers.
- *
- * @author $Author$
- * @version $Revision$
  */
 public class JMSEvent extends TransportEvent {
 
@@ -19,7 +16,7 @@ public class JMSEvent extends TransportEvent {
             Level level,
             String ip,
             String message) {
-        super(source, Component.GW_JMSRECV, ip, level, message, NAME);
+        super(source, Component.GW_JMSRECV, ip, level, NAME, message);
     }
 
     //- PRIVATE
