a92b6904c2831d424cecff3585500ca35520a13e
steve <steve@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2007-12-17 20:19:28 +0000

    Fix for SSM-1440 (Identity attributes expiration date attribute duplicated for IIP)
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@17871 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/identity/mapping/InternalAttributeMapping.java
+++ src/com/l7tech/identity/mapping/InternalAttributeMapping.java
@@ -5,21 +5,16 @@ package com.l7tech.identity.mapping;
 
 import com.l7tech.identity.IdentityProviderConfig;
 import com.l7tech.identity.IdentityProviderConfigManager;
-import com.l7tech.policy.variable.DataType;
 
 /**
  * Mappings of {@link AttributeConfig}s to Internal Identity Providers
  * @author alex
  */
 public class InternalAttributeMapping extends PersistentAttributeMapping {
-    public static final AttributeHeader INTERNAL_USER_EXPIRATION = new AttributeHeader("expiration", "Expiration Date", DataType.DATE_TIME, UsersOrGroups.USERS, AttributeHeader.Builtin.BUILTIN);
+    public static final AttributeHeader INTERNAL_USER_EXPIRATION = PersistentAttributeMapping.PERSISTENT_USER_EXPIRATION;
 
     public static AttributeHeader[] getBuiltinAttributes() {
-        AttributeHeader[] superAtts = PersistentAttributeMapping.getBuiltinAttributes();
-        AttributeHeader[] atts = new AttributeHeader[superAtts.length + 1];
-        System.arraycopy(superAtts, 0, atts, 0, superAtts.length);
-        atts[atts.length-1] = INTERNAL_USER_EXPIRATION;
-        return atts;
+        return PersistentAttributeMapping.getBuiltinAttributes();
     }
 
     public InternalAttributeMapping() {
