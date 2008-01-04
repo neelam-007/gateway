2ebce50465514e640db5432c83d4c5566ca33df0
steve <steve@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2008-01-04 00:23:20 +0000

    Fix for SSM-1451 (Policy Fragment is labeled as Deleted when user has no permission to read)
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@17969 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/console/tree/policy/IncludeAssertionPolicyNode.java
+++ src/com/l7tech/console/tree/policy/IncludeAssertionPolicyNode.java
@@ -5,11 +5,13 @@ package com.l7tech.console.tree.policy;
 
 import com.l7tech.common.policy.Policy;
 import com.l7tech.common.util.ExceptionUtils;
+import com.l7tech.common.security.rbac.PermissionDeniedException;
 import com.l7tech.console.tree.AbstractTreeNode;
 import com.l7tech.console.util.Registry;
 import com.l7tech.policy.assertion.Include;
 import com.l7tech.policy.assertion.Assertion;
 import com.l7tech.policy.assertion.composite.CompositeAssertion;
+import com.l7tech.objectmodel.FindException;
 
 import java.util.logging.Logger;
 import java.util.logging.Level;
@@ -24,6 +26,7 @@ public class IncludeAssertionPolicyNode extends AssertionTreeNode<Include> {
     private static final Logger logger = Logger.getLogger(IncludeAssertionPolicyNode.class.getName());
 
     private Policy policy;
+    private boolean permissionDenied;
 
     public IncludeAssertionPolicyNode(Include assertion) {
         super(assertion);
@@ -45,11 +48,18 @@ public class IncludeAssertionPolicyNode extends AssertionTreeNode<Include> {
         }
     }
 
+    @Override
     public String getName() {
         Policy policy = getPolicy();
         StringBuilder sb = new StringBuilder("Include: ");
         if (policy == null) {
-            sb.append("Deleted Policy #").append(assertion.getPolicyOid());
+            if ( permissionDenied ) {
+                sb.append("Permission Denied");
+            } else {
+                sb.append("Deleted");
+            }
+            sb.append(" Policy #");
+            sb.append(assertion.getPolicyOid());
             String name = assertion.getPolicyName();
             if (name != null) sb.append(" (").append(name).append(")");
         } else {
@@ -60,15 +70,20 @@ public class IncludeAssertionPolicyNode extends AssertionTreeNode<Include> {
 
     private Policy getPolicy() {
         if (policy == null) {
+            permissionDenied = false;
             try {
                 policy = Registry.getDefault().getPolicyAdmin().findPolicyByPrimaryKey(assertion.getPolicyOid());
-            } catch (Exception e) {
+            } catch ( PermissionDeniedException pde ) {
+                logger.log(Level.WARNING, "Couldn't load included policy [permission denied]");                
+                permissionDenied = true;
+            } catch ( FindException e ) {
                 logger.log(Level.WARNING, "Couldn't load included policy", e);
             }
         }
         return policy;
     }
 
+    @Override
     protected String iconResource(boolean open) {
         return "com/l7tech/console/resources/folder.gif";
     }
