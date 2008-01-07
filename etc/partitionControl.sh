d1e7eb0cd6c057a01bf81fb23ef111bfd6832901
megery <megery@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2008-01-07 19:58:11 +0000

    bug 4435, 4538:
    better "usage" which actually appears if it doesn't recognize the command issued.
    
    partitionControl can now be used to manage all partitions with one command
    
    start/stop/usage are all multiple partition aware but can still be used with a single partition name if desired
    
    run requires a single partition name to be provided
    
    
    
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@18006 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- etc/partitionControl.sh
+++ etc/partitionControl.sh
@@ -28,6 +28,39 @@ usage() {
     echo "  usage - show this message"
     echo "  partition-name - the name of the partition to control"
     echo
+    exit 0;
+}
+
+check_environment() {
+    if [ ! -d "${SSG_HOME}" ] ; then
+        if [ -z "${SSG_HOME}" ] ; then
+            echo ""
+            echo "SSG_HOME is not set! Please set SSG_HOME to the installation root of the SecureSpan Gateway (ex. /ssg)"
+            exit 1;
+        else
+            echo
+            echo "Invalid SSG_HOME! Please set SSG_HOME to the installation root of the SecureSpan Gateway (ex. /ssg)"
+            exit 1;
+        fi
+    fi
+}
+
+check_user() {
+    if [ ${USER} != "root" ] ; then
+        echo
+        echo "This script must be run as root"
+        echo
+        exit 1;
+    fi
+}
+
+check_enabled() {
+    if [ -e ${ENABLED_FILE} ] ; then
+        return 0
+    else
+        explain_not_enabled
+        return 1
+    fi
 }
 
 explain_not_enabled() {
@@ -36,7 +69,6 @@ explain_not_enabled() {
     echo "To enable it, configure this partition using the SecureSpan Configuration Wizard or "
     echo "create a file called \"enabled\" in the partition directory [${PARTITION_DIR}]"
     echo
-    return 1
 }
 
 build_paths() {
@@ -54,88 +86,71 @@ do_firewall() {
     fi
 }
 
-do_control() {
-    do_firewall
-    if [ "${PARTITION_NAME}"  == "default_" ] && [ -e  /usr/local/Tarari ] ; then
-    	ORIGINAL_JAVA_OPTS="-Dcom.l7tech.common.xml.tarari.enable=true $ORIGINAL_JAVA_OPTS"
-    fi
-
-    if  [ -e "${SSG_HOME}/etc/conf/partitions/${PARTITION_NAME}/cluster_hostname" ]; then
-    	RMI_HOSTNAME="$(<${SSG_HOME}/etc/conf/partitions/${PARTITION_NAME}/cluster_hostname)"
+control_one_or_more_partitions() {
+    if [ -z "${PARTITION_NAME}" ] ; then
+        for partition_dir in ${ALL_PARTITIONS}
+        do
+            PARTITION_NAME="${partition_dir}"
+            control_single_partition
+        done
     else
-        RMI_HOSTNAME="$(hostname)"
+        control_single_partition
     fi
 
-    echo ${ORIGINAL_JAVA_OPTS} | grep java.rmi.server.hostname &>/dev/null
-    if [ $? -eq 0 ] ; then
-        ORIGINAL_JAVA_OPTS=$(echo ${ORIGINAL_JAVA_OPTS} | sed "s/-Djava.rmi.server.hostname=[^ ]*/-Djava.rmi.server.hostname=${RMI_HOSTNAME}/")
-    else
-        ORIGINAL_JAVA_OPTS="${ORIGINAL_JAVA_OPTS} -Djava.rmi.server.hostname=${RMI_HOSTNAME}"
-    fi
-
-    JAVA_OPTS="${ORIGINAL_JAVA_OPTS} -Djava.security.properties==${PARTITION_DIR}/java.security -Dcom.l7tech.server.partitionName=${PARTITION_NAME}"
-    export JAVA_OPTS
-    export SSG_HOME
-    export GATEWAY_PID
-    export GATEWAY_SHUTDOWN
-
-    if [ "${COMMAND}" == "start" ] ; then
-        if [ -f "${GATEWAY_PID}" ]  && [ -d "/proc/$(< ${GATEWAY_PID})" ] ; then
-            return 1
-        fi
-
-        (su $SSGUSER -c "${SSG_HOME}/bin/gateway.sh ${COMMAND}") <&- &>/dev/null &
-    else
-        (su $SSGUSER -c "${SSG_HOME}/bin/gateway.sh ${COMMAND}")
-    fi
 }
 
-COMMAND=${1}
-PARTITION_NAME=${2}
+control_single_partition() {
+    fail_if_no_partition
+    build_paths
 
-ensure_JDK
-
-if [ ${USER} != "root" ] ; then
-    echo
-    echo "This script must be run as root"
-    echo
-    exit 1;
-fi
-
-if [ -z "${COMMAND}" ] ; then
-    echo
-    echo "Please specify a command."
-    usage
-    exit 1;
-fi
-
-if [ -z "${PARTITION_NAME}" ] ; then
-    echo
-    echo "No partition name specified."
-    usage
-    exit 1;
-fi
-
-if [ "${COMMAND}" == "usage" ] ; then
-    usage;
-    exit 0;
-fi
-
-if [ ! -d "${SSG_HOME}" ] ; then
-    if [ -z "${SSG_HOME}" ] ; then
-        echo ""
-        echo "SSG_HOME is not set! Please set SSG_HOME to the installation root of the SecureSpan Gateway (ex. /ssg)"
-        exit 1;
-    else
-        echo
-        echo "Invalid SSG_HOME! Please set SSG_HOME to the installation root of the SecureSpan Gateway (ex. /ssg)"
-        exit 1;
+    #stop here if this is a status check
+    if [ "${COMMAND}" == "status" ] ; then
+        do_status
+        return
     fi
-fi
 
-build_paths
+    echo
+    echo "Attempting to ${COMMAND} the ${PARTITION_NAME} partition ..."
+    check_enabled
+    if [ $? -eq 0 ] ; then
 
-if [ "${COMMAND}" == "status" ] ; then
+        do_firewall
+
+        if [ "${PARTITION_NAME}"  == "default_" ] && [ -e  /usr/local/Tarari ] ; then
+            ORIGINAL_JAVA_OPTS="-Dcom.l7tech.common.xml.tarari.enable=true $ORIGINAL_JAVA_OPTS"
+        fi
+
+        if  [ -e "${SSG_HOME}/etc/conf/partitions/${PARTITION_NAME}/cluster_hostname" ]; then
+            RMI_HOSTNAME="$(<${SSG_HOME}/etc/conf/partitions/${PARTITION_NAME}/cluster_hostname)"
+        else
+            RMI_HOSTNAME="$(hostname)"
+        fi
+
+        echo ${ORIGINAL_JAVA_OPTS} | grep java.rmi.server.hostname &>/dev/null
+        if [ $? -eq 0 ] ; then
+            ORIGINAL_JAVA_OPTS=$(echo ${ORIGINAL_JAVA_OPTS} | sed "s/-Djava.rmi.server.hostname=[^ ]*/-Djava.rmi.server.hostname=${RMI_HOSTNAME}/")
+        else
+            ORIGINAL_JAVA_OPTS="${ORIGINAL_JAVA_OPTS} -Djava.rmi.server.hostname=${RMI_HOSTNAME}"
+        fi
+
+        JAVA_OPTS="${ORIGINAL_JAVA_OPTS} -Djava.security.properties==${PARTITION_DIR}/java.security -Dcom.l7tech.server.partitionName=${PARTITION_NAME}"
+        export JAVA_OPTS
+        export SSG_HOME
+        export GATEWAY_PID
+        export GATEWAY_SHUTDOWN
+
+        if [ "${COMMAND}" == "start" ] ; then
+            if [ -f "${GATEWAY_PID}" ]  && [ -d "/proc/$(< ${GATEWAY_PID})" ] ; then
+                return 1
+            fi
+            (su $SSGUSER -c "${SSG_HOME}/bin/gateway.sh ${COMMAND}") <&- &>/dev/null &
+        else
+            (su $SSGUSER -c "${SSG_HOME}/bin/gateway.sh ${COMMAND}")
+        fi
+    fi  
+}
+
+do_status() {
     STATUS=1
     if [ -f "${GATEWAY_PID}" ]  && [ -d "/proc/$(< ${GATEWAY_PID})" ] ; then
         STATUS=0
@@ -146,12 +161,54 @@ if [ "${COMMAND}" == "status" ] ; then
     else
         echo "${PARTITION_NAME} is inactive."
     fi
+    return $STATUS
+}
 
-    exit ${STATUS}
+fail_if_no_partition() {
+    if [ -z "${PARTITION_NAME}" ] ; then
+        echo
+        echo "A partition name must be specified for the \"${COMMAND}\" command"
+        usage
+        exit 1;
+    fi
+}
+
+ensure_JDK
+COMMAND=${1}
+PARTITION_NAME=${2}
+
+if [ -z "${COMMAND}" ] ; then
+    echo
+    echo "Please specify a command."
+    usage
+    exit 1;
 fi
 
-if [ -e ${ENABLED_FILE} ] ; then
-    do_control
-else
-    explain_not_enabled
-fi
+check_environment
+
+case "$COMMAND" in
+    start)
+        check_user
+        control_one_or_more_partitions
+        ;;
+    stop)
+        check_user
+        control_one_or_more_partitions
+        ;;
+    status)
+        check_user
+        control_one_or_more_partitions
+        ;;
+    run)
+        check_user
+        control_single_partition
+        ;;
+    usage)
+        usage
+        ;;
+    *)
+        echo "${COMMAND} is not a valid command."
+        echo
+        usage
+        ;;
+esac
