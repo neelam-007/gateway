f2eb3683251e7c888f27fe3da65e4aff6865eaab
njordan <njordan@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2008-01-15 21:59:37 +0000

    Disabled imports and includes for XSL documents in Tarari mode. (#SSG-2000)
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@18079 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/common/xml/tarari/TarariCompiledStylesheetImpl.java
+++ src/com/l7tech/common/xml/tarari/TarariCompiledStylesheetImpl.java
@@ -11,9 +11,11 @@ import com.l7tech.common.util.Functions;
 import com.tarari.xml.XmlParseException;
 import com.tarari.xml.XmlResult;
 import com.tarari.xml.XmlSource;
+import com.tarari.xml.util.UriResolver;
 import com.tarari.xml.rax.RaxDocument;
 import com.tarari.xml.xslt11.Stylesheet;
 import com.tarari.xml.xslt11.parser.XsltParseException;
+import com.tarari.xml.xslt11.parser.StylesheetParser;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
@@ -30,12 +32,37 @@ import java.text.ParseException;
 public class TarariCompiledStylesheetImpl implements TarariCompiledStylesheet {
     private final Stylesheet master;
 
+    // Empty XSLT document
+    private static final byte[] EMPTY_XSL;
+    static {
+        byte[] bytes = null;
+        try {
+            bytes = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<xsl:stylesheet xmlns:xsl=\"http://www.w3.org/1999/XSL/Transform\" version=\"1.0\"></xsl:stylesheet>".getBytes("UTF-8");
+        } catch(java.io.UnsupportedEncodingException e) {
+            // Will never happen
+        }
+
+        EMPTY_XSL = bytes;
+    }
+    private static final UriResolver NON_FETCHING_URI_RESOLVER = new UriResolver() {
+        public XmlSource resolveUri(String s, String s1) throws java.io.IOException {
+            return new XmlSource(s, EMPTY_XSL);
+        }
+    };
+
     // Thread-local XmlSource for transformations.
     private static final ThreadLocal xmlSource = new ThreadLocal() {
         protected Object initialValue() {
             return new XmlSource(new EmptyInputStream());
         }
     };
+
+    public static StylesheetParser getStylesheetParser() {
+        StylesheetParser stylesheetParser = new StylesheetParser();
+        stylesheetParser.setUriResolver(NON_FETCHING_URI_RESOLVER);
+
+        return stylesheetParser;
+    }
 
     /**
      * Wrap the specified Tarari Stylesheet instance.
@@ -47,7 +74,7 @@ public class TarariCompiledStylesheetImpl implements TarariCompiledStylesheet {
         try {
             InputSource is = new InputSource(new StringReader(stylesheet));
             is.setSystemId("");
-            master = Stylesheet.create(new XmlSource(is));
+            master = getStylesheetParser().parseStylesheet(new XmlSource(is));
         } catch (XsltParseException e) {
             throw (ParseException)new ParseException("Unable to parse XSLT: " + ExceptionUtils.getMessage(e), 0).initCause(e);
         } catch (IOException e) {
