eb129427d47f42a0aeaff5c048f80624beb3e56c
ghuang <ghuang@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2008-01-16 00:14:24 +0000

    Fixed the SSG-1985 (Downgrade Audit Message #100 (Couldn't get style for BindingOperation {0}; assuming "document") to FINE.)  The reason of changing INFO to FINE is "Document style is the default, there's no need to yell about it."
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@18080 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/common/audit/CommonMessages.java
+++ src/com/l7tech/common/audit/CommonMessages.java
@@ -10,7 +10,7 @@ import java.util.logging.Level;
  * The ID range 100-999 inclusive is reserved for these messages.
  */
 public class CommonMessages extends Messages {
-    public static final M WSDL_OPERATION_NO_STYLE         = m(100, Level.INFO, "Couldn''t get style for BindingOperation {0}; assuming \"document\"");
+    public static final M WSDL_OPERATION_NO_STYLE         = m(100, Level.FINE, "Couldn''t get style for BindingOperation {0}; assuming \"document\"");
     public static final M WSDL_OPERATION_PART_TYPE        = m(101, Level.INFO, "Part {0} has both an element and a type");
     public static final M WSDL_OPERATION_BAD_STYLE        = m(102, Level.INFO, "Unsupported style ''{0}'' for {1}");
     public static final M WSDL_OPERATION_NO_QNAMES_FOR_OP = m(103, Level.INFO, "Unable to find payload element QNames for BindingOperation {0}");
