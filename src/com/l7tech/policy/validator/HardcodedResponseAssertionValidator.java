afbbc579f6e154565dfbc2d2709c0b619630364b
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2008-01-18 23:35:18 +0000

    SSM-1467: There is now a validator warning if a template response is given an XML content type but the response body is empty or isn't well-formed XML.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@18121 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/policy/validator/HardcodedResponseAssertionValidator.java
+++ src/com/l7tech/policy/validator/HardcodedResponseAssertionValidator.java
@@ -1,10 +1,13 @@
 package com.l7tech.policy.validator;
 
 import com.l7tech.common.mime.ContentTypeHeader;
+import com.l7tech.common.util.ExceptionUtils;
+import com.l7tech.common.util.XmlUtil;
 import com.l7tech.common.xml.Wsdl;
 import com.l7tech.policy.AssertionPath;
 import com.l7tech.policy.PolicyValidatorResult;
 import com.l7tech.policy.assertion.HardcodedResponseAssertion;
+import org.xml.sax.SAXException;
 
 import java.io.IOException;
 
@@ -13,19 +16,35 @@ import java.io.IOException;
  */
 public class HardcodedResponseAssertionValidator implements AssertionValidator {
     private final HardcodedResponseAssertion ass;
-    private Throwable e;
+    private String ctypeErr;
+    private String xmlErr;
 
     public HardcodedResponseAssertionValidator(HardcodedResponseAssertion ass) {
         this.ass = ass;
+        ContentTypeHeader ctype = null;
         try {
-            ContentTypeHeader.parseValue(ass.getResponseContentType());
+            ctype = ContentTypeHeader.parseValue(ass.getResponseContentType());
         } catch (IOException e) {
-            this.e = e;
+            this.ctypeErr = ExceptionUtils.getMessage(e);
+        }
+
+        try {
+            if (ctype != null && ctype.isXml()) {
+                final String body = ass.responseBodyString();
+                if (body == null || body.trim().length() < 1)
+                    xmlErr = "it is completely empty";
+                else
+                    XmlUtil.stringToDocument(body);
+            }
+        } catch (SAXException e) {
+            this.xmlErr = ExceptionUtils.getMessage(e);
         }
     }
 
     public void validate(AssertionPath path, Wsdl wsdl, boolean soap, PolicyValidatorResult result) {
-        if (e != null)
-            result.addError(new PolicyValidatorResult.Error(ass, path, "the content type is invalid. " + e.getMessage(), null));
+        if (ctypeErr != null)
+            result.addError(new PolicyValidatorResult.Error(ass, path, "The content type is invalid: " + ctypeErr, null));
+        if (xmlErr != null)
+            result.addWarning(new PolicyValidatorResult.Warning(ass, path, "XML response is not well-formed: " + xmlErr, null));
     }
 }
