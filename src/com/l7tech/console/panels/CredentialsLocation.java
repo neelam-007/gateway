002f7960048f06c23422a14cffd4e7ef7d563a64
steve <steve@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2008-01-29 18:00:37 +0000

    Fix for SSM-1498 (Publish SOAP Web Service wizard - missing authentication methods)
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@18201 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/console/panels/CredentialsLocation.java
+++ src/com/l7tech/console/panels/CredentialsLocation.java
@@ -2,13 +2,16 @@ package com.l7tech.console.panels;
 
 import com.l7tech.policy.assertion.SslAssertion;
 import com.l7tech.policy.assertion.TrueAssertion;
+import com.l7tech.policy.assertion.Assertion;
 import com.l7tech.policy.assertion.credential.http.HttpBasic;
 import com.l7tech.policy.assertion.credential.http.HttpDigest;
 import com.l7tech.policy.assertion.credential.http.CookieCredentialSourceAssertion;
+import com.l7tech.policy.assertion.credential.http.HttpNegotiate;
 import com.l7tech.policy.assertion.credential.wss.WssBasic;
 import com.l7tech.policy.assertion.credential.wss.EncryptedUsernameTokenAssertion;
 import com.l7tech.policy.assertion.xmlsec.RequestWssX509Cert;
 import com.l7tech.policy.assertion.xmlsec.SecureConversation;
+import com.l7tech.policy.assertion.xmlsec.RequestWssKerberos;
 
 import javax.swing.*;
 import java.util.Map;
@@ -57,8 +60,7 @@ public class CredentialsLocation {
         Object[] values = newCredentialsLocationMap(soap).keySet().toArray();
         Object[] newValues = new Object[values.length - 1];
         System.arraycopy(values, 1, newValues, 0, values.length - 1);
-        ComboBoxModel cbm = new DefaultComboBoxModel(newValues);
-        return cbm;
+        return new DefaultComboBoxModel(newValues);
     }
 
     /**
@@ -70,18 +72,20 @@ public class CredentialsLocation {
      * @return the new credentials location map
      */
     static Map newCredentialsLocationMap(boolean soap) {
-        Map credentialsLocationMap = new TreeMap();
+        Map<String, Assertion> credentialsLocationMap = new TreeMap<String, Assertion>();
         credentialsLocationMap.put("Anonymous", new TrueAssertion());
 
         credentialsLocationMap.put("HTTP Basic", new HttpBasic());
         credentialsLocationMap.put("HTTP Digest", new HttpDigest());
         credentialsLocationMap.put("HTTP Cookie (Single-sign-on token)", new CookieCredentialSourceAssertion());
         credentialsLocationMap.put("SSL or TLS with Client Certificate", new SslAssertion(true));
+        credentialsLocationMap.put("Windows Integrated", new HttpNegotiate() );
         if (soap) {
             credentialsLocationMap.put("WS Token Basic", new WssBasic());
             credentialsLocationMap.put("Encrypted UsernameToken", new EncryptedUsernameTokenAssertion());
             credentialsLocationMap.put("WSS Signature", new RequestWssX509Cert());
             credentialsLocationMap.put("WS Secure Conversation", new SecureConversation());
+            credentialsLocationMap.put("WSS Kerberos", new RequestWssKerberos());
         }
 
         return credentialsLocationMap;
