2419ae594e31f604b808ba4d65041be0064534b9
steve <steve@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2008-02-07 18:15:35 +0000

    Fix for SSG-2271 (New NCES licenses needs to work with 4.3)
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@18319 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/assertions/NcesValidatorAssertion/src/com/l7tech/external/assertions/ncesval/NcesValidatorAssertion.java
+++ modules/assertions/NcesValidatorAssertion/src/com/l7tech/external/assertions/ncesval/NcesValidatorAssertion.java
@@ -89,7 +89,7 @@ public class NcesValidatorAssertion extends Assertion implements UsesVariables {
 
         // request default feature set name for our class name, since we are a known optional module
         // that is, we want our required feature set to be "assertion:NcesValidator" rather than "set:modularAssertions"
-        meta.put(AssertionMetadata.FEATURE_SET_NAME, "set:modularAssertions");
+        meta.put(AssertionMetadata.FEATURE_SET_NAME, "(fromClass)");
 
         meta.put(AssertionMetadata.POLICY_VALIDATOR_CLASSNAME, "com.l7tech.external.assertions.ncesval.NcesValidatorAssertionValidator");
         meta.put(AssertionMetadata.PROPERTIES_EDITOR_CLASSNAME, "com.l7tech.external.assertions.ncesval.console.NcesValidatorAssertionPropertiesDialog");
