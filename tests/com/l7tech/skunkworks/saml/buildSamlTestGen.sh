f3f076c947aa0b04a4d8f9df08f9ea68896874cf
steve <steve@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2008-03-19 21:48:32 +0000

    Update skunkworks build for SAML generation util.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@18549 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- tests/com/l7tech/skunkworks/saml/buildSamlTestGen.sh
+++ tests/com/l7tech/skunkworks/saml/buildSamlTestGen.sh
@@ -47,7 +47,7 @@ for arg in "$@" ; do
   fi
 done
 
-ANT_JARS="$SRC_ROOT/lib/ant.jar:$SRC_ROOT/lib/optional.jar:$SRC_ROOT/lib/xercesImpl.jar:$SRC_ROOT/lib/xml-apis.jar:$SRC_ROOT/lib/junit.jar:$SRC_ROOT/lib/ant-contrib.jar"
+ANT_JARS="$SRC_ROOT/lib/ant.jar:$SRC_ROOT/lib/ant-launcher.jar:$SRC_ROOT/lib/ant-nodeps.jar:$SRC_ROOT/lib/xercesImpl.jar:$SRC_ROOT/lib/xml-apis.jar:$SRC_ROOT/lib/tools/junit-4.4.jar:$SRC_ROOT/lib/ant-junit.jar:$SRC_ROOT/lib/ant-contrib-1.0b3.jar:$SRC_ROOT/lib/xalan-2.5.2.jar:$SRC_ROOT/lib/ant-apache-bcel.jar:$SRC_ROOT/lib/tools/ant-trax.jar:$SRC_ROOT/lib/tools/bcel-5.2.jar"
 CLASSPATH="${JDK_CLASSES}:${ANT_JARS}"
 export CLASSPATH
 
