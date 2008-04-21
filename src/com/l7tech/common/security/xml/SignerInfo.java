f554021c9b58ff5fd1077aa5b309fd6292bed562
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2008-04-21 21:25:27 +0000

    Removed unused static method
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@18655 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- src/com/l7tech/common/security/xml/SignerInfo.java
+++ src/com/l7tech/common/security/xml/SignerInfo.java
@@ -1,8 +1,8 @@
 package com.l7tech.common.security.xml;
 
-import java.security.*;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.cert.X509Certificate;
-import java.util.NoSuchElementException;
 
 /**
  * Class <code>SignerInfo</code> is the simple holder for a public
@@ -14,44 +14,6 @@ public class SignerInfo {
 
     private PrivateKey privateKey;
     private X509Certificate[] certificateChain;
-
-    /**
-     * Create the <code>SignerInfo</code> from the private key and certificate retrieved
-     * from the keystore.
-     *
-     * @param keyStore the keystore
-     * @param keyAlias the key alias in the key store
-     * @param password the key password
-     * @return the <code>SignerInfo</code> instance for the obtaied key and certificate
-     * @throws KeyStoreException thrown on error when working with the keystore
-     * @throws NoSuchAlgorithmException thrown if the key alghoritm cannot be found
-     * @throws UnrecoverableKeyException thrown if unable to retrieve the key (password wrong)
-     * @throws NoSuchElementException thrown if unable to find the key or certificate for the alias
-     */
-    public static SignerInfo newInstance(KeyStore keyStore, String keyAlias, String password)
-      throws KeyStoreException, NoSuchAlgorithmException, UnrecoverableKeyException {
-        if (keyStore == null || keyAlias == null) {
-            throw new IllegalArgumentException();
-        }
-
-        X509Certificate[] certs = (X509Certificate[])keyStore.getCertificateChain(keyAlias);
-        if (certs == null || certs.length == 0) {
-            throw new NoSuchElementException("No certificate for alias '"+keyAlias+"' in keystore");
-        }
-        char[] pwd = password !=null ? password.toCharArray() : new char[] {};
-        Key key = keyStore.getKey(keyAlias, pwd);
-        if (key == null) {
-            throw new NoSuchElementException("No private key for alias '"+keyAlias+"' in keystore");
-        }
-
-        PrivateKey pk = null;
-        if (key instanceof PrivateKey) {
-            pk = (PrivateKey)key;
-        } else {
-            throw new IllegalArgumentException("Expected private key, received :"+key.getClass()+"\n"+key);
-        }
-        return new SignerInfo(pk, certs);
-    }
 
     /**
      * Constructs a signer info from the given private key and certificate.
