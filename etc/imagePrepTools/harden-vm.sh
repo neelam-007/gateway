880a93f59043c56bb2f3099f2e1a764141ad4604
abailward <abailward@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2008-07-10 21:09:57 +0000

    Added a script for vmware cleanup for imaging
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@19345 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ etc/imagePrepTools/harden-vm.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+echo "Cleaning up ssh..."
+rm -rf ~/.ssh/
+
+echo "Cleaning mysql..."
+service mysqld stop
+rm -f /var/lib/mysql/ib*
+rm -f /var/lib/mysql/*.log
+
+echo "Cleaning logs..."
+rm -f /var/log/*/*
+find /var/log -maxdepth 1 -type f | grep -v '/btmp$' | xargs rm -f
+
+echo "Zeroing disk space..."
+# Zero out space only on hardware image, no VMWare
+MB_TO_ZERO=$(df -B 1M / | tail -1 | awk '{print $4}')
+dd if=/dev/zero of=/tmp/zeros bs=1M count=$MB_TO_ZERO
+rm -f /tmp/zeros
+
+echo "Aging accounts..."
+# software only - set expire of passwords to now as no sealsys
+chage -d 0 root
+chage -d 0 ssgconfig
+
+echo "Cleaning network...."
+ifdown eth0
+rm -f /etc/resolv.conf.*
+echo "" > /etc/resolv.conf
+echo "" > /etc/security/opasswd
+
+# below only applies if run interactively
+echo "Done, now run:"
+echo ""
+echo "export HISTSIZE=0"
+echo "rm -f /root/.bash_history"
+echo "rm $0"
+echo "poweroff -d"
+echo ""
+echo "And enjoy your shiny new VM"
