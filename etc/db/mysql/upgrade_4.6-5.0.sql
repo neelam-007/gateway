a0b1c86e846333388a2db2c17a84ff727069168f
steve <steve@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2008-07-11 18:22:49 +0000

    SSG-2523: Add description to generated roles for policies.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@19350 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- etc/db/mysql/upgrade_4.6-5.0.sql
+++ etc/db/mysql/upgrade_4.6-5.0.sql
@@ -13,6 +13,12 @@ BEGIN;
 ALTER TABLE rbac_assignment ADD CONSTRAINT `identity_provider_ibfk_1` FOREIGN KEY (`provider_oid`) REFERENCES `identity_provider` (`objectid`) ON DELETE CASCADE;
 COMMIT;
 
+
+--
+-- Add description to POLICY roles
+--
+UPDATE rbac_role set description='Users assigned to the {0} role have the ability to read, update and delete the {1} policy.' where entity_type='POLICY' and entity_oid IS NOT NULL and description is NULL;
+
 --
 -- Reenable FK at very end of script
 --
