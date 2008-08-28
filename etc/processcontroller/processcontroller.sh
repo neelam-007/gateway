ffa242e59894aeb0d332c43abb7ecd8a0e29214d
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2008-08-28 19:24:17 +0000

    Embryonic PC scripts
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@19872 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ etc/processcontroller/processcontroller.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#############################################################################
+# SecureSpan Process Controller Launch Script
+#############################################################################
+
+#
+if [ -z "${SSPC_HOME}" ] ; then
+  SSPC_HOME="/opt/SecureSpan/Gateway/Controller"
+fi
+if [ -z "${JAVA_HOME}" ] ; then
+  JAVA_HOME="/ssg/jdk"
+fi
+
+PID_FILE=""
+if [ ! -z "${1}" ] ; then
+  PID_FILE="${1}"
+fi
+
+#
+function fail() {
+  echo "$2"
+  exit ${1}
+}
+
+#
+cd "${SSPC_HOME}" || fail "Directory not found: ${SSPC_HOME}"
+
+#
+"${JAVA_HOME}/bin/java" -jar Controller.jar &>/dev/null <&- &
+if [ ${?} -eq 0 ] ; then
+  [ -z "${PID_FILE}" ] || echo "${!}" > "${PID_FILE}"
+else
+  [ -z "${PID_FILE}" ] || rm -f "${PID_FILE}" &>/dev/null
+  fail "${?}" "Error starting Process Controller."
+fi
+
+exit 0;
