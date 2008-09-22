814d004a84be773c664622183a212afbdf8cdc34
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2008-09-22 19:38:48 +0000

    SchemaUpdater done
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@20280 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/skunkworks/src/main/java/com/l7tech/skunkworks/SchemaUpdater.java
+++ modules/skunkworks/src/main/java/com/l7tech/skunkworks/SchemaUpdater.java
@@ -1,19 +1,70 @@
 package com.l7tech.skunkworks;
 
-import org.hibernate.cfg.Configuration;
+import com.mysql.jdbc.Driver;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.dialect.MySQLDialect;
 import org.hibernate.tool.hbm2ddl.SchemaUpdate;
 
 /**
  * Copyright (C) 2006 Layer 7 Technologies Inc.
  */
 public class SchemaUpdater {
-    private final Configuration config;
+    private final AnnotationConfiguration config;
 
     public SchemaUpdater() {
-        config = new Configuration();
-        config.addFile("etc/db/SSG.hbm.xml");
-        config.addFile("etc/db/audit.hbm.xml");
-        config.addFile("etc/db/rbac.hbm.xml");
+        config = new AnnotationConfiguration();
+        config.addResource("com/l7tech/server/resources/SSG.hbm.xml");
+        config.setProperty("hibernate.dialect", MySQLDialect.class.getName());
+        config.setProperty("hibernate.connection.driver_class", Driver.class.getName());
+        config.setProperty("hibernate.connection.url", "jdbc:mysql://localhost/ssg");
+        config.setProperty("hibernate.connection.username", "gateway");
+        config.setProperty("hibernate.connection.password", "7layer");
+        config.addAnnotatedClass(com.l7tech.gateway.common.transport.SsgConnector.class);
+        config.addAnnotatedClass(com.l7tech.gateway.common.transport.SsgConnectorProperty.class);
+        config.addAnnotatedClass(com.l7tech.gateway.common.transport.jms.JmsConnection.class);
+        config.addAnnotatedClass(com.l7tech.gateway.common.transport.jms.JmsEndpoint.class);
+
+        config.addAnnotatedClass(com.l7tech.identity.IdentityProviderConfig.class);
+        config.addAnnotatedClass(com.l7tech.identity.internal.InternalGroup.class);
+        config.addAnnotatedClass(com.l7tech.identity.internal.InternalUser.class);
+        config.addAnnotatedClass(com.l7tech.identity.internal.InternalGroupMembership.class);
+        config.addAnnotatedClass(com.l7tech.identity.ldap.LdapIdentityProviderConfig.class);
+        config.addAnnotatedClass(com.l7tech.identity.fed.FederatedIdentityProviderConfig.class);
+        config.addAnnotatedClass(com.l7tech.identity.fed.FederatedUser.class);
+        config.addAnnotatedClass(com.l7tech.identity.fed.FederatedGroup.class);
+        config.addAnnotatedClass(com.l7tech.identity.fed.FederatedGroupMembership.class);
+        config.addAnnotatedClass(com.l7tech.identity.fed.VirtualGroup.class);
+
+        config.addAnnotatedClass(com.l7tech.server.wsdm.subscription.Subscription.class);
+
+        config.addAnnotatedClass(com.l7tech.gateway.common.security.rbac.Role.class);
+        config.addAnnotatedClass(com.l7tech.gateway.common.security.rbac.RoleAssignment.class);
+        config.addAnnotatedClass(com.l7tech.gateway.common.security.rbac.Permission.class);
+        config.addAnnotatedClass(com.l7tech.gateway.common.security.rbac.ScopePredicate.class);
+        config.addAnnotatedClass(com.l7tech.gateway.common.security.rbac.AttributePredicate.class);
+        config.addAnnotatedClass(com.l7tech.gateway.common.security.rbac.ObjectIdentityPredicate.class);
+
+        config.addAnnotatedClass(com.l7tech.gateway.common.audit.AuditRecord.class);
+        config.addAnnotatedClass(com.l7tech.gateway.common.audit.AdminAuditRecord.class);
+        config.addAnnotatedClass(com.l7tech.gateway.common.audit.MessageSummaryAuditRecord.class);
+        config.addAnnotatedClass(com.l7tech.gateway.common.audit.SystemAuditRecord.class);
+        config.addAnnotatedClass(com.l7tech.gateway.common.audit.AuditDetail.class);
+        config.addAnnotatedClass(com.l7tech.gateway.common.audit.MessageSummaryAuditDetail.class);
+
+        config.addAnnotatedClass(com.l7tech.gateway.common.mapping.MessageContextMappingKeys.class);
+        config.addAnnotatedClass(com.l7tech.gateway.common.mapping.MessageContextMappingValues.class);
+
+        config.addAnnotatedClass(com.l7tech.gateway.common.cluster.ClusterNodeInfo.class);
+        config.addAnnotatedClass(com.l7tech.gateway.common.cluster.ClusterProperty.class);
+        config.addAnnotatedClass(com.l7tech.gateway.common.log.SinkConfiguration.class);
+        config.addAnnotatedClass(com.l7tech.gateway.common.schema.SchemaEntry.class);
+        config.addAnnotatedClass(com.l7tech.gateway.common.security.RevocationCheckPolicy.class);
+        config.addAnnotatedClass(com.l7tech.security.cert.TrustedCert.class);
+        config.addAnnotatedClass(com.l7tech.server.identity.cert.CertEntryRow.class);
+        config.addAnnotatedClass(com.l7tech.server.security.keystore.KeystoreFile.class);
+        config.addAnnotatedClass(com.l7tech.server.security.sharedkey.SharedKeyRecord.class);
+
+        config.addAnnotatedClass(com.l7tech.server.sla.CounterIDRecord.class);
     }
 
     public static void main(String[] args) {
@@ -22,6 +73,6 @@ public class SchemaUpdater {
     }
 
     private void doIt() {
-        new SchemaUpdate(config).execute(false, true);
+        new SchemaUpdate(config).execute(true, false);
     }
 }
