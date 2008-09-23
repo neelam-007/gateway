7e3f3e858e814ec907727f935dda0d44e49757d0
steve <steve@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2008-09-23 19:13:47 +0000

    SSG-2270: Override any configured XSLT output encoding as UTF-8.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@20356 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/policy/src/main/java/com/l7tech/xml/xslt/StylesheetCompiler.java
+++ modules/policy/src/main/java/com/l7tech/xml/xslt/StylesheetCompiler.java
@@ -2,6 +2,7 @@ package com.l7tech.xml.xslt;
 
 import com.l7tech.common.io.XmlUtil;
 import com.l7tech.util.ExceptionUtils;
+import com.l7tech.util.SyspropUtil;
 import com.l7tech.xml.TarariLoader;
 import com.l7tech.xml.tarari.GlobalTarariContext;
 import com.l7tech.xml.tarari.TarariCompiledStylesheet;
@@ -12,14 +13,18 @@ import org.w3c.dom.Document;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.w3c.dom.Element;
 import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import java.io.StringReader;
+import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
@@ -38,6 +43,7 @@ public class StylesheetCompiler {
     /* Namespace attributes used for xalan extension */
     private static final String XALAN_EXTENSION_NS1 = "http://xml.apache.org/xalan";
     private static final String XALAN_EXTENSION_NS2 = "http://xml.apache.org/xslt";
+    private static final String PROP_DISABLE_FORCE_ENCODING = "com.l7tech.xml.disableForceXsltEncoding"; 
 
     /**
      * Compile the specified XSLT stylesheet.
@@ -48,20 +54,33 @@ public class StylesheetCompiler {
      */
     public static CompiledStylesheet compileStylesheet(String xslt) throws ParseException {
         final GlobalTarariContext gtc = TarariLoader.getGlobalContext();
+        final Document cleanXslt = preprocessStylesheet( xslt );
         TarariCompiledStylesheet tarariStylesheet =
-                gtc == null ? null : gtc.compileStylesheet(xslt);
-        final Templates templates = compileSoftware(xslt);
+                gtc == null ? null : gtc.compileStylesheet(toString(cleanXslt));
+        final Templates templates = compileSoftware(cleanXslt);
         return new CompiledStylesheet(templates, getVariablesUsed(templates), tarariStylesheet);
     }
 
     /**
-     * @param thing  the stylesheet to parse
+     * Convert the DOM to string
+     */
+    private static String toString( final Document document ) throws ParseException {
+        try {
+            return XmlUtil.nodeToString( document );
+        } catch (IOException e) {
+            throw (ParseException)new ParseException(ExceptionUtils.getMessage(e), 0).initCause(e);
+        }
+    }
+
+    /**
+     * @param document the stylesheet to compile
      * @return successfully compiled stylesheet.  Never null
      * @throws java.text.ParseException if the stylesheet can't be parsed
      */
-    private static Templates compileSoftware(String thing) throws ParseException {
+    private static Templates compileSoftware( final Document document ) throws ParseException {
         // Prepare a software template
         try {
+            // Configure transformer
             TransformerFactory transfactory = TransformerFactory.newInstance();
             transfactory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
             transfactory.setURIResolver(XmlUtil.getSafeURIResolver());
@@ -71,14 +90,6 @@ public class StylesheetCompiler {
                 public void error(TransformerException exception) throws TransformerException { }
                 public void fatalError(TransformerException exception) throws TransformerException { fatals.add(exception); }
             });
-            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            dbf.setNamespaceAware( true );
-            DocumentBuilder db = dbf.newDocumentBuilder();
-            db.setEntityResolver( XmlUtil.getSafeEntityResolver() );
-            Document document = db.parse(new InputSource(new StringReader(thing)));
-
-            // remove any output elements that use xalan extensions from the stylesheet doc before compiling
-            removeOutputXalanExtensions(document);
 
             // create the XSL transform template
             Templates result = transfactory.newTemplates(new DOMSource(document));
@@ -89,10 +100,41 @@ public class StylesheetCompiler {
                 }
                 throw new ParseException("Unable to parse stylesheet: transformer factory returned null", 0);
             }
+            
             return result;
         } catch (TransformerConfigurationException e) {
             throw (ParseException)new ParseException(ExceptionUtils.getMessage(e), 0).initCause(e);
         } catch (Exception e) {
+            throw (ParseException)new ParseException(ExceptionUtils.getMessage(e), 0).initCause(e);
+        }
+    }
+
+    /**
+     * Preprocess the given XSLT and remove any XALAN specifics, and ensure output encoding is UTF-8.
+     */
+    private static Document preprocessStylesheet(String thing) throws ParseException {
+        // Prepare a software template
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            dbf.setNamespaceAware( true );
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            db.setEntityResolver( XmlUtil.getSafeEntityResolver() );
+            Document document = db.parse(new InputSource(new StringReader(thing)));
+
+            // remove any output elements that use xalan extensions from the stylesheet doc before compiling
+            removeOutputXalanExtensions(document);
+
+            // remove any output elements that use xalan extensions from the stylesheet doc before compiling
+            if ( !SyspropUtil.getBoolean(PROP_DISABLE_FORCE_ENCODING)) {
+                configureOutputEncoding(document);
+            }
+
+            return document;
+        } catch (IOException e) {
+            throw (ParseException)new ParseException(ExceptionUtils.getMessage(e), 0).initCause(e);
+        } catch (SAXException e) {
+            throw (ParseException)new ParseException(ExceptionUtils.getMessage(e), 0).initCause(e);
+        } catch (ParserConfigurationException e) {
             throw (ParseException)new ParseException(ExceptionUtils.getMessage(e), 0).initCause(e);
         }
     }
@@ -153,4 +195,29 @@ public class StylesheetCompiler {
             }
         }
     }
+
+    private static void configureOutputEncoding(final Document document) {
+
+        // find the output elements with charset and ensure utf-8
+        NodeList nodes = document.getDocumentElement().getChildNodes();
+        for (int i=0; i<nodes.getLength(); i++) {
+            Node node = nodes.item(i);
+            if ( node.getNodeType() == Node.ELEMENT_NODE &&
+                 NAMESPACE_XSLT.equals(node.getNamespaceURI()) &&
+                 "output".equals(node.getLocalName())) {
+
+                Element output = (Element) node;
+
+                // check the element
+                String outputEnc = output.getAttribute("encoding");
+                if ( output.hasAttribute("encoding") && !outputEnc.equalsIgnoreCase("UTF-8") ) {
+                    if ( logger.isLoggable(Level.FINE) ) {
+                        logger.fine("Modifying output encoding for XSLT from '"+outputEnc+"' to 'UTF-8'.");
+                    }
+                    output.setAttribute("encoding", "UTF-8");
+                }
+            }
+        }
+    }
+
 }
