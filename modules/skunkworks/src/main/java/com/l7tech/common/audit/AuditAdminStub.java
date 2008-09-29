b71519438514d385331c9a889cd902f9ef1aecd1
jphillips <jphillips@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2008-09-29 20:33:38 +0000

    Added new findHeaders() method to AuditAdminStub.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@20627 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/skunkworks/src/main/java/com/l7tech/common/audit/AuditAdminStub.java
+++ modules/skunkworks/src/main/java/com/l7tech/common/audit/AuditAdminStub.java
@@ -9,6 +9,7 @@ import com.l7tech.objectmodel.FindException;
 import com.l7tech.gateway.common.audit.AuditRecord;
 import com.l7tech.gateway.common.audit.AuditSearchCriteria;
 import com.l7tech.gateway.common.audit.AuditAdmin;
+import com.l7tech.gateway.common.audit.AuditRecordHeader;
 import com.l7tech.gateway.common.logging.SSGLogRecord;
 
 import java.util.Collection;
@@ -24,35 +25,39 @@ public class AuditAdminStub implements AuditAdmin {
     }
 
     public AuditRecord findByPrimaryKey(long oid) throws FindException {
-        throw new UnsupportedOperationException("Not supoprted in stub mode");
+        throw new UnsupportedOperationException("Not supported in stub mode");
     }
 
     public Collection<AuditRecord> find( AuditSearchCriteria criteria) throws FindException {
-        throw new UnsupportedOperationException("Not supoprted in stub mode");
+        throw new UnsupportedOperationException("Not supported in stub mode");
+    }
+
+    public Collection<AuditRecordHeader> findHeaders( AuditSearchCriteria criteria) throws FindException {
+        throw new UnsupportedOperationException("Not supported in stub mode");
     }
 
     public void deleteOldAuditRecords() {
-        throw new UnsupportedOperationException("Not supoprted in stub mode");
+        throw new UnsupportedOperationException("Not supported in stub mode");
     }
 
     public Collection<AuditRecord> findAuditRecords(String nodeid, Date startMsgDate, Date endMsgDate, int size) throws FindException {
-        throw new UnsupportedOperationException("Not supoprted in stub mode");
+        throw new UnsupportedOperationException("Not supported in stub mode");
     }
 
     public Date getLastAcknowledgedAuditDate() {
-        throw new UnsupportedOperationException("Not supoprted in stub mode");
+        throw new UnsupportedOperationException("Not supported in stub mode");
     }
 
     public Date markLastAcknowledgedAuditDate() {
-        throw new UnsupportedOperationException("Not supoprted in stub mode");
+        throw new UnsupportedOperationException("Not supported in stub mode");
     }
 
     public OpaqueId downloadAllAudits(long fromTime, long toTime, long[] serviceOids, int chunkSizeInBytes) {
-        throw new UnsupportedOperationException("Not supoprted in stub mode");
+        throw new UnsupportedOperationException("Not supported in stub mode");
     }
 
     public DownloadChunk downloadNextChunk(OpaqueId context) {
-        throw new UnsupportedOperationException("Not supoprted in stub mode");
+        throw new UnsupportedOperationException("Not supported in stub mode");
     }
 
     public SSGLogRecord[] getSystemLog(String nodeid, long startMsgNumber, long endMsgNumber, Date startMsgDate, Date endMsgDate, int size) {
