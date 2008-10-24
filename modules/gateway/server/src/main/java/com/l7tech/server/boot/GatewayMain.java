265a4bcd2707cf38323b9b15bf88962c74957b8e
steve <steve@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2008-10-24 21:46:58 +0000

    Modify gatewaymain so startup errors are logged.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@20909 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/server/src/main/java/com/l7tech/server/boot/GatewayMain.java
+++ modules/gateway/server/src/main/java/com/l7tech/server/boot/GatewayMain.java
@@ -2,6 +2,9 @@ package com.l7tech.server.boot;
 
 import com.l7tech.util.ExceptionUtils;
 
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
 /**
  * An entry point that starts the Gateway server process and runs it until it is shut down.
  * @noinspection UseOfSystemOutOrSystemErr,CallToSystemExit
@@ -12,6 +15,8 @@ public class GatewayMain {
             new GatewayBoot().runUntilShutdown();
             System.exit(0); // force exit even if there are non-daemon threads created by mistake (Bug #4384)
         } catch (Throwable e) {
+            // init logger here to avoid initializing logging framework early on startup
+            Logger.getLogger(GatewayMain.class.getName()).log(Level.WARNING, "Error starting server.", e);
             e.printStackTrace(System.err);
             System.err.println("\n\n\n**** Unable to start the server: " + ExceptionUtils.getMessage(e) + "\n\n\n");
             System.exit(77);
