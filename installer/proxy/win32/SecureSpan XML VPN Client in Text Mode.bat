143620c498e7d50f09de8fc2c14b9c6c574acb3f
steve <steve@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2008-10-29 22:04:55 +0000

    SSB-610: Update batch file for modified class name.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@20978 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- installer/proxy/win32/SecureSpan XML VPN Client in Text Mode.bat
+++ installer/proxy/win32/SecureSpan XML VPN Client in Text Mode.bat
@@ -1,1 +1,1 @@
-@jre\bin\java -Dfile.encoding=UTF-8  -Dsun.net.inetaddr.ttl=10 -Dnetworkaddress.cache.ttl=10 -Xms96M -Xmx96M -Xss256k -classpath Client.jar com.l7tech.proxy.Main
+@jre\bin\java -Dfile.encoding=UTF-8  -Dsun.net.inetaddr.ttl=10 -Dnetworkaddress.cache.ttl=10 -Xms96M -Xmx96M -Xss256k -classpath Client.jar com.l7tech.client.Main
