5c931131a838d23eb98d23f1ac7d6c9718c768fe
steve <steve@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2008-11-26 04:29:16 +0000

    Add reporting module.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@21385 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ modules/gateway/reporting/src/main/java/com/l7tech/gateway/standardreports/Codegen.java
@@ -0,0 +1,276 @@
+package com.l7tech.gateway.standardreports;
+
+import javax.tools.*;
+import java.io.*;
+import java.net.URI;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Logger;
+
+/**
+ * Utility for compilation of in-memory Java code stored as a String to an in-memory Class instance, using
+ * no temporary files and requiring no disk write access.
+ * <p/>
+ * Relies on Java 6 JavaCompiler support being available in the current environment.
+ * @noinspection ClassLoaderInstantiation
+ */
+public class Codegen {
+    protected static final Logger logger = Logger.getLogger(Codegen.class.getName());
+
+    private final String className;
+    private final String javaSource;
+    private DiagnosticCollector<JavaFileObject> diagnostics = null;
+    private List<MemoryJavaFile> extraCompilationUnits = new ArrayList<MemoryJavaFile>();
+    private List<MemoryJavaClass> extraClasses = new ArrayList<MemoryJavaClass>();
+
+    /**
+     * Create a Codegen that will compile the specified Java source, which is expected to contain the complete
+     * definition of the specified fully-qualified classname.
+     * @param className a fully qualified classname, ie "MyDefaultPackageClass", or "com.yoyodyne.Blah"
+     * @param javaSource the content of the .java file that would be used to represent a complete definition
+     *                   of className.  If the className isn't in the default package, must include a package declaration.
+     */
+    public Codegen(String className, String javaSource) {
+        this.className = className;
+        this.javaSource = javaSource;
+    }
+
+    public Iterable<? extends JavaFileObject> getCompilationUnits(MemoryJavaFile file) {
+        List<MemoryJavaFile> ret = new ArrayList<MemoryJavaFile>();
+        ret.add(file);
+        ret.addAll(extraCompilationUnits);
+        return ret;
+    }
+
+    public void addJavaFile(String classname, String javaSource) {
+        extraCompilationUnits.add(new MemoryJavaFile(classname, javaSource));
+    }
+
+    public void addClassFile(String classname, byte[] classSource) {
+        extraClasses.add(new MemoryJavaClass(classname, classSource));
+    }
+
+    /** Exception thrown if there is a problem compiling some dynamic code. */
+    public static class CompileException extends Exception {
+        public CompileException(Throwable cause) {
+            super(cause);
+        }
+
+        public CompileException(String message) {
+            super(message);
+        }
+    }
+
+    /**
+     * Compile the class and return a ready-to-use Class instance.
+     *
+     * @return the compiled Class.  Never null.
+     * @throws Codegen.CompileException if there is a problem compiling the class.  Call
+     *                                                        {@link #getDiagnostics()} for more information.
+     * @throws ClassNotFoundException if the input compiled successfully but did not produce the expected className
+     *                                among its output.
+     *
+     * @throws IllegalStateException if compile() has already been called on this instance
+     * @throws UnsupportedOperationException  if no JavaCompiler service is available in this environment
+     */
+    public byte[] compile() throws CompileException, ClassNotFoundException, IllegalStateException, UnsupportedOperationException {
+        if (diagnostics != null)
+            throw new IllegalStateException("Compilation has already been attempted");
+        diagnostics = new DiagnosticCollector<JavaFileObject>();
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        if (compiler == null)
+            throw new UnsupportedOperationException("No JavaCompiler available");
+        MemoryJavaFile file = new MemoryJavaFile(className, javaSource);
+        Iterable<? extends JavaFileObject> compilationUnits = getCompilationUnits(file);
+        ConcurrentHashMap<String, MemoryJavaClass> bytecodes = new ConcurrentHashMap<String, MemoryJavaClass>();
+        JavaFileManager fileManager = makeFileManager(compiler, diagnostics, bytecodes, extraClasses);
+        JavaCompiler.CompilationTask task = compiler.getTask(null, fileManager, diagnostics, null, null, compilationUnits);
+        boolean success = task.call();
+
+        if (!success)
+            throw new CompileException("Compilation failed; check diagnostics");
+
+        return bytecodes.values().iterator().next().getBytes();
+    }
+
+    /**
+     * Get the diagnostic information from the last compile attempt.
+     *
+     * @return the diagnostics.  Never null.
+     * @throws IllegalStateException if {@link #compile} has not yet been called.
+     */
+    public List<? extends Diagnostic> getDiagnostics() throws IllegalStateException {
+        if (diagnostics == null) throw new IllegalStateException("No compile attempted yet");
+        return diagnostics.getDiagnostics();
+    }
+
+    private Map<String,Set<JavaFileObject>> buildPackages( final Collection<MemoryJavaClass> javaClasses ) {
+        Map<String,Set<JavaFileObject>> classesByPackage = new HashMap<String,Set<JavaFileObject>>();
+
+        for ( MemoryJavaClass javaClass : javaClasses ) {
+            String className = javaClass.getClassName();
+            String packageName = "";
+            if ( className.indexOf( '.' ) > 0 ) {
+                packageName = className.substring( 0, className.lastIndexOf( '.' ) );
+            }
+            Set<JavaFileObject> packageClasses = classesByPackage.get( packageName );
+            if ( packageClasses == null ) {
+                packageClasses = new HashSet<JavaFileObject>();
+                classesByPackage.put( packageName, packageClasses );
+            }
+            packageClasses.add( javaClass );
+        }
+
+        return classesByPackage;
+    }
+
+    /**
+     * Create a JavaFileManager that will collect .class file output in the specified ConcurrentHashMap.
+     *
+     * @param compiler the compiler that will be using this
+     * @param diagnostics  diagnostics collector to pass to StandardFileManager
+     * @param bytecodeOutput  map in which to collect output .class files. Required
+     * @param javaClasses the input classes. Required.
+     * @return the new JavaFileManager.  Never null.
+     */
+    private JavaFileManager makeFileManager( final JavaCompiler compiler,
+                                             final DiagnosticCollector<JavaFileObject> diagnostics,
+                                             final ConcurrentHashMap<String, MemoryJavaClass> bytecodeOutput,
+                                             final Collection<MemoryJavaClass> javaClasses )
+    {
+        final StandardJavaFileManager sfm = compiler.getStandardFileManager(diagnostics, null, null);
+        final Map<String,Set<JavaFileObject>> javaClassesByPackage = buildPackages(javaClasses);
+        return new ForwardingJavaFileManager<StandardJavaFileManager>(sfm) {
+            @Override
+            public String inferBinaryName( Location location, JavaFileObject file ) {
+                logger.finest("inferBinaryName: location=" + location + "  file=" + file.getName());
+                String resourceName = file.getName();
+                String className = null;
+                if ( resourceName.endsWith( ".class" ) ) {
+                    className = resourceName.substring( 0, resourceName.length()-6 ).replace( '/', '.' );
+                    String packageName = className.substring( 0, className.lastIndexOf( '.' )+1);
+                    if ( packageName.startsWith( "." )) packageName = packageName.substring( 1 );
+                    if ( packageName.endsWith( "." )) packageName = packageName.substring( 0, packageName.length()-1 );
+                    if ( !javaClassesByPackage.keySet().contains(packageName) ) {
+                        className = null;
+                    }
+                }
+
+                if ( StandardLocation.CLASS_PATH == location && className != null) {
+                    return className;
+                } else {
+                    return super.inferBinaryName( location, file );
+                }
+            }
+
+            @Override
+            public Iterable<JavaFileObject> list( Location location, String packageName, Set<JavaFileObject.Kind> kinds, boolean recurse ) throws IOException {
+                logger.finest("list: location=" + location + "  packageName=" + packageName + "  kinds=" + kinds + " recurse="+recurse);
+                if ( StandardLocation.CLASS_PATH == location && javaClassesByPackage.keySet().contains( packageName ) ) {
+                    return javaClassesByPackage.get( packageName );
+                } else {
+                    return super.list( location, packageName, kinds, recurse );
+                }
+            }
+
+            @Override
+            public JavaFileObject getJavaFileForInput(Location location, String className, JavaFileObject.Kind kind) throws IOException {
+                logger.finest("getJavaFileForInput: location=" + location + "  className=" + className + "  kind=" + kind);
+                if (JavaFileObject.Kind.CLASS.equals(kind))
+                    return getMemoryJavaClass(className);
+                return super.getJavaFileForInput(location, className, kind);
+            }
+
+            @Override
+            public JavaFileObject getJavaFileForOutput(Location location, String className, JavaFileObject.Kind kind, FileObject sibling) throws IOException {
+                logger.finest("getJavaFileForOutput: location=" + location + "  className=" + className + "  kind=" + kind);
+                if (JavaFileObject.Kind.CLASS.equals(kind))
+                    return getMemoryJavaClass(className);
+                throw new IllegalArgumentException("Unable to output file of kind " + kind);
+            }
+
+            private MemoryJavaClass getMemoryJavaClass(String className) {
+                MemoryJavaClass c = bytecodeOutput.get(className);
+                if (c != null)
+                    return c;
+                c = new MemoryJavaClass(className);
+                MemoryJavaClass old = bytecodeOutput.putIfAbsent(className, c);
+                return old != null ? old : c;
+            }
+
+            @Override
+            public FileObject getFileForInput(Location location, String packageName, String relativeName) throws IOException {
+                logger.finest("getFileForInput: location=" + location + "  packageName=" + packageName + "  relativeName=" + relativeName);
+                return super.getFileForInput(location, packageName, relativeName);
+            }
+
+            @Override
+            public FileObject getFileForOutput(Location location, String packageName, String relativeName, FileObject sibling) throws IOException {
+                logger.finest("getFileForOutput: location=" + location + "  packageName=" + packageName + "  relativeName=" + relativeName);
+                throw new IllegalArgumentException("Unable to output file with no kind");
+            }
+        };
+    }
+
+    /**
+     * Represents a virtual .class file.  Supports writing as bytes or characters, and reading previously-written
+     * data back as bytes or characters.
+     */
+    private static class MemoryJavaClass extends SimpleJavaFileObject {
+        private final String className;
+        private ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        public MemoryJavaClass(String className, byte[] data) {
+            super(URI.create("string:///" + className.replace('.','/') + Kind.CLASS.extension),Kind.CLASS);
+            this.className = className;
+            if ( data != null ) {
+                baos.write( data, 0, data.length );
+            }
+        }
+
+        public MemoryJavaClass(String className) {
+            this(className, null);
+        }
+
+        @Override
+        public InputStream openInputStream() throws IOException {
+            return new ByteArrayInputStream(baos.toByteArray());
+        }
+
+        @Override
+        public OutputStream openOutputStream() throws IOException {
+            baos = new ByteArrayOutputStream();
+            return baos;
+        }
+
+        @Override
+        public CharSequence getCharContent(boolean ignoreEncodingErrors) throws IOException {
+            return baos.toString();
+        }
+
+        public String getClassName() {
+            return className;
+        }
+
+        public byte[] getBytes() {
+            return baos.toByteArray();
+        }
+    }
+
+    /**
+     * Represents a virtual .java file.  Supports reading as characters.
+     */
+    private static class MemoryJavaFile extends SimpleJavaFileObject {
+        final String source;
+
+        MemoryJavaFile(String name, String source) {
+            super(URI.create("string:///" + name.replace('.','/') + Kind.SOURCE.extension), Kind.SOURCE);
+            this.source = source;
+        }
+
+        @Override
+        public CharSequence getCharContent(boolean ignoreEncodingErrors) {
+            return source;
+        }
+    }
+}
---
+++ modules/gateway/reporting/src/main/java/com/l7tech/gateway/standardreports/GatewayJavaReportCompiler.java
@@ -0,0 +1,117 @@
+package com.l7tech.gateway.standardreports;
+
+import net.sf.jasperreports.engine.design.JRAbstractJavaCompiler;
+import net.sf.jasperreports.engine.design.JRCompilationSourceCode;
+import net.sf.jasperreports.engine.design.JRSourceCompileTask;
+import net.sf.jasperreports.engine.design.JRCompilationUnit;
+import net.sf.jasperreports.engine.design.JRClassGenerator;
+import net.sf.jasperreports.engine.JRException;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.Collections;
+import java.util.ArrayList;
+
+import com.l7tech.util.ExceptionUtils;
+import com.l7tech.util.ResourceUtils;
+import com.l7tech.common.io.IOUtils;
+
+import javax.tools.Diagnostic;
+
+/**
+ *
+ */
+public class GatewayJavaReportCompiler extends JRAbstractJavaCompiler {
+
+    //- PUBLIC
+
+    public GatewayJavaReportCompiler() {
+        super( false );
+    }
+
+    public static void registerClass( final Class compilationClass ) {
+        classList.add( compilationClass ); 
+    }
+
+    //- PROTECTED
+
+    @Override
+    protected void checkLanguage(final String language) throws JRException {
+        if ( !"java".equals(language) ) {
+            throw new JRException("Compiler does not support language '"+language+"'.");    
+        }
+    }
+
+    @Override
+    protected JRCompilationSourceCode generateSourceCode(final JRSourceCompileTask jrSourceCompileTask) throws JRException {
+        return JRClassGenerator.generateClass( jrSourceCompileTask );        
+    }
+
+    @Override
+    protected String compileUnits(final JRCompilationUnit[] jrCompilationUnits, final String classpath, final File file) throws JRException {
+        for ( JRCompilationUnit cu : jrCompilationUnits ) {
+            Codegen codegen = new Codegen( cu.getName(), cu.getSourceCode());
+            synchronized(classList) {
+                for ( Class compileClass : classList ) {
+                    codegen.addClassFile(compileClass.getName(), getClassSource(compileClass.getName()));
+                }
+            }
+            try {
+                byte[] reportClass = codegen.compile();
+                cu.setCompileData(reportClass);
+            } catch (Codegen.CompileException e) {
+                return makeCompileDetailMessage(codegen.getDiagnostics());
+            } catch (ClassNotFoundException e) {
+                throw new JRException("Compilation failed to produce the expected class: " + ExceptionUtils.getMessage(e), e);
+            }
+        }
+
+        return null; //success ...
+    }
+
+    @Override
+    protected String getSourceFileName(final String unitName) {
+        return unitName + ".java";
+    }
+
+    //- PRIVATE
+
+    private static final List<Class> classList = Collections.synchronizedList( new ArrayList<Class>() );
+
+    private static byte[] getClassSource( final String name ) {
+        final String resourcePath = name.replace( '.', '/' ) + ".class";
+        InputStream is = null;
+        try {
+            is = GatewayJavaReportCompiler.class.getClassLoader().getResourceAsStream(resourcePath);
+            if (is == null)
+                throw new IllegalArgumentException("Unable to find CompiledTemplate resource: " + resourcePath);
+            return IOUtils.slurpStream(is);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Unable to read resource: " + resourcePath + ": " + ExceptionUtils.getMessage(e), e);
+        } finally {
+            ResourceUtils.closeQuietly(is);
+        }
+    }
+
+    private static String makeCompileDetailMessage(List<? extends Diagnostic> diagnostics) {
+        StringBuilder sb = new StringBuilder("Compilation failed: ");
+
+        for (Diagnostic diagnostic : diagnostics) {
+            sb.append('[');
+            sb.append(" code=").append(diagnostic.getCode());
+            sb.append(" kind=").append(diagnostic.getKind());
+            sb.append(" position=").append(diagnostic.getPosition());
+            sb.append(" startPosition=").append(diagnostic.getStartPosition());
+            sb.append(" endPosition=").append(diagnostic.getEndPosition());
+            sb.append(" source=").append(diagnostic.getSource());
+            sb.append(" message=\"").append(diagnostic.getMessage(null)).append('\"');
+            sb.append("]\n");
+        }
+
+        return sb.toString();
+    }
+
+    
+}
