1adf0790bb5817d9a958c1214c117c830ec79cd7
steve <steve@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2008-11-28 22:46:44 +0000

    EntityCrud now has Secured annotations.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@21472 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/server/src/main/java/com/l7tech/server/EntityCrud.java
+++ modules/gateway/server/src/main/java/com/l7tech/server/EntityCrud.java
@@ -7,6 +7,13 @@ import com.l7tech.objectmodel.DeleteException;
 import com.l7tech.objectmodel.Entity;
 import com.l7tech.objectmodel.SaveException;
 import com.l7tech.objectmodel.UpdateException;
+import com.l7tech.objectmodel.EntityHeader;
+import com.l7tech.objectmodel.EntityHeaderSet;
+import com.l7tech.objectmodel.FindException;
+import com.l7tech.objectmodel.EntityType;
+import com.l7tech.gateway.common.security.rbac.Secured;
+import com.l7tech.gateway.common.security.rbac.OperationType;
+import com.l7tech.gateway.common.security.rbac.MethodStereotype;
 
 import java.io.Serializable;
 
@@ -16,7 +23,30 @@ import java.io.Serializable;
  * @author alex
  */
 public interface EntityCrud extends EntityFinder {
-    Serializable save(Entity e) throws SaveException;
-    void update(Entity e) throws UpdateException;
-    void delete(Entity e) throws DeleteException;
+
+    @Secured(stereotype=MethodStereotype.SAVE, operation=OperationType.CREATE)
+    Serializable save(Entity entity) throws SaveException;
+
+    @Secured(stereotype=MethodStereotype.UPDATE, operation=OperationType.UPDATE)
+    void update(Entity entity) throws UpdateException;
+
+    @Secured(stereotype=MethodStereotype.DELETE_ENTITY, operation=OperationType.DELETE)
+    void delete(Entity entity) throws DeleteException;
+
+    @Secured(stereotype=MethodStereotype.FIND_HEADERS, operation=OperationType.READ)
+    @Override
+    EntityHeaderSet<EntityHeader> findAll(Class<? extends Entity> entityClass) throws FindException;
+
+    @Secured(stereotype=MethodStereotype.FIND_BY_PRIMARY_KEY, operation=OperationType.READ)
+    @Override
+    Entity find(EntityHeader header) throws FindException;
+
+    @Secured(stereotype=MethodStereotype.FIND_BY_PRIMARY_KEY, operation=OperationType.READ)
+    @Override
+    <ET extends Entity> ET find(Class<ET> clazz, Serializable pk) throws FindException;
+
+    // TODO [steve] new method sterotype for FIND_HEADER
+    //@Secured(stereotype=MethodStereotype.FIND_HEADERS, operation=OperationType.READ, inferType=true)
+    @Override
+    EntityHeader findHeader(EntityType etype, Serializable pk) throws FindException;
 }
