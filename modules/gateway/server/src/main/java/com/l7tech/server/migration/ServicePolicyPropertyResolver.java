08d6c0f9c01a41e2607afd245fdf2469f5deda44
jbufu <jbufu@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2008-12-18 19:48:54 +0000

    g EM-170: When updating services with modified policies, the policy version from the target cluster must be preserved (along with the oid) for the update to succeed.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@21949 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/server/src/main/java/com/l7tech/server/migration/ServicePolicyPropertyResolver.java
+++ modules/gateway/server/src/main/java/com/l7tech/server/migration/ServicePolicyPropertyResolver.java
@@ -5,18 +5,26 @@ import com.l7tech.objectmodel.migration.PropertyResolverException;
 import com.l7tech.objectmodel.Entity;
 import com.l7tech.objectmodel.EntityHeader;
 import com.l7tech.gateway.common.service.PublishedService;
+import com.l7tech.policy.Policy;
 
 /**
+ * When applying mappings for Service entities, and if the Service needs to up updated (rather than copied),
+ * the OID and version of the policy from the mapped/target entity must be preserved.
+ *
  * @author jbufu
  */
 public class ServicePolicyPropertyResolver extends DefaultEntityPropertyResolver {
 
     public void applyMapping(Entity sourceEntity, String propName, Object targetValue, EntityHeader originalHeader) throws PropertyResolverException {
 
-        PublishedService service = (PublishedService) sourceEntity;
-        long policyOid = service.getPolicy().getOid();
+        Policy originalPolicy = ((PublishedService) sourceEntity).getPolicy();
+        long originalPolicyOid = originalPolicy.getOid();
+        int originalPolicyVersion = originalPolicy.getVersion();
+
         super.applyMapping(sourceEntity, propName, targetValue, originalHeader);
-        service.getPolicy().setOid(policyOid);
+
+        ((PublishedService) sourceEntity).getPolicy().setOid(originalPolicyOid);
+        ((PublishedService) sourceEntity).getPolicy().setVersion(originalPolicyVersion);
 
     }
 }
