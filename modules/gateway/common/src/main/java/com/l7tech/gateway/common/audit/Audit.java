d7aab80b1437a995c557e72209476221813a8526
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2009-01-13 02:25:47 +0000

    Added some javadoc
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@22086 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/common/src/main/java/com/l7tech/gateway/common/audit/Audit.java
+++ modules/gateway/common/src/main/java/com/l7tech/gateway/common/audit/Audit.java
@@ -8,9 +8,28 @@ package com.l7tech.gateway.common.audit;
  * @author alex
  */
 public interface Audit {
+    /**
+     * Audit and log the specified detail message, including parameters and an optional Throwable.
+     *
+     * @param msg  an AuditDetailMessage.  Required.
+     * @param params parameters to fill in if this detail message has parameters to fill in, or null
+     * @param e a Throwable to save with this detail message, or null
+     * @see AssertionMessages for a collection of AuditDetailMessage instances
+     */
     void logAndAudit(AuditDetailMessage msg, String[] params, Throwable e);
 
+    /**
+     * Audit and log the specified detail message, filled in with the specified parameters.
+     *
+     * @param msg  an AuditDetailMessage.  Required.
+     * @param params parameters to fill in if this detail message has parameters to fill in
+     */
     void logAndAudit(AuditDetailMessage msg, String... params);
 
+    /**
+     * Audit and log the specified detail message.
+     *
+     * @param msg  an AuditDetailMessage.  Required.
+     */
     void logAndAudit(AuditDetailMessage msg);
 }
