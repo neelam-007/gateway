4dad3e303c285940367c69144c93aba7f6bcf321
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2009-01-15 01:50:58 +0000

    Use strict equals() since other Map.Entry implementations won't have the same hashCode even if they are otherwise equal
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@22122 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/utility/src/main/java/com/l7tech/util/Pair.java
+++ modules/utility/src/main/java/com/l7tech/util/Pair.java
@@ -31,17 +31,15 @@ public class Pair<L,R> implements Serializable, Map.Entry<L, R> {
         throw new UnsupportedOperationException();
     }
 
-    /** @noinspection RedundantIfStatement*/
+    @SuppressWarnings({"RedundantIfStatement"})
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null) return false;
-        if (!(o instanceof Map.Entry)) {
-            return false;
-        }
-        Map.Entry entry = (Map.Entry) o;
+        if (o == null || getClass() != o.getClass()) return false;
 
-        if (left != null ? !left.equals(entry.getKey()) : entry.getKey() != null) return false;
-        if (right != null ? !right.equals(entry.getValue()) : entry.getValue() != null) return false;
+        Pair pair = (Pair) o;
+
+        if (left != null ? !left.equals(pair.left) : pair.left != null) return false;
+        if (right != null ? !right.equals(pair.right) : pair.right != null) return false;
 
         return true;
     }
