7bb823bd0fb80d1f5c4cb4da50630b3a0ac80659
alex <alex@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2009-01-30 03:34:43 +0000

    Monitoring object model progress
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@22267 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/processcontroller/src/main/java/com/l7tech/server/processcontroller/monitoring/PropertySamplingException.java
+++ modules/gateway/processcontroller/src/main/java/com/l7tech/server/processcontroller/monitoring/PropertySamplingException.java
@@ -7,33 +7,14 @@ import com.l7tech.server.management.config.monitoring.ComponentType;
 import com.l7tech.server.management.config.monitoring.PropertyTrigger;
 
 /**
- * Thrown to indicate that a property sample could not be obtained.  Note that the individual identity of the original
- * Trigger is not retained, because the same property may be sampled on behalf of more than one trigger.
+ * Thrown to indicate that a property sample could not be obtained.
  */
 public class PropertySamplingException extends Exception {
-    private final ComponentType componentType;
-    private final String componentId;
-    private final String propertyName;
-
-    public PropertySamplingException(final ComponentType componentType, final String componentId, final String propertyName, String message, Throwable cause) {
+    public PropertySamplingException(String message, Throwable cause) {
         super(message, cause);
-        this.componentType = componentType;
-        this.componentId = componentId;
-        this.propertyName = propertyName;
     }
 
-    /** The type of the component whose property was to be sampled */
-    public ComponentType getComponentType() {
-        return componentType;
-    }
-
-    /** The ID of the component whose property was to be sampled */
-    public String getComponentId() {
-        return componentId;
-    }
-
-    /** The name of the property that was to be sampled */
-    public String getPropertyName() {
-        return propertyName;
+    public PropertySamplingException(Throwable cause) {
+        super(cause);
     }
 }
