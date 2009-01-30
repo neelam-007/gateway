cdb10b836b885e1cf10952e1be55ec9e15c6d6bf
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2009-01-30 04:47:59 +0000

    nicer formatting
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@22270 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/processcontroller/src/test/java/com/l7tech/server/processcontroller/monitoring/CpuIdleSamplerTest.java
+++ modules/gateway/processcontroller/src/test/java/com/l7tech/server/processcontroller/monitoring/CpuIdleSamplerTest.java
@@ -13,8 +13,8 @@ public class CpuIdleSamplerTest {
                 "procs -----------memory---------- ---swap-- -----io---- --system-- -----cpu------\n" +
                 " r  b   swpd   free   buff  cache   si   so    bi    bo   in   cs us sy id wa st\n" +
                 " 1  0   6592  18536  36088 839708    0    0     8    80    1    1  1  1 97  1  0\n" +
-                " 1  0   6592  18536  36088 839708    0    0     8    80    1    1 15  8 76  1  0\n"
-        );
+                " 1  0   6592  18536  36088 839708    0    0     8    80    1    1 15  8 76  1  0\n");
+
         assertEquals(76, got);
     }
 }
