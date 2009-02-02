066084d73bbb26de6e95808885dd07487220a0d7
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2009-02-02 19:02:33 +0000

    Added javadoc.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@22285 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/processcontroller/src/main/java/com/l7tech/server/processcontroller/monitoring/NtpStatus.java
+++ modules/gateway/processcontroller/src/main/java/com/l7tech/server/processcontroller/monitoring/NtpStatus.java
@@ -1,12 +1,20 @@
 package com.l7tech.server.processcontroller.monitoring;
 
 /**
- *
+ * Represents a host's current NTP status.
  */
 public enum NtpStatus {
+    /** The ntpd is running and the clock is synchronized. */
     OK,
 
+    /** 
+     * The ntpd is running but the clock is not currently synchronized.
+     * This status is also reported if the ntpd is in the process of starting up.
+     */
     UNSYNCHRONIZED,
 
+    /**
+     * The ntpd cannot be contacted.  It may have failed or may not be running.
+     */
     UNKNOWN,
 }
