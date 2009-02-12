8585a1408d00258be3c43e534e64c9402af14933
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2009-02-12 23:06:50 +0000

    Switching to stricter equals check
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@22466 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/ems/src/main/java/com/l7tech/server/ems/enterprise/SsgNode.java
+++ modules/ems/src/main/java/com/l7tech/server/ems/enterprise/SsgNode.java
@@ -145,7 +145,7 @@ public class SsgNode extends NamedEntityImp implements JSON.Convertible, Compara
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof SsgNode)) return false;
+        if (o == null || getClass() != o.getClass()) return false;
         if (!super.equals(o)) return false;
 
         SsgNode ssgNode = (SsgNode) o;
