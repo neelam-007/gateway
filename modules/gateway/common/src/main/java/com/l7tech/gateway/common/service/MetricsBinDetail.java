298587f7a8c73bb87e37aed3e4de2955379f04a2
steve <steve@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2009-02-25 21:20:54 +0000

    SSG-2740: Implement metrics bin details equals/hashCode for business identity.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@22741 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/common/src/main/java/com/l7tech/gateway/common/service/MetricsBinDetail.java
+++ modules/gateway/common/src/main/java/com/l7tech/gateway/common/service/MetricsBinDetail.java
@@ -103,6 +103,29 @@ public class MetricsBinDetail extends PersistentEntityImp {
         this.sumBackendResponseTime = sumBackendResponseTime;
     }
 
+    @SuppressWarnings({"RedundantIfStatement"})
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        MetricsBinDetail that = (MetricsBinDetail) o;
+
+        if (mappingValuesOid != that.mappingValuesOid) return false;
+        if (metricsBinOid != that.metricsBinOid) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (int) (metricsBinOid ^ (metricsBinOid >>> 32));
+        result = 31 * result + (int) (mappingValuesOid ^ (mappingValuesOid >>> 32));
+        return result;
+    }
+
     //- PRIVATE
 
     private long metricsBinOid;
