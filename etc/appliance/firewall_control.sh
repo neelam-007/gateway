3f2be53052e8b2c60b8d301e36517e524a7185ea
megery <megery@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2009-02-27 22:41:14 +0000

    bug 6757: changed the firewall config and control script to use the same marker for the custom section as in 4.6.5. This allows the firewall on systems that have been customized and then upgraded to still work.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@22781 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- etc/appliance/firewall_control.sh
+++ etc/appliance/firewall_control.sh
@@ -24,7 +24,7 @@ if [ ! -z "${1}" ] ; then
     fi
 
     find "${RULES_FILES}" -type f -exec cat {} \; > "${RULES_HOME}/${RULES_EXT}"
-    sed "/# ADD CUSTOM ACCEPT RULES HERE/ r ${RULES_HOME}/${RULES_EXT}" "${RULES_SOURCE}" > "${RULES_HOME}/${RULES_ALL}" 
+    sed "/# ADD CUSTOM ALLOW RULES HERE/ r ${RULES_HOME}/${RULES_EXT}" "${RULES_SOURCE}" > "${RULES_HOME}/${RULES_ALL}" 
     
     cat "${RULES_HOME}/${RULES_ALL}" | "${RESTORE_COMMAND}"    
 fi
