a563f482033079d3ef349d6b9e7d610aefe92bed
steve <steve@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2009-03-08 23:39:00 +0000

    bug 6523: Added shutdown logging in ESM.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@22904 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/ems/src/main/java/com/l7tech/server/ems/EsmMain.java
+++ modules/ems/src/main/java/com/l7tech/server/ems/EsmMain.java
@@ -8,85 +8,125 @@ import com.l7tech.server.ServerConfig;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.logging.LogManager;
+import java.util.concurrent.CountDownLatch;
 import java.io.File;
 
 /**
  * Process entry point that starts the EMS server.
  */
 public class EsmMain {
-    private static final Logger logger = Logger.getLogger(EsmMain.class.getName());
 
     public static void main(String[] args) {
-        BuildInfo.setProduct( EsmMain.class.getPackage(), "Layer 7 Enterprise Service Manager" );
-        System.setProperty("org.apache.cxf.nofastinfoset", "true");
-
-        // initialize config location
-        System.setProperty(ServerConfig.PROPS_RESOURCE_PROPERTY, "/com/l7tech/server/ems/resources/emconfig.properties");
-        if ( new File("var").exists() ) {
-            System.setProperty(ServerConfig.PROPS_OVER_PATH_PROPERTY, "var/emconfig.properties");
+        if ( System.getProperty("java.util.logging.manager") == null ) {
+            System.setProperty("java.util.logging.manager", EsmLogManager.class.getName());
         }
+        try {
+            BuildInfo.setProduct( EsmMain.class.getPackage(), "Layer 7 Enterprise Service Manager" );
+            System.setProperty("org.apache.cxf.nofastinfoset", "true");
 
-        // configure logging if the logs directory is found, else leave console output
-        if ( new File("var/logs").exists() ) {
-            JdkLoggerConfigurator.configure("com.l7tech.server.ems", "com/l7tech/server/ems/resources/logging.properties", "etc/logging.properties", true, true);
-        }
-
-        logger.info("Starting " + BuildInfo.getLongBuildString());        
-
-        if ( SyspropUtil.getBoolean("com.l7tech.ems.development") ) {
-            System.setProperty("com.l7tech.ems.showExceptions", "true");
-            System.setProperty("com.l7tech.ems.enableHttpListener", "true");
-            ServerConfig serverConfig = ServerConfig.getInstance();
-            if ( serverConfig.getProperty("em.admin.user") == null ) {
-                logger.info("Creating default administration account on startup.");
-                serverConfig.putProperty("em.admin.user", "admin");
-                serverConfig.putProperty("em.admin.pass", "a41306e4b1b5858d3e3d705dd2e738e2");
+            // initialize config location
+            System.setProperty(ServerConfig.PROPS_RESOURCE_PROPERTY, "/com/l7tech/server/ems/resources/emconfig.properties");
+            if ( new File("var").exists() ) {
+                System.setProperty(ServerConfig.PROPS_OVER_PATH_PROPERTY, "var/emconfig.properties");
             }
-        }
 
-        // add shutdown handler
-        final Object shutdown = new Object();
-        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable(){
-            @Override
-            public void run() {
-                synchronized(shutdown) {
-                    shutdown.notify();
+            // configure logging if the logs directory is found, else leave console output
+            if ( new File("var/logs").exists() ) {
+                JdkLoggerConfigurator.configure("com.l7tech.server.ems", "com/l7tech/server/ems/resources/logging.properties", "etc/logging.properties", true, true);
+            }
+
+            // create Logger after initializing logging framework
+            Logger logger = Logger.getLogger(EsmMain.class.getName());
+            logger.info("Starting " + BuildInfo.getLongBuildString());
+
+            if ( SyspropUtil.getBoolean("com.l7tech.ems.development") ) {
+                System.setProperty("com.l7tech.ems.showExceptions", "true");
+                System.setProperty("com.l7tech.ems.enableHttpListener", "true");
+                ServerConfig serverConfig = ServerConfig.getInstance();
+                if ( serverConfig.getProperty("em.admin.user") == null ) {
+                    logger.info("Creating default administration account on startup.");
+                    serverConfig.putProperty("em.admin.user", "admin");
+                    serverConfig.putProperty("em.admin.pass", "a41306e4b1b5858d3e3d705dd2e738e2");
                 }
             }
-        }));
 
-        final Esm ems = new Esm();
-        try {
-            ems.start();
-        } catch (Throwable t) {
-            final String msg = "Unable to start EMS: " + ExceptionUtils.getMessage(t);
-            logger.log(Level.SEVERE, msg, t);
-            System.err.println(msg);
-            t.printStackTrace(System.err);
-            System.exit(1);
-        }
+            // add shutdown handler
+            final CountDownLatch shutdown = new CountDownLatch(1);
+            final CountDownLatch exit = new CountDownLatch(1);
+            Runtime.getRuntime().addShutdownHook(new Thread(new Runnable(){
+                @Override
+                public void run() {
+                    synchronized(shutdown) {
+                        shutdown.countDown();
+                        try {
+                            exit.await(); // wait for shutdown to complete
+                        } catch(InterruptedException ie) {
+                            // exit
+                        }
+                    }
+                }
+            }));
 
-        try {
-            waitForShutdown(shutdown);
-            logger.info("Shutting down.");
-            ems.stop();
-        } catch (Exception t) {
-            final String msg = "Exception while waiting for EMS shutdown: " + ExceptionUtils.getMessage(t);
-            logger.log(Level.SEVERE, msg, t);
-            System.err.println(msg);
-            t.printStackTrace(System.err);
-            System.exit(2);
-        }
-    }
-
-    private static void waitForShutdown(final Object shutdown) throws Exception {
-        // Wait forever until server is shut down
-        synchronized (shutdown) {
+            final Esm ems = new Esm();
             try {
-                shutdown.wait();
-            } catch (InterruptedException e) {
-                throw new Exception("thread interrupted");
+                ems.start();
+            } catch (Throwable t) {
+                final String msg = "Unable to start EMS: " + ExceptionUtils.getMessage(t);
+                logger.log(Level.SEVERE, msg, t);
+                System.err.println(msg);
+                t.printStackTrace(System.err);
+                System.exit(1);
+            }
+
+            try {
+                waitForShutdown(shutdown);
+                logger.info("Shutting down.");
+                ems.stop();
+                logger.info("Main thread exiting.");
+            } catch (Exception t) {
+                final String msg = "Exception while waiting for EMS shutdown: " + ExceptionUtils.getMessage(t);
+                logger.log(Level.SEVERE, msg, t);
+                System.err.println(msg);
+                t.printStackTrace(System.err);
+                System.exit(2);
+            } finally {
+                exit.countDown();
+            }
+        } finally {
+            LogManager manager = LogManager.getLogManager();
+            if ( manager instanceof EsmLogManager ) {
+                ((EsmLogManager)manager).resetLogs();    
             }
         }
     }
+
+    private static void waitForShutdown(final CountDownLatch shutdown) throws Exception {
+        // Wait forever until server is shut down
+        try {
+            shutdown.await();
+        } catch (InterruptedException e) {
+            throw new Exception("thread interrupted");
+        }
+    }
+
+    /**
+     * This prevents JDK logging shutdown when the JUL shutdown hook is invoked.
+     *
+     * <p>The ESM will reset the underlying manager when shutdown of components
+     * is completed.</p>
+     *
+     * Perhaps inspired by JBossJDKLogManager.
+     */
+    public static final class EsmLogManager extends LogManager implements JdkLoggerConfigurator.ResettableLogManager {
+        @Override
+        public void reset() throws SecurityException {
+        }
+
+        @Override
+        public void resetLogs() throws SecurityException {
+            super.reset();
+        }
+    }
+
 }
