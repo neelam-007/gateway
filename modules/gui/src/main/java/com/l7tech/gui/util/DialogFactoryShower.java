c05412aba2be9e9fa3c004f36dfb00ead521f7ba
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2009-03-18 18:05:59 +0000

    Added some early null checks.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@23066 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gui/src/main/java/com/l7tech/gui/util/DialogFactoryShower.java
+++ modules/gui/src/main/java/com/l7tech/gui/util/DialogFactoryShower.java
@@ -6,14 +6,17 @@ import javax.swing.*;
 
 /** A convenient implementation of {@link com.l7tech.gui.util.DialogShower}. */
 public class DialogFactoryShower implements DialogShower {
+    private final Functions.Nullary<JDialog> dialogFactory;
     private JDialog dialog;
-    private Functions.Nullary<JDialog> dialogFactory;
 
     public DialogFactoryShower(JDialog dialog) {
+        if (dialog == null) throw new NullPointerException("dialog");
+        this.dialogFactory = null;
         this.dialog = dialog;
     }
 
     public DialogFactoryShower(Functions.Nullary<JDialog> dialogFactory) {
+        if (dialogFactory == null) throw new NullPointerException("dialogFactory");
         this.dialogFactory = dialogFactory;
     }
 
