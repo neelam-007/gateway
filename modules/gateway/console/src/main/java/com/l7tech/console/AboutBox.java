54f53a8a4adfebad910a4e6f46a91ab067ec4bcf
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2009-04-07 17:01:05 +0000

    SSM-2182: Removed "home page" text from SSM About box.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@23294 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/console/src/main/java/com/l7tech/console/AboutBox.java
+++ modules/gateway/console/src/main/java/com/l7tech/console/AboutBox.java
@@ -31,7 +31,7 @@ public class AboutBox extends JDialog implements ActionListener {
     JLabel logoLabel =
       new JLabel(new ImageIcon(cl.getResource(MainWindow.RESOURCE_PATH + "/" + LOGO_IMAGE)), JLabel.CENTER);
     JLabel resLabel = new JLabel("", JLabel.CENTER);
-    JLabel urlLabel = new JLabel("Home Page -- http://www.layer7tech.com", JLabel.CENTER);
+    JLabel urlLabel = new JLabel("http://www.layer7tech.com", JLabel.CENTER);
 
     JPanel infoPanel = new JPanel(new GridBagLayout());
     String product = "SecureSpan Manager";
