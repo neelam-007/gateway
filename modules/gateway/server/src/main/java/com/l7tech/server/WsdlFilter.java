bba7767e5792815b58d1d4d1a63bf7d093f102fd
steve <steve@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2009-05-07 03:30:37 +0000

    g FR-153: Add support for access of gateway WSDL documents using "?wsdl"
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@23481 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/server/src/main/java/com/l7tech/server/WsdlFilter.java
+++ modules/gateway/server/src/main/java/com/l7tech/server/WsdlFilter.java
@@ -3,6 +3,7 @@ package com.l7tech.server;
 import java.io.IOException;
 import java.util.logging.Logger;
 import java.util.logging.Level;
+import java.util.*;
 import java.text.MessageFormat;
 import javax.servlet.Filter;
 import javax.servlet.ServletRequest;
@@ -26,42 +27,58 @@ import com.l7tech.common.http.HttpConstants;
  */
 public class WsdlFilter implements Filter {
 
+    //- PUBLIC
+
     /**
      * Get ServiceManager from the application context.
      *
      * @param filterConfig The configuration for the filter
      * @throws ServletException if an error occurs
      */
-    public void init(FilterConfig filterConfig) throws ServletException {
+    @Override
+    public void init( final FilterConfig filterConfig ) throws ServletException {
         // config
+        String pprefixes = filterConfig.getInitParameter(PARAM_PASSTHROUGH_PREFIXES);
+        if ( pprefixes == null || pprefixes.length() == 0 ) pprefixes = DEFAULT_PASSTHROUGH_PREFIXES;
+
         String forwardUriVal = filterConfig.getInitParameter(PARAM_FORWARD_URI);
         if(forwardUriVal==null) {
             logger.config("Using default wsdl forward uri.");
             forwardUriVal = DEFAULT_FORWARD_URI;
         }
+
+        serverConfig = (ServerConfig) filterConfig.getServletContext().getAttribute("serverConfig");
+        passthroughPrefixes = pprefixes.split(",\\s*");
         wsdlForwardUri = forwardUriVal;
-        logger.config("Wsdl forward uri is '"+wsdlForwardUri+"'.");
+
+        logger.config("Passthrough prefixes " + Arrays.asList(passthroughPrefixes));
+        logger.config("Wsdl forward uri is '" + wsdlForwardUri + "'.");
     }
 
     /**
      * Nothing to destroy.
      */
+    @Override
     public void destroy() {
     }
 
     /**
      * If the request is a GET and the query string is "wsdl" then attempt to resolve the url to a service.
      */
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        if(servletRequest instanceof HttpServletRequest) {
+    @Override
+    public void doFilter( final ServletRequest servletRequest,
+                          final ServletResponse servletResponse,
+                          final FilterChain filterChain) throws IOException, ServletException {
+        if(isEnabled() && servletRequest instanceof HttpServletRequest) {
             HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-            if(HttpConstants.METHOD_GET.equals(httpServletRequest.getMethod())) {
+            if(HttpConstants.METHOD_GET.equals(httpServletRequest.getMethod()) &&
+               !isPassThrough(httpServletRequest.getRequestURI() )) {
                 String qs = httpServletRequest.getQueryString();
                 if(qs!=null && QUERYSTRING_WSDL.equalsIgnoreCase(qs)) {
                     // Initialize processing context
                     String forwardUri = null;
                     try {
-                        forwardUri = MessageFormat.format(wsdlForwardUri, new Object[]{httpServletRequest.getRequestURI()});
+                        forwardUri = MessageFormat.format(wsdlForwardUri, httpServletRequest.getRequestURI());
                     }
                     catch(IllegalArgumentException iae) {
                         logger.log(Level.WARNING, "Error generating wsdl forwarding uri.", iae);
@@ -91,9 +108,31 @@ public class WsdlFilter implements Filter {
 
     private static final Logger logger = Logger.getLogger(WsdlFilter.class.getName());
     private static final String QUERYSTRING_WSDL = "wsdl";
+    public static final String PARAM_PASSTHROUGH_PREFIXES = "passthroughPrefixes";
+    public static final String DEFAULT_PASSTHROUGH_PREFIXES = "/ssg/";
     private static final String PARAM_FORWARD_URI = "wsdl-forward-uri";
     private static final String DEFAULT_FORWARD_URI = "/ssg/wsdl?uri=";
 
+    private ServerConfig serverConfig;
     private String wsdlForwardUri;
+    private String[] passthroughPrefixes;  
 
+    private boolean isEnabled() {
+        return serverConfig.getBooleanPropertyCached("wsdlQuery", false, 60000);
+    }
+
+    private boolean isPassThrough( final String uri ) {
+        boolean passThrough = false;
+
+        if ( uri != null ) {
+            for ( String prefix : passthroughPrefixes  ) {
+                if ( uri.startsWith( prefix ) ) {
+                    passThrough = true;
+                    break;
+                }
+            }
+        }
+
+        return passThrough;
+    }
 }
