af5b2618b867b795f3c2792bd2a126d32d682465
steve <steve@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2009-06-27 00:57:42 +0000

    SSG-3233: SSM policy validation warning updated for (non tag) incorrect target identity.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@23790 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/policy/src/main/java/com/l7tech/policy/validator/PathValidator.java
+++ modules/policy/src/main/java/com/l7tech/policy/validator/PathValidator.java
@@ -523,8 +523,14 @@ class PathValidator {
                 result.addWarning(new PolicyValidatorResult.Warning(a, assertionPath,
                   "Assertion targets an identity, but signing is not required. The \"Target Identity\" should be cleared, or the assertion should require a signature.", null));
             } else if ( !ArrayUtils.contains( getIdentityTargetOptions(a.getPath()[0], a, target), ((IdentityTargetable)a).getIdentityTarget() ) ) {
-                result.addWarning(new PolicyValidatorResult.Warning(a, assertionPath,
-                  "Assertion targets an invalid identity, so will always fail. The \"Target Identity\" should be corrected.", null));
+                IdentityTarget identityTarget = ((IdentityTargetable)a).getIdentityTarget();
+                if ( identityTarget.getTargetIdentityType() == IdentityTarget.TargetIdentityType.TAG ) {
+                    result.addWarning(new PolicyValidatorResult.Warning(a, assertionPath,
+                      "Assertion targets an invalid identity, so will always fail. The \"Target Identity\" should be corrected.", null));
+                } else {
+                    result.addWarning(new PolicyValidatorResult.Warning(a, assertionPath,
+                      "Assertion targets an identity not explicitly required by the policy, so may fail. The \"Target Identity\" should be corrected.", null));
+                }
             }
         }
 
