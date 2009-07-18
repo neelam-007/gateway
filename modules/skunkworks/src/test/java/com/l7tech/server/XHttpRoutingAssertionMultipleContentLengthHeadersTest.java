d1babf0a7d7379379fb09bbd10b30ae105d758b0
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2009-07-18 04:55:56 +0000

    Renaming test so it runs after PolicyProcessingPerformanceTest.  If it runs first, it screws up the initialization of the other test.  (Thanks to Sjones for figuring this out for me!)
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@24054 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/skunkworks/src/test/java/com/l7tech/server/XHttpRoutingAssertionMultipleContentLengthHeadersTest.java
+++ modules/skunkworks/src/test/java/com/l7tech/server/XHttpRoutingAssertionMultipleContentLengthHeadersTest.java
@@ -20,7 +20,7 @@ import static org.junit.Assert.*;
 /**
  *
  */
-public class HttpRoutingAssertionMultipleContentLengthHeadersTest {
+public class XHttpRoutingAssertionMultipleContentLengthHeadersTest {
     private static TestingHttpClientFactory testingHttpClientFactory;
 
     @BeforeClass
