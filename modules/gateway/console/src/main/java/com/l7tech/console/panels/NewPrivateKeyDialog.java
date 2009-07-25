c1d08f32fd3795c2ad90a01f09db9cdde19f51a0
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2009-07-25 05:07:50 +0000

    bug 7544: Added the ability to see additional elliptic cuve names in the New Private Key dialog if the SSM is running with the undocumented system property "com.l7tech.console.keyStore.showAdditionalNamedCurves" set to "true".
    This behavior is not sufficient to fix Bug #7544 and is disabled by default.
    
    (Among the issues with enabling this system property: There is no indication given in the UI that (for example) secp256r1, P-256, and prime256v1 all refer to the same curve, so a user may be surprised if they create a "P-256" curve and see the resulting cert shows "secp256r1" instead.  The UI does not know about, and can't help you with, a provider such as Luna that recognizes some curve name synonyms but not others -- a user will have to try all the synonyms to find the one that is recognized.  The list is long and disorganized.  The long list duplicates secp384r1.)
    
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@24147 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/console/src/main/java/com/l7tech/console/panels/NewPrivateKeyDialog.java
+++ modules/gateway/console/src/main/java/com/l7tech/console/panels/NewPrivateKeyDialog.java
@@ -10,6 +10,7 @@ import com.l7tech.gui.util.Utilities;
 import com.l7tech.gui.widgets.PleaseWaitDialog;
 import com.l7tech.gui.widgets.SquigglyTextField;
 import com.l7tech.util.ExceptionUtils;
+import com.l7tech.util.SyspropUtil;
 
 import javax.security.auth.x500.X500Principal;
 import javax.swing.*;
@@ -37,6 +38,8 @@ import java.util.logging.Logger;
  */
 public class  NewPrivateKeyDialog extends JDialog {
     protected static final Logger logger = Logger.getLogger(NewPrivateKeyDialog.class.getName());
+
+    private static final String PROP_SHOW_ADDITIONAL_NAMED_CURVES = "com.l7tech.console.keyStore.showAdditionalNamedCurves";
 
     private static final String TITLE = "Create Private Key";
     private static final String DEFAULT_EXPIRY = Long.toString(365 * 5);
@@ -237,6 +240,53 @@ public class  NewPrivateKeyDialog extends JDialog {
                 rsasize(2048, 60 * 20, 17),
                 curvename("secp384r1")
         ));
+
+        if (SyspropUtil.getBoolean(PROP_SHOW_ADDITIONAL_NAMED_CURVES, false)) {
+            types.addAll(Arrays.asList(
+                curvename("sect163k1"),
+                curvename("sect163r1"),
+                curvename("sect163r2"),
+                curvename("sect193r1"),
+                curvename("sect193r2"),
+                curvename("sect233k1"),
+                curvename("sect233r1"),
+                curvename("sect239k1"),
+                curvename("sect283k1"),
+                curvename("sect283r1"),
+                curvename("sect409k1"),
+                curvename("sect409r1"),
+                curvename("sect571k1"),
+                curvename("sect571r1"),
+                curvename("secp160k1"),
+                curvename("secp160r1"),
+                curvename("secp160r2"),
+                curvename("secp192k1"),
+                curvename("secp192r1"),
+                curvename("secp224k1"),
+                curvename("secp224r1"),
+                curvename("secp256k1"),
+                curvename("secp256r1"),
+                curvename("secp384r1"),
+                curvename("secp521r1"),
+                curvename("prime192v1"),
+                curvename("prime256v1"),
+                curvename("K-163"),
+                curvename("B-163"),
+                curvename("K-233"),
+                curvename("B-233"),
+                curvename("K-283"),
+                curvename("B-283"),
+                curvename("K-409"),
+                curvename("B-409"),
+                curvename("K-571"),
+                curvename("B-571"),
+                curvename("P-192"),
+                curvename("P-224"),
+                curvename("P-256"),
+                curvename("P-384"),
+                curvename("P-521")
+            ));
+        }
 
         if (keystoreInfo.getKeyStoreType() != null && keystoreInfo.getKeyStoreType().toLowerCase().contains("pkcs11"))
             usingHsm = true;
