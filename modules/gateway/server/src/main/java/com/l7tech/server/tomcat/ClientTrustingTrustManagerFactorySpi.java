c847a35c279a841503351fc4ed313e2c70fafe1c
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2009-08-24 19:37:07 +0000

    bug 7367: Merging Franco's fix to trunk.  The cluster property has been renamed to "io.httpsAcceptedClientCa" but in all other ways the behavior is substantially as it was in Franco's patch.
    This cluster property is hidden in the SSM UI.  However, if you manually type it in, the SSM will display the description, confirming that it recognizes the cluster property and showing you how to use it.
    
    Note that if there is more than one PEM certificate the values must be comma delimited.  This differs from the usual convention for storing a bunch of PEM certs in a single text file (letting the PEM markers serve as their own delimiters).
    
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@24322 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/server/src/main/java/com/l7tech/server/tomcat/ClientTrustingTrustManagerFactorySpi.java
+++ modules/gateway/server/src/main/java/com/l7tech/server/tomcat/ClientTrustingTrustManagerFactorySpi.java
@@ -1,18 +1,26 @@
 package com.l7tech.server.tomcat;
 
+import com.l7tech.common.io.CertUtils;
 import com.l7tech.security.cert.KeyUsageActivity;
 import com.l7tech.security.cert.KeyUsageChecker;
+import com.l7tech.server.ServerConfig;
+import com.l7tech.util.ArrayUtils;
 import com.l7tech.util.ExceptionUtils;
 
 import javax.net.ssl.ManagerFactoryParameters;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactorySpi;
 import javax.net.ssl.X509TrustManager;
+import java.io.IOException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+import java.security.cert.CertificateEncodingException;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -31,13 +39,16 @@ public class ClientTrustingTrustManagerFactorySpi extends TrustManagerFactorySpi
 
     //- PROTECTED
 
+    @Override
     protected TrustManager[] engineGetTrustManagers() {
         return new TrustManager[]{new ClientTrustingTrustManager()};
     }
 
+    @Override
     protected void engineInit(KeyStore keyStore) throws KeyStoreException {
     }
 
+    @Override
     protected void engineInit(ManagerFactoryParameters managerFactoryParameters) throws InvalidAlgorithmParameterException {
     }
 
@@ -49,9 +60,60 @@ public class ClientTrustingTrustManagerFactorySpi extends TrustManagerFactorySpi
      *
      */
     private static class ClientTrustingTrustManager implements X509TrustManager {
+        private final X509Certificate[] outputTrustedCAs;
+
         private ClientTrustingTrustManager() {
+            outputTrustedCAs = loadConfiguredIssuerCerts();
         }
 
+        static X509Certificate[] loadConfiguredIssuerCerts() {
+            // set the trusted client CAs
+            X509Certificate[] ret = new X509Certificate[0];
+
+            // try to get setting in cluster properties
+            ServerConfig configs = ServerConfig.getInstance();
+            if (configs == null) {
+                logger.warning("cannot get server config");
+                return ret;
+            }
+
+            String sslAcceptedClientCA = configs.getProperty("ioHttpsAcceptedClientCa");
+            if (sslAcceptedClientCA == null || sslAcceptedClientCA.length() <= 0) {
+                logger.info("io.httpsAcceptedClientCa not set or empty");
+                return ret;
+            }
+
+            Set<X509Certificate> outputCertsInList = new TreeSet<X509Certificate>(new EncodedCertificateComparator());
+
+            String[] acceptedCAsItems = sslAcceptedClientCA.split(",");
+            for (String item: acceptedCAsItems) {
+                if (item != null && item.length() > 0) {
+                    try {
+                        X509Certificate acert = CertUtils.decodeFromPEM(item);
+                        if (outputCertsInList.contains(acert)) {
+                            logger.warning("duplicated cert " + acert.getSubjectDN());
+                        } else {
+                            outputCertsInList.add(acert);
+                        }
+                    } catch (IOException e) {
+                        logger.log(Level.WARNING, "could not parse pem cert from io.httpsAcceptedClientCa: " + ExceptionUtils.getMessage(e), ExceptionUtils.getDebugException(e));
+                    } catch (CertificateException e) {
+                        logger.log(Level.WARNING, "could not parse pem cert from io.httpsAcceptedClientCa: " + ExceptionUtils.getMessage(e), ExceptionUtils.getDebugException(e));
+                    } catch (RuntimeException e) {
+                        CertificateException ce = ExceptionUtils.getCauseIfCausedBy(e, CertificateException.class);
+                        if (ce != null)
+                            logger.log(Level.WARNING, "could not encode pem cert from io.httpsAcceptedClientCa: " + ExceptionUtils.getMessage(e), ExceptionUtils.getDebugException(e));
+                        else
+                            throw e;
+                    }
+                }
+            }
+            logger.info("Set listener with " + outputCertsInList.size() + " trusted ca certs");
+            ret = outputCertsInList.toArray(new X509Certificate[outputCertsInList.size()]);
+            return ret;
+        }
+
+        @Override
         public void checkClientTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
             try {
                 if (x509Certificates != null && x509Certificates.length > 0)
@@ -62,29 +124,28 @@ public class ClientTrustingTrustManagerFactorySpi extends TrustManagerFactorySpi
             }
         }
 
+        @Override
         public void checkServerTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
             throw new CertificateException("This TrustManager only supports client checks.");
         }
 
+        @Override
         public X509Certificate[] getAcceptedIssuers() {
-            return new X509Certificate[0];
+            return outputTrustedCAs;
         }
+    }
 
-        public boolean equals(Object obj) {
-            boolean equal = false;
-
-            if (obj == this) {
-                equal = true;
+    /**
+     * An X509Certificate comparator that compares by the certificate encoded form.
+     */
+    static class EncodedCertificateComparator implements Comparator<X509Certificate> {
+        @Override
+            public int compare(X509Certificate a, X509Certificate b) {
+            try {
+                return ArrayUtils.compareArrays(a.getEncoded(), b.getEncoded());
+            } catch (CertificateEncodingException e) {
+                throw new RuntimeException(e);
             }
-            else if (obj instanceof ClientTrustingTrustManager) {
-                equal = true;
-            }
-
-            return equal;
-        }
-
-        public int hashCode() {
-            return 17 * ClientTrustingTrustManager.class.hashCode();
         }
     }
 }
