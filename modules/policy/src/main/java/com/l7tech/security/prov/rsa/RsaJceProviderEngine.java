b7a998889e404ece07d76516d47d03d362fe76a4
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2009-10-07 02:19:49 +0000

    SSG-3433: When the RSA library is put into FIPS mode, and SunJSSE is registered as a Provider, we will reregister SunJSSE in FIPS mode using the RSA library as its FIPS provider if the system property "com.l7tech.security.sunjsse.fips.enabled" is set to "true".  This system property currently defaults to false since the Gateway will not start if SunJSSE is in FIPS mode until we change all uses of SSLContext.getInstance("SSL") to ask for "TLS" instead, and remove all uses of custom KeyManager implementations.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@24579 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/policy/src/main/java/com/l7tech/security/prov/rsa/RsaJceProviderEngine.java
+++ modules/policy/src/main/java/com/l7tech/security/prov/rsa/RsaJceProviderEngine.java
@@ -10,6 +10,7 @@ import com.rsa.jsafe.provider.JsafeJCE;
 import java.security.Provider;
 import java.security.Security;
 import java.util.logging.Logger;
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * A JceProvider engine for RSA Crypto-J 4.0 FIPS 140.
@@ -18,6 +19,7 @@ public class RsaJceProviderEngine extends JceProvider {
     private static final Logger logger = Logger.getLogger(RsaJceProviderEngine.class.getName());
 
     private static final boolean FIPS = SyspropUtil.getBoolean("com.l7tech.security.fips.enabled", false);
+    private static final boolean SUNJSSE_FIPS = SyspropUtil.getBoolean("com.l7tech.security.sunjsse.fips.enabled", false);
 
     private static final Provider PROVIDER;
 
@@ -32,6 +34,20 @@ public class RsaJceProviderEngine extends JceProvider {
                     logger.severe("RSA library failed to initialize in FIPS 140 mode");
                     throw new RuntimeException("RSA JCE Provider is supposed to be in FIPS mode but is not");
                 }
+
+                if (SUNJSSE_FIPS) {
+                    // If the SunJSSE provider is being used for SSL, replace it with the FIPS version
+                    Provider sunjsse = Security.getProvider("SunJSSE");
+                    if (sunjsse == null) {
+                        logger.info("SunJSSE provider is not installed -- unable to put SunJSSE into FIPS mode (continuing anyway)");
+                        // Continue anyway, in case we are running with a non-Sun or specially-configured JVM
+                    } else {
+                        Provider fipsJsse = (Provider)Class.forName("com.sun.net.ssl.internal.ssl.Provider").getConstructor(Provider.class).newInstance(PROVIDER);
+                        Security.removeProvider("SunJSSE");
+                        Security.addProvider(fipsJsse);
+                        logger.info("Reregistered SunJSSE in FIPS mode using RSA library as provider");
+                    }
+                }
             } else {
                 logger.info("Initializing RSA library in non-FIPS 140 mode");
                 if (CryptoJ.isFIPS140Compliant())
@@ -41,6 +57,18 @@ public class RsaJceProviderEngine extends JceProvider {
             }
         } catch (JSAFE_InvalidUseException e) {
             throw new RuntimeException("Unable to set FIPS 140 mode (with SSL and ECC): " + ExceptionUtils.getMessage(e), e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException("Unable to put SunJSSE SSL library into FIPS mode: " + ExceptionUtils.getMessage(e), e);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException("Unable to put SunJSSE SSL library into FIPS mode: " + ExceptionUtils.getMessage(e), e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException("Unable to put SunJSSE SSL library into FIPS mode: " + ExceptionUtils.getMessage(e), e);
+        } catch (InstantiationException e) {
+            throw new RuntimeException("Unable to put SunJSSE SSL library into FIPS mode: " + ExceptionUtils.getMessage(e), e);
+        } catch (ClassNotFoundException e) {
+            // This might happen semi-legitimately if a later Sun JDK removes/renames/changes the com.sun.net.ssl.internal.ssl.Provider class,
+            // or if we are running on a non-Sun JDK that nevertheless has a Provider installed named "SunJSSE"
+            throw new RuntimeException("Unable to put SunJSSE SSL library into FIPS mode: " + ExceptionUtils.getMessage(e), e);
         }
     }
 
