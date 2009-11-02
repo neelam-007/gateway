1b305f60ec696385d27d275840fe1e56412c77fd
steve <steve@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2009-11-02 17:49:36 +0000

    UDDI notification assertion now has a policy validation warning shown when used outside of a UDDI notification internal service.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@24791 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/assertions/UDDINotificationAssertion/src/main/java/com/l7tech/external/assertions/uddinotification/UDDINotificationAssertion.java
+++ modules/gateway/assertions/UDDINotificationAssertion/src/main/java/com/l7tech/external/assertions/uddinotification/UDDINotificationAssertion.java
@@ -4,6 +4,10 @@ import com.l7tech.policy.assertion.Assertion;
 import com.l7tech.policy.assertion.AssertionMetadata;
 import com.l7tech.policy.assertion.DefaultAssertionMetadata;
 import static com.l7tech.policy.assertion.AssertionMetadata.*;
+import com.l7tech.policy.validator.AssertionValidator;
+import com.l7tech.policy.AssertionPath;
+import com.l7tech.policy.PolicyValidatorResult;
+import com.l7tech.wsdl.Wsdl;
 
 /**
  * 
@@ -24,10 +28,33 @@ public class UDDINotificationAssertion extends Assertion {
         meta.put(POLICY_NODE_ICON, "com/l7tech/console/resources/server16.gif");
         meta.put(FEATURE_SET_NAME, "(fromClass)");
         meta.put(IS_ROUTING_ASSERTION, Boolean.TRUE);
+        meta.put(POLICY_VALIDATOR_CLASSNAME, UDDINotificationAssertion.Validator.class.getName());
         meta.put(MODULE_LOAD_LISTENER_CLASSNAME, "com.l7tech.external.assertions.uddinotification.server.UDDINotificationModuleLifecycle");
 
         meta.put(META_INITIALIZED, Boolean.TRUE);
         return meta;
+    }
+
+    public static class Validator implements AssertionValidator {
+        private UDDINotificationAssertion assertion;
+
+        public Validator( final UDDINotificationAssertion assertion ) {
+            this.assertion = assertion;
+        }
+
+        @Override
+        public void validate( final AssertionPath path,
+                              final Wsdl wsdl,
+                              final boolean soap,
+                              final PolicyValidatorResult result ) {
+            if ( (wsdl == null) || (!"urn:uddi-org:subr_v3".equals(wsdl.getTargetNamespace())) ){
+                result.addWarning(new PolicyValidatorResult.Warning(
+                                   assertion,
+                                   path,
+                                   "Assertion is for use only with a UDDI Notification Service",
+                                   null));
+            }
+        }
     }
 
 }
