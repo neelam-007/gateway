053bc686100ab8dffb1b1f2cd3f336d3a2b13378
darmstrong <darmstrong@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2009-12-10 18:21:10 +0000

    Very minor change to an existing system message, based on how it is being used.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@25298 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/common/src/main/java/com/l7tech/gateway/common/audit/SystemMessages.java
+++ modules/gateway/common/src/main/java/com/l7tech/gateway/common/audit/SystemMessages.java
@@ -126,7 +126,7 @@ public class SystemMessages extends Messages {
     public static final M UDDI_PUBLISH_ENDPOINT_FAILED          = m(2284, Level.WARNING, "Error publishing endpoint ''{0}''.");
     public static final M UDDI_REMOVE_SERVICE_FAILED            = m(2285, Level.WARNING, "Could not delete proxied BusinessService ''{0}''.");
     public static final M UDDI_WSPOLICY_PUBLISH_FAILED          = m(2286, Level.WARNING, "Error publishing ws-policy attachment ''{0}''.");
-    public static final M UDDI_NOTIFICATION_PROCESSING_FAILED   = m(2287, Level.WARNING, "Error processing UDDI notification for serviceKey ''{0}''.");
+    public static final M UDDI_NOTIFICATION_PROCESSING_FAILED   = m(2287, Level.WARNING, "Error processing UDDI notification: ''{0}''.");
     public static final M UDDI_NOTIFICATION_SERVICE_DISABLED    = m(2288, Level.WARNING, "UDDI Notification has caused Published Service to be disabled. Published Service ID ''{0}''.");
     public static final M UDDI_NOTIFICATION_SERVICE_DELETED     = m(2289, Level.WARNING, "UDDI Notification that monitored BusinessService has been deleted. Deleting SSG records for serviceKey ''{0}''.");
     public static final M UDDI_MAINTENANCE_SERVICE_DELETED      = m(2290, Level.WARNING, "Error deleting record of proxied business service which has been deleted from UDDI Registry #({0}) with serviceKey ''{1}''");
