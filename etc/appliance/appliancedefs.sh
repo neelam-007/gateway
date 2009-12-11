c8a630cbe1f6945f1c7300a6dff0694be190867d
megery <megery@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2009-12-11 20:45:24 +0000

    SSG-3721: the rpm build now sets the jvmarch value in appliancedefs.sh, and this is used to decide if the -XX:+UseCompressedOops option should be included in the jvm options.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@25309 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- etc/appliance/appliancedefs.sh
+++ etc/appliance/appliancedefs.sh
@@ -2,7 +2,8 @@
 # Defines SSG_JAVA_HOME, etc
 
 # Set to 1 if using 32-bit jvm, to avoid trying to allocate more than 2gb of ram for the jvm
-using32bitjvm=1
+usebigmemory=1
+jvmarch=x86_64
 
 ulimit -s 2048
 
@@ -24,7 +25,7 @@ system_ram=`grep MemTotal /proc/meminfo |cut -c 15-23`
 multiplier="2/3"
 #
 let java_ram="$system_ram*$multiplier"
-if [ $using32bitjvm -ne 0 -a `expr $java_ram \> 2074412` == 1 ]; then
+if [ $usebigmemory -ne 0 -a `expr $java_ram \> 2074412` == 1 ]; then
 	# we have more ram than java can use
 	# FIXME: when we start running 64 bit JVM
 	java_ram=2074412;
@@ -34,7 +35,8 @@ fi
 SSG_JAVA_HOME="/opt/SecureSpan/JDK"
 
 # Setting larger permsize for java 1.6
-NODE_OPTS="-Xmx${java_ram}k -Xss256k -XX:+UseCompressedOops -XX:+UseParallelOldGC"
+NODE_OPTS="-Xmx${java_ram}k -Xss256k -XX:+UseParallelOldGC"
+[ "${jvmarch}" == "x86_64 ] && NODE_OPTS="$NODE_OPTS -XX:+UseCompressedOops"
 
 export SSG_JAVA_HOME
 export NODE_OPTS
--- etc/appliance/ssg-appliance.spec
+++ etc/appliance/ssg-appliance.spec
@@ -51,6 +51,7 @@ mv %{buildroot}/jdk %{buildroot}/opt/SecureSpan/JDK
 #set innodb data file values for i386 / VM builds
 sed -i -e "s/^\(innodb_data_file_path=ibdata\):[^:]*:/\1:100M:/" %{buildroot}/etc/my.cnf.ssg
 sed -i -e "s/:autoextend:max:.*$/:autoextend:max:3072M/" %{buildroot}/etc/my.cnf.ssg
+sed -i -e "s/^\(jvmarch=\).*$/\1i386/" %{buildroot}/opt/SecureSpan/Gateway/runtime/etc/profile.d/appliancedefs.sh
 %endif
 
 %files
