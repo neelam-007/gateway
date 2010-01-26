58434338d27407d1d8ec41cb2ebb023ef879e5f8
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2010-01-26 18:49:28 +0000

    Committing test utility for WS-SecureConversation performance.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@25437 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ modules/gateway/server/src/test/java/com/l7tech/server/SecureConversationPerformanceTester.java
@@ -0,0 +1,67 @@
+package com.l7tech.server;
+
+import com.l7tech.common.io.XmlUtil;
+import com.l7tech.identity.UserBean;
+import com.l7tech.message.Message;
+import com.l7tech.policy.assertion.credential.LoginCredentials;
+import com.l7tech.policy.assertion.credential.http.HttpBasic;
+import com.l7tech.security.token.http.HttpBasicToken;
+import com.l7tech.security.xml.WssDecoratorTest;
+import com.l7tech.security.xml.decorator.DecorationRequirements;
+import com.l7tech.security.xml.decorator.WssDecoratorImpl;
+import com.l7tech.security.xml.processor.ProcessorResult;
+import com.l7tech.security.xml.processor.SecurityContext;
+import com.l7tech.security.xml.processor.SecurityContextFinder;
+import com.l7tech.security.xml.processor.WssProcessorImpl;
+import com.l7tech.server.secureconversation.SecureConversationContextManager;
+import com.l7tech.server.secureconversation.SecureConversationSession;
+import com.l7tech.test.BenchmarkRunner;
+import com.l7tech.util.SoapConstants;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;                                                                          
+
+/**
+ *
+ */
+public class SecureConversationPerformanceTester {
+    @Test
+    public void testSignatureValidationPerformance() throws Exception {
+        WssDecoratorTest.TestDocument td = new WssDecoratorTest().getSigningOnlyWithSecureConversationTestDocument();
+        final SecureConversationSession session = SecureConversationContextManager.getInstance().createContextForUser(new UserBean(3, "foo"),
+                LoginCredentials.makeLoginCredentials(new HttpBasicToken("foo", "blah".toCharArray()),
+                        HttpBasic.class), SoapConstants.WSSC_NAMESPACE2);
+        td.req.setSecureConversationSession(new DecorationRequirements.SimpleSecureConversationSession(session.getIdentifier(),
+                session.getSharedSecret(),
+                session.getSecConvNamespaceUsed()));
+        td.req.getElementsToSign().clear();
+        td.req.getElementsToEncrypt().clear();
+
+        Message message = new Message(td.c.message);
+        new WssDecoratorImpl().decorateMessage(message, td.req);
+        final String reqXml = XmlUtil.nodeToString(message.getXmlKnob().getDocumentReadOnly());
+
+        System.out.println("Request (reformamted): " + XmlUtil.nodeToFormattedString(XmlUtil.stringToDocument(reqXml)));
+
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                Message req = new Message(XmlUtil.stringAsDocument(reqXml));
+                try {
+                    final WssProcessorImpl wssProcessor = new WssProcessorImpl(req);
+                    wssProcessor.setSecurityContextFinder(new SecurityContextFinder() {
+                        @Override
+                        public SecurityContext getSecurityContext(String securityContextIdentifier) {
+                            return SecureConversationContextManager.getInstance().getSecurityContext(securityContextIdentifier);
+                        }
+                    });
+                    ProcessorResult got = wssProcessor.processMessage();
+                    assertTrue(got.getElementsThatWereSigned().length > 0);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+
+        new BenchmarkRunner(r, 1000000, 10, "WS-SecureConversation: Process signed request").run();
+    }
+}
