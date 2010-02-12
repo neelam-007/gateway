de191399a9f7e27481542a032be2a81478c0b0d7
steve <steve@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2010-02-12 19:54:06 +0000

    Remove incorrect directories from previous commit.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@25506 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/assertions/GatewayManagementAssertion/src/main/java/com/l7tech/external/assertions/wsmanagement/server/model/package-info.java
+++
@@ -1,6 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema (
-        namespace = "http://www.layer7tech.com/managmement/api",
-        elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED,
-        xmlns = { @javax.xml.bind.annotation.XmlNs(prefix = "l7", namespaceURI="http://www.layer7tech.com/managmement/api") }
-)
-package com.l7tech.external.assertions.wsmanagement.server.model;
