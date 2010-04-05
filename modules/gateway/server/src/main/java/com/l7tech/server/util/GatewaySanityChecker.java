af669533d81b561f5e60494f961e655bd90b760e
jbufu <jbufu@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2010-04-05 21:29:09 +0000

    bug 8594: Added JMS provider type upgrade task to GatewaySanityChecker.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@25882 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/server/src/main/java/com/l7tech/server/util/GatewaySanityChecker.java
+++ modules/gateway/server/src/main/java/com/l7tech/server/util/GatewaySanityChecker.java
@@ -67,7 +67,8 @@ public class GatewaySanityChecker extends ApplicationObjectSupport implements In
                               "com.l7tech.server.upgrade.Upgrade51To52AddClusterProperties",
                               "com.l7tech.server.upgrade.Upgrade51To52UpdateRoles",
                               "com.l7tech.server.upgrade.Upgrade52To53UpdateRoles",
-                              "com.l7tech.server.upgrade.Upgrade52to53UpdateCommunitySchemas");
+                              "com.l7tech.server.upgrade.Upgrade52to53UpdateCommunitySchemas",
+                              "com.l7tech.server.upgrade.Upgrade52To53UpdateJmsProviderType");
             p(Starting.class, "com.l7tech.server.upgrade.Upgrade365To37AddSampleMessagePermissions");
             p(Started.class,  "com.l7tech.server.upgrade.Upgrade35To36AddRoles",
                               "com.l7tech.server.upgrade.Upgrade42To43AddPolicyPermissions",
