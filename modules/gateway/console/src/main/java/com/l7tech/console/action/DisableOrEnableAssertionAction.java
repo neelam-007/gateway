1b74c17afd01789bfdf6ed53f7f62caf3d634adc
ghuang <ghuang@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2010-05-20 23:02:56 +0000

    Fixed an issue - The disabling status of child assertions will not change right after the user disables a composite assertion.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@26194 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/console/src/main/java/com/l7tech/console/action/DisableOrEnableAssertionAction.java
+++ modules/gateway/console/src/main/java/com/l7tech/console/action/DisableOrEnableAssertionAction.java
@@ -2,14 +2,15 @@ package com.l7tech.console.action;
 
 import com.l7tech.console.tree.policy.AssertionTreeNode;
 import com.l7tech.console.tree.policy.PolicyTreeModel;
-import com.l7tech.console.panels.WorkSpacePanel;
 import com.l7tech.console.util.TopComponents;
-import com.l7tech.console.poleditor.PolicyEditorPanel;
 import com.l7tech.policy.assertion.Assertion;
 import com.l7tech.policy.assertion.composite.CompositeAssertion;
 
 import javax.swing.*;
+import javax.swing.tree.TreeNode;
 import javax.swing.tree.TreePath;
+import java.util.Enumeration;
+import java.util.List;
 import java.util.logging.Logger;
 import java.util.ArrayList;
 
@@ -20,62 +21,71 @@ public abstract class DisableOrEnableAssertionAction extends NodeAction {
     protected static final int DISABLE_ASSERTION_ACTION_IDX = 0;
     protected static final int ENABLE_ASSERTION_ACTION_IDX = 1;
 
+    private final PolicyTreeModel policyTreeModel;
     private Logger logger = Logger.getLogger(DisableOrEnableAssertionAction.class.getName());
     private int actionIdx;
 
     public DisableOrEnableAssertionAction(AssertionTreeNode node, int actionIdx) {
         super(node);
         this.actionIdx = actionIdx;
+        policyTreeModel = (PolicyTreeModel) TopComponents.getInstance().getPolicyTree().getModel();
     }
 
     public void performAction() {
-        // Step1:
         // Perform a disable/enable action on selected assertion(s) - could be a single assertion or
-        // multiple assertions.  Also, perform disable/enable on descendant or ancestor if appliably
-        Assertion[] allSelectedAssertions = getAllSelectedAssertions();
+        // multiple assertions.  Also, perform disable/enable on descendant or ancestor if applicable
+        List<AssertionTreeNode> allSelectedAssertionNodes = getAllSelectedAssertionNodes();
         switch (actionIdx) {
             case DISABLE_ASSERTION_ACTION_IDX:
-                for (Assertion currAssertion: allSelectedAssertions) disableAssertion(currAssertion);
+                for (AssertionTreeNode node: allSelectedAssertionNodes) {
+                    // Disable the assertion
+                    disableAssertion(node);
+
+                    // Notify the assertion is disabled, then Save buttons will be activated.
+                    notifyAssertionTreeNodeChanged(node);
+                }
                 break;
             case ENABLE_ASSERTION_ACTION_IDX:
-                for (Assertion currAssertion: allSelectedAssertions) enableAssertion(currAssertion);
+                for (AssertionTreeNode node: allSelectedAssertionNodes) {
+                    // Enable the assertion
+                    enableAssertion(node);
+
+                    // Notify the assertion is enabled, then Save buttons will be activated.
+                    notifyAssertionTreeNodeChanged(node);
+                }
                 break;
             default:
                 logger.warning("Invalid action index: " + actionIdx);
-                return;
         }
-
-        // Step2: notify the assertion is disabled/enabled, then Save buttons will be activated.
-        notifyAssertionTreeNodeChanged();
     }
 
     /**
      * Collect all selected assertions.
      * @return all selected assertion.
      */
-    private Assertion[] getAllSelectedAssertions() {
+    private List<AssertionTreeNode> getAllSelectedAssertionNodes() {
         final JTree policyTree = TopComponents.getInstance().getPolicyTree();
         TreePath[] paths = policyTree.getSelectionPaths();
-        java.util.List<Assertion> assertions = new ArrayList<Assertion>();
+        List<AssertionTreeNode> nodeList = new ArrayList<AssertionTreeNode>();
 
         if (paths != null) {
             for (TreePath path : paths) {
-                AssertionTreeNode assertionTreeNode = (AssertionTreeNode) path.getLastPathComponent();
-                assertions.add(assertionTreeNode.asAssertion());
+                nodeList.add((AssertionTreeNode) path.getLastPathComponent());
             }
         }
 
-        return assertions.toArray(new Assertion[assertions.size()]);
+        return nodeList;
     }
 
     /**
-     * Disable the assertion and its descendant if appliable
-     * @param assertion: an assertion to be diabled.
+     * Disable the assertion and its descendant if applicable
+     * @param node: an assertion node to be disabled.
      */
-    private void disableAssertion(Assertion assertion) {
+    private void disableAssertion(AssertionTreeNode node) {
         // Update the disable status of the assertion associated with the node.
+        Assertion assertion = node.asAssertion();
         assertion.setEnabled(false);
-        // Update the diable status of its descendant.
+        // Update the disable status of its descendant.
         if (assertion instanceof CompositeAssertion) {
             ((CompositeAssertion)assertion).disableDescendant();
         }
@@ -83,10 +93,11 @@ public abstract class DisableOrEnableAssertionAction extends NodeAction {
 
     /**
      *  Enable the assertion and its ancestor.
-     * @param assertion: an assertion to be enabled.
+     * @param node: an assertion node to be enabled.
      */
-    private void enableAssertion(Assertion assertion) {
+    private void enableAssertion(AssertionTreeNode node) {
         // Update the enable status of the assertion associated with the node.
+        Assertion assertion = node.asAssertion();
         assertion.setEnabled(true);
         // Update the enable status of its ancestor.
         assertion.enableAncestor();
@@ -99,12 +110,26 @@ public abstract class DisableOrEnableAssertionAction extends NodeAction {
      * There are three places where we add treeModelListeners (two from {@link com.l7tech.console.poleditor.PolicyEditorPanel
      * and one from {@link com.l7tech.console.tree.policy.PolicyToolBar }.)
      */
-    private void notifyAssertionTreeNodeChanged() {
-        final JTree tree = TopComponents.getInstance().getPolicyTree();
-        final PolicyTreeModel policyTreeModel = (PolicyTreeModel)tree.getModel();
-
-        if (node instanceof AssertionTreeNode) {
-            policyTreeModel.assertionTreeNodeChanged((AssertionTreeNode)node);
+    private void notifyAssertionTreeNodeChanged(AssertionTreeNode node) {
+        switch (actionIdx) {
+            case DISABLE_ASSERTION_ACTION_IDX:
+                // Notify all child nodes.
+                if (node.asAssertion() instanceof CompositeAssertion) {
+                    for (Enumeration e = node.children(); e.hasMoreElements(); )
+                        notifyAssertionTreeNodeChanged((AssertionTreeNode) e.nextElement());
+                }
+                // Notify itself.
+                policyTreeModel.assertionTreeNodeChanged(node);
+                break;
+            case ENABLE_ASSERTION_ACTION_IDX:
+                // Notify all parent nodes and itself..
+                for (TreeNode tn: node.getPath()) {
+                    if (tn.getParent() == null) continue;
+                    policyTreeModel.assertionTreeNodeChanged((AssertionTreeNode)tn);
+                }
+                break;
+            default:
+                logger.warning("Invalid action index: " + actionIdx);
         }
     }
 }
