8d4bc41b35df469de2d3c98561a9d06f32950c34
steve <steve@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2010-06-16 21:17:13 +0000

    Add tests for RbacServices.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@26316 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ modules/gateway/server/src/test/java/com/l7tech/server/security/rbac/RbacServicesTest.java
@@ -0,0 +1,134 @@
+package com.l7tech.server.security.rbac;
+
+import com.l7tech.gateway.common.security.rbac.OperationType;
+import com.l7tech.gateway.common.security.rbac.Role;
+import com.l7tech.gateway.common.service.PublishedService;
+import com.l7tech.identity.User;
+import com.l7tech.identity.UserBean;
+import com.l7tech.objectmodel.EntityType;
+import com.l7tech.objectmodel.FindException;
+import com.l7tech.server.EntityFinder;
+import com.l7tech.server.EntityFinderStub;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ *
+ */
+public class RbacServicesTest {
+
+    //- PUBLIC
+
+    @Test
+    public void testPermittedForEntity() throws Exception {
+        final RbacServices rbacServices = new RbacServicesImpl(roleManager , entityFinder );
+
+        final PublishedService service = new PublishedService();
+        service.setOid( 123 );
+        service.setName( "Test Service" );
+
+        final boolean permitted1 = rbacServices.isPermittedForEntity( new UserBean("none"), service, OperationType.READ, null );
+        assertFalse("No permissions", permitted1);
+
+        final boolean permitted2 = rbacServices.isPermittedForEntity( new UserBean("any"), service, OperationType.READ, null );
+        assertTrue("Any permission", permitted2);
+
+        final boolean permitted3 = rbacServices.isPermittedForEntity( new UserBean("some"), service, OperationType.READ, null );
+        assertTrue("Some permission", permitted3);
+
+        final boolean permitted4 = rbacServices.isPermittedForEntity( new UserBean("some-other"), service, OperationType.READ, null );
+        assertFalse("Some other entity permission", permitted4);
+
+        final boolean permitted5 = rbacServices.isPermittedForSomeEntityOfType( new UserBean("other-ent"), OperationType.READ, EntityType.SERVICE );
+        assertFalse("Other entity type permission", permitted5);
+
+        final boolean permitted6 = rbacServices.isPermittedForSomeEntityOfType( new UserBean("other-perm"), OperationType.READ, EntityType.SERVICE );
+        assertFalse("Other operation permission", permitted6);
+    }
+
+    @Test
+    public void testPermittedForAnyEntity() throws Exception {
+        final RbacServices rbacServices = new RbacServicesImpl(roleManager , entityFinder );
+
+        final boolean permitted1 = rbacServices.isPermittedForAnyEntityOfType( new UserBean("none"), OperationType.READ, EntityType.SERVICE );
+        assertFalse("No permissions", permitted1);
+
+        final boolean permitted2 = rbacServices.isPermittedForAnyEntityOfType( new UserBean("any"), OperationType.READ, EntityType.SERVICE );
+        assertTrue("Any permission", permitted2);
+
+        final boolean permitted3 = rbacServices.isPermittedForAnyEntityOfType( new UserBean("some"), OperationType.READ, EntityType.SERVICE );
+        assertFalse("Some permission", permitted3);
+
+        final boolean permitted4 = rbacServices.isPermittedForSomeEntityOfType( new UserBean("other-ent"), OperationType.READ, EntityType.SERVICE );
+        assertFalse("Other entity permission", permitted4);
+
+        final boolean permitted5 = rbacServices.isPermittedForSomeEntityOfType( new UserBean("other-perm"), OperationType.READ, EntityType.SERVICE );
+        assertFalse("Other operation permission", permitted5);
+    }
+
+    @Test
+    public void testPermittedForSomeEntity() throws Exception {
+        final RbacServices rbacServices = new RbacServicesImpl(roleManager , entityFinder );
+
+        final boolean permitted1 = rbacServices.isPermittedForSomeEntityOfType( new UserBean("none"), OperationType.READ, EntityType.SERVICE );
+        assertFalse("No permissions", permitted1);
+
+        final boolean permitted2 = rbacServices.isPermittedForSomeEntityOfType( new UserBean("any"), OperationType.READ, EntityType.SERVICE );
+        assertTrue("Any permission", permitted2);
+
+        final boolean permitted3 = rbacServices.isPermittedForSomeEntityOfType( new UserBean("some"), OperationType.READ, EntityType.SERVICE );
+        assertTrue("Some permission", permitted3);
+
+        final boolean permitted4 = rbacServices.isPermittedForSomeEntityOfType( new UserBean("other-ent"), OperationType.READ, EntityType.SERVICE );
+        assertFalse("Other entity permission", permitted4);
+
+        final boolean permitted5 = rbacServices.isPermittedForSomeEntityOfType( new UserBean("other-perm"), OperationType.READ, EntityType.SERVICE );
+        assertFalse("Other operation permission", permitted5);
+    }
+
+    //- PRIVATE
+
+    private final EntityFinder entityFinder = new EntityFinderStub();
+    private final RoleManager roleManager = new MockRoleManager( entityFinder ){
+            @Override
+            public Collection<Role> getAssignedRoles( final User user ) throws FindException {
+                if ( "some".equals( user.getLogin() ) ) {
+                    Collection<Role> roles = new ArrayList<Role>();
+                    Role role = new Role();
+                    role.addEntityPermission( OperationType.READ, EntityType.SERVICE, "123" );
+                    roles.add( role );
+                    return roles;
+                } else if ( "some-other".equals( user.getLogin() ) ) {
+                    Collection<Role> roles = new ArrayList<Role>();
+                    Role role = new Role();
+                    role.addEntityPermission( OperationType.READ, EntityType.SERVICE, "456" );
+                    roles.add( role );
+                    return roles;
+                } else if ( "any".equals( user.getLogin() ) ) {
+                    Collection<Role> roles = new ArrayList<Role>();
+                    Role role = new Role();
+                    role.addEntityPermission( OperationType.READ, EntityType.SERVICE, null );
+                    roles.add( role );
+                    return roles;
+                } else if ( "other-ent".equals( user.getLogin() ) ) {
+                    Collection<Role> roles = new ArrayList<Role>();
+                    Role role = new Role();
+                    role.addEntityPermission( OperationType.READ, EntityType.POLICY, null );
+                    roles.add( role );
+                    return roles;
+                } else if ( "other-perm".equals( user.getLogin() ) ) {
+                    Collection<Role> roles = new ArrayList<Role>();
+                    Role role = new Role();
+                    role.addEntityPermission( OperationType.UPDATE, EntityType.SERVICE, null );
+                    roles.add( role );
+                    return roles;
+                } else { // none
+                    return Collections.emptyList();
+                }
+            }
+        };
+}
