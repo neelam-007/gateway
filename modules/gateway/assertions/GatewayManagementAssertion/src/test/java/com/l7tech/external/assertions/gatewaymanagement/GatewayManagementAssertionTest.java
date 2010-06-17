0e63aea2d64bd4984663130b591863d385e934ba
steve <steve@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2010-06-17 00:11:06 +0000

    SSM-2766: Fix for Gateway Management: Have better validator messages in the service policy.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@26324 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/assertions/GatewayManagementAssertion/src/test/java/com/l7tech/external/assertions/gatewaymanagement/GatewayManagementAssertionTest.java
+++ modules/gateway/assertions/GatewayManagementAssertion/src/test/java/com/l7tech/external/assertions/gatewaymanagement/GatewayManagementAssertionTest.java
@@ -1,7 +1,10 @@
 package com.l7tech.external.assertions.gatewaymanagement;
 
+import com.l7tech.policy.AssertionPath;
 import com.l7tech.policy.PolicyValidatorResult;
+import com.l7tech.policy.assertion.Assertion;
 import com.l7tech.policy.assertion.AssertionMetadata;
+import com.l7tech.policy.assertion.identity.AuthenticationAssertion;
 import com.l7tech.policy.validator.PolicyValidationContext;
 import org.junit.Test;
 
@@ -13,16 +16,30 @@ import static org.junit.Assert.assertEquals;
 public class GatewayManagementAssertionTest {
 
     @Test
-    public void testValidationWarning() {
+    public void testValidationWarningsAuthenticated() {
         final GatewayManagementAssertion gatewayManagementAssertion = new GatewayManagementAssertion();
         final GatewayManagementAssertion.Validator validator =
                 new GatewayManagementAssertion.Validator( gatewayManagementAssertion );
 
         final PolicyValidatorResult result = new PolicyValidatorResult();
-        validator.validate( null, new PolicyValidationContext(null, null, null, true), result );
+        validator.validate( new AssertionPath( new Assertion[]{ new AuthenticationAssertion(), gatewayManagementAssertion } ), new PolicyValidationContext(null, null, null, true), result );
 
         assertEquals( "Validation warning present", 1, result.getWarningCount() );
         assertEquals( "Validation warning message", "Assertion is for use only with a Gateway Management Service", result.getWarnings().get(0).getMessage());
+    }
+
+    @Test
+    public void testValidationWarningsAnonymous() {
+        final GatewayManagementAssertion gatewayManagementAssertion = new GatewayManagementAssertion();
+        final GatewayManagementAssertion.Validator validator =
+                new GatewayManagementAssertion.Validator( gatewayManagementAssertion );
+
+        final PolicyValidatorResult result = new PolicyValidatorResult();
+        validator.validate( new AssertionPath( new Assertion[]{ gatewayManagementAssertion } ), new PolicyValidationContext(null, null, null, true), result );
+
+        assertEquals( "Validation warning present", 2, result.getWarningCount() );
+        assertEquals( "Validation warning message", "Assertion is for use only with a Gateway Management Service", result.getWarnings().get(0).getMessage());
+        assertEquals( "Validation warning message", "An authentication assertion should precede this assertion, anonymous users have no access permissions.", result.getWarnings().get(1).getMessage());
     }
 
     @Test
