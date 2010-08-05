0cdd76ee4da0444507cd49674d87cf0e740761d8
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2010-08-05 05:47:37 +0000

    Reinstated corrected AuditRecordTest.testOrderedDetails(), and added a blacklist-based test that any bean properties of AuditRecord or its subclasses are either included in the signature hash or are explicitly marked as excluded.  The following audit record properties do not appear to be included in the signature hash but possibly should be:   mappingValuesEntity, mappingValuesOid and LogRecord.parameters.  (Request ID looks like it is included but the tests can't currently confirm this.)
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@26495 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/common/src/test/java/com/l7tech/gateway/common/audit/AuditRecordTest.java
+++ modules/gateway/common/src/test/java/com/l7tech/gateway/common/audit/AuditRecordTest.java
@@ -6,10 +6,16 @@ import com.l7tech.identity.User;
 import com.l7tech.policy.assertion.AssertionStatus;
 import com.l7tech.security.token.SecurityTokenType;
 import com.l7tech.test.BugNumber;
-import org.junit.Ignore;
+import com.l7tech.util.BeanUtils;
+import com.l7tech.util.ExceptionUtils;
 import org.junit.Test;
 
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.logging.Level;
 
 import static org.junit.Assert.*;
@@ -17,51 +23,163 @@ import static org.junit.Assert.*;
 /**
  * Puts audit records through their paces.
  */
+@SuppressWarnings({"ThrowableInstanceNeverThrown"})
 public class AuditRecordTest {
+    // Any audit record (or one of its three tested subclasses) propery listed here is not expected to be covered by the signature.
+    // All other properties ARE expected to be covered by the signature.
+    private String[] propertiesNotCoveredBySignature = {
+            // Omitted because they change after creation
+            "oid",
+            "signature",
+            "version", 
+
+            // inherited from LogRecord and probably not useful to include in sig
+            "sourceMethodName",
+            "sourceClassName",
+            "threadID",
+            "resourceBundle",
+            "resourceBundleName",
+            "loggerName",
+            "sequenceNumber",
+            "thrown",
+            "parameters",  // TODO these should probably be included anyway
+
+            // TODO These should probably be included
+            "mappingValuesEntity",
+            "mappingValuesOid",
+
+            // This one apparently IS included but this test currently does not appear to permute it properly
+            "reqId",
+            "strRequestId",
+    };
+
+    int ordinal = 1;
+    long gen = 23423;
+    long seq = 53553;
 
     @Test
     @BugNumber(8968)
     public void testSigningSystem() throws Exception {
-        testSigning(makeSystemAuditRecord());
+        testSignatureDigest(makeSystemAuditRecord());
     }
 
     @Test
     @BugNumber(8968)
     public void testSigningMessage() throws Exception {
-        testSigning(makeMessageAuditRecord());
+        testSignatureDigest(makeMessageAuditRecord());
     }
 
     @Test
     @BugNumber(8968)
     public void testSigningAdmin() throws Exception {
-        testSigning(makeAdminAuditRecord());
+        testSignatureDigest(makeAdminAuditRecord());
     }
 
-    private void testSigning(AuditRecord rec) throws Exception {
+    private void testSignatureDigest(AuditRecord rec) throws Exception {
+        Set<PropertyDescriptor> props = BeanUtils.omitProperties(BeanUtils.getProperties(rec.getClass()), propertiesNotCoveredBySignature);
+
+        // Ensure digest changes when any property is modified
+        for (PropertyDescriptor prop : props) {
+            testSignatureDigest(rec, prop);
+        }
+    }
+
+
+    private void testSignatureDigest(AuditRecord rec, PropertyDescriptor propertyToPermute) throws Exception {
+        String propName = propertyToPermute.getName();
         rec.setSignature("dummy");
 
         byte[] oldDigest = rec.computeSignatureDigest();
-        assertNotNull(oldDigest);
+        assertNotNull("computeSignatureDigest must return a non-null hash (after a dummy signature value is set)", oldDigest);
 
-        String oldMessage = rec.getMessage();
-        rec.setMessage(oldMessage + " yop");
+        AuditRecord backup = rec.getClass().newInstance();
+        BeanUtils.copyProperties(rec, backup);
+
+        permuteProperty(rec, propertyToPermute);
 
         byte[] newDigest = rec.computeSignatureDigest();
-        assertFalse(Arrays.equals(oldDigest, newDigest));
+        assertFalse("Hash must change after permuting property: " + propName, Arrays.equals(oldDigest, newDigest));
 
-        rec.setMessage(oldMessage);
+        restoreProperty(rec, propertyToPermute, backup);
+
         byte[] restoredDigest = rec.computeSignatureDigest();
-        assertTrue(Arrays.equals(oldDigest, restoredDigest));
+        assertTrue("Hash must be restored after restoring property: " + propName, Arrays.equals(oldDigest, restoredDigest));
+    }
+
+    private void permuteProperty(AuditRecord rec, PropertyDescriptor pd) throws InvocationTargetException, IllegalAccessException {
+        String propName = pd.getName();
+
+        Object prop = pd.getReadMethod().invoke(rec);
+
+        Object newProp;
+        if ("strLvl".equals(propName)) {
+            newProp = "INFO".equals(prop) ? "WARNING" : "INFO";
+        } else if ("level".equals(propName)) {
+            newProp = Level.INFO.equals(prop) ? Level.WARNING : Level.INFO;
+        } else if ("parameters".equals(propName)) {
+            newProp = prop != null ? null : new Object[] { "yop" };
+        } else if ("thrown".equals(propName)) {
+            newProp = prop != null ? null : new RuntimeException("yop");
+        } else if (pd.getPropertyType().isAssignableFrom(RequestId.class)) {
+            newProp = new RequestId(gen++, seq++);
+        } else if ("authenticationType".equals(propName)) {
+            newProp = prop != null ? null : SecurityTokenType.FTP_CREDENTIAL;
+        } else if ("details".equals(propName)) {
+            newProp = prop != null && ((Collection)prop).size() > 0 ? new HashSet<AuditDetail>()
+                    : new HashSet<AuditDetail>(Arrays.asList(newDetail(MessageProcessingMessages.EVENT_MANAGER_EXCEPTION)));
+        } else if ("action".equals(propName) && rec instanceof AdminAuditRecord) {
+            newProp = "U".equals(String.valueOf(prop)) ? 'D' : 'U';
+        } else {
+            newProp = permuteValue(prop, pd.getPropertyType());
+        }
+
+        if (newProp != prop) {
+            try {
+                pd.getWriteMethod().invoke(rec, newProp);
+            } catch (Exception e) {
+                fail("Failed permuting " + propName + " value of " + prop + " to " + newProp + ": " + ExceptionUtils.getMessage(e));
+            }
+        } else {
+            throw new RuntimeException("Didn't permute " + prop);
+        }
+    }
+
+    private Object permuteValue(Object prop, Class propClass) {
+        if (String.class == propClass) {
+            return prop + " yop";
+        } else if (Integer.class == propClass || int.class == propClass) {
+            Integer i = (Integer)prop;
+            return i == null ? 1 : i + 1;
+        } else if (Long.class == propClass || long.class == propClass) {
+            Long l = (Long)prop;
+            return l == null ? 1 : l + 1;
+        } else if (Byte.class == propClass || byte.class == propClass) {
+            Byte b = (Byte)prop;
+            return b == null ? 1 : b + 1;
+        } else if (Short.class == propClass || short.class == propClass) {
+            Short s = (Short)prop;
+            return s == null ? 1 : s + 1;
+        } else if (Character.class == propClass || char.class == propClass) {
+            Character c = (Character)prop;
+            return c == null ? 1 : c + 1;
+        } else {
+            throw new RuntimeException("Don't know how to permute: " + propClass);
+        }
+    }
+
+    private void restoreProperty(AuditRecord rec, PropertyDescriptor propertyToPermute, AuditRecord backup) throws InvocationTargetException, IllegalAccessException {
+        BeanUtils.copyProperties(backup, rec, Arrays.asList(propertyToPermute));
     }
 
     @Test
-    @Ignore("Currently being debugged")
     public void testOrderedDetails() {
         AuditRecord rec = makeMessageAuditRecord();
 
-        rec.getDetails().add(new AuditDetail(AssertionMessages.EXCEPTION_SEVERE));
-        rec.getDetails().add(new AuditDetail(SystemMessages.AUDIT_ARCHIVER_ERROR));
-        rec.getDetails().add(new AuditDetail(MessageProcessingMessages.ERROR_WSS_PROCESSING_INFO));
+        Set<AuditDetail> details = new HashSet<AuditDetail>();        
+        details.add(newDetail(AssertionMessages.EXCEPTION_SEVERE));
+        details.add(newDetail(SystemMessages.AUDIT_ARCHIVER_ERROR));
+        details.add(newDetail(MessageProcessingMessages.ERROR_WSS_PROCESSING_INFO));
+        rec.setDetails(details);
 
         AuditDetail[] ordered = rec.getDetailsInOrder();
         assertEquals(ordered[0].getMessageId(), AssertionMessages.EXCEPTION_SEVERE.getId());
@@ -69,6 +187,11 @@ public class AuditRecordTest {
         assertEquals(ordered[2].getMessageId(), MessageProcessingMessages.ERROR_WSS_PROCESSING_INFO.getId());
     }
 
+    private AuditDetail newDetail(AuditDetailMessage msg) {
+        AuditDetail detail = new AuditDetail(msg);
+        detail.setOrdinal(ordinal++);
+        return detail;
+    }
 
     private AuditRecord makeAdminAuditRecord() {
         AuditRecord auditRecord = new AdminAuditRecord(Level.INFO, "node1", 1234, User.class.getName(), "testuser", AdminAuditRecord.ACTION_UPDATED, "updated", -1, "admin", "1111", "2.3.4.5");
@@ -80,7 +203,7 @@ public class AuditRecordTest {
     }
 
     private AuditRecord makeMessageAuditRecord() {
-        AuditRecord auditRecord = new MessageSummaryAuditRecord(Level.INFO, "node1", "req4545", AssertionStatus.NONE, "3.2.1.1", null, 4833, null, 9483, 200, 232, 8859, "ACMEWarehouse", "listProducts", true, SecurityTokenType.HTTP_BASIC, -2, "alice", "41123", 49585);
+        AuditRecord auditRecord = new MessageSummaryAuditRecord(Level.INFO, "node1", "2342345-4545", AssertionStatus.NONE, "3.2.1.1", null, 4833, null, 9483, 200, 232, 8859, "ACMEWarehouse", "listProducts", true, SecurityTokenType.HTTP_BASIC, -2, "alice", "41123", 49585);
         auditRecord.setThrown(new RuntimeException("main record throwable"));
         final AuditDetail detail1 = new AuditDetail(Messages.EXCEPTION_INFO_WITH_MORE_INFO, new String[]{"foomp"}, new IllegalArgumentException("Exception for foomp detail"));
         auditRecord.getDetails().add(detail1);
