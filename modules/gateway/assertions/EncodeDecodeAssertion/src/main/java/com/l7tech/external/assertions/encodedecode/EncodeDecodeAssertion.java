e4c8aadc52cb9809c4e82e626a97952fa62b003d
steve <steve@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2010-08-20 16:51:37 +0000

    SSM-2871: Fix for Encode Decode: Capitalise the word "data" in the assertion name in the palette
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@26630 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/assertions/EncodeDecodeAssertion/src/main/java/com/l7tech/external/assertions/encodedecode/EncodeDecodeAssertion.java
+++ modules/gateway/assertions/EncodeDecodeAssertion/src/main/java/com/l7tech/external/assertions/encodedecode/EncodeDecodeAssertion.java
@@ -111,7 +111,7 @@ public class EncodeDecodeAssertion extends Assertion implements SetsVariables, U
         if (Boolean.TRUE.equals(meta.get(META_INITIALIZED)))
             return meta;
 
-        final String baseName = "Encode / Decode data";
+        final String baseName = "Encode / Decode Data";
         meta.put( SHORT_NAME, baseName );
         meta.put( DESCRIPTION, baseName );
         meta.put( PALETTE_FOLDERS, new String[] { "xml" } );
