7acc80e4b770f99ec7ff1cffcaf91a362a1231d6
jbufu <jbufu@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2010-10-14 22:17:28 +0000

    bug 8709: IPv6 support for IpListPanels / GetIpDialog (used by the HTTP routing assertion, BRA, Bridge SSG Network Configuration).
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@26871 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gui/src/main/java/com/l7tech/gui/widgets/GetIpDialog.java
+++ modules/gui/src/main/java/com/l7tech/gui/widgets/GetIpDialog.java
@@ -5,8 +5,10 @@
 
 package com.l7tech.gui.widgets;
 
+import com.l7tech.common.io.InetAddressUtil;
 import com.l7tech.gui.util.Utilities;
 import com.l7tech.util.Functions;
+import com.l7tech.util.Pair;
 import com.l7tech.util.ValidationUtils;
 
 import javax.swing.*;
@@ -74,6 +76,7 @@ public class GetIpDialog extends JDialog {
         okButton = new JButton("OK");
         JButton cancelButton = new JButton("Cancel");
         Action buttonAction = new AbstractAction() {
+            @Override
             public void actionPerformed(ActionEvent e) {
                 if (e.getSource() == okButton) {
                     String s = getIpRangeTextField().getText();
@@ -90,7 +93,7 @@ public class GetIpDialog extends JDialog {
                                      GridBagConstraints.EAST,
                                      GridBagConstraints.HORIZONTAL,
                                      new Insets(0, 20, 0, 0), 0, 0));
-        Utilities.equalizeButtonSizes(new AbstractButton[] { okButton, cancelButton });
+        Utilities.equalizeButtonSizes(okButton, cancelButton);
         p.add(okButton,
               new GridBagConstraints(1, 2, 1, 1, 0.0, 0.0,
                                      GridBagConstraints.EAST,
@@ -107,10 +110,15 @@ public class GetIpDialog extends JDialog {
         checkValid();
     }
 
-    private static final Pattern ipPattern = Pattern.compile("(\\d+)\\.(\\d+)\\.(\\d+)\\.(\\d+)");
-    private static final Pattern badIpChars = Pattern.compile("[^0-9.]");
+    private static final Pattern ipv4Pattern = Pattern.compile("^(\\d+)\\.(\\d+)\\.(\\d+)\\.(\\d+)$");
+    private static final Pattern badIpChars = Pattern.compile("[^0-9a-fA-F.:]");
 
-    /** Check for valid IP, attempting to narrow in on the invalid portion if it isn't valid. */
+    /**
+     * Check if the provided string is a valid IP address or URL based on the validateIPFormat member flag.
+     * If invalid IP, attempts to determine the bad character range and accordingly squiggly-set the text field.
+     *
+     * @return true if the the provided string is a valid IP or URL, false otherwise
+     */
     private boolean isValidIp(String s) {
         if (!validateIPFormat) {
             // Check if it is a valid URL.
@@ -122,38 +130,77 @@ public class GetIpDialog extends JDialog {
             }
             return isValidURL; 
         }
-        boolean ret = false;
-        int pos = -1;
-        int end = -1;
-        try {
-            Matcher matcher = ipPattern.matcher(s);
-            if (!matcher.matches())
-                return false;
 
-            for (int i = 0; i < 4; ++i) {
-                final String matched = matcher.group(i + 1);
-                pos = matcher.start(i + 1);
-                end = matcher.end(i + 1);
-                int c = Integer.parseInt(matched);
-                if (c < 0 || c > 255)
-                    return false;
-            }
-
-            getIpRangeTextField().setNone();
-            ret = true;
+        Pair<Integer,Integer> badRange = getBadRange(s);
+        if (badRange == null) {
+            getIpRangeTextField().setNone(); // valid IP
             return true;
-        } catch (NumberFormatException nfe) {
+        } else if (badRange.left == null || badRange.left < 0 || badRange.right == null || badRange.right < 0) {
+            getIpRangeTextField().setAll();
             return false;
-        } finally {
-            if (ret) {
-                getIpRangeTextField().setNone();
-            } else {
-                if (pos < 0)
-                    getIpRangeTextField().setAll();
-                else
-                    getIpRangeTextField().setRange(pos, end);
-            }
+        } else {
+            getIpRangeTextField().setRange(badRange.left, badRange.right);
+            return false;
         }
+    }
+
+    /**
+     * Attempt to determine the bad character range that makes the provided string an invalid IP address.
+     *
+     * @return null if the provided string is a valid IP address,
+     *         the start and end positions of the detected invalid range, or
+     *         Pair<-1,-1> if the invalid range could not be determined.
+     */
+    private Pair<Integer, Integer> getBadRange(String s) {
+        if ( InetAddressUtil.isValidIpAddress(s) || InetAddressUtil.isValidIpv6Address(s) ) {
+            return null;
+        }
+
+        Matcher matcher4 = ipv4Pattern.matcher(s);
+        if (matcher4.matches()) {
+            // looks like a IPv4 address
+            int start = -1;
+            int end = -1;
+            for (int i = 0; i < 4; ++i) {
+                final String matched = matcher4.group(i + 1);
+                start = matcher4.start(i + 1);
+                end = matcher4.end(i + 1);
+                try {
+                    int c = Integer.parseInt(matched);
+                    if (c < 0 || c > 255)
+                        break;
+                } catch (NumberFormatException nfe) {
+                    break;
+                }
+            }
+            return new Pair<Integer, Integer>(start, end);
+        } else {
+            // assume IPv6 format
+            int start = -1;
+            int end = -1;
+            boolean wasEmpty = false;
+            String[] nibbles = s.split(":");
+            for(int i = 0; i < nibbles.length; i++) {
+                start = end + 1;
+                end += nibbles[i].length() + 1;
+                if ( i < 8 && isValidIpv6Nibbles(nibbles[i], ! wasEmpty) || (i == nibbles.length-1 && InetAddressUtil.isValidIpAddress(nibbles[i])) ) {
+                    wasEmpty |= nibbles[i].length() == 0;
+                    continue;
+                }
+                break;
+            }
+            return new Pair<Integer, Integer>(start, end);
+        }
+    }
+
+    private boolean isValidIpv6Nibbles(String nibble, boolean allowEmpty) {
+        if (nibble == null) return false;
+        if (nibble.length() == 0) return allowEmpty;
+        if (nibble.length() > 4) return false;
+        for(char c : nibble.toCharArray()) {
+            if (Character.digit(c, 16) == -1) return false;
+        }
+        return true;
     }
 
     public Functions.Unary<Boolean, String> getContextVariableValidator() {
@@ -191,6 +238,7 @@ public class GetIpDialog extends JDialog {
             ipRangeTextField = new SquigglyTextField();
             // Block bad inserts immediately
             ipRangeTextField.getDocument().addUndoableEditListener(new UndoableEditListener() {
+                @Override
                 public void undoableEditHappened(UndoableEditEvent e) {
                     if (validateIPFormat) {
                         if (badIpChars.matcher(ipRangeTextField.getText()).find())
