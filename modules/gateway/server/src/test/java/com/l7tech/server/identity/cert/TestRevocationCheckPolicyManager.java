c54f49c77da192c2241586d5bdec10a23ae06b9a
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2010-11-03 21:38:15 +0000

    Adding a fake RevocationCheckPolicyManager that can be used for unit tests.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@27033 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ modules/gateway/server/src/test/java/com/l7tech/server/identity/cert/TestRevocationCheckPolicyManager.java
@@ -0,0 +1,39 @@
+package com.l7tech.server.identity.cert;
+
+import com.l7tech.gateway.common.security.RevocationCheckPolicy;
+import com.l7tech.objectmodel.EntityHeader;
+import com.l7tech.objectmodel.FindException;
+import com.l7tech.objectmodel.UpdateException;
+import com.l7tech.server.EntityManagerStub;
+
+import java.util.Collection;
+
+/**
+ * A fake RevocationCheckPolicyManager for unit tests.
+ */
+public class TestRevocationCheckPolicyManager extends EntityManagerStub<RevocationCheckPolicy, EntityHeader> implements RevocationCheckPolicyManager {
+    @Override
+    public void updateDefault(long oid, RevocationCheckPolicy revocationCheckPolicy) throws FindException, UpdateException {
+        // set default
+        if ( revocationCheckPolicy.isDefaultPolicy() ) {
+            Collection<RevocationCheckPolicy> policies =  findAll();
+            for (RevocationCheckPolicy policy : policies) {
+                if (policy.isDefaultPolicy() && policy.getOid()!=oid) {
+                    policy.setDefaultPolicy(false);
+                    update(policy);
+                }
+            }
+        }
+    }
+
+    @Override
+    public RevocationCheckPolicy getDefaultPolicy() throws FindException {
+        Collection<RevocationCheckPolicy> policies =  findAll();
+        for (RevocationCheckPolicy policy : policies) {
+            if (policy.isDefaultPolicy())
+                return policy;
+        }
+        return null;
+    }
+
+}
