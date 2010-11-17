20580cab64e3657cd0c131dc6296114dc9b1a09a
steve <steve@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2010-11-17 18:02:47 +0000

    SSM-3020: Fix for HTTP Options: Sort the list of stored passwords in HTTP Options
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@27161 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/console/src/main/java/com/l7tech/console/panels/SecurePasswordComboBox.java
+++ modules/gateway/console/src/main/java/com/l7tech/console/panels/SecurePasswordComboBox.java
@@ -5,8 +5,12 @@ import com.l7tech.gateway.common.security.password.SecurePassword;
 import com.l7tech.gui.util.DialogDisplayer;
 import com.l7tech.objectmodel.FindException;
 import com.l7tech.util.ExceptionUtils;
+import com.l7tech.util.Resolver;
+import com.l7tech.util.ResolvingComparator;
 
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -24,7 +28,13 @@ public class SecurePasswordComboBox extends JComboBox {
 
     public void reloadPasswordList() {
         try {
-            securePasswords = Registry.getDefault().getTrustedCertManager().findAllSecurePasswords();
+            securePasswords = new ArrayList<SecurePassword>(Registry.getDefault().getTrustedCertManager().findAllSecurePasswords());
+            Collections.sort( securePasswords, new ResolvingComparator<SecurePassword,String>( new Resolver<SecurePassword,String>(){
+                @Override
+                public String resolve( final SecurePassword key ) {
+                    return key.getName().toLowerCase();
+                }
+            }, false ) );
             setModel(new DefaultComboBoxModel(securePasswords.toArray()));
         } catch (FindException e) {
             final String msg = "Unable to list stored passwords: " + ExceptionUtils.getMessage(e);
