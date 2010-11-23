971d7faa32197cbf4319e6745215966069b482c0
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2010-11-23 01:34:23 +0000

    SSG-4212: The Encode to MTOM Format assertion dialog's Add xpath button now defaults to the SOAP 1.2 Envelope namespace URI if a SOAP 1.2 service is currently open for editing.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@27225 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/assertions/MTOMAssertion/src/main/java/com/l7tech/external/assertions/mtom/console/MtomAssertionPropertiesDialogSupport.java
+++ modules/gateway/assertions/MTOMAssertion/src/main/java/com/l7tech/external/assertions/mtom/console/MtomAssertionPropertiesDialogSupport.java
@@ -2,12 +2,19 @@ package com.l7tech.external.assertions.mtom.console;
 
 import com.l7tech.console.panels.AssertionPropertiesOkCancelSupport;
 import com.l7tech.console.panels.XpathBasedAssertionPropertiesDialog;
+import com.l7tech.console.poleditor.PolicyEditorPanel;
+import com.l7tech.console.tree.EntityWithPolicyNode;
+import com.l7tech.console.util.TopComponents;
 import com.l7tech.console.util.VariablePrefixUtil;
+import com.l7tech.gateway.common.service.PublishedService;
 import com.l7tech.gui.util.DialogDisplayer;
 import com.l7tech.gui.util.Utilities;
+import com.l7tech.objectmodel.Entity;
+import com.l7tech.objectmodel.FindException;
 import com.l7tech.policy.assertion.*;
 import com.l7tech.policy.assertion.annotation.ProcessesResponse;
 import com.l7tech.util.Functions;
+import com.l7tech.xml.soap.SoapVersion;
 import com.l7tech.xml.xpath.XpathExpression;
 import com.l7tech.xml.xpath.XpathUtil;
 
@@ -64,7 +71,7 @@ public abstract class MtomAssertionPropertiesDialogSupport<T extends Assertion> 
                                      final Functions.UnaryVoid<XpathExpression> callback ) {
         final XpathBasedAssertion holder = targetMessageType==TargetMessageType.RESPONSE ?
                 new ResponseXpathBasedAssertion() : new XpathBasedAssertion(){};
-        holder.setXpathExpression( xpathExpression != null ? xpathExpression : holder.createDefaultXpathExpression(true, null) );
+        holder.setXpathExpression( xpathExpression != null ? xpathExpression : holder.createDefaultXpathExpression(true, findServiceSoapVersion()) );
         final XpathBasedAssertionPropertiesDialog ape = new XpathBasedAssertionPropertiesDialog(parent, holder);
         JDialog dlg = ape.getDialog();
         dlg.setTitle(titlePrefix + " - XPath Expression");
@@ -80,6 +87,26 @@ public abstract class MtomAssertionPropertiesDialogSupport<T extends Assertion> 
                     callback.call( pruneNamespaces(holder.getXpathExpression(), new HashMap<String,String>()) );
             }
         });
+    }
+
+    private static SoapVersion findServiceSoapVersion() {
+        SoapVersion ver = null;
+
+        PolicyEditorPanel pep = TopComponents.getInstance().getPolicyEditorPanel();
+        if (pep != null) {
+            EntityWithPolicyNode pn = pep.getPolicyNode();
+            try {
+                Entity got = pn.getEntity();
+                if (got instanceof PublishedService) {
+                    PublishedService service = (PublishedService) got;
+                    ver = service.getSoapVersion();
+                }
+            } catch (FindException e) {
+                /* FALLTHROUGH and return null */
+            }
+        }
+
+        return ver;
     }
 
     //- PRIVATE
