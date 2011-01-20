639a2f02291e27bbdf72f0b3dc162a04ab2e7951
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2011-01-20 22:24:35 +0000

    Removed SsgFaker, FunctionalTest, and handful of old tests that rely on spinning up actual proxy servers and making actual HTTP connections through them.  Moved the meat of the BridgeWsdlProxyTest into a new WsdlUtilTest and converted it from a functional test into a proper unit test.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@27613 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/wsdl/src/test/java/com/l7tech/wsdl/WsdlUtilTest.java
+++ modules/wsdl/src/test/java/com/l7tech/wsdl/WsdlUtilTest.java
@@ -1,158 +1,51 @@
-package com.l7tech.proxy;
+package com.l7tech.wsdl;
 
-import com.l7tech.common.http.GenericHttpException;
-import com.l7tech.common.http.GenericHttpRequestParams;
-import com.l7tech.common.http.SimpleHttpClient;
-import com.l7tech.common.http.prov.jdk.UrlConnectionHttpClient;
-import com.l7tech.util.InetAddressUtil;
 import com.l7tech.common.io.XmlUtil;
 import com.l7tech.test.BugNumber;
-import com.l7tech.util.ResourceUtils;
-import com.l7tech.xml.DomElementCursor;
-import com.l7tech.xml.ElementCursor;
-import com.l7tech.xml.InvalidXpathException;
-import com.l7tech.xml.xpath.*;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import org.xml.sax.SAXException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
-import javax.xml.xpath.XPathExpressionException;
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.logging.Logger;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
- * Test the WSDL proxying feature built into the XML VPN Client (aka SecureSpan Bridge aka SecureSpan Agent aka Client Proxy).
+ *
  */
-public class BridgeWsdlProxyTest {
-    private static final Logger log = Logger.getLogger(BridgeWsdlProxyTest.class.getName());
-
-    private BridgeTestHarness bt;
-
-    @Before
-    public void setUp() throws Exception {
-        ResourceUtils.closeQuietly(bt);
-        bt = new BridgeTestHarness();
-        bt.reset();
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        ResourceUtils.closeQuietly(bt);
-        bt = null;
-    }
-
-    @Test
-    public void testWsilRewriting() throws Exception {
-        bt.ssgFaker.setWsdlProxyResponseBody(SIMPLE_WSIL);
-        bt.ssgFake.setUseSslByDefault(false);
-
-        String bridgeUrl = bt.proxyUrl + bt.ssg0ProxyEndpoint + "/wsil";
-        SimpleHttpClient httpClient = new SimpleHttpClient(new UrlConnectionHttpClient());
-
-        SimpleHttpClient.SimpleHttpResponse response = httpClient.get(new GenericHttpRequestParams(new URL(bridgeUrl)));
-        assertEquals(200, response.getStatus());
-        assertTrue(response.getContentType().isXml());
-
-        log.info("Received result: " + new String(response.getBytes()));
-
-        // Make sure all the URLs got rewritten to point at the VPN client
-        ElementCursor wsil = new DomElementCursor( XmlUtil.stringToDocument(new String(response.getBytes())));
-        CompiledXpath matchDescriptions = new XpathExpression("//*[local-name()='description']").compile();
-        String expectedUrlPrefix = bt.proxyUrl + bt.ssg0ProxyEndpoint + "/wsdl?";
-
-        List<ElementCursor> descriptions = matchNodes(wsil, matchDescriptions);
-        assertTrue(!descriptions.isEmpty());
-        for (ElementCursor description : descriptions) {
-            String loc = description.getAttributeValue("location");
-            assertTrue(InetAddressUtil.isSubUrl(expectedUrlPrefix, loc));
-        }
-    }
-
+public class WsdlUtilTest {
     @Test
     public void testWsdlRewriting() throws Exception {
-        doTestWsdlRewriting(SIMPLE_WSDL, null);
+        doTestWsdlRewriting(SIMPLE_WSDL);
     }
 
     @Test
     @BugNumber(9108)
     public void testWsdlRewritingHttpBindings() throws Exception {
-        doTestWsdlRewriting(REST_WSDL, "http://" + InetAddressUtil.getLocalHostUrlAddress() + ":5555/ssg0/egis/");
+        doTestWsdlRewriting(REST_WSDL);
     }
 
-    private void doTestWsdlRewriting(String wsdlString, String expectedUrlPrefix) throws GenericHttpException, MalformedURLException, SAXException, InvalidXpathException, XPathExpressionException {
-        bt.ssgFaker.setWsdlProxyResponseBody(wsdlString);
-        bt.ssgFake.setUseSslByDefault(false);
+    private void doTestWsdlRewriting(String wsdlString) throws Exception {
+        Document wsdlDoc = XmlUtil.stringAsDocument(wsdlString);
 
-        String bridgeUrl = bt.proxyUrl + bt.ssg0ProxyEndpoint + "/wsdl?serviceoid=22609924";
-        SimpleHttpClient httpClient = new SimpleHttpClient(new UrlConnectionHttpClient());
+        WsdlUtil.rewriteAddressLocations(wsdlDoc, new WsdlUtil.LocationBuilder() {
+            @Override
+            public String buildLocation(Element address) throws MalformedURLException {
+                return "urn:rewritten:1234";
+            }
+        });
 
-        SimpleHttpClient.SimpleHttpResponse response = httpClient.get(new GenericHttpRequestParams(new URL(bridgeUrl)));
-        assertEquals(200, response.getStatus());
-        assertTrue(response.getContentType().isXml());
 
-        log.info("Received result: " + new String(response.getBytes()));
-
-        // Make sure all the URLs got rewritten to point at the VPN client
-        ElementCursor wsil = new DomElementCursor(XmlUtil.stringToDocument(new String(response.getBytes())));
-        CompiledXpath matchDescriptions = new XpathExpression("//*[local-name()='service']/*[local-name()='port']/*[local-name()='address']").compile();
-        if (expectedUrlPrefix == null)
-            expectedUrlPrefix = bt.proxyUrl + bt.ssg0ProxyEndpoint + "/service/";
-
-        List<ElementCursor> descriptions = matchNodes(wsil, matchDescriptions);
-        assertTrue(!descriptions.isEmpty());
-        for (ElementCursor description : descriptions) {
-            String loc = description.getAttributeValue("location");
-            assertTrue("loc should start with " + expectedUrlPrefix + ": " + loc, InetAddressUtil.isSubUrl(expectedUrlPrefix, loc));
+        NodeList nl = wsdlDoc.getElementsByTagName("*");
+        int nll = nl.getLength();
+        for (int i = 0; i < nll; i++) {
+            Element element = (Element) nl.item(i);
+            if ("address".equals(element.getLocalName())) {
+                assertEquals("urn:rewritten:1234", element.getAttribute("location"));
+            }
         }
     }
-
-    public static List<ElementCursor> matchNodes(ElementCursor cursor, CompiledXpath xpath) throws XPathExpressionException {
-        cursor.moveToRoot();
-        XpathResult result = cursor.getXpathResult(xpath, null, true);
-        if (!result.matches()) return Collections.emptyList();
-        XpathResultNodeSet ns = result.getNodeSet();
-        if (ns.isEmpty()) return Collections.emptyList();
-
-        List<ElementCursor> ret = new ArrayList<ElementCursor>();
-        XpathResultIterator it = ns.getIterator();
-        while (it.hasNext()) {
-            ElementCursor ec = it.nextElementAsCursor();
-            assertNotNull(ec);
-            ret.add(ec);
-        }
-        return ret;
-    }
-
-    private static final String SIMPLE_WSIL =
-            "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-            "<?xml-stylesheet type=\"text/xsl\" href=\"/ssg/wsil2xhtml.xml\"?>\n" +
-            "<inspection xmlns=\"http://schemas.xmlsoap.org/ws/2001/10/inspection/\">\n" +
-            "\t<service>\n" +
-            "\t\t<abstract>Warehouse</abstract>\n" +
-            "\t\t<description referencedNamespace=\"http://schemas.xmlsoap.org/wsdl/\" location=\"http://localhost:8080/ssg/wsdl?serviceoid=21299203\"/>\n" +
-            "\t</service>\n" +
-            "\t<service>\n" +
-            "\t\t<abstract>DeadOrAlive</abstract>\n" +
-            "\n" +
-            "\t\t<description referencedNamespace=\"http://schemas.xmlsoap.org/wsdl/\" location=\"http://localhost:8080/ssg/wsdl?serviceoid=22609922\"/>\n" +
-            "\t</service>\n" +
-            "\t<service>\n" +
-            "\t\t<abstract>GeoServe</abstract>\n" +
-            "\t\t<description referencedNamespace=\"http://schemas.xmlsoap.org/wsdl/\" location=\"http://localhost:8080/ssg/wsdl?serviceoid=22609923\"/>\n" +
-            "\t</service>\n" +
-            "\t<service>\n" +
-            "\t\t<abstract>AuthenticateService</abstract>\n" +
-            "\n" +
-            "\t\t<description referencedNamespace=\"http://schemas.xmlsoap.org/wsdl/\" location=\"http://localhost:8080/ssg/wsdl?serviceoid=22609924\"/>\n" +
-            "\t</service>\n" +
-            "</inspection>";
 
     private static final String SIMPLE_WSDL =
             "<definitions xmlns=\"http://schemas.xmlsoap.org/wsdl/\" xmlns:http=\"http://schemas.xmlsoap.org/wsdl/http/\" xmlns:mime=\"http://schemas.xmlsoap.org/wsdl/mime/\" xmlns:s=\"http://www.w3.org/2001/XMLSchema\" xmlns:s0=\"http://webservices.geomonster.com/\" xmlns:soap=\"http://schemas.xmlsoap.org/wsdl/soap/\" xmlns:soapenc=\"http://schemas.xmlsoap.org/soap/encoding/\" xmlns:sourcens0=\"http://webservices.geomonster.com/\" xmlns:tm=\"http://microsoft.com/wsdl/mime/textMatching/\" xmlns:tns=\"http://tempuri.org/\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" name=\"NewService\" targetNamespace=\"http://tempuri.org/\">\n" +
@@ -397,5 +290,4 @@ public class BridgeWsdlProxyTest {
             "        </wsdl:port>\n" +
             "    </wsdl:service>\n" +
             "</wsdl:definitions>";
-
 }
