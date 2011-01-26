a21d274071608e2df069102222de4c40ea80fdab
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2011-01-26 03:19:02 +0000

    Adding additional test cases from Wynne
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@27643 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/utility/src/test/java/com/l7tech/util/BufferPoolByteArrayOutputStreamTest.java
+++ modules/utility/src/test/java/com/l7tech/util/BufferPoolByteArrayOutputStreamTest.java
@@ -5,27 +5,20 @@
 
 package com.l7tech.util;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 /**
  * @author mike
  */
-public class BufferPoolByteArrayOutputStreamTest extends TestCase {
+public class BufferPoolByteArrayOutputStreamTest {
 
-    public BufferPoolByteArrayOutputStreamTest(String name) {
-        super(name);
-    }
-
-    public static Test suite() {
-        return new TestSuite(BufferPoolByteArrayOutputStreamTest.class);
-    }
-
-    public static void main(String[] args) {
-        junit.textui.TestRunner.run(suite());
-    }
-
+    @Test
     public void testOutputStream() throws Exception {
         final String STR = "blah blah blah asdfasdfas ";
         StringBuffer sb = new StringBuffer();
@@ -35,5 +28,46 @@ public class BufferPoolByteArrayOutputStreamTest extends TestCase {
             sb.append(STR);
         }
         assertEquals(bo.toString(), sb.toString());
+    }
+
+    @Test
+    public void testWrite() throws IOException {
+            String testString = "Test string";
+            String longString = "This is a really really really really long string.";
+            BufferPoolByteArrayOutputStream stream = new BufferPoolByteArrayOutputStream();
+
+            stream.write(testString.getBytes());
+
+            assertEquals("Matches original", testString,stream.toString());
+
+            stream.write(testString.getBytes(),5,4);
+            assertEquals("Append part of original string", testString+testString.substring(5,9),stream.toString() );
+
+            stream.reset();
+            assertTrue("String empty after reset",stream.toString().isEmpty());
+            assertTrue("buffer empty after rest",stream.size()==0);
+
+            try{
+                new BufferPoolByteArrayOutputStream(-1);
+                fail("Illegal argument not thrown");
+            }catch(IllegalArgumentException ex){
+                // Ok
+            }
+
+            stream = new BufferPoolByteArrayOutputStream(10);
+            stream.write(testString.getBytes());
+            stream.write(longString.getBytes());
+            assertEquals("Test expanding internal buffer",testString+longString,stream.toString() );
+
+
+            String streamString = new String(stream.toByteArray());
+            System.out.println("expected length: " + (testString+longString).length());
+            assertEquals("string lengths match", (testString+longString).length(), streamString.length());
+            assertEquals("test to string after expanding internal buffer",testString+longString,streamString );
+
+            for(int i = 0 ; i < 100; ++i)
+                stream.write('?');
+            stream.writeTo(System.out);
+            assertEquals("Test append byte",testString.length()+longString.length()+100,stream.size() );
     }
 }
