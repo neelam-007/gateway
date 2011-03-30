480da02872a2fa5eac8fe3caa76438cfb6790cb2
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2011-03-30 23:52:02 +0000

    The RSA Cipher.getInstance() transform names can now be overridden when using the Generic JCE provider engine.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@28037 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/policy/src/main/java/com/l7tech/security/prov/generic/GenericJceProviderEngine.java
+++ modules/policy/src/main/java/com/l7tech/security/prov/generic/GenericJceProviderEngine.java
@@ -1,11 +1,22 @@
 package com.l7tech.security.prov.generic;
 
 import com.l7tech.security.prov.JceProvider;
+import com.l7tech.util.SyspropUtil;
 
 /**
  * "Generic" JceProviderEngine.  Assumes that the underlying JDK is already configured.
  */
 public class GenericJceProviderEngine extends JceProvider {
+    private final String rsaNoPaddingCipherName;
+    private final String rsaOaepPaddingCipherName;
+    private final String rsaPkcs1PaddingCipherName;
+
+    public GenericJceProviderEngine() {
+        rsaNoPaddingCipherName = SyspropUtil.getString("com.l7tech.common.security.prov.generic.rsaNoPaddingCipherName", "RSA/NONE/NoPadding");
+        rsaOaepPaddingCipherName = SyspropUtil.getString("com.l7tech.common.security.prov.generic.rsaOaepPaddingCipherName", "RSA/ECB/OAEPPadding");
+        rsaPkcs1PaddingCipherName = SyspropUtil.getString("com.l7tech.common.security.prov.generic.rsaPkcs1PaddingCipherName", "RSA/ECB/PKCS1Padding");
+    }
+
     @Override
     public String getDisplayName() {
         return "Generic";
@@ -13,16 +24,16 @@ public class GenericJceProviderEngine extends JceProvider {
 
     @Override
     public String getRsaNoPaddingCipherName() {
-        return "RSA/NONE/NoPadding";
+        return rsaNoPaddingCipherName;
     }
 
     @Override
     public String getRsaOaepPaddingCipherName() {
-        return "RSA/ECB/OAEPPadding";
+        return rsaOaepPaddingCipherName;
     }
 
     @Override
     public String getRsaPkcs1PaddingCipherName() {
-        return "RSA/ECB/PKCS1Padding";
+        return rsaPkcs1PaddingCipherName;
     }
 }
