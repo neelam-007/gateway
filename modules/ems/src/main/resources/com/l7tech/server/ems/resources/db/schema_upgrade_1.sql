83d093894a189ccf9247c2d0d33052a08b5f25d6
steve <steve@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2011-03-31 18:24:12 +0000

    Remove password enforcement and policy manager beans from the ESM. ESM users are now enabled in the upgrade script.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@28049 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/ems/src/main/resources/com/l7tech/server/ems/resources/db/schema_upgrade_1.sql
+++ modules/ems/src/main/resources/com/l7tech/server/ems/resources/db/schema_upgrade_1.sql
@@ -2,7 +2,7 @@
 -- Schema upgrade script to update database to schema version 1
 -- --------------------------------------------------------------------------
 --
---
+-- Note that some schema updates occur automatically.
 --
 
 --
@@ -19,4 +19,9 @@ insert into schema_version values ( 1 );
 -- Changes to support offline policy migration
 --
 alter table migration alter column target_cluster_oid null;
+
+--
+-- Internal user changes
+--
+update internal_user set enabled = 1;
 
