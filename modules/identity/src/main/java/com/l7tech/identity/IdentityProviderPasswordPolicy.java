de18fddc3d27368589c0258af5be07db34dde8cf
wlui <wlui@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2011-04-08 00:10:46 +0000

    SSM-3207: fix password policy description words
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@28114 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/identity/src/main/java/com/l7tech/identity/IdentityProviderPasswordPolicy.java
+++ modules/identity/src/main/java/com/l7tech/identity/IdentityProviderPasswordPolicy.java
@@ -145,17 +145,17 @@ public class IdentityProviderPasswordPolicy extends PersistentEntityImp {
 
         StringBuilder desc = new StringBuilder();
         desc.append("<html><body>Password changes must comply all of the following:<ul>");
-        if (getIntegerProperty(MIN_PASSWORD_LENGTH)>0){desc.append("<li>"); desc.append(MessageFormat.format("at least {0} characters",(Integer)properties.get(MIN_PASSWORD_LENGTH))); desc.append("</li>");}
-        if (getIntegerProperty(MAX_PASSWORD_LENGTH)>0){desc.append("<li>"); desc.append(MessageFormat.format("less than {0} characters",(Integer)properties.get(MAX_PASSWORD_LENGTH))); desc.append("</li>");}
-        if (getIntegerProperty(UPPER_MIN)>0){desc.append("<li>"); desc.append(MessageFormat.format("at least {0} upper case characters",(Integer)properties.get(UPPER_MIN))); desc.append("</li>");}
-        if (getIntegerProperty(LOWER_MIN)>0){desc.append("<li>"); desc.append(MessageFormat.format("at least {0} lower case characters",(Integer)properties.get(LOWER_MIN))); desc.append("</li>");}
-        if (getIntegerProperty(NUMBER_MIN)>0){desc.append("<li>"); desc.append(MessageFormat.format("at least {0} numbers",(Integer)properties.get(NUMBER_MIN))); desc.append("</li>");}
-        if (getIntegerProperty(SYMBOL_MIN)>0){desc.append("<li>"); desc.append(MessageFormat.format("at least {0} special characters",(Integer)properties.get(SYMBOL_MIN))); desc.append("</li>");}
-        if (getIntegerProperty(NON_NUMERIC_MIN)>0){desc.append("<li>"); desc.append(MessageFormat.format("at least {0} non-numeric characters",(Integer)properties.get(NON_NUMERIC_MIN))); desc.append("</li>");}
-        if (getIntegerProperty(CHARACTER_DIFF_MIN)>0){desc.append("<li>"); desc.append(MessageFormat.format("at least {0} unique characters",(Integer)properties.get(CHARACTER_DIFF_MIN))); desc.append("</li>");}
+        if (getIntegerProperty(MIN_PASSWORD_LENGTH)>0){desc.append("<li>"); desc.append(MessageFormat.format("at least {0} characters",getIntegerProperty(MIN_PASSWORD_LENGTH))); desc.append("</li>");}
+        if (getIntegerProperty(MAX_PASSWORD_LENGTH)>0){desc.append("<li>"); desc.append(MessageFormat.format("less than {0} characters",getIntegerProperty(MAX_PASSWORD_LENGTH))); desc.append("</li>");}
+        if (getIntegerProperty(UPPER_MIN)>0){desc.append("<li>"); desc.append(MessageFormat.format("at least {0} upper case characters",getIntegerProperty(UPPER_MIN))); desc.append("</li>");}
+        if (getIntegerProperty(LOWER_MIN)>0){desc.append("<li>"); desc.append(MessageFormat.format("at least {0} lower case characters",getIntegerProperty(LOWER_MIN))); desc.append("</li>");}
+        if (getIntegerProperty(NUMBER_MIN)>0){desc.append("<li>"); desc.append(MessageFormat.format("at least {0} numbers",getIntegerProperty(NUMBER_MIN))); desc.append("</li>");}
+        if (getIntegerProperty(SYMBOL_MIN)>0){desc.append("<li>"); desc.append(MessageFormat.format("at least {0} special characters",getIntegerProperty(SYMBOL_MIN))); desc.append("</li>");}
+        if (getIntegerProperty(NON_NUMERIC_MIN)>0){desc.append("<li>"); desc.append(MessageFormat.format("at least {0} non-numeric characters",getIntegerProperty(NON_NUMERIC_MIN))); desc.append("</li>");}
+        if (getIntegerProperty(CHARACTER_DIFF_MIN)>0){desc.append("<li>"); desc.append(MessageFormat.format("at least {0} unique characters",getIntegerProperty(CHARACTER_DIFF_MIN))); desc.append("</li>");}
         if (getBooleanProperty(NO_REPEAT_CHARS)){desc.append("<li>"); desc.append("no consecutive repeating characters"); desc.append("</li>");}
-        if (getIntegerProperty(REPEAT_FREQUENCY)>0){desc.append("<li>"); desc.append("passwords are not reused within {0} password changes"); desc.append("</li>");}
-        if (getBooleanProperty(ALLOWABLE_CHANGES)){desc.append("<li>"); desc.append("allow One Password Change Per 24 hours"); desc.append("</li>");}
+        if (getIntegerProperty(REPEAT_FREQUENCY)>0){desc.append("<li>"); desc.append(MessageFormat.format("passwords are not reused within {0} password changes",getIntegerProperty(REPEAT_FREQUENCY))); desc.append("</li>");}
+        if (getBooleanProperty(ALLOWABLE_CHANGES)){desc.append("<li>"); desc.append("allow one password change per 24 hours"); desc.append("</li>");}
 
         desc.append("</ul> </body></html>");
         return desc.toString();
