7eea0d9449ad5c603ad55d583011bfadeadcff4f
steve <steve@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2011-04-12 21:04:55 +0000

    Remove changes that are no longer required.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@28178 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/policy/src/main/java/com/l7tech/policy/assertion/credential/CredentialFormat.java
+++ modules/policy/src/main/java/com/l7tech/policy/assertion/credential/CredentialFormat.java
@@ -1,21 +1,9 @@
-/*
- * Copyright (C) 2003 Layer 7 Technologies Inc.
- *
- * $Id$
- */
-
 package com.l7tech.policy.assertion.credential;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * Typesafe enum. Not currently persistent.
  *
  * @author alex
- * @version $Revision$
  */
 public class CredentialFormat {
     /**
@@ -52,12 +40,6 @@ public class CredentialFormat {
      * An opaque token such as a single sign on session cookie.
      */
     public static final CredentialFormat OPAQUETOKEN = new CredentialFormat("Opaque Token");
-
-    public static final Set<CredentialFormat> POLICY_MANAGER_ADMINISTRATION =
-            Collections.unmodifiableSet(new HashSet<CredentialFormat>(Arrays.asList(CLEARTEXT, CLIENTCERT)));
-
-    public static final Set<CredentialFormat> ESM_ADMINISTRATION =
-            Collections.unmodifiableSet(new HashSet<CredentialFormat>(Arrays.asList(CLEARTEXT)));
 
     public String toString() {
         return "<CredentialFormat name='" + _name + "'/>";
