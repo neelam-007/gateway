a5afda44fcd3611cc84b5bf01b98cbce6f650ed4
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2011-04-13 21:44:30 +0000

    SSM no longer logs NPE when opening private keys dialog when not all special key roles are assigned.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@28208 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/console/src/main/java/com/l7tech/console/util/DefaultAliasTracker.java
+++ modules/gateway/console/src/main/java/com/l7tech/console/util/DefaultAliasTracker.java
@@ -1,17 +1,14 @@
 package com.l7tech.console.util;
 
 import com.l7tech.gateway.common.audit.LogonEvent;
-import com.l7tech.gateway.common.cluster.ClusterProperty;
-import com.l7tech.gateway.common.cluster.ClusterStatusAdmin;
 import com.l7tech.gateway.common.security.TrustedCertAdmin;
-import com.l7tech.objectmodel.FindException;
+import com.l7tech.gateway.common.security.keystore.SsgKeyEntry;
 import com.l7tech.util.ExceptionUtils;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -24,11 +21,9 @@ public class DefaultAliasTracker implements ApplicationListener {
     public static final String CLUSTER_PROP_DEFAULT_CA = "keyStore.defaultCa.alias";
     public static final String CLUSTER_PROP_AUDIT_VIEWER = "keyStore.auditViewer.alias";
 
-    private static final Pattern KEYSTORE_ID_AND_ALIAS_PATTERN = Pattern.compile("^(-?\\d+):(.*)$");
-
-    private String defaultSslAlias = null;
-    private String defaultCaAlias = null;
-    private String defaultAuditViewerAlias = null;
+    private SsgKeyEntry defaultSslAlias = null;
+    private SsgKeyEntry defaultCaAlias = null;
+    private SsgKeyEntry defaultAuditViewerAlias = null;
     private boolean defaultSslMutability;
     private boolean defaultCaMutability;
     private boolean defaultAuditViewerMutability;
@@ -38,7 +33,7 @@ public class DefaultAliasTracker implements ApplicationListener {
      */
     public String getDefaultSslAlias() {
         checkUpdate();
-        return defaultSslAlias;
+        return defaultSslAlias == null ? null : defaultSslAlias.getAlias();
     }
 
     /**
@@ -46,12 +41,12 @@ public class DefaultAliasTracker implements ApplicationListener {
      */
     public String getDefaultCaAlias() {
         checkUpdate();
-        return defaultCaAlias;
+        return defaultCaAlias == null ? null : defaultCaAlias.getAlias();
     }
 
     public String getDefaultAuditViewerAlias() {
         checkUpdate();
-        return defaultAuditViewerAlias;
+        return defaultAuditViewerAlias == null ? null : defaultAuditViewerAlias.getAlias();
     }
 
     /**
@@ -83,54 +78,25 @@ public class DefaultAliasTracker implements ApplicationListener {
             updateDefaultAliases();
     }
 
-    private String getAlias(String clusterPropertyName, String defaultVal) throws FindException {
-        final ClusterStatusAdmin csa = Registry.getDefault().getClusterStatusAdmin();
-        ClusterProperty prop = csa.findPropertyByName(clusterPropertyName);
-        if (prop == null)
-            return defaultVal;
-        String value = prop.getValue();
-        if (value == null)
-            return defaultVal;
-        Matcher matcher = KEYSTORE_ID_AND_ALIAS_PATTERN.matcher(value);
-        if (!matcher.matches())
-            return value;
-        return matcher.group(2);
-    }
-
     private void updateDefaultAliases() {
         defaultSslMutability = Registry.getDefault().getTrustedCertManager().isDefaultKeyMutable(TrustedCertAdmin.SpecialKeyType.SSL);
         defaultCaMutability = Registry.getDefault().getTrustedCertManager().isDefaultKeyMutable(TrustedCertAdmin.SpecialKeyType.CA);
         defaultAuditViewerMutability = Registry.getDefault().getTrustedCertManager().isDefaultKeyMutable(TrustedCertAdmin.SpecialKeyType.AUDIT_VIEWER);
 
         try {
-            defaultSslAlias = Registry.getDefault().getTrustedCertManager().findDefaultKey(TrustedCertAdmin.SpecialKeyType.SSL).getAlias();
+            defaultSslAlias = Registry.getDefault().getTrustedCertManager().findDefaultKey(TrustedCertAdmin.SpecialKeyType.SSL);
         } catch (Exception e) {
             logger.log(Level.WARNING, "Unable to determine default SSL alias using findDefaultKey: " + ExceptionUtils.getMessage(e), e);
-            try {
-                defaultSslAlias = getAlias(CLUSTER_PROP_DEFAULT_SSL, "SSL");
-            } catch (FindException e1) {
-                logger.log(Level.WARNING, "Unable to determine default SSL alias using cluster property: " + ExceptionUtils.getMessage(e), e);
-            }
         }
         try {
-            defaultCaAlias = Registry.getDefault().getTrustedCertManager().findDefaultKey(TrustedCertAdmin.SpecialKeyType.CA).getAlias();
+            defaultCaAlias = Registry.getDefault().getTrustedCertManager().findDefaultKey(TrustedCertAdmin.SpecialKeyType.CA);
         } catch (Exception e) {
             logger.log(Level.WARNING, "Unable to determine default CA alias using findDefaultKey: " + ExceptionUtils.getMessage(e), e);
-            try {
-                defaultCaAlias = getAlias(CLUSTER_PROP_DEFAULT_CA, null);
-            } catch (FindException e1) {
-                logger.log(Level.WARNING, "Unable to determine default CA alias using cluster property: " + ExceptionUtils.getMessage(e), e);
-            }
         }
         try {
-            defaultAuditViewerAlias = Registry.getDefault().getTrustedCertManager().findDefaultKey(TrustedCertAdmin.SpecialKeyType.AUDIT_VIEWER).getAlias();
+            defaultAuditViewerAlias = Registry.getDefault().getTrustedCertManager().findDefaultKey(TrustedCertAdmin.SpecialKeyType.AUDIT_VIEWER);
         } catch (Exception e) {
             logger.log(Level.WARNING, "Unable to determine default audit viewer alias using findDefaultKey: " + ExceptionUtils.getMessage(e), e);
-            try {
-                defaultAuditViewerAlias = getAlias(CLUSTER_PROP_AUDIT_VIEWER, null);
-            } catch (FindException e1) {
-                logger.log(Level.WARNING, "Unable to determine default audit viewer alias using cluster property: " + ExceptionUtils.getMessage(e), e);
-            }
         }
     }
 
