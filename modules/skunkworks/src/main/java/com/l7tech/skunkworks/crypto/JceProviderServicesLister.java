ad404eeaefb620ed59546e5d863df611bacf6ca7
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2011-05-06 00:56:35 +0000

    Improve formatting of output for JceProviderServicesLister
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@28457 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/skunkworks/src/main/java/com/l7tech/skunkworks/crypto/JceProviderServicesLister.java
+++ modules/skunkworks/src/main/java/com/l7tech/skunkworks/crypto/JceProviderServicesLister.java
@@ -2,7 +2,7 @@ package com.l7tech.skunkworks.crypto;
 
 import java.security.Provider;
 import java.security.Security;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Lists JCE services.
@@ -11,9 +11,17 @@ public class JceProviderServicesLister {
     public static void main(String[] args) {
         Provider[] provs = Security.getProviders();
         for (Provider prov : provs) {
-            Set<Provider.Service> servs = prov.getServices();
+            System.out.println("\nProvider: " + prov.getName() + " (" + prov + ")");
+            List<Provider.Service> servs = new ArrayList<Provider.Service>(prov.getServices());
+            Collections.sort(servs, new Comparator<Provider.Service>() {
+                @Override
+                public int compare(Provider.Service a, Provider.Service b) {
+                    int r = a.getType().compareTo(b.getType());
+                    return r != 0 ? r : a.getAlgorithm().compareTo(b.getAlgorithm());
+                }
+            });
             for (Provider.Service serv : servs) {
-                System.out.println(serv.getType() + "." + serv.getAlgorithm() + " \t\t" + serv.getClassName());
+                System.out.printf("   %-28s\t%-37s\t%-80s\n", serv.getType(), serv.getAlgorithm(), serv.getClassName());
             }
         }
     }
