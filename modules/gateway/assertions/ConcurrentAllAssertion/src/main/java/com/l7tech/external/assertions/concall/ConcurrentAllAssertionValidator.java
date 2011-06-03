52de3725202d419f71fac81bffecee004e56b4e6
ghuang <ghuang@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2011-06-03 23:50:58 +0000

    SSM-3338: Fixed by displaying a validation warning if a 'Run All Assertions Concurrently' assertion is added into the audit viewer policy.  The warning message is "The concurrent all assertion is included in the audit viewer policy. The audit viewer private key would not be available within this assertion."
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@28690 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/assertions/ConcurrentAllAssertion/src/main/java/com/l7tech/external/assertions/concall/ConcurrentAllAssertionValidator.java
+++ modules/gateway/assertions/ConcurrentAllAssertion/src/main/java/com/l7tech/external/assertions/concall/ConcurrentAllAssertionValidator.java
@@ -1,17 +1,25 @@
 package com.l7tech.external.assertions.concall;
 
+import com.l7tech.policy.AssertionPath;
+import com.l7tech.policy.PolicyType;
 import com.l7tech.policy.PolicyUtil;
+import com.l7tech.policy.PolicyValidatorResult;
 import com.l7tech.policy.assertion.Assertion;
 import com.l7tech.policy.assertion.RoutingAssertion;
 import com.l7tech.policy.validator.AssertionValidatorSupport;
+import com.l7tech.policy.validator.PolicyValidationContext;
 import com.l7tech.util.Functions;
 
 /**
  * Policy validator for ConcurrentAllAssertion.
  */
+@SuppressWarnings({"UnusedDeclaration"})
 public class ConcurrentAllAssertionValidator extends AssertionValidatorSupport<ConcurrentAllAssertion> {
+    private ConcurrentAllAssertion assertion;
+
     public ConcurrentAllAssertionValidator(ConcurrentAllAssertion assertion) {
         super(assertion);
+        this.assertion = assertion;
 
         // Check for children that don't belong within a ConcurrentAllAssertion
         PolicyUtil.visitDescendantsAndSelf(assertion, new Functions.UnaryVoid<com.l7tech.policy.assertion.Assertion>() {
@@ -34,5 +42,16 @@ public class ConcurrentAllAssertionValidator extends AssertionValidatorSupport<C
                 }
             }
         });
+    }
+
+    @Override
+    public void validate(AssertionPath path, PolicyValidationContext pvc, PolicyValidatorResult result) {
+        super.validate(path, pvc, result);
+
+        //If the concurrent all assertion is found in the audit viewer policy, a validator warning will be generated.
+        if (PolicyType.TAG_AUDIT_VIEWER.equals(pvc.getPolicyInternalTag())) {
+            final String warningMessage = "The concurrent all assertion is included in the audit viewer policy. The audit viewer private key would not be available within this assertion.";
+            result.addWarning(new PolicyValidatorResult.Warning(assertion, warningMessage, null));
+        }
     }
 }
