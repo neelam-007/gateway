f8ac9a90a6a83ab284d7eb949f887138cbdf4663
steve <steve@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2011-06-16 19:38:44 +0000

    Versions updated to 6.2.0 (Gateway) and 1.7.0 (ESM)
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@28784 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- etc/db/mysql/ssg.sql
+++ etc/db/mysql/ssg.sql
@@ -1696,6 +1696,6 @@ CREATE TABLE ssg_version (
    current_version char(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8;
 
-INSERT INTO ssg_version (current_version) VALUES ('6.1.0');
+INSERT INTO ssg_version (current_version) VALUES ('6.2.0');
 
 SET FOREIGN_KEY_CHECKS = 1;
