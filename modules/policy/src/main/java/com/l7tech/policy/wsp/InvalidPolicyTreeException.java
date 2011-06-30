16de2c9ebedbf23959b6f7e46c48904cd76b5ed0
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2011-06-30 02:19:27 +0000

    Making InvalidPolicyTreeException public so it can be thrown from modular type mappings.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@28901 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/policy/src/main/java/com/l7tech/policy/wsp/InvalidPolicyTreeException.java
+++ modules/policy/src/main/java/com/l7tech/policy/wsp/InvalidPolicyTreeException.java
@@ -10,7 +10,7 @@ package com.l7tech.policy.wsp;
  * Exception thrown if a policy tree cannot be serialized to XML for reasons related to the content
  * of the policy tree.
  */
-class InvalidPolicyTreeException extends IllegalArgumentException {
+public class InvalidPolicyTreeException extends IllegalArgumentException {
     public InvalidPolicyTreeException() {
     }
 
