109a917477221b0b8a646d4a4e38978e380b3030
steve <steve@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2011-08-03 19:36:31 +0000

    Remove @ProcessesRequest from MessageTargetable assertion
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@28994 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/assertions/JsonTransformationAssertion/src/main/java/com/l7tech/external/assertions/jsontransformation/JsonTransformationAssertion.java
+++ modules/gateway/assertions/JsonTransformationAssertion/src/main/java/com/l7tech/external/assertions/jsontransformation/JsonTransformationAssertion.java
@@ -2,7 +2,6 @@ package com.l7tech.external.assertions.jsontransformation;
 
 import com.l7tech.external.assertions.jsontransformation.server.JsonTransformationAdminImpl;
 import com.l7tech.policy.assertion.*;
-import com.l7tech.policy.assertion.annotation.ProcessesRequest;
 import com.l7tech.policy.wsp.Java5EnumTypeMapping;
 import com.l7tech.policy.wsp.SimpleTypeMappingFinder;
 import com.l7tech.policy.wsp.TypeMapping;
@@ -11,18 +10,14 @@ import org.springframework.context.ApplicationContext;
 
 import java.util.*;
 
+public class JsonTransformationAssertion extends MessageTargetableAssertion {
 
-@ProcessesRequest
-public class JsonTransformationAssertion  extends MessageTargetableAssertion {
-	
-	static public enum Transformation {
+    public enum Transformation {
         XML_to_JSON, JSON_to_XML
     }
 
     private String rootTagString;
     private MessageTargetableSupport destinationMessageTarget;
-
-
     private Transformation transform = JsonTransformationAssertion.Transformation.XML_to_JSON;
 
     public JsonTransformationAssertion() {
@@ -30,13 +25,13 @@ public class JsonTransformationAssertion  extends MessageTargetableAssertion {
         destinationMessageTarget = new MessageTargetableSupport(TargetMessageType.REQUEST, true);
     }
 
-	public void setRootTagString(String rootTagString) {
-		this.rootTagString = rootTagString;
-	}
+    public void setRootTagString(String rootTagString) {
+        this.rootTagString = rootTagString;
+    }
 
-	public String getRootTagString() {
-		return rootTagString;
-	}
+    public String getRootTagString() {
+        return rootTagString;
+    }
 
     public MessageTargetableSupport getDestinationMessageTarget() {
         return destinationMessageTarget;
@@ -71,18 +66,11 @@ public class JsonTransformationAssertion  extends MessageTargetableAssertion {
     //
     private static final String META_INITIALIZED = JsonTransformationAssertion.class.getName() + ".metadataInitialized";
 
+    @Override
     public AssertionMetadata meta() {
         DefaultAssertionMetadata meta = super.defaultMeta();
         if (Boolean.TRUE.equals(meta.get(META_INITIALIZED)))
             return meta;
-
-        // Cluster properties used by this assertion
-        Map<String, String[]> props = new HashMap<String, String[]>();
-        //props.put(NAME, new String[] {
-        //        DESCRIPTION,
-        //        DEFAULT
-        //});
-        meta.put(AssertionMetadata.CLUSTER_PROPERTIES, props);
 
         // Set description for GUI
         meta.put(AssertionMetadata.SHORT_NAME, "Apply JSON Transformation");
