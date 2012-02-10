ae31849c170559021b8a1984ab4b76e433811d78
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2012-02-10 01:58:04 +0000

    Add work-around for XMLEncoder classloader problem that may have been fixed in Java 7
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@30707 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/server/src/main/java/com/l7tech/server/entity/GenericEntityManagerImpl.java
+++ modules/gateway/server/src/main/java/com/l7tech/server/entity/GenericEntityManagerImpl.java
@@ -499,6 +499,8 @@ public class GenericEntityManagerImpl extends HibernateEntityManager<GenericEnti
     static void regenerateValueXml(GenericEntity that) {
         PoolByteArrayOutputStream baos = new PoolByteArrayOutputStream(1024);
         String xml = that.getValueXml();
+        ClassLoader oldContext = Thread.currentThread().getContextClassLoader();
+        Thread.currentThread().setContextClassLoader(that.getClass().getClassLoader());
         try {
             that.setValueXml(""); // set to empty while serializing to prevent including the XML in the XML
             XMLEncoder encoder = new XMLEncoder(new NonCloseableOutputStream(baos));
@@ -508,6 +510,7 @@ public class GenericEntityManagerImpl extends HibernateEntityManager<GenericEnti
         } finally {
             ResourceUtils.closeQuietly(baos);
             that.setValueXml(xml);
+            Thread.currentThread().setContextClassLoader(oldContext);
         }
     }
 
