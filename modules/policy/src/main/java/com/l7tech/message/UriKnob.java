10435b87359e42ac8726f4a9a1acaab3385d4168
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2012-03-02 18:58:27 +0000

    UriKnob should extend MessageKnob
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@30980 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/policy/src/main/java/com/l7tech/message/UriKnob.java
+++ modules/policy/src/main/java/com/l7tech/message/UriKnob.java
@@ -3,7 +3,7 @@ package com.l7tech.message;
 /**
  * Interface extended by message knobs belonging to transports that provide incoming request URI path information.
  */
-public interface UriKnob {
+public interface UriKnob extends MessageKnob {
     /**
      * URI part of the URL for this request (e.g. /ssg/soap). Never null or empty.
      *
