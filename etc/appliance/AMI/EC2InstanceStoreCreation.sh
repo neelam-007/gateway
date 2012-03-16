e78a49530ee48f7ffcfa39a310eb1346ede5b0fe
rmoshfeghi <rmoshfeghi@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2012-03-16 00:17:20 +0000

    Scripts to create AMI images.  Will work for both 32 and 64 bit images.  Tested with 6.2 build
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@31052 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ etc/appliance/AMI/EC2InstanceStoreCreation.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+
+#global variables
+BUILD_DIR=builddir
+MANIFEST_DIR=manifest
+USAGE_MESSAGE='Usage: EC2InstanceStoreCreation.sh <build directory> <buildzip> <xen ec2 tgz file> <sudo package>'
+RCLOCAL=rc.local
+GET_CREDS=get-credentials.sh
+
+#command line parameters
+BUILD_DIR=$1
+if [ Z${BUILD_DIR} == Z ]; then
+  echo ${USAGE_MESSAGE}  
+  exit 1
+fi
+
+BUILD_ZIP=$2
+if [ Z${BUILD_ZIP} == Z ]; then
+  echo ${USAGE_MESSAGE} 
+  exit 1
+fi
+
+EC2_XEN=$3
+if [ Z${EC2_XEN} == Z ]; then
+  echo ${USAGE_MESSAGE} 
+  exit 1
+fi
+
+SUDO_PACKAGE=$4
+if [ Z${SUDO_PACKAGE} == Z ]; then
+  echo ${USAGE_MESSAGE} 
+  exit 1
+fi
+
+if [ ! -r ./${RCLOCAL} ]; then
+  echo "${RCLOCAL} needs to exist in the same folder as ${0}"
+  exit 2
+fi
+
+if [ ! -r ./${GET_CREDS} ]; then
+  echo "${GET_CREDS} needs to exist in the same folder as ${0}"
+  exit 3
+fi
+
+if [ -d ./${BUILD_DIR} -o -f ./${BUILD_DIR} ]; then
+  echo "${BUILD_DIR} already exists in this folder.  Please pick another name"
+  exit 4
+fi
+
+if [ ! -r ${BUILD_ZIP} ]; then
+  echo "${BUILD_ZIP} is not readable.  Plese provide a proper file."
+  exit 5
+fi
+
+if [ ! -r ${EC2_XEN} ]; then
+  echo "${EC2_XEN} is not readable.  Plese provide a proper file."
+  exit 6
+fi
+
+if [ ! -r ${SUDO_PACKAGE} ]; then
+  echo "${SUDO_PACKAGE} is not readable.  Plese provide a proper file."
+  exit 7
+fi
+
+# setup initial filesystem
+mkdir -p ./${BUILD_DIR}
+cd ./${BUILD_DIR}
+mkdir -p lost+found
+chmod 700 lost+found
+mkdir proc
+mkdir sys
+mkdir tmp
+chmod 1777 tmp
+tar xzf ../${BUILD_ZIP}
+
+#setup Amazon specific files
+tar xzf ../${EC2_XEN}  
+cp ../${SUDO_PACKAGE} .
+cp ../${RCLOCAL} ./etc/rc.d/
+cp ../${GET_CREDS} ./usr/local/sbin/
+chmod u+x ./usr/local/sbin/${GET_CREDS}
+
+# modify build as chroot
+mount /proc ./proc -t proc
+cat > ./EC2InstanceStoreCreation_chroot.sh <<EOF
+#!/bin/bash
+
+echo "hwcap 0 nosegneg" > /etc/ld.so.conf.d/xen.conf
+ldconfig -v
+
+echo >/etc/sysconfig/network-scripts/ifcfg-eth0
+echo DEVICE=eth0 >> /etc/sysconfig/network-scripts/ifcfg-eth0
+echo ONBOOT=yes >> /etc/sysconfig/network-scripts/ifcfg-eth0
+echo BOOTPROTO=dhcp >> /etc/sysconfig/network-scripts/ifcfg-eth0
+
+sed -i -e 's/s0:2345:respawn:\/sbin\/agetty -L 9600 ttyS0 vt100/#s0:2345:respawn:\/sbin\/agetty -L 9600 ttyS0 vt100/' "/etc/inittab"
+sed -i -e 's/s1:2345:respawn:\/sbin\/agetty -L 9600 ttyS1 vt100/#s1:2345:respawn:\/sbin\/agetty -L 9600 ttyS1 vt100/' "/etc/inittab"
+
+chkconfig kudzu off
+
+chage -E -1 -I -1 -m 0 -M -1 root
+chage -E -1 -I -1 -m 0 -M -1 ssgconfig
+
+sed -i -e 's/PermitRootLogin no/PermitRootLogin yes/' "/etc/ssh/sshd_config"
+sed -i -e 's/#RSAAuthentication yes/RSAAuthentication yes/' "/etc/ssh/sshd_config"
+sed -i -e 's/PasswordAuthentication yes/PasswordAuthentication no/' "/etc/ssh/sshd_config"
+
+echo 'root' >> /etc/ssh/ssh_allowed_users
+
+rpm -Uvh --oldpackage /${SUDO_PACKAGE}
+rm -f /${SUDO_PACKAGE}
+
+touch /root/firstrun
+chage -d `date +%F` -m 0 -M -1 ssgconfig
+
+umount /proc
+exit
+EOF
+
+chmod 700 ./EC2InstanceStoreCreation_chroot.sh
+chroot ../${BUILD_DIR}
+
+exit 0
