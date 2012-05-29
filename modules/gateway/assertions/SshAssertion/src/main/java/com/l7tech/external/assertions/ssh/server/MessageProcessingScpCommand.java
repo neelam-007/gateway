1309b194a3ca8e8f4b36d32ac7204c957e050227
kdiep <kdiep@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2012-05-29 22:34:22 +0000

    SSG-5743: handle the 'T' protocol header to prevent the underlying scp command to hang indefinately.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@31679 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/assertions/SshAssertion/src/main/java/com/l7tech/external/assertions/ssh/server/MessageProcessingScpCommand.java
+++ modules/gateway/assertions/SshAssertion/src/main/java/com/l7tech/external/assertions/ssh/server/MessageProcessingScpCommand.java
@@ -18,6 +18,7 @@ import com.l7tech.util.ExceptionUtils;
 import com.l7tech.util.ResourceUtils;
 import com.l7tech.util.TruncatingInputStream;
 import org.apache.commons.lang.StringUtils;
+import org.apache.sshd.common.util.DirectoryScanner;
 import org.apache.sshd.server.SessionAware;
 import org.apache.sshd.server.SshFile;
 import org.apache.sshd.server.command.ScpCommand;
@@ -165,5 +166,124 @@ class MessageProcessingScpCommand extends ScpCommand implements SessionAware {
         }
 
         return success;
+    }
+
+    @Override
+    public void run() {
+        int exitValue = OK;
+        String exitMessage = null;
+        try {
+            if (optT)
+            {
+                ack();
+
+                for (; ;)
+                {
+                    String line;
+                    boolean isDir = false;
+                    int c = readAck(true);
+                    switch (c)
+                    {
+                        case -1:
+                            return;
+                        case 'D':
+                            isDir = true;
+                        case 'C':
+                            line = ((char) c) + readLine();
+                            break;
+                        // apache's ScpCommand class never handled T
+                        // these case statement will never be reached and it keeps on continuing
+                        // readAck(true) from above will block and causes everything to hang indefinately
+                        case 'T':
+                            readLine();
+                            ack();
+                            continue;
+                        case 'E':
+                            readLine();
+                            return;
+                        default:
+                            //a real ack that has been acted upon already
+                            continue;
+                    }
+
+                    if (optR && isDir)
+                    {
+                        writeDir(line, root.getFile(path));
+                    }
+                    else
+                    {
+                        writeFile(line, root.getFile(path));
+                    }
+                }
+            } else if (optF) {
+                String pattern = path;
+                int idx = pattern.indexOf('*');
+                if (idx >= 0) {
+                    String basedir = "";
+                    int lastSep = pattern.substring(0, idx).lastIndexOf('/');
+                    if (lastSep >= 0) {
+                        basedir = pattern.substring(0, lastSep);
+                        pattern = pattern.substring(lastSep + 1);
+                    }
+                    String[] included = new DirectoryScanner(basedir, pattern).scan();
+                    for (String path : included) {
+                        SshFile file = root.getFile(basedir + "/" + path);
+                        if (file.isFile()) {
+                            readFile(file);
+                        } else if (file.isDirectory()) {
+                            if (!optR) {
+                                out.write(WARNING);
+                                out.write((path + " not a regular file\n").getBytes());
+                            } else {
+                                readDir(file);
+                            }
+                        } else {
+                            out.write(WARNING);
+                            out.write((path + " unknown file type\n").getBytes());
+                        }
+                    }
+                } else {
+                    String basedir = "";
+                    int lastSep = pattern.lastIndexOf('/');
+                    if (lastSep >= 0) {
+                        basedir = pattern.substring(0, lastSep);
+                        pattern = pattern.substring(lastSep + 1);
+                    }
+                    SshFile file = root.getFile(basedir + "/" + pattern);
+                    if (!file.doesExist()) {
+                        throw new IOException(file + ": no such file or directory");
+                    }
+                    if (file.isFile()) {
+                        readFile(file);
+                    } else if (file.isDirectory()) {
+                        if (!optR) {
+                            throw new IOException(file + " not a regular file");
+                        } else {
+                            readDir(file);
+                        }
+                    } else {
+                        throw new IOException(file + ": unknown file type");
+                    }
+                }
+            } else {
+                throw new IOException("Unsupported mode");
+            }
+        } catch (IOException e) {
+            try {
+                exitValue = ERROR;
+                exitMessage = e.getMessage();
+                out.write(exitValue);
+                out.write(exitMessage.getBytes());
+                out.write('\n');
+                out.flush();
+            } catch (IOException e2) {
+                // Ignore
+            }
+            log.info("Error in scp command", e);
+        } finally {
+            if (callback != null) {
+                callback.onExit(exitValue, exitMessage);
+            }
+        }
     }
 }
