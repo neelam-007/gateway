e85981ecae74619c3ace2c3cbdfb263ea4385e48
rraquepo <rraquepo@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2012-05-31 18:35:26 +0000

    SSM-3200:  LDAP DN Injection, LDAP Search Injection, and XPath Injection is also now enabled if Apply protection to Response (body) is chosen
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@31709 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/policy/src/main/java/com/l7tech/policy/assertion/CodeInjectionProtectionType.java
+++ modules/policy/src/main/java/com/l7tech/policy/assertion/CodeInjectionProtectionType.java
@@ -13,7 +13,7 @@ import java.util.regex.Pattern;
 
 /**
  * Enum for types of code injection protection (i.e., types of attack to protect against).
- *
+ * <p/>
  * <p><i>JDK compatibility:</i> 1.4
  *
  * @author rmak
@@ -22,36 +22,54 @@ import java.util.regex.Pattern;
 public class CodeInjectionProtectionType implements Serializable {
     private static int _nextOrdinal = 0;
 
-    private final int _ordinal = _nextOrdinal ++;
+    private final int _ordinal = _nextOrdinal++;
 
-    /** String representation used in XML serialization. Must not change for backward compatibility. */
+    /**
+     * String representation used in XML serialization. Must not change for backward compatibility.
+     */
     private final String _wspName;
 
-    /** For UI display. Can be internationalized. */
+    /**
+     * For UI display. Can be internationalized.
+     */
     private final String _displayName;
 
-    /** For UI display. Can be internationalized. */
+    /**
+     * For UI display. Can be internationalized.
+     */
     private final String _description;
 
-    /** Regular expression pattern to detect code injection. */
+    /**
+     * Regular expression pattern to detect code injection.
+     */
     private final Pattern _pattern;
 
-    /** Whether this type of protection is applicable to request messages. */
+    /**
+     * Whether this type of protection is applicable to request messages.
+     */
     private final boolean _applicableToRequest;
 
-    /** Whether this type of protection is applicable to response messages. */
+    /**
+     * Whether this type of protection is applicable to response messages.
+     */
     private final boolean _applicableToResponse;
 
-    /** Whether the code protects against characters which cannot always be displayed. If true, any such chars will
-     * be converted to their unicode value.**/
+    /**
+     * Whether the code protects against characters which cannot always be displayed. If true, any such chars will
+     * be converted to their unicode value.*
+     */
     private final boolean _containsNonIdentifiableCharacters;
 
-    /** Map for looking up instance by wspName.
-        The keys are of type String. The values are of type {@link CodeInjectionProtectionType}. */
+    /**
+     * Map for looking up instance by wspName.
+     * The keys are of type String. The values are of type {@link CodeInjectionProtectionType}.
+     */
     private static final Map<String, CodeInjectionProtectionType> _byWspName = new HashMap<String, CodeInjectionProtectionType>();
 
-    /** Map for looking up instance by displayName.
-        The keys are of type String. The values are of type {@link CodeInjectionProtectionType}. */
+    /**
+     * Map for looking up instance by displayName.
+     * The keys are of type String. The values are of type {@link CodeInjectionProtectionType}.
+     */
     private static final Map<String, CodeInjectionProtectionType> _byDisplayName = new HashMap<String, CodeInjectionProtectionType>();
 
     // Warning: Never change wspName; in order to maintain backward compatibility.
@@ -82,14 +100,14 @@ public class CodeInjectionProtectionType implements Serializable {
             "Block messages which contain metacharacters that can be used to inject code into LDAP DN values. These metacharacters are \\,+\"<>;",
             Pattern.compile("[\\\\,+\"<>;]"),
             true,
-            false);
+            true);
     public static final CodeInjectionProtectionType LDAP_SEARCH_INJECTION = new CodeInjectionProtectionType(
             "ldapSearchInjection",
             "LDAP Search Injection",
             "Block messages which contain metacharacters that can be used to inject code into LDAP search values. The metacharacters are \\*()\\u0000",
             Pattern.compile("[\\\\*()\u0000]"),
             true,
-            false,
+            true,
             true);
     public static final CodeInjectionProtectionType XPATH_INJECTION = new CodeInjectionProtectionType(
             "xpathInjection",
@@ -97,7 +115,7 @@ public class CodeInjectionProtectionType implements Serializable {
             "Block messages which contain metacharacters that can be used to inject code into XPath queries. The metacharacters are \"*^';&<>()",
             Pattern.compile("[\"*^';&<>()]"),
             true,
-            false);
+            true);
 
     private static final CodeInjectionProtectionType[] _values = new CodeInjectionProtectionType[]{
             HTML_JAVASCRIPT,
@@ -114,7 +132,7 @@ public class CodeInjectionProtectionType implements Serializable {
     /**
      * Returns the enum constant of this type with the specified wspName.
      *
-     * @param wspName  wspName of enum constant
+     * @param wspName wspName of enum constant
      * @return the enum constant with the specified wspName; <code>null</code> if
      *         this enum type has no constant with the specified wspName
      */
@@ -153,32 +171,44 @@ public class CodeInjectionProtectionType implements Serializable {
         this(wspName, displayName, description, pattern, applicableToRequest, applicableToResponse, false);
     }
 
-    /** @return string representation used in XML serialization */
+    /**
+     * @return string representation used in XML serialization
+     */
     public String getWspName() {
         return _wspName;
     }
 
-    /** @return name for UI display */
+    /**
+     * @return name for UI display
+     */
     public String getDisplayName() {
         return _displayName;
     }
 
-    /** @return long description for UI display (suitable for tooltip or help) */
+    /**
+     * @return long description for UI display (suitable for tooltip or help)
+     */
     public String getDescription() {
         return _description;
     }
 
-    /** @return the regular expression pattern used to detect code injection */
+    /**
+     * @return the regular expression pattern used to detect code injection
+     */
     public Pattern getPattern() {
         return _pattern;
     }
 
-    /** @return whether this type of protection is applicable to request messages */
+    /**
+     * @return whether this type of protection is applicable to request messages
+     */
     public boolean isApplicableToRequest() {
         return _applicableToRequest;
     }
 
-    /** @return whether this type of protection is applicable to response messages */
+    /**
+     * @return whether this type of protection is applicable to response messages
+     */
     public boolean isApplicableToResponse() {
         return _applicableToResponse;
     }
@@ -187,7 +217,9 @@ public class CodeInjectionProtectionType implements Serializable {
         return _containsNonIdentifiableCharacters;
     }
 
-    /** @return a String representation suitable for UI display */
+    /**
+     * @return a String representation suitable for UI display
+     */
     public String toString() {
         return _displayName;
     }
@@ -201,14 +233,15 @@ public class CodeInjectionProtectionType implements Serializable {
         return new EnumTranslator() {
             @Override
             public String objectToString(Object target) {
-                CodeInjectionProtectionType type = (CodeInjectionProtectionType)target;
+                CodeInjectionProtectionType type = (CodeInjectionProtectionType) target;
                 return type.getWspName();
             }
 
             @Override
             public Object stringToObject(String wspName) throws IllegalArgumentException {
                 CodeInjectionProtectionType type = CodeInjectionProtectionType.fromWspName(wspName);
-                if (type == null) throw new IllegalArgumentException("Unknown CodeInjectionProtectionType: '" + wspName + "'");
+                if (type == null)
+                    throw new IllegalArgumentException("Unknown CodeInjectionProtectionType: '" + wspName + "'");
                 return type;
             }
         };
