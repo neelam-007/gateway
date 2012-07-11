1d765096b32419925a573515d2b101f1617d954b
kdiep <kdiep@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2012-07-11 18:50:14 +0000

    ug FR-440: modified the wrong sql upgrade file initially.  rollback incorrect file and committing the correct one
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@32031 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- etc/db/mysql/upgrade_6.2.0-7.0.0.sql
+++ etc/db/mysql/upgrade_6.2.0-7.0.0.sql
@@ -14,6 +14,11 @@ SET FOREIGN_KEY_CHECKS=0;
 --
 UPDATE ssg_version SET current_version = '7.0.0';
 
+--
+-- Add the SSPC log/sink entry
+--
+INSERT INTO sink_config VALUES (-811,0,'sspc','Process Controller Log','FILE',0,'FINEST','SSPC','<java version="1.6.0" class="java.beans.XMLDecoder"><object class="java.util.HashMap"><void method="put"><string>file.maxSize</string><string>20000</string></void><void method="put"><string>file.format</string><string>STANDARD</string></void><void method="put"><string>file.logCount</string><string>10</string></void></object></java>');
+
 -- External audits enhancements
 ALTER TABLE audit_message DROP FOREIGN KEY message_context_mapping;
 ALTER TABLE audit_message DROP COLUMN mapping_values_oid ;
