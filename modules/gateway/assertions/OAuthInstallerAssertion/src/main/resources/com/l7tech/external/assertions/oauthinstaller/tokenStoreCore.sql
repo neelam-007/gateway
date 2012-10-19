d586c9ba0c157c252015c7ff9d90f8ad356a21e2
alee <alee@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2012-10-19 21:29:33 +0000

    bug 13304: Added policy fragment which stores to cache and oauth_session (new table). Added policy fragment which looks up from cache then oauth_session. Refactored logic for oauth 2 clients authcode, implicit, client credentials, saml, resource owner password creds to use these new fragments. Added context variable for oauth 2 version. Note: you will need to create a new table in your oauth database (oauth_session) - see newly added tokenStoreCore.sql file which was only added to subversion for change tracking.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@33326 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ modules/gateway/assertions/OAuthInstallerAssertion/src/main/resources/com/l7tech/external/assertions/oauthinstaller/tokenStoreCore.sql
@@ -0,0 +1,104 @@
+-- Layer7 Technologies Inc.
+-- 19/10/2012 (DD/MM/YYYY)
+--
+-- This script is used to create tables on a mysql server using the oauth installation tool.
+--
+drop table if exists oauth_session;
+drop table if exists oauth_token;
+drop table if exists oauth_initiate;
+drop table if exists oauth_client_key;
+drop table if exists oauth_client;
+
+--
+-- This table holds valid client application values as defined when the application was registered
+--
+CREATE TABLE oauth_client (
+  client_ident varchar(128) primary key,
+  name varchar(128) not null COMMENT 'The associated name of the application using this client_id',
+  type varchar(128) not null DEFAULT 'oob' COMMENT 'used with oauth 2.0',
+  description varchar(256) null,
+  organization varchar(128) not null,
+  registered_by varchar(128) not null,
+  created bigint DEFAULT 0 not null,
+  constraint uk_oc_name_org unique (name, organization)
+) ENGINE=InnoDB DEFAULT CHARACTER SET utf8;
+
+--
+-- This table holds valid client keys
+--
+CREATE TABLE oauth_client_key (
+  client_key varchar(128) primary key COMMENT 'oauth_consumer_key or client_id',
+  secret varchar(128) not null COMMENT 'oauth_consumer_key_secret or client_secret',
+  scope varchar(512) not null DEFAULT 'oob' COMMENT 'for oauth2, to be defined by the customer and handled accordingly within the policy',
+  callback varchar(256) not null DEFAULT 'oob' COMMENT 'in oauth2 = redirect_uri, contains one URI',
+  environment varchar(128) not null DEFAULT 'ALL' COMMENT 'COULD BE SOMETHING LIKE Test, Prod, Integration',
+  expiration bigint not null DEFAULT 0 COMMENT 'Date until this key is valid',
+  status varchar(128) not null COMMENT 'for validation purposes, ENABLED or DISABLED',
+  created bigint DEFAULT 0 not null,
+  created_by varchar(128) not null,
+  client_ident varchar(128) not null COMMENT 'The client that owns this key',
+  client_name varchar(128) not null COMMENT 'The name of the client that owns this key. This makes it a non normalized table but what counts is performance, performance, performance ...',
+  constraint ock_fk_clientIdent foreign key (client_ident) references oauth_client (client_ident) ON DELETE CASCADE,
+  constraint ock_fk_clientName foreign key (client_name) references oauth_client (name) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARACTER SET utf8;
+
+--
+-- Lets insert values for the oauth 1.0 test client
+--
+insert into oauth_client (client_ident, name, description, organization, registered_by)
+values ('123456789', 'OAuth1Client', 'OAuth 1.0 test client hosted on the ssg', 'Layer7 Technologies Inc.', 'Tactical Team');
+insert into oauth_client (client_ident, name, description, organization, registered_by, type)
+values ('123456799', 'OAuth2Client', 'OAuth 2.0 test client hosted on the ssg', 'Layer7 Technologies Inc.', 'Tactical Team', 'confidential');
+
+insert into oauth_client_key (client_key, secret, status, created_by, client_ident, client_name)
+values ('acf89db2-994e-427b-ac2c-88e6101f9433', '74d5e0db-cd8b-4d8e-a989-95a0746c3343', 'ENABLED', 'Tactical Team', '123456789', 'OAuth1Client');
+
+insert into oauth_client_key (client_key, secret, status, created_by, client_ident, client_name, callback)
+values ('54f0c455-4d80-421f-82ca-9194df24859d', 'a0f2742f-31c7-436f-9802-b7015b8fd8e6', 'ENABLED', 'Tactical Team', '123456799', 'OAuth2Client', 'YOUR_SSG/oauth/v2/client/authcode,YOUR_SSG/oauth/v2/client/implicit');
+
+-- This table holds access_tokens and refresh_tokens
+-- oauth 1.0 = access_token
+-- oauth 2.0 = access_token, refresh_token
+--
+CREATE TABLE oauth_token (
+  token varchar(128) primary key,
+  secret varchar(128) null COMMENT 'null for oauth 2.0, it does not provide a secret',
+  expiration bigint not null,
+  scope varchar(512) COMMENT 'for 0auth 2.0, the scope granted by the resource owner',
+  resource_owner varchar(128) not null COMMENT 'the authenticated user that granted the token',
+  created bigint DEFAULT 0 COMMENT 'the date this token (or these tokens in oauth 2.0) were created',
+  rtoken varchar(128) unique key,
+  rexpiration bigint DEFAULT 0 COMMENT 'DEFAULT 0 because otherwise timestamp will be set to now() on an update',
+  status varchar(128) not null COMMENT 'for validation purposes, ENABLED or DISABLED',
+  client_key varchar(128) not null COMMENT 'the client_key this token was issued for',
+  client_name varchar(128) not null COMMENT 'The name of the client that owns this key. This makes it a non normalized table but what counts is performance, performance, performance ...'
+) ENGINE=InnoDB DEFAULT CHARACTER SET utf8;
+
+--
+-- This table holds temporary tokens
+-- oauth 1.0 = request_token
+-- oauth 2.0 = authorization_code
+--
+CREATE TABLE oauth_initiate (
+  token varchar(128) primary key COMMENT 'for oauth 1.0, 2.0',
+  secret varchar(128) null COMMENT 'null for oauth 2.0, it does not provide a secret',
+  expiration bigint not null DEFAULT 0 COMMENT 'for oauth 1.0, 2.0, DEFAULT 0 because otherwise timestamp will be set to now() on an update',
+  scope varchar(4096) COMMENT 'for oauth 2.0, the scope granted by the resource owner',
+  resource_owner varchar(128) null COMMENT 'the authenticated user that granted the token',
+  created bigint DEFAULT 0 COMMENT 'the date this token (or these tokens in oauth 2.0) were created',
+  verifier varchar(128) COMMENT 'for oauth 1.0',
+  callback varchar(256) COMMENT 'for oauth 1.0, 2.0',
+  client_key varchar(128) not null COMMENT 'the client that received this token',
+  client_name varchar(128) not null COMMENT 'The name of the client that owns this key. This makes it a non normalized table but what counts is performance, performance, performance ...'
+) ENGINE=InnoDB DEFAULT CHARACTER SET utf8;
+
+--
+-- This table holds session info
+--
+CREATE TABLE oauth_session (
+  session_key varchar(128) not null,
+  session_group varchar(128) not null,
+  expiration bigint not null,
+  value mediumtext not null,
+  primary key (session_key, session_group)
+) ENGINE=InnoDB DEFAULT CHARACTER SET utf8;
