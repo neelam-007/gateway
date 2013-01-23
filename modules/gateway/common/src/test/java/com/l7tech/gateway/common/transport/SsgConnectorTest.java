004791fd471bf8043268eff50e81c6667020bca4
mike <mike@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2013-01-23 01:52:25 +0000

    Adding unit test for SsgConnector.endpointSet()
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@34457 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/common/src/test/java/com/l7tech/gateway/common/transport/SsgConnectorTest.java
+++ modules/gateway/common/src/test/java/com/l7tech/gateway/common/transport/SsgConnectorTest.java
@@ -1,8 +1,11 @@
 package com.l7tech.gateway.common.transport;
 
+import com.l7tech.test.BugNumber;
+import org.junit.Test;
+
 import static com.l7tech.gateway.common.transport.SsgConnector.CLIENT_AUTH_NEVER;
-import static org.junit.Assert.*;
-import org.junit.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  *
@@ -37,5 +40,17 @@ public class SsgConnectorTest {
     private void ne(Object a, Object b) {
         assertFalse(a.equals(b));
         assertFalse(b.equals(a));
+    }
+
+    @Test
+    @BugNumber(13718)
+    public void testEndpointSetIncludesChildEndpoints() {
+        SsgConnector a = new SsgConnector();
+        a.setEndpoints("MESSAGE_INPUT,ADMIN_REMOTE");
+
+        assertTrue(a.endpointSet().contains(SsgConnector.Endpoint.MESSAGE_INPUT));
+        assertTrue(a.endpointSet().contains(SsgConnector.Endpoint.ADMIN_REMOTE));
+        assertTrue(a.endpointSet().contains(SsgConnector.Endpoint.ADMIN_REMOTE_ESM));
+        assertTrue(a.endpointSet().contains(SsgConnector.Endpoint.ADMIN_REMOTE_SSM));
     }
 }
