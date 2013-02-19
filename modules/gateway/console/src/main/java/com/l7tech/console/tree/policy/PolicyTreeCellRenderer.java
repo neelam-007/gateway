3e02b6474472a245a99857aee7a13fd723a73c84
ghuang <ghuang@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2013-02-19 23:21:26 +0000

    SSM-4222 Children assertions in an include Policy now are rendered correctly when the include policy is disabled.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@34778 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/console/src/main/java/com/l7tech/console/tree/policy/PolicyTreeCellRenderer.java
+++ modules/gateway/console/src/main/java/com/l7tech/console/tree/policy/PolicyTreeCellRenderer.java
@@ -53,7 +53,7 @@ public class PolicyTreeCellRenderer extends DefaultTreeCellRenderer {
         AssertionTreeNode node = ((AssertionTreeNode)value);
         setText(node.getName());
         validated = node.getValidatorMessages().isEmpty();
-        enabled = node.isAssertionEnabled() || isIncluded(node);
+        enabled = isIncluded(node)? node.asAssertion().isEnabled(): node.isAssertionEnabled();
         setToolTipText(node.getTooltipText());
 
         Image image =expanded ? node.getOpenedIcon() : node.getIcon();
