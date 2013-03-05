c5b58fc325b5e5cf95ccd3c137749b142b4a6558
vkazakov <vkazakov@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2013-03-05 01:04:55 +0000

    SSG - 6571 - Create JDBC Integration tests. Renaming JdbcCallHelperIntegrationTest so that it is not run by the build.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@34964 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/skunkworks/src/test/java/com/l7tech/skunkworks/jdbc/JdbcCallHelperIntegrationTests.java
+++ modules/skunkworks/src/test/java/com/l7tech/skunkworks/jdbc/JdbcCallHelperIntegrationTests.java
@@ -16,7 +16,7 @@ import java.util.List;
  *
  * @author Victor Kazakov
  */
-public class JdbcCallHelperIntegrationTest extends JdbcCallHelperIntegrationAbstractBaseTestClass {
+public class JdbcCallHelperIntegrationTests extends JdbcCallHelperIntegrationAbstractBaseTestClass {
 
     /**
      * Tests retrieving a clob from a procedure.
