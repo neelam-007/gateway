5b435268e501e71e27131cfc680e45af6950da7c
megery <megery@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2013-03-19 04:38:35 +0000

    remove ActionalAssertiona and associated SDK from UneasyRooster tree. If needed, the assertion can still be rebuilt using another branch but this should not be shipped without a license.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@35205 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/assertions/ActionalAssertion/src/main/java/com/l7tech/external/assertions/actional/server/InterceptorException.java
+++
@@ -1,9 +0,0 @@
-package com.l7tech.external.assertions.actional.server;
-
-/**
- */
-class InterceptorException extends RuntimeException {
-    InterceptorException(String message) {
-        super(message);
-    }
-}
