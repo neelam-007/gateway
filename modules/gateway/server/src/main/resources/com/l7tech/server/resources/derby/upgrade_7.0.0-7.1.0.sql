7d9e2b91abca9aeca46155572b65fd6a39252064
vkazakov <vkazakov@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2013-04-11 19:06:10 +0000

    SSG-6774 - adding column updates to the derby upgrade script.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@35480 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/server/src/main/resources/com/l7tech/server/resources/derby/upgrade_7.0.0-7.1.0.sql
+++ modules/gateway/server/src/main/resources/com/l7tech/server/resources/derby/upgrade_7.0.0-7.1.0.sql
@@ -118,3 +118,8 @@ INSERT INTO cluster_properties
 INSERT INTO cluster_properties
     (objectid, version, propkey, propvalue, properties)
     values (-700101, 0, 'upgrade.task.700101', 'com.l7tech.server.upgrade.Upgrade70to71UpdateGatewayManagementWsdl', null);
+
+-- Increasing the length of the subject dn to allow for certificates with longer subject dn's
+-- See SSG-6774
+ALTER TABLE client_cert ALTER COLUMN subject_dn SET DATA TYPE VARCHAR(2048);
+ALTER TABLE trusted_cert ALTER COLUMN subject_dn SET DATA TYPE VARCHAR(2048);
