6321faa67287e4330c17d36d5f067982dd28e1e2
wlui <wlui@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2013-04-17 21:17:34 +0000

    Fix test to use a config and check returned error.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@35605 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/server/src/test/java/com/l7tech/server/jdbc/JdbcConnectionPoolManagerTest.java
+++ modules/gateway/server/src/test/java/com/l7tech/server/jdbc/JdbcConnectionPoolManagerTest.java
@@ -1,12 +1,17 @@
 package com.l7tech.server.jdbc;
 
 import com.l7tech.gateway.common.jdbc.JdbcConnection;
+import com.l7tech.server.ApplicationContexts;
+import com.l7tech.server.ServerConfigStub;
+import com.l7tech.util.Pair;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.junit.Before;
 import org.junit.Test;
 
-import javax.naming.NameNotFoundException;
 
-import static org.junit.Assert.assertNotNull;
+import java.util.Collections;
+
+import static org.junit.Assert.assertTrue;
 
 public class JdbcConnectionPoolManagerTest {
 
@@ -25,12 +30,16 @@ public class JdbcConnectionPoolManagerTest {
 
     }
 
-    @Test(expected = NullPointerException.class)
+    @Test
     public void testInvalidDriverClass() throws Exception {
         JdbcConnection jdbcConnection = new JdbcConnection();
         jdbcConnection.setName("TEST");
         jdbcConnection.setDriverClass("com.test.driver");
-        jdbcConnectionPoolManager.updateConnectionPool(jdbcConnection, false);
+
+        ApplicationContexts.inject(jdbcConnectionPoolManager, Collections.singletonMap("serverConfig", new ServerConfigStub()));
+        Pair<ComboPooledDataSource, String> result = jdbcConnectionPoolManager.updateConnectionPool(jdbcConnection, false);
+        assertTrue("No datasource created", result.left == null);
+        assertTrue("Driver class not supported", result.right.contains("Driver class com.test.driver is not supported."));
     }
 
 }
