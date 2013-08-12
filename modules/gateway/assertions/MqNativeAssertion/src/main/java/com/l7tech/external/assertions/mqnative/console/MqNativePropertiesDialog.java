e9e36e4fb1ff6d2ca3ca59649da94d976eb067b1
vkazakov <vkazakov@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2013-08-12 18:56:27 +0000

    SSG-7222 - MQ native inbound queue, associate with service combo box was not properly being populated.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@37134 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/assertions/MqNativeAssertion/src/main/java/com/l7tech/external/assertions/mqnative/console/MqNativePropertiesDialog.java
+++ modules/gateway/assertions/MqNativeAssertion/src/main/java/com/l7tech/external/assertions/mqnative/console/MqNativePropertiesDialog.java
@@ -681,7 +681,7 @@ public class MqNativePropertiesDialog extends JDialog {
                 if (mqNativeActiveConnector.getHardwiredServiceGoid() != null && !Goid.isDefault(mqNativeActiveConnector.getHardwiredServiceGoid())){
                     associateQueue = ServiceComboBox.populateAndSelect(serviceNameCombo, true, mqNativeActiveConnector.getHardwiredServiceGoid());
                 }else{
-                    associateQueue = ServiceComboBox.populateAndSelect(serviceNameCombo, true, GoidEntity.DEFAULT_GOID);
+                    associateQueue = ServiceComboBox.populateAndSelect(serviceNameCombo, false, GoidEntity.DEFAULT_GOID);
                 }
                 if(associateQueue) {
                     associateQueueWithPublishedService.setSelected(true);
@@ -789,7 +789,7 @@ public class MqNativePropertiesDialog extends JDialog {
         );
 
         if(!populatedTheServiceList)
-            ServiceComboBox.populateAndSelect(serviceNameCombo, true, GoidEntity.DEFAULT_GOID);
+            ServiceComboBox.populateAndSelect(serviceNameCombo, false, GoidEntity.DEFAULT_GOID);
         enableOrDisableComponents();
     }
 
