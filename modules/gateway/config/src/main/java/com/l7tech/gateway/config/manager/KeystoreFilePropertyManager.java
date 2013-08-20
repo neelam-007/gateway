ccde9e2a7dd895415e501a3628b714c4525bc836
vkazakov <vkazakov@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2013-08-20 18:01:23 +0000

    SSG-7222 - fixing KeystoreFilePropertyManager to work with the Goid'ed keystore_file table
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@37357 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/config/src/main/java/com/l7tech/gateway/config/manager/KeystoreFilePropertyManager.java
+++ modules/gateway/config/src/main/java/com/l7tech/gateway/config/manager/KeystoreFilePropertyManager.java
@@ -2,6 +2,8 @@ package com.l7tech.gateway.config.manager;
 
 import com.l7tech.common.io.NonCloseableOutputStream;
 import com.l7tech.gateway.config.manager.db.DBActions;
+import com.l7tech.objectmodel.Goid;
+import com.l7tech.objectmodel.GoidEntity;
 import com.l7tech.server.management.config.node.DatabaseConfig;
 import com.l7tech.server.management.config.node.DatabaseType;
 import com.l7tech.server.management.config.node.NodeConfig;
@@ -13,7 +15,10 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.text.MessageFormat;
 import java.util.*;
 import java.util.logging.Level;
@@ -44,7 +49,7 @@ public class KeystoreFilePropertyManager {
 
     public static void main(final String[] argsArray) {
         JdkLoggerConfigurator.configure("com.l7tech.logging", "com/l7tech/gateway/config/client/logging.properties", "configlogging.properties");
-        int oid = -1;
+        Goid goid = GoidEntity.DEFAULT_GOID;
         String action = "process";
         String propertyName = null;
         String propertyValue = null;
@@ -52,7 +57,8 @@ public class KeystoreFilePropertyManager {
             LinkedList<String> args = new LinkedList<String>(Arrays.asList(argsArray));
 
             action = args.removeFirst();                                                        
-            oid = Integer.parseInt(args.removeFirst());
+            String id = args.removeFirst();
+            goid = getGoid(id);
             propertyName = args.removeFirst();
 
             if (!args.isEmpty())
@@ -60,8 +66,8 @@ public class KeystoreFilePropertyManager {
 
             if (!args.isEmpty())
                 usage("Too many arguments.");
-        } catch (NumberFormatException nfe) {
-            usage("Object ID must be an integer.");
+        } catch (IllegalArgumentException iae) {
+            usage("ID must be a long or a goid .");
         } catch (NoSuchElementException e) {
             usage("Not enough arguments.");
         }
@@ -72,20 +78,20 @@ public class KeystoreFilePropertyManager {
                 if (propertyValue != null)
                     usage("Property value should not be provided for get.");
 
-                propertyValue = kpm.getProperty(oid, propertyName);
+                propertyValue = kpm.getProperty(goid, propertyName);
                 System.out.println(propertyValue);
                 System.exit(EXIT_STATUS_OK);
             } else if ("set".equalsIgnoreCase(action)) {
                 if (propertyValue == null)
                     usage("New property value must be provided for set.");
 
-                kpm.setProperty(oid, propertyName, propertyValue);
+                kpm.setProperty(goid, propertyName, propertyValue);
                 System.exit(EXIT_STATUS_OK);
             } else if ("clear".equalsIgnoreCase(action)) {
                 if (propertyValue != null)
                     usage("Property value should not be provided for clear.");
 
-                kpm.clearProperty(oid, propertyName);
+                kpm.clearProperty(goid, propertyName);
                 System.exit(EXIT_STATUS_OK);
             } else {
                 usage("Invalid action.");
@@ -93,11 +99,21 @@ public class KeystoreFilePropertyManager {
         } catch (NoSuchPropertyException e) {
             fatal("No such property: " + propertyName, null, EXIT_STATUS_NO_SUCH_PROPERTY);
         } catch (NoSuchObjectIdException e) {
-            fatal("No such keystore objectid: " + propertyName, null, EXIT_STATUS_NO_SUCH_OBJECT_ID);
+            fatal("No such keystore id: " + propertyName, null, EXIT_STATUS_NO_SUCH_OBJECT_ID);
         } catch (Throwable e) {
             fatal("Unable to " + action + " keystore property: " + ExceptionUtils.getMessage(e), e, EXIT_STATUS_ERROR);
         }
     }
+
+    private static Goid getGoid(String id) {
+        //try to get a goid from the input.
+        //if it is a goid string just use that, otherwise assume it was oid so create a default goid using the oid
+        try{
+            return Goid.parseGoid(id);
+        } catch(IllegalArgumentException e) {
+            return new Goid(0,Long.parseLong(id));
+        }
+   }
 
     private static void fatal(String msg, Throwable t, int status) {
         logger.log(Level.WARNING, msg, t);
@@ -147,50 +163,51 @@ public class KeystoreFilePropertyManager {
         this.dbConfig = dbConfig;
     }
 
-    public void setProperty(int oid, String propertyName, String propertyValueStr) throws SQLException, NoSuchObjectIdException {
+    public void setProperty(Goid goid, String propertyName, String propertyValueStr) throws SQLException, NoSuchObjectIdException {
         if ("databytes".equalsIgnoreCase(propertyName)) {
             throw new SQLException("set not supported for databytes property");
         }
 
-        Map<String, String> properties = loadProperties(oid);
+        Map<String, String> properties = loadProperties(goid);
         properties.put(propertyName, propertyValueStr);
-        saveProperties(oid, properties);
+        saveProperties(goid, properties);
     }
 
-    public String getProperty(int oid, String propertyName) throws SQLException, NoSuchPropertyException, NoSuchObjectIdException {
+    public String getProperty(Goid goid, String propertyName) throws SQLException, NoSuchPropertyException, NoSuchObjectIdException {
         if ("databytes".equalsIgnoreCase(propertyName)) {
             throw new SQLException("get not supported for databytes property");
         }
 
-        Map<String, String> properties = loadProperties(oid);
+        Map<String, String> properties = loadProperties(goid);
         String result = properties.get(propertyName);
         if (result == null)
             throw new NoSuchPropertyException();
         return result;
     }
 
-    public void clearProperty(int oid, String propertyName) throws SQLException, NoSuchObjectIdException {
+    public void clearProperty(Goid goid, String propertyName) throws SQLException, NoSuchObjectIdException {
         // Recognizes special property name: databytes
         if ("databytes".equalsIgnoreCase(propertyName)) {
-            clearDataBytes(oid);
+            clearDataBytes(goid);
         } else {
-            Map<String, String> properties = loadProperties(oid);
+            Map<String, String> properties = loadProperties(goid);
             properties.remove(propertyName);
-            saveProperties(oid, properties);
+            saveProperties(goid, properties);
         }
     }
 
-    private Map<String, String> loadProperties(int oid) throws SQLException, NoSuchObjectIdException {
+    private Map<String, String> loadProperties(Goid goid) throws SQLException, NoSuchObjectIdException {
         final Map<String, String> properties;
 
         DBActions dbactions = new DBActions();
         Connection connection = null;
-        Statement statement = null;
+        PreparedStatement statement = null;
         ResultSet resultSet = null;
         try {
             connection = dbactions.getConnection(dbConfig, false);
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery("select properties from keystore_file where objectid = " + oid);
+            statement = connection.prepareStatement("select properties from keystore_file where goid = ?");
+            statement.setBytes(1, goid.getBytes());
+            resultSet = statement.executeQuery();
             if (resultSet.next()) {
                 String xml = resultSet.getString(1);
                 if ( xml != null && xml.length() > 0 ) {
@@ -212,7 +229,7 @@ public class KeystoreFilePropertyManager {
         return properties;
     }
 
-    private void saveProperties(int oid, Map<String, String> properties) throws SQLException, NoSuchObjectIdException {
+    private void saveProperties(Goid goid, Map<String, String> properties) throws SQLException, NoSuchObjectIdException {
         final String xml;
         if ( properties == null )
             throw new NullPointerException("properties");
@@ -232,8 +249,9 @@ public class KeystoreFilePropertyManager {
         ResultSet resultSet = null;
         try {
             connection = dbactions.getConnection(dbConfig, false);
-            statement = connection.prepareStatement("update keystore_file set properties = ? where objectid = " + oid);
+            statement = connection.prepareStatement("update keystore_file set properties = ? where goid = ?");
             statement.setString(1, xml);
+            statement.setBytes(2, goid.getBytes());
             statement.execute();
             int updateCount = statement.getUpdateCount();
             if (updateCount < 1) {
@@ -246,14 +264,15 @@ public class KeystoreFilePropertyManager {
         }
     }
 
-    private void clearDataBytes(int oid) throws SQLException, NoSuchObjectIdException {
+    private void clearDataBytes(Goid goid) throws SQLException, NoSuchObjectIdException {
         DBActions dbactions = new DBActions();
         Connection connection = null;
         PreparedStatement statement = null;
         ResultSet resultSet = null;
         try {
             connection = dbactions.getConnection(dbConfig, false);
-            statement = connection.prepareStatement("update keystore_file set databytes = null where objectid = " + oid);
+            statement = connection.prepareStatement("update keystore_file set databytes = null where goid = ?");
+            statement.setBytes(1, goid.getBytes());
             statement.execute();
             int updateCount = statement.getUpdateCount();
             if (updateCount < 1) {
