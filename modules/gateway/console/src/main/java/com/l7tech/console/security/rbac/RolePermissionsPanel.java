12a3a8e62a8ae8f4cd3b1fd86e40fac88f200e81
alee <alee@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2013-09-05 20:21:50 +0000

    SSM-4492 Load count on configure with permissions.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@37789 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/console/src/main/java/com/l7tech/console/security/rbac/RolePermissionsPanel.java
+++ modules/gateway/console/src/main/java/com/l7tech/console/security/rbac/RolePermissionsPanel.java
@@ -98,6 +98,7 @@ public class RolePermissionsPanel extends JPanel {
         this.role = null;
         this.permissions = permissions;
         loadTable();
+        loadCount();
     }
 
     private void initButtons() {
