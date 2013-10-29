dd479510c5113ff9de5f91ecc261402ddab0f845
alee <alee@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2013-10-29 00:19:36 +0000

    SSG-7778 Added more integration tests for header handling. Created createServiceMessageTemplate.xml which can be used to send wsman messages to create a service with any policy. Added notes on how to use each wsman message template.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/branches/reverse_proxy_feature@38577 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/server/src/test/java/com/l7tech/server/HttpRoutingIntegrationTest.java
+++ modules/gateway/server/src/test/java/com/l7tech/server/HttpRoutingIntegrationTest.java
@@ -4,12 +4,18 @@ import com.l7tech.common.http.*;
 import com.l7tech.common.http.prov.apache.components.HttpComponentsClient;
 import com.l7tech.common.io.PermissiveX509TrustManager;
 import com.l7tech.common.io.XmlUtil;
+import com.l7tech.policy.AssertionRegistry;
+import com.l7tech.policy.assertion.*;
+import com.l7tech.policy.assertion.composite.AllAssertion;
+import com.l7tech.policy.wsp.WspConstants;
+import com.l7tech.policy.wsp.WspWriter;
+import com.l7tech.util.HexUtils;
 import com.l7tech.util.IOUtils;
 import com.l7tech.util.NamespaceContextImpl;
 import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.NotNull;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.w3c.dom.Document;
@@ -44,15 +50,19 @@ public class HttpRoutingIntegrationTest {
     private static final PasswordAuthentication PASSWORD_AUTHENTICATION = new PasswordAuthentication("admin", "password".toCharArray());
     private static final String ECHO_HEADERS_SERVICE_RESOURCE = "com/l7tech/server/wsman/createEchoHeadersService.xml";
     private static final String BASIC_ROUTING_SERVICE_RESOURCE = "com/l7tech/server/wsman/createBasicRoutingService.xml";
-    private static final String DELTE_SERVICE_RESOURCE = "com/l7tech/server/wsman/deleteService.xml";
+    private static final String SERVICE_TEMPLATE_RESOURCE = "com/l7tech/server/wsman/createServiceMessageTemplate.xml";
+    private static final String DELETE_SERVICE_RESOURCE = "com/l7tech/server/wsman/deleteService.xml";
     private static final String CREATE_ACTION = "http://schemas.xmlsoap.org/ws/2004/09/transfer/Create";
     private static final String DELETE_ACTION = "http://schemas.xmlsoap.org/ws/2004/09/transfer/Delete";
-    private GenericHttpClient client;
-    private XPath xPath;
-    private List<String> createdServiceIds;
+    private static GenericHttpClient client;
+    private static XPath xPath;
+    private static List<String> createdServiceIds;
+    private static final String SERVICENAME = "===SERVICENAME===";
+    private static final String SERVICEURL = "===SERVICEURL===";
+    private static final String SERVICEPOLICY = "===SERVICEPOLICY===";
 
-    @Before
-    public void setup() throws Exception {
+    @BeforeClass
+    public static void setup() throws Exception {
         client = new HttpComponentsClient();
         xPath = XPathFactory.newInstance().newXPath();
         final HashMap<String, String> map = new HashMap<>();
@@ -70,20 +80,27 @@ public class HttpRoutingIntegrationTest {
         createdServiceIds = new ArrayList<>();
         createdServiceIds.add(createService(ECHO_HEADERS_SERVICE_RESOURCE));
         createdServiceIds.add(createRoutingService(BASIC_ROUTING_SERVICE_RESOURCE, "http://" + BASE_URL + ":8080/echoHeaders"));
+        final AssertionRegistry assertionRegistry = new AssertionRegistry();
+        assertionRegistry.setApplicationContext(null);
+        assertionRegistry.registerAssertion(HardcodedResponseAssertion.class);
+        WspConstants.setTypeMappingFinder(assertionRegistry);
     }
 
-    @After
-    public void teardown() throws Exception {
+    @AfterClass
+    public static void teardown() throws Exception {
         for (final String createdServiceId : createdServiceIds) {
             deleteService(createdServiceId);
         }
     }
 
+    /**
+     * Extra request headers should not be passed through by default.
+     */
     @Test
-    public void basicRouteToEchoHeaders() throws Exception {
-        final GenericHttpRequestParams params = new GenericHttpRequestParams();
-        params.setTargetUrl(new URL("http://" + BASE_URL + ":8080/basicRoutingService"));
-        System.out.println("======Sending request to BasicRoutingService======");
+    public void basicRouteWithDefaultRoutingAssertionConfiguration() throws Exception {
+        final GenericHttpRequestParams params = new GenericHttpRequestParams(new URL("http://" + BASE_URL + ":8080/basicRoutingService"));
+        // header should not be routed
+        params.addExtraHeader(new GenericHttpHeader("Extra-Header", "Should Not Be Routed"));
 
         final GenericHttpResponse response = sendRequest(params, HttpMethod.GET, null);
         final String responseBody = printResponseDetails(response);
@@ -97,9 +114,109 @@ public class HttpRoutingIntegrationTest {
         assertTrue(headers.containsKey("Date"));
 
         final Map<String, Collection<String>> routedHeaders = getRoutedHeaders(responseBody);
-        assertSingleHeaderValue(routedHeaders, "user-agent", "Apache-HttpClient/4.2.5 (java 1.5)");
+        assertEquals(3, routedHeaders.size());
+        assertEquals(1, routedHeaders.get("user-agent").size());
+        assertTrue(routedHeaders.get("user-agent").iterator().next().contains("Layer7-SecureSpan-Gateway"));
         assertSingleHeaderValue(routedHeaders, "host", BASE_URL + ":8080");
         assertSingleHeaderValue(routedHeaders, "connection", "Keep-Alive");
+        assertFalse(routedHeaders.containsKey("Extra-Header"));
+    }
+
+    /**
+     * Non-application headers such as Date should not be passed through by default.
+     */
+    @Test
+    public void basicRouteWithDefaultRoutingAssertionConfigurationFiltersNonApplicationHeaders() throws Exception {
+        final GenericHttpRequestParams params = new GenericHttpRequestParams(new URL("http://" + BASE_URL + ":8080/basicRoutingService"));
+        // these headers should not be routed
+        params.addExtraHeader(new GenericHttpHeader("Date", "shouldNotBePassed"));
+        params.addExtraHeader(new GenericHttpHeader("Host", "shouldBeReplacedWithGatewayHost"));
+        params.addExtraHeader(new GenericHttpHeader("Server", "shouldNotBePassed"));
+        params.addExtraHeader(new GenericHttpHeader("Connection", "shouldBeReplaced"));
+
+        final GenericHttpResponse response = sendRequest(params, HttpMethod.GET, null);
+        final String responseBody = printResponseDetails(response);
+        assertEquals(200, response.getStatus());
+
+        final Map<String, Collection<String>> routedHeaders = getRoutedHeaders(responseBody);
+        assertEquals(3, routedHeaders.size());
+        assertEquals(1, routedHeaders.get("user-agent").size());
+        assertTrue(routedHeaders.get("user-agent").iterator().next().contains("Layer7-SecureSpan-Gateway"));
+        assertSingleHeaderValue(routedHeaders, "host", BASE_URL + ":8080");
+        assertSingleHeaderValue(routedHeaders, "connection", "Keep-Alive");
+        assertFalse(routedHeaders.containsKey("Date"));
+        assertFalse(routedHeaders.containsKey("Server"));
+    }
+
+    /**
+     * Cookie and SOAPAction request headers should be passed through by default.
+     */
+    @Test
+    public void basicRouteWithDefaultRoutingAssertionConfigurationPassesCookieAndSoapAction() throws Exception {
+        final GenericHttpRequestParams params = new GenericHttpRequestParams(new URL("http://" + BASE_URL + ":8080/basicRoutingService"));
+        params.addExtraHeader(new GenericHttpHeader("Cookie", "foo=bar"));
+        params.addExtraHeader(new GenericHttpHeader("SOAPAction", "testSoapAction"));
+
+        final GenericHttpResponse response = sendRequest(params, HttpMethod.GET, null);
+        final String responseBody = printResponseDetails(response);
+        assertEquals(200, response.getStatus());
+
+        final Map<String, Collection<String>> routedHeaders = getRoutedHeaders(responseBody);
+        assertEquals(5, routedHeaders.size());
+        assertEquals(1, routedHeaders.get("user-agent").size());
+        assertTrue(routedHeaders.get("user-agent").iterator().next().contains("Layer7-SecureSpan-Gateway"));
+        assertSingleHeaderValue(routedHeaders, "host", BASE_URL + ":8080");
+        assertSingleHeaderValue(routedHeaders, "connection", "Keep-Alive");
+        assertSingleHeaderValue(routedHeaders, "cookie", "foo=bar");
+        assertSingleHeaderValue(routedHeaders, "soapaction", "testSoapAction");
+    }
+
+    /**
+     * If the route response sets a cookie, it should be passed through by default.
+     */
+    @Test
+    public void responseSetCookieHeaderPassedByDefault() throws Exception {
+        final HttpRoutingAssertion routeAssertion = new HttpRoutingAssertion();
+        routeAssertion.setProtectedServiceUrl("http://" + BASE_URL + ":8080/setCookieService");
+        final String routePolicy = WspWriter.getPolicyXml(new AllAssertion(Collections.singletonList(routeAssertion)));
+        final Map<String, String> routeParams = new HashMap<>();
+        routeParams.put(SERVICENAME, "RouteToSetCookieService");
+        routeParams.put(SERVICEURL, "/routeToSetCookieService");
+        routeParams.put(SERVICEPOLICY, routePolicy);
+        createdServiceIds.add(createServiceFromTemplate(routeParams));
+
+        final HardcodedResponseAssertion templateResponseAssertion = new HardcodedResponseAssertion();
+        templateResponseAssertion.setResponseContentType("text/plain");
+        templateResponseAssertion.setBase64ResponseBody(HexUtils.encodeBase64(new String("${request.http.allheadervalues}").getBytes()));
+        final AddHeaderAssertion addHeaderAssertion = new AddHeaderAssertion();
+        addHeaderAssertion.setTarget(TargetMessageType.RESPONSE);
+        addHeaderAssertion.setHeaderName("Set-Cookie");
+        addHeaderAssertion.setHeaderValue("foo=bar");
+        final String setCookiePolicy = WspWriter.getPolicyXml(new AllAssertion(Arrays.asList(new Assertion[]{templateResponseAssertion, addHeaderAssertion})));
+        final Map<String, String> setCookieParams = new HashMap<>();
+        setCookieParams.put(SERVICENAME, "SetCookieService");
+        setCookieParams.put(SERVICEURL, "/setCookieService");
+        setCookieParams.put(SERVICEPOLICY, setCookiePolicy);
+        createdServiceIds.add(createServiceFromTemplate(setCookieParams));
+
+        final GenericHttpResponse response = sendRequest(new GenericHttpRequestParams(new URL("http://" + BASE_URL + ":8080/routeToSetCookieService")), HttpMethod.GET, null);
+        printResponseDetails(response);
+        assertEquals(200, response.getStatus());
+        final Map<String, Collection<String>> responseHeaders = getResponseHeaders(response);
+        assertSingleHeaderValue(responseHeaders, "Set-Cookie", "foo=bar; Domain=" + BASE_URL + "; Path=/routeToSetCookieService");
+    }
+
+    private static String createServiceFromTemplate(final Map<String, String> toReplace) throws Exception {
+        final InputStream resourceStream = HttpRoutingIntegrationTest.class.getClassLoader().getResourceAsStream(SERVICE_TEMPLATE_RESOURCE);
+        String request = new String(IOUtils.slurpStream(resourceStream));
+        for (final Map.Entry<String, String> entry : toReplace.entrySet()) {
+            request = request.replace(entry.getKey(), entry.getValue());
+        }
+        final GenericHttpResponse response = callWsman(request, CREATE_ACTION);
+        final Document doc = XmlUtil.parse(response.getInputStream());
+        final String id = getValue(doc, "//wsman:Selector");
+        System.out.println("Created Service from resource " + SERVICE_TEMPLATE_RESOURCE + " with id=" + id);
+        return id;
     }
 
     private Map<String, Collection<String>> getRoutedHeaders(final String responseBody) throws IOException {
@@ -144,12 +261,11 @@ public class HttpRoutingIntegrationTest {
         System.out.println("Response body:");
         final String responseBody = new String(IOUtils.slurpStream(response.getInputStream()));
         System.out.println(responseBody);
-        System.out.println("============");
         return responseBody;
     }
 
-    private String createService(final String resource) throws Exception {
-        final InputStream resourceStream = this.getClass().getClassLoader().getResourceAsStream(resource);
+    private static String createService(final String resource) throws Exception {
+        final InputStream resourceStream = HttpRoutingIntegrationTest.class.getClassLoader().getResourceAsStream(resource);
         final String request = new String(IOUtils.slurpStream(resourceStream));
         final GenericHttpResponse response = callWsman(request, CREATE_ACTION);
         final Document doc = XmlUtil.parse(response.getInputStream());
@@ -158,8 +274,8 @@ public class HttpRoutingIntegrationTest {
         return id;
     }
 
-    private String createRoutingService(final String resource, final String routeUrl) throws Exception {
-        final InputStream resourceStream = this.getClass().getClassLoader().getResourceAsStream(resource);
+    private static String createRoutingService(final String resource, final String routeUrl) throws Exception {
+        final InputStream resourceStream = HttpRoutingIntegrationTest.class.getClassLoader().getResourceAsStream(resource);
         final String baseRequest = new String(IOUtils.slurpStream(resourceStream));
         final String request = baseRequest.replaceAll("===ROUTEURL===", routeUrl);
         final GenericHttpResponse response = callWsman(request, CREATE_ACTION);
@@ -169,21 +285,22 @@ public class HttpRoutingIntegrationTest {
         return id;
     }
 
-    private void deleteService(@NotNull final String id) throws Exception {
-        final InputStream resourceStream = this.getClass().getClassLoader().getResourceAsStream(DELTE_SERVICE_RESOURCE);
+    private static void deleteService(@NotNull final String id) throws Exception {
+        final InputStream resourceStream = HttpRoutingIntegrationTest.class.getClassLoader().getResourceAsStream(DELETE_SERVICE_RESOURCE);
         final String baseRequest = new String(IOUtils.slurpStream(resourceStream));
         final String request = baseRequest.replaceAll("===SERVICEID===", id);
         callWsman(request, DELETE_ACTION);
         System.out.println("Deleted Service with id=" + id);
     }
 
-    protected String getValue(final Document document, final String xpathExpression) throws Exception {
+    private static String getValue(final Document document, final String xpathExpression) throws Exception {
         final XPathExpression expression = xPath.compile(xpathExpression);
         final Node node = (Node) expression.evaluate(document, XPathConstants.NODE);
         return node.getTextContent();
     }
 
-    private GenericHttpResponse sendRequest(final GenericHttpRequestParams params, final HttpMethod method, final String body) throws Exception {
+    private static GenericHttpResponse sendRequest(final GenericHttpRequestParams params, final HttpMethod method, final String body) throws Exception {
+        System.out.println("======Sending request to " + params.getTargetUrl() + "======");
         final GenericHttpRequest request = client.createRequest(method, params);
         if (body != null) {
             request.setInputStream(new ByteArrayInputStream(body.getBytes()));
@@ -191,7 +308,7 @@ public class HttpRoutingIntegrationTest {
         return request.getResponse();
     }
 
-    private GenericHttpResponse callWsman(final String requestBody, final String soapAction) throws Exception {
+    private static GenericHttpResponse callWsman(final String requestBody, final String soapAction) throws Exception {
         final GenericHttpRequestParams params = new GenericHttpRequestParams(new URL("https://" + BASE_URL + ":8443/wsman"));
         params.setPasswordAuthentication(PASSWORD_AUTHENTICATION);
         params.setSslSocketFactory(getSSLSocketFactory());
