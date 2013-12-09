e1b079f94143a2495c92d7d455b7e6d098d45040
mlyons <mlyons@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2013-12-09 23:02:31 +0000

    Fix test that doesn't compile on Windows-1252 encoding and doesn't succeed unless system file.encoding is UTF-8.  It now compiles on Windows, and at runtime temporarily switches the encoding to UTF-8 so that the test passes.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@38991 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/siteminder/src/test/java/com/ca/siteminder/util/SiteMinderUtilTest.java
+++ modules/siteminder/src/test/java/com/ca/siteminder/util/SiteMinderUtilTest.java
@@ -4,11 +4,15 @@ package com.ca.siteminder.util;
 import com.l7tech.gateway.common.siteminder.SiteMinderFipsModeOption;
 import com.l7tech.gateway.common.siteminder.SiteMinderHost;
 import com.l7tech.test.BugId;
+import com.l7tech.util.Charsets;
 import com.l7tech.util.FileUtils;
+import com.l7tech.util.HexUtils;
+import com.l7tech.util.SyspropUtil;
 import netegrity.siteminder.javaagent.Attribute;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
-
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -20,13 +24,13 @@ import static junit.framework.Assert.assertNotNull;
 
 public class SiteMinderUtilTest {
 
-    public static final String HOST_NAME = "юра-тест";
+    public static final String HOST_NAME = new String(HexUtils.decodeBase64("0Y7RgNCwLdGC0LXRgdGC"), Charsets.UTF8);
 
     private static final String SMHOST_FILE = "#NOTE: PKCS11 crypto provider is deprecated. Please use ETPKI instead. (/tmp/SMHOST3917880917627939729.tmp/smHost.conf)\n" +
             "#This file contains bootstrap information required by the SiteMinder Agent API to connect to Policy Servers at startup.\n" +
             "#Be sure the IP addresses and ports below identify valid listening Policy Servers.\n" +
             "#Please do not edit the encrypted SharedSecret entry.\n" +
-            "hostname=\"юра-тест\"\n" +
+            "hostname=\"" + HOST_NAME + "\"\n" +
             "sharedsecret=\"{RC2}V3Z2avTEt2FfxCg5foFfwSQt1b6ViV40myJ/aY/F7o9z0M0Px5CRBLQCjlofD4FyOyE2pU+UQ4j6cv9cXD041H9fGqNhRglbd/ow3zZFu+FMkZn++DY1p9W5mNBy/uhoNOk18+ZgJe7YmZAMcfSbD27TMTY7sJMwF+XDK4Od/Av3YxG8F8KJ8XSsRI8sE3Mc\"\n" +
             "sharedsecrettime=\"0\"\n" +
             "enabledynamichco=\"NO\"\n" +
@@ -38,6 +42,25 @@ public class SiteMinderUtilTest {
             "fipsmode=\"COMPAT\"\n" +
             "\n" +
             "# <EOF>\n";
+
+    private static String oldFileEncoding;
+
+    @BeforeClass
+    public static void changeEncoding() {
+        // This test must run with a file.encoding of UTF-8
+        oldFileEncoding = SyspropUtil.getProperty("file.encoding");
+        SyspropUtil.setProperty("file.encoding", "UTF-8");
+    }
+
+    @AfterClass
+    public static void restoreEncoding() {
+        // Restore the previous file.encoding for the other tests
+        if (oldFileEncoding != null && oldFileEncoding.length() > 0) {
+            SyspropUtil.setProperty("file.encoding", oldFileEncoding);
+        } else {
+            SyspropUtil.clearProperty("file.encoding");
+        }
+    }
 
     @Ignore("Requires connection to the SiteMinder Policy Server")
     @Test
