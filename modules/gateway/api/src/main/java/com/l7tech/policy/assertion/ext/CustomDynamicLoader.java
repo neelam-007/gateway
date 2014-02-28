a6a7da90d77beac5167c3304a4327efbf4f30f95
tveninov <tveninov@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2014-02-28 23:30:03 +0000

    Custom Assertion Enhancements: Ability to change a Custom Assertion on the Gateway without restarting (i.e. hot swap):  - small javadoc fix
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@39670 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/api/src/main/java/com/l7tech/policy/assertion/ext/CustomDynamicLoader.java
+++ modules/gateway/api/src/main/java/com/l7tech/policy/assertion/ext/CustomDynamicLoader.java
@@ -4,7 +4,7 @@ package com.l7tech.policy.assertion.ext;
  * Implement this interface to indicate that this custom assertion can be dynamically loaded, reloaded or unloaded,
  * while SSG is still running.
  * <p/>
- * It's very important to properly implement {@link #onUnload()} method to free all resources allocated by the custom assertion<br/>
+ * It's very important to properly implement {@link #onUnload(ServiceFinder)} method to free all resources allocated by the custom assertion<br/>
  * It's the responsibility of the custom assertion to guarantee that all allocated resources are freed accordingly,
  * once the method is executed, otherwise there is a potential risk of memory leaks.
  */
