9b883ac672d06d8dbecb23c26ca403e882afb9c7
wlui <wlui@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2014-04-29 20:38:38 +0000

    SSG-8338: code cleanup, fix unit test
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@40188 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/server/src/main/java/com/l7tech/server/licensing/BootstrapLicenseService.java
+++ modules/gateway/server/src/main/java/com/l7tech/server/licensing/BootstrapLicenseService.java
@@ -7,12 +7,14 @@ import com.l7tech.server.event.system.Started;
 import com.l7tech.server.util.PostStartupApplicationListener;
 import com.l7tech.util.ConfigFactory;
 import com.l7tech.util.ExceptionUtils;
+import com.l7tech.util.IOUtils;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.nio.file.Files;
 import java.util.concurrent.Callable;
 import java.util.logging.Logger;
 
@@ -20,13 +22,14 @@ import java.util.logging.Logger;
  * At gateway startup, if no license is installed, then try loading license file found in "bootstrap.folder.license"
  */
 public class BootstrapLicenseService implements PostStartupApplicationListener {
-    private static String BOOTSTRAP_LICENSE_FOLDER = ConfigFactory.getProperty("bootstrap.folder.license");
+    private final String BOOTSTRAP_LICENSE_FOLDER;
 
     private LicenseDocumentManager licenseDocumentManager;
     private final Logger logger = Logger.getLogger(getClass().getName());
 
     public BootstrapLicenseService(LicenseDocumentManager licenseDocumentManager) {
         this.licenseDocumentManager = licenseDocumentManager;
+        BOOTSTRAP_LICENSE_FOLDER = ConfigFactory.getProperty("bootstrap.folder.license");
     }
 
     @Override
@@ -41,32 +44,29 @@ public class BootstrapLicenseService implements PostStartupApplicationListener {
             AdminInfo.find(false).wrapCallable(new Callable<LicenseDocument>() {
                 @Override
                 public LicenseDocument call() throws Exception {
-
-                    if (licenseDocumentManager.findAll().isEmpty() && BOOTSTRAP_LICENSE_FOLDER != null) {
-                        File licenseFolder = new File(BOOTSTRAP_LICENSE_FOLDER);
-                        if (!licenseFolder.exists()) return null;
-                        if (!licenseFolder.isDirectory()) return null;
-                        File[] licenseFiles = licenseFolder.listFiles();
-                        if (licenseFiles == null || licenseFiles.length == 0) return null;
-                        if (licenseFiles.length > 1) {
-                            logger.warning("More than one license found.");
-                        }
-                        // attempt to load license from file
-                        logger.info("Installing license from: " + licenseFiles[0].getCanonicalPath());
-                        InputStream is = new FileInputStream(licenseFiles[0]);
-                        try {
-                            String licenseXml = XmlUtil.nodeToString(XmlUtil.parse(is));
-                            is.close();
-
-                            final LicenseDocument licenseDocument = new LicenseDocument(licenseXml);
-                            licenseDocumentManager.saveWithImmediateFlush(licenseDocument);
-                            return licenseDocument;
-                        } finally {
-                            is.close();
-                        }
+                if (licenseDocumentManager.findAll().isEmpty() && BOOTSTRAP_LICENSE_FOLDER != null) {
+                    File licenseFolder = new File(BOOTSTRAP_LICENSE_FOLDER);
+                    if (!licenseFolder.exists()) return null;
+                    if (!licenseFolder.isDirectory()) return null;
+                    File[] licenseFiles = licenseFolder.listFiles();
+                    if (licenseFiles == null || licenseFiles.length == 0) return null;
+                    if (licenseFiles.length > 1) {
+                        logger.warning("More than one license found in " + licenseFolder.getCanonicalPath());
+                        return null;
                     }
-
-                    return null;
+                    // attempt to load license from file
+                    logger.info("Installing license from: " + licenseFiles[0].getCanonicalPath());
+                    InputStream is = new FileInputStream(licenseFiles[0]);
+                    try {
+                        byte[] docBytes = IOUtils.slurpStream(is);
+                        final LicenseDocument licenseDocument = new LicenseDocument(new String(docBytes));
+                        licenseDocumentManager.saveWithImmediateFlush(licenseDocument);
+                        return licenseDocument;
+                    } finally {
+                        is.close();
+                    }
+                }
+                return null;
                 }
             }).call();
         } catch (Exception e) {
--- modules/gateway/server/src/test/java/com/l7tech/server/licensing/LicenseBootstrapServiceTest.java
+++ modules/gateway/server/src/test/java/com/l7tech/server/licensing/LicenseBootstrapServiceTest.java
@@ -2,29 +2,54 @@ package com.l7tech.server.licensing;
 
 import com.l7tech.gateway.common.Component;
 import com.l7tech.server.event.system.Started;
+import com.l7tech.util.FileUtils;
 import com.l7tech.util.SyspropUtil;
-import org.junit.Ignore;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
 import java.net.URL;
 
 import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertNotNull;
-import static junit.framework.Assert.assertTrue;
 
 public class LicenseBootstrapServiceTest {
 
-    @Ignore
+    private File tmpDir;
+
+    @Before
+    public void setUp() throws Exception {
+        tmpDir = FileUtils.createTempDirectory("bootstrapLicense", null, null, false);
+        SyspropUtil.setProperty("com.l7tech.bootstrap.folder.license", tmpDir.getPath());
+    }
+
+    @After
+    public void after() throws Exception {
+        SyspropUtil.clearProperty("com.l7tech.bootstrap.folder.license");
+        FileUtils.deleteDir(tmpDir);
+    }
+
     @Test
-    public void Test() throws Exception{
-        URL license = this.getClass().getResource("bootstrap/ValidLicenseDocument.xml");
-        assertNotNull(license);
-        File licenseFile = new File(license.getFile());
-        SyspropUtil.setProperty("com.l7tech.bootstrap.folder.license",licenseFile.getParent() );
+    public void TestLoadLicense() throws Exception{
+        URL otherLicense = LicenseBootstrapServiceTest.class.getResource("ValidLicenseDocumentWithoutSignature.xml");
+        File serviceFile = new File(tmpDir,"license.xml");
+        FileUtils.copyFile(new File(otherLicense.getFile()),serviceFile);
+
         LicenseDocumentManager docManager = new LicenseDocumentManagerStub();
         BootstrapLicenseService service = new BootstrapLicenseService(docManager);
         service.onApplicationEvent(new Started(this, Component.GATEWAY, "Test"));
         assertEquals(1, docManager.findAll().size());
+    }
+
+    @Test
+    public void TestMoreThanOneLicense() throws Exception{
+        URL otherLicense = LicenseBootstrapServiceTest.class.getResource("ValidLicenseDocumentWithoutSignature.xml");
+        FileUtils.copyFile(new File(otherLicense.getFile()), new File(tmpDir,"license.xml"));
+        FileUtils.copyFile(new File(otherLicense.getFile()), new File(tmpDir,"license2.xml"));
+
+        LicenseDocumentManager docManager = new LicenseDocumentManagerStub();
+        BootstrapLicenseService service = new BootstrapLicenseService(docManager);
+        service.onApplicationEvent(new Started(this, Component.GATEWAY, "Test"));
+        assertEquals(0, docManager.findAll().size());
     }
 }
