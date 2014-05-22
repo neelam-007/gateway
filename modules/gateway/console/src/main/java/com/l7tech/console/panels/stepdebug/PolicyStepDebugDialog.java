7ad7661723563c9deed5ccfd8f7de718c644cae7
kpak <kpak@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2014-05-22 17:55:06 +0000

    SSM-4614: Service Debugger - Make the task icon of the debugger dialog in the taskbar flash when the debugger is updated. The task icon won't flash if the debugger dialog already has focus.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@40347 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/console/src/main/java/com/l7tech/console/panels/stepdebug/PolicyStepDebugDialog.java
+++ modules/gateway/console/src/main/java/com/l7tech/console/panels/stepdebug/PolicyStepDebugDialog.java
@@ -344,6 +344,7 @@ public class PolicyStepDebugDialog extends JFrame {
                     @Override
                     public void run() {
                         updateComponents();
+                        toFront();
                     }
                 });
             }
