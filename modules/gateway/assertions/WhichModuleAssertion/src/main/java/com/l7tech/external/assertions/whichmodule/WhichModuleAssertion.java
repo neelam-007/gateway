389ac02a0981d3bd22be9902664e0d1f152ff2e1
vkazakov <vkazakov@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2014-06-13 01:19:17 +0000

    SSG-7847 SSG-8649 - REST Management API  - adding a reference to a generic entity from the which module assertion to help with testing.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@40484 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/assertions/WhichModuleAssertion/src/main/java/com/l7tech/external/assertions/whichmodule/WhichModuleAssertion.java
+++ modules/gateway/assertions/WhichModuleAssertion/src/main/java/com/l7tech/external/assertions/whichmodule/WhichModuleAssertion.java
@@ -10,6 +10,8 @@ import com.l7tech.gateway.common.security.rbac.Secured;
 import com.l7tech.gui.util.DialogDisplayer;
 import com.l7tech.gui.util.ImageCache;
 import com.l7tech.gui.util.Utilities;
+import com.l7tech.objectmodel.FindException;
+import com.l7tech.objectmodel.Goid;
 import com.l7tech.policy.AssertionPath;
 import com.l7tech.policy.PolicyValidatorResult;
 import com.l7tech.policy.assertion.*;
@@ -18,10 +20,11 @@ import com.l7tech.policy.validator.PolicyValidationContext;
 import com.l7tech.policy.variable.DataType;
 import com.l7tech.policy.variable.VariableMetadata;
 import com.l7tech.policy.wsp.WspReader;
+import com.l7tech.search.Dependency;
 import com.l7tech.server.message.PolicyEnforcementContext;
-import com.l7tech.server.policy.module.ModularAssertionModule;
 import com.l7tech.server.policy.ServerAssertionRegistry;
 import com.l7tech.server.policy.assertion.AbstractServerAssertion;
+import com.l7tech.server.policy.module.ModularAssertionModule;
 import com.l7tech.util.ExceptionUtils;
 import com.l7tech.util.Functions;
 import org.springframework.context.ApplicationContext;
@@ -51,6 +54,7 @@ public class WhichModuleAssertion extends Assertion implements SetsVariables {
     protected static final Logger logger = Logger.getLogger(WhichModuleAssertion.class.getName());
 
     private String assertionXml = "";
+    private Goid demoGenericGoid;
 
     public String getAssertionXml() {
         return assertionXml;
@@ -64,7 +68,7 @@ public class WhichModuleAssertion extends Assertion implements SetsVariables {
 
     @Override
     public VariableMetadata[] getVariablesSet() {
-        return new VariableMetadata[] {
+        return new VariableMetadata[]{
                 new VariableMetadata("module.exists", false, false, null, false, DataType.BOOLEAN),
                 new VariableMetadata("module.name", false, false, null, false, DataType.STRING),
                 new VariableMetadata("module.sha1", false, false, null, false, DataType.STRING),
@@ -77,19 +81,19 @@ public class WhichModuleAssertion extends Assertion implements SetsVariables {
         DefaultAssertionMetadata meta = defaultMeta();
         meta.put(AssertionMetadata.DESCRIPTION, "Gets info about the module that offers the specified assertion (identified by its XML representation); fails if it is unrecognized");
         meta.put(AssertionMetadata.PALETTE_NODE_ICON, "com/l7tech/console/resources/ServerLogs.gif");
-        meta.put(AssertionMetadata.PALETTE_FOLDERS, new String[] { "policyLogic" });
+        meta.put(AssertionMetadata.PALETTE_FOLDERS, new String[]{"policyLogic"});
         meta.put(AssertionMetadata.POLICY_ADVICE_CLASSNAME, "auto");
         meta.put(AssertionMetadata.SERVER_ASSERTION_CLASSNAME, getClass().getName() + "$ServerImpl");
         meta.put(AssertionMetadata.PROPERTIES_EDITOR_CLASSNAME, getClass().getName() + "$PropDialog");
         meta.put(AssertionMetadata.POLICY_VALIDATOR_CLASSNAME, getClass().getName() + "$Validator");
-        meta.put(AssertionMetadata.GLOBAL_ACTION_CLASSNAMES, new String[] { getClass().getName() + "$CustomAction" });
-        meta.put(AssertionMetadata.EXTENSION_INTERFACES_FACTORY, new Functions.Unary< Collection<ExtensionInterfaceBinding>, ApplicationContext >() {
+        meta.put(AssertionMetadata.GLOBAL_ACTION_CLASSNAMES, new String[]{getClass().getName() + "$CustomAction"});
+        meta.put(AssertionMetadata.EXTENSION_INTERFACES_FACTORY, new Functions.Unary<Collection<ExtensionInterfaceBinding>, ApplicationContext>() {
             @Override
             public Collection<ExtensionInterfaceBinding> call(ApplicationContext appContext) {
                 ExtensionInterfaceBinding binding = new ExtensionInterfaceBinding<DemoExtensionInterface>(DemoExtensionInterface.class, null, new DemoExtensionInterface() {
                     @Override
                     public DemoReturnVal[] demoHello(DemoArgument[] args) {
-                        return new DemoReturnVal[] { new DemoReturnVal("Hello from Gateway anon inner class!  You sent: " + args[0].s) };
+                        return new DemoReturnVal[]{new DemoReturnVal("Hello from Gateway anon inner class!  You sent: " + args[0].s)};
                     }
                 });
                 return Collections.singletonList(binding);
@@ -97,6 +101,15 @@ public class WhichModuleAssertion extends Assertion implements SetsVariables {
         });
 
         return meta;
+    }
+
+    @Dependency(methodReturnType = Dependency.MethodReturnType.GOID, type = Dependency.DependencyType.GENERIC)
+    public Goid getDemoGenericGoid() {
+        return demoGenericGoid;
+    }
+
+    public void setDemoGenericGoid(Goid demoGenericGoid) {
+        this.demoGenericGoid = demoGenericGoid;
     }
 
     @SuppressWarnings({"UnusedDeclaration"})
@@ -108,7 +121,7 @@ public class WhichModuleAssertion extends Assertion implements SetsVariables {
         @Override
         public void actionPerformed(ActionEvent e) {
             final DemoExtensionInterface demoInterface = Registry.getDefault().getExtensionInterface(DemoExtensionInterface.class, null);
-            DemoExtensionInterface.DemoReturnVal[] response = demoInterface.demoHello(new DemoExtensionInterface.DemoArgument[] { new DemoExtensionInterface.DemoArgument("Info from SSM packed inside DemoArgument") });
+            DemoExtensionInterface.DemoReturnVal[] response = demoInterface.demoHello(new DemoExtensionInterface.DemoArgument[]{new DemoExtensionInterface.DemoArgument("Info from SSM packed inside DemoArgument")});
             DialogDisplayer.showMessageDialog(TopComponents.getInstance().getTopParent(), "Which Module Assertion .AAR File", "Response from Gateway from DemoReturnVal: " + response[0].v, null);
         }
     }
@@ -116,14 +129,14 @@ public class WhichModuleAssertion extends Assertion implements SetsVariables {
     /**
      * A sample interface to demo use of an admin extension interface.
      */
-    @Transactional(propagation=Propagation.REQUIRED, rollbackFor=Throwable.class)
+    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Throwable.class)
     @Administrative
-    @Secured(types=TRUSTED_CERT)
+    @Secured(types = TRUSTED_CERT)
     public static interface DemoExtensionInterface {
 
         // Demonstrate passing arguments and return values that are themselves classes loaded from the .aar
         // Naturally, passing serializable JDK classes works as well, as does passing classes shared between the core gateway and SSM (as long as they are serializable)
-        @Secured(stereotype=DELETE_MULTI, types=SSG_KEY_ENTRY)
+        @Secured(stereotype = DELETE_MULTI, types = SSG_KEY_ENTRY)
         public DemoReturnVal[] demoHello(DemoArgument[] args);
 
         public static class DemoArgument implements Serializable {
@@ -182,7 +195,7 @@ public class WhichModuleAssertion extends Assertion implements SetsVariables {
                 return AssertionStatus.NONE;
 
             } catch (IOException e) {
-                logAndAudit(AssertionMessages.EXCEPTION_INFO_WITH_MORE_INFO, new String[] { ExceptionUtils.getMessage(e) }, e);
+                logAndAudit(AssertionMessages.EXCEPTION_INFO_WITH_MORE_INFO, new String[]{ExceptionUtils.getMessage(e)}, e);
                 return AssertionStatus.SERVER_ERROR;
             }
         }
@@ -190,12 +203,19 @@ public class WhichModuleAssertion extends Assertion implements SetsVariables {
 
     public static class PropDialog extends AssertionPropertiesEditorSupport<WhichModuleAssertion> {
         private JTextArea field = new JTextArea();
+        private JComboBox<String> demoGenericEntityCombo = new JComboBox<>();
         private JButton ok;
         private boolean confirmed = false;
 
         public PropDialog(Window parent, WhichModuleAssertion bean) {
+
             super(parent, "Assertion XML");
             JPanel panel = new JPanel(new BorderLayout());
+            JPanel contentPanel = new JPanel(new FlowLayout());
+            contentPanel.add(new JLabel("Demo Generic Entity: "));
+            contentPanel.add(demoGenericEntityCombo);
+            panel.add(contentPanel, BorderLayout.NORTH);
+            panel.add(new JScrollPane(field), BorderLayout.CENTER);
             panel.add(new JScrollPane(field), BorderLayout.CENTER);
             panel.add(makeButtons(), BorderLayout.SOUTH);
             setContentPane(panel);
@@ -207,7 +227,7 @@ public class WhichModuleAssertion extends Assertion implements SetsVariables {
         private JPanel makeButtons() {
             ok = new JButton("OK");
             JButton cancel = new JButton("Cancel");
-            Utilities.equalizeButtonSizes(new JButton[] { ok, cancel });
+            Utilities.equalizeButtonSizes(new JButton[]{ok, cancel});
             Utilities.setEscKeyStrokeDisposes(this);
             getRootPane().setDefaultButton(ok);
 
@@ -245,17 +265,47 @@ public class WhichModuleAssertion extends Assertion implements SetsVariables {
                 // can't happen
             }
             field.setText(xml);
+
+            DefaultComboBoxModel<String> comboBoxModel = new DefaultComboBoxModel<>();
+            try {
+                Collection<DemoGenericEntity> demoGenericEntities = getEntityManager().findAll();
+                comboBoxModel.addElement("<Select Entity>");
+                for (DemoGenericEntity demoGenericEntity : demoGenericEntities) {
+                    comboBoxModel.addElement(demoGenericEntity.getName() + ":" + demoGenericEntity.getId());
+                }
+            } catch (FindException e) {
+                comboBoxModel.removeAllElements();
+                comboBoxModel.addElement("Error Listing Entities.");
+            }
+            if (assertion.getDemoGenericGoid() != null) {
+                for (int i = 1; i < comboBoxModel.getSize(); i++) {
+                    if (assertion.getDemoGenericGoid().toString().equals(comboBoxModel.getElementAt(i).split(":")[1])) {
+                        comboBoxModel.setSelectedItem(comboBoxModel.getElementAt(i));
+                        break;
+                    }
+                }
+            }
+            demoGenericEntityCombo.setModel(comboBoxModel);
+        }
+
+        private static DemoGenericEntityAdmin getEntityManager() {
+            return Registry.getDefault().getExtensionInterface(DemoGenericEntityAdmin.class, null);
         }
 
         @Override
         public WhichModuleAssertion getData(WhichModuleAssertion assertion) {
             assertion.setAssertionXml(field.getText());
+            if (demoGenericEntityCombo.getSelectedIndex() > 0) {
+                assertion.setDemoGenericGoid(Goid.parseGoid(demoGenericEntityCombo.getSelectedItem().toString().split(":")[1]));
+            } else {
+                assertion.setDemoGenericGoid(null);
+            }
             return assertion;
         }
 
         @Override
         protected void configureView() {
-            ok.setEnabled( !isReadOnly() );
+            ok.setEnabled(!isReadOnly());
         }
     }
 
@@ -278,7 +328,7 @@ public class WhichModuleAssertion extends Assertion implements SetsVariables {
                 result.addError(
                         new PolicyValidatorResult.Error(assertion,
                                 "Invalid policy assertion XML: " + ExceptionUtils.getMessage(xmlProblem),
-                                                        xmlProblem));
+                                xmlProblem));
         }
     }
 }
