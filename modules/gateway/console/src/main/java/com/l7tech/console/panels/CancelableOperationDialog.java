065139b352e3d61260fe5c0a548f6d152fb87d44
mlyons <mlyons@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2014-07-23 22:01:06 +0000

    SSM-4692: Change cancelable operation dialog to be application modal rather than document modal to make it less likely to get stuck behind other dialogs.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@40779 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/console/src/main/java/com/l7tech/console/panels/CancelableOperationDialog.java
+++ modules/gateway/console/src/main/java/com/l7tech/console/panels/CancelableOperationDialog.java
@@ -30,7 +30,7 @@ public class CancelableOperationDialog extends JDialog {
     }
 
     public CancelableOperationDialog(Window owner, String title, String message, JProgressBar progressBar) {
-        super(owner, title, ModalityType.DOCUMENT_MODAL);
+        super(owner, title, ModalityType.APPLICATION_MODAL);
         this.messageLabel.setText(message);
         this.progressBar = progressBar;
         if ( owner==null ) {
@@ -159,7 +159,7 @@ public class CancelableOperationDialog extends JDialog {
                 progressBar.setIndeterminate(true);
                 final CancelableOperationDialog cancelDialog =
                         new CancelableOperationDialog(parent, dialogTitle, dialogMessage, progressBar);
-                cancelDialog.setModalityType(ModalityType.DOCUMENT_MODAL);
+                cancelDialog.setModalityType(ModalityType.APPLICATION_MODAL);
                 return cancelDialog;
             }
         });
