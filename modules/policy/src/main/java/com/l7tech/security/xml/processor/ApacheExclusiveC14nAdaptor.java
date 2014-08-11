c7dced983abbd354084274a01962d4608421e328
mlyons <mlyons@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2014-08-11 22:59:45 +0000

    SSG-9068: Simplified the ApacheExclusiveC14nAdaptor.  It now just calls the Apache library's init method and no longer contains a complex initialization hack that involves forcing the library to verify a test signature to put it through its paces.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/branches/icefish_component_upgrades@40892 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/policy/src/main/java/com/l7tech/security/xml/processor/ApacheExclusiveC14nAdaptor.java
+++ modules/policy/src/main/java/com/l7tech/security/xml/processor/ApacheExclusiveC14nAdaptor.java
@@ -35,10 +35,12 @@ public class ApacheExclusiveC14nAdaptor extends Transform implements Canonicaliz
 
     private String prefixList = null;
 
+    static {
+        org.apache.xml.security.Init.init();
+    }
+
     private static org.apache.xml.security.c14n.Canonicalizer makeApacheC11r() {
         try {
-            if (!apacheInit.get())
-                initApacheC11r();
             return org.apache.xml.security.c14n.Canonicalizer.getInstance("http://www.w3.org/2001/10/xml-exc-c14n#");
         } catch (InvalidCanonicalizerException e) {
             throw new RuntimeException("Unable to initialize Apache exclusive canonicalizer: " + ExceptionUtils.getMessage(e), e);
@@ -121,48 +123,6 @@ public class ApacheExclusiveC14nAdaptor extends Transform implements Canonicaliz
             outputStream.write(result);
         } catch (CanonicalizationException e) {
             throw new IOException(e);
-        }
-    }
-
-    private static AtomicBoolean apacheInit = new AtomicBoolean(false);
-
-    private static final String MINIMAL_SIGNED_XML =
-            "<SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\"><SOAP-ENV:Header><wsse:Security xmlns:wsse=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd\" xmlns:wsu=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd\" SOAP-ENV:mustUnderstand=\"1\"><wsu:Timestamp wsu:Id=\"Timestamp-1-905e2dc529da31a389276ff3bfdaddda\"><wsu:Created>2008-11-17T23:26:43.164566982Z</wsu:Created><wsu:Expires>2008-11-17T23:31:43.164Z</wsu:Expires></wsu:Timestamp><wsse:BinarySecurityToken EncodingType=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary\" ValueType=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-x509-token-profile-1.0#X509v3\" wsu:Id=\"BinarySecurityToken-0-410c34e0a37b8d5b555a2383b0f88e49\">MIIBizCCATWgAwIBAgIJAJXEos4jEooJMA0GCSqGSIb3DQEBBQUAMA8xDTALBgNVBAMMBHRlc3QwHhcNMDgxMTE3MjMxNjQyWhcNMjgxMTEyMjMxNjQyWjAPMQ0wCwYDVQQDDAR0ZXN0MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAKagK9wVwmqzPPFkZiNuotholM7mcsM8lU25vxJaGhE/hJfZADiFk+LEOfM9MR9AOzM7aeMNJpB0YtcperCeDocCAwEAAaN0MHIwDAYDVR0TAQH/BAIwADAOBgNVHQ8BAf8EBAMCBeAwEgYDVR0lAQH/BAgwBgYEVR0lADAdBgNVHQ4EFgQUwrBRJLFI4+UP6o3FsnUzqhk422gwHwYDVR0jBBgwFoAUwrBRJLFI4+UP6o3FsnUzqhk422gwDQYJKoZIhvcNAQEFBQADQQAxPYfzI0y5KCFAHiwk4pAwvB+zPo1b0/rlH4e6QaQssNffxrjg3akFzKBIXIFHLc78s75rgy2UljAhCm0llMGy</wsse:BinarySecurityToken><ds:Signature xmlns:ds=\"http://www.w3.org/2000/09/xmldsig#\"><ds:SignedInfo><ds:CanonicalizationMethod Algorithm=\"http://www.w3.org/2001/10/xml-exc-c14n#\"></ds:CanonicalizationMethod><ds:SignatureMethod Algorithm=\"http://www.w3.org/2000/09/xmldsig#rsa-sha1\"></ds:SignatureMethod><ds:Reference URI=\"#Timestamp-1-905e2dc529da31a389276ff3bfdaddda\"><ds:Transforms><ds:Transform Algorithm=\"http://www.w3.org/2001/10/xml-exc-c14n#\"></ds:Transform></ds:Transforms><ds:DigestMethod Algorithm=\"http://www.w3.org/2000/09/xmldsig#sha1\"></ds:DigestMethod><ds:DigestValue>5IYzmjA3NqsM1NIatXRbEQyiPUo=</ds:DigestValue></ds:Reference></ds:SignedInfo><ds:SignatureValue>gpoeK36EAgi366WTsc9JOTWRGD734aYVxPmiJ8JGDojdVVMZNgmQcPNNduW67plIPj1ZvqzcwN7naH66biHHow==</ds:SignatureValue><ds:KeyInfo><wsse:SecurityTokenReference><wsse:Reference URI=\"#BinarySecurityToken-0-410c34e0a37b8d5b555a2383b0f88e49\" ValueType=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-x509-token-profile-1.0#X509v3\"></wsse:Reference></wsse:SecurityTokenReference></ds:KeyInfo></ds:Signature></wsse:Security></SOAP-ENV:Header><SOAP-ENV:Body></SOAP-ENV:Body></SOAP-ENV:Envelope>";
-
-    // During tests we found numerous problems using the JDK 6 dsig internal classes by themselves since they
-    // had various initialization requirements.  In case this is also the case for the Apache c11r classes,
-    // we run through a complete signature verification using the public APIs to ensure that everything is good to go
-    // from now on in the various internal static fields.
-    private synchronized static void initApacheC11r() {
-        if (!apacheInit.get()) {
-            try {
-                org.apache.xml.security.Init.init();
-
-                Document doc = XmlUtil.stringAsDocument(MINIMAL_SIGNED_XML);
-
-                NodeList allElements = doc.getElementsByTagName( "*" );
-                for ( int i = 0; i < allElements.getLength(); i++ ) {
-                    final Element elem = (Element) allElements.item( i );
-                    String id = elem.getAttribute( "wsu:Id" );
-                    if ( id != null && id.length() > 0 ) {
-                        elem.setIdAttribute( "wsu:Id", true );
-                    }
-                }
-
-                final Element sechdr = SoapUtil.getSecurityElement(doc);
-                final Element bst = DomUtils.findOnlyOneChildElementByName(sechdr, SoapUtil.SECURITY_NAMESPACE, "BinarySecurityToken");
-                final Element signature = DomUtils.findOnlyOneChildElementByName(sechdr, SoapUtil.DIGSIG_URI, "Signature");
-
-                X509Certificate signingCert = CertUtils.decodeCert(HexUtils.decodeBase64(XmlUtil.getTextValue(bst), true));
-                XMLSignature xmlSignature = new XMLSignature(signature, "");
-
-                if (!xmlSignature.checkSignatureValue(signingCert.getPublicKey()))
-                    throw new RuntimeException("Unable to initialize Apache canonicalizers: unable to validate test signature");
-
-                apacheInit.set(true);
-            } catch (Exception e) {
-                throw new RuntimeException("Unable to initialize Apache canonicalizers: " + ExceptionUtils.getMessage(e), e);
-            }
         }
     }
 
