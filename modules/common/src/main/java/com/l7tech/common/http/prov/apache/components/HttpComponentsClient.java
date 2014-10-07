789c637eea346fb91ecdb18fb015a82c1390587a
mlyons <mlyons@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2014-10-07 00:52:08 +0000

    SSB-702: Un-reverting changeset 40822 so we can give it some proper testing in Javelin, now that we have some time.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@41204 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/common/src/main/java/com/l7tech/common/http/prov/apache/components/HttpComponentsClient.java
+++ modules/common/src/main/java/com/l7tech/common/http/prov/apache/components/HttpComponentsClient.java
@@ -622,7 +622,10 @@ public class HttpComponentsClient implements RerunnableGenericHttpClient{
             char[] password = pw.getPassword();
             UsernamePasswordCredentials creds =  new UsernamePasswordCredentials(username, new String(password));
             client.getCredentialsProvider().setCredentials(AuthScope.ANY, creds);
-            if(params.isPreemptiveAuthentication()) { //set preemptive authentication
+
+            if (params.isPreemptiveAuthentication()) {
+                //set preemptive authentication
+                clientParams.setParameter(ClientPNames.HANDLE_AUTHENTICATION, false);
                 // Create AuthCache instance
                 AuthCache authCache = new BasicAuthCache();
                 // Generate BASIC scheme object and add it to the local auth cache
@@ -632,6 +635,7 @@ public class HttpComponentsClient implements RerunnableGenericHttpClient{
                 // Add AuthCache to the execution context
                 state.setAttribute(ClientContext.AUTH_CACHE, authCache);
             }
+
             clientParams.setParameter(AuthPNames.CREDENTIAL_CHARSET, ConfigFactory.getProperty(PROP_CREDENTIAL_CHARSET, DEFAULT_CREDENTIAL_CHARSET));
         } else if ( !proxyConfigured ) {
             client.getCredentialsProvider().clear();
