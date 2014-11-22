adfa58f200f98e24656cd7a6ffe71cd3479a7561
mlyons <mlyons@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2014-11-22 04:45:32 +0000

    SSM-4889: Added GUI validation for Buffer Data assertion properties dialog.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@41555 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/assertions/BufferDataAssertion/src/main/java/com/l7tech/external/assertions/bufferdata/console/BufferDataPropertiesDialog.java
+++ modules/gateway/assertions/BufferDataAssertion/src/main/java/com/l7tech/external/assertions/bufferdata/console/BufferDataPropertiesDialog.java
@@ -1,35 +1,51 @@
 package com.l7tech.external.assertions.bufferdata.console;
 
 import com.l7tech.console.panels.AssertionPropertiesOkCancelSupport;
+import com.l7tech.console.panels.TargetVariablePanel;
 import com.l7tech.console.util.ClusterPropertyCrud;
 import com.l7tech.console.util.Registry;
 import com.l7tech.external.assertions.bufferdata.BufferDataAssertion;
+import com.l7tech.gui.FilterDocument;
 import com.l7tech.gui.NumberField;
 import com.l7tech.objectmodel.FindException;
 import com.l7tech.policy.assertion.Assertion;
+import com.l7tech.policy.variable.VariableMetadata;
 
 import javax.swing.*;
 import java.awt.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 public class BufferDataPropertiesDialog extends AssertionPropertiesOkCancelSupport<BufferDataAssertion> {
     private JPanel contentPane;
     private JTextField bufferNameTextField;
-    private JTextField appendDataFromVariableTextField;
     private JTextField maximumBufferTimeTextField;
     private JTextField maximumBufferSizeTextField;
     private JTextField variablePrefixTextField;
     private JTextArea explanatoryText;
     private JScrollPane explanatoryScrollPane;
+    private JPanel targetVariablePanelPanel;
+    private TargetVariablePanel variableNameField;
 
-    public BufferDataPropertiesDialog( Frame owner, Assertion assertion ) {
+    public BufferDataPropertiesDialog( Frame owner, final Assertion assertion ) {
         super( BufferDataAssertion.class, owner, assertion, true );
         initComponents();
+
+        variableNameField.setAssertion( assertion, null );
+        addPropertyChangeListener( "policyPosition", new PropertyChangeListener() {
+            @Override
+            public void propertyChange( PropertyChangeEvent evt ) {
+                PolicyPosition pos = getPolicyPosition();
+                if ( pos != null )
+                    variableNameField.setAssertion( assertion, pos.getPreviousAssertion() );
+            }
+        } );
     }
 
     @Override
     public void setData( BufferDataAssertion a ) {
         bufferNameTextField.setText( a.getBufferName() );
-        appendDataFromVariableTextField.setText( a.getNewDataVarName() );
+        variableNameField.setVariable( a.getNewDataVarName() );
         maximumBufferSizeTextField.setText( Long.toString( a.getMaxSizeBytes() ) );
         maximumBufferTimeTextField.setText( Long.toString( a.getMaxAgeMillis() ) );
         variablePrefixTextField.setText( a.getVariablePrefix() );
@@ -38,11 +54,17 @@ public class BufferDataPropertiesDialog extends AssertionPropertiesOkCancelSuppo
     @Override
     public BufferDataAssertion getData( BufferDataAssertion a ) throws ValidationException {
         a.setBufferName( bufferNameTextField.getText() );
-        a.setNewDataVarName( appendDataFromVariableTextField.getText() );
+        String err = variableNameField.getErrorMessage();
+        if ( err != null )
+            throw new ValidationException( err );
+        a.setNewDataVarName( variableNameField.getVariable() );
         a.setExtractIfFull( true );
         a.setMaxAgeMillis( parseLong( "Maximum Buffer Time", maximumBufferTimeTextField.getText(), Long.MAX_VALUE ) );
         a.setMaxSizeBytes( parseLong( "Maximum Buffer Size", maximumBufferSizeTextField.getText(), getConfiguredGlobalBufferSizeLimit() ) );
-        a.setVariablePrefix( variablePrefixTextField.getText() );
+        String prefix = variablePrefixTextField.getText();
+        if ( !VariableMetadata.isNameValid( prefix, false ) )
+            throw new ValidationException( "Invalid prefix" );
+        a.setVariablePrefix( prefix );
         return a;
     }
 
@@ -73,8 +95,21 @@ public class BufferDataPropertiesDialog extends AssertionPropertiesOkCancelSuppo
         explanatoryScrollPane.setBackground( bg );
         explanatoryScrollPane.setOpaque( false );
 
+        variableNameField = new TargetVariablePanel();
+        variableNameField.setValueWillBeRead( true );
+        variableNameField.setValueWillBeWritten( false );
+        targetVariablePanelPanel.setLayout( new BorderLayout() );
+        targetVariablePanelPanel.add( variableNameField, BorderLayout.CENTER );
+
         maximumBufferSizeTextField.setDocument( new NumberField() );
         maximumBufferTimeTextField.setDocument( new NumberField() );
+
+        variablePrefixTextField.setDocument( new FilterDocument( 200, new FilterDocument.Filter() {
+            @Override
+            public boolean accept( String s ) {
+                return s != null && VariableMetadata.isNameValid( s );
+            }
+        } ) );
 
         return contentPane;
     }
