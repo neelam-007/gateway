db5811cbba82ac804bf0e4a9c8e2c1f0894142d6
ymoiseyenko <ymoiseyenko@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2014-12-30 19:34:37 +0000

    SSM-4928 Set hard defaults for some connection options as defined in the Cassandra and Java docs. Fixed some type inconsistencies  in the Cassandra cluster-wide properties
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@41855 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/console/src/main/java/com/l7tech/console/panels/CassandraPropertiesDialog.java
+++ modules/gateway/console/src/main/java/com/l7tech/console/panels/CassandraPropertiesDialog.java
@@ -8,14 +8,18 @@ import com.l7tech.gui.util.RunOnChangeListener;
 import com.l7tech.gui.util.Utilities;
 import com.l7tech.objectmodel.FindException;
 import com.l7tech.util.MutablePair;
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.math.NumberUtils;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CassandraPropertiesDialog extends JDialog {
+    private static final String[] hostDistance = {"LOCAL", "REMOTE", "IGNORED"};
     // Connection pooling options
     public static final String HOST_DISTANCE = "hostDistance";
     private static final String CORE_CONNECTION_PER_HOST = "coreConnectionsPerHost";
@@ -40,10 +44,13 @@ public class CassandraPropertiesDialog extends JDialog {
     private JButton okButton;
     private JButton cancelButton;
     private JComboBox propNameComboBox;
+    private JLabel propertyNameLabel;
+    private JLabel propertyValueLabel;
 
     private MutablePair<String, String> property;
     private InputValidator inputValidator;
     private boolean confirmed;
+
 
     public CassandraPropertiesDialog(Dialog owner, MutablePair<String, String> property) {
         super(owner, "Additional Properties");
@@ -59,23 +66,39 @@ public class CassandraPropertiesDialog extends JDialog {
         getRootPane().setDefaultButton(okButton);
         Utilities.setEscKeyStrokeDisposes(this);
 
+        populatePropNameComboBox();
+
         inputValidator = new InputValidator(this, this.getTitle());
-        inputValidator.constrainTextFieldToBeNonEmpty("Value", propValueTextField, new InputValidator.ValidationRule() {
+
+        inputValidator.ensureComboBoxSelection(getFieldLabelTrimmed(propertyNameLabel), propNameComboBox);
+
+        inputValidator.constrainTextFieldToBeNonEmpty(getFieldLabelTrimmed(propertyValueLabel), propValueTextField, new InputValidator.ValidationRule() {
             @Override
             public String getValidationError() {
                 String propName = (String) propNameComboBox.getSelectedItem();
-                if(QUERY_FETCH_SIZE.equals(propName)) {
-                    return getNumberValidationErrorString(propName, Integer.MAX_VALUE);
-                }
-                else if(MAX_RECORDS.equals(propName)) {
-                    return getNumberValidationErrorString(propName, 10000);
+                switch(propName) {
+                    case CORE_CONNECTION_PER_HOST:
+                    case QUERY_FETCH_SIZE:
+                        return getNumberValidationErrorString(propName, 1, Integer.MAX_VALUE);
+                    case MAX_RECORDS:
+                        return getNumberValidationErrorString(propName, 1, 10000);
+                    case MAX_CONNECTION_PER_HOST:
+                        return getNumberValidationErrorString(propName, 2, Integer.MAX_VALUE);
+                    case MAX_SIMUL_REQ_PER_CONNECTION_THRESHOLD:
+                    case MIN_SIMUL_REQ_PER_CONNECTION_THRESHOLD:
+                    case CONNECTION_TIMEOUT_MILLIS:
+                    case READ_TIMEOUT_MILLIS:
+                    case RECEIVE_BUFFER_SIZE:
+                    case SEND_BUFFER_SIZE:
+                    case SO_LINGER:
+                        return getNumberValidationErrorString(propName, 0, Integer.MAX_VALUE);
+                    case HOST_DISTANCE:
+                        return ArrayUtils.contains(hostDistance, propValueTextField.getText()) ? null : propName + " value must be one of " + ArrayUtils.toString(hostDistance);
                 }
 
                 return null;
             }
         });
-
-        populatePropNameComboBox();
 
         inputValidator.attachToButton(okButton, new ActionListener() {
             @Override
@@ -103,19 +126,64 @@ public class CassandraPropertiesDialog extends JDialog {
                         if(clusterProperty != null && clusterProperty.getValue() != null) {
                             propValueTextField.setText(clusterProperty.getValue());
                         }
+                        else {
+                            //set default values
+                            propValueTextField.setText(getDefaultValueForProperty(propName));
+                        }
                     } catch (FindException fe) {
                         JOptionPane.showMessageDialog(CassandraPropertiesDialog.this, "Unable to retrieve defaults for property " + propName, "Error", JOptionPane.ERROR_MESSAGE);
                     }
                 }
             }
         });
-//        ((JTextField)propNameComboBox.getEditor().getEditorComponent()).getDocument().addDocumentListener(propNameListener);
         propNameComboBox.addItemListener(propNameListener);
+        propNameComboBox.setSelectedIndex(-1);
     }
 
-    private String getNumberValidationErrorString(String propName, int maxVal) {
-        if(!NumberUtils.isNumber(propValueTextField.getText()) || (NumberUtils.toInt(propValueTextField.getText()) < 1 || NumberUtils.toInt(propValueTextField.getText()) > maxVal)) {
-            return propName + " value must be the number between 1 and " + Integer.toString(maxVal);
+    private static final Pattern labelPattern = Pattern.compile("^(.*)(:\\s+\\*)\\s*");
+
+    private String getFieldLabelTrimmed(JLabel label) {
+        Matcher m =labelPattern.matcher(label.getText());
+        if(m.find()) {
+            return m.group(1);
+        }
+        return label.getText();
+    }
+
+    private String getDefaultValueForProperty(String propName) {
+        if(propName == null) return null;
+        switch (propName) {
+            case HOST_DISTANCE:
+                return "LOCAL";
+            case CORE_CONNECTION_PER_HOST:
+                return "1";
+            case MAX_CONNECTION_PER_HOST:
+                return "2";
+            case MAX_SIMUL_REQ_PER_CONNECTION_THRESHOLD:
+                return "128";
+            case MIN_SIMUL_REQ_PER_CONNECTION_THRESHOLD:
+                return "25";
+            case CONNECTION_TIMEOUT_MILLIS:
+                return "5000";
+            case READ_TIMEOUT_MILLIS:
+                return "12000";
+            case KEEP_ALIVE:
+                return "false";
+            case TCP_NO_DELAY:
+                return "false";
+            case MAX_RECORDS:
+                return "10";
+            case QUERY_FETCH_SIZE:
+                return "5000";
+            default:
+                return null;
+
+        }
+    }
+
+    private String getNumberValidationErrorString(String propName, int minValue, int maxVal) {
+        if(!NumberUtils.isNumber(propValueTextField.getText()) || (NumberUtils.toInt(propValueTextField.getText()) < minValue || NumberUtils.toInt(propValueTextField.getText()) > maxVal)) {
+            return propName + " value must be the number between "+ Integer.toString(minValue) + " and " + Integer.toString(maxVal);
         }
         return null;
     }
