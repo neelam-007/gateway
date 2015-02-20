2382eab5dfcc190bf92902169431751c2391a1d6
mlyons <mlyons@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2015-02-20 07:18:34 +0000

    SSG-10858: Stack traces will now be logged for RESTMAN server assertion failures caused by exceptions if the com.l7tech.logging.debug system property is set to true.  A missing authenticated user now produces the error message "An authenticated user is required but not present".  The assertion will now work without a resolved service as long as a message variable is targeted.  (Well, possibly -- I haven't gotten far enough yet to see whether the null service ID will be a problem.)  Otherwise, when no resolved service is present in the context an error is produced: "A resolved service is required unless a message variable is targeted".  If the service routing URI does not contain any asterixes the error "Could not calculate URI" will now be produced.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@42384 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/assertions/GatewayManagementAssertion/src/main/java/com/l7tech/external/assertions/gatewaymanagement/server/ServerRESTGatewayManagementAssertion.java
+++ modules/gateway/assertions/GatewayManagementAssertion/src/main/java/com/l7tech/external/assertions/gatewaymanagement/server/ServerRESTGatewayManagementAssertion.java
@@ -18,6 +18,7 @@ import com.l7tech.server.message.AuthenticationContext;
 import com.l7tech.server.message.PolicyEnforcementContext;
 import com.l7tech.server.policy.assertion.AbstractMessageTargetableServerAssertion;
 import com.l7tech.util.CollectionUtils;
+import com.l7tech.util.ExceptionUtils;
 import com.l7tech.util.IOUtils;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -81,6 +82,10 @@ public class ServerRESTGatewayManagementAssertion extends AbstractMessageTargeta
 
         try {
             final User user = context.getDefaultAuthenticationContext().getLastAuthenticatedUser();
+            if ( null == user )
+                throw new IllegalStateException( "An authenticated user is required but not present" );
+            if ( null == context.getService() && !assertion.getTarget().equals(TargetMessageType.OTHER) )
+                throw new IllegalStateException( "A resolved service is required unless a message variable is targeted" );
 
             Map<String, Object> varMap =  context.getVariableMap(assertion.getVariablesUsed(), getAudit());
 
@@ -88,7 +93,7 @@ public class ServerRESTGatewayManagementAssertion extends AbstractMessageTargeta
             final URI uri = getURI(context, varMap, message, assertion);
             final URI baseUri = getBaseURI(context, message, assertion);
             //The service ID should always be a constant.
-            final String serviceId = context.getService().getId();
+            final String serviceId = context.getService() == null ? null : context.getService().getId();
             final HttpMethod action = getAction(varMap, message, assertion);
             final String contentType = getContentType( message);
 
@@ -193,7 +198,7 @@ public class ServerRESTGatewayManagementAssertion extends AbstractMessageTargeta
             String baseURI = context.getService().getRoutingUri();
             Pattern pattern = Pattern.compile(baseURI.replace("*", "(.*)"));
             Matcher matcher = pattern.matcher(requestURI);
-            if (matcher.matches())
+            if ( matcher.matches() && matcher.groupCount() > 0 )
                 uri = matcher.group(1);
             else
                 throw new IllegalArgumentException("Could not calculate uri");
@@ -234,7 +239,7 @@ public class ServerRESTGatewayManagementAssertion extends AbstractMessageTargeta
             errorRseponse = HttpServletResponse.SC_METHOD_NOT_ALLOWED;
         }
 
-        getAudit().logAndAudit(AssertionMessages.GATEWAYMANAGEMENT_ERROR, exception.getMessage());
+        getAudit().logAndAudit(AssertionMessages.GATEWAYMANAGEMENT_ERROR, new String[] { exception.getMessage() }, ExceptionUtils.getDebugException( exception ) );
         return errorRseponse;
     }
 }
