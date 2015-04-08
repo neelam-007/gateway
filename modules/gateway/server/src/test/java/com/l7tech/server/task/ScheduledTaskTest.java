f660ccfa1ee8155791ebd4d4309240f40aa41451
rballantyne <rballantyne@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2015-04-08 21:24:53 +0000

    Create Missing Directory from trunk in trunk_jdk8
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/branches/trunk_jdk8_post_javelin@42652 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ modules/gateway/server/src/test/java/com/l7tech/server/task/ScheduledTaskTest.java
@@ -0,0 +1,45 @@
+package com.l7tech.server.task;
+
+import com.l7tech.objectmodel.Goid;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.quartz.Job;
+import org.quartz.JobDataMap;
+import org.quartz.JobExecutionContext;
+
+import static org.mockito.Mockito.*;
+
+/**
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ScheduledTaskTest {
+    @Mock
+    JobExecutionContext executionContext;
+    @Mock
+    JobDataMap jobDataMap;
+    @Mock
+    ScheduledPolicyRunner policyRunner;
+    @Mock
+    ScheduledTaskJobManager jobManger;
+
+    @Test
+    public void runPolicy() throws Exception {
+
+        Goid policyID = new Goid(4, 5);
+        when(jobDataMap.getString(Matchers.eq(ScheduledTaskJobManager.JOB_DETAIL_POLICY_GOID))).thenReturn(policyID.toString());
+        when(jobDataMap.getString(Matchers.eq(ScheduledTaskJobManager.JOB_DETAIL_NODE))).thenReturn(ScheduledTaskJobManager.JOB_DETAIL_NODE_ONE);
+        when(executionContext.getMergedJobDataMap()).thenReturn(jobDataMap);
+        ScheduledPolicyRunner.setInstance(policyRunner);
+        stub(policyRunner.isClusterMaster()).toReturn(true);
+
+        Job job = new ScheduledServiceQuartzJob();
+        job.execute(executionContext);
+
+        verify(policyRunner).runBackgroundTask(policyID);
+        verify(policyRunner).isClusterMaster();
+    }
+
+}
