8da697114dab6952833d06c9d77714983c486cac
mlyons <mlyons@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2015-04-30 21:18:40 +0000

    SSG-8039: Adding KeystoreCertReplacer stand-alone sample utility.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@42771 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ modules/skunkworks/src/main/java/com/l7tech/skunkworks/KeystoreCertReplacer.java
@@ -0,0 +1,100 @@
+package com.l7tech.skunkworks;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.Key;
+import java.security.KeyStore;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.Enumeration;
+
+/**
+ * Small command line utility to replace all instances of a cert in a keystore.
+ * Throwaway command line code, no error checking -- do not reuse!
+ */
+public class KeystoreCertReplacer {
+
+    public static void main( String[] args ) throws Exception {
+
+        if ( args.length < 5 )
+            usage();
+
+        String kspath = args[0];
+        char[] passphrase = args[1].toCharArray();
+        String oldCertPemPath = args[2];
+        String outkspath = args[3];
+        String newCertPemPath = args[4];
+
+        // Load old cert
+        X509Certificate oldCert = loadCert( oldCertPemPath );
+        X509Certificate newCert = loadCert( newCertPemPath );
+
+        // TODO would be a good idea to check here to make sure that new cert
+        // can sanely drop-in replace the old cert (check DN, SKI, and AKI at least) in a chain.
+        // For now we just check DN.
+        if ( !oldCert.getSubjectDN().equals( newCert.getSubjectDN() ) )
+            throw new RuntimeException( "New cert subject DN differs from old cert subject DN" );
+
+        // Load keystore
+        KeyStore ks = KeyStore.getInstance( "PKCS12" );
+        try ( InputStream is = new FileInputStream( kspath ) ) {
+            ks.load( is, passphrase );
+        }
+
+        // Scan all key entry cert chains for old cert, and replace with new cert, leaving rest of chain as-is
+        int entriesModified = 0;
+        Enumeration<String> aliases = ks.aliases();
+        while ( aliases.hasMoreElements() ) {
+            String alias = aliases.nextElement();
+            if ( !ks.isKeyEntry( alias ) )
+                continue;
+
+            Certificate[] chain = ks.getCertificateChain( alias );
+            Certificate[] newChain = new Certificate[ chain.length ];
+            System.arraycopy( chain, 0, newChain, 0, chain.length );
+            boolean chainChanged = false;
+            for ( int i = 0; i < chain.length; i++ ) {
+                Certificate cert = chain[i];
+
+                if ( cert.equals( oldCert ) ) {
+                    System.err.println( "Replacing cert in chain for alias " + alias );
+                    newChain[i] = newCert;
+                    chainChanged = true;
+                }
+            }
+
+            if ( chainChanged ) {
+                Key key = ks.getKey( alias, passphrase );
+                ks.setKeyEntry( alias, key, passphrase, newChain );
+                entriesModified++;
+            }
+        }
+
+        if ( entriesModified < 1 ) {
+            System.err.println( "Keystore did not contain any matching certificate in a key entry cert chain.  Output file not created." );
+            System.exit( 2 );
+        }
+
+        try ( OutputStream out = new FileOutputStream( outkspath ) ) {
+            ks.store( out, passphrase );
+        }
+
+        System.err.println( "Output file " + outkspath + " created.  " + entriesModified + " certificate chains were updated." );
+    }
+
+    private static void usage() {
+        System.err.println( "Usage: KeystoreCertReplacer <inputKeystore> <passphrase> <oldCertPemPath> <outputKeystore> <newCertPemPath>" );
+        System.exit( 1 );
+    }
+
+    private static X509Certificate loadCert( String pemPath ) throws Exception {
+        CertificateFactory cf = CertificateFactory.getInstance( "X.509" );
+        try ( InputStream is = new FileInputStream( pemPath ) ) {
+            return (X509Certificate) cf.generateCertificate( is );
+        }
+    }
+
+}
