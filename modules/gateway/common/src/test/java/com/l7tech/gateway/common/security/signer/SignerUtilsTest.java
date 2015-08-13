3c7d8462ce0add7d87718fc0b8866ef1d30a7b9b
tveninov <tveninov@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2015-08-13 16:38:55 +0000

    Adding convenient unit test for signing ServerModuleFile(s) inside SKAR(s).
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@43464 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/common/src/test/java/com/l7tech/gateway/common/security/signer/SignerUtilsTest.java
+++ modules/gateway/common/src/test/java/com/l7tech/gateway/common/security/signer/SignerUtilsTest.java
@@ -1,29 +1,51 @@
 package com.l7tech.gateway.common.security.signer;
 
+import com.l7tech.common.io.TeeOutputStream;
+import com.l7tech.common.io.XmlUtil;
 import com.l7tech.security.cert.TestCertificateGenerator;
 import com.l7tech.util.*;
+import org.apache.commons.lang.StringUtils;
 import org.hamcrest.Matchers;
 import org.jetbrains.annotations.NotNull;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.jetbrains.annotations.Nullable;
+import org.junit.*;
+import org.junit.rules.TemporaryFolder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
 import java.io.*;
 import java.security.DigestInputStream;
 import java.security.MessageDigest;
 import java.security.PrivateKey;
 import java.security.cert.X509Certificate;
-import java.util.Properties;
+import java.util.*;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class SignerUtilsTest {
 
+    @ClassRule
+    public static TemporaryFolder tmpFolder;
+
+
+    @BeforeClass
+    public static void setUpOnce() throws Exception {
+        tmpFolder = new TemporaryFolder();
+        tmpFolder.create();
+    }
+
     @Before
     public void setUp() throws Exception {
-
+        xPath = XPathFactory.newInstance().newXPath();
+        final HashMap<String, String> map = new HashMap<>();
+        map.put("l7", "http://ns.l7tech.com/2010/04/gateway-management");
+        xPath.setNamespaceContext(new NamespaceContextImpl(map));
     }
 
     @After
@@ -468,5 +490,249 @@ public class SignerUtilsTest {
                 // expected IOException
             }
         }
+    }
+
+    /**
+     * Convenient  unit test to sign {@code ServerModuleFile)'s within a SKAR file.<br/>
+     * The output skar file will be in the same folder as the skarToSign suffixed with "-signed"<br/>
+     * Should work with skar of skars.<br/>
+     * Note: any existing output file will be overwritten.<br/>
+     * Note: by default the method signs using gatewayKeyStore.p12, change the path before running in your environment.
+     * <p/>
+     * Run in your IDE environment, add skarToSign files to your choosing and modify the signer keystore file to point to your signer keystore.
+     */
+    @Ignore
+    @Test
+    public void testSignServerModuleFilesInsideSkar() throws Exception {
+        signServerModuleFilesInsideSkar(new File("\\\\filer2.l7tech.com\\departments\\Dev\\tluong\\SolutionKitManager\\SimpleSolutionKit-1.0-20150716-signed.skar"));
+        signServerModuleFilesInsideSkar(new File("\\\\filer2.l7tech.com\\departments\\Dev\\tluong\\SolutionKitManager\\SimpleSolutionKit-1.0-20150716.skar"));
+        signServerModuleFilesInsideSkar(new File("\\\\filer2.l7tech.com\\departments\\Dev\\tluong\\SolutionKitManager\\SimpleSolutionKit-1.1-20150803.skar"));
+        signServerModuleFilesInsideSkar(new File("\\\\filer2.l7tech.com\\departments\\Dev\\tluong\\SolutionKitManager\\demo sprint 02.1\\SimpleSolutionKit-1.0-20150716.skar"));
+        signServerModuleFilesInsideSkar(new File("\\\\filer2.l7tech.com\\departments\\Dev\\tluong\\SolutionKitManager\\demo sprint 02.1\\SimpleSolutionKit-1.1-20150716.skar"));
+    }
+
+    private void signServerModuleFilesInsideSkar(final File skarFileToSign) throws Exception {
+        Assert.assertNotNull(skarFileToSign);
+        Assert.assertTrue(skarFileToSign.exists() && skarFileToSign.isFile());
+
+        final File signedSkarFile = new File(skarFileToSign.getParent(), generateSignedName(skarFileToSign.getName()));
+        try (
+                final BufferedInputStream bis = new BufferedInputStream(new FileInputStream(skarFileToSign));
+                final BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(signedSkarFile))
+        ) {
+            signServerModuleFilesInsideSkar(
+                    bis,
+                    new File("D:\\work\\Signer\\gatewayKeyStore.p12"),
+                    "PKCS12",
+                    "7layer",
+                    null,
+                    "7layer",
+                    bos
+            );
+        }
+    }
+
+    private static String generateSignedName(final String fileName) {
+        Assert.assertTrue(StringUtils.isNotBlank(fileName));
+        int i = fileName.lastIndexOf('.');
+        if (i > 0) {
+            return fileName.substring(0, i) + "-signed" + fileName.substring(i);
+        }
+        return fileName + "-signed";
+    }
+
+    private XPath xPath;
+    private static final String SK_INSTALL_BUNDLE_FILENAME = "InstallBundle.xml";
+    private void signServerModuleFilesInsideSkar(
+            final InputStream skarFileToSignStream,
+            final File signerKeyStore,
+            final String storeType,
+            @Nullable final String storePass,
+            @Nullable final String alias,
+            @Nullable final String entryPass,
+            final OutputStream outputStream
+    ) throws Exception {
+        Assert.assertNotNull(skarFileToSignStream);
+        Assert.assertNotNull(signerKeyStore);
+        Assert.assertTrue(signerKeyStore.exists() && signerKeyStore.isFile());
+        Assert.assertTrue(StringUtils.isNotBlank(storeType));
+
+        // reed the
+        try (
+                final ZipInputStream zis = new ZipInputStream(skarFileToSignStream);
+                final ZipOutputStream zos = new ZipOutputStream(outputStream)
+        ) {
+            ZipEntry entry = zis.getNextEntry();
+            // loop while there are more entries
+            while (entry != null) {
+                // check if entry is directory
+                try {
+                    if (!entry.isDirectory()) {
+                        final String fileName = entry.getName();
+                        if (SK_INSTALL_BUNDLE_FILENAME.equals(fileName)) {
+                            final Document doc = XmlUtil.parse(
+                                    new FilterInputStream(zis) {
+                                        @Override public void close() throws IOException {
+                                            // don't close the zip stream
+                                        }
+                                    }
+                            );
+                            // get all SERVER_MODULE_FILE nodes
+                            final NodeList nodes = (NodeList)xPath.compile("/l7:Bundle/l7:References/l7:Item[l7:Type='SERVER_MODULE_FILE']/l7:Resource/l7:ServerModuleFile").evaluate(doc.getDocumentElement(), XPathConstants.NODESET);
+                            for (int i = 0; i < nodes.getLength(); ++i) {
+                                final Node node = nodes.item(i);
+                                if (node!= null) {
+                                    try {
+                                        DomUtils.findExactlyOneChildElementByName((Element) node, "http://ns.l7tech.com/2010/04/gateway-management", "Signature");
+                                    } catch (final MissingRequiredElementException ignore) {
+                                        final Element dataElem = DomUtils.findExactlyOneChildElementByName((Element) node, "http://ns.l7tech.com/2010/04/gateway-management", "ModuleData");
+                                        try (
+                                                final ByteArrayInputStream byesStream = new ByteArrayInputStream(HexUtils.decodeBase64(DomUtils.getTextValue(dataElem)));
+                                                final ByteArrayOutputStream bos = new ByteArrayOutputStream()
+                                        ) {
+                                            SignerUtils.signWithKeyStore(
+                                                    signerKeyStore, 
+                                                    storeType, 
+                                                    storePass != null ? storePass.toCharArray() : null,
+                                                    alias,
+                                                    entryPass != null ? entryPass.toCharArray() : null,
+                                                    byesStream,
+                                                    bos
+                                            );
+                                            final Map<String, String> sigPropsMap = gatSignaturePropertiesMap(
+                                                    getSignatureProperties(bos.toByteArray()),
+                                                    SignerUtils.ALL_SIGNING_PROPERTIES
+                                            );
+                                            // generate sig xml elem
+                                            //final Element sigElem = DomUtils.createAndAppendElement((Element) node, "Signature");
+                                            final Element sigElem = createAndInsertAfterElement((Element) node, "Signature", dataElem);
+                                            for (final Map.Entry<String, String> propEntry : sigPropsMap.entrySet()) {
+                                                final Element propElem = DomUtils.createAndAppendElement(sigElem, "Property");
+                                                propElem.setAttribute("key", propEntry.getKey());
+                                                final Element propValueElem = DomUtils.createAndAppendElement(propElem, "StringValue");
+                                                propValueElem.setTextContent(propEntry.getValue());
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+
+                            zos.putNextEntry(new ZipEntry(fileName));
+                            try (
+                                    final Reader reader = new StringReader(XmlUtil.nodeToFormattedString(doc));
+                                    final Writer writer = new BufferedWriter(new OutputStreamWriter(new FilterOutputStream(zos) {
+                                        @Override public void close() throws IOException {
+                                            // don't close the zip stream
+                                        }
+                                    }))
+                            ) {
+                                IOUtils.copyStream(reader, writer);
+                            }
+
+                        } else if (StringUtils.isNotBlank(fileName) && fileName.toLowerCase().endsWith(".skar")) {
+                            // Get the input bytes for a child SKAR, create a new input stream, and call recursively call the load method.
+                            try (final BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(tmpFolder.newFile()))) {
+                                // create the sip entry
+                                zos.putNextEntry(new ZipEntry(fileName));
+                                // don't close streams
+                                final TeeOutputStream tos = new TeeOutputStream(bos, new FilterOutputStream(zos){
+                                    @Override public void close() throws IOException {
+                                        // don't close the zip stream
+                                    }
+                                });
+                                signServerModuleFilesInsideSkar(
+                                        new FilterInputStream(zis) {
+                                            @Override public void close() throws IOException {
+                                                // don't close the zip stream
+                                            }
+                                        },
+                                        signerKeyStore,
+                                        storeType,
+                                        storePass,
+                                        alias,
+                                        entryPass,
+                                        tos
+                                );
+                            }
+                        } else {
+                            // anything else simply add to the zip
+                            zos.putNextEntry(new ZipEntry(fileName));
+                            IOUtils.copyStream(zis, zos);
+                        }
+                    } else {
+                        // anything else simply add to the zip
+                        zos.putNextEntry(new ZipEntry(entry.getName()));
+                    }
+                } finally {
+                    // close the entry
+                    zis.closeEntry();
+                }
+
+                // finally move to the next entry
+                entry = zis.getNextEntry();
+            }
+        }
+    }
+
+    private static Element createAndInsertAfterElement(final Element parent, String localName, final Element afterElem) {
+        Assert.assertNotNull(parent);
+        Assert.assertTrue(StringUtils.isNotBlank(localName));
+        Assert.assertNotNull(afterElem);
+        final Element element = parent.getOwnerDocument().createElementNS(parent.getNamespaceURI(), localName);
+        element.setPrefix(parent.getPrefix());
+        Node afterElemSibling = afterElem.getNextSibling();
+        while (!(afterElemSibling instanceof Element) && afterElemSibling != null) {
+            afterElemSibling = afterElemSibling.getNextSibling();
+        }
+        if (afterElemSibling == null) {
+            parent.appendChild(element);
+        } else {
+            parent.insertBefore(element, afterElemSibling);
+        }
+        return element;
+    }
+
+    private static Properties getSignatureProperties(final byte[] signedContent) throws Exception {
+        Assert.assertNotNull(signedContent);
+        try (final InputStream is = new ByteArrayInputStream(signedContent)) {
+            // process the signed zip file
+            return SignerUtils.walkSignedZip(
+                    is,
+                    new SignedZipVisitor<Void, Properties>() {
+                        @Override
+                        public Void visitData(@NotNull final ZipInputStream zis) throws IOException {
+                            // don't care
+                            return null;
+                        }
+
+                        @Override
+                        public Properties visitSignature(@NotNull final ZipInputStream zis) throws IOException {
+                            // read the signature
+                            final Properties sigProps = new Properties();
+                            sigProps.load(zis);
+                            return sigProps;
+                        }
+                    },
+                    true
+            ).right;
+        }
+    }
+
+    private static Map<String, String> gatSignaturePropertiesMap(
+            final Properties sigProps,
+            final String[] keys
+    ) throws IOException {
+        Assert.assertNotNull(sigProps);
+        Assert.assertNotNull(keys);
+        Assert.assertThat(keys, Matchers.not(Matchers.emptyArray()));
+
+        final Map<String, String> ret = new TreeMap<>();
+        for (final String key : keys) {
+            final String value = (String) sigProps.get(key);
+            if (value != null) {
+                ret.put(key, value);
+            }
+        }
+        return ret.isEmpty() ? null : Collections.unmodifiableMap(ret);
     }
 }
