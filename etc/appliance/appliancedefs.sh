0b0b4643224b13c6afbf27713697868b62482d19
mlyons <mlyons@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2015-08-27 22:10:44 +0000

    SSG-10592: Simplified Gateway appliance memory sizing heuristic using recommendation from Support.  Removed usebigmemory option.  The appliance will now always configure the Gateway's maximum heap size to half of total system memory.  Removed logic that caps heap size 2 or 4 gb.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@43601 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- etc/appliance/appliancedefs.sh
+++ etc/appliance/appliancedefs.sh
@@ -1,8 +1,6 @@
 # LAYER 7 TECHNOLOGIES
 # Defines SSG_JAVA_HOME, etc
 
-# Set to 1 if using 32-bit jvm, to avoid trying to allocate more than 2gb of ram for the jvm
-usebigmemory=1
 jvmarch=x86_64
 
 ulimit -s 2048
@@ -22,20 +20,8 @@ alias stopssg='/etc/rc.d/init.d/ssg stop'
 
 system_ram=`grep MemTotal /proc/meminfo | awk '{print $2}'`
 # Maximum amount of RAM to use
-multiplier="2/3"
-#
+multiplier="1/2"
 let java_ram="$system_ram*$multiplier"
-#if we have a lot of ram, we need to check whether we are enabled to use it or not.
-if [ `expr $java_ram \> 2074412` == 1 ] ; then
-    if [ $usebigmemory -eq 1 ]; then
-	    # we have more ram than java can use
-	    # FIXME: when we start running 64 bit JVM
-	    java_ram=2074412;
- 	    # CAP at 2 gigs
-    else
-        java_ram=4194304;
-    fi
-fi
 
 SSG_JAVA_HOME="/opt/SecureSpan/JDK"
 
