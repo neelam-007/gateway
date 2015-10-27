f6ae38e9f5309feac38827ed3910b2c4041bcd96
tveninov <tveninov@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2015-10-27 03:23:24 +0000

    SSG-11581 - adding unit testing for ProtectedEntityTracker and RbacServices.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@44202 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

---
+++ modules/gateway/server/src/test/java/com/l7tech/server/security/rbac/ProtectedEntityTrackerTest.java
@@ -0,0 +1,275 @@
+package com.l7tech.server.security.rbac;
+
+import com.l7tech.gateway.common.module.ServerModuleFile;
+import com.l7tech.gateway.common.security.password.SecurePassword;
+import com.l7tech.gateway.common.security.rbac.EntityProtectionInfo;
+import com.l7tech.gateway.common.service.PublishedService;
+import com.l7tech.objectmodel.Entity;
+import com.l7tech.objectmodel.EntityType;
+import com.l7tech.objectmodel.encass.EncapsulatedAssertionConfig;
+import com.l7tech.objectmodel.folder.Folder;
+import com.l7tech.policy.CustomKeyValueStore;
+import com.l7tech.policy.Policy;
+import com.l7tech.server.ServerConfig;
+import com.l7tech.server.ServerConfigParams;
+import com.l7tech.server.ServerConfigStub;
+import com.l7tech.util.CollectionUtils;
+import com.l7tech.util.ConfigFactory;
+import com.l7tech.util.Pair;
+import org.hamcrest.Matchers;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+public class ProtectedEntityTrackerTest {
+
+    private ProtectedEntityTracker protectedEntityTracker;
+    private ServerConfig config;
+
+    @Before
+    public void setUp() throws Exception {
+        config = new ServerConfigStub();
+        config.putProperty(ServerConfigParams.PARAM_PROTECTED_ENTITY_TRACKER_ENABLE, String.valueOf(true));
+        protectedEntityTracker = new ProtectedEntityTracker(config);
+        ConfigFactory.clearCachedConfig();
+
+        Assert.assertNotNull(protectedEntityTracker);
+        Assert.assertTrue(protectedEntityTracker.isEnabled());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        // clean up
+        protectedEntityTracker.bulkUpdateReadOnlyEntitiesList(Collections.<Pair< EntityType, String>>emptyList());
+        Assert.assertThat(protectedEntityTracker.getProtectedEntities(), Matchers.anyOf(Matchers.nullValue(Map.class), Matchers.equalTo(Collections.<String, EntityProtectionInfo>emptyMap())));
+    }
+
+    @Test
+    public void testIsEnabled() throws Exception {
+        // add few test samples
+        protectedEntityTracker.bulkUpdateReadOnlyEntitiesList(
+                CollectionUtils.list(
+                        Pair.pair(EntityType.SERVER_MODULE_FILE, "id1"),
+                        Pair.pair(EntityType.SECURE_PASSWORD, "id2"),
+                        Pair.pair(EntityType.CUSTOM_KEY_VALUE_STORE, "id3"),
+                        Pair.pair(EntityType.FOLDER, "id4")
+                )
+        );
+
+        // by default the tracker is enabled
+        Assert.assertTrue(protectedEntityTracker.isEnabled());
+        Assert.assertTrue(protectedEntityTracker.isEntityProtectionEnabled());
+        Assert.assertThat(protectedEntityTracker.getProtectedEntities(), Matchers.allOf(Matchers.notNullValue(), Matchers.not(Matchers.equalTo(Collections.<String, EntityProtectionInfo>emptyMap()))));
+
+        // disable it
+        config.putProperty(ServerConfigParams.PARAM_PROTECTED_ENTITY_TRACKER_ENABLE, String.valueOf(false));
+        ConfigFactory.clearCachedConfig();
+
+        // test that the tracker is disabled
+        Assert.assertFalse(protectedEntityTracker.isEnabled());
+        Assert.assertFalse(protectedEntityTracker.isEntityProtectionEnabled());
+        Assert.assertThat(protectedEntityTracker.getProtectedEntities(), Matchers.anyOf(Matchers.nullValue(Map.class), Matchers.equalTo(Collections.<String, EntityProtectionInfo>emptyMap())));
+    }
+
+    @Test
+    public void testBulkUpdateReadOnlyEntitiesList() throws Exception {
+        // make sure the tracker is enabled and there are no protected entities
+        Assert.assertTrue(protectedEntityTracker.isEnabled());
+        Assert.assertThat(protectedEntityTracker.getProtectedEntities(), Matchers.anyOf(Matchers.nullValue(Map.class), Matchers.equalTo(Collections.<String, EntityProtectionInfo>emptyMap())));
+
+        // add few test samples
+        protectedEntityTracker.bulkUpdateReadOnlyEntitiesList(
+                CollectionUtils.list(
+                        Pair.pair(EntityType.SERVER_MODULE_FILE, "id1"),
+                        Pair.pair(EntityType.SECURE_PASSWORD, "id2"),
+                        Pair.pair(EntityType.CUSTOM_KEY_VALUE_STORE, "id3"),
+                        Pair.pair(EntityType.FOLDER, "id4")
+                )
+        );
+        // test protected entities
+        Map<String, EntityProtectionInfo> entityProtectionInfoMap = protectedEntityTracker.getProtectedEntities();
+        Assert.assertThat(entityProtectionInfoMap, Matchers.allOf(Matchers.notNullValue(), Matchers.not(Matchers.equalTo(Collections.<String, EntityProtectionInfo>emptyMap()))));
+        Assert.assertThat(entityProtectionInfoMap.values(), Matchers.hasSize(4));
+        Assert.assertThat(entityProtectionInfoMap,
+                Matchers.allOf(
+                        Matchers.hasKey("id1"),
+                        Matchers.hasKey("id2"),
+                        Matchers.hasKey("id3"),
+                        Matchers.hasKey("id4")
+                )
+        );
+        Assert.assertFalse(protectedEntityTracker.isReadOnlyEntity(new ServerModuleFile()));
+        Assert.assertFalse(protectedEntityTracker.isReadOnlyEntity(new ServerModuleFile() {
+            @Override
+            public String getId() {
+                return "id2";
+            }
+        }));
+        Assert.assertTrue(protectedEntityTracker.isReadOnlyEntity(new ServerModuleFile() {
+            @Override
+            public String getId() {
+                return "id1";
+            }
+        }));
+        Assert.assertTrue(protectedEntityTracker.isReadOnlyEntity(new SecurePassword() {
+            @Override
+            public String getId() {
+                return "id2";
+            }
+        }));
+        Assert.assertTrue(protectedEntityTracker.isReadOnlyEntity(new CustomKeyValueStore() {
+            @Override
+            public String getId() {
+                return "id3";
+            }
+        }));
+        Assert.assertTrue(protectedEntityTracker.isReadOnlyEntity(new Folder() {
+            @Override
+            public String getId() {
+                return "id4";
+            }
+        }));
+
+        // add few test samples (id11, id12, id13, id14 and id15) with duplicates (id11, id13 and id14)
+        protectedEntityTracker.bulkUpdateReadOnlyEntitiesList(
+                CollectionUtils.list(
+                        Pair.pair(EntityType.SERVER_MODULE_FILE, "id11"),
+                        Pair.pair(EntityType.SECURE_PASSWORD, "id12"),
+                        Pair.pair(EntityType.CUSTOM_KEY_VALUE_STORE, "id13"),
+                        Pair.pair(EntityType.FOLDER, "id14"),
+                        Pair.pair(EntityType.POLICY, "id14"), // override id114
+                        Pair.pair(EntityType.SERVICE, "id13"), // override id113
+                        Pair.pair(EntityType.FOLDER, "id15"),
+                        Pair.pair(EntityType.ENCAPSULATED_ASSERTION, "id11") // override id11
+                )
+        );
+        // test protected entities
+        entityProtectionInfoMap = protectedEntityTracker.getProtectedEntities();
+        Assert.assertThat(entityProtectionInfoMap, Matchers.allOf(Matchers.notNullValue(), Matchers.not(Matchers.equalTo(Collections.<String, EntityProtectionInfo>emptyMap()))));
+        Assert.assertThat(entityProtectionInfoMap.values(), Matchers.hasSize(5));
+        Assert.assertThat(entityProtectionInfoMap,
+                Matchers.allOf(
+                        Matchers.hasKey("id11"),
+                        Matchers.hasKey("id12"),
+                        Matchers.hasKey("id13"),
+                        Matchers.hasKey("id14"),
+                        Matchers.hasKey("id15")
+                )
+        );
+        Assert.assertTrue(protectedEntityTracker.isReadOnlyEntity(new EncapsulatedAssertionConfig() {
+            @Override
+            public String getId() {
+                return "id11";
+            }
+        }));
+        Assert.assertFalse(protectedEntityTracker.isReadOnlyEntity(new ServerModuleFile() {
+            @Override
+            public String getId() {
+                return "id11";
+            }
+        }));
+        Assert.assertTrue(protectedEntityTracker.isReadOnlyEntity(new SecurePassword() {
+            @Override
+            public String getId() {
+                return "id12";
+            }
+        }));
+        Assert.assertTrue(protectedEntityTracker.isReadOnlyEntity(new PublishedService() {
+            @Override
+            public String getId() {
+                return "id13";
+            }
+        }));
+        Assert.assertFalse(protectedEntityTracker.isReadOnlyEntity(new CustomKeyValueStore() {
+            @Override
+            public String getId() {
+                return "id13";
+            }
+        }));
+        Assert.assertTrue(protectedEntityTracker.isReadOnlyEntity(new Policy() {
+            @Override
+            public String getId() {
+                return "id14";
+            }
+        }));
+        Assert.assertFalse(protectedEntityTracker.isReadOnlyEntity(new Folder() {
+            @Override
+            public String getId() {
+                return "id14";
+            }
+        }));
+        Assert.assertTrue(protectedEntityTracker.isReadOnlyEntity(new Folder() {
+            @Override
+            public String getId() {
+                return "id15";
+            }
+        }));
+    }
+
+    @Test
+    public void testDoWithEntityProtectionDisabled() throws Exception {
+        // make sure its enabled and there are no protected entities
+        Assert.assertTrue(protectedEntityTracker.isEnabled());
+        Assert.assertTrue(protectedEntityTracker.isEntityProtectionEnabled());
+        Assert.assertThat(protectedEntityTracker.getProtectedEntities(), Matchers.anyOf(Matchers.nullValue(Map.class), Matchers.equalTo(Collections.<String, EntityProtectionInfo>emptyMap())));
+
+        // add few test samples
+        protectedEntityTracker.bulkUpdateReadOnlyEntitiesList(
+                CollectionUtils.list(
+                        Pair.pair(EntityType.SERVER_MODULE_FILE, "id1"),
+                        Pair.pair(EntityType.SECURE_PASSWORD, "id2"),
+                        Pair.pair(EntityType.CUSTOM_KEY_VALUE_STORE, "id3"),
+                        Pair.pair(EntityType.SERVICE, "id4")
+                )
+        );
+
+        // make sure our test samples are read-only
+        final Collection<Entity> testEntities = Collections.unmodifiableCollection(
+                CollectionUtils.<Entity>list(
+                        new ServerModuleFile() {
+                            @Override
+                            public String getId() {
+                                return "id1";
+                            }
+                        },
+                        new SecurePassword() {
+                            @Override
+                            public String getId() {
+                                return "id2";
+                            }
+                        },
+                        new CustomKeyValueStore() {
+                            @Override
+                            public String getId() {
+                                return "id3";
+                            }
+                        },
+                        new PublishedService() {
+                            @Override
+                            public String getId() {
+                                return "id4";
+                            }
+                        }
+                )
+        );
+        Assert.assertThat(testEntities, Matchers.allOf(Matchers.notNullValue(), Matchers.hasSize(4)));
+        for (final Entity entity : testEntities) {
+            Assert.assertTrue(protectedEntityTracker.isReadOnlyEntity(entity));
+        }
+
+        // now test with protection disabled
+        protectedEntityTracker.doWithEntityProtectionDisabled(new Callable<Void>() {
+            @Override
+            public Void call() throws Exception {
+                Assert.assertFalse(protectedEntityTracker.isEntityProtectionEnabled());
+                return null;
+            }
+        });
+    }
+}
--- modules/gateway/server/src/test/java/com/l7tech/server/security/rbac/RbacServicesTest.java
+++ modules/gateway/server/src/test/java/com/l7tech/server/security/rbac/RbacServicesTest.java
@@ -1,20 +1,32 @@
 package com.l7tech.server.security.rbac;
 
+import com.l7tech.gateway.common.module.ServerModuleFile;
+import com.l7tech.gateway.common.security.password.SecurePassword;
+import com.l7tech.gateway.common.security.rbac.EntityProtectionInfo;
 import com.l7tech.gateway.common.security.rbac.OperationType;
 import com.l7tech.gateway.common.security.rbac.Role;
 import com.l7tech.gateway.common.service.PublishedService;
 import com.l7tech.identity.User;
 import com.l7tech.identity.UserBean;
+import com.l7tech.objectmodel.Entity;
 import com.l7tech.objectmodel.EntityType;
 import com.l7tech.objectmodel.FindException;
 import com.l7tech.objectmodel.Goid;
-import com.l7tech.server.EntityFinder;
-import com.l7tech.server.EntityFinderStub;
+import com.l7tech.objectmodel.encass.EncapsulatedAssertionConfig;
+import com.l7tech.server.*;
+import com.l7tech.util.CollectionUtils;
+import com.l7tech.util.ConfigFactory;
+import com.l7tech.util.Functions;
+import com.l7tech.util.Pair;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.Callable;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -93,6 +105,107 @@ public class RbacServicesTest {
         assertFalse("Other operation permission", permitted5);
     }
 
+    @Test
+    public void testPermittedForEntityWithProtectedEntityTracker() throws Exception {
+        final ServerConfig config = new ServerConfigStub();
+        config.putProperty(ServerConfigParams.PARAM_PROTECTED_ENTITY_TRACKER_ENABLE, String.valueOf(true));
+        final ProtectedEntityTracker protectedEntityTracker = new ProtectedEntityTracker(config);
+        ConfigFactory.clearCachedConfig();
+        Assert.assertNotNull(protectedEntityTracker);
+        Assert.assertTrue(protectedEntityTracker.isEnabled());
+        final RbacServices rbacServices = new RbacServicesImpl(roleManager , entityFinder, protectedEntityTracker);
+
+        // create our test admin user
+        final User adminUser = newUserBean("admin");
+
+        // create sample entities
+        final Collection<Entity> testEntities = Collections.unmodifiableCollection(
+                CollectionUtils.<Entity>list(
+                        new PublishedService() {{
+                            setGoid(new Goid(0, 1));
+                            setName("Test Service");
+                        }},
+                        new ServerModuleFile() {{
+                            setGoid(new Goid(0, 2));
+                            setName("Test SMF");
+                        }},
+                        new SecurePassword() {{
+                            setGoid(new Goid(0, 3));
+                            setName("Test Password");
+                        }},
+                        new EncapsulatedAssertionConfig() {{
+                            setGoid(new Goid(0, 4));
+                            setName("Test Encapsulated Assertion");
+                        }}
+                )
+        );
+        // make sure admin has full access to our test entities
+        for (final Entity entity : testEntities) {
+            Assert.assertTrue(rbacServices.isPermittedForEntity(adminUser, entity, OperationType.READ, null));
+            Assert.assertTrue(rbacServices.isPermittedForEntity(adminUser, entity, OperationType.CREATE, null));
+            Assert.assertTrue(rbacServices.isPermittedForEntity(adminUser, entity, OperationType.UPDATE, null));
+            Assert.assertTrue(rbacServices.isPermittedForEntity(adminUser, entity, OperationType.DELETE, null));
+            Assert.assertTrue(rbacServices.isPermittedForEntity(adminUser, entity, OperationType.OTHER, "other"));
+        }
+
+        // mark our test entities as read-only
+        protectedEntityTracker.bulkUpdateReadOnlyEntitiesList(
+                Functions.map(
+                        testEntities,
+                        new Functions.Unary<Pair<EntityType, String>, Entity>() {
+                            @Override
+                            public Pair<EntityType, String> call(final Entity entity) {
+                                Assert.assertNotNull(entity);
+                                return Pair.pair(EntityType.findTypeByEntity(entity.getClass()), entity.getId());
+                            }
+                        }
+                )
+        );
+
+        // now that our test entities are marked as read-only admin shouldn't have other then read permissions
+        for (final Entity entity : testEntities) {
+            Assert.assertTrue(rbacServices.isPermittedForEntity(adminUser, entity, OperationType.READ, null));
+            Assert.assertFalse(rbacServices.isPermittedForEntity(adminUser, entity, OperationType.CREATE, null));
+            Assert.assertFalse(rbacServices.isPermittedForEntity(adminUser, entity, OperationType.UPDATE, null));
+            Assert.assertFalse(rbacServices.isPermittedForEntity(adminUser, entity, OperationType.DELETE, null));
+            Assert.assertFalse(rbacServices.isPermittedForEntity(adminUser, entity, OperationType.OTHER, "other"));
+        }
+
+        // now do with entity protection disabled, admin user should have full access
+        protectedEntityTracker.doWithEntityProtectionDisabled(
+                new Callable<Void>() {
+                    @Override
+                    public Void call() throws Exception {
+                        for (final Entity entity : testEntities) {
+                            Assert.assertTrue(rbacServices.isPermittedForEntity(adminUser, entity, OperationType.READ, null));
+                            Assert.assertTrue(rbacServices.isPermittedForEntity(adminUser, entity, OperationType.CREATE, null));
+                            Assert.assertTrue(rbacServices.isPermittedForEntity(adminUser, entity, OperationType.UPDATE, null));
+                            Assert.assertTrue(rbacServices.isPermittedForEntity(adminUser, entity, OperationType.DELETE, null));
+                            Assert.assertTrue(rbacServices.isPermittedForEntity(adminUser, entity, OperationType.OTHER, "other"));
+                        }
+                        return null;
+                    }
+                }
+        );
+
+        // finally try with protectedEntityTracker disabled altogether
+        // disable it
+        config.putProperty(ServerConfigParams.PARAM_PROTECTED_ENTITY_TRACKER_ENABLE, String.valueOf(false));
+        ConfigFactory.clearCachedConfig();
+        // test that protectedEntityTracker is disabled
+        Assert.assertFalse(protectedEntityTracker.isEnabled());
+        Assert.assertFalse(protectedEntityTracker.isEntityProtectionEnabled());
+        Assert.assertThat(protectedEntityTracker.getProtectedEntities(), Matchers.anyOf(Matchers.nullValue(Map.class), Matchers.equalTo(Collections.<String, EntityProtectionInfo>emptyMap())));
+        // admin user should have full access
+        for (final Entity entity : testEntities) {
+            Assert.assertTrue(rbacServices.isPermittedForEntity(adminUser, entity, OperationType.READ, null));
+            Assert.assertTrue(rbacServices.isPermittedForEntity(adminUser, entity, OperationType.CREATE, null));
+            Assert.assertTrue(rbacServices.isPermittedForEntity(adminUser, entity, OperationType.UPDATE, null));
+            Assert.assertTrue(rbacServices.isPermittedForEntity(adminUser, entity, OperationType.DELETE, null));
+            Assert.assertTrue(rbacServices.isPermittedForEntity(adminUser, entity, OperationType.OTHER, "other"));
+        }
+    }
+
     //- PRIVATE
 
     private final EntityFinder entityFinder = new EntityFinderStub();
@@ -100,34 +213,43 @@ public class RbacServicesTest {
             @Override
             public Collection<Role> getAssignedRoles( final User user ) throws FindException {
                 if ( "some".equals( user.getLogin() ) ) {
-                    Collection<Role> roles = new ArrayList<Role>();
+                    Collection<Role> roles = new ArrayList<>();
                     Role role = new Role();
                     role.addEntityPermission( OperationType.READ, EntityType.SERVICE, new Goid(0,123).toHexString() );
                     roles.add( role );
                     return roles;
                 } else if ( "some-other".equals( user.getLogin() ) ) {
-                    Collection<Role> roles = new ArrayList<Role>();
+                    Collection<Role> roles = new ArrayList<>();
                     Role role = new Role();
                     role.addEntityPermission( OperationType.READ, EntityType.SERVICE, new Goid(0,456).toHexString() );
                     roles.add( role );
                     return roles;
                 } else if ( "any".equals( user.getLogin() ) ) {
-                    Collection<Role> roles = new ArrayList<Role>();
+                    Collection<Role> roles = new ArrayList<>();
                     Role role = new Role();
                     role.addEntityPermission( OperationType.READ, EntityType.SERVICE, null );
                     roles.add( role );
                     return roles;
                 } else if ( "other-ent".equals( user.getLogin() ) ) {
-                    Collection<Role> roles = new ArrayList<Role>();
+                    Collection<Role> roles = new ArrayList<>();
                     Role role = new Role();
                     role.addEntityPermission( OperationType.READ, EntityType.POLICY, null );
                     roles.add( role );
                     return roles;
                 } else if ( "other-perm".equals( user.getLogin() ) ) {
-                    Collection<Role> roles = new ArrayList<Role>();
+                    Collection<Role> roles = new ArrayList<>();
                     Role role = new Role();
                     role.addEntityPermission( OperationType.UPDATE, EntityType.SERVICE, null );
                     roles.add( role );
+                    return roles;
+                } else if ( "admin".equals( user.getLogin() ) ) {
+                    Collection<Role> roles = new ArrayList<>();
+                    Role role = new Role();
+                    for (OperationType type : OperationType.ALL_CRUD) {
+                        role.addEntityPermission(type, EntityType.ANY, null);
+                    }
+                    role.addEntityOtherPermission(EntityType.ANY, null, "other");
+                    roles.add(role);
                     return roles;
                 } else { // none
                     return Collections.emptyList();
