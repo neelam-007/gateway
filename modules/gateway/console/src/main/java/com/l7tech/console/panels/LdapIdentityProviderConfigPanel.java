b64446df72862a5e924d8be5dc1d3ade27f87d92
cdragunsky <cdragunsky@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2016-05-09 22:25:49 +0000

    SSG-13429: LDAP Reconnect Timeout UI - Error Message Should be update to include 0 as vaild input - changing message to state that 0 is acceptted
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@46455 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/console/src/main/java/com/l7tech/console/panels/LdapIdentityProviderConfigPanel.java
+++ modules/gateway/console/src/main/java/com/l7tech/console/panels/LdapIdentityProviderConfigPanel.java
@@ -173,7 +173,7 @@ public class LdapIdentityProviderConfigPanel extends IdentityProviderStepPanel<L
             public String getValidationError() {
                 Matcher matcher = millisecondPattern.matcher(reconnectTimeoutTextField.getText());
                 if (!matcher.matches()) {
-                    return "Reconnect Timeout should be a number of milliseconds between 1 and 999999";
+                    return "Reconnect Timeout should be a number of milliseconds between 0 and 999999";
                 }
                 return null;
             }
