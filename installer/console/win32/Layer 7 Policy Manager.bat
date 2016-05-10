34b2b9446a05603ff563d3dfab780c8b6cf8e327
cdragunsky <cdragunsky@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2016-05-10 23:34:16 +0000

    SSB-717: Rebrand XVC install artifacts on Windows - deleting old versions of the files which for some reason did not get deleted when I renamed them
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@46468 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- installer/console/win32/Layer 7 Policy Manager.bat
+++
@@ -1,3 +0,0 @@
-@jre\bin\java -Xms128M -Xmx256M -Xss256k -Dfile.encoding=UTF-8 -Duser.language=en -Duser.country=US -Dsun.net.inetaddr.ttl=10 -Dnetworkaddress.cache.ttl=10 -jar Manager.jar
-@echo Layer 7 Policy Manager has finished.  Its final log messages are shown above.
-@pause
