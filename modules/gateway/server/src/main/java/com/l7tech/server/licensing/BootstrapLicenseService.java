4d278081faa04db2d71b8308644ace04ae80be3b
mlyons <mlyons@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2016-05-11 21:45:20 +0000

    SSG-13439: When the Gateway is started with the system property com.l7tech.bootstrap.env.license.enable=true, the BootstrapLicenseServlet will now check the SSG_LICENSE environment variable (in addition to the bootstrap/license/ folder) for a license.  When provided via the SSG_LICENSE env var, the value of the variable must be a Base 64 ecoded, Gzipped license XML file.  This is exactly the same format (and env var name) used by the current Gateway docker container.
    The environment variable the Gateway checks can be changed from the default of SSG_LICENSE to some other env var name by setting the system property com.l7tech.bootstrap.env.license.variable to the name of the evironment variable to use instead.
    
    
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@46476 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/server/src/main/java/com/l7tech/server/licensing/BootstrapLicenseService.java
+++ modules/gateway/server/src/main/java/com/l7tech/server/licensing/BootstrapLicenseService.java
@@ -4,29 +4,37 @@ import com.l7tech.gateway.common.licensing.LicenseDocument;
 import com.l7tech.server.event.AdminInfo;
 import com.l7tech.server.event.system.Initialized;
 import com.l7tech.server.util.PostStartupApplicationListener;
-import com.l7tech.util.ConfigFactory;
-import com.l7tech.util.ExceptionUtils;
-import com.l7tech.util.IOUtils;
+import com.l7tech.util.*;
 import org.springframework.context.ApplicationEvent;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.concurrent.Callable;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.zip.GZIPInputStream;
 
 /**
  * At gateway startup, if no license is installed, then try loading license file found in "bootstrap.folder.license"
  */
 public class BootstrapLicenseService implements PostStartupApplicationListener {
     private final String BOOTSTRAP_LICENSE_FOLDER;
+    private final String ENV_LICENSE_VAR;
+    String licenseFromEnv;
 
     private LicenseDocumentManager licenseDocumentManager;
     private final Logger logger = Logger.getLogger(getClass().getName());
 
+
     public BootstrapLicenseService(LicenseDocumentManager licenseDocumentManager) {
         this.licenseDocumentManager = licenseDocumentManager;
         BOOTSTRAP_LICENSE_FOLDER = ConfigFactory.getProperty("bootstrap.folder.license");
+        ENV_LICENSE_VAR = ConfigFactory.getProperty( "bootstrap.env.license.variable" );
+        licenseFromEnv = ENV_LICENSE_VAR != null && ENV_LICENSE_VAR.trim().length() > 0
+                ? System.getenv( ENV_LICENSE_VAR )
+                : null;
     }
 
     @Override
@@ -38,34 +46,53 @@ public class BootstrapLicenseService implements PostStartupApplicationListener {
 
     private void loadLicenseFromFile() {
         try {
-            if (licenseDocumentManager.findAll().isEmpty() && BOOTSTRAP_LICENSE_FOLDER != null) {
-                File licenseFolder = new File(BOOTSTRAP_LICENSE_FOLDER);
-                if (!licenseFolder.exists()) return;
-                if (!licenseFolder.isDirectory()) return;
-                File[] licenseFiles = licenseFolder.listFiles();
-                if (licenseFiles == null || licenseFiles.length == 0) return;
-                for (final File licenseFile : licenseFiles) {
-                    AdminInfo.find(false).wrapCallable(new Callable<LicenseDocument>() {
-                        @Override
-                        public LicenseDocument call() throws Exception {
-                            // attempt to load license from file
-                            logger.info("Installing license from: " + licenseFile.getCanonicalPath());
-                            InputStream is = new FileInputStream(licenseFile);
-                            try {
-                                byte[] docBytes = IOUtils.slurpStream(is);
+            if ( licenseDocumentManager.findAll().isEmpty() ) {
+                if ( ConfigFactory.getBooleanProperty( "bootstrap.env.license.enable", false ) &&
+                        licenseFromEnv != null && licenseFromEnv.trim().length() > 0 )
+                {
+                    // Check for single license in environment variable
+                    byte[] bytes = HexUtils.decodeBase64( licenseFromEnv, true );
+                    try ( InputStream is = new GZIPInputStream( new ByteArrayInputStream( bytes ) ) ) {
+                        final byte[] docBytes = IOUtils.slurpStream( is );
+                        AdminInfo.find(false).wrapCallable(new Callable<LicenseDocument>() {
+                            @Override
+                            public LicenseDocument call() throws Exception {
+                                // attempt to load license from file
+                                logger.info( "Installing license from " + ENV_LICENSE_VAR + " environment variable" );
                                 final LicenseDocument licenseDocument = new LicenseDocument(new String(docBytes));
                                 licenseDocumentManager.saveWithImmediateFlush(licenseDocument);
                                 return licenseDocument;
-                            } finally {
-                                is.close();
                             }
+                        }).call();
+                    }
+                }
 
-                        }
-                    }).call();
+                if ( BOOTSTRAP_LICENSE_FOLDER != null ) {
+                    // Might be license files on the filesystem
+                    File licenseFolder = new File(BOOTSTRAP_LICENSE_FOLDER);
+                    if (!licenseFolder.exists()) return;
+                    if (!licenseFolder.isDirectory()) return;
+                    File[] licenseFiles = licenseFolder.listFiles();
+                    if (licenseFiles == null || licenseFiles.length == 0) return;
+                    for (final File licenseFile : licenseFiles) {
+                        AdminInfo.find(false).wrapCallable(new Callable<LicenseDocument>() {
+                            @Override
+                            public LicenseDocument call() throws Exception {
+                                // attempt to load license from file
+                                logger.info("Installing license from: " + licenseFile.getCanonicalPath());
+                                try ( InputStream is = new FileInputStream( licenseFile ) ) {
+                                    byte[] docBytes = IOUtils.slurpStream( is );
+                                    final LicenseDocument licenseDocument = new LicenseDocument( new String( docBytes ) );
+                                    licenseDocumentManager.saveWithImmediateFlush( licenseDocument );
+                                    return licenseDocument;
+                                }
+                            }
+                        }).call();
+                    }
                 }
             }
         } catch (Exception e) {
-            logger.warning(ExceptionUtils.getMessageWithCause(e));
+            logger.log( Level.WARNING, "Unable to install license: " + ExceptionUtils.getMessageWithCause(e), ExceptionUtils.getDebugException( e ) );
         }
     }
 }
--- modules/gateway/server/src/test/java/com/l7tech/server/licensing/LicenseBootstrapServiceTest.java
+++ modules/gateway/server/src/test/java/com/l7tech/server/licensing/LicenseBootstrapServiceTest.java
@@ -2,14 +2,18 @@ package com.l7tech.server.licensing;
 
 import com.l7tech.gateway.common.Component;
 import com.l7tech.server.event.system.Initialized;
+import com.l7tech.test.BugId;
 import com.l7tech.util.FileUtils;
+import com.l7tech.util.HexUtils;
+import com.l7tech.util.IOUtils;
 import com.l7tech.util.SyspropUtil;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.File;
+import java.io.*;
 import java.net.URL;
+import java.util.zip.GZIPOutputStream;
 
 import static junit.framework.Assert.assertEquals;
 
@@ -26,6 +30,7 @@ public class LicenseBootstrapServiceTest {
     @After
     public void after() throws Exception {
         SyspropUtil.clearProperty("com.l7tech.bootstrap.folder.license");
+        SyspropUtil.clearProperty("com.l7tech.bootstrap.env.license.enable");
         FileUtils.deleteDir(tmpDir);
     }
 
@@ -39,6 +44,47 @@ public class LicenseBootstrapServiceTest {
         BootstrapLicenseService service = new BootstrapLicenseService(docManager);
         service.onApplicationEvent(new Initialized(this, Component.GATEWAY, "Test"));
         assertEquals(1, docManager.findAll().size());
+    }
+
+    @Test
+    @BugId( "SSG-13439" )
+    public void TestLoadLicenseFromEnvironment() throws Exception{
+        URL otherLicense = LicenseBootstrapServiceTest.class.getResource("ValidLicenseDocumentWithoutSignature.xml");
+        byte[] licenseBytes = IOUtils.slurpUrl( otherLicense );
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try ( InputStream is = new ByteArrayInputStream( licenseBytes ); GZIPOutputStream os = new GZIPOutputStream( baos ) ) {
+            IOUtils.copyStream( is, os );
+        }
+        String ssgLicense = HexUtils.encodeBase64( baos.toByteArray() );
+
+        // Enable load from env var
+        SyspropUtil.setProperty( "com.l7tech.bootstrap.env.license.enable", "true" );
+
+        LicenseDocumentManager docManager = new LicenseDocumentManagerStub();
+        BootstrapLicenseService service = new BootstrapLicenseService(docManager);
+        service.licenseFromEnv = ssgLicense; // poke license in for test (can't modify env vars, sadly)
+        service.onApplicationEvent(new Initialized(this, Component.GATEWAY, "Test"));
+        assertEquals( "License in environment must be installed if system property enabled",
+                1, docManager.findAll().size() );
+    }
+
+    @Test
+    @BugId( "SSG-13439" )
+    public void TestLoadLicenseFromEnvironment_notEnabled() throws Exception{
+        URL otherLicense = LicenseBootstrapServiceTest.class.getResource("ValidLicenseDocumentWithoutSignature.xml");
+        byte[] licenseBytes = IOUtils.slurpUrl( otherLicense );
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try ( InputStream is = new ByteArrayInputStream( licenseBytes ); GZIPOutputStream os = new GZIPOutputStream( baos ) ) {
+            IOUtils.copyStream( is, os );
+        }
+        String ssgLicense = HexUtils.encodeBase64( baos.toByteArray() );
+
+        LicenseDocumentManager docManager = new LicenseDocumentManagerStub();
+        BootstrapLicenseService service = new BootstrapLicenseService(docManager);
+        service.licenseFromEnv = ssgLicense; // poke license in for test (can't modify env vars, sadly)
+        service.onApplicationEvent(new Initialized(this, Component.GATEWAY, "Test"));
+        assertEquals( "License in environment must be ignored if system property not enabled",
+                0, docManager.findAll().size() );
     }
 
     @Test
