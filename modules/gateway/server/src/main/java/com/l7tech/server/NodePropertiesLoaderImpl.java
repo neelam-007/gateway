c604ab662eb4cd3e9d17aa6f39c76224acffdb3e
jwilliams <jwilliams@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2016-09-19 18:01:42 +0000

    US213972: Updated diskless config feature based on func spec review. System property enabling diskless config renamed to have com.l7tech prefix, many environment variables now have default values and are not required, and the node ID will generate a new ID if not specified.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@47338 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/server/src/main/java/com/l7tech/server/NodePropertiesLoaderImpl.java
+++ modules/gateway/server/src/main/java/com/l7tech/server/NodePropertiesLoaderImpl.java
@@ -1,23 +1,38 @@
 package com.l7tech.server;
 
+import com.l7tech.util.HexUtils;
 import com.l7tech.util.IOUtils;
 import com.l7tech.util.SyspropUtil;
+import org.apache.commons.lang.StringUtils;
 
 import java.io.*;
+import java.security.SecureRandom;
+import java.util.HashMap;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static java.util.Map.Entry;
 
 /**
- * This is a singleton that consults the disklessConfig system property on startup, and if it is defined and has
- * a value of true then it loads node properties from environment variables. The names of these environment variables
- * and the corresponding node properties are defined in the nodePropsEnvVarDefs.properties file, and are based on the
- * variable names already for our Docker release. If disklessConfig is undefined or is set to false then the node
- * properties will be read from the standard node.properties file.
+ * This is a singleton that consults the com.l7tech.disklessConfig system property on startup, and if it is defined and
+ * has a value of true then it loads node properties from environment variables. The names of these environment
+ * variables and the corresponding node properties and their default values (where applicable) are defined in the
+ * nodePropsEnvVarDefs.properties file, and are based on the variable names already for our Docker release.
+ * If com.l7tech.disklessConfig is undefined or is set to false then the node properties will be read from the standard
+ * node.properties file.
  *
  * @author Jamie Williams - jamie.williams2@ca.com
  */
 public class NodePropertiesLoaderImpl implements NodePropertiesLoader {
+    private static final Logger logger = Logger.getLogger(NodePropertiesLoaderImpl.class.getName());
+
+    private static final String DISKLESS_CONFIG_SYSTEM_PROP = "com.l7tech.disklessConfig";
     private static final String NODE_PROPERTIES_FILE_NAME = "node.properties";
     private static final String ENV_VAR_DEFINITIONS_FILE = "resources/nodePropsEnvVarDefs.properties";
+    private static final String VARIABLE_SUFFIX = "variable";
+    private static final String DEFAULT_SUFFIX = "default";
+    private static final String GENERATE_SPECIAL_DEFAULT = "GENERATE";
 
     private static NodePropertiesLoader INSTANCE = null;
 
@@ -26,7 +41,7 @@ public class NodePropertiesLoaderImpl implements NodePropertiesLoader {
     private final boolean diskless;
 
     private NodePropertiesLoaderImpl() {
-        this.diskless = SyspropUtil.getBoolean("disklessConfig", false);
+        this.diskless = SyspropUtil.getBoolean(DISKLESS_CONFIG_SYSTEM_PROP, false);
 
         loadNodeProperties();
     }
@@ -42,18 +57,55 @@ public class NodePropertiesLoaderImpl implements NodePropertiesLoader {
                 throw new IllegalStateException("Could not load node properties environment variable definitions");
             }
 
+            HashMap<String,String> variables = new HashMap<>();
+            HashMap<String,String> defaults = new HashMap<>();
+
             // add all node properties from environment variables
-            for (String nodePropertyName : envVarDefs.stringPropertyNames()) {
-                String environmentVariable = envVarDefs.getProperty(nodePropertyName);
+            for (String propertyName : envVarDefs.stringPropertyNames()) {
+                int suffixSeparatorIndex = propertyName.lastIndexOf('.');
 
-                String nodePropertyValue = System.getenv(environmentVariable);
+                String nodePropertyName = propertyName.substring(0, suffixSeparatorIndex);
+                String suffix = propertyName.substring(suffixSeparatorIndex + 1);
+                String value = envVarDefs.getProperty(propertyName);
 
-                if (null == nodePropertyValue) {
-                    throw new IllegalStateException("The '" + environmentVariable +
-                            "' environment variable must be defined for the '" + nodePropertyName +
-                            "' node property when using diskless config mode");
-                } else {
-                    properties.setProperty(nodePropertyName, nodePropertyValue);
+                switch (suffix) {
+                    case VARIABLE_SUFFIX:
+                        variables.put(nodePropertyName, value);
+                        break;
+                    case DEFAULT_SUFFIX:
+                        defaults.put(nodePropertyName, value);
+                        break;
+                    default:
+                        logger.fine("Unrecognized node property definition: " + propertyName);
+                        break;
+                }
+            }
+
+            for (Entry<String, String> variableDef : variables.entrySet()) {
+                String environmentVariableValue = System.getenv(variableDef.getValue());
+
+                // no environment variable defined or value empty
+                if (StringUtils.isBlank(environmentVariableValue)) {
+                    // if there's a default, use it
+                    if (defaults.containsKey(variableDef.getKey())) {
+                        String defaultValue = defaults.get(variableDef.getKey());
+
+                        // do special handling for properties that should have generated defaults (i.e. node.id)
+                        if (GENERATE_SPECIAL_DEFAULT.equals(defaultValue)) {
+                            defaultValue = generateRandomNodeId();
+                            logger.fine("Generated value for ' " + variableDef.getKey() + "': " + defaultValue);
+                        } else {
+                            logger.fine("Using default value for '" + variableDef.getKey() + "': " + defaultValue);
+                        }
+
+                        properties.setProperty(variableDef.getKey(), defaultValue);
+                    } else { // otherwise this is a required property and we can't start
+                        throw new IllegalStateException("The '" + variableDef.getValue() +
+                                "' environment variable must be defined for the '" + variableDef.getKey() +
+                                "' node property when using diskless config mode");
+                    }
+                } else { // use the value set to the environment variable
+                    properties.setProperty(variableDef.getKey(), environmentVariableValue);
                 }
             }
         } else {
@@ -72,6 +124,12 @@ public class NodePropertiesLoaderImpl implements NodePropertiesLoader {
                 throw new IllegalStateException("Error accessing node properties", e);
             }
         }
+    }
+
+    private String generateRandomNodeId() {
+        byte[] nodeIdBytes = new byte[32];
+        new SecureRandom().nextBytes(nodeIdBytes);
+        return HexUtils.encodeBase64(nodeIdBytes, true);
     }
 
     @Override
