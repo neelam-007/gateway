a351f7791a1ba50225adbe78a4fbda3ad1f22d0f
jwilliams <jwilliams@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2016-10-07 23:20:35 +0000

    DE213018: Changed the 'io.mqRoutingConvertMessageApplicationDataFormat' to 'io.mqConvertMessageApplicationDataFormat' and 'io.mqRoutingForceReturnPropertiesInMQRFH2Header' cluster property to ' 'io.mqForceReturnPropertiesInMQRFH2Header' for consistency.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@47421 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/server/src/main/java/com/l7tech/server/ServerConfigParams.java
+++ modules/gateway/server/src/main/java/com/l7tech/server/ServerConfigParams.java
@@ -205,8 +205,8 @@ public interface ServerConfigParams {
     String PARAM_IO_MQ_MESSAGE_MAX_BYTES = "ioMqMessageMaxBytes";
     String PARAM_IO_MQ_SET_ALL_CONTEXT = "ioMqRoutingSetAllContext";
     String PARAM_IO_MQ_INCLUDE_REPLY_QUEUE_MANAGER_NAME = "io.mqIncludeReplyToQueueManagerName";
-    String PARAM_IO_MQ_FORCE_RETURN_PROPS_IN_MQRFH2_HEADER = "io.mqRoutingForceReturnPropertiesInMQRFH2Header";
-    String PARAM_IO_MQ_CONVERT_MESSAGE_APPLICATION_DATA_FORMAT = "io.MqRoutingConvertMessageApplicationDataFormat";
+    String PARAM_IO_MQ_FORCE_RETURN_PROPS_IN_MQRFH2_HEADER = "io.mqForceReturnPropertiesInMQRFH2Header";
+    String PARAM_IO_MQ_CONVERT_MESSAGE_APPLICATION_DATA_FORMAT = "io.mqConvertMessageApplicationDataFormat";
     String PARAM_DATE_TIME_CUSTOM_FORMATS = "datetime.customFormats";
     String PARAM_DATE_TIME_AUTO_FORMATS = "datetime.autoFormats";
     String PARAM_THROUGHPUTQUOTA_ENFORCE_MAX_QUOTA = "throughputQuota.enforce_max_quota";
