412f5a072937a7aaf68b4a6c032ecf9c0531f4a8
mlyons <mlyons@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2016-11-07 23:38:32 +0000

    DE248282: Add JavaDoc discouraging use of per permissive trust manager in production TLS code.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@47587 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/common/src/main/java/com/l7tech/common/io/PermissiveX509TrustManager.java
+++ modules/common/src/main/java/com/l7tech/common/io/PermissiveX509TrustManager.java
@@ -6,6 +6,13 @@ import java.security.cert.CertificateException;
 
 /**
  * An X509 Trust manager that trusts everything.
+ * <p/>
+ * <b>THIS DOES NOT CHECK THE SERVER CERTIFICATE.
+ * DO NOT USE THIS IN PRODUCTION TLS CODE UNLESS YOU ARE DOING YOUR OWN CERT OR PIN CHECKING!
+ * If you don't know whether you are doing your own cert or pin checking, you probably are not!
+ * </b>
+ * See "The most dangerous code in the world: validating SSL certificates in non-browser software"
+ * http://crypto.stanford.edu/~dabo/pubs/abstracts/ssl-client-bugs.html
  */
 public class PermissiveX509TrustManager implements X509TrustManager {
     public void checkClientTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
