8bd4c7a383e2b83832e5f2a40df6afa488ed7b4f
jkarlson <jkarlson@219f1c6e-4a0e-43c8-96f2-6e749be13fd4>
2016-11-30 01:42:43 +0000

    US215963: Tests for previous changes.
    git-svn-id: https://thesource.l7tech.com/thesource/UneasyRooster/trunk@47714 219f1c6e-4a0e-43c8-96f2-6e749be13fd4

--- modules/gateway/assertions/LDAPQueryAssertion/src/test/java/com/l7tech/external/assertions/ldapquery/server/ServerLDAPQueryAssertionTest.java
+++ modules/gateway/assertions/LDAPQueryAssertion/src/test/java/com/l7tech/external/assertions/ldapquery/server/ServerLDAPQueryAssertionTest.java
@@ -20,9 +20,15 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.support.StaticListableBeanFactory;
 
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.directory.Attribute;
+import javax.naming.directory.DirContext;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -62,40 +68,169 @@ public class ServerLDAPQueryAssertionTest {
     }
 
     @Test
-    public void testSingleResult() throws Exception {
-        doTestResults( AssertionStatus.NONE, 0, false, false, false, new String[]{ "a" }, "a" );
-        doTestResults( AssertionStatus.NONE, 0, false, false, false, new String[0], null );
-        doTestResults( AssertionStatus.NONE, 0, true, false, true, new String[]{ "a", "b" }, "a, b" );
-        doTestResults( AssertionStatus.NONE, 0, true, false, false, new String[]{ "a", "b" }, new String[]{ "a", "b" } );
-        doTestResults( AssertionStatus.NONE, 0, false, true, false, new String[]{ "a" }, "a" );
-        doTestResults( AssertionStatus.FALSIFIED, 0, false, true, false, new String[]{ "a", "b" }, null );
+    public void testSingleResultClassicBehaviour() throws Exception {
+        doTestResults( AssertionStatus.NONE, 0, false, false, false, new String[]{ "a" }, "a", false);
+        doTestResults( AssertionStatus.NONE, 0, false, false, false, new String[0], null, false);
+        doTestResults( AssertionStatus.NONE, 0, true, false, true, new String[]{ "a", "b" }, "a, b", false);
+        doTestResults( AssertionStatus.NONE, 0, true, false, false, new String[]{ "a", "b" }, new String[]{ "a", "b" }, false);
+        doTestResults( AssertionStatus.NONE, 0, false, true, false, new String[]{ "a" }, "a", false);
+        doTestResults( AssertionStatus.FALSIFIED, 0, false, true, false, new String[]{ "a", "b" }, null, false);
     }
 
     @Test
-    public void testMultipleResults() throws Exception {
-        doTestResults( AssertionStatus.NONE, 1, false, false, false, new String[]{ "a" }, new String[]{ "a" } );
-        doTestResults( AssertionStatus.NONE, 1, false, false, false, new String[0], null );
-        doTestResults( AssertionStatus.NONE, 1, true, false, true, new String[]{ "a", "b" }, new String[]{ "a, b" } );
-        doTestResults( AssertionStatus.NONE, 1, true, false, false, new String[]{ "a", "b" }, new String[]{ "a", "b" } );
-        doTestResults( AssertionStatus.NONE, 1, false, true, false, new String[]{ "a" }, new String[]{ "a" } );
-
-        doTestResults( AssertionStatus.NONE, 2, false, false, false, new String[]{ "a" }, new String[]{ "a", "a" } );
-        doTestResults( AssertionStatus.NONE, 2, false, false, false, new String[0], null );
-        doTestResults( AssertionStatus.NONE, 2, true, false, true, new String[]{ "a", "b" }, new String[]{ "a, b", "a, b" } );
-        doTestResults( AssertionStatus.NONE, 2, true, false, false, new String[]{ "a", "b" }, new String[]{ "a", "b", "a", "b" } );
-        doTestResults( AssertionStatus.NONE, 2, false, true, false, new String[]{ "a" }, new String[]{ "a", "a" } );
-
-        doTestResults( AssertionStatus.FALSIFIED, 1, false, true, false, new String[]{ "a", "b" }, null );
+    public void testSingleResultIncludeEmptyAttributeBehaviour() throws Exception {
+        doTestResults( AssertionStatus.NONE, 0, false, false, false, new String[]{ "a" }, "a", true);
+        doTestResults( AssertionStatus.NONE, 0, false, false, false, new String[0], "", true);
+        doTestResults( AssertionStatus.NONE, 0, true, false, true, new String[]{ "a", "b" }, "a, b", true);
+        doTestResults( AssertionStatus.NONE, 0, true, false, false, new String[]{ "a", "b" }, new String[]{ "a", "b" }, true);
+        doTestResults( AssertionStatus.NONE, 0, false, true, false, new String[]{ "a" }, "a", true);
+        doTestResults( AssertionStatus.FALSIFIED, 0, false, true, false, new String[]{ "a", "b" }, null, true);
     }
 
-    private void doTestResults( final AssertionStatus expectedStatus,
-                                final int repeats, // 0 for multiple results not expected
-                                final boolean multivalued,
-                                final boolean failMultivalued,
-                                final boolean joinMultivalued,
-                                final String[] results,
-                                final Object expectedValue ) throws Exception {
+    @Test
+    public void testMultipleResultsClassicBehaviour() throws Exception {
+        doTestResults( AssertionStatus.NONE, 1, false, false, false, new String[]{ "a" }, new String[]{ "a" }, false);
+        doTestResults( AssertionStatus.NONE, 1, false, false, false, new String[0], null, false);
+        doTestResults( AssertionStatus.NONE, 1, true, false, true, new String[]{ "a", "b" }, new String[]{ "a, b" }, false);
+        doTestResults( AssertionStatus.NONE, 1, true, false, false, new String[]{ "a", "b" }, new String[]{ "a", "b" }, false);
+        doTestResults( AssertionStatus.NONE, 1, false, true, false, new String[]{ "a" }, new String[]{ "a" }, false);
+
+        doTestResults( AssertionStatus.NONE, 2, false, false, false, new String[]{ "a" }, new String[]{ "a", "a" }, false);
+        doTestResults( AssertionStatus.NONE, 2, false, false, false, new String[0], null, false);
+        doTestResults( AssertionStatus.NONE, 2, true, false, true, new String[]{ "a", "b" }, new String[]{ "a, b", "a, b" }, false);
+        doTestResults( AssertionStatus.NONE, 2, true, false, false, new String[]{ "a", "b" }, new String[]{ "a", "b", "a", "b" }, false);
+        doTestResults( AssertionStatus.NONE, 2, false, true, false, new String[]{ "a" }, new String[]{ "a", "a" }, false);
+
+        doTestResults( AssertionStatus.FALSIFIED, 1, false, true, false, new String[]{ "a", "b" }, null, false);
+    }
+
+    @Test
+    public void testMultipleResultIncludeEmptyAttributeBehaviour() throws Exception {
+        doTestResults( AssertionStatus.NONE, 1, false, false, false, new String[]{ "a" }, new String[]{ "a" }, true);
+        doTestResults( AssertionStatus.NONE, 1, false, false, false, new String[0], new String[]{ "" }, true);
+        doTestResults( AssertionStatus.NONE, 1, true, false, true, new String[]{ "a", "b" }, new String[]{ "a, b" }, true);
+
+        doTestResults( AssertionStatus.NONE, 1, true, false, false, new String[]{ "a", "b" }, new String[]{ "a", "b" }, true);
+        doTestResults( AssertionStatus.NONE, 1, false, true, false, new String[]{ "a" }, new String[]{ "a" }, true);
+
+        doTestResults( AssertionStatus.NONE, 2, false, false, false, new String[]{ "a" }, new String[]{ "a", "a" }, true);
+        doTestResults( AssertionStatus.NONE, 2, false, false, false, new String[0], new String[] {"", ""}, true);
+        doTestResults( AssertionStatus.NONE, 2, true, false, true, new String[]{ "a", "b" }, new String[]{ "a, b", "a, b" }, true);
+        doTestResults( AssertionStatus.NONE, 2, true, false, false, new String[]{ "a", "b" }, new String[]{ "a", "b", "a", "b" }, true);
+        doTestResults( AssertionStatus.NONE, 2, false, true, false, new String[]{ "a" }, new String[]{ "a", "a" }, true);
+
+        doTestResults( AssertionStatus.FALSIFIED, 1, false, true, false, new String[]{ "a", "b" }, null, true);
+    }
+
+    @Test
+    public void testMultipleMultiValueResults() throws Exception {
+        doJoinMultiValueTestResults(new String[]{ "a", "" }, true, new String[] { "a" }, new String[] { null });
+        doJoinMultiValueTestResults(new String[]{ "a", "" }, true, new String[] { "a" }, new String[] { "" });
+        doJoinMultiValueTestResults(new String[]{ "a, b", "c" }, true, new String[] { "a", "b" }, new String[] { "c" });
+        doJoinMultiValueTestResults(new String[]{ "a, b", "" }, true, new String[] { "a", "b" }, new String[] { null });
+        doJoinMultiValueTestResults(new String[]{ "a, , b", "" }, true, new String[] { "a", "", "b" }, new String[] { null });
+        doJoinMultiValueTestResults(new String[]{ ", b", "" }, true, new String[] { "", "b" }, new String[] { null });
+    }
+
+    @Test
+    public void testIncludeEmptyAttributes() throws Exception{
+        doJoinMultiValueTestResults(new String[]{ "" }, true, new String[] { null });
+        doJoinMultiValueTestResults(new String[]{}, false, new String[] { null });
+    }
+
+    private void doJoinMultiValueTestResults(final Object expectedValue,
+                               boolean includeEmptyAttributes,
+                               final String[]... results
+                               ) throws Exception {
         final LDAPQueryAssertion assertion = new LDAPQueryAssertion();
+        assertion.setIncludeEmptyAttributes(includeEmptyAttributes);
+        assertion.setSearchFilter( "(cn=*)" );
+        assertion.setAllowMultipleResults( true );
+        assertion.setQueryMappings( new QueryAttributeMapping[]{
+                new QueryAttributeMapping( "cn", "lqcn", true, false, true )
+        } );
+
+        final ServerLDAPQueryAssertion test = new ServerLDAPQueryAssertion( assertion, beanFactory() ){
+            @Override
+            protected int doSearch( final String filter,
+                                    final String[] attributeNames,
+                                    final int maxResults,
+                                    final Functions.BinaryVoidThrows<QueryAttributeMapping, ServerLDAPQueryAssertion.SimpleAttribute, Exception> resultCallback ) throws FindException {
+                try {
+                    for (int i=0; i < results.length; i++) {
+
+                        Attribute attribute = null;
+                        if (countNotNull(results[i]) > 0) {
+                            final int finalI = i;
+                            attribute = new StubAttribute() {
+                                @Override
+                                public Object get(int index) throws NamingException {
+                                    return results[finalI][index];
+                                }
+
+                                @Override
+                                public int size() {
+                                    return results[finalI].length;
+                                }
+                            };
+                        }
+
+                        resultCallback.call( assertion.getQueryMappings()[0], new SimpleAttribute( "cn=a", attribute ));
+                    }
+                } catch ( Exception e ) {
+                    throw ExceptionUtils.wrap( e );
+                }
+                return 1;
+            }
+        };
+
+        final AssertionStatus status = evaluateAssertion( test, Collections.emptyMap(), new Functions.UnaryVoid<PolicyEnforcementContext>(){
+            @Override
+            public void call( final PolicyEnforcementContext policyEnforcementContext ) {
+                try {
+                    final Object value = policyEnforcementContext.getVariable( "lqcn" );
+                    if ( expectedValue == null ) {
+                        assertNull( "Result should be null", null );
+                    } else {
+                        assertNotNull( "Result var present", value );
+                        if ( expectedValue instanceof String[] ) {
+                            assertTrue( "Result is array", value instanceof String[] );
+                            assertArrayEquals( "Result var value", (String[])expectedValue, (String[])value );
+                        } else {
+                            assertEquals( "Result var value", expectedValue, value );
+                        }
+                    }
+                } catch ( NoSuchVariableException e ) {
+                    if ( expectedValue != null ) throw new RuntimeException(e);
+                }
+            }
+        } );
+        assertEquals(AssertionStatus.NONE, status);
+    }
+
+    private static int countNotNull(final String[] array) {
+        int count = 0;
+        for (final String a : array) {
+            if (a != null) {
+                count++;
+            }
+        }
+        return count;
+        // This should be replaceable with:
+        // Arrays.stream(array).filter(x -> x != null).count();
+        // But something doesn't work in the build process.
+    }
+
+    private void doTestResults(final AssertionStatus expectedStatus,
+                               final int repeats, // 0 for multiple results not expected
+                               final boolean multivalued,
+                               final boolean failMultivalued,
+                               final boolean joinMultivalued,
+                               final String[] results,
+                               final Object expectedValue,
+                               boolean includeEmptyAttributes) throws Exception {
+        final LDAPQueryAssertion assertion = new LDAPQueryAssertion();
+        assertion.setIncludeEmptyAttributes(includeEmptyAttributes);
         assertion.setSearchFilter( "(cn=*)" );
         assertion.setAllowMultipleResults( repeats > 0 );
         assertion.setQueryMappings( new QueryAttributeMapping[]{
@@ -254,5 +389,88 @@ public class ServerLDAPQueryAssertionTest {
         beanFactory.addBean( "identityProviderFactory", new IdentityProviderFactory( new TestIdentityProviderConfigManager() ) );
         beanFactory.addBean( "managedBackgroundTimer", new MockTimer() );
         return beanFactory;
+    }
+
+    private static class StubAttribute implements Attribute {
+
+        @Override
+        public NamingEnumeration<?> getAll() throws NamingException {
+            return null;
+        }
+
+        @Override
+        public Object get() throws NamingException {
+            return null;
+        }
+
+        @Override
+        public int size() {
+            return 0;
+        }
+
+        @Override
+        public String getID() {
+            return null;
+        }
+
+        @Override
+        public boolean contains(Object attrVal) {
+            return false;
+        }
+
+        @Override
+        public boolean add(Object attrVal) {
+            return false;
+        }
+
+        @Override
+        public boolean remove(Object attrval) {
+            return false;
+        }
+
+        @Override
+        public void clear() {
+
+        }
+
+        @Override
+        public DirContext getAttributeSyntaxDefinition() throws NamingException {
+            return null;
+        }
+
+        @Override
+        public DirContext getAttributeDefinition() throws NamingException {
+            return null;
+        }
+
+        @Override
+        public Object clone() {
+            return null;
+        }
+
+        @Override
+        public boolean isOrdered() {
+            return false;
+        }
+
+        @Override
+        public Object get(int ix) throws NamingException {
+            return null;
+        }
+
+        @Override
+        public Object remove(int ix) {
+            return null;
+        }
+
+        @Override
+        public void add(int ix, Object attrVal) {
+
+        }
+
+        @Override
+        public Object set(int ix, Object attrVal) {
+            return null;
+        }
     }
 }
