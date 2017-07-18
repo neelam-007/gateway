ea62d1f932a11c017039bcd17f8208740f089ed3
Alex Huang <alex.huang@ca.com>
2017-07-18 11:07:52 -0700

    DE256114 : Made 'length' constant final

--- modules/gateway/server/src/main/java/com/l7tech/server/policy/variable/LengthSuffixSelector.java
+++ modules/gateway/server/src/main/java/com/l7tech/server/policy/variable/LengthSuffixSelector.java
@@ -6,7 +6,7 @@ import java.text.MessageFormat;
 import java.util.Collection;
 
 public class LengthSuffixSelector implements ExpandVariables.SuffixSelector {
-    static String LENGTH_SUFFIX = "length";
+    static final String LENGTH_SUFFIX = "length";
 
     @Override
     public ExpandVariables.Selector.Selection select(Object value, Syntax.SyntaxErrorHandler handler, boolean strict) {
