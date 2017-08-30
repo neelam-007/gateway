b9789ab4ca7476611f26ed907483d8929dfcbc4e
leean11 <leean11@ca.com>
2017-08-30 11:44:21 -0700

    US358679 More code cleanup.

--- modules/gateway/assertions/GatewayManagementAssertion/src/test/java/com/l7tech/external/assertions/gatewaymanagement/server/BundleBuilder.java
+++ modules/gateway/assertions/GatewayManagementAssertion/src/test/java/com/l7tech/external/assertions/gatewaymanagement/server/BundleBuilder.java
@@ -13,25 +13,29 @@ import java.util.List;
  * Helps to create {@link Bundle}s for more readable unit tests.
  */
 public class BundleBuilder {
-    private ItemBuilder<ServiceMO> serviceItemBuilder = new ItemBuilder<ServiceMO>("serviceMOBuilder", EntityType.SERVICE.toString());;
-    private ItemBuilder<ServiceAliasMO> serviceAliasBuilder = new ItemBuilder<ServiceAliasMO>("serviceAliasBuilder", EntityType.SERVICE_ALIAS.toString());;
+    private ItemBuilder<ServiceMO> serviceItemBuilder = new ItemBuilder<ServiceMO>("serviceMOBuilder", EntityType.SERVICE.toString());
+    private ItemBuilder<ServiceAliasMO> serviceAliasBuilder = new ItemBuilder<ServiceAliasMO>("serviceAliasBuilder", EntityType.SERVICE_ALIAS.toString());
     private List<Mapping> mappings =  new ArrayList<>();
     private List<Item> items = new ArrayList<>();
 
     public BundleBuilder addServiceAlias(final PublishedService service, final PublishedServiceAlias alias) {
         final ServiceMO serviceMO = ManagedObjectFactory.createService();
         serviceMO.setId(serviceMO.getId());
-        ServiceDetail detail = ManagedObjectFactory.createServiceDetail();
+        final ServiceDetail detail = ManagedObjectFactory.createServiceDetail();
         detail.setName(service.getName());
         serviceMO.setServiceDetail(detail);
         items.add(serviceItemBuilder.setContent(serviceMO).build());
-        mappings.add(createMapping(EntityType.SERVICE.toString(), Mapping.Action.NewOrUpdate, service.getId()));
+        mappings.add(new MappingBuilder().withType(EntityType.SERVICE.toString()).
+                withAction(Mapping.Action.NewOrUpdate).
+                withSrcId(service.getId()).build());
 
         final ServiceAliasMO aliasMO = ManagedObjectFactory.createServiceAlias();
         aliasMO.setId(alias.getId());
         aliasMO.setServiceReference(new ManagedObjectReference(ServiceMO.class, service.getId()));
         items.add(serviceAliasBuilder.setContent(aliasMO).build());
-        mappings.add(createMapping(EntityType.SERVICE_ALIAS.toString(), Mapping.Action.NewOrUpdate, alias.getId()));
+        mappings.add(new MappingBuilder().withType(EntityType.SERVICE_ALIAS.toString()).
+                withAction(Mapping.Action.NewOrUpdate).
+                withSrcId(alias.getId()).build());
         return this;
     }
 
@@ -40,13 +44,5 @@ public class BundleBuilder {
         bundle.setMappings(mappings);
         bundle.setReferences(items);
         return bundle;
-    }
-
-    private Mapping createMapping(final String type, final Mapping.Action action, final String id) {
-        final Mapping mappingForTest = ManagedObjectFactory.createMapping();
-        mappingForTest.setType(type);
-        mappingForTest.setAction(action);
-        mappingForTest.setSrcId(id);
-        return mappingForTest;
     }
 }
