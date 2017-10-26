e81ddbc34a59be02707c188b00c395c0dc48fea8
ychan <oyves.chan@ca.com>
2017-10-26 11:47:57 -0700

    US397375: SK upgrades uses restman multi bundle bundle mapping results have the solution kit name + delete or solution kit name + install to differentiate solution kits Rest man and GUI both handle the multi-bundle result Previously Resolving entities seem to be saved however sometimes it doesn’t work? Could be my build switching back and forth branches We may want to force re-resolve entities or leave as default behavior depending on QA effort On upgrade, check the guids of previously installed solution kits and match the guids from the loaded skar file, if none found display an error When upgrading a parent with new solution kits, all new solution kits are available to be selected Verify when upgrading a child, only a child SK is available for selection (this should work but need to double check)

---
+++ modules/gateway/common/src/main/java/com/l7tech/gateway/common/solutionkit/SolutionKitInfo.java
@@ -0,0 +1,47 @@
+package com.l7tech.gateway.common.solutionkit;
+
+import com.l7tech.util.DeserializeSafe;
+import com.l7tech.util.Pair;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * A SolutionKitInfo class to hold information needed for importing multiple bundles at a time in Restman.
+ * Created by chaoy01 on 2017-10-20.
+ */
+@DeserializeSafe
+public class SolutionKitInfo implements Serializable {
+    private Map<SolutionKit, String> solutionKitDelete;
+    private Map<SolutionKit, String> solutionKitInstall;
+    private SolutionKit parentSolutionKit;
+    private boolean isUpgrade;
+
+    public SolutionKitInfo(Map<SolutionKit, String> solutionKitDelete, Map<SolutionKit, String> solutionKitInstall, SolutionKit parentSolutionKit, boolean isUpgrade) {
+        this.solutionKitDelete = solutionKitDelete;
+        this.solutionKitInstall = solutionKitInstall;
+        this.parentSolutionKit = parentSolutionKit;
+        this.isUpgrade = isUpgrade;
+    }
+
+    public Map<SolutionKit, String> getSolutionKitDelete() {
+        return solutionKitDelete;
+    }
+
+    public Map<SolutionKit, String> getSolutionKitInstall() {
+        return solutionKitInstall;
+    }
+
+    public boolean isUpgrade() {
+        return isUpgrade;
+    }
+
+    public void setUpgrade(boolean upgrade) {
+        isUpgrade = upgrade;
+    }
+
+    public SolutionKit getParentSolutionKit() {
+        return parentSolutionKit;
+    }
+}
