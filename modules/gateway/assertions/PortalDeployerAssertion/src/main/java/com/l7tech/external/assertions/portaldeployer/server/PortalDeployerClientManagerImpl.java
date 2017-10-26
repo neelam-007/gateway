0b00eed6d1ce24510b567a6fbca6dd36f006467c
Mike Chester <michael.chester@ca.com>
2017-10-26 10:51:58 -0700

    US398788 - Add new ClientManager and interface
    The manager is responsible for handling the configuration of clients.
    Currently it only manages one client and topic, but could be expanded to
    handle multiple topics.
    
    This was added to remove a lot of the configuration from the module
    listener, and keep the client portion of the code as dumb as possible.
    
    Client manages the connection, disconnects, subscribing and handling of messages.
    Manager handles starting/stopping the connection and configuring th
    client topics/settings.

---
+++ modules/gateway/assertions/PortalDeployerAssertion/src/main/java/com/l7tech/external/assertions/portaldeployer/server/PortalDeployerClientManagerImpl.java
@@ -0,0 +1,48 @@
+package com.l7tech.external.assertions.portaldeployer.server;
+
+import com.l7tech.external.assertions.portaldeployer.server.client.PortalDeployerClient;
+import com.l7tech.external.assertions.portaldeployer.server.client.PortalDeployerClientException;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * @author chemi11, 2017-10-24
+ */
+public class PortalDeployerClientManagerImpl implements PortalDeployerClientManager {
+
+  private int connectTimeout = 60;
+  private int keepAliveInterval = 30;
+
+  private PortalDeployerClient portalDeployerClient;
+  private PortalDeployerClientConfigurationManager configurationManager;
+  private PortalDeployerSslConfigurationManager sslConfigurationManager;
+
+  PortalDeployerClientManagerImpl(final ApplicationContext context) {
+    configurationManager = new PortalDeployerClientConfigurationManagerImpl(context);
+    sslConfigurationManager = new PortalDeployerSslConfigurationManagerImpl(context);
+  }
+
+  private synchronized PortalDeployerClient getClient() throws PortalDeployerConfigurationException, PortalDeployerClientException {
+    if (portalDeployerClient == null) {
+      portalDeployerClient = new PortalDeployerClient(
+          String.format("wss://%s/", configurationManager.getBrokerHost()),
+          String.format("%s_%s_%s", configurationManager.getTenantId(), configurationManager.getTenantGatewayUuid(), "1"),
+          String.format("%s/api/cmd/deploy/tenantGatewayUuid/%s", configurationManager.getTenantId(), configurationManager.getTenantGatewayUuid()),
+          // TODO(chemi11) update to use config manager
+          connectTimeout,
+          keepAliveInterval,
+          sslConfigurationManager.getSniEnabledSocketFactory(configurationManager.getBrokerHost())
+      );
+    }
+    return portalDeployerClient;
+  }
+
+  public void start() throws PortalDeployerClientException, PortalDeployerConfigurationException {
+    //TODO: implement builder and fix client ids/topics to pull from somewhere
+    getClient().start();
+  }
+
+  public void stop() throws PortalDeployerClientException, PortalDeployerConfigurationException {
+    getClient().stop();
+    portalDeployerClient = null;
+  }
+}
