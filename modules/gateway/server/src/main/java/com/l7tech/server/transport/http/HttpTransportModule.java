375281f819136d222230592d88930b3a76a36818
Saxena, Nikhil <Nikhil.Saxena@ca.com>
2017-10-27 15:32:02 +0530

    US411572 : Merge 9.2.00-CR06 to trunk (#356)
    * US411572 : Merge 9.2.00-CR06 to trunk
    
    * US411572 : Merge 9.2.00-CR06 to trunk
    
    * US411572 : Merge 9.2.00-CR06 to trunk
    
    * US411572 : Reverting unnecessary changes from Pull Request
    
    * US411572 : Reverting unnecessary changes

--- modules/gateway/server/src/main/java/com/l7tech/server/transport/http/HttpTransportModule.java
+++ modules/gateway/server/src/main/java/com/l7tech/server/transport/http/HttpTransportModule.java
@@ -742,7 +742,8 @@ public class HttpTransportModule extends TransportModule implements PropertyChan
 
         ProtocolHandler ph = c.getProtocolHandler();
         if (ph instanceof Http11Protocol) {
-            ((Http11Protocol)ph).setExecutor(executor);
+            // DE286454 : adding the Private Thread Pool feature for HTTP listen ports
+            ((Http11Protocol)ph).setExecutor( createAndRegisterExecutorIfRequired( executorName(ssgConn), ssgConn ) );
         } else
             throw new ListenerException("Unable to start HTTP listener on port " + c.getPort() + ": Unrecognized protocol handler: " + ph.getClass().getName());
 
