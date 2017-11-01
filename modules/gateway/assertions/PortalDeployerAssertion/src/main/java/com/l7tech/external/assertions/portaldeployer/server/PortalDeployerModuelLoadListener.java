25fb20418141750eeb237749af101b57e5169d3a
Jay Bagtas <jay.bagtas@ca.com>
2017-11-01 09:50:27 -0700

    US398788 TA878838 - general improvements and implemented module unload
    - added new cluster property portal.deployer.status that can be set to
    STARTED, STOPPED, START_FAILED, STOP_FAILED to communicate a running
    state of portal deployer
    - added module unload

--- modules/gateway/assertions/PortalDeployerAssertion/src/main/java/com/l7tech/external/assertions/portaldeployer/server/PortalDeployerModuelLoadListener.java
+++ modules/gateway/assertions/PortalDeployerAssertion/src/main/java/com/l7tech/external/assertions/portaldeployer/server/PortalDeployerModuelLoadListener.java
@@ -4,6 +4,8 @@ import static com.l7tech.external.assertions.portaldeployer.server.PortalDeploye
 import com.l7tech.external.assertions.portaldeployer.server.client.PortalDeployerClientException;
 import com.l7tech.gateway.common.cluster.ClusterProperty;
 import com.l7tech.objectmodel.FindException;
+import com.l7tech.objectmodel.SaveException;
+import com.l7tech.objectmodel.UpdateException;
 import com.l7tech.server.GatewayState;
 import com.l7tech.server.cluster.ClusterPropertyManager;
 import com.l7tech.server.event.admin.AdminEvent;
@@ -11,19 +13,29 @@ import com.l7tech.server.event.admin.Created;
 import com.l7tech.server.event.admin.Updated;
 import com.l7tech.server.event.system.ReadyForMessages;
 import com.l7tech.server.util.ApplicationEventProxy;
+import com.l7tech.util.ExceptionUtils;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 /**
  * Class that runs when the Portal Deployer modass is loaded. onModuleLoaded is executed via its binding specified
- * in {@link com.l7tech.external.assertions.portaldeployer.PortalDeployerAssertion} metadata.
+ * in {@link com.l7tech.external.assertions.portaldeployer.PortalDeployerAssertion} metadata. onModuleUnloaded is called
+ * when the Portal Deployer modass is unloaded during events such as gateway shutdown, modass unloading (deletion of
+ * assertion, loading of new assertion).
  */
 public class PortalDeployerModuelLoadListener implements ApplicationListener {
   private static final Logger logger = Logger.getLogger(PortalDeployerModuelLoadListener.class.getName());
+  static final String PD_STATUS_CP = "portal.deployer.status";
 
   /**
    * Initializes portal deployer dependencies and starts a client if portal.deployer.enabled cluster property is
@@ -33,36 +45,63 @@ public class PortalDeployerModuelLoadListener implements ApplicationListener {
   public static synchronized void onModuleLoaded(@NotNull ApplicationContext context) {
     logger.log(Level.FINE, "PortalDeployer onModuleLoaded executing");
     if (instance == null) {
-      instance = new PortalDeployerModuelLoadListener(context);
+      instance = new PortalDeployerModuelLoadListener(context,
+              PortalDeployerClientManagerImpl.getInstance(context),
+              Executors.newSingleThreadExecutor());
     }
+    instance.checkIfPortalDeployerEnabledOnceGatewayReady();
+  }
 
-    //only try and start portal deployer if gateway is fully started
-    GatewayState gatewayState = context.getBean("gatewayState", GatewayState.class);
-    if (gatewayState.isReadyForMessages()) {
-      checkIfPortalDeployerEnabled();
-    } else {
-      applicationEventProxy.addApplicationListener(new ApplicationListener() {
-        @Override
-        public void onApplicationEvent(ApplicationEvent event) {
-          if (event instanceof ReadyForMessages) {
-            checkIfPortalDeployerEnabled();
-          }
-        }
-      });
+  /**
+   * Unloads portal deployer by calling destroy. 
+   */
+  public static synchronized void onModuleUnloaded() {
+    if (instance != null) {
+      logger.log(Level.INFO, "PortalDeployer Module load listener is shutting down");
+      try {
+        instance.destroy();
+      } catch (final Exception e) {
+        logger.log(Level.WARNING, String.format("PortalDeployer Module load listener threw exception on shutdown: %s" +
+                ExceptionUtils.getMessage(e), e));
+      } finally {
+        instance = null;
+      }
     }
-
   }
 
   private static PortalDeployerModuelLoadListener instance = null;
-  private static ClusterPropertyManager clusterPropertyManager;
-  private static ApplicationEventProxy applicationEventProxy;
-  private final PortalDeployerClientManager portalDeployerClientManager;
+  private ClusterPropertyManager clusterPropertyManager;
+  private ApplicationEventProxy applicationEventProxy;
+  private PortalDeployerClientManager portalDeployerClientManager;
+  private PlatformTransactionManager transactionManager;
+  private GatewayState gatewayState;
+  private ExecutorService executorService;
 
-  PortalDeployerModuelLoadListener(final ApplicationContext context) {
+  protected enum PortalDeployerStatus {STARTED, STOPPED, START_FAILED, STOP_FAILED}
+
+  PortalDeployerModuelLoadListener(final ApplicationContext context,
+                                   final PortalDeployerClientManager portalDeployerClientManager,
+                                   final ExecutorService executorService) {
     clusterPropertyManager = context.getBean("clusterPropertyManager", ClusterPropertyManager.class);
     applicationEventProxy = context.getBean("applicationEventProxy", ApplicationEventProxy.class);
+    transactionManager = context.getBean("transactionManager", PlatformTransactionManager.class);
+    gatewayState = context.getBean("gatewayState", GatewayState.class);
     applicationEventProxy.addApplicationListener(this);
-    portalDeployerClientManager = new PortalDeployerClientManagerImpl(context);
+    this.portalDeployerClientManager = portalDeployerClientManager;
+    this.executorService = executorService;
+  }
+
+  void checkIfPortalDeployerEnabledOnceGatewayReady() {
+    //only try and start portal deployer if gateway is fully started
+    if (gatewayState.isReadyForMessages()) {
+      checkIfPortalDeployerEnabled();
+    } else {
+      applicationEventProxy.addApplicationListener(event -> {
+        if (event instanceof ReadyForMessages) {
+          checkIfPortalDeployerEnabled();
+        }
+      });
+    }
   }
 
   /**
@@ -92,10 +131,22 @@ public class PortalDeployerModuelLoadListener implements ApplicationListener {
     }
   }
 
-  private static void checkIfPortalDeployerEnabled() {
+  /**
+   * Stop the Portal Deployer client and unhook it from the Gateway dependencies that may prevent garbage cleanup.
+   * @throws PortalDeployerClientException
+   * @throws PortalDeployerConfigurationException
+   */
+  void destroy() throws PortalDeployerClientException, PortalDeployerConfigurationException {
+    applicationEventProxy.removeApplicationListener(this);
+    portalDeployerClientManager.stop();
+    updatePortalDeployerStatus(PortalDeployerStatus.STOPPED);
+    executorService.shutdown();
+  }
+
+  private void checkIfPortalDeployerEnabled() {
     try {
       if (Boolean.parseBoolean(clusterPropertyManager.getProperty(PD_ENABLED_CP))) {
-        instance.handleUpdateOfPortalDeployerEnabledClusterProperty(Boolean.TRUE.toString());
+        handleUpdateOfPortalDeployerEnabledClusterProperty(Boolean.TRUE.toString());
       }
     } catch (FindException e) {
       logger.log(Level.INFO, String.format("Cluster property [%s] not found, PortalDeployer not enabled during " +
@@ -111,21 +162,47 @@ public class PortalDeployerModuelLoadListener implements ApplicationListener {
     }
   }
 
+  /**
+   * Stops the manager and sets portal.deployer.status as STARTED if successful or START_FAILED if not.
+   */
   private void startManager() {
     logger.log(Level.INFO, "Starting Portal Deployer MQTT Client");
     try {
       portalDeployerClientManager.start();
+      updatePortalDeployerStatus(PortalDeployerStatus.STARTED);
     } catch (PortalDeployerClientException | PortalDeployerConfigurationException e) {
       logger.log(Level.WARNING, "exception caught when starting portal deployer manager", e);
+      updatePortalDeployerStatus(PortalDeployerStatus.START_FAILED);
     }
   }
 
+  /**
+   * Stops the manager and sets portal.deployer.status as STOPPED if successful or STOP_FAILED if not.
+   */
   private void stopManager() {
     logger.log(Level.INFO, "Stopping Portal Deployer MQTT Client");
     try {
       portalDeployerClientManager.stop();
+      updatePortalDeployerStatus(PortalDeployerStatus.STOPPED);
     } catch (PortalDeployerClientException | PortalDeployerConfigurationException e) {
       logger.log(Level.WARNING, "exception caught when stopping portal deployer manager", e);
+      updatePortalDeployerStatus(PortalDeployerStatus.STOP_FAILED);
     }
+  }
+
+  private void updatePortalDeployerStatus(PortalDeployerStatus status) {
+    //execute update of cluster property in separate thread as the update doesn't succeed if the calling thread
+    //is already based on modificaiton of a cluster property i.e onApplicationEvent
+    executorService.execute(() -> new TransactionTemplate(transactionManager).execute(new TransactionCallbackWithoutResult() {
+      @Override
+      protected void doInTransactionWithoutResult(TransactionStatus transactionStatus) {
+        try {
+          clusterPropertyManager.putProperty(PD_STATUS_CP, status.name());
+        } catch (FindException | SaveException | UpdateException e) {
+          logger.log(Level.WARNING, String.format("unable to save Portal Deployer status cluster property " +
+                  "[name=%s," + "value=%s]", PD_STATUS_CP, status));
+        }
+      }
+    }));
   }
 }
---
+++ modules/gateway/assertions/PortalDeployerAssertion/src/test/java/com/l7tech/external/assertions/portaldeployer/server/PortalDeployerModuleLoadListenerTest.java
@@ -0,0 +1,239 @@
+package com.l7tech.external.assertions.portaldeployer.server;
+
+import static com.l7tech.external.assertions.portaldeployer.server.PortalDeployerClientConfigurationManagerImpl
+        .PD_ENABLED_CP;
+import static com.l7tech.external.assertions.portaldeployer.server.PortalDeployerModuelLoadListener.PD_STATUS_CP;
+import static com.l7tech.external.assertions.portaldeployer.server.PortalDeployerModuelLoadListener
+        .PortalDeployerStatus.*;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
+import com.l7tech.gateway.common.cluster.ClusterProperty;
+import com.l7tech.server.GatewayState;
+import com.l7tech.server.cluster.ClusterPropertyManager;
+import com.l7tech.server.event.admin.Created;
+import com.l7tech.server.event.admin.Updated;
+import com.l7tech.server.util.ApplicationEventProxy;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import org.jetbrains.annotations.NotNull;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+
+/**
+ * Created by BAGJO04 on 2017-10-31.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class PortalDeployerModuleLoadListenerTest {
+
+  @Mock
+  private ApplicationContext context;
+  @Mock
+  private ApplicationEventProxy applicationEventProxy;
+  @Mock
+  private PortalDeployerClientManager portalDeployerClientManager;
+  @Mock
+  private PlatformTransactionManager transactionManager;
+  @Mock
+  private ClusterPropertyManager clusterPropertyManager;
+  @Mock
+  private GatewayState gatewayState;
+
+  private ExecutorService executorService;
+  private PortalDeployerModuelLoadListener portalDeployerModuelLoadListener;
+
+  private boolean shutdownCalled = false;
+
+  @Before
+  public void setup() {
+    when(context.getBean("clusterPropertyManager", ClusterPropertyManager.class)).thenReturn(clusterPropertyManager);
+    when(context.getBean("applicationEventProxy", ApplicationEventProxy.class)).thenReturn(applicationEventProxy);
+    when(context.getBean("transactionManager", PlatformTransactionManager.class)).thenReturn(transactionManager);
+    when(context.getBean("transactionManager", PlatformTransactionManager.class)).thenReturn(transactionManager);
+    when(context.getBean("gatewayState", GatewayState.class)).thenReturn(gatewayState);
+    executorService = new SynchronousExecutorService();
+
+    portalDeployerModuelLoadListener = new PortalDeployerModuelLoadListener(context, portalDeployerClientManager, executorService);
+  }
+
+  @Test
+  public void test_destroy() throws Exception {
+    portalDeployerModuelLoadListener = new PortalDeployerModuelLoadListener(context, portalDeployerClientManager, executorService);
+    portalDeployerModuelLoadListener.destroy();
+
+    verify(applicationEventProxy, times(1)).removeApplicationListener(any(PortalDeployerModuelLoadListener.class));
+    verify(clusterPropertyManager, times(1)).putProperty(PD_STATUS_CP, STOPPED.name());
+    verify(portalDeployerClientManager, times(1)).stop();
+    assertTrue(shutdownCalled);
+  }
+
+  @Test
+  public void test_loadModule_gatewayReady_portalDeployerStarted() throws Exception {
+    when(gatewayState.isReadyForMessages()).thenReturn(true);
+    when(clusterPropertyManager.getProperty(PD_ENABLED_CP)).thenReturn("true");
+    when(transactionManager.getTransaction(any(TransactionDefinition.class))).thenReturn(mock(TransactionStatus.class));
+    portalDeployerModuelLoadListener.checkIfPortalDeployerEnabledOnceGatewayReady();
+
+    verify(portalDeployerClientManager, times(1)).start();
+    verify(clusterPropertyManager, times(1)).putProperty(PD_STATUS_CP, STARTED.name());
+  }
+
+  @Test
+  public void test_loadModule_gatewayNotReady_portalDeployerNotStarted() throws Exception {
+    when(gatewayState.isReadyForMessages()).thenReturn(false);
+    when(clusterPropertyManager.getProperty(PD_ENABLED_CP)).thenReturn("false");
+    portalDeployerModuelLoadListener.checkIfPortalDeployerEnabledOnceGatewayReady();
+
+    verify(portalDeployerClientManager, never()).start();
+    verify(clusterPropertyManager, never()).putProperty(PD_STATUS_CP, STARTED.name());
+  }
+
+  @Test
+  public void test_onApplicationEvent_portalDeployerEnabled_createdCpTrue() throws Exception {
+    Created cpCreatedEvent = mock(Created.class);
+    when(cpCreatedEvent.getEntity()).thenReturn(new ClusterProperty(PD_ENABLED_CP, "true"));
+    portalDeployerModuelLoadListener.onApplicationEvent(cpCreatedEvent);
+
+    verify(portalDeployerClientManager, times(1)).start();
+    verify(clusterPropertyManager, times(1)).putProperty(PD_STATUS_CP, STARTED.name());
+  }
+
+  @Test
+  public void test_onApplicationEvent_portalDeployerDisabled_createdCpFalse() throws Exception {
+    Created cpCreatedEvent = mock(Created.class);
+    when(cpCreatedEvent.getEntity()).thenReturn(new ClusterProperty(PD_ENABLED_CP, "false"));
+    portalDeployerModuelLoadListener.onApplicationEvent(cpCreatedEvent);
+
+    verify(portalDeployerClientManager, times(1)).stop();
+    verify(clusterPropertyManager, times(1)).putProperty(PD_STATUS_CP, STOPPED.name());
+  }
+
+  @Test
+  public void test_onApplicationEvent_portalDeployerEnabled_updatedCpTrue() throws Exception {
+    Updated cpUpdatedEvent = mock(Updated.class);
+    when(cpUpdatedEvent.getEntity()).thenReturn(new ClusterProperty(PD_ENABLED_CP, "true"));
+    portalDeployerModuelLoadListener.onApplicationEvent(cpUpdatedEvent);
+
+    verify(portalDeployerClientManager, times(1)).start();
+    verify(clusterPropertyManager, times(1)).putProperty(PD_STATUS_CP, STARTED.name());
+  }
+
+  @Test
+  public void test_onApplicationEvent_portalDeployerDisabled_updatedCpFalse() throws Exception {
+    Updated cpUpdatedEvent = mock(Updated.class);
+    when(cpUpdatedEvent.getEntity()).thenReturn(new ClusterProperty(PD_ENABLED_CP, "false"));
+    portalDeployerModuelLoadListener.onApplicationEvent(cpUpdatedEvent);
+
+    verify(portalDeployerClientManager, times(1)).stop();
+    verify(clusterPropertyManager, times(1)).putProperty(PD_STATUS_CP, STOPPED.name());
+  }
+
+  @Test
+  public void test_onApplicationEvent_portalDeployerFailed_exceptionDuringStart() throws Exception {
+    Updated cpUpdatedEvent = mock(Updated.class);
+    when(cpUpdatedEvent.getEntity()).thenReturn(new ClusterProperty(PD_ENABLED_CP, "true"));
+    doThrow(mock(PortalDeployerConfigurationException.class)).when(portalDeployerClientManager).start();
+    portalDeployerModuelLoadListener.onApplicationEvent(cpUpdatedEvent);
+
+    verify(portalDeployerClientManager, times(1)).start();
+    verify(clusterPropertyManager, times(1)).putProperty(PD_STATUS_CP, START_FAILED.name());
+  }
+
+  @Test
+  public void test_onApplicationEvent_portalDeployerFailed_exceptionDuringStop() throws Exception {
+    Updated cpUpdatedEvent = mock(Updated.class);
+    when(cpUpdatedEvent.getEntity()).thenReturn(new ClusterProperty(PD_ENABLED_CP, "false"));
+    doThrow(mock(PortalDeployerConfigurationException.class)).when(portalDeployerClientManager).stop();
+    portalDeployerModuelLoadListener.onApplicationEvent(cpUpdatedEvent);
+
+    verify(portalDeployerClientManager, times(1)).stop();
+    verify(clusterPropertyManager, times(1)).putProperty(PD_STATUS_CP, STOP_FAILED.name());
+  }
+
+  class SynchronousExecutorService implements ExecutorService {
+
+    @Override
+    public void execute(@NotNull Runnable command) {
+      command.run();
+    }
+
+    @Override
+    public void shutdown() {
+      shutdownCalled = true;
+    }
+
+    @NotNull
+    @Override
+    public List<Runnable> shutdownNow() {
+      return null;
+    }
+
+    @Override
+    public boolean isShutdown() {
+      return false;
+    }
+
+    @Override
+    public boolean isTerminated() {
+      return false;
+    }
+
+    @Override
+    public boolean awaitTermination(long timeout, @NotNull TimeUnit unit) throws InterruptedException {
+      return false;
+    }
+
+    @NotNull
+    @Override
+    public <T> Future<T> submit(@NotNull Callable<T> task) {
+      return null;
+    }
+
+    @NotNull
+    @Override
+    public <T> Future<T> submit(@NotNull Runnable task, T result) {
+      return null;
+    }
+
+    @NotNull
+    @Override
+    public Future<?> submit(@NotNull Runnable task) {
+      return null;
+    }
+
+    @NotNull
+    @Override
+    public <T> List<Future<T>> invokeAll(@NotNull Collection<? extends Callable<T>> tasks) throws InterruptedException {
+      return null;
+    }
+
+    @NotNull
+    @Override
+    public <T> List<Future<T>> invokeAll(@NotNull Collection<? extends Callable<T>> tasks, long timeout, @NotNull TimeUnit unit) throws InterruptedException {
+      return null;
+    }
+
+    @NotNull
+    @Override
+    public <T> T invokeAny(@NotNull Collection<? extends Callable<T>> tasks) throws InterruptedException, ExecutionException {
+      return null;
+    }
+
+    @Override
+    public <T> T invokeAny(@NotNull Collection<? extends Callable<T>> tasks, long timeout, @NotNull TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+      return null;
+    }
+  }
+}
