b162c1f51678cd94b52569b41d50b8024ed2a026
Victor Kazakov <kazvictor@gmail.com>
2018-02-20 15:21:53 -0800

    Allowing extensions to be tagged for simple retrieval Adding a unit test for the AbstractRegistryImpl

--- modules/extension-api/src/main/java/com/ca/apim/gateway/extension/ExtensionAccess.java
+++ modules/extension-api/src/main/java/com/ca/apim/gateway/extension/ExtensionAccess.java
@@ -5,19 +5,30 @@ import java.util.Collection;
 
 /**
  * ExtensionAccess allows for retrieval of registered Extensions
+ *
  * @param <E> Extension
  */
-public interface ExtensionAccess <E extends Extension> {
+public interface ExtensionAccess<E extends Extension> {
     /**
      * Get the registered extension with the specified key
+     *
      * @param key the key used to register the extension
      * @return the Extension
      */
-     E getExtension(final String key);
+    E getExtension(final String key);
 
     /**
      * Get all extensions that are registered
+     *
      * @return an immutable collection of extensions registered
      */
     Collection<E> getAllExtensions();
+
+    /**
+     * Returns all extensions with the given tags
+     *
+     * @param tags The tags to return extensions with
+     * @return The extensions with the given tags. An empty collection if no extensions exist with the given tags
+     */
+    Collection<E> getTaggedExtensions(final String... tags);
 }
--- modules/gateway/server/src/main/java/com/l7tech/server/extension/registry/AbstractRegistryImpl.java
+++ modules/gateway/server/src/main/java/com/l7tech/server/extension/registry/AbstractRegistryImpl.java
@@ -4,36 +4,45 @@ import com.ca.apim.gateway.extension.Extension;
 import com.ca.apim.gateway.extension.ExtensionAccess;
 import com.ca.apim.gateway.extension.ExtensionRegistry;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Abstract implementation for ExtensionRegistry and ExtensionAccess
+ *
  * @param <E> Extension
  */
 public abstract class AbstractRegistryImpl<E extends Extension> implements ExtensionRegistry<E>, ExtensionAccess<E> {
 
     private final ConcurrentMap<String, E> extensionsMap;
+    private final ConcurrentMap<String, Collection<E>> tagExtensionMap;
 
     /**
      * Constructor
      */
     protected AbstractRegistryImpl() {
         extensionsMap = new ConcurrentHashMap<>();
+        tagExtensionMap = new ConcurrentHashMap<>();
     }
 
     /**
      * Get logger.  The logger to be used to log messages in this class
+     *
      * @return Logger
      */
     protected abstract Logger getLogger();
 
     /**
      * Get the registered extension
+     *
      * @param key the key used to register the extension
      * @return the extension if it is registered; null is returned if no such key is found
      */
@@ -49,7 +58,19 @@ public abstract class AbstractRegistryImpl<E extends Extension> implements Exten
     }
 
     /**
+     * Get all extensions registered with the given tags.
+     *
+     * @param tags The tags to return extensions with
+     * @return The extensions with the given tags. An empty collection if no extensions exist with the given tags
+     */
+    @Override
+    public Collection<E> getTaggedExtensions(final String... tags) {
+        return Arrays.stream(tags).flatMap(tag -> tagExtensionMap.getOrDefault(tag, Collections.emptySet()).stream()).collect(Collectors.toSet());
+    }
+
+    /**
      * Get all registered extensions
+     *
      * @return a Collection of registered extensions
      */
     @Override
@@ -60,15 +81,23 @@ public abstract class AbstractRegistryImpl<E extends Extension> implements Exten
     /**
      * Register the extension.  If the key is already registered in the registry, the new Extension provided will
      * overwrite the currently registered extension.
-     * @param key the key
+     *
+     * @param key       the key
      * @param extension the extension
+     * @param tags      A list of tags to tag the extension with
      */
     @Override
-    public void register(final String key, final E extension) {
+    public void register(final String key, final E extension, final String... tags) {
         E existingExtension = extensionsMap.put(key, extension);
 
+        for (final String tag : tags) {
+            tagExtensionMap.merge(tag, new HashSet<>(Collections.singleton(extension)), (n, o) ->
+                    Stream.concat(n.stream(), o.stream()).collect(Collectors.toSet()));
+        }
+
         if (existingExtension != null) {
-            getLogger().log(Level.WARNING, "Overwriting already registered extension with key {0}.");
+            tagExtensionMap.values().forEach(s -> s.remove(existingExtension));
+            getLogger().log(Level.WARNING, "Overwriting already registered extension with key {0}.", key);
         }
 
         getLogger().log(Level.INFO, "Registered extension {0} with key {1}",
@@ -80,7 +109,10 @@ public abstract class AbstractRegistryImpl<E extends Extension> implements Exten
      */
     @Override
     public void unregister(final String key) {
-        extensionsMap.remove(key);
+        final E extension = extensionsMap.remove(key);
+        if (extension != null) {
+            tagExtensionMap.values().forEach(s -> s.remove(extension));
+        }
         getLogger().log(Level.INFO, "Unregistered extension with key {0}", key);
     }
 }
