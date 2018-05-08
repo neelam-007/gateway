869ad6cb9be95904ec6229e3d346685042d5e44e
ychan <oyves.chan@ca.com>
2018-05-08 10:32:15 -0700

    DE288689: Adding patcher.properties as shipped logging file, and add location to check for sspc logs for error events

--- modules/gateway/processcontroller/src/main/java/com/l7tech/server/processcontroller/patching/client/PatchCli.java
+++ modules/gateway/processcontroller/src/main/java/com/l7tech/server/processcontroller/patching/client/PatchCli.java
@@ -100,7 +100,7 @@ public class PatchCli {
         }
 
         if (thrown != null) {
-            logger.log(Level.WARNING, errPrefix + ExceptionUtils.getMessage(thrown), thrown instanceof PatchException ? null : thrown);
+            logger.log(Level.WARNING, errPrefix + ExceptionUtils.getMessage(thrown), ExceptionUtils.getDebugException(thrown));
             System.out.println(errPrefix + ExceptionUtils.getMessage(thrown) + "\n");
             System.exit(PATCH_API_ERROR);
         }
@@ -131,7 +131,7 @@ public class PatchCli {
         // configure logging if the logs directory is found, else leave console output
         final File logsDir = new File("/opt/SecureSpan/Controller/var/logs");
         if ( logsDir.exists() && logsDir.canWrite() ) {
-            JdkLoggerConfigurator.configure("com.l7tech.server.processcontroller.patching.client", "com/l7tech/server/processcontroller/patching/client/resources/logging.properties", "etc/conf/patchinglogging.properties");
+            JdkLoggerConfigurator.configure("com.l7tech.server.processcontroller.patching.client", "com/l7tech/server/processcontroller/patching/client/resources/logging.properties", "etc/conf/patcher.properties");
         } else if ( !SyspropUtil.getBoolean( "com.l7tech.server.log.console", false ) ){
             final Logger rootLogger = Logger.getLogger( "" );
             for ( final Handler handler : rootLogger.getHandlers() ) {
