5ab20a1c8ba65cffdbc54b6611317b14e868aee2
Alle, Nithya <Nithya.Alle@ca.com>
2018-06-18 17:41:29 +0530

    US499126: removing JS assertion dir and adding remote module assertion dependency. (#859)

--- modules/gateway/assertions/JavaScriptAssertion/src/main/java/com/l7tech/external/assertions/js/features/ScriptMalformedUrlException.java
+++
@@ -1,8 +0,0 @@
-package com.l7tech.external.assertions.js.features;
-
-public class ScriptMalformedUrlException extends JavaScriptException {
-
-    public ScriptMalformedUrlException(final String message, final Throwable throwable) {
-        super(message, throwable);
-    }
-}
